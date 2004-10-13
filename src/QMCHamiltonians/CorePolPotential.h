e3fd14bd52f07ef872607a399a0b44c49bcf6c40
qmc <qmc>
2004-10-13 17:14:39 +0000

    Fixed bugs, added local term
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@84 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCHamiltonians/CorePolPotential.h
+++ src/QMCHamiltonians/CorePolPotential.h
@@ -24,269 +24,217 @@
 
 namespace ohmmsqmc {
 
-  struct CPPelel: public QMCHamiltonianBase {
-    int Centers;
+  /**
+     The effective core polarization
+     \f[
+     V_{CPP} = -\frac{1}{2}\sum_{C}\alpha_C {\bf f}_C \cdot {\bf f}_C
+     \f]
+     Electric field which acts on core \f$C\f$ due to the charges of 
+     valence electrons \f$i\f$ and the other cores \f$C'\f$
+     \f[
+     {\bf f}_C = \sum_i \frac{{\bf r}_{Ci}}{r_{Ci}^3}C(r_{Ci},\rho_C)
+     -\sum_{C' \ne C} \frac{{\bf R}_{CC'}}{R_{CC'}^3}Z_{C'} = 
+     {\bf f}_C^e + {\bf f}_C^n
+     \f]
+     \f[
+     {\bf r}_{Ci} = {\bf r}_i - {\bf r}_C
+     \f]
+     \f[
+     {\bf R}_{CC'} = {\bf R}_{C'} - {\bf R}_{C}
+     \f]
+
+     \f$ C(r_{Ci},\rho_C) \f$ is a cut-off function for \f$ {\bf f}_C^e \f$ 
+     with an adjustable parameter \f$ \rho_C. \f$
+
+     \f[
+     V_{CPP} = -\frac{1}{2}\sum_C \left\{ \:\:\: \sum_i 
+     \frac{1}{r_{Ci}^4}C^2(r_{Ci},\rho_C) + 
+     \sum_{i \ne j} \frac{{\bf r}_{Ci} \cdot {\bf r}_{Ci}}{r^3_{Ci}r^3_{Ci}}
+     C(r_{Ci},\rho_C) C^2(r_{Cj},\rho_C) \right. 
+     \f]
+     \f[ 
+     -2 \left. \sum_i \sum_{C' \ne C} \frac{{\bf r}_{Ci} \cdot 
+     {\bf R}_{CC'}}{r^3_{Ci}R^3_{CC'}} Z_{C'}C(r_{Ci},\rho_C) 
+     + \left| \sum_{C' \ne C}  \frac{{\bf R}_{CC'}}{R^3_{CC'}} Z_{C'} 
+     \right|^2 \:\:\: \right\}
+     \f]
+  */
+  struct CorePolPotential: public QMCHamiltonianBase {
+    ///the number of ions 
+    int nCenters;
+    ///the number of electrons
+    int nParticles;
     RealType alpha, r_b, r_binv;
+    RealType eCoreCore;
     RealType C;
-    DistanceTableData* d_table;
+    ///the ion-electron DistanceTable
+    DistanceTableData* d_ie;
+    ///the ion-ion DistanceTable
+    DistanceTableData* d_ii;
+    ///CoreCoef(C) = 1.0 if C=Ge, 0.0 for all other ions
     vector<bool> CoreCoef;
-    Matrix<PosType> CoreCoreDipole, ElCoreDipole;
+    ///CoreCoreDipole(C,C') \f$= \frac{Z_{C'} {\bf R_{CC'}}}{R_{CC'}^3}\f$
+    Matrix<PosType> CoreCoreDipole;
+    ///ElCoreDipole(C,i) \f$= \frac{{\bf r_{Ci}}f({\bar{r_{bCi}}}{r_{Ci}^3}\f$
+    Matrix<PosType> ElCoreDipole;
 
-    //index for attribute charge
-    int iz = ions.Species.addAttribute("charge");
-
-    CPPelel(ParticleSet& ions, ParticleSet& els): 
-      d_table(NULL) { 
+    ///constructor
+    CorePolPotential(ParticleSet& ions, ParticleSet& els): 
+      d_ie(NULL), d_ii(NULL), alpha(0.3558), r_b(0.7048),
+      eCoreCore(0.0) { 
       
-      d_table = DistanceTable::getTable(DistanceTable::add(ions,els));
-      d_ii;
-      Centers = ions.getTotalNum();
-      alpha = 0.3558;
+      //set the distance tables
+      d_ie = DistanceTable::getTable(DistanceTable::add(ions,els));
+      d_ii = DistanceTable::getTable(DistanceTable::add(ions,ions));
+      nCenters = ions.getTotalNum();
+      nParticles = els.getTotalNum();
       C = -0.5*alpha;
-      r_b = 0.7048;
       r_binv = 1.0/r_b;
 
-      CoreCoef.resize(Centers);
-      CoreCoreDipole.resize(Centers,Centers);
-      ElCoreDipole.reisze(Centers,els.getTotalNum());
+      CoreCoef.resize(nCenters);
+      CoreCoreDipole.resize(nCenters,nCenters);
+      ElCoreDipole.resize(nCenters,d_ie->getTotNadj());
 
-      for(int i=0; i<Centers; i++){
-	string sname = ions.Species.speciesName[ions.GroupID[i]];
+      CoreCoreDipole = 0.0;
+      ElCoreDipole = 0.0;
+
+      //only calculate the cpp for Ge atoms
+      for(int iat=0; iat<nCenters; iat++){
+	string sname = ions.Species.speciesName[ions.GroupID[iat]];
 	if(sname == "Ge"){
-	  cout << "Adding a core-electron potential for " << sname << endl;
-	  CoreCoef[i] = true;
+	  //	  LOGMSG("Adding a core-electron potential for " << sname)
+	  CoreCoef[iat] = true;
 	}
-	else CoreCoef[i] = false;
+	else CoreCoef[iat] = false;
       }
 
+      //index for attribute charge
+      int iz = ions.Species.addAttribute("charge");
+
+      //calculate the Core-Core Dipole matrix
       int nn=0;
-      for(int i=0; i<Centers; i++) {
-	for(int j=i+1;j<Centers; j++, nn++) {
-	  RealType rinv3 = pow(d_ii->rinv(nn),3);
-	  PosType dipole = rinv3*d_ii->dr(nn);
-	  CoreCoreDipole(i,j) = dipole*ions.Species(iz,ions.GroupID[j]);//charge of j
-	  CoreCoreDipole(j,i) = dipole*ions.Species(iz,ions.GroupID[i]);//charge of i
+      for(int iat=0; iat<nCenters; iat++) {
+	for(int jat=iat+1;jat<nCenters; jat++, nn++) {
+	  //check to see if both ions are Ge
+	  if(CoreCoef[iat]*CoreCoef[jat]){
+	    RealType rinv3 = pow(d_ii->rinv(nn),3);//(1/R^3)
+	    PosType dipole = rinv3*d_ii->dr(nn);//(\vec{R}/R^3)
+	    CoreCoreDipole(iat,jat) = dipole*ions.Species(iz,ions.GroupID[jat]);//charge of jat
+	    CoreCoreDipole(jat,iat) = dipole*ions.Species(iz,ions.GroupID[iat]);//charge of iat
+	  }
 	}
       }
+
+      //calculate the core-core term (constant)
+      nn = 0;
+      for(int iat=0; iat<nCenters; iat++) {
+	for(int jat=iat+1;jat<nCenters; jat++, nn++) {
+	  eCoreCore += dot(CoreCoreDipole(iat,jat),CoreCoreDipole(iat,jat));
+	  /////
+	  eCoreCore += dot(CoreCoreDipole(jat,iat),CoreCoreDipole(jat,iat));
+	  /////
+	}
+      }
+
     }
     
-    ~CPPelel() { }
+    ~CorePolPotential() { }
 
     inline ValueType evaluate(ParticleSet& P) {
-
       RealType esum=0.0;
-      int nn = P.getTotalNum();
-      vector<RealType> fc(nn),rinv2(nn);
-      vector<PosType> dr(nn);
-      int nnoff = 0;
-      for(int iat=0; iat<Centers; iat++) {
-	if(CoreCoef[iat]) {
-	  for(int j=0; j<nn; j++, nnoff++) {
-	    dr[j] = d_table->dr(nnoff);
-	    rinv2[j] = pow(d_table->rinv(nnoff),2); // 1/r^2
-	    fc[j] = fcpp(d_table->r(nnoff)*r_binv); //*rinv2[nnj]*d_table->rinv(j); // f(r)/r^3
-	    enum += pow(rinv2[j],2)*fc[j];
-	  }
 
-	  for(int j=0; j<nn; j++) {
-            for(int k=j+1; k<nn; k++) {
-              esum += dot(dr[j],dr[k])*rinv2[j]*rinv2[k]*fc[j]*fc[k];
-	    }
-	  }
-	  for(int j=nnoff, nnj=0; nnj<nn-1; nnj++,j++) {
-	    esum += CoreCoef[iat]*fc[nnj]*fc[nnj]*rinv2[nnj];
-	    for(int k=j+1,nnk=nnj+1; nnk<nn; nnk++,k++) {
-	      esum += fc[nnj]*fc[nnk]*dot(d_table->dr(j),d_table->dr(k));
-	    }
-	  }
-	  for(int jat=iat+1; jat<Centers; jat++) {
-	    int i=nnoff;
-	    int j=d_table->M[jat];
-	    for(int k=0; nnj=0; nni=0; k<nn; j++; i++; nni++; nnj++; k++){
-	      esum += dot(CoreCoreDipole(iat,jat),d_table->dr(j))*fc[nnj];
-	      esum += dot(CoreCoreDipole(jat,iat),d_table->dr(i))*fc[nni];
+      //calculate the Electron-Core Dipole matrix
+      int nn=0;
+      for(int iat=0; iat<nCenters; iat++) {
+	if(CoreCoef[iat]){
+	  for(int nn=d_ie->M[iat]; nn<d_ie->M[iat+1]; nn++){
+	    RealType rinv3 = pow(d_ie->rinv(nn),3);//(1/r^3)
+	    PosType dipole = rinv3*d_ie->dr(nn);//(\vec{r}/r^3)
+	    ElCoreDipole(iat,nn) = dipole*fcpp(d_ie->r(nn)*r_binv);
 	  }
 	}
+      }
+
+      //now loop over the ions
+      for(int iat=0; iat<nCenters; iat++) {
+	//loop over the electrons
+	for(int nn=d_ie->M[iat]; nn<d_ie->M[iat+1]; nn++)
+	  esum += dot(ElCoreDipole(iat,nn),ElCoreDipole(iat,nn));
+	
+	//loop over distinct pairs of electrons
+	for(int nnj=d_ie->M[iat]; nnj<d_ie->M[iat+1]; nnj++){
+	  for(int nnk=nnj+1; nnk<d_ie->M[iat+1]; nnk++)
+	    esum += 2.0*dot(ElCoreDipole(iat,nnj),ElCoreDipole(iat,nnk));
+	  //////
+	  //  esum += dot(ElCoreDipole(iat,nnj),ElCoreDipole(iat,nnk));
+	  //////
 	}
-      } //iat
-
-
-      return C*esum;
+	
+	//loop over ions and electrons 
+	for(int jat=iat+1; jat<nCenters; jat++) {
+	  int nni = d_ie->M[iat];
+	  int nnj = d_ie->M[jat];
+	  for(int k=0; k<nParticles; k++, nni++, nnj++){
+	    esum -= 2.0*dot(CoreCoreDipole(iat,jat),ElCoreDipole(iat,nni));
+	    esum -= 2.0*dot(CoreCoreDipole(jat,iat),ElCoreDipole(jat,nnj));
+	  }
+	}
+      }//iat
+      return C*(esum+eCoreCore);
     }
 
 
     inline ValueType 
     evaluate(ParticleSet& P, RealType& x) {
       RealType esum=0.0;
-      int nn = P.getTotalNum();
-      vector<RealType> fc(nn),rinv2(nn);
-      for(int iat=0; iat<Centers; iat++) {
-	int nnoff = d_table->M[iat];
-	for(int j=nnoff, nnj=0; nnj<nn; j++, nnj++) {
-	  rinv2[nnj] = pow(d_table->rinv(j),2);
-	  fc[nnj] = fcpp(d_table->r(j)*r_binv)*rinv2[nnj]*d_table->rinv(j);
-	}
-	for(int j=nnoff, nnj=0; nnj<nn-1; nnj++,j++) {
-	  esum += CoreCoef[iat]*fc[nnj]*fc[nnj]*rinv2[nnj];
-	  for(int k=j+1,nnk=nnj+1; nnk<nn; nnk++,k++) {
-            esum += CoreCoef[iat]*fc[nnj]*fc[nnk]*dot(d_table->dr(j),d_table->dr(k));
+
+      //calculate the Electron-Core Dipole matrix
+      int nn=0;
+      for(int iat=0; iat<nCenters; iat++) {
+	if(CoreCoef[iat]){
+	  for(int nn=d_ie->M[iat]; nn<d_ie->M[iat+1]; nn++){
+	    RealType rinv3 = pow(d_ie->rinv(nn),3);//(1/r^3)
+	    PosType dipole = rinv3*d_ie->dr(nn);//(\vec{r}/r^3)
+	    ElCoreDipole(iat,nn) = dipole*fcpp(d_ie->r(nn)*r_binv);
 	  }
 	}
       }
 
-      x = C*esum;
-      return x;
+      //now loop over the ions
+      for(int iat=0; iat<nCenters; iat++) {
+	//loop over the electrons
+	for(int nn=d_ie->M[iat]; nn<d_ie->M[iat+1]; nn++)
+	  esum += dot(ElCoreDipole(iat,nn),ElCoreDipole(iat,nn));
+	
+	//loop over distinct pairs of electrons
+	for(int nnj=d_ie->M[iat]; nnj<d_ie->M[iat+1]; nnj++){
+	  for(int nnk=nnj+1; nnk<d_ie->M[iat+1]; nnk++)
+	    esum += dot(ElCoreDipole(iat,nnj),ElCoreDipole(iat,nnk));
+	}
+	
+	//loop over ions and electrons 
+	for(int jat=iat+1; jat<nCenters; jat++) {
+	  int nni = d_ie->M[iat];
+	  int nnj = d_ie->M[jat];
+	  for(int k=0; k<nParticles; k++, nni++, nnj++){
+	    esum -= 2.0*dot(CoreCoreDipole(iat,jat),ElCoreDipole(iat,nni));
+	    esum -= 2.0*dot(CoreCoreDipole(jat,iat),ElCoreDipole(iat,nnj));
+	  }
+	}
+      }//iat
+      x = C*(esum+eCoreCore);
+      return x;     
     }
 
     inline RealType fcpp(RealType z) {
       return pow((1.0-exp(-1.0*z*z)),2.0);
     }
 
+    void evaluate(WalkerSetRef& W, ValueVectorType& LE){}
 
-#ifdef WALKERSTORAGE_COLUMNMAJOR
-    inline void 
-    evaluate(WalkerSetRef& P, ValueVectorType& LE) {
-      ValueVectorType e(P.walkers());
-      for(int iw=0; iw<P.walkers(); iw++) {
-	e=0.0;
-	for(int iat=0; iat<Centers; iat++) {
-	  for(int nn=d_table->M[iat]; nn<d_table->M[iat+1]; nn++) {
-	    e[iw] += d_table->rinv(iw,nn);
-	  }
-	}
-      }
-      for(int iw=0; iw<P.walkers(); iw++) { LE[iw] += e[iw];} 
-    }
-#else
-    inline void 
-    evaluate(WalkerSetRef& P, ValueVectorType& LE) {
-      for(int iw=0; iw<P.walkers(); iw++) {
-	RealType e=0.0;
-	for(int iat=0; iat<Centers; iat++) {
-	  RealType esub = 0.0;
-	  for(int nn=d_table->M[iat]; nn<d_table->M[iat+1]; nn++) {
-	    esub += d_table->rinv(iw,nn);
-	  }
-	  e += esub; ///multiply z
-	}
-	LE[iw] += e;
-      }
-    }
-#endif
   };
 }
 #endif
-
-
-//  struct CPPcorecore: public QMCHamiltonianBase {
-
-//    RealType alpha;
-//    RealType C;
-//    DistanceTableData* d_table;
-    
-//    CPPcorecore(ParticleSet& ref): d_sum(0.0) { 
-
-//      alpha = 0.3558; 
-//      C = -0.5*alpha;
-
-//      IndexType iii = DistanceTable::add(ref);
-//      DistanceTableData* d_ii = DistanceTable::getTable(iii);
-//      d_ii->create(1);
-     
-//      int charge = ref.Species.addAttribute("charge");
-//      int nat = ref.getTotalNum();
-//      vector<RealType> Z(nat);
-//      for(int iat=0; iat<nat;iat++) {
-//        Z[iat] = ref.Species(charge,ref.GroupID[iat]);
-//      }
-//      d_ii->evaluate(ref);
-//      d_sum = 0.0;
-//      for(int i=0; i< nat; i++) {
-//        PosType fcc = 0.0;
-//        for(int nn=d_ii->M[i]; nn<d_ii->M[i+1]; nn++) {
-//           fcc += Z[ref.GroupID[d_ii->J[nn]]]*pow(d_ii->rinv(nn),3.0)*d_ii->dr(nn);
-// 	}
-//         d_sum += C*dot(fcc,fcc);
-//       }
-//       LOGMSG("Energy of ion-ion interaction " << d_sum)
-//    }
-   
-//    ~CPPcorecore() { }
-
-//    inline ValueType evaluate(ParticleSet& P) { return d_sum; }
-
-//    inline ValueType evaluate(ParticleSet& P, RealType& x) {
-//      ///only for consistency
-//      return x=d_sum;
-//    }
-   
-//  };
-
-//  struct CPPelcore: public QMCHamiltonianBase {
-//    RealType alpha;
-//    int Centers;
-//    vector<RealType> Z;
-//    DistanceTableData* d_table;
-//    DistanceTableData* d_ii;
-    
-//    CPPelcore(ParticleSet& ions, ParticleSet& els): d_table(NULL) { 
-     
-//      alpha = 0.3558; 
-//      IndexType iii = DistanceTable::add(ions);
-//      DistanceTableData* d_ii = DistanceTable::getTable(iii);
-//      d_ii->create(1);
-
-//      d_table = DistanceTable::getTable(DistanceTable::add(ions,els));
-//      int iz = ions.Species.addAttribute("charge");
-//      Centers = ions.getTotalNum();
-//      Z.resize(Centers);
-     
-//      int charge = ref.Species.addAttribute("charge");
-//      int nat = ref.getTotalNum();
-//      vector<RealType> Z(nat);
-//      vector<PosType> fcc(nat);
-//      for(int iat=0; iat<nat;iat++) {
-//        Z[iat] = ref.Species(charge,ref.GroupID[iat]);
-//      }
-//      d_ii->evaluate(ref);
-//      d_sum = 0.0;
-//      for(int i=0; i<nat; i++) {
-//        for(int nn=d_ii->M[i]; nn<d_ii->M[i+1]; nn++) {
-//           fcc[i] += Z[ref.GroupID[d_ii->J[nn]]]*pow(d_ii->rinv(nn),3.0)*d_ii->dr(nn);
-// 	}
-//       }
-//    }
-   
-//    ~CPPelcore() { }
-
-//    inline ValueType evaluate(ParticleSet& P) {
-
-//      d_ie->evaluate(P);
-//      for(int iat=0; iat<Centers; iat++) {
-//        int eln = 0;
-// 	for(int nn=d_ie->M[i]; nn<d_ie->M[iat+1]; nn++; eln++) {
-// 	  esum+=alpha*fcc[iat]*pow(d_ie->rinv[nn],3.0)*dot(ions.R[iat],P.R[eln]);
-// 	}
-//      }
-//      return esum;
-//    }
-
-//  inline ValueType evaluate(ParticleSet& P, RealType& x) {
-
-//      d_ie->evaluate(P);
-//      for(int iat=0; iat<Centers; iat++) {
-//        int eln = 0;
-// 	for(int nn=d_ie->M[i]; nn<d_ie->M[iat+1]; nn++; eln++) {
-// 	  esum+=alpha*fcc[iat]*pow(d_ie->rinv[nn],3.0)*dot(ions.R[iat],P.R[eln]);
-// 	}
-//      }
-//      x = esum;
-//      return esum;
-//    }
-
-//  };
-
-
 
 /***************************************************************************
  * $RCSfile$   $Author$
