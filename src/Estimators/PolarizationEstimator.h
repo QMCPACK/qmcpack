05f98e82e84da9d4c5873890a0ca44c66d102747
Dyutiman Das <ddas>
2004-10-21 21:48:15 +0000

    *** empty log message ***
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@94 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Estimators/PolarizationEstimator.h
@@ -0,0 +1,73 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003- by Jeongnim Kim 
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_POLARIZATIONESTIMATOR_H
+#define OHMMS_QMC_POLARIZATIONESTIMATOR_H
+#include <fstream>
+#include "OhmmsData/libxmldefs.h"
+#include "Estimators/ScalarEstimatorBase.h"
+
+namespace ohmmsqmc {
+
+  template<class T>
+  class PolarizationEstimator: public ScalarEstimatorBase<T> {
+
+    ///local data
+    T z_sum, z_sq_sum;
+    int pindex_0, pindex_1;
+  public:
+
+    typedef typename ScalarEstimatorBase<T>::Walker_t Walker_t;
+  
+    PolarizationEstimator():z_sum(0.0), 
+			    z_sq_sum(0.0) { }
+
+    void add2Record(RecordNamedProperty<T>& record) {
+      pindex_0 = record.add("Pol-z");
+      pindex_1 = record.add("Pol-z-var");
+    }
+
+    inline void accumulate(const Walker_t& awalker, T wgt) {
+      for(int i=0; i<awalker.size(); i++) {
+	T z = awalker.R[i][2];
+	z_sum += wgt*z;
+	z_sq_sum += wgt*z*z;
+      }
+    }
+
+    ///reset all the cumulative sums to zero
+    inline void reset() { 
+      z_sum=T();z_sq_sum=T();
+    }
+
+    inline void report(RecordNamedProperty<T>& record, T wgtinv) {
+      b_average =  z_sum*wgtinv;
+      b_variance = z_sq_sum*wgtinv-b_average*b_average;
+      record[pindex_0]=b_average;
+      record[pindex_1]=b_variance;
+      z_sum = T(); z_sq_sum = T(); 
+    }
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
