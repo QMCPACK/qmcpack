d094356df1fcf553cea7967d804b51613ddb054b
Jeongnim Kim <jeongnim.kim@intel.com>
2004-10-21 22:32:18 +0000

    Interfaces to global sums.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@101 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Message/CommOperators.h
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 1998-2002 by Jeongnim Kim
+//
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_COMMUNICATION_OPERATORS_H
+#define OHMMS_COMMUNICATION_OPERATORS_H
+#include "Message/Communicate.h"
+
+template<class T> void gsum(T&, int) { }
+
+#ifdef HAVE_MPI
+template<>
+inline void gsum(double& g, int gid) {
+  double gt = g;
+  MPI_Allreduce(&(gt), &(g), 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+}
+
+template<unsigned N>
+inline void gsum(TinyVector<double,N>& g, int gid) {
+  //TinyVector<double,N> gt = g;
+  //MPI_Allreduce(gt.begin(), g.begin(), N, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  TinyVector<double,N> gt;
+  MPI_Allreduce(g.begin(), gt.begin(), N, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  gt = g;
+}
+
+template<>
+inline void gsum(std::vector<double>& g, int gid) {
+  std::vector<double> gt(g.size(), 0.0);
+  MPI_Allreduce(&(g[0]),&(gt[0]),g.size(),MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  g = gt;
+}
+
+#endif
+
+#endif
+
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
