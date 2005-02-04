804f9c520be8442c88d35832ee243cdb28f3aa50
Jeongnim Kim <jeongnim.kim@intel.com>
2005-02-04 21:25:13 +0000

    Stand-alone applications to process output files from external packages based on Gaussian orbitals. - gto2grid.cpp converts Gaussian Orbitals to radial grid functors: use xml description. - parse.cpp converts Gaussian outputs to an input for qmcPlusPlus. - Currently, only CasinoParser is valid. - Future usage of parse.cpp   parse -p (casino|gaussian98|crystal|...) -f input_file -o output_file
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@182 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCTools/parse.cpp
@@ -0,0 +1,86 @@
+#include "Tools/CasinoParser.h"
+#include "Message/Communicate.h"
+#include "Utilities/OhmmsInfo.h"
+
+int main(int argc, char **argv) {
+
+  if(argc<2) {
+    std::cout << "Usage: casinon <filename> " << std::endl;
+    return 1;
+  }
+
+  OHMMS::Controller->initialize(argc,argv);
+
+  OhmmsInfo welcome(argc,argv,OHMMS::Controller->mycontext());
+
+  std::cout.setf(std::ios::scientific, std::ios::floatfield);
+  std::cout.setf(std::ios::right,std::ios::adjustfield);
+  std::cout.precision(12);
+
+  QMCGaussianParserBase::init();
+
+  CasinoParser parser;
+  parser.parse(argv[1]);
+
+  //parser.search(fin, "GEOMETRY");
+  //parser.getGeometry(fin);
+  //parser.search(fin, "BASIS");
+  //parser.getGaussianCenters(fin);
+  //parser.search(fin, "MULTIDETERMINANT");
+  //parser.search(fin, "EIGENVECTOR");
+
+  xmlDocPtr doc = xmlNewDoc((const xmlChar*)"1.0");
+  xmlNodePtr wf_root = xmlNewNode(NULL, BAD_CAST "determinantset");
+  xmlNewProp(wf_root,(const xmlChar*)"type",(const xmlChar*)"MolecularOrbital");
+
+  xmlNodePtr bset = parser.createBasisSet();
+  xmlAddChild(wf_root,bset);
+
+  xmlNodePtr slaterdet = parser.createDeterminantSet();
+  xmlAddChild(wf_root,slaterdet);
+
+  xmlDocSetRootElement(doc, wf_root);
+
+  xmlXPathContextPtr m_context = xmlXPathNewContext(doc);
+  xmlXPathObjectPtr result
+    = xmlXPathEvalExpression((const xmlChar*)"//basis",m_context);
+  if(!xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+    for(int ic=0; ic<result->nodesetval->nodeNr; ic++) {
+      xmlNodePtr cur = result->nodesetval->nodeTab[ic];
+      parser.map2GridFunctors(cur);
+    }
+  }
+  xmlXPathFreeObject(result);
+
+  xmlSaveFormatFile("test.xml",doc,1);
+  xmlFreeDoc(doc);
+  return 0;
+}
+
+/*
+int main(int argc, char **argv) {
+
+  char buffer[200];
+  std::string _txt;
+  std::string _data;
+  double _t;
+  std::cout.setf(std::ios::scientific, std::ios::floatfield);
+  std::cout.setf(std::ios::right,std::ios::adjustfield);
+  std::cout.precision(12);
+  while(std::cin.getline( buffer, sizeof ( buffer ) ) ){
+    std::istringstream stream(buffer);
+    if(isdigit(buffer[1])) {
+      while(stream>>_t) {
+        std::cout << std::setw(21) << _t ;
+      }
+      std::cout << std::endl;
+    } else {
+      if(stream>>_t) {
+        std::cout << "probably numbers " << buffer << std::endl;
+      } else {
+        std::cout << "probably statement " << buffer << std::endl;
+      }
+    }
+  }
+}
+*/
