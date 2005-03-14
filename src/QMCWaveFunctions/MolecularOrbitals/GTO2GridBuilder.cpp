eee0e51b7b9a5b40e790db536f4dd020f2c243d6
Jeongnim Kim <jeongnim.kim@intel.com>
2005-03-14 21:14:22 +0000

    GaussianBasisSet is taken out to Numerics.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@243 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/MolecularOrbitals/GTO2GridBuilder.cpp
+++ src/QMCWaveFunctions/MolecularOrbitals/GTO2GridBuilder.cpp
@@ -18,83 +18,26 @@
 #include "Numerics/Transform2GridFunctor.h"
 #include "Numerics/OneDimCubicSpline.h"
 #include "Numerics/HDFNumericAttrib.h"
+#include "Numerics/GaussianBasisSet.h"
 #include "QMCWaveFunctions/MolecularOrbitals/GridMolecularOrbitals.h"
 #include "QMCWaveFunctions/MolecularOrbitals/GTO2GridBuilder.h"
 
 namespace ohmmsqmc {
 
-  int DFactorial(int l) {
-    return (l==1)? 1: DFactorial(l-2);
-  }
-
-  template<class T>
-  inline void BasicGaussian<T>::reset(T sig, T c) {
-      Sigma = sig; Coeff = c;
-      CoeffP = -2.0*Sigma*Coeff;
-      DEBUGMSG("Gassuian " << Sigma << " " << Coeff)
-    }
-
-
-  template<class T>
-  GaussianCombo<T>::GaussianCombo(int l, bool normalized): 
-    Normalized(normalized)
-  {
-    L = static_cast<T>(l);
-    //Everything related to L goes to NormL and NormPow
-    NormL = pow(2,L+1)*sqrt(2.0/static_cast<T>(DFactorial(2*l+1)))*pow(2.0/M_PI,0.25);
-    NormPow = 0.5*(L+1.0)+0.25;
-  }
-
-  template<class T>
-  bool GaussianCombo<T>::put(xmlNodePtr cur) {
-    InParam.push_back(cur);
-    return true;
-  }
-
-  template<class T>
-  void GaussianCombo<T>::reset() {
-    int n=gset.size();
-    while(n<InParam.size()) {
-      gset.push_back(new BasicGaussian<T>());
-      n++;
-    }
-    for(int i=0; i<InParam.size(); i++) {
-      const xmlChar* aptr = xmlGetProp(InParam[i],(const xmlChar *)"alpha");
-      const xmlChar* cptr = xmlGetProp(InParam[i],(const xmlChar *)"c");
-      if(aptr == 0 || cptr == 0) continue;
-      T alpha = atof((const char*)aptr);
-      T c = atof((const char*)cptr);
-      //get the normalization factor
-      if(!Normalized) c *= NormL*pow(alpha,NormPow); 
-      gset[i]->reset(alpha,c);
-    }
-  }
-
   bool
   GTO2GridBuilder::addRadialOrbital(xmlNodePtr cur, 
 				    const QuantumNumberType& nlms) {
 
-    RadialOrbitalType *radorb =  NULL;
-
     int n=nlms[0];
     int l=nlms[1];
 
+    //Using default <radfunc exponent="alpha" contraction="c"/>
     GaussianCombo<ValueType> gaussian(l,Normalized);
- 
-    xmlNodePtr s = cur->xmlChildrenNode;
-    while(s != NULL) {
-      string cname((const char*)(s->name));
-      if(cname == "Rnl") {
-        gaussian.put(s);
-      }
-      s = s->next;
-    }
-
-    gaussian.reset();
+    gaussian.putBasisGroup(cur);
 
     //pointer to the grid
     GridType* agrid = m_orbitals->Grids[0];
-    radorb = new OneDimCubicSpline<ValueType>(agrid);
+    RadialOrbitalType *radorb = new OneDimCubicSpline<ValueType>(agrid);
 
     //spline the slater type orbital
     Transform2GridFunctor<GaussianCombo<RealType>,RadialOrbitalType> transform(gaussian, *radorb);
--- src/QMCWaveFunctions/MolecularOrbitals/GTO2GridBuilder.h
+++ src/QMCWaveFunctions/MolecularOrbitals/GTO2GridBuilder.h
@@ -26,54 +26,6 @@
 
 namespace ohmmsqmc {
 
-  template<class T>
-  struct BasicGaussian {
-    T Sigma;
-    T Coeff;
-    T CoeffP;
-    BasicGaussian(): Sigma(1.0), Coeff(1.0) { } 
-    inline BasicGaussian(T sig, T c) { 
-      reset(sig,c);
-    } 
-    void reset(T sig, T c);
-    inline void setgrid(T r) { }
-    inline T f(T r2) {
-      return Coeff*exp(-Sigma*r2);
-    }
-    inline T df(T r, T r2) {
-      return CoeffP*r*exp(-Sigma*r2);
-    }
-  };
-
-  template<class T>
-  struct GaussianCombo {
-    typedef T value_type;
-    ///Boolean
-    bool Normalized;
-
-    T L;
-    T NormL;
-    T NormPow;
-    std::vector<xmlNodePtr> InParam;
-    std::vector<BasicGaussian<T>* > gset;
-    explicit GaussianCombo(int l, bool normalized);
-    void reset();
-    inline value_type f(value_type r) {
-      value_type res=0;
-      value_type r2 = r*r;
-      for(int i=0; i<gset.size(); i++) res += gset[i]->f(r2);
-      return res;
-    }
-    inline value_type df(value_type r) {
-      value_type res=0;
-      value_type r2 = r*r;
-      for(int i=0; i<gset.size(); i++) res += gset[i]->df(r,r2);
-      return res;
-    }
-    bool put(xmlNodePtr cur);
-  };
-
-
   /**Class to convert GaussianTypeOrbital to a radial orbital on a log grid.
    *
    * For a center,
