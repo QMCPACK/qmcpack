7c481a35037c3aa21758f4cce727e61e7c74eadf
Jeongnim Kim <jeongnim.kim@intel.com>
2005-04-04 14:46:00 +0000

    Clean up and bug fixes.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@259 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/MolecularOrbitals/MolecularOrbitalBuilder.cpp
+++ src/QMCWaveFunctions/MolecularOrbitals/MolecularOrbitalBuilder.cpp
@@ -38,9 +38,7 @@ namespace ohmmsqmc {
     bool usegrid = true;
     if(xmlHasProp(cur, (const xmlChar*)"usegrid")) {
       string a((const char*)(xmlGetProp(cur, (const xmlChar *)"usegrid")));
-      if(a == "false" || a == "0") {
-	usegrid = false;
-      }
+      usegrid = !(a == "no" || a == "false" || a == "0");
     }
 
     if(usegrid) {
@@ -58,43 +56,41 @@ namespace ohmmsqmc {
 
   bool MolecularOrbitalBuilder::putOpen(const string& fname_in){
 
-      xmlDocPtr doc=NULL;
-      xmlNsPtr ns;
-      xmlNodePtr cur;
-      
-      // build an XML tree from a the file;
-      doc = xmlParseFile(fname_in.c_str());
-      if (doc == NULL) {
-	ERRORMSG(fname_in << " does not exist")
-	  return false;
-      } 
+    xmlDocPtr doc=NULL;
+    xmlNsPtr ns;
+    xmlNodePtr cur;
+    
+    // build an XML tree from a the file;
+    doc = xmlParseFile(fname_in.c_str());
+    if (doc == NULL) {
+      ERRORMSG(fname_in << " does not exist")
+        return false;
+    } 
 
-      ///using XPath instead of recursive search
-      xmlXPathContextPtr context;
-      // xmlXPathObjectPtr result;
-      context = xmlXPathNewContext(doc);
-      const char* name = OrbitalBuilderBase::detset_tag.c_str();
-      string sdname("//");
-      sdname.append(OrbitalBuilderBase::detset_tag);
-      xmlXPathObjectPtr result
-	= xmlXPathEvalExpression((const xmlChar*)(sdname.c_str()),context);
-      //      result = xmlXPathEvalExpression((const xmlChar*)"//determinantset",context);
+    ///using XPath instead of recursive search
+    xmlXPathContextPtr context;
+    // xmlXPathObjectPtr result;
+    context = xmlXPathNewContext(doc);
+    const char* name = OrbitalBuilderBase::detset_tag.c_str();
+    string sdname("//");
+    sdname.append(OrbitalBuilderBase::detset_tag);
+    xmlXPathObjectPtr result
+      = xmlXPathEvalExpression((const xmlChar*)(sdname.c_str()),context);
+    //      result = xmlXPathEvalExpression((const xmlChar*)"//determinantset",context);
   
-      if(xmlXPathNodeSetIsEmpty(result->nodesetval)) {
-	
-	ERRORMSG(fname_in << " does not contain any Data!")
-	  
-	  } else {
-	    xmlNodePtr cur = result->nodesetval->nodeTab[0];
-	    putSpecial(cur);
-	  }
-
-      //free local objects
-      xmlXPathFreeObject(result);
-      xmlFreeDoc(doc);
-      
-      return true;
+    if(xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+      ERRORMSG(fname_in << " does not contain any Data!")
+    } else {
+      putSpecial(result->nodesetval->nodeTab[0]);
     }
+
+    //free local objects
+    xmlXPathFreeObject(result);
+    xmlXPathFreeContext(context);
+    xmlFreeDoc(doc);
+    
+    return true;
+  }
 
 }
 /***************************************************************************
