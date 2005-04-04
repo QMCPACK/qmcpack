47b2cda95757569222a197fde9a2f4118e12e10e
Jeongnim Kim <jeongnim.kim@intel.com>
2005-04-04 14:03:20 +0000

    A RGFBuilderBase (Radial-Grid-Functor) that maps a mixture of Gaussian/Slater/Pade to a Numerial Radial Grid.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@257 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/MolecularOrbitals/Any2GridBuilder.h
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+#ifndef OHMMS_QMC_ANY2RADIALGRIDFUNCTOR_H
+#define OHMMS_QMC_ANY2RADIALGRIDFUNCTOR_H
+
+#include "QMCWaveFunctions/MolecularOrbitals/RGFBuilderBase.h"
+
+namespace ohmmsqmc {
+
+  /**Class to convert SlaterTypeOrbital to a radial orbital on a log grid.
+   *
+   * For a center,
+   *   - only one grid is used
+   *   - any number of radial orbitals 
+   */
+  struct Any2GridBuilder: public RGFBuilderBase {
+
+    ///constructor
+    Any2GridBuilder(xmlNodePtr cur);
+
+    ///implement the virtual function
+    bool addRadialOrbital(xmlNodePtr cur, const QuantumNumberType& nlms);
+
+    bool Normalized;
+    RealType m_rcut;
+    QuantumNumberType m_nlms;
+
+    void addGaussian(xmlNodePtr cur);
+    void addSlater(xmlNodePtr cur);
+    void addPade(xmlNodePtr cur);
+  };
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
