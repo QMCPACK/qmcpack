c778e228c682ee99c6f7544b69cf456a4ff12d14
Jeongnim Kim <jeongnim.kim@intel.com>
2005-04-14 17:43:12 +0000

    Add cubic-spline based on m-relation.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@286 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Numerics/NRSplineFunctions.h
+++ src/Numerics/NRSplineFunctions.h
@@ -49,6 +49,159 @@ NRCubicSpline(const T* x, const T* y, int n, T yp1, T ypn, T* y2) {
   for (k=n-2;k>=0;k--)
     y2[k]=y2[k]*y2[k+1]+u[k];
 }
+
+/**template function: converted from Numerical Recipe spline.c and with PBC
+ * @param x first address of the x-axis
+ * @param y first address of the y-axis
+ * @param n number of data point
+ * @param d1 first address of the first-derivatives coefficients
+ * @param d2 first address of the second-derivatives coefficients
+ *
+ *Note that the range of data is [0,n) instead of [1,n]
+ */
+template<class T>
+inline void 
+NRCubicSplinePBC(const T* x, const T* y, int n, T* d1, T* d2 ) {
+
+  //store the last valid index
+  int last(n-1),last1(n-2),last2(n-3);
+
+  //temporary array for the d1(frist) and d3(third) derivatives
+  std::vector<T> d3(n);
+
+  //clear the contents
+  d1[0]=d2[0]=d3[0]=0.0;
+  d1[last]=d2[last]=d3[last]=0.0;
+
+  if(n == 2) {
+    d1[0]=(y[1]-y[0])/(x[1]-x[0]);
+    d1[1]=d1[0];
+  } else {
+    d3[0]=x[1]-x[0];
+    d2[1]=(y[1]-y[0])/d3[0];
+    for(int cur=1, next=2; cur<last; cur++, next++) {
+      d3[cur]=x[next]-x[cur];
+      d1[cur]=2.0*(d3[cur-1]+d3[cur]);
+      d2[next]=(y[next]-y[cur])/d3[cur];
+      d2[cur]=d2[next]-d2[cur];
+    }
+  }
+
+  //Time to take care of BC
+  std::vector<T> wk(n,0.0);
+  d1[0]=2.0*(d3[0]+d3[last1]);
+  d2[0]=(y[1]-y[0])/d3[0]-(y[last]-y[last1])/d3[last1];
+  wk[0]=d3[last1];
+  wk[last2]=d3[last2];
+  wk[last1]=d3[last1];
+
+  //forward elimination
+  for(int cur=1, prev=0; cur<last1; cur++,prev++) {
+    T t(d3[prev]/d1[prev]);
+    d1[cur] -= t*d3[prev];
+    d2[cur] -= t*d2[prev];
+    wk[cur] -= t*wk[prev];
+
+    T q(wk[last1]/d1[prev]);
+    wk[last1]=-q*d3[prev];
+    d1[last1]-= q*wk[prev];
+    d2[last1]-= q*d2[prev];
+  }
+  //correct the last-1
+  wk[last1]+=d3[last2];
+
+  T t(wk[last1]/d1[last2]);
+  d1[last1] -= t*wk[last2];
+  d2[last1] -= t*d2[last2];
+
+  //back substitution
+  d2[last1] /= d1[last1];
+  d2[last2] = (d2[last2]-wk[last2]*d2[last1])/d1[last2];
+  for(int cur=last2-1,next=last2; next>0; cur--,next--) {
+    d2[cur]=(d2[cur]-d3[cur]*d2[next]-wk[cur]*d2[last1])/d1[cur];
+  }
+  d2[last]=d2[0];
+
+  for(int cur=0,next=1; cur<last; cur++,next++) {
+    d1[cur]=(y[next]-y[cur])/d3[cur]-d3[cur]*(d2[next]+2.0*d2[cur]);
+    d3[cur]=6.0*(d2[next]-d2[cur])/d3[cur];
+    d2[cur]*=6.0;
+  }
+  d1[last]=d1[0];
+  d2[last]=d2[0];
+}
+
+template<class T>
+inline void 
+NRCubicSplineFirst(const T* x, const T* y, int n, T* d1, T* d2 ) {
+
+  int last(n-1),last1(n-2),last2(n-3);
+
+  //store the last valid index
+  T a1=d1[0];
+  T an=d1[last];
+
+  //temporary array for the d1(frist) and d3(third) derivatives
+  std::vector<T> d3(n);
+
+  //clear the contents
+  d2[0]=d3[0]=0.0;
+  d1[last]=d2[last]=d3[last]=0.0;
+
+  if(n == 2) {
+    d1[0]=(y[1]-y[0])/(x[1]-x[0]);
+    d1[1]=d1[0];
+  } else {
+    d3[0]=x[1]-x[0];
+    d2[1]=(y[1]-y[0])/d3[0];
+    for(int cur=1, next=2; cur<last; cur++, next++) {
+      d3[cur]=x[next]-x[cur];
+      d1[cur]=2.0*(d3[cur-1]+d3[cur]);
+      d2[next]=(y[next]-y[cur])/d3[cur];
+      d2[cur]=d2[next]-d2[cur];
+    }
+  }
+
+  T elem21(d3[0]), elemnn1(d3[last1]);
+
+  d1[0]=2.0*d3[0];
+  d2[0]=(y[1]-y[0])/d3[0]-a1;
+
+  d1[last]=2.0*d3[last1];
+  d2[last]=-(y[last]-y[last1])/d3[last1]+an;
+
+  //forward elimination
+  T t1(elem21/d1[0]);
+  d1[1] -= t1*d3[0];
+  d2[1] -= t1*d2[0];
+  for(int cur=2, prev=1; cur<last; cur++, prev++) {
+    T t(d3[prev]/d1[prev]);
+    d1[cur] -= t*d3[prev];
+    d2[cur] -= t*d2[prev];
+  }
+  t1=elemnn1/d1[last1];
+  d1[last] -= t1*d3[last1];
+  d2[last] -= t1*d2[last1];
+
+  //back substitution
+  d2[last] /= d1[last];
+
+  for(int cur=last1; cur>=0; cur--) {
+    d2[cur]=(d2[cur]-d3[cur]*d2[cur+1])/d1[cur];
+  }
+  //d2[0]=(d2[0]-d3[0]*d2[1])/d1[0];
+
+  d3[0]=x[1]-x[0];
+  d3[last1]=x[last]-x[last1];
+  for(int cur=0,next=1; cur<last; cur++,next++) {
+    d1[cur]=(y[next]-y[cur])/d3[cur]-d3[cur]*(d2[next]+2.0*d2[cur]);
+    d3[cur] =6.0*(d2[next]-d2[cur])/d3[cur];
+    d2[cur]*=6.0;
+  }
+  T hn(x[last]-x[last1]);
+  d1[last]=an;
+  d2[last]=d2[last1]+hn*d3[last1];
+}
 #endif
 /***************************************************************************
  * $RCSfile$   $Author$
