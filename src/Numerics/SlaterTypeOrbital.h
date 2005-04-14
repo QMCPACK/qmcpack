917dcc65b9bd2c2932f6aa4c3f8218fe410d84b9
Jeongnim Kim <jeongnim.kim@intel.com>
2005-04-14 17:35:20 +0000

    Two functions are clearly differenciated: evaluate -> calculate the value only evaluateAll -> calculate the value and first and second derivatives.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@281 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Numerics/SlaterTypeOrbital.h
+++ src/Numerics/SlaterTypeOrbital.h
@@ -22,7 +22,128 @@
 #include "Numerics/RadialOrbitalBase.h"
 #include <cmath>
 
-/**class for Slater-type orbitals
+/** class to evaluate the normalization factors for the Slater-Type orbitals
+ */
+template<class T>
+struct STONorm {
+
+  std::vector<T> Factorial;
+
+  explicit STONorm(int nmax=1) {
+    set(nmax);
+  }
+
+  inline void set(int nmax) {
+    int n = 2*nmax+2;
+    Factorial.resize(n+1);
+    Factorial[0] = 1.0;
+    for (int i=1;i<n+1;i++) Factorial[i] = Factorial[i-1]*static_cast<T>(i);
+  }
+
+  inline T operator()(int n, T screen) {
+    return 
+      1.0/sqrt(Factorial[2*n+2]*4.0*(4.0*atan(1.0))/pow(2.0*screen,2*n+3));
+  } 
+
+};
+
+
+
+/** Generic Slater-Type Orbital
+ *
+ * This class evalaute \f$ \frac{\chi_{n,\xi}(r)}{r^l} \f$
+ * where a normalized STO is defined as 
+ * \f$\chi_{n,\xi}(r) =  C r^{n-1} \exp{-\xi r}\f$ 
+ * C is a contraction factor.
+ * The physical principal quantum number n has to be a positive integer,
+ * where n-1 is the number of nodes.
+ */
+template<class T>
+struct GenericSTO: public RadialOrbitalBase<T> {
+
+  typedef T value_type;
+
+  int ID;
+  ///Principal number
+  int N;
+  ///N-l-1
+  int Power;
+  T Z;
+  T Norm;
+  T Y, dY, d2Y;
+
+  GenericSTO(): N(-1), Power(-1), Z(1.0), Norm(1.0) { } 
+
+  /** constructor with a known contraction factor 
+   */
+  explicit GenericSTO(int power, T z, T norm=1.0):  
+    N(-1), Power(power), Z(z), Norm(norm) { } 
+
+  /** constructor with a set of quantum numbers
+   * @param n principal quantum number
+   * @param l angular quantum number
+   * @param z exponent
+   *
+   * Power = n-l-1
+   * Contraction factor is the normalization factor evaluated based on N and Z.
+   */
+  explicit GenericSTO(int n, int l, T z) : 
+    N(n), Power(n-l-1), Z(z) {
+    reset();
+  }
+
+  inline void reset() {
+    if(N>0) {
+      STONorm<T> anorm(N);
+      Norm = anorm(N-1,Z);
+    }
+  }
+
+  inline void setgrid(T r) { }
+
+  inline T f(T r) const {
+    return exp(-Z*r)*Norm*pow(r,Power);
+  }
+
+  inline T df(T r) const {
+    T rnl = exp(-Z*r)*Norm;
+    if(Power == 0) {
+      return  -Z*rnl;
+    } else {
+      return rnl*pow(r,Power)*(Power/r-Z);
+    }
+  }
+
+  /** return the value only
+   * @param r distance
+   * @param rinv inverse of r
+   */
+  inline T evaluate(T r, T rinv) {
+    return Y = Norm*pow(r,Power)*exp(-Z*r);
+  }
+
+  inline void evaluateAll(T r, T rinv) {
+    Y = evaluate(r,rinv,dY,d2Y);
+  }
+
+  inline T evaluate(T r, T rinv, T& drnl, T& d2rnl) {
+    T rnl = Norm*exp(-Z*r);
+    if(Power == 0) {
+      drnl = -Z*rnl;
+      d2rnl = rnl*Z*Z;
+    } else {
+      rnl *= pow(r,Power);
+      T x = Power*rinv-Z;
+      drnl = rnl*x;
+      d2rnl = rnl*(x*x-Power*rinv*rinv);
+    }
+    return rnl;
+  }
+
+};
+
+/**class for Slater-type orbitals,
+ *
  *@f[
  *\Psi_{n,l,m}({\bf R}) = N r^{n-1} \exp{-Zr} Y_{lm}(\theta,\phi)
  *@f]
@@ -49,7 +170,11 @@ struct RadialSTO {
     return  (NminusOne/r-Z)*rnl;
   }
 
-  inline void evaluate(T r, T rinv) {
+  inline T evaluate(T r) {
+    return pow(r,NminusOne)*exp(-Z*r)*Norm;
+  }
+
+  inline void evaluateAll(T r, T rinv) {
     Y = evaluate(r,rinv,dY,d2Y);
   }
 
@@ -62,105 +187,6 @@ struct RadialSTO {
   }
 };
 
-template<class T>
-struct STONorm {
-
-  std::vector<T> Factorial;
-
-  explicit STONorm(int nmax=1) {
-    set(nmax);
-  }
-
-  inline void set(int nmax) {
-    int n = 2*nmax+2;
-    Factorial.resize(n+1);
-    Factorial[0] = 1.0;
-    for (int i=1;i<n+1;i++) Factorial[i] = Factorial[i-1]*static_cast<T>(i);
-  }
-
-  inline T operator()(int n, T screen) {
-    return 
-      1.0/sqrt(Factorial[2*n+2]*4.0*(4.0*atan(1.0))/pow(2.0*screen,2*n+3));
-  } 
-
-};
-
-
-/** Generic Slater-Type Orbital
- *
- *The analytic form
- \f[
- \chi_{n,\xi}(r) =  N r^{n-1} \exp{-\xi r} 
- \f] 
- *
- *@note For QMC we are interested in
- \f[ 
- \frac{\chi_{n,\xi}(r)}{r^l} =  N r^{n-l-1} \exp{-\xi r} 
- /f]
-*/
-template<class T>
-struct GenericSTO: public RadialOrbitalBase<T> {
-
-  typedef T value_type;
-
-  int ID;
-  ///Principal number
-  int N;
-  ///N-l-1
-  int Power;
-  T Z;
-  T Norm;
-  T Y, dY, d2Y;
-
-  GenericSTO(): N(-1), Power(-1), Z(1.0), Norm(1.0) { } 
-
-  ///This should be removed
-  explicit GenericSTO(int n, T z, T norm=1.0):  
-    Power(n), Z(z), Norm(norm) { } 
-
-  explicit GenericSTO(int n, int l, T z) : N(n), Power(n-l-1), Z(z) {
-    reset();
-  }
-
-  inline void reset() {
-    STONorm<T> anorm(N);
-    Norm = anorm(N-1,Z);
-  }
-
-  inline void setgrid(T r) { }
-
-  inline T f(T r) const {
-    return exp(-Z*r)*Norm*pow(r,Power);
-  }
-
-  inline T df(T r) const {
-    T rnl = exp(-Z*r)*Norm;
-    if(Power == 0) {
-      return  -Z*rnl;
-    } else {
-      return rnl*pow(r,Power)*(Power/r-Z);
-    }
-  }
-
-  inline void evaluate(T r, T rinv) {
-    Y = evaluate(r,rinv,dY,d2Y);
-  }
-
-  inline T evaluate(T r, T rinv, T& drnl, T& d2rnl) {
-    T rnl = Norm*exp(-Z*r);
-    if(N == 0) {
-      drnl = -Z*rnl;
-      d2rnl = rnl*Z*Z;
-    } else {
-      rnl *= pow(r,Power);
-      T x = Power*rinv-Z;
-      drnl = rnl*x;
-      d2rnl = rnl*(x*x-Power*rinv*rinv);
-    }
-    return rnl;
-  }
-
-};
 
 
 #endif
