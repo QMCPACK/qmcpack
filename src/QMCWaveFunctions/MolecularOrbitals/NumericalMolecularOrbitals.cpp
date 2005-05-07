ee8268fcbd314ed5114a16746306d67901ad6aaa
Jeongnim Kim <jeongnim.kim@intel.com>
2005-05-07 16:35:45 +0000

    Generic numberical orbitals using TriCubicSplineT. Instead of instantiating NumericalMolecularOrbitals, QMC/MO2Grid3D.cpp is used to transform molecular orbitals on a grid.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@312 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/MolecularOrbitals/NumericalMolecularOrbitals.cpp
@@ -0,0 +1,209 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+#include "Utilities/OhmmsInfo.h"
+#include "Particle/DistanceTableData.h"
+#include "Particle/DistanceTable.h"
+#include "QMCWaveFunctions/MolecularOrbitals/NumericalMolecularOrbitals.h"
+#include "QMCWaveFunctions/MolecularOrbitals/GridMolecularOrbitals.h"
+#include "QMCWaveFunctions/LCOrbitals.h"
+#include "OhmmsData/AttributeSet.h"
+#include "Numerics/HDFSTLAttrib.h"
+#include "Numerics/HDFTriCubicSpline.h"
+#include "Utilities/Clock.h"
+
+namespace ohmmsqmc {
+
+  NumericalMolecularOrbitals::NumericalMolecularOrbitals(TrialWaveFunction& wfs, 
+					                 ParticleSet& ions, 
+					                 ParticleSet& els): 
+    OrbitalBuilderBase(wfs), Original(0), elsRef(els)
+  { 
+    Original = new GridMolecularOrbitals(wfs,ions,els);
+  }   
+
+  bool NumericalMolecularOrbitals::put(xmlNodePtr cur){
+
+    LOGMSG("NumericMolecularOrbitals::put")
+
+    typedef GridMolecularOrbitals::BasisSetType BasisSetType;
+    typedef LCOrbitals<BasisSetType>            SPOSetType;
+
+    int nels=0;
+    //map<string,SPOSetType*> InOrbs;
+    vector<SPOSetType*> InOrbs;
+    map<string,int> DetCounter;
+    BasisSetType *basisSet=0;
+    cur = cur->xmlChildrenNode;
+    while(cur != NULL) {
+      string cname((const char*)(cur->name));
+      if(cname == basisset_tag) {
+        basisSet = Original->addBasisSet(cur);
+      } else if(cname == sd_tag) {
+        nels=0;
+        xmlNodePtr tcur = cur->xmlChildrenNode;
+        while(tcur != NULL) {
+	  string tname((const char*)(tcur->name));
+	  if(tname == det_tag) {
+            string detname("det");
+            const xmlChar* a=xmlGetProp(tcur,(const xmlChar*)"id");
+            if(a) {
+              detname = (const char*)a;
+            } 
+            //else {
+            //  ostringstream idassigned(detname);
+            //  idassigned << "dummy";
+            //}
+            bool newset=true;
+            a=xmlGetProp(tcur,(const xmlChar*)"ref");
+            if(a) {
+              string detref((const char*)a);
+              if(DetCounter.find(detref) != DetCounter.end()) {
+                newset=false;
+              }
+            }
+            a=xmlGetProp(tcur,(const xmlChar*)"orbitals");
+            int norbs = atoi((const char*)a);
+            if(newset) {
+              SPOSetType* psi=new SPOSetType(basisSet,nels);
+              psi->put(tcur);
+              psi->setName(detname);
+              psi->resize(norbs);
+
+              DetCounter[detname]=InOrbs.size();
+              InOrbs.push_back(psi);
+
+              //InOrbs[detname]=psi;
+            }
+            nels+=norbs;
+	  }
+	  tcur = tcur->next;
+	}
+      }
+      cur = cur->next;
+    }
+
+    //resize with respect to the number of electrons
+    basisSet->resize(nels);
+
+    DistanceTable::create(1);
+    elsRef.update(1);
+
+    //Need only one electron to calculate this
+    //map<string,SPOSetType*>::iterator oit(InOrbs.begin());
+    SPOSetType* inorb=InOrbs[0];
+    double ri = -5.0;
+    double rf = 5.0;
+    std::vector<int> npts(3);
+    npts[0]=101;npts[1]=101;npts[2]=101;
+    //Create one-dimensional grids for three orthogonal directions
+    typedef LinearGrid<double> GridType;
+    GridType gridX, gridY, gridZ;
+    gridX.set(ri,rf,npts[0]);
+    gridY.set(ri,rf,npts[1]);
+    gridZ.set(ri,rf,npts[2]);
+
+
+    XYZCubicGrid<ValueType> grid3(&gridX,&gridY,&gridZ);
+
+    typedef TriCubicSplineT<ValueType> NOType;
+    vector<NOType*> torb;
+    for(int i=0; i<inorb->numOrbitals(); i++) {
+      torb.push_back(new NOType(&grid3));
+      if(i) torb[i]->setGridManager(false);
+    }
+
+    //TriCubicSplineT<ValueType> torb(&grid3);
+    
+    vector<ValueType> phi(inorb->numOrbitals(),0.0);
+    vector<vector<ValueType>* > dat;
+    int ntot = npts[0]*npts[1]*npts[2];
+    for(int is=0; is<inorb->numOrbitals(); is++) 
+      dat.push_back(new vector<ValueType>(ntot,0));
+    //int lastpsi=inorb->numOrbitals()-2;
+    //int lastpsi=0;
+
+    //PosType dr(1.1,1.2,1.5);
+    //PosType newpos(elsRef.makeMove(0,dr));
+    ////cout << "p(ix,iy,iz) " << newpos << "(" << ix << "," << iy << "," << iz << ")" << endl;
+    //inorb->evaluate(elsRef,0,phi);
+    //cout << newpos << " " << phi[lastpsi] << endl;
+    PosType pos(elsRef.R[0]);
+    int ng=0;
+
+    Pooma::Clock timer;
+    timer.start();
+    for(int ix=0; ix<npts[0]; ix++) {
+      RealType x(gridX(ix));
+      for(int iy=0; iy<npts[1]; iy++) {
+        RealType y(gridY(iy));
+        for(int iz=0; iz<npts[2]; iz++) {
+          PosType dr(x,y,gridZ(iz));
+          PosType newpos(elsRef.makeMove(0,dr-pos));
+          //cout << "p(ix,iy,iz) " << newpos << "(" << ix << "," << iy << "," << iz << ")" << endl;
+          int item=torb[0]->index(ix,iy,iz);
+          inorb->evaluate(elsRef,0,phi);
+          for(int is=0; is<phi.size(); is++) (*torb[is])(item)=phi[is];
+          //for(int is=0; is<phi.size(); is++) dat[is]->operator[](offset)=phi[is];
+          //torb(ix,iy,iz)=phi[lastpsi];
+          //dat[offset]=phi[lastpsi];
+        }
+      }
+    }
+    timer.stop();
+    cout << "Time to evaluate " << torb.size() << " orbitals = " << timer.cpu_time() << endl;
+
+    timer.start();
+    for(int is=0; is<torb.size(); is++) torb[is]->reset(true);
+    timer.stop();
+    cout << "Time to evaluate spline coefficients = " << timer.cpu_time() << endl;
+
+    for(int ix=0; ix<npts[0]-1; ix++) {
+      double x(gridX(ix));
+      for(int iy=0; iy<npts[1]-1; iy++) {
+        double y(gridY(iy));
+        int offset=npts[2]*(iy+npts[1]*ix);
+        for(int iz=0; iz<npts[2]-1; iz++,offset++) {
+           TinyVector<double,3> p(x,y,gridZ(iz));
+           for(int is=0; is<dat.size(); is++) 
+             dat[is]->operator[](offset) = torb[is]->evaluate(p);
+        }
+      }
+    }
+
+    for(int is=0; is<dat.size(); is++) {
+      char oname[128];
+      sprintf(oname,"out%04d.h5",is);
+      hid_t h_file = H5Fcreate(oname,H5F_ACC_TRUNC,H5P_DEFAULT,H5P_DEFAULT);
+      HDFAttribIO<std::vector<double> > dump(*dat[is],npts);
+      dump.write(h_file,"Eigvector");
+
+      HDFAttribIO<TriCubicSplineT<double> > dump1(*torb[is]);
+      dump1.write(h_file,"CubicSpline");
+
+      H5Fclose(h_file);
+    }
+    //HDFAttribIO<TriCubicSplineT<double> > dump1(torb);
+    //dump1.write(h_file,"spline0000");
+
+    //stop for now
+    //OHMMS::Controller->finalize();
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCWaveFunctions/MolecularOrbitals/NumericalMolecularOrbitals.h
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_NUMERICAL_MOLECULARORBITALS_H
+#define OHMMS_QMC_NUMERICAL_MOLECULARORBITALS_H
+#include "QMCWaveFunctions/OrbitalBuilderBase.h"
+
+namespace ohmmsqmc {
+
+  class GridMolecularOrbitals;
+
+  class NumericalMolecularOrbitals: public OrbitalBuilderBase {
+
+    GridMolecularOrbitals *Original;
+    ParticleSet& elsRef;
+
+    public:
+
+    /** constructor
+     * \param wfs reference to the wavefunction
+     * \param ions reference to the ions
+     * \param els reference to the electrons
+     */
+    NumericalMolecularOrbitals(TrialWaveFunction& wfs, ParticleSet& ions, ParticleSet& els);
+
+    /** initialize the Antisymmetric wave function for electrons
+     *@param cur the current xml node
+     *
+     */
+    bool put(xmlNodePtr cur);
+
+  private:
+
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
