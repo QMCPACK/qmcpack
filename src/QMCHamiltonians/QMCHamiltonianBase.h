68360e5f2fe44135528b70c30bcf18c7d4388d3e
Jeongnim Kim <jeongnim.kim@intel.com>
2005-05-14 23:27:20 +0000

    Move QMCHamiltonian class declaration to QMCHamiltonian.h
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@349 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCHamiltonians/QMCHamiltonianBase.h
+++ src/QMCHamiltonians/QMCHamiltonianBase.h
@@ -14,17 +14,17 @@
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
+/**@file QMCHamiltonianBase.h
+ *@brief Declaration of QMCHamiltonianBase
+ */
 #ifndef OHMMS_QMC_HAMILTONIANBASE_H
 #define OHMMS_QMC_HAMILTONIANBASE_H
+
 #include "Particle/ParticleSet.h"
 #include "Utilities/PooledData.h"
 
-/**@file QMCHamiltonianBase.h
- *@brief Declaration of QMCHamiltonianBase and QMCHamiltonian
- */
 namespace ohmmsqmc {
 
-  class ParticeBase;
   class WalkerSetRef;
   class DistanceTableData;
 
@@ -67,70 +67,6 @@ namespace ohmmsqmc {
     void evaluate(WalkerSetRef& W, ValueVectorType& LE) = 0;
 
     inline void setTau(RealType tau) { Tau = tau;}
-  };
-
-  /**  Collection of Local Energy Operators 
-   *
-   * Note that QMCHamiltonian is not derived from QMCHmailtonianBase.
-   */
-  class QMCHamiltonian  {
-
-  public:
-
-    typedef QMCHamiltonianBase::RealType        RealType;
-    typedef QMCHamiltonianBase::ValueType       ValueType;
-    typedef QMCHamiltonianBase::ValueVectorType ValueVectorType;
-
-    ///constructor
-    QMCHamiltonian();
-    ///destructor
-    ~QMCHamiltonian();
-
-    void add(QMCHamiltonianBase* h, const string& aname);
-    bool remove(const string& aname);
-
-    ///return the name of ith Hamiltonian 
-    inline string getName(int i) const { return Hname[i];}
-
-    ///return the value of Hamiltonian i
-    inline RealType operator[](int i) const { return Hvalue[i];}
-
-    ///return the number of Hamiltonians
-    inline int size() const { return H.size();}
-
-    ///copy the local content to iterator first
-    template<class IT>
-    inline void copy(IT first) { std::copy(Hvalue.begin(), Hvalue.end(), first);}
-
-    template<class IT>
-    inline void copy(IT first, RealType wgt) { 
-      vector<RealType>::iterator it(Hvalue.begin()),it_end(Hvalue.end());
-      while(it != it_end) {*first++ = wgt*(*it++);}
-      //for(int i=0; i<Hvalue.size(); i++,first++) *first = wgt*Hvalue[i];
-    }
-
-    QMCHamiltonianBase* getHamiltonian(const string& aname);
-    
-    inline RealType getLocalEnergy() { return LocalEnergy;}
-    inline RealType getLocalPotential() { return LocalEnergy-Hvalue[0];}
-
-    inline void setTau(RealType tau) {for(int i=0; i< H.size();++i)H[i]->setTau(tau); }
-    
-    ValueType evaluate(ParticleSet& P);
-
-    void evaluate(WalkerSetRef& W, ValueVectorType& LE);
-
-   private:
-    ///Current Local Energy
-    RealType LocalEnergy;
-    ///vector of Hamiltonians
-    std::vector<QMCHamiltonianBase*> H;
-    ///map the name to an index
-    std::map<string,int> Hmap;
-    ///vector containing the names of the Hamiltonians
-    std::vector<string> Hname;
-    ///vector containing the values of the Hamiltonians
-    std::vector<RealType> Hvalue;
   };
 }
 #endif
