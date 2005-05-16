8ca448da0595f3fc70fc5dd495cbae76b16268a8
Jeongnim Kim <jeongnim.kim@intel.com>
2005-05-16 02:57:00 +0000

    put function is empty. Constructor is enough.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@362 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/AtomicOrbitals/HeSTOClementiRottie.cpp
+++ src/QMCWaveFunctions/AtomicOrbitals/HeSTOClementiRottie.cpp
@@ -68,33 +68,33 @@ namespace ohmmsqmc {
   bool 
   HePresetHFBuilder::put(xmlNodePtr cur){
 
-    DistanceTableData* d_ei = NULL;
-    cur = cur->xmlChildrenNode;
-    while(cur != NULL) {
-      string cname((const char*)(cur->name));
-      if(cname == dtable_tag) {
-	string dtable((const char*)(xmlGetProp(cur,(const xmlChar *)"source")));
-	dtable.append((const char*)(xmlGetProp(cur,(const xmlChar *)"target")));
-	d_ei= DistanceTable::getTable(dtable.c_str());
-	LOGMSG("DistanceTable is selected = " << dtable)
-      }
-      cur = cur->next;
-    }
-    typedef DiracDeterminant<HePresetHF> Det_t;
-    HePresetHF* heorb = new HePresetHF;
-    heorb->setTable(d_ei);
+    //DistanceTableData* d_ei = NULL;
+    //cur = cur->xmlChildrenNode;
+    //while(cur != NULL) {
+    //  string cname((const char*)(cur->name));
+    //  if(cname == dtable_tag) {
+    //    string dtable((const char*)(xmlGetProp(cur,(const xmlChar *)"source")));
+    //    dtable.append((const char*)(xmlGetProp(cur,(const xmlChar *)"target")));
+    //    d_ei= DistanceTable::getTable(dtable.c_str());
+    //    LOGMSG("DistanceTable is selected = " << dtable)
+    //  }
+    //  cur = cur->next;
+    //}
+    //typedef DiracDeterminant<HePresetHF> Det_t;
+    //HePresetHF* heorb = new HePresetHF;
+    //heorb->setTable(d_ei);
 
-    Det_t *DetU = new Det_t(*heorb,0);
-    DetU->set(0,1);
-    Det_t* DetD = new Det_t(*heorb,1);
-    DetD->set(1,1);
+    //Det_t *DetU = new Det_t(*heorb,0);
+    //DetU->set(0,1);
+    //Det_t* DetD = new Det_t(*heorb,1);
+    //DetD->set(1,1);
 
-    SlaterDeterminant<HePresetHF> *asymmpsi=new SlaterDeterminant<HePresetHF>;
+    //SlaterDeterminant<HePresetHF> *asymmpsi=new SlaterDeterminant<HePresetHF>;
 
-    asymmpsi->add(DetU);
-    asymmpsi->add(DetD);
+    //asymmpsi->add(DetU);
+    //asymmpsi->add(DetD);
 
-    targetPsi.add(asymmpsi);
+    //targetPsi.add(asymmpsi);
     return true;
   }
 }
