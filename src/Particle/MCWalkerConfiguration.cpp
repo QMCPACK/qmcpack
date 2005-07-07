6fe3619fe978f2d1fad93563ce049c8814b04072
Jeongnim Kim <jeongnim.kim@intel.com>
2005-07-07 14:25:29 +0000

    Changes due to multiple H/Psi MC.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@429 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Particle/MCWalkerConfiguration.cpp
+++ src/Particle/MCWalkerConfiguration.cpp
@@ -22,80 +22,111 @@
 using namespace ohmmsqmc;
 
 MCWalkerConfiguration::MCWalkerConfiguration(): 
-UpdateMode(Update_Walker) {
-
+OwnWalkers(true),ReadyForPbyP(false),UpdateMode(Update_Walker) {
+  initPropertyList();
 }
 
-
-MCWalkerConfiguration::MCWalkerConfiguration(const MCWalkerConfiguration& mcw, int nw):
-  ParticleSet(mcw)
+MCWalkerConfiguration::MCWalkerConfiguration(const MCWalkerConfiguration& mcw)
+: ParticleSet(mcw), OwnWalkers(true),
+  UpdateMode(Update_Walker), ReadyForPbyP(false)
 {
-  cout << "The number of particles " << getLocalNum() << endl;
-  for(int i=0; i<nw; i++) WalkerList.push_back(0);
+  initPropertyList();
 }
 
 ///default destructor
 MCWalkerConfiguration::~MCWalkerConfiguration(){
-
-  destroyWalker(WalkerList.begin(), WalkerList.end());
-  for(int iw=0; iw<DataSet.size(); iw++) delete DataSet[iw];
-
   DEBUGMSG("MCWalkerConfiguration::~MCWalkerConfiguration")
- //WalkerList.clear();
+  if(OwnWalkers) destroyWalkers(WalkerList.begin(), WalkerList.end());
 }
+
 
 void MCWalkerConfiguration::createWalkers(int n) {
-  for(int i=0; i<n; i++) {
-    WalkerList.push_back(new Walker_t(GlobalNum));
-    WalkerList.back()->R = R;
-    WalkerList.back()->Drift = 0.0;
+  while(n) {
+    Walker_t* awalker=new Walker_t(GlobalNum);
+    awalker->R = R;
+    awalker->Drift = 0.0;
+    WalkerList.push_back(awalker);
+    --n;
   }
 }
 
-void MCWalkerConfiguration::resize(int nw, int np) {
+void MCWalkerConfiguration::resize(int numWalkers, int numPtcls) {
+
   WARNMSG("MCWalkerConfiguration::resize cleans up the walker list.")
-  iterator it = WalkerList.begin();
-  while(it != WalkerList.end()) {
-    delete *it++;
-  }
-  WalkerList.erase(WalkerList.begin(),WalkerList.end());
-  R.resize(np);
-  GlobalNum = np;
-  createWalkers(nw);  
-}
 
+  ParticleSet::resize(unsigned(numPtcls));
 
-void MCWalkerConfiguration::addWalkers(vector<int>& Copies,
-				       vector<Walker_t*>& InactiveList) {
-  for(int i=0; i<Copies.size(); i++) {
-    for(int j=0; j<Copies[i]; j++) {
-      WalkerList.push_back(new Walker_t(*InactiveList[i]));
+  int dn=numWalkers-WalkerList.size();
+  if(dn>0) createWalkers(dn);
+
+  if(dn<0) {
+    int nw=-dn;
+    if(nw<WalkerList.size())  {
+      iterator it = WalkerList.begin();
+      while(nw) {
+        delete *it; ++it; --nw;
+      }
+      WalkerList.erase(WalkerList.begin(),WalkerList.begin()-dn);
     }
   }
+  //iterator it = WalkerList.begin();
+  //while(it != WalkerList.end()) {
+  //  delete *it++;
+  //}
+  //WalkerList.erase(WalkerList.begin(),WalkerList.end());
+  //R.resize(np);
+  //GlobalNum = np;
+  //createWalkers(nw);  
 }
 
-
-void MCWalkerConfiguration::copyWalker(iterator walkerit, int n) {
-
-  for(int i=0; i<n; i++)
-    WalkerList.push_back(new Walker_t(**walkerit));
-    //WalkerList.insert(WalkerList.end(),new Walker_t(**walkerit));
-}
+//void MCWalkerConfiguration::addWalkers(vector<int>& Copies,
+//				       vector<Walker_t*>& InactiveList) {
+//  for(int i=0; i<Copies.size(); i++) {
+//    for(int j=0; j<Copies[i]; j++) {
+//      WalkerList.push_back(new Walker_t(*InactiveList[i]));
+//    }
+//  }
+//}
+//void MCWalkerConfiguration::copyWalker(iterator walkerit, int n) {
+//
+//  for(int i=0; i<n; i++)
+//    WalkerList.push_back(new Walker_t(**walkerit));
+//}
+/////returns the next valid iterator
+//MCWalkerConfiguration::iterator 
+//MCWalkerConfiguration::destroyWalker(iterator walkerit) {
+//  delete *walkerit;
+//  return WalkerList.erase(walkerit);
+//}
 
 ///returns the next valid iterator
 MCWalkerConfiguration::iterator 
-MCWalkerConfiguration::destroyWalker(iterator walkerit) {
-  delete *walkerit;
-  return WalkerList.erase(walkerit);
+MCWalkerConfiguration::destroyWalkers(iterator first, iterator last) {
+  if(OwnWalkers) {
+    iterator it = first;
+    while(it != last) { delete *it++;}
+  }
+  return WalkerList.erase(first,last);
 }
 
-///returns the next valid iterator
-MCWalkerConfiguration::iterator 
-MCWalkerConfiguration::destroyWalker(iterator itstart, iterator itend) {
-  iterator it = itstart;
-  while(it != itend) { delete *it++;}
-  return WalkerList.erase(itstart,itend);
+void 
+MCWalkerConfiguration::copyWalkerRefs(Walker_t* head, Walker_t* tail) {
+
+  if(OwnWalkers) { //destroy the current walkers
+    destroyWalkers(WalkerList.begin(), WalkerList.end());
+    WalkerList.clear();
+    OwnWalkers=false;//set to false to prevent deleting the Walkers
+  }
+
+  if(WalkerList.size()<2) {
+    WalkerList.push_back(0);
+    WalkerList.push_back(0);
+  }
+
+  WalkerList[0]=head;
+  WalkerList[1]=tail;
 }
+
 
 /**@param first the iterator of the first walker to work on
  *@param last the iterator of the last walker to work on
@@ -108,17 +139,14 @@ void MCWalkerConfiguration::sample(iterator it, RealType tauinv) {
   R += (*it)->R + (*it)->Drift;
 }
 
-void MCWalkerConfiguration::clear() {
-  WalkerList.clear();
-}
-
-
-void MCWalkerConfiguration::copy(iterator first, iterator last){
-  while(first != last) {
-    WalkerList.push_back(*first); first++;
-  }
-}
-
+//void MCWalkerConfiguration::clear() {
+//  WalkerList.clear();
+//}
+//void MCWalkerConfiguration::copy(iterator first, iterator last){
+//  while(first != last) {
+//    WalkerList.push_back(*first); first++;
+//  }
+//}
 void MCWalkerConfiguration::reset() {
   iterator it=WalkerList.begin();
   while(it != WalkerList.end()) {(*it)->reset();it++;}
@@ -126,13 +154,21 @@ void MCWalkerConfiguration::reset() {
 
 bool MCWalkerConfiguration::createAuxDataSet(int nfield) {
 
-  if(DataSet.size()) return false;
+  if(ReadyForPbyP) return false;
 
-  for(int iw=0; iw<WalkerList.size(); iw++) {
-    DataSet.push_back(new WalkerData_t);
-    DataSet[iw]->reserve(nfield);
+  ReadyForPbyP=true;
+  UpdateMode=Update_Particle;
+  iterator it(WalkerList.begin());
+  iterator it_end(WalkerList.end());
+  while(it!=it_end) {
+    (*it)->DataSet.reserve(nfield); ++it;
   }
 
+  //if(DataSet.size()) return false;
+  //for(int iw=0; iw<WalkerList.size(); iw++) {
+  //  DataSet.push_back(new WalkerData_t);
+  //  DataSet[iw]->reserve(nfield);
+  //}
   return true;
 }
 
@@ -161,7 +197,7 @@ MCWalkerConfiguration::copyFromBuffer(PooledData<RealType>& buf) {
 
 
 int MCWalkerConfiguration::branch(int maxcopy, int Nmax, int Nmin) {
-    if(DataSet.empty()) {
+
   iterator it = WalkerList.begin();
   int iw=0, nw = WalkerList.size();
 
@@ -169,19 +205,13 @@ int MCWalkerConfiguration::branch(int maxcopy, int Nmax, int Nmin) {
   vector<int> ncopy;
   ncopy.reserve(nw);
 
-  //Temporary for vectorization: any line with energy or Energy is commented out
-  //since the performance is not improved by using the vectroized container.
-  // vector<RealType> energy;
-  // energy.reserve(Energy.size());
-  // int ncols = Energy.cols();
   int num_walkers=0;
   while(it != WalkerList.end()) {
-    int nc = std::min(static_cast<int>((*it)->Properties(MULTIPLICITY)),maxcopy);
+    int nc = std::min(static_cast<int>((*it)->Multiplicity),maxcopy);
     if(nc) {
       num_walkers += nc;
       good.push_back(*it);
       ncopy.push_back(nc-1);
-      //  energy.insert(energy.end(), Energy[iw], Energy[iw]+ncols);
     } else {
       bad.push_back(*it);
     }
@@ -220,119 +250,21 @@ int MCWalkerConfiguration::branch(int maxcopy, int Nmax, int Nmin) {
   WalkerList.clear();
   WalkerList.insert(WalkerList.begin(), good.begin(), good.end());
 
-  //  Energy.resize(num_walkers,ncols);
-  //  std::copy(energy.begin(), energy.end(), Energy.data());
-  //  vector<RealType>::iterator ie=energy.begin();
-  //  RealType *e_start = Energy.data()+energy.size();
   int cur_walker = good.size();
   for(int i=0; i<good.size(); i++) { //,ie+=ncols) {
     for(int j=0; j<ncopy[i]; j++, cur_walker++) {
       WalkerList.push_back(new Walker_t(*(good[i])));
-      //std::copy(ie,ie+ncols,e_start);
-      //e_start += ncols;
     }
   }
 
   iw=0;
   it=WalkerList.begin();
   while(it != WalkerList.end()) {
-    (*it)->Properties(WEIGHT) = 1.0;
-    (*it)->Properties(MULTIPLICITY) = 1.0;
+    (*it)->Weight= 1.0;
+    (*it)->Multiplicity=1.0;
     it++;
   }
-
   return iw;
-    } else {
-	return branch2(maxcopy, Nmax, Nmin);
-    }
-}
-
-int MCWalkerConfiguration::branch2(int maxcopy, int Nmax, int Nmin) {
-    
-      iterator it = WalkerList.begin();
-      // int iwalker=0;
-    int iw=0, nw = WalkerList.size();
-
-    vector<Walker_t*> good, bad;
-    vector<WalkerData_t*> good_data, bad_data;
-    vector<int> ncopy;
-    
-  ncopy.reserve(nw);
-  
-  int num_walkers=0;
-  while(it != WalkerList.end()) {
-      int nc = std::min(static_cast<int>((*it)->Properties(MULTIPLICITY)),maxcopy);
-    if(nc) {
-	num_walkers += nc;
-	good.push_back(*it);
-	good_data.push_back(DataSet[iw]);	
-	ncopy.push_back(nc-1);
-	
-    } else {
-	bad.push_back(*it);
-	bad_data.push_back(DataSet[iw]);
-    }
-    iw++;it++;
-  }
-
-
-  //remove bad walkers
-  for(int i=0; i<bad.size(); i++){
-      delete bad[i];
-      delete bad_data[i];
-  }
-
-  //check if the projected number of walkers is too small or too large
-  if(num_walkers>Nmax) {
-    int nsub=0;
-    int nsub_target=num_walkers-static_cast<int>(0.9*Nmax);
-    int i=0;
-    while(i<ncopy.size() && nsub<nsub_target) {
-      if(ncopy[i]) {ncopy[i]--; nsub++;}
-      i++;
-    }
-    num_walkers -= nsub;
-  } else  if(num_walkers < Nmin) {
-    int nadd=0;
-    int nadd_target = static_cast<int>(Nmin*1.1)-num_walkers;
-    if(nadd_target>good.size()) {
-      cerr << "Too few walkers to copy! Abort." << endl;
-      OHMMS::Controller->abort();
-    } else {
-      int i=0;
-      while(i<ncopy.size() && nadd<nadd_target) {
-	ncopy[i]++; nadd++;i++;
-      }
-    }
-    num_walkers +=  nadd;
-  }
-
-  //clear the WalkerList to populate them with the good walkers
-  WalkerList.clear();
-  WalkerList.insert(WalkerList.begin(), good.begin(), good.end());
-
-  DataSet.clear();
-  DataSet.insert(DataSet.begin(), good_data.begin(), good_data.end());
-
-
-  int cur_walker = good.size();
-  for(int i=0; i<good.size(); i++) { //,ie+=ncols) {
-    for(int j=0; j<ncopy[i]; j++, cur_walker++) {
-      WalkerList.push_back(new Walker_t(*(good[i])));
-      DataSet.push_back(new WalkerData_t(*good_data[i]));
-    }
-  }
-
-  iw=0;
-  it=WalkerList.begin();
-  while(it != WalkerList.end()) {
-    (*it)->Properties(WEIGHT) = 1.0;
-    (*it)->Properties(MULTIPLICITY) = 1.0;
-    it++;
-  }
-
-  return iw;
-
 }
 
 void MCWalkerConfiguration::setUpdateMode(int updatemode) { 
@@ -340,6 +272,7 @@ void MCWalkerConfiguration::setUpdateMode(int updatemode) {
   ///get the tables for which this particle set is the visitor or source
   if(DistTables.empty())  DistanceTable::getTables(ObjectTag,DistTables);
 
+  LOGMSG("MCWalkerConfiguration::setUpdateMode is called")
   UpdateMode = updatemode;
   if(UpdateMode == Update_All) {
     DistanceTable::create(WalkerList.size());
@@ -353,6 +286,26 @@ void MCWalkerConfiguration::loadWalker(Walker_t& awalker) {
   for(int i=0; i< DistTables.size(); i++) {
     DistTables[i]->evaluate(*this);
   }
+}
+
+/** reset the Property container of all the walkers
+ */
+void MCWalkerConfiguration::resetWalkerProperty(int ncopy) {
+  int m(PropertyList.size());
+  LOGMSG("Resetting Properties of the walkers " << ncopy << " x " << m)
+  iterator it(WalkerList.begin()),it_end(WalkerList.end());
+  while(it != it_end) {
+    (*it)->resizeProperty(ncopy,m); ++it;
+  }
+}
+
+void MCWalkerConfiguration::initPropertyList() {
+  //Need to add the default Properties according to the enumeration
+  PropertyList.add("LogPsi");
+  PropertyList.add("SignPsi");
+  PropertyList.add("UmbrellaWeight");
+  PropertyList.add("LocalEnergy");
+  PropertyList.add("LocalPotential");
 }
 
 
