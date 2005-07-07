3b7451732a70ac0c5e854c273087ef3f50447df4
Jeongnim Kim <jeongnim.kim@intel.com>
2005-07-07 18:42:44 +0000

    DummyQMC for testing.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@445 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCDrivers/DummyQMC.cpp
@@ -0,0 +1,85 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "QMCDrivers/DummyQMC.h"
+#include "Utilities/OhmmsInfo.h"
+#include "Particle/MCWalkerConfiguration.h"
+#include "Particle/DistanceTable.h"
+#include "Particle/HDFWalkerIO.h"
+#include "ParticleBase/ParticleUtility.h"
+#include "ParticleBase/RandomSeqGenerator.h"
+#include "Message/Communicate.h"
+#include "Utilities/Clock.h"
+
+namespace ohmmsqmc { 
+
+  /// Constructor.
+  DummyQMC::DummyQMC(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h): 
+    QMCDriver(w,psi,h) { 
+    RootName = "dummy";
+    QMCType ="dummy";
+  }
+  
+  bool DummyQMC::run() { 
+    
+    RealType oneovertau = 1.0/Tau;
+    RealType oneover2tau = 0.5*oneovertau;
+    RealType g = sqrt(Tau);
+    
+    cout << "Lattice of ParticleSet " << endl;
+    W.Lattice.print(cout);
+
+    //property container to hold temporary properties, such as a local energy
+    //MCWalkerConfiguration::PropertyContainer_t Properties;
+    MCWalkerConfiguration::Walker_t& thisWalker(**W.begin());
+
+    //create a 3N-Dimensional Gaussian with variance=1
+    makeGaussRandom(deltaR);
+
+    //new poosition
+    W.R = g*deltaR + thisWalker.R + thisWalker.Drift;
+
+    //apply boundary condition: put everything back to a unit cell
+    W.applyBC(W.R);
+    
+    //update the distance table associated with W
+    DistanceTable::update(W);
+    
+    //evaluate wave function
+    //update the properties: note that we are getting \f$\sum_i \ln(|psi_i|)\f$ and catching the sign separately
+    ValueType logpsi(Psi.evaluateLog(W));
+ 
+    RealType eloc=H.evaluate(W);
+
+    return true;
+  }
+  
+  bool 
+  DummyQMC::put(xmlNodePtr q){ 
+    //nothing to do
+    return true;
+  }
+
+}
+
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCDrivers/DummyQMC.h
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_DUMMY_H
+#define OHMMS_QMC_DUMMY_H
+#include "QMCDrivers/QMCDriver.h" 
+namespace ohmmsqmc {
+
+  /** Implements the DummyQMC algorithm. 
+   */
+  class DummyQMC: public QMCDriver {
+  public:
+    /// Constructor.
+    DummyQMC(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h);
+
+    bool run();
+    bool put(xmlNodePtr cur);
+ 
+  private:
+    /// Copy Constructor (disabled)
+    DummyQMC(const DummyQMC& a): QMCDriver(a) { }
+    /// Copy operator (disabled).
+    DummyQMC& operator=(const DummyQMC&) { return *this;}
+
+  };
+}
+
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
