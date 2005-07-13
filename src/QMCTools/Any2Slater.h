84054d8a5c8ce01477daef500a2fd2f16bfb1d94
Jeongnim Kim <jeongnim.kim@intel.com>
2005-07-13 22:53:24 +0000

    Utility application to convert GTO orbitals of S (l=0) to Slater-type orbitals. To be generalized for multiple STO orbitals using the contractions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@452 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCTools/Any2Slater.h
@@ -0,0 +1,93 @@
+#ifndef OHMMS_NONLINEAR_FITTING_H
+#define OHMMS_NONLINEAR_FITTING_H
+
+#include "Numerics/SlaterTypeOrbital.h"
+#include "Numerics/OneDimIntegration.h"
+#include "Optimize/Minimize.h"
+
+class Any2Slater : public MinimizeFunction {
+  
+  public:
+
+    typedef OneDimGridFunctor<scalar> SourceType;
+    typedef LogGrid<scalar>           GridType;
+
+    Any2Slater(SourceType& in): Source(in),
+                                cg_tolerance(1e-6),cg_stepsize(0.001),cg_epsilon(1e-6){ 
+      OptParams.resize(2);
+
+      psi_sq.resize(Source.size());
+      scalar r2psi2=-10000.0;
+      int igMax=-1;
+      for(int ig=1; ig<Source.size(); ig++) {
+        psi_sq[ig]=Source(ig)*Source(ig);
+        scalar r=Source.r(ig);
+        scalar t=r*r*psi_sq[ig];
+        if(t>r2psi2) {r2psi2=t;igMax=ig;}
+      }
+      //integrate_RK2_forward(psi_sq,psi_norm);
+
+      igMax+=2;
+      Target.N=1;
+      Target.Power=0;
+      Target.Z=1/Source.r(igMax);
+
+      Target.reset();
+      Source.grid().locate(0.2*Source.r(igMax));
+      minIndex=Source.grid().Loc;
+
+      cout << "Initial exponent " << Target.Z << " at " << Source.r(igMax) << endl;
+      cout << "The index of the cutoff " << minIndex << " at " << Source.r(minIndex) << endl;
+      OptParams[0]=Target.Z;
+    }
+
+    bool put(xmlNodePtr cur) { mPtr=cur; return true;}
+
+    ///return the number of optimizable parameters
+    int NumParams() { return OptParams.size(); }
+
+    ///assign optimization parameter i
+    scalar& Params(int i) { return OptParams[i]; }
+    ///return optimization parameter i
+    scalar Params(int i) const { return OptParams[i]; }
+
+    scalar Cost() {
+      Target.Z=OptParams[0];
+      //Target.Norm=OptParams[1];
+      Target.reset();
+      scalar del=0.0;
+      for(int ig=minIndex; ig<Source.size(); ig++) {
+        //scalar y= Target.f((*myGrid)(ig))-Source[ig];
+        scalar r=Source.r(ig);
+        scalar y= Target.f(r);
+        del += r*r*(y*y-psi_sq[ig]);
+      }
+      return del;
+    }
+
+    void WriteStuff() { 
+      cout << "Slater Z = " << Target.Z <<  "    Norm = " << Target.Norm << endl;
+    }
+
+    bool optimize() {
+      ConjugateGradient CG;
+      CG.Tolerance = cg_tolerance;
+      CG.StepSize = cg_stepsize;
+      CG.epsilon = cg_epsilon;
+      CG.Minimize(*this);
+      return true;
+    }
+ 
+  private:
+
+    SourceType& Source;
+    scalar cg_tolerance;
+    scalar cg_stepsize;
+    scalar cg_epsilon;
+    int minIndex;
+    xmlNodePtr mPtr;
+    GenericSTO<scalar> Target;
+    vector<scalar> OptParams;
+    Vector<scalar> psi_sq, psi_norm;
+  };
+#endif
---
+++ src/QMCTools/gto2slater.cpp
@@ -0,0 +1,161 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+#include "Message/Communicate.h"
+#include "Utilities/OhmmsInfo.h"
+#include "Numerics/OneDimGridBase.h"
+#include "Numerics/OneDimCubicSpline.h"
+#include "Numerics/Transform2GridFunctor.h"
+#include "Numerics/GaussianBasisSet.h"
+#include "QMCTools/Any2Slater.h"
+
+struct GTO2Slater {
+
+  typedef GaussianCombo<double>      GTOType;
+  typedef LogGrid<double>            GridType;
+
+  bool Normalized;
+  GridType myGrid;
+  xmlNodePtr gridPtr;
+  map<string,xmlNodePtr> sPtr;
+
+  GTO2Slater():Normalized(false),gridPtr(0){}
+  int parse(const char* fname);
+  bool put(xmlNodePtr);
+  void optimize();
+};
+
+int main(int argc, char** argv) {
+
+  OHMMS::Controller->initialize(argc,argv);
+
+  OhmmsInfo welcome(argc,argv,OHMMS::Controller->mycontext());
+
+  GTO2Slater transformer;
+  transformer.parse(argv[1]);
+  return 0;
+}
+
+int GTO2Slater::parse(const char* fname) {
+
+  // build an XML tree from a the file;
+  xmlDocPtr m_doc = xmlParseFile(fname);
+  if (m_doc == NULL) {
+    ERRORMSG("File " << fname << " is invalid")
+    xmlFreeDoc(m_doc);
+    return 1;
+  }    
+  // Check the document is of the right kind
+  xmlNodePtr cur = xmlDocGetRootElement(m_doc);
+  if (cur == NULL) {
+    ERRORMSG("Empty document");
+    xmlFreeDoc(m_doc);
+    return 1;
+  }
+
+  xmlXPathContextPtr m_context = xmlXPathNewContext(m_doc);
+  xmlXPathObjectPtr result
+    = xmlXPathEvalExpression((const xmlChar*)"//atomicBasisSet",m_context);
+  if(!xmlXPathNodeSetIsEmpty(result->nodesetval)) {
+    for(int ic=0; ic<result->nodesetval->nodeNr; ic++) {
+        cout << "Going to optimize" << endl;
+      if(put(result->nodesetval->nodeTab[ic])) {
+        optimize();
+      }
+    }
+  }
+
+  xmlXPathFreeObject(result);
+
+  return 1;
+}
+
+bool GTO2Slater::put(xmlNodePtr cur) {
+  cur = cur->children;
+  while(cur != NULL) {
+    string cname((const char*)(cur->name));
+    if(cname == "grid") 
+      gridPtr = cur;
+    else if(cname == "basisGroup") {
+      string rid("invalid");
+      string rtype("Gaussian");
+      string norm("no");
+      int l=0;
+      OhmmsAttributeSet inAttrib;
+      inAttrib.add(rid,"rid");
+      inAttrib.add(l,"l");
+      inAttrib.add(rtype,"type");
+      inAttrib.add(norm,"normalized");
+      inAttrib.put(cur);
+      if(rtype == "Gaussian" && l == 0) { //pick only S
+        if(norm == "yes") 
+          Normalized=true;
+        else
+          Normalized=false;
+        map<string,xmlNodePtr>::iterator it(sPtr.find(rid));
+        if(it == sPtr.end()) {
+          sPtr[rid]=cur;
+        }
+      }
+    }
+    cur=cur->next;
+  }
+
+  if(sPtr.empty()) 
+    return false;
+  return 
+    true;
+}
+
+  void GTO2Slater::optimize() {
+
+    double ri = 1e-5;
+    double rf = 10.0;
+    int npts = 101;
+    string gridType("log");
+    if(gridPtr) {
+      OhmmsAttributeSet radAttrib;
+      radAttrib.add(gridType,"type"); 
+      radAttrib.add(npts,"npts"); 
+      radAttrib.add(ri,"ri"); radAttrib.add(rf,"rf");
+      radAttrib.put(gridPtr);
+    }
+    myGrid.set(ri,rf,npts);
+
+    typedef OneDimCubicSpline<double> RadialOrbitalType;
+    RadialOrbitalType radorb(&myGrid);
+
+    int L= 0;
+    map<string,xmlNodePtr>::iterator it(sPtr.begin()),it_end(sPtr.end());
+    while(it != it_end) {
+      GTOType gset(L,Normalized); 
+      gset.putBasisGroup((*it).second);
+
+      //convert to a radial functor
+      Transform2GridFunctor<GTOType,RadialOrbitalType> transform(gset, radorb);
+      transform.generate(myGrid.rmin(),myGrid.rmax(),myGrid.size());
+
+      //optimize it
+      Any2Slater gto2slater(radorb);
+      gto2slater.optimize();
+      ++it;
+    }
+    sPtr.clear();
+  }
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
