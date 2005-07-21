6902f65f48e86c384f4eead8ddd32902d0115472
Jeongnim Kim <jeongnim.kim@intel.com>
2005-07-21 19:53:28 +0000

    Add template inline functions to add generic functions on iterators.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@462 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Utilities/IteratorUtility.h
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_ITERATOR_UTILITIES_H
+#define OHMMS_ITERATOR_UTILITIES_H
+
+/** delete the pointers in [first,last)
+ */
+template<class IT>
+inline void delete_iter(IT first, IT last) {
+  while(first != last) { delete *first; ++first;}
+}
+
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
