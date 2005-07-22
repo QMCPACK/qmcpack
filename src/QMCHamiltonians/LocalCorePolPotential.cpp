19a4fc508b67632b4f10d51694557774ad64494b
Jeongnim Kim <jeongnim.kim@intel.com>
2005-07-22 13:31:14 +0000

    Add LocalCorePotPotential for general purposes. This replaces GeCorePolPotential.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@466 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCHamiltonians/LocalCorePolPotential.cpp
@@ -0,0 +1,140 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003  by Jeongnim Kim and Jordan Vincent
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "QMCHamiltonians/LocalCorePolPotential.h"
+#include "Particle/DistanceTable.h"
+#include "Particle/DistanceTableData.h"
+#include "Utilities/OhmmsInfo.h"
+#include "Utilities/IteratorUtility.h"
+
+namespace ohmmsqmc {
+ 
+  LocalCorePolPotential::LocalCorePolPotential(ParticleSet& ions, 
+					       ParticleSet& els): 
+    FirstTime(true), eCoreCore(0.0), IonSys(ions), d_ie(0), d_ii(0) { 
+    
+    //set the distance tables
+    d_ie = DistanceTable::getTable(DistanceTable::add(ions,els));
+    d_ii = DistanceTable::getTable(DistanceTable::add(ions));
+
+    nCenters = ions.getTotalNum();
+    nParticles = els.getTotalNum();
+  
+    Centers.resize(nCenters,0);
+    CoreCoef.resize(nCenters,false);
+    CoreCoreDipole.resize(nCenters,0.0);
+    CoreElDipole.resize(nCenters,nParticles);
+    CoreElDipole = 0.0;
+  }
+  
+  LocalCorePolPotential::~LocalCorePolPotential() { 
+    for(int iat=0; iat<nCenters; iat++) { 
+    	if(Centers[iat]) delete Centers[iat];
+    }
+  }
+  
+  /** process xml node for each element
+   * @param cur xmlnode <element name="string" alpha="double" rb="double"/> 
+   */
+  bool LocalCorePolPotential::CPP_Param::put(xmlNodePtr cur) {
+    const xmlChar* a_ptr = xmlGetProp(cur,(const xmlChar *)"alpha");
+    const xmlChar* b_ptr = xmlGetProp(cur,(const xmlChar *)"rb");
+    if(a_ptr) alpha = atof((const char*)a_ptr);
+    if(b_ptr) r_b = atof((const char*)b_ptr);
+    C = -0.5*alpha;
+    one_over_rr = 1.0/r_b/r_b;
+    LOGMSG("\talpha = " << alpha << " rb = " << r_b)
+  }
+  
+  /** process xml node for CPP
+   * @param cur xmlnode containing element+
+   * 
+   * element/@name is used to find the index of the element of the 
+   * IonSys::SpeciesSet.
+   */
+  bool LocalCorePolPotential::put(xmlNodePtr cur){
+    string ename;
+    cur= cur->children;
+    bool success(false);
+    while(cur != NULL){
+      string cname((const char*)cur->name);
+      if(cname == "element"){
+      	const xmlChar* e_ptr = xmlGetProp(cur,(const xmlChar*)"name");
+      	if(e_ptr){
+          int itype = IonSys.getSpeciesSet().addSpecies((const char*)e_ptr);
+          if(Centers[itype]==0) Centers[itype]=new CPP_Param;
+          LOGMSG("CPP parameters for " << IonSys.getSpeciesSet().speciesName[itype])
+          success &= Centers[itype]->put(cur);
+      	}
+      }
+      cur=cur->next;
+	  }
+    return success;
+  }
+
+  LocalCorePolPotential::Return_t
+  LocalCorePolPotential::evaluate(ParticleSet& P) {
+
+    if(FirstTime) {
+      //index for attribute charge
+      SpeciesSet& Species(IonSys.getSpeciesSet());
+      int iz = Species.addAttribute("charge");
+      //calculate the Core-Core Dipole matrix
+      for(int iat=0; iat<nCenters; iat++) {
+        for(int nn=d_ii->M[iat]; nn<d_ii->M[iat+1]; nn++) { 
+          int jat(d_ii->J[nn]);
+          RealType rinv3 = pow(d_ii->rinv(nn),3);//(1/R_{JI}^3) R_{JI} = R_J-R_I
+          PosType dipole(rinv3*d_ii->dr(nn));//(\vec{R_{JI}}/R_{JI}^3)
+          //Sign and the charge of the paired ion are taken into account here 
+          CoreCoreDipole[iat] -= dipole*Species(iz,IonSys.GroupID[jat]);
+          CoreCoreDipole[jat] += dipole*Species(iz,IonSys.GroupID[iat]);
+        }
+      }
+
+      RealType corecore(0.0);
+      for(int iat=0; iat<nCenters; iat++) {
+        if(Centers[iat]) 
+        corecore+= Centers[iat]->C*dot(CoreCoreDipole[iat],CoreCoreDipole[iat]);
+      }
+      LOGMSG("Core-Core Dipole = " << corecore);
+      FirstTime=false;
+    }
+    //calculate the Electron-Core Dipole matrix
+    //CoreElDipole=0.0;
+
+    RealType e = 0.0;
+    for(int iat=0; iat<nCenters; iat++) {
+      if(Centers[iat]) {
+        PosType cc(CoreCoreDipole[iat]);
+        for(int nn=d_ie->M[iat]; nn<d_ie->M[iat+1]; nn++){
+          int eid(d_ie->J[nn]);
+          RealType rinv3 = pow(d_ie->rinv(nn),3);//(1/r^3)
+          PosType dipole = rinv3*d_ie->dr(nn);//(\vec{r}/r^3)
+          //cc +=  dipole*fcpp(d_ie->r(nn)*r_binv); 
+          cc += dipole*((*Centers[iat])(d_ie->r(nn)));
+        }
+        e += Centers[iat]->C*dot(cc,cc);
+      }
+    }
+    return e;
+  }
+}
+
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCHamiltonians/LocalCorePolPotential.h
@@ -0,0 +1,130 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003  by Jeongnim Kim and Jordan Vincent
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_LOCAL_COREPOLPOTENTIAL_H
+#define OHMMS_QMC_LOCAL_COREPOLPOTENTIAL_H
+#include "Particle/ParticleSet.h"
+#include "Particle/WalkerSetRef.h"
+#include "QMCHamiltonians/QMCHamiltonianBase.h"
+#include "OhmmsPETE/OhmmsMatrix.h"
+
+namespace ohmmsqmc {
+
+  /** The effective core polarization
+
+     \f[
+     V_{CPP} = -\frac{1}{2}\sum_{C}\alpha_C {\bf f}_C \cdot {\bf f}_C
+     \f]
+     Electric field which acts on core \f$C\f$ due to the charges of 
+     valence electrons \f$i\f$ and the other cores \f$C'\f$
+     \f[
+     {\bf f}_C = \sum_i \frac{{\bf r}_{Ci}}{r_{Ci}^3}C(r_{Ci},\rho_C)
+     -\sum_{C' \ne C} \frac{{\bf R}_{CC'}}{R_{CC'}^3}Z_{C'} = 
+     {\bf f}_C^e + {\bf f}_C^n
+     \f]
+     \f[
+     {\bf r}_{Ci} = {\bf r}_i - {\bf r}_C
+     \f]
+     \f[
+     {\bf R}_{CC'} = {\bf R}_{C'} - {\bf R}_{C}
+     \f]
+
+     \f$ C(r_{Ci},\rho_C) \f$ is a cut-off function for \f$ {\bf f}_C^e \f$ 
+     with an adjustable parameter \f$ \rho_C. \f$
+
+     \f[
+     V_{CPP} = -\frac{1}{2}\sum_C \left\{ \:\:\: \sum_i 
+     \frac{1}{r_{Ci}^4}C^2(r_{Ci},\rho_C) + 
+     \sum_{i \ne j} \frac{{\bf r}_{Ci} \cdot {\bf r}_{Ci}}{r^3_{Ci}r^3_{Ci}}
+     C(r_{Ci},\rho_C) C^2(r_{Cj},\rho_C) \right. 
+     \f]
+     \f[ 
+     -2 \left. \sum_i \sum_{C' \ne C} \frac{{\bf r}_{Ci} \cdot 
+     {\bf R}_{CC'}}{r^3_{Ci}R^3_{CC'}} Z_{C'}C(r_{Ci},\rho_C) 
+     + \left| \sum_{C' \ne C}  \frac{{\bf R}_{CC'}}{R^3_{CC'}} Z_{C'} 
+     \right|^2 \:\:\: \right\}
+     \f]
+  */
+  struct LocalCorePolPotential: public QMCHamiltonianBase {
+
+    struct CPP_Param {
+      RealType alpha, C,
+               r_b, one_over_rr;
+      inline CPP_Param(RealType a=1.0, RealType r=1.0): 
+        alpha(a),C(-0.5*a),r_b(r),one_over_rr(1/r/r){}
+
+      inline void set(RealType a, RealType r) {
+        alpha=a; C=-0.5*a;r_b=r; one_over_rr=1/r/r;
+      }
+      inline RealType operator()(RealType r) {
+        RealType z=1.0-exp(r*r*one_over_rr);
+        return z*z;
+      }
+      bool put(xmlNodePtr cur);
+    };
+
+    bool FirstTime;
+    ///the number of ions 
+    int nCenters;
+    ///the number of electrons
+    int nParticles;
+    ///the CoreCore term
+    RealType eCoreCore;
+
+    ///reference to the ionic system
+    ParticleSet& IonSys;
+    ///the ion-electron DistanceTable
+    DistanceTableData* d_ie;
+    ///the ion-ion DistanceTable
+    DistanceTableData* d_ii;
+
+    vector<CPP_Param*> Centers;
+
+    ///CoreCoef(C) = 1.0 if C=Ge, 0.0 for all other ions
+    vector<bool> CoreCoef;
+    ///CoreCoreDipole(C,C') \f$= \frac{Z_{C'} {\bf R_{CC'}}}{R_{CC'}^3}\f$
+    vector<PosType> CoreCoreDipole;
+    ///ElCoreDipole(C,i) \f$= \frac{{\bf r_{Ci}}f({\bar{r_{bCi}}}{r_{Ci}^3}\f$
+    Matrix<PosType> CoreElDipole;
+
+    ///constructor
+    LocalCorePolPotential(ParticleSet& ions, ParticleSet& els);
+
+    ~LocalCorePolPotential();
+
+    Return_t evaluate(ParticleSet& P);
+
+    inline Return_t 
+    evaluate(ParticleSet& P, RealType& x) {
+      return x=evaluate(P);
+    }
+
+    bool put(xmlNodePtr);
+
+    //inline RealType fcpp(RealType z) {
+    //  return pow((1.0-exp(-1.0*z*z)),2);
+    //}
+
+  };
+}
+#endif
+
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
+
