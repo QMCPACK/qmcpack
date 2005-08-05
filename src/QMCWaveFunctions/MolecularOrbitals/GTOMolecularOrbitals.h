fca2a6ba5c635bb1276049ec4ce699729c8e9b02
Jeongnim Kim <jeongnim.kim@intel.com>
2005-08-05 15:24:43 +0000

    Molecular Orbital Builder for analytic GTO.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@525 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/MolecularOrbitals/GTOMolecularOrbitals.h
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_GAUSSIANORBITAL_MOLECULARORBITALS_H
+#define OHMMS_QMC_GAUSSIANORBITAL_MOLECULARORBITALS_H
+
+#include "QMCWaveFunctions/OrbitalBuilderBase.h"
+#include "QMCWaveFunctions/SphericalOrbitalSet.h"
+#include "QMCWaveFunctions/MolecularOrbitals/MolecularOrbitalBasis.h"
+#include "Numerics/GaussianBasisSet.h"
+
+namespace ohmmsqmc {
+
+  /**Class to add a set of Gaussian Type atomic orbital basis functions 
+   *to the collection of basis functions.
+   *
+   @brief Example of a ROT (Radial Orbital Type)
+  */
+  class GTOMolecularOrbitals: public OrbitalBuilderBase {
+  public:
+
+    typedef GaussianCombo<RealType>                    RadialOrbitalType;
+    typedef SphericalOrbitalSet<RadialOrbitalType>     CenteredOrbitalType;
+    typedef MolecularOrbitalBasis<CenteredOrbitalType> BasisSetType;
+
+    ///constructor
+    GTOMolecularOrbitals(ParticleSet& els, TrialWaveFunction& wfs, ParticleSet& ions);
+
+    ///implement vritual function
+    bool put(xmlNodePtr cur);
+
+    ///returns a BasisSet
+    BasisSetType* addBasisSet(xmlNodePtr cur);
+
+  private:
+
+    enum {DONOT_EXPAND=0, GAUSSIAN_EXPAND=1, NATURAL_EXPAND};
+
+    bool Normalized;
+    BasisSetType*      BasisSet;
+    DistanceTableData* d_table;
+    map<string,int>    RnlID;
+    map<string,int>    CenterID;
+    ///map for (n,l,m,s) to its quantum number index
+    map<string,int> nlms_id;
+   
+    int expandYlm(const string& rnl, const QuantumNumberType& nlms, 
+                  int num, CenteredOrbitalType* aos, xmlNodePtr cur1,
+                  bool expandlm);
+
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
