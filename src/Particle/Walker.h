7b2be55a49d0a850643b7d63e59331d66fa5aee0
Jeongnim Kim <jeongnim.kim@intel.com>
2005-08-25 19:03:43 +0000

    Age is copied.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@596 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Particle/Walker.h
+++ src/Particle/Walker.h
@@ -116,8 +116,7 @@ namespace ohmmsqmc {
     ///copy the content of a walker
     inline void makeCopy(const Walker& a) {    
       resize(a.R.size());
-      Age=0;
-      //Age=a.Age;
+      Age=a.Age;
       Multiplicity=a.Multiplicity;
       R = a.R;
       Drift = a.Drift;
