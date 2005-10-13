137b4bf0ba974279ea705bf6970bf374969fc022
Jeongnim Kim <jeongnim.kim@intel.com>
2005-10-13 00:50:22 +0000

    Add read/write function to a hdf5 file for restart.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@699 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Utilities/BoostRandom.h
+++ src/Utilities/BoostRandom.h
@@ -24,8 +24,10 @@
 #endif
 
 #include <ctime>        
+#include <sstream>        
 #include <boost/random.hpp>
 #include "Message/Communicate.h"
+#include "OhmmsData/HDFAttribIO.h"
 
 template<class T>
 class BoostRandom {
@@ -33,37 +35,47 @@ class BoostRandom {
 public:
 
   typedef T Return_t;
-  typedef boost::minstd_rand base_generator_type;
-  typedef boost::mt19937     generator_type;
+  typedef boost::minstd_rand      base_generator_type;
+  typedef boost::mt19937          generator_type;
   typedef boost::variate_generator<generator_type,boost::uniform_real<T> > uniform_generator_type;
 
-  BoostRandom(): baseSeed(0), unit_dist(0,1), generator(0), uni(0) { 
-    //init(OHMMS::Controller->mycontext(),OHMMS::Controller->ncontexts(),-1);
-  }
+  BoostRandom(): baseSeed(0), state_array(0), unit_dist(0,1), generator(0), uni(0) { }
 
   BoostRandom(int iseed): 
     baseSeed(iseed), unit_dist(0,1), generator(0), uni(0)
-  {
-    //init(OHMMS::Controller->mycontext(),OHMMS::Controller->ncontexts(),iseed);
-  }
+  { }
  
   ~BoostRandom() {
     if(uni) delete uni;
     if(generator) delete generator;
+    if(state_array) delete [] state_array;
   }
 
-  void init(int i, int nstr, int iseed) {
-    if(iseed<0) {
-      iseed=static_cast<unsigned int>(std::time(0))%16081+(i+1)*nstr+i;
+  /** initialize the generator
+   * @param i thread index
+   * @param nstr number of threads
+   * @param iseed_in input seed
+   *
+   * Initialize generator with the seed. 
+   */
+  void init(int i, int nstr, int iseed_in) {
+    baseSeed=iseed_in;
+    if(iseed_in<=0) {
+      baseSeed=static_cast<uint32_t>(std::time(0))%16081+(i+1)*nstr+i;
     } 
-    baseSeed=iseed;
-    base_generator.seed(iseed);
+
+    //use the constructor with the seed
+    //generator->seed(baseSeed) is not working!!!
     if(generator == 0) {
-      generator = new generator_type(base_generator);
+      generator = new generator_type(baseSeed);
       uni = new uniform_generator_type(*generator,unit_dist);
+      std::stringstream a;
+      a << uni->engine();
+      state_size=a.str().size();
+      state_array=new char[state_size+128];
+      state_name="mt19937";
     }
-    //generator.seed(base_generator);
-    std::cout << "Node " << i << " seed " << iseed << std::endl;
+
   }
 
   inline uniform_generator_type& getGenerator() { return *uni;}
@@ -86,8 +98,51 @@ public:
     g2 = v2*fac;
   }
 
+  inline void read(std::istream& rin) {
+    rin >> uni->engine();
+  }
+
+  inline void write(std::ostream& rout) const {
+    rout << uni->engine();
+  }
+
+  /** read the state from a hdf5 group
+   * @param gid group id
+   */
+  inline void read(hid_t gid) {
+    hid_t dataset = H5Dopen(gid,state_name.c_str());
+    hid_t ret= H5Dread(dataset,H5T_NATIVE_CHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT,state_array);
+    H5Dclose(dataset);
+
+    std::istringstream a(state_array);
+    a >> uni->engine();
+  }
+
+  /** write the state to a hdf5 group
+   * @param gid group id
+   * @param overwrite if true, open the dataset
+   */
+  inline void write(hid_t gid, bool overwrite) {
+    std::ostringstream a;
+    a << uni->engine();
+    if(overwrite) {
+      hid_t dataset = H5Dopen(gid,state_name.c_str());
+      hid_t ret = H5Dwrite(dataset,H5T_NATIVE_CHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT,a.str().c_str());
+      H5Dclose(dataset);
+    } else {
+      hid_t dataspace = H5Screate_simple(1,&state_size,NULL);
+      hid_t dataset = H5Dcreate(gid,state_name.c_str(),H5T_NATIVE_CHAR,dataspace,H5P_DEFAULT);
+      hid_t ret = H5Dwrite(dataset,H5T_NATIVE_CHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT,a.str().c_str());
+      H5Dclose(dataset);
+      H5Sclose(dataspace);
+    }
+  }
+
 private:
-  unsigned int baseSeed;
+  uint32_t baseSeed;
+  char* state_array;
+  std::string state_name;
+  hsize_t state_size;
   base_generator_type    base_generator;
   boost::uniform_real<T> unit_dist;
   generator_type         *generator;
