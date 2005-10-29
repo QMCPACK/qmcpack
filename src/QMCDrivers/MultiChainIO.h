f3e5079514efab1f524de5c5aa04504482eed3d9
Jeongnim Kim <jeongnim.kim@intel.com>
2005-10-29 17:39:05 +0000

    I/O class to handle RMC restart.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@740 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCDrivers/MultiChainIO.h
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003- by Jeongnim Kim 
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMS_QMC_MULTICHAIN_HDF_IO_H
+#define OHMMS_QMC_MULTICHAIN_HDF_IO_H
+
+#include "OhmmsData/HDFAttribIO.h"
+#include "QMCDrivers/MultiChain.h"
+
+namespace ohmmsqmc {
+
+  /** Writes a set of walker configurations to an HDF5 file. */
+  class HDFMultiChainOutput {
+
+    bool AppendMode;
+    ///number of times file has been written to
+    int Counter;
+    ///version number
+    int m_version;
+    ///string file name
+    string h5file;
+
+  public:
+
+    HDFMultiChainOutput(const string& fname, int count);
+    ~HDFMultiChainOutput();
+
+    bool get(MultiChain&);
+
+    template<class CT>
+    bool write(CT& anything) {
+      anything.write(h_config);
+      return true;
+    }
+
+  };
+
+  /** Reads a set of walker configurations from an HDF5 file. */
+
+  class HDFMultiChainInput {
+
+    ///id of the first set
+    int FirstSet;
+    ///id of the last set
+    int LastSet;
+    ///number of times file has been accesed
+    int Counter;
+    ///number of sets of walker configurations
+    hsize_t NumSets;
+    ///id for HDF5 file 
+    hid_t h_file;
+    ///id for HDF5 main group 
+    hid_t h_config;
+
+  public:
+
+    HDFMultiChainInput(const string& fname);
+    ~HDFMultiChainInput();
+
+    ///version number
+    int m_version;
+    ///string file name
+    string h5file;
+
+    bool put(MultiChain&);
+
+    template<class CT>
+    bool read(CT& anything) {
+      anything.read(h_config);
+      return true;
+    }
+
+  };
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
