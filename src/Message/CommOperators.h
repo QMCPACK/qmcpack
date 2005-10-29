54eecf9386d8c65b325e69ef405ce1ad66297f05
Jeongnim Kim <jeongnim.kim@intel.com>
2005-10-29 17:27:57 +0000

    Add specializations for integer and vector<int>.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@735 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Message/CommOperators.h
+++ src/Message/CommOperators.h
@@ -24,9 +24,9 @@ template<class T> inline void gsum(T&, int) { }
 
 #ifdef HAVE_MPI
 template<>
-inline void gsum(double& g, int gid) {
-  double gt = g;
-  MPI_Allreduce(&(gt), &(g), 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+inline void gsum(int& g, int gid) {
+  int gt = g;
+  MPI_Allreduce(&(gt), &(g), 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 }
 
 template<unsigned N>
@@ -39,11 +39,34 @@ inline void gsum(TinyVector<double,N>& g, int gid) {
 }
 
 template<>
+inline void gsum(std::vector<int>& g, int gid) {
+  std::vector<int> gt(g.size(), 0.0);
+  MPI_Allreduce(&(g[0]),&(gt[0]),g.size(),MPI_INT,MPI_SUM,MPI_COMM_WORLD);
+  g = gt;
+}
+
+template<>
+inline void gsum(double& g, int gid) {
+  double gt = g;
+  MPI_Allreduce(&(gt), &(g), 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+}
+
+template<unsigned N>
+inline void gsum(TinyVector<int,N>& g, int gid) {
+  //TinyVector<double,N> gt = g;
+  //MPI_Allreduce(gt.begin(), g.begin(), N, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  TinyVector<int,N> gt(g);
+  MPI_Allreduce(g.begin(), gt.begin(), N, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  g = gt;
+}
+
+template<>
 inline void gsum(std::vector<double>& g, int gid) {
   std::vector<double> gt(g.size(), 0.0);
   MPI_Allreduce(&(g[0]),&(gt[0]),g.size(),MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
   g = gt;
 }
+
 
 #endif
 
