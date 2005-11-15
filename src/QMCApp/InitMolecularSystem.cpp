ea7f5fcca1a280400d25ff738b747109efbca49f
Jeongnim Kim <jeongnim.kim@intel.com>
2005-11-15 20:40:50 +0000

    *** empty log message ***
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@776 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCApp/InitMolecularSystem.cpp
+++ src/QMCApp/InitMolecularSystem.cpp
@@ -121,8 +121,8 @@ namespace qmcplusplus {
 
     vector<LoneElectron> loneQ;
 
+    double rmin=cutoff; 
     for(int iat=0; iat<Centers; iat++) { 
-      double rmin=cutoff; 
       for(int nn=d_ii->M[iat]; nn<d_ii->M[iat+1]; nn++){
         rmin = std::min(rmin,d_ii->r(nn));
       }
