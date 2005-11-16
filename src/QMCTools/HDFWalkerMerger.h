a95f3835e410a262b232193c745c447bbf70c45b
Jeongnim Kim <jeongnim.kim@intel.com>
2005-11-16 17:23:25 +0000

    Utility functions to merge hd5 files.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@788 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCTools/HDFWalkerMerger.h
@@ -0,0 +1,78 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2004- by Jeongnim Kim and Jordan Vincent
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_WALKER_MERGE_IO_H
+#define QMCPLUSPLUS_WALKER_MERGE_IO_H
+
+#include <string>
+#include "OhmmsPETE/OhmmsMatrix.h"
+#include "Numerics/HDFSTLAttrib.h"
+
+namespace qmcplusplus {
+
+  /** Writes a set of walker configurations to an HDF5 file. */
+
+  class HDFWalkerMerger {
+
+    ///the physical dimension
+    int Dimension;
+    ///the number of separate files
+    int NumCopy;
+    ///the number of configuration
+    int NumConfig;
+    ///the number of particles
+    int NumPtcl;
+
+    ///maxmimum number of walkers for any config
+    hsize_t MaxNumWalkers;
+    ///file root
+    std::string FileRoot;
+
+    ///numWalkerIn[node]->operator[](iconfig) returns the number of walkers
+    std::vector<std::vector<hsize_t>*> numWalkersIn;
+    /** offset of the walkers 
+     *
+     * OffSet(NumCopy,*) is the total number of walkers for the merged
+     * configuration.
+     */
+    Matrix<hsize_t>                    OffSet;
+
+    /** summary is accumulated */
+    std::vector<double>                Summary;
+
+    /** write header information */
+    void writeHeader(hid_t git);
+
+    /** initialize sizes */
+    void init();
+  public:
+
+    HDFWalkerMerger(const std::string& froot, int ncopy);
+    ~HDFWalkerMerger();
+
+    void merge();
+  };
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCTools/h5merge.cpp
@@ -0,0 +1,22 @@
+#include "Utilities/RandomGenerator.h"
+#include <vector>
+using namespace std;
+#include "QMCTools/HDFWalkerMerger.h"
+#include "Utilities/OhmmsInfo.h"
+
+int main(int argc, char **argv) {
+
+  OHMMS::Controller->initialize(argc,argv);
+
+  OhmmsInfo welcome(argc,argv,OHMMS::Controller->mycontext());
+  Random.init(0,1,-1);
+
+  if(argc<2) {
+    std::cerr << " Usage: h5merge rootname number-of-processor " << std::endl;
+    return 1;
+  }
+
+  qmcplusplus::HDFWalkerMerger merger(argv[1],atoi(argv[2]));
+  merger.merge();
+  return 0;
+}
