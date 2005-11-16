8bfee26f176104d35e0b8fcb769c31dd72e47749
Jeongnim Kim <jeongnim.kim@intel.com>
2005-11-16 16:26:03 +0000

    A master header file for Walker I/O classes.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@782 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Particle/HDFWalkerIO.h
+++ src/Particle/HDFWalkerIO.h
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////
-// (c) Copyright 2004- by Jeongnim Kim and Jordan Vincent
+// (c) Copyright 2005- by Jeongnim Kim
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //   Jeongnim Kim
@@ -13,89 +13,16 @@
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
-//   Department of Physics, Ohio State University
-//   Ohio Supercomputer Center
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
 #ifndef QMCPLUSPLUS_WALKER_IO_H
 #define QMCPLUSPLUS_WALKER_IO_H
 
-#include "OhmmsData/HDFAttribIO.h"
-
-namespace qmcplusplus {
-
-  /** Writes a set of walker configurations to an HDF5 file. */
-
-  class HDFWalkerOutput {
-
-    bool AppendMode;
-    ///number of times file has been written to
-    int Counter;
-    ///id for HDF5 file 
-    hid_t h_file;
-    ///id for HDF5 main group 
-    hid_t h_config;
-    ///id for the random number generator
-    hid_t h_random;
-  public:
-
-    HDFWalkerOutput(const string& fname, bool append=false, int count=0);
-    ~HDFWalkerOutput();
-    bool get(MCWalkerConfiguration&);
-
-    template<class CT>
-    void write(CT& anything) {
-      anything.write(h_config,AppendMode);
-    }
-
-    /** return the file ID **/
-    hid_t getFileID() { return h_file;}
-
-    /** return the config_collection file ID **/
-    hid_t getConfigID() { return h_config;}
-  };
-
-  /** Reads a set of walker configurations from an HDF5 file. */
-
-  class HDFWalkerInput {
-
-    ///id of the first set
-    int FirstSet;
-    ///id of the last set
-    int LastSet;
-    ///number of times file has been accesed
-    int Counter;
-    ///number of sets of walker configurations
-    int NumSets;
-    ///id for HDF5 file 
-    hid_t h_file;
-    ///id for HDF5 main group 
-    hid_t h_config;
-
-  public:
-
-    HDFWalkerInput(const string& fname, int ipart=0, int nparts=1);
-    ~HDFWalkerInput();
-    //int put(MCWalkerConfiguration&);
-    bool put(MCWalkerConfiguration&, int ic);
-
-    bool append(MCWalkerConfiguration& w);
-    bool append(MCWalkerConfiguration& w, int nwalkers);
-
-    template<class CT>
-    void read(CT& anything) {
-      anything.read(h_config);
-    }
+#include "Particle/HDFWalkerInput0.h"
+#include "Particle/HDFWalkerOutput.h"
+#include "Particle/HDFWalkerInputManager.h"
 
 
-    /** read the state of the number generator
-     * @param restart if true, read the state
-     */
-    void getRandomState(bool restart);
-
-  };
-
-}
 #endif
 /***************************************************************************
  * $RCSfile$   $Author$
