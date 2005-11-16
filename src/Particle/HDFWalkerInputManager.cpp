8298136eadc55ca3912e8ba6d9ffc96b3d419acf
Jeongnim Kim <jeongnim.kim@intel.com>
2005-11-16 16:23:57 +0000

    Manager class for input engines to handle old format and new format with collective functions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@781 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Particle/HDFWalkerInputManager.cpp
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "Particle/HDFWalkerInputManager.h"
+#include "Message/Communicate.h"
+#include "OhmmsData/AttributeSet.h"
+#include "Particle/HDFWalkerInput0.h"
+#include "Particle/HDFWalkerInputCollect.h"
+
+namespace qmcplusplus {
+
+  HDFWalkerInputManager::HDFWalkerInputManager(MCWalkerConfiguration& w):
+  Wref(w), CurrentFileRoot("invalid") {
+  }
+  HDFWalkerInputManager::~HDFWalkerInputManager() {
+  }
+
+  bool HDFWalkerInputManager::put(xmlNodePtr cur) {
+
+    app_error() << "HDFWalkerInputManager::put(xmlNodePtr cur) "
+      << " is not implemented." << endl;
+    return false;
+  }
+
+  bool HDFWalkerInputManager::put(std::vector<xmlNodePtr> wset) {
+    int pid=OHMMS::Controller->mycontext(); 
+    int nfile=wset.size();
+    for(int ifile=0; ifile<nfile; ifile++) {
+      string cfile("invalid"), target("e"), collect("no");
+      int anode=-1, nwalkers=-1, nblocks=1;
+      OhmmsAttributeSet pAttrib;
+      pAttrib.add(cfile,"href"); pAttrib.add(cfile,"file"); 
+      pAttrib.add(target,"target"); pAttrib.add(target,"ref"); 
+      pAttrib.add(anode,"node");
+      pAttrib.add(nwalkers,"walkers");
+      pAttrib.add(nblocks,"blocks");
+      pAttrib.add(collect,"collect");
+      pAttrib.put(wset[ifile]);
+
+      if(collect == "no") { // use old method
+        int pid_target= (anode<0)?pid:anode;
+        if(pid_target == pid && cfile != "invalid") {
+          HDFWalkerInput0 WO(cfile); 
+          WO.append(Wref,nwalkers);
+          //read random state
+          WO.getRandomState(true);
+          CurrentFileRoot = cfile;
+        }
+      } else {
+        HDFWalkerInputCollect WO(cfile);
+        WO.put(Wref,nblocks);
+        CurrentFileRoot = cfile;
+      }
+    }
+    return true;
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/Particle/HDFWalkerInputManager.h
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_WALKER_INPUT_MANAGER_H
+#define QMCPLUSPLUS_WALKER_INPUT_MANAGER_H
+
+#include "Particle/MCWalkerConfiguration.h"
+#include "OhmmsData/HDFAttribIO.h"
+#include <queue>
+
+namespace qmcplusplus {
+
+  class HDFWalkerInputManager {
+
+    MCWalkerConfiguration& Wref;
+    std::string CurrentFileRoot;
+
+    public:
+
+      HDFWalkerInputManager(MCWalkerConfiguration& w);
+      ~HDFWalkerInputManager();
+      bool put(xmlNodePtr cur);
+      bool put(std::vector<xmlNodePtr> mset);
+      std::string getLastFile() { return CurrentFileRoot;}
+    
+  };
+}
+
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
