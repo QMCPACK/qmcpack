b89f1badb4f0a212eb1df882c766633403cd87f7
Jeongnim Kim <jeongnim.kim@intel.com>
2005-11-22 03:24:49 +0000

    Matrix operators with inline static functions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@812 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Numerics/MatrixOperators.h
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//   Department of Physics, Ohio State University
+//   Ohio Supercomputer Center
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_MATRIXOPERATORS_H
+#define QMCPLUSPLUS_MATRIXOPERATORS_H
+#include "OhmmsPETE/OhmmsMatrix.h"
+struct MatrixOperators {
+
+  inline static void product(const Matrix<double>& A,
+      const Matrix<double>& B, Matrix<double>& C) {
+
+    const char transa = 'N';
+    const char transb = 'N';
+    const double one=1.0;
+    const double zero=0.0;
+    dgemm(transa, transb, B.cols(), A.rows(), B.rows(),
+        one, B.data(), B.cols(), A.data(), A.cols(),
+        zero, C.data(), C.cols());
+  }
+};
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
