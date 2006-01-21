36888e83f32069e80f63503402886d54ce4c78d5
Jeongnim Kim <jeongnim.kim@intel.com>
2006-01-21 15:49:41 +0000

    Factory class to build a QMCHamiltonian and to clone itself. HamiltonianFactory::RenamedProperty is used to handle naming issues.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@859 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCHamiltonians/HamiltonianFactory.cpp
@@ -0,0 +1,345 @@
+/////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/**@file HamiltonianFactory.cpp
+ *@brief Definition of a HamiltonianFactory 
+ */
+#include "QMCHamiltonians/HamiltonianFactory.h"
+#include "QMCHamiltonians/QMCHamiltonian.h"
+#include "QMCHamiltonians/BareKineticEnergy.h"
+#include "QMCHamiltonians/CoulombPotential.h"
+#include "QMCHamiltonians/IonIonPotential.h"
+#include "QMCHamiltonians/LocalPPotential.h"
+#include "QMCHamiltonians/NonLocalPPotential.h"
+#include "QMCHamiltonians/LocalCorePolPotential.h"
+#if !defined(QMCPLUSPLUS_RELEASE)
+#include "QMCHamiltonians/CoulombPBC.h"
+#endif
+#include "OhmmsData/AttributeSet.h"
+
+namespace qmcplusplus {
+  HamiltonianFactory::HamiltonianFactory(ParticleSet* qp, 
+    PtclPoolType& pset, OrbitalPoolType& oset): 
+    targetPtcl(qp), targetH(0), 
+  ptclPool(pset),psiPool(oset), myNode(NULL), psiName("psi0") 
+  {}
+
+  bool HamiltonianFactory::build(xmlNodePtr cur, bool buildtree) {
+
+    if(cur == NULL) return false;
+
+    string htype("generic"), source("i");
+    OhmmsAttributeSet hAttrib;
+    hAttrib.add(htype,"type"); 
+    hAttrib.add(source,"source");
+    hAttrib.put(cur);
+
+    renameProperty(source);
+
+    bool attach2Node=false;
+    if(buildtree) {
+      if(myNode == NULL) {
+        myNode = xmlCopyNode(cur,2);
+      } else {
+        attach2Node=true;
+      }
+    }
+
+    if(targetH==0) {
+      targetH  = new QMCHamiltonian;
+      targetH->setName(myName);
+      targetH->addOperator(new BareKineticEnergy,"Kinetic");
+    }
+
+    cur = cur->children;
+    while(cur != NULL) {
+      string cname((const char*)cur->name);
+      const xmlChar* t = xmlGetProp(cur,(const xmlChar*)"type");
+      if(t) { // accept only if it has type
+        string pot_type((const char*)t);
+        if(cname == "pairpot") {
+          if(pot_type == "coulomb") {
+	    addCoulombPotential(cur);
+          } else if(pot_type == "pseudo") {
+            addPseudoPotential(cur);
+          } else if(pot_type == "cpp") {
+            addCorePolPotential(cur);
+          }
+        } else if(cname == "constant") { 
+          if(pot_type == "coulomb") { //ugly!!!
+            t = xmlGetProp(cur,(const xmlChar*)"source");
+            string nuclei("i");
+            if(t) nuclei=(const char*)t;
+
+            renameProperty(nuclei);
+
+            PtclPoolType::iterator pit(ptclPool.find(nuclei));
+            if(pit != ptclPool.end()) {
+              ParticleSet* ion=(*pit).second;
+              if(ion->getTotalNum()>1) 
+		if(ion->Lattice.BoxBConds[0]){
+#if defined(QMCPLUSPLUS_RELEASE)
+                  ERRORMSG("This version cannot handle PBC. The IonIon potential will be wrong.")
+		  targetH->addOperator(new IonIonPotential(*ion),"IonIon");
+#else
+		  targetH->addOperator(new CoulombPBCAA(*ion),"IonIon");
+#endif
+		}
+		else {
+		  targetH->addOperator(new IonIonPotential(*ion),"IonIon");
+		}
+             }
+          }
+        }
+      }
+      if(attach2Node) xmlAddChild(myNode,xmlCopyNode(cur,1));
+      cur = cur->next;
+    }
+
+    if(targetH->size() == 1) {//no external potential is provided, use type
+
+      WARNMSG("Using pre-determined hamiltonian for molecular systems.")
+
+      PtclPoolType::iterator pit(ptclPool.find(source));
+      if(pit == ptclPool.end()) {
+        ERRORMSG("No ionic system " << source << " exists.")
+        return false;
+      }
+      ParticleSet* ion=(*pit).second;
+      if(targetPtcl->Lattice.BoxBConds[0]){
+#if defined(QMCPLUSPLUS_RELEASE)
+        ERRORMSG("This version cannot handle PBC. The ElecElec potential will be wrong.")
+	targetH->addOperator(new CoulombPotentialAA(*targetPtcl),"ElecElec");
+#else
+	targetH->addOperator(new CoulombPBCAA(*targetPtcl),"ElecElec");
+#endif
+      }
+      else{
+	targetH->addOperator(new CoulombPotentialAA(*targetPtcl),"ElecElec");
+      }
+
+      if(htype == "molecule" || htype=="coulomb"){
+	if(targetPtcl->Lattice.BoxBConds[0]){
+#if defined(QMCPLUSPLUS_RELEASE)
+          ERRORMSG("This version cannot handle PBC. The Coulomb potential will be wrong.")
+	  targetH->addOperator(new CoulombPotentialAB(*ion,*targetPtcl),"Coulomb");
+#else
+	  targetH->addOperator(new CoulombPBCAB(*ion,*targetPtcl),"Coulomb");
+#endif
+	} else {
+	  targetH->addOperator(new CoulombPotentialAB(*ion,*targetPtcl),"Coulomb");
+	}
+      } else if(htype == "siesta" || htype=="pseudo") {
+        if(!psiPool.empty())  {
+          TrialWaveFunction* psi = (*(psiPool.begin())).second->targetPsi;
+          targetH->addOperator(new NonLocalPPotential(*ion,*targetPtcl,*psi),"NonLocal");
+        }
+      //} else if(htype == "cpp") {
+      //  xmlChar* att2=xmlGetProp(cur,(const xmlChar*)"species");
+      //  string stype("Ge");
+      //  if(att2) stype = (const char*)att2;
+      //  targetH->add(new LocalPPotential(*ion,*qp), "PseudoPot");
+      //  targetH->add(new LocalCorePolPotential(*ion,*qp), "GeCPP");
+      } else {
+        ERRORMSG(htype << " is diabled")
+      }
+
+      if(ion->getTotalNum()>1) 
+	if(ion->Lattice.BoxBConds[0]){
+#if defined(QMCPLUSPLUS_RELEASE)
+          ERRORMSG("This version cannot handle PBC. The IonIon potential will be wrong.")
+          targetH->addOperator(new IonIonPotential(*ion),"IonIon");
+#else
+          targetH->addOperator(new CoulombPBCAA(*ion),"IonIon");
+#endif
+	}
+	else{
+          targetH->addOperator(new IonIonPotential(*ion),"IonIon");
+	}
+    }
+
+    return true;
+  }
+
+  void 
+  HamiltonianFactory::addCoulombPotential(xmlNodePtr cur) {
+
+    string a("e"),title("ElecElec");
+    OhmmsAttributeSet hAttrib;
+    hAttrib.add(title,"id"); hAttrib.add(title,"name"); 
+    hAttrib.add(a,"source"); 
+    hAttrib.put(cur);
+
+    renameProperty(a);
+
+    PtclPoolType::iterator pit(ptclPool.find(a));
+    if(pit == ptclPool.end()) {
+      ERRORMSG("Missing source ParticleSet" << a)
+      return;
+    }
+
+    ParticleSet* source = (*pit).second;
+
+    if(source == targetPtcl) {
+      //CHECK PBC and create CoulombPBC for el-el
+      if(source->getTotalNum()>1)  {
+	  if(targetPtcl->Lattice.BoxBConds[0]) {
+#if defined(QMCPLUSPLUS_RELEASE)
+            ERRORMSG("This version cannot handle PBC. The " << title << " potential will be wrong.")
+	    targetH->addOperator(new CoulombPotentialAA(*targetPtcl),title);
+#else
+	    targetH->addOperator(new CoulombPBCAA(*targetPtcl),title);
+#endif
+	  }
+	  else {
+	    targetH->addOperator(new CoulombPotentialAA(*targetPtcl),title);
+	  }
+      }
+    } else {
+      if(targetPtcl->Lattice.BoxBConds[0]) {
+#if defined(QMCPLUSPLUS_RELEASE)
+        ERRORMSG("This version cannot handle PBC. The " << title << " potential will be wrong.")
+	targetH->addOperator(new CoulombPotentialAB(*source,*targetPtcl),title);
+#else
+	targetH->addOperator(new CoulombPBCAB(*source,*targetPtcl),title);
+#endif
+      } else {
+	targetH->addOperator(new CoulombPotentialAB(*source,*targetPtcl),title);
+      }
+    }
+  }
+
+  void 
+  HamiltonianFactory::addPseudoPotential(xmlNodePtr cur) {
+
+    string src("i"),title("PseudoPot"),wfname("invalid");
+
+    OhmmsAttributeSet pAttrib;
+    pAttrib.add(title,"name");
+    pAttrib.add(src,"source");
+    pAttrib.add(wfname,"wavefunction");
+    pAttrib.put(cur);
+
+    renameProperty(src);
+    renameProperty(wfname);
+
+    PtclPoolType::iterator pit(ptclPool.find(src));
+    if(pit == ptclPool.end()) {
+      ERRORMSG("Missing source ParticleSet" << src)
+      return;
+    }
+
+    ParticleSet* ion=(*pit).second;
+
+    OrbitalPoolType::iterator oit(psiPool.find(wfname));
+    TrialWaveFunction* psi=0;
+    if(oit == psiPool.end()) {
+      if(psiPool.empty()) return;
+      cout << "  Cannot find " << wfname << " in the pool "<< endl;
+      psi=(*(psiPool.begin())).second->targetPsi;
+    } else {
+      psi=(*oit).second->targetPsi;
+    }
+
+    //remember the TrialWaveFunction used by this pseudopotential
+    psiName=wfname;
+    targetH->addOperator(new NonLocalPPotential(*ion,*targetPtcl,*psi), title);
+  }
+
+  void 
+  HamiltonianFactory::addCorePolPotential(xmlNodePtr cur) {
+
+    string src("i"),title("CorePol");
+
+    OhmmsAttributeSet pAttrib;
+    pAttrib.add(title,"name");
+    pAttrib.add(src,"source");
+    pAttrib.put(cur);
+
+    PtclPoolType::iterator pit(ptclPool.find(src));
+    if(pit == ptclPool.end()) {
+      ERRORMSG("Missing source ParticleSet" << src)
+      return;
+    }
+    ParticleSet* ion=(*pit).second;
+
+    QMCHamiltonianBase* cpp=(new LocalCorePolPotential(*ion,*targetPtcl));
+    cpp->put(cur); 
+    targetH->addOperator(cpp, title);
+  }
+
+  void HamiltonianFactory::renameProperty(const string& a, const string& b){
+    RenamedProperty[a]=b;
+  }
+
+  void HamiltonianFactory::setCloneSize(int np) {
+    myClones.resize(np,0);
+  }
+
+  //TrialWaveFunction*
+  //HamiltonianFactory::cloneWaveFunction(ParticleSet* qp, int ip) {
+  //  HamiltonianFactory* aCopy= new HamiltonianFactory(qp,ptclPool);
+  //  aCopy->put(myNode,false);
+  //  myClones[ip]=aCopy;
+  //  return aCopy->targetPsi;
+  //}
+
+  void HamiltonianFactory::renameProperty(string& aname) {
+    map<string,string>::iterator it(RenamedProperty.find(aname));
+    if(it != RenamedProperty.end()) {
+      aname=(*it).second;
+    }
+  }
+  HamiltonianFactory::~HamiltonianFactory() {
+    //clean up everything
+  }
+
+  HamiltonianFactory*
+  HamiltonianFactory::clone(ParticleSet* qp, TrialWaveFunction* psi, 
+      int ip, const string& aname) {
+    HamiltonianFactory* aCopy=new HamiltonianFactory(qp, ptclPool, psiPool);
+    aCopy->setName(aname);
+
+    aCopy->renameProperty("e",qp->getName());
+    aCopy->renameProperty(psiName,psi->getName());
+
+    aCopy->build(myNode,false);
+    myClones[ip]=aCopy;
+
+    aCopy->get(app_log());
+    return aCopy;
+  }
+
+  bool HamiltonianFactory::get(std::ostream& os) const {
+    targetH->get(os);
+    return true;
+  }
+
+  bool HamiltonianFactory::put(std::istream& ) {
+    return true;
+  }
+
+  bool HamiltonianFactory::put(xmlNodePtr cur) {
+    return build(cur,true);
+  }
+
+  void HamiltonianFactory::reset() { }
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCHamiltonians/HamiltonianFactory.h
@@ -0,0 +1,103 @@
+/////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/**@file HamiltonianFactory.h
+ *@brief Declaration of a HamiltonianFactory 
+ */
+#ifndef QMCPLUSPLUS_HAMILTONIAN_FACTORY_H
+#define QMCPLUSPLUS_HAMILTONIAN_FACTORY_H
+
+#include "QMCWaveFunctions/WaveFunctionFactory.h"
+#include "QMCHamiltonians/QMCHamiltonian.h"
+namespace qmcplusplus {
+
+  /** Factory class to build a many-body wavefunction 
+   */
+  struct HamiltonianFactory: public OhmmsElementBase {
+
+    typedef map<string,ParticleSet*> PtclPoolType;
+    typedef map<string,WaveFunctionFactory*> OrbitalPoolType;
+
+    ///target ParticleSet
+    ParticleSet* targetPtcl;
+    ///many-body wavefunction object
+    QMCHamiltonian* targetH;
+    ///reference to the PtclPoolType
+    PtclPoolType&  ptclPool;
+    ///reference to the WaveFunctionFactory Pool
+    OrbitalPoolType&  psiPool;
+    ///input node for a many-body wavefunction
+    xmlNodePtr myNode;
+    
+    ///name of the TrialWaveFunction
+    string psiName;
+
+    ///list of the old to new name
+    map<string,string> RenamedProperty;
+
+    ///constructor
+    HamiltonianFactory(ParticleSet* qp, PtclPoolType& pset, OrbitalPoolType& oset);
+
+    ///destructor
+    ~HamiltonianFactory();
+
+    ///write to ostream
+    bool get(std::ostream& ) const;
+
+    ///read from istream
+    bool put(std::istream& );
+
+    ///read from xmlNode
+    bool put(xmlNodePtr cur);
+
+    ///reset member data
+    void reset();
+
+    /** process xmlNode to populate targetPsi
+     */
+    bool build(xmlNodePtr cur, bool buildtree=true);
+
+    void addCoulombPotential(xmlNodePtr cur);
+    void addPseudoPotential(xmlNodePtr cur);
+    void addCorePolPotential(xmlNodePtr cur);
+
+    /** add a property whose name will be renamed by b
+     * @param a target property whose name should be replaced by b
+     * @param b new property name
+     */
+    void renameProperty(const string& a, const string& b);
+
+    /** renamd a property
+     * @param a current name
+     *
+     * If a is found among the RenamedProperty, a is replaced,
+     */
+    void renameProperty(string& a);
+
+    void setCloneSize(int np);
+
+    HamiltonianFactory* clone(ParticleSet* qp, TrialWaveFunction* psi,
+        int ip, const string& aname);
+
+    vector<HamiltonianFactory*> myClones;
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
