1f7b3db9b93725b59b9b614bdd2c1189f7fc48a0
Jeongnim Kim <jeongnim.kim@intel.com>
2006-01-22 18:39:55 +0000

    Pass random generator as an argument.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@879 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/ParticleBase/RandomSeqGenerator.h
+++ src/ParticleBase/RandomSeqGenerator.h
@@ -13,56 +13,72 @@
   *\param n the number of type T to be assigned
   *\brief Assign Gaussian distributed random numbers using Box-Mueller algorithm. Called by overloaded funtions makeGaussRandom
   */
-template<class T> 
-inline void assignGaussRand(T* restrict a, unsigned n) {
+//template<class T> 
+//inline void assignGaussRand(T* restrict a, unsigned n) {
+//  for (int i=0; i+1<n; i+=2) {
+//    T temp1=1-0.9999999999*Random(), temp2=Random();
+//    a[i]  =sqrt(-2.0*log(temp1))*cos(6.283185306*temp2);
+//    a[i+1]=sqrt(-2.0*log(temp1))*sin(6.283185306*temp2);
+//  }
+//  if (n%2==1) {
+//    T temp1=1-0.9999999999*Random(), temp2=Random();
+//    a[n-1]=sqrt(-2.0*log(temp1))*cos(6.283185306*temp2);
+//  }
+//}
+
+template<class T, class RG> 
+inline void assignGaussRand(T* restrict a, unsigned n, RG& rng) {
   for (int i=0; i+1<n; i+=2) {
-    T temp1=1-0.9999999999*Random(), temp2=Random();
+    T temp1=1-0.9999999999*rng(), temp2=rng();
     a[i]  =sqrt(-2.0*log(temp1))*cos(6.283185306*temp2);
     a[i+1]=sqrt(-2.0*log(temp1))*sin(6.283185306*temp2);
   }
   if (n%2==1) {
-    T temp1=1-0.9999999999*Random(), temp2=Random();
+    T temp1=1-0.9999999999*rng(), temp2=rng();
     a[n-1]=sqrt(-2.0*log(temp1))*cos(6.283185306*temp2);
   }
 }
-
 /*!\fn template<class T> void assignUniformRand(T* restrict a, unsigned n)
   *\param a the starting pointer
   *\param n the number of type T to be assigned
   *\brief Assign unifor distributed random numbers [0,1)
   */
-template<class T>
-inline void assignUniformRand(T* restrict a, unsigned n) {
-  for(int i=0; i<n;i++) a[i] = Random();
-  //generate(a,a+n,Random);
+//template<class T>
+//inline void assignUniformRand(T* restrict a, unsigned n) {
+//  for(int i=0; i<n;i++) a[i] = Random();
+//  //generate(a,a+n,Random);
+//}
+template<class T, class RG>
+inline void assignUniformRand(T* restrict a, unsigned n, RG& rng) {
+  for(int i=0; i<n;i++) a[i] = rng();
 }
 
 #if defined(HAVE_LIBBLITZ)
 ///specialized functions: stick to overloading
 inline void makeGaussRandom(blitz::Array<TinyVector<double,3>, 2>& a) {
-  assignGaussRand(&(a(0,0)[0]), a.size()*3);
+  assignGaussRand(&(a(0,0)[0]), a.size()*3, Random);
 }
 #endif
 
 ///specialized functions: stick to overloading
 inline void makeGaussRandom(Matrix<TinyVector<double,3> >& a) {
-  assignGaussRand(&(a(0,0)[0]), a.size()*3);
+  assignGaussRand(&(a(0,0)[0]), a.size()*3, Random);
 }
 
 inline void makeGaussRandom(ParticleAttrib<TinyVector<double,3> >& a) {
-  assignGaussRand(&(a[0][0]), a.size()*3);
+  assignGaussRand(&(a[0][0]), a.size()*3, Random);
 }
 
 inline void makeGaussRandom(ParticleAttrib<double>& a) {
-  assignGaussRand(&(a[0]), a.size());
+  assignGaussRand(&(a[0]), a.size(), Random);
 }
 
 inline void makeUniformRandom(ParticleAttrib<TinyVector<double,3> >& a) {
-  assignUniformRand(&(a[0][0]), a.size()*3);
+  assignUniformRand(&(a[0][0]), a.size()*3, Random);
 }
 
 inline void makeUniformRandom(ParticleAttrib<double>& a) {
-  assignUniformRand(&(a[0]), a.size());
+  assignUniformRand(&(a[0]), a.size(), Random);
 }
 inline void makeSphereRandom(ParticleAttrib<TinyVector<double,3> >& a) {
   for(int i=0; i<a.size(); i++) {
@@ -79,6 +95,11 @@ inline void makeSphereRandom(ParticleAttrib<TinyVector<double,3> >& a) {
       }
     }
   }
+}
+
+template<class RG>
+inline void makeGaussRandomWithEngine(ParticleAttrib<TinyVector<double,3> >& a, RG& rng) {
+  assignGaussRand(&(a[0][0]), a.size()*3, rng);
 }
 
 #endif
