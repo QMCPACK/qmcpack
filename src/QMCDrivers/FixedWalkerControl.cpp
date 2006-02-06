90f63fc8e14ff606ecb89dc7d5dc9f916e5c5fa7
Jeongnim Kim <jeongnim.kim@intel.com>
2006-02-06 16:37:20 +0000

    Adding reconfiguration method for fixed-number-of-walker DMC.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@898 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCDrivers/FixedWalkerControl.cpp
@@ -0,0 +1,378 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "QMCDrivers/FixedWalkerControl.h"
+#include "Utilities/IteratorUtility.h"
+#include "Utilities/UtilityFunctions.h"
+using namespace qmcplusplus;
+
+/** default constructor
+ *
+ * set SwapMode
+ */
+FixedWalkerControl::FixedWalkerControl() {
+  SwapMode=1;
+  NumContexts=OHMMS::Controller->ncontexts();
+  MyContext=OHMMS::Controller->mycontext();
+
+#ifdef MCWALKERSET_MPI_DEBUG
+  char fname[128];
+  sprintf(fname,"test.%d",MyContext);
+  ofstream fout(fname);
+#endif
+}
+
+void FixedWalkerControl::getIndexPermutation(MCWalkerConfiguration& W) {
+
+  MCWalkerConfiguration::iterator it(W.begin());
+  MCWalkerConfiguration::iterator it_end(W.end());
+  RealType wtot=0.0;
+  int nw=0;
+  while(it != it_end) {
+    wtot += wConf[nw]=(*it)->Weight;
+    ++nw;++it;
+  }
+
+  RealType nwInv=1.0/static_cast<RealTyle>(nw);
+  RealType dstep=UnitZeta*nwInv;
+
+  for(int iw=0; iw<nw;iw++) {
+    Zeta[iw]=wtot*(dstep+static_cast<RealType>(iw)*nwInv);
+  }
+
+  //assign negative
+  std::fill(IndexCopy.begin(),IndexCopy.end(),-1);
+
+  int ind=0;
+  RealType wCur=0.0;
+  //surviving walkers
+  int icdiff=0;
+  it=W.begin();
+  for(int iw=0; iw<nw; iw++) {
+    RealType tryp=wCur+fabs(wConf[iw]);
+    int ni=0;
+    do {
+      IndexCopy[ind]=iw;
+      ind++;
+      ni++;
+    } while(Zeta[iw]<tryp && Zeta[iw] >= wCur);
+    wCur+=fabs(wConf[iw]);
+    if(ni) {
+      icdiff++;
+    } 
+  }
+
+  shuffleIndex(nw);
+}
+
+void FixedWalkerControl::shuffleIndex(int nw) {
+  vector<int> ipip(nw,0);
+  for(int iw=0; iw<nw; iw++) ipip(IndexCopy[iw])+=1;
+
+  vector<int> indz;
+  for(int iw=0; iw<nw; iw++) {
+    if(ipip[iw]==0) {
+      indz.push_back(iw);
+    }
+  }
+
+  int ikilled=0;
+  for(int iw=0; iw<nw; iw++) {
+    if(ipip[iw] != 0) {
+      IndexCopy[iw]=iw;
+      for(int i=1;i<ipip[iw]; i++) {
+        IndexCopy[indz[ikilled++]]=iw;
+      }
+    }
+  }
+
+}
+/** Convert M. Casula's branchinggo.f90 */
+int 
+FixedWalkerControl::branch(int iter, MCWalkerConfiguration& W, RealType trigger) {
+
+  getIndexPermutation(W);
+
+  //set Weight and Multiplicity to default values
+  MCWalkerConfiguration::iterator it(W.begin()),it_end(W.end());
+  while(it != it_end) {
+    (*it)->Weight= 1.0;
+    (*it)->Multiplicity=1.0;
+    ++it;
+  }
+
+  return Cur_pop;
+}
+
+/** swap Walkers with Irecv/Send 
+ *
+ * The algorithm ensures that the load per node can differ only by one walker.
+ * The communication is one-dimensional. 
+ */
+void FixedWalkerControl::swapWalkersAsync(MCWalkerConfiguration& W) {
+  NumSwaps++;
+
+  OffSet[0]=0;
+  for(int i=0; i<NumContexts; i++) OffSet[i+1]=OffSet[i]+NumPerNode[i];
+  FairDivideLow(Cur_pop,NumContexts,FairOffSet);
+
+  int toLeft=FairOffSet[MyContext]-OffSet[MyContext];
+  int toRight=FairOffSet[MyContext+1]-OffSet[MyContext+1];
+
+  Walker_t& wRef(*W[0]);
+
+  int num_deleted=0;
+  int last=NumPerNode[MyContext]-1;
+
+  OOMPI_Packed* recvLeftBuffer=0;
+  OOMPI_Packed* recvRightBuffer=0;
+  OOMPI_Request recvLeftRequest;
+  OOMPI_Request recvRightRequest;
+
+  //initiate Irecv
+  if(toLeft<0) { // recv from node-1 
+    int dn=-toLeft;
+    recvLeftBuffer = new OOMPI_Packed(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    recvLeftRequest = OOMPI_COMM_WORLD[MyContext-1].Irecv(*recvLeftBuffer,MPI_ANY_TAG);
+  } 
+  if(toRight>0) { //recv from node+1
+    recvRightBuffer = new OOMPI_Packed(toRight*wRef.byteSize(),OOMPI_COMM_WORLD);
+    recvRightRequest = OOMPI_COMM_WORLD[MyContext+1].Irecv(*recvRightBuffer,MPI_ANY_TAG);
+  }
+  
+  //send the data
+  if(toLeft>0) { //send to node-1
+    int dn=toLeft;
+    num_deleted+=dn;
+    OOMPI_Packed sendBuffer(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    while(dn) {
+      W[last]->putMessage(sendBuffer); --dn; --last;
+    }
+    OOMPI_COMM_WORLD[MyContext-1].Send(sendBuffer);
+  }
+  if(toRight<0) { // send to node+1
+    int dn=-toRight;
+    num_deleted+=dn;
+    OOMPI_Packed sendBuffer(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    while(dn) {
+      W[last]->putMessage(sendBuffer); --dn; --last;
+    }
+    OOMPI_COMM_WORLD[MyContext+1].Send(sendBuffer);
+  } 
+
+  while(num_deleted>0) {
+    W.pop_back(); --num_deleted;
+  }
+
+  if(recvLeftBuffer) {
+    recvLeftRequest.Wait();
+    int dn=-toLeft;
+    while(dn) {
+      Walker_t *awalker= new Walker_t(wRef);
+      awalker->getMessage(*recvLeftBuffer);
+      W.push_back(awalker);
+      --dn;
+    }
+    delete recvLeftBuffer;
+    recvLeftBuffer=0;
+  }
+
+  if(recvRightBuffer) {
+    recvRightRequest.Wait();
+    int dn=toRight;
+    while(dn) {
+      Walker_t *awalker= new Walker_t(wRef);
+      awalker->getMessage(*recvRightBuffer);
+      W.push_back(awalker);
+      --dn;
+    }
+    delete recvRightBuffer;
+    recvRightBuffer=0;
+  }
+}
+
+/** swap Walkers with Recv/Send 
+ *
+ * The algorithm ensures that the load per node can differ only by one walker.
+ * The communication is one-dimensional. 
+ */
+void FixedWalkerControl::swapWalkersBlocked(MCWalkerConfiguration& W) {
+  NumSwaps++;
+
+  OffSet[0]=0;
+  for(int i=0; i<NumContexts; i++) OffSet[i+1]=OffSet[i]+NumPerNode[i];
+  FairDivideLow(Cur_pop,NumContexts,FairOffSet);
+
+  int toLeft=FairOffSet[MyContext]-OffSet[MyContext];
+  int toRight=FairOffSet[MyContext+1]-OffSet[MyContext+1];
+
+  Walker_t& wRef(*W[0]);
+
+  vector<Walker_t*> newW;
+
+  int num_deleted=0;
+  int last=NumPerNode[MyContext]-1;
+  //scehdule irecv
+  if(toLeft<0) { // recv from node-1 
+    int dn=-toLeft;
+    OOMPI_Packed recvBuffer(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    OOMPI_COMM_WORLD[MyContext-1].Recv(recvBuffer);
+    while(dn) {
+      Walker_t *awalker= new Walker_t(wRef);
+      awalker->getMessage(recvBuffer);
+      newW.push_back(awalker);
+      --dn;
+    }
+  } else if(toLeft>0) { //send to node-1
+    int dn=toLeft;
+    num_deleted+=dn;
+    OOMPI_Packed sendBuffer(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    while(dn) {
+      W[last]->putMessage(sendBuffer); --dn; --last;
+    }
+    OOMPI_COMM_WORLD[MyContext-1].Send(sendBuffer);
+  }
+
+  if(toRight<0) { // send to node+1
+    int dn=-toRight;
+    num_deleted+=dn;
+    OOMPI_Packed sendBuffer(dn*wRef.byteSize(),OOMPI_COMM_WORLD);
+    while(dn) {
+      W[last]->putMessage(sendBuffer); --dn; --last;
+    }
+    OOMPI_COMM_WORLD[MyContext+1].Send(sendBuffer);
+  } else if(toRight>0) { //recv from node+1
+    OOMPI_Packed recvBuffer(toRight*wRef.byteSize(),OOMPI_COMM_WORLD);
+    OOMPI_COMM_WORLD[MyContext+1].Recv(recvBuffer);
+    int dn=toRight;
+    while(dn) {
+      Walker_t *awalker= new Walker_t(wRef);
+      awalker->getMessage(recvBuffer);
+      newW.push_back(awalker);
+      --dn;
+    }
+  }
+
+  while(num_deleted>0) {
+    W.pop_back(); --num_deleted;
+  }
+
+  if(newW.size()) W.insert(W.end(),newW.begin(),newW.end());
+}
+
+/** swap walkers using (low,high) ordered pairs
+ *
+ * The communication occur only between the (low,high) pairs.
+ * This does not guarantee a perfect balance swapWalkersBlocked and swapWalkersAsync
+ * try to achieve. However, the number of messages and their sizes are less than
+ * other methods.
+ */
+void FixedWalkerControl::swapWalkersMap(MCWalkerConfiguration& W) {
+
+  NumSwaps++;
+  multimap<int,int> nw_map;
+  for(int i=0; i<NumContexts; i++) {
+    nw_map.insert(pair<int,int>(NumPerNode[i],i));
+  }
+  // multimap key is sorted with ascending order 
+  multimap<int,int>::iterator it(nw_map.begin());
+  multimap<int,int>::reverse_iterator it_b(nw_map.end());
+  bool notpaired=true;
+  int target_context=-1;
+  int half=NumContexts/2;
+  int item=0;
+  bool minorcontext;
+  while(notpaired &&item<half) {
+    int i=(*it).second;
+    int j=(*it_b).second;
+    if(i == MyContext) {
+      target_context=j;
+      notpaired=false;
+      minorcontext=true;
+    } else if(j == MyContext) {
+      target_context= i;
+      notpaired=false;
+      minorcontext=false;
+    } 
+    ++it; ++it_b; ++item;
+  }
+
+  int nw_tot=NumPerNode[MyContext]+NumPerNode[target_context];
+  int nw_L=nw_tot/2;
+  int nw_R=nw_tot-nw_L;
+  int dnw(0);
+  if(minorcontext) {
+    dnw=nw_L-NumPerNode[MyContext];//how many to get
+  } else {
+    dnw=NumPerNode[MyContext]-nw_R;//how many to send
+  }
+
+  if(dnw) {//something to swap
+    if(minorcontext) {//open recv buffer
+      Walker_t& wRef(*W[0]);
+      OOMPI_Packed recvBuffer(dnw*wRef.byteSize(),OOMPI_COMM_WORLD);
+      OOMPI_COMM_WORLD[target_context].Recv(recvBuffer);
+      //create walkers
+      int last = W.getActiveWalkers();
+      while(dnw) {
+        Walker_t *awalker= new Walker_t(wRef);
+        awalker->getMessage(recvBuffer);
+        W.push_back(awalker);
+        --dnw; ++last;
+      }
+    } else {
+      Walker_t& wRef(*W[0]);
+      OOMPI_Packed sendBuffer(dnw*wRef.byteSize(),OOMPI_COMM_WORLD);
+      int last=W.getActiveWalkers()-1;
+      while(dnw) {
+        W[last]->putMessage(sendBuffer);
+        --dnw; --last;
+      }
+      OOMPI_COMM_WORLD[target_context].Send(sendBuffer);
+      //last=WalkerList.size()-1;
+      //while(dnw_save) {
+      //  delete WalkerList[last]; 
+      //  WalkerList.pop_back();
+      //  --dnw_save; --last;
+      //}
+      //destroyWalkers(WalkerList.begin()+nsub[MyContext], WalkerList.end());
+      W.destroyWalkers(W.begin()+nw_R, W.end());
+    }
+  }
+
+  /* not used yet
+  struct lessNode {
+    inline bool operator()(const pair<int,int>& a,
+        const pair<int,int>& b) const {
+      return a.second < b.second;
+    }
+  };
+  //typedef pair<int,int> mytype;
+  //vector<mytype> id2n(NumContexts);
+  //for(int i=0; i<NumContexts; i++) {
+  //  id2n=pair<int,int>(i,NumPerNode[i]);
+  //}
+  //
+  //std::sort(id2n.begin(),id2n.end(),lessNode);
+  */
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
+
---
+++ src/QMCDrivers/FixedWalkerControl.h
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_RECONFIGURATION_WALKER_CONTROL_H
+#define QMCPLUSPLUS_RECONFIGURATION_WALKER_CONTROL_H
+
+#include "QMCDrivers/WalkerControlBase.h"
+
+namespace qmcplusplus {
+
+  /** Class to handle walker controls with simple global sum
+   *
+   * Base class to handle serial mode with branching only
+   */
+  struct FixedWalkerControl: public WalkerControlBase {
+
+    ///number of mpi contexts
+    int NumContexts;
+    /// context on a node
+    int MyContext;
+    //random number [0,1)
+    RealType UnitZeta;
+
+    vector<int>      IndexCopy;
+    //weight per walker
+    vector<RealType> wConf;
+    //comb
+    vector<RealType> Zeta;
+    /** default constructor
+     *
+     * Set the SwapMode to zero so that instantiation can be done
+     */
+    FixedWalkerControl();
+
+    /** perform branch and swap walkers as required */
+    int branch(int iter, MCWalkerConfiguration& W, RealType trigger);
+
+    /** collect the energies */
+    inline void collect(TinyVector<RealType,2>& eavgwgt) {
+      gsum(eavgwgt,0);
+    }
+
+    void swapWalkersAsync(MCWalkerConfiguration& W);
+    void swapWalkersBlocked(MCWalkerConfiguration& W);
+    void swapWalkersMap(MCWalkerConfiguration& W);
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
+
