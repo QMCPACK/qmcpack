5c0387ab32a0489441c8e222f46d075e2890661c
qmc <qmc>
2006-04-21 05:27:35 +0000

    Improved algorithm for finding parallelpiped containing a sphere and sped up initialization of map from index of a k vector to the index of -k vector
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@986 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/LongRange/KContainer.cpp
+++ src/LongRange/KContainer.cpp
@@ -1,5 +1,6 @@
 #include "Message/Communicate.h"
 #include "LongRange/KContainer.h"
+#include <map>
 
 using namespace qmcplusplus;
 
@@ -73,36 +74,10 @@ KContainer::FindApproxMMax() {
   //Estimate the size of the parallelpiped that encompases a sphere of kcutoff.
   //mmax is stored as integer translations of the reciprocal cell vectors.
   //Does not depend on orthorhombic cells.
-  Matrix<RealType> mmat;
-  mmat.resize(3,3);
-  for(int j=0;j<3;j++)
-    for(int i=0;i<3;i++){
-      mmat[i][j] = 0.0;
-      for(int k=0;k<3;k++)
-	mmat[i][j] = mmat[i][j] + Lattice.b(k)[i]* Lattice.b(j)[k];
-    }
-	  
-  TinyVector<RealType,3> x,temp;
-  RealType tempr;
-  for(int idim=0;idim<3;idim++){
-    int i = ((idim-1)%3);
-    int j = (idim%3);
-    int k = ((idim+1)%3);
-    
-    x[i] = 1.0;
-    x[j] = (mmat[j][k]*mmat[k][i] - mmat[k][k]*mmat[i][j]);
-    x[j]/= (mmat[j][j]*mmat[k][k] - mmat[j][k]*mmat[j][k]);
-    x[k] = -(mmat[k][i] + mmat[j][k]*x[j])/mmat[k][k];
-    
-    for(i=0;i<3;i++){
-      temp[i] = 0.0;
-	for(j=0;j<3;j++)
-	  temp[i] += mmat[i][j]*x[j];
-    }
-    tempr = dot(x,temp);
 
-    mmax[idim] = static_cast<int>(sqrt(4.0*kcut2)) + 1;  
-  }
+  // see rmm, Electronic Structure, p. 85 for details
+  for (int i = 0; i < 3; i++) 
+    mmax[i] = static_cast<int>(floor(sqrt(dot(Lattice.a(i),Lattice.a(i))) * kcutoff / (2 * M_PI))) + 1;
 }
 
 void 
@@ -170,22 +145,18 @@ KContainer::BuildKLists() {
 
   //Update a record of the number of k vectors
   numk = kpts.size();
-
+  
   //Now fill the array that returns the index of -k when given the index of k.
   minusk.resize(numk);
+
+  // Create a map from the hash value for each k vector to the index
+  std::map<int, int> hashToIndex;
+  for (int ki=0; ki<numk; ki++) {
+    hashToIndex[GetHashOfVec(kpts[ki], numk)] = ki;
+  }
+
+  // Use the map to find the index of -k from the index of k
   for(int ki=0; ki<numk; ki++) {
-    int kj=0;
-    do {
-      if(kpts[ki][0] == -kpts[kj][0] 
-	 &&kpts[ki][1] == -kpts[kj][1] 
-	 &&kpts[ki][2] == -kpts[kj][2])
-	break;
-      kj++;
-    } while(kj<numk);
-    if(kj==numk){
-      LOGMSG("Error finding -K in list");
-      OHMMS::Controller->abort();
-    }
-    minusk[ki] = kj;
+    minusk[ki] = hashToIndex[ GetHashOfVec(-1 * kpts[ki], numk) ];
   }
 }
