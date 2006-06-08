ff90cb6465c853face2dfcb301e3928e390c5b4f
Jeongnim Kim <jeongnim.kim@intel.com>
2006-06-08 22:08:24 +0000

    Builder class for Two-Body PBC jastrow functions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1127 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/JAAPBCBuilder.cpp
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////// // (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "Utilities/OhmmsInfo.h"
+#include "QMCWaveFunctions/JAAPBCBuilder.h"
+#include "QMCWaveFunctions/LRTwoBodyJastrow.h"
+
+namespace qmcplusplus {
+
+  /** constructor
+   * @param p target ParticleSet whose wave function is to be initialized
+   *@param psi the wavefunction
+   *@param psets a vector containing the ParticleSets
+   * 
+   *Jastrow wavefunctions chose the needed distance tables and the 
+   *DistanceTableData objects are initialized based on the source 
+   *and target particle sets.
+   */
+  JAAPBCBuilder::JAAPBCBuilder(ParticleSet& p, TrialWaveFunction& psi):
+    OrbitalBuilderBase(p,psi)
+  { }
+
+  bool JAAPBCBuilder::put(xmlNodePtr cur) {
+    LRTwoBodyJastrow *J2 = new LRTwoBodyJastrow(targetPtcl);
+    bool success = J2->put(cur, targetPsi.VarList);
+    J2->setOptimizable(true);
+    targetPsi.addOrbital(J2);
+    return success;
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
