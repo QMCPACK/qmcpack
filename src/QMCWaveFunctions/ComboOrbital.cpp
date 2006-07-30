8267d73864de1735bd26588adc84ec400cdf1f72
Jeongnim Kim <jeongnim.kim@intel.com>
2006-07-30 13:48:41 +0000

    A composite Orbital with constraints.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1211 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/ComboOrbital.cpp
@@ -0,0 +1,127 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file ComboOrbital.cpp
+ * @brief Definitions of ComboOrbital
+ */
+#include "QMCWaveFunctions/ComboOrbital.h"
+#include "Utilities/IteratorUtility.h"
+
+namespace qmcplusplus {
+
+  ComboOrbital::~ComboOrbital() {
+    delete_iter(Psi.begin(), Psi.end());
+    delete Constraints;
+  }
+
+  void ComboOrbital::reset() {
+    Constraints->apply();
+  }
+
+  void ComboOrbital::resetTargetParticleSet(ParticleSet& P) {
+    for(int i=0; i<Psi.size(); i++) Psi[i]->resetTargetParticleSet(P);
+  }
+
+  ComboOrbital::ValueType
+    ComboOrbital::evaluateLog(ParticleSet& P, 
+        ParticleSet::ParticleGradient_t& G, ParticleSet::ParticleLaplacian_t& L) {
+      LogValue=0.0;
+      for(int i=0; i<Psi.size(); i++) 
+        LogValue += Psi[i]->evaluateLog(P,G,L);
+      return LogValue;
+    }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::ratio(ParticleSet& P, int iat,
+        ParticleSet::ParticleGradient_t& dG,
+        ParticleSet::ParticleLaplacian_t& dL) {
+      RealType r(1.0);
+      for(int i=0; i<Psi.size(); i++) 
+        r *= Psi[i]->ratio(P,iat,dG,dL);
+      return r;
+    }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::ratio(ParticleSet& P, int iat) {
+      RealType r(1.0);
+      for(int i=0; i<Psi.size(); i++) 
+        r *= Psi[i]->ratio(P,iat);
+      return r;
+    }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::logRatio(ParticleSet& P, int iat,
+        ParticleSet::ParticleGradient_t& dG,
+        ParticleSet::ParticleLaplacian_t& dL) {
+      RealType r(0.0);
+      for(int i=0; i<Psi.size(); i++) 
+        r += Psi[i]->logRatio(P,iat,dG,dL);
+      return r;
+    }
+
+  void ComboOrbital::acceptMove(ParticleSet& P, int iat) {
+    for(int i=0; i<Psi.size(); i++) 
+      Psi[i]->acceptMove(P,iat);
+  }
+
+  void ComboOrbital::restore(int iat) {
+    for(int i=0; i<Psi.size(); i++) 
+      Psi[i]->restore(iat);
+  }
+
+  void ComboOrbital::update(ParticleSet& P, 
+      ParticleSet::ParticleGradient_t& dG, 
+      ParticleSet::ParticleLaplacian_t& dL,
+      int iat) {
+    for(int i=0; i<Psi.size(); i++) 
+      Psi[i]->update(P,dG,dL,iat);
+  }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::registerData(ParticleSet& P, BufferType& buf) {
+      LogValue=0.0;
+      for(int i=0; i<Psi.size(); i++) 
+        LogValue += Psi[i]->registerData(P,buf);
+      return LogValue;
+    }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::updateBuffer(ParticleSet& P, BufferType& buf) {
+      LogValue=0.0;
+      for(int i=0; i<Psi.size(); i++) 
+        LogValue += Psi[i]->updateBuffer(P,buf);
+      return LogValue;
+    }
+
+  void 
+    ComboOrbital::copyFromBuffer(ParticleSet& P, BufferType& buf){
+      for(int i=0; i<Psi.size(); i++) 
+        Psi[i]->copyFromBuffer(P,buf);
+    }
+
+  ComboOrbital::ValueType 
+    ComboOrbital::evaluate(ParticleSet& P,BufferType& buf) {
+      LogValue=0.0;
+      for(int i=0; i<Psi.size(); i++) 
+        LogValue += Psi[i]->evaluate(P,buf);
+      return LogValue;
+    }
+}
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
---
+++ src/QMCWaveFunctions/ComboOrbital.h
@@ -0,0 +1,102 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file ComboOrbital.h
+ * @brief Declaration of a Composite Orbital
+ */
+#ifndef QMCPLUSPLUS_GENERIC_COMBO_WITHCONSTRAINTS_H
+#define QMCPLUSPLUS_GENERIC_COMBO_WITHCONSTRAINTS_H
+#include "QMCWaveFunctions/OrbitalBase.h"
+#include "QMCWaveFunctions/OrbitalConstraintsBase.h"
+
+namespace qmcplusplus {
+
+  /** A composite Orbital
+   */
+  struct ComboOrbital: public OrbitalBase {
+
+    ///A list of OrbitalBase* 
+    vector<OrbitalBase*> Psi;
+    ///Contraints on Psi
+    OrbitalConstraintsBase* Constraints;
+
+    ComboOrbital(OrbitalConstraintsBase* control):
+      Constraints(control) {
+        Optimizable=true;
+      }
+
+    ~ComboOrbital();
+
+    void setContraints(OrbitalConstraintsBase* control) {
+      Constraints=control;
+    }
+
+    void reset();
+
+    void resetTargetParticleSet(ParticleSet& P);
+
+    ValueType
+      evaluate(ParticleSet& P, 
+          ParticleSet::ParticleGradient_t& G, 
+          ParticleSet::ParticleLaplacian_t& L) {
+        return std::exp(evaluateLog(P,G,L));
+      }
+
+    ValueType
+      evaluateLog(ParticleSet& P, 
+          ParticleSet::ParticleGradient_t& G, ParticleSet::ParticleLaplacian_t& L);
+
+    ValueType 
+      ratio(ParticleSet& P, int iat,
+          ParticleSet::ParticleGradient_t& dG,
+          ParticleSet::ParticleLaplacian_t& dL);
+
+    ValueType 
+      ratio(ParticleSet& P, int iat);
+
+    ValueType 
+      logRatio(ParticleSet& P, int iat,
+          ParticleSet::ParticleGradient_t& dG,
+          ParticleSet::ParticleLaplacian_t& dL);
+
+    void acceptMove(ParticleSet& P, int iat);
+
+    void restore(int iat);
+
+    void update(ParticleSet& P, 
+        ParticleSet::ParticleGradient_t& dG, 
+        ParticleSet::ParticleLaplacian_t& dL,
+        int iat);
+
+    ValueType 
+      registerData(ParticleSet& P, BufferType& buf);
+
+    ValueType 
+      updateBuffer(ParticleSet& P, BufferType& buf);
+
+    void 
+      copyFromBuffer(ParticleSet& P, BufferType& buf);
+
+    ValueType 
+      evaluate(ParticleSet& P,BufferType& buf);
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
