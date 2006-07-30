ade1f086534fcf0ca9f4672914225d83fffa1da1
Jeongnim Kim <jeongnim.kim@intel.com>
2006-07-30 21:04:07 +0000

    *** empty log message ***
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1222 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/OrbitalSetTraits.h
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file BasisSetBase.h
+ * @brief Declaration of a base class of BasisSet
+ */
+#ifndef QMCPLUSPLUS_ORBITALSETTRAITS_H
+#define QMCPLUSPLUS_ORBITALSETTRAITS_H
+
+#include "Configuration.h"
+#include "OhmmsPETE/OhmmsMatrix.h"
+#include "OhmmsPETE/OhmmsVector.h"
+
+namespace qmcplusplus {
+
+  /** trait class to handel a set of Orbitals
+   */
+  struct OrbitalSetTraits: public QMCTraits {
+    enum {MAXINDEX=2+DIM};
+    typedef Vector<ValueType> ValueVector_t;
+    typedef Matrix<ValueType> ValueMatrix_t;
+    typedef Vector<GradType>  GradVector_t;
+    typedef Matrix<GradType>  GradMatrix_t;
+  };
+}
+
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
