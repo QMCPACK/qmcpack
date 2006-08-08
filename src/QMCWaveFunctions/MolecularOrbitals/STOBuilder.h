3e614ea3832e57be2f149653c2311dc3a3bf3450
Jeongnim Kim <jeongnim.kim@intel.com>
2006-08-08 16:52:55 +0000

    A radial-functor builder for analytic STO functors for MolecularBasisBuilder and AtomicBasisBuilder.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1240 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/MolecularOrbitals/STOBuilder.h
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_SLATERTYPEORBITAL_BUILDER_H
+#define QMCPLUSPLUS_SLATERTYPEORBITAL_BUILDER_H
+
+#include "Configuration.h"
+#include "QMCWaveFunctions/SphericalOrbitalSet.h"
+#include "Numerics/SlaterBasisSet.h"
+
+namespace qmcplusplus {
+
+  class STOBuilder: public QMCTraits {
+
+  public:
+
+    typedef SlaterCombo<RealType>                      RadialOrbitalType;
+    typedef SphericalOrbitalSet<RadialOrbitalType>     CenteredOrbitalType;
+
+    ///true, if the RadialOrbitalType is normalized
+    bool Normalized;
+    ///the radial orbitals
+    CenteredOrbitalType* m_orbitals;
+    ///the species
+    std::string m_species;
+    ///constructor
+    STOBuilder(xmlNodePtr cur=NULL);
+
+    ///assign a CenteredOrbitalType to work on
+    void setOrbitalSet(CenteredOrbitalType* oset, const std::string& acenter) { 
+      m_orbitals = oset;
+      m_species = acenter;
+    }
+
+    bool addGrid(xmlNodePtr cur) { return true;}
+
+    bool addRadialOrbital(xmlNodePtr cur, const QuantumNumberType& nlms);
+
+    bool putCommon(xmlNodePtr cur);
+
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author$
+ * $Revision$   $Date$
+ * $Id$ 
+ ***************************************************************************/
