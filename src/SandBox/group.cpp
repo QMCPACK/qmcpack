a66f3e3ad0b9cfe12db2444fdb09c44f8854f3ad
Jeongnim Kim <jeongnim.kim@intel.com>
2006-08-20 17:51:34 +0000

    *** empty log message ***
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1291 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/SandBox/group.cpp
+++ src/SandBox/group.cpp
@@ -6,6 +6,7 @@
 #include "Utilities/RandomGenerator.h"
 #include "Utilities/Timer.h"
 #include "Message/Communicate.h"
+#include "Message/CommunicateGroup.h"
 using namespace qmcplusplus;
 
 int main(int argc, char** argv) {
@@ -19,41 +20,47 @@ int main(int argc, char** argv) {
   std::cout.setf(std::ios::right,std::ios::adjustfield);
   std::cout.precision(12);
 
-  const int ndims=2;
-  int dims[]={2,2};
-  int coords[ndims];
-  bool periods[]={false,false};
+  double sumL=0.0,sumG=0.0;
+  for(int i=0; i<1000; i++) sumL += Random();
 
   int ndiv=atoi(argv[1]);
+  Communicate newComm(OHMMS::Controller->split(ndiv));
+  std::cout << OHMMS::Controller->mycontext() << " Rank = " << newComm.mycontext() << " Size = " << newComm.ncontexts() << " " << sumL << std::endl;
+  MPI_Allreduce(&(sumL), &(sumG), 1, MPI_DOUBLE, MPI_SUM, newComm.getID());
 
-  int p=OHMMS::Controller->mycontext()/ndiv;
-  int q=OHMMS::Controller->mycontext()%ndiv;
-
+  //int p=OHMMS::Controller->mycontext()/ndiv;
+  //int q=OHMMS::Controller->mycontext()%ndiv;
+  //MPI_Comm row;
+  //MPI_Comm_split(OHMMS::Controller->getID(),p,q,&row);
+  //int new_rank, new_size;
+  //MPI_Comm_rank(row,&new_rank);
+  //MPI_Comm_size(row,&new_size);
+  //std::cout << OHMMS::Controller->mycontext() << " Rank = " << new_rank << " Size = " << new_size << " " << sumL << std::endl;
+  //MPI_Allreduce(&(sumL), &(sumG), 1, MPI_DOUBLE, MPI_SUM, row);
+ 
+  std::cout << OHMMS::Controller->mycontext() << " Local sum = " << sumL << " Global sum " << sumG << std::endl;
+  //const int ndims=2;
+  //int dims[]={2,2};
+  //int coords[ndims];
+  //bool periods[]={false,false};
   //MPI_Comm what=c.Get_mpi();
   //OOMPI_Cart_comm cart(OOMPI_COMM_WORLD,ndims,dims,periods);
   //cart.Coords(coords);
-
   //std::cout << " Rank = " << cart.Rank() << " " << coords[0] << " " << coords[1] << std::endl;
   //
   //std::cout << "World communicator " << OHMMS::Controller->getID() <<std::endl;
   //OOMPI_Intra_comm c(OHMMS::Controller->getID());
-
+  //THIS DOES NOT WORK NO IDEA
   //c.Split(p,q);
   //std::cout << c.Get_mpi() << " Rank = " << c.Rank() << " Size = " << c.Size() << std::endl;
 
-  MPI_Comm row;
-  MPI_Comm_split(OHMMS::Controller->getID(),p,q,&row);
-  int new_rank, new_size;
-  MPI_Comm_rank(row,&new_rank);
-  MPI_Comm_size(row,&new_size);
 
-  double sumL=0.0,sumG=0.0;
+  //alternatively, use group
+  CommunicateGroup testGroup(*(OHMMS::Controller),ndiv);
+  sumL=0.0;
   for(int i=0; i<1000; i++) sumL += Random();
-
-  std::cout << OHMMS::Controller->mycontext() << " Rank = " << new_rank << " Size = " << new_size << " " << sumL << std::endl;
-
-  MPI_Allreduce(&(sumL), &(sumG), 1, MPI_DOUBLE, MPI_SUM, row);
-
+  std::cout << OHMMS::Controller->mycontext() << " Rank = " << testGroup.mycontext() << " Size = " << testGroup.ncontexts() << " " << sumL << std::endl;
+  MPI_Allreduce(&(sumL), &(sumG), 1, MPI_DOUBLE, MPI_SUM, testGroup.getID());
   std::cout << OHMMS::Controller->mycontext() << " Local sum = " << sumL << " Global sum " << sumG << std::endl;
 
   OHMMS::Controller->finalize();
