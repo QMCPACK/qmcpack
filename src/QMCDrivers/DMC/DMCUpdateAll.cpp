04e0c8178e3491f06e742dd864611d0d6dc31524
Jeongnim Kim <jeongnim.kim@intel.com>
2006-09-04 20:31:13 +0000

    Branch is done simulatenously.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1313 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/DMC/DMCUpdateAll.cpp
+++ src/QMCDrivers/DMC/DMCUpdateAll.cpp
@@ -96,15 +96,16 @@ namespace qmcplusplus {
       }
 
       thisWalker.Weight *= branchEngine->branchGF(Tau,emixed,0.0);
-      if(MaxAge) {
-        RealType M=thisWalker.Weight;
-        if(thisWalker.Age > MaxAge) M = std::min(0.5,M);
-        else if(thisWalker.Age > 0) M = std::min(1.0,M);
-        thisWalker.Multiplicity = M + RandomGen();
-        branchEngine->accumulate(eold,M);
-      } else {
-        branchEngine->accumulate(eold,1);
-      }
+      branchEngine->accumulate(eold,1);
+      //if(MaxAge) {
+      //  RealType M=thisWalker.Weight;
+      //  if(thisWalker.Age > MaxAge) M = std::min(0.5,M);
+      //  else if(thisWalker.Age > 0) M = std::min(1.0,M);
+      //  thisWalker.Multiplicity = M + RandomGen();
+      //  branchEngine->accumulate(eold,M);
+      //} else {
+      //  branchEngine->accumulate(eold,1);
+      //}
 
       if(accepted) 
         ++nAccept;
