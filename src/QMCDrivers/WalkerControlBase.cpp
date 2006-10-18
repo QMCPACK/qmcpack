772c79453ead0ffe2d0df6f3c4994ada418a949a
Jeongnim Kim <jeongnim.kim@intel.com>
2006-10-18 16:59:03 +0000

    Added estimator capabilities. WalkerControlBase is owned by a DMCEnergyEstimator to reduce  MPI operations.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1392 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/WalkerControlBase.cpp
+++ src/QMCDrivers/WalkerControlBase.cpp
@@ -18,9 +18,25 @@
 #include "QMCDrivers/WalkerControlBase.h"
 namespace qmcplusplus {
 
+  WalkerControlBase::WalkerControlBase(): 
+    SwapMode(0), Nmin(1), Nmax(10), MaxCopy(10) {
+      accumData.resize(LE_MAX);
+      curData.resize(LE_MAX);
+  }
+
+  void WalkerControlBase::reset() {
+    std::fill(accumData.begin(),accumData.end(),0.0);
+  }
+
   int WalkerControlBase::branch(int iter, MCWalkerConfiguration& W, RealType trigger) {
 
     sortWalkers(W);
+
+    RealType wgtInv(1.0/curData[WEIGHT_INDEX]);
+    accumData[ENERGY_INDEX]     += curData[ENERGY_INDEX]*wgtInv;
+    accumData[ENERGY_SQ_INDEX]  += curData[ENERGY_SQ_INDEX]*wgtInv;
+    accumData[WALKERSIZE_INDEX] += curData[WALKERSIZE_INDEX];
+    accumData[WEIGHT_INDEX]     += curData[WEIGHT_INDEX];
 
     int nw_tot = copyWalkers(W);
 
@@ -35,6 +51,9 @@ namespace qmcplusplus {
     return nw_tot;
   }
 
+
+  /** evaluate curData and mark the bad/good walkers
+   */
   void WalkerControlBase::sortWalkers(MCWalkerConfiguration& W) {
 
     MCWalkerConfiguration::iterator it(W.begin());
@@ -42,8 +61,15 @@ namespace qmcplusplus {
     vector<Walker_t*> bad;
     NumWalkers=0;
     MCWalkerConfiguration::iterator it_end(W.end());
+    RealType esum=0.0,e2sum=0.0,wsum=0.0,ecum=0.0;
     while(it != it_end) {
       int nc = std::min(static_cast<int>((*it)->Multiplicity),MaxCopy);
+      RealType wgt((*it)->Weight);
+      RealType e((*it)->Properties(LOCALENERGY));
+      esum += wgt*e;
+      e2sum += wgt*e*e;
+      wsum += wgt;
+      ecum += e;
       if(nc) {
         NumWalkers += nc;
         good_w.push_back(*it);
@@ -54,6 +80,16 @@ namespace qmcplusplus {
       ++it;
     }
 
+    //temp is an array to perform reduction operations
+    std::fill(curData.begin(),curData.end(),0);
+
+    //update curData
+    curData[ENERGY_INDEX]=esum;
+    curData[ENERGY_SQ_INDEX]=e2sum;
+    curData[WALKERSIZE_INDEX]=W.getActiveWalkers();
+    curData[WEIGHT_INDEX]=wsum;
+    curData[EREF_INDEX]=ecum;
+    
     //remove bad walkers empty the container
     for(int i=0; i<bad.size(); i++) delete bad[i];
 
@@ -106,15 +142,6 @@ namespace qmcplusplus {
     ncopy_w.clear();
     return W.getActiveWalkers();
   }
-
-//#if defined(HAVE_MPI)
-//  WalkerControlBase::RealType WalkerControlBase::average(RealType eavg, RealType wgt) {
-//    gEavgWgt[0]=eavg;
-//    gEavgWgt[1]=wgt;
-//    gsum(gEavgWgt,0);
-//    return gEavgWgt[0]/gEavgWgt[1];
-//  }
-//#endif
 
 }
 /***************************************************************************
--- src/QMCDrivers/WalkerControlBase.h
+++ src/QMCDrivers/WalkerControlBase.h
@@ -25,14 +25,24 @@
 
 namespace qmcplusplus {
 
-  /** Class to handle walker controls
+  /** Base class to control the walkers for DMC simulations.
    *
-   * Base class to handle serial mode with branching only
+   * The virtual functions are implemented for a serial execution with a usual birth/death
+   * process. Inherited classes implement other WalkerControl algorithms by implementing
+   * branch function.
+   * - 2006/10/18
+   * -- curData and accumData are added.
+   * -- branch function handles averaging of the local energies to apply the weight over the 
+   *   current walker sample correctly. 
+   * -- removes an extra global reduction.
+   *   --- curData should be used for global reductions
    */
   struct WalkerControlBase: public QMCTraits {
 
     ///typedef of Walker_t
     typedef MCWalkerConfiguration::Walker_t Walker_t;
+
+    enum {ENERGY_INDEX=0, ENERGY_SQ_INDEX, WALKERSIZE_INDEX, WEIGHT_INDEX, EREF_INDEX, LE_MAX};
 
     ///0 is default
     int SwapMode;
@@ -44,12 +54,12 @@ namespace qmcplusplus {
     IndexType MaxCopy;
     ///current number of walkers per processor
     IndexType NumWalkers;
-    ///Global energy/weight
-    TinyVector<RealType,2> gEavgWgt;
-
+    ///any accumulated data over a block
+    vector<RealType> accumData;
+    ///any temporary data
+    vector<RealType> curData;
     ///temporary storage for good walkers
     vector<Walker_t*> good_w;
-
     ///temporary storage for copy counters
     vector<int> ncopy_w;
 
@@ -57,11 +67,26 @@ namespace qmcplusplus {
      *
      * Set the SwapMode to zero so that instantiation can be done
      */
-    WalkerControlBase(): SwapMode(0), Nmin(1), Nmax(10), MaxCopy(10) {}
+    WalkerControlBase();
 
-    /** empty destructor to clean up the derived classes */
-    virtual ~WalkerControlBase() {}
-    
+    /** return a value accumulated during a block
+     * @param i index of the data
+     *
+     * use enum for i, see DMCEnergyEstimator
+     */
+    inline RealType getValue(int i) {
+      return accumData[i];
+    }
+
+    /** return a current value 
+     * @param i index of the data
+     *
+     * use enum for i, see DMCEnergyEstimator
+     */
+    inline RealType getCurrentValue(int i) {
+      return curData[i];
+    }
+
     /** sort Walkers between good and bad and prepare branching
      */
     void sortWalkers(MCWalkerConfiguration& W);
@@ -69,30 +94,18 @@ namespace qmcplusplus {
      */
     int copyWalkers(MCWalkerConfiguration& W);
 
+    /** empty destructor to clean up the derived classes */
+    virtual ~WalkerControlBase() {}
+
+    /** reset to accumulate data */
+    virtual void reset();
+    
     /** perform branch and swap walkers as required */
     virtual int branch(int iter, MCWalkerConfiguration& W, RealType trigger);
 
     virtual RealType getFeedBackParameter(int ngen, RealType tau) {
       return 1.0/(static_cast<RealType>(ngen)*tau);
     }
-
-#if defined(HAVE_MPI)
-
-    /** collect the energies */
-    inline RealType average(RealType eavg, RealType wgt) {
-      gEavgWgt[0]=eavg;
-      gEavgWgt[1]=wgt;
-      gsum(gEavgWgt,0);
-      return gEavgWgt[0]/gEavgWgt[1];
-    }
-#else
-
-    /** collect the energies */
-    inline RealType average(RealType eavg, RealType wgt) {
-      gEavgWgt[0]=eavg; gEavgWgt[1]=wgt;
-      return eavg/wgt;
-    }
-#endif
   };
 
 }
