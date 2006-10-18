a595ae030b6a7cca10c75138dcc6d7d2c5e327e0
Jeongnim Kim <jeongnim.kim@intel.com>
2006-10-18 15:58:06 +0000

    Changes due to "de-templating ScalarEstimatorBase".
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1382 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Estimators/LocalEnergyEstimator.h
+++ src/Estimators/LocalEnergyEstimator.h
@@ -19,9 +19,8 @@
 // -*- C++ -*-
 #ifndef QMCPLUSPLUS_LOCALENERGYESTIMATOR_H
 #define QMCPLUSPLUS_LOCALENERGYESTIMATOR_H
-#include <strstream>
-#include "OhmmsData/libxmldefs.h"
 #include "Estimators/ScalarEstimatorBase.h"
+#include "QMCHamiltonians/QMCHamiltonian.h"
 
 namespace qmcplusplus {
 
@@ -50,14 +49,9 @@ namespace qmcplusplus {
    *The formula for the LocalEnergy 
    \f[ E_L({\bf R}) = \frac{\hat{H} \Psi({\bf R})}{ \Psi({\bf R})} \f]
   */
-  template<class T>
-  class LocalEnergyEstimator: public ScalarEstimatorBase<T> {
+  class LocalEnergyEstimator: public ScalarEstimatorBase {
 
     enum {ENERGY_INDEX, ENERGY_SQ_INDEX, POTENTIAL_INDEX, LE_MAX};
-
-    using ScalarEstimatorBase<T>::CollectSum;
-    using ScalarEstimatorBase<T>::b_average;
-    using ScalarEstimatorBase<T>::b_variance;
 
     ///locator of the first data this object handles
     int LocalEnergyIndex;
@@ -70,52 +64,35 @@ namespace qmcplusplus {
     std::vector<string> elocal_name;
 
     ///vector to contain all the constituents of the local energy
-    std::vector<T>  elocal;
+    std::vector<RealType>  elocal;
 
   public:
 
-    typedef typename ScalarEstimatorBase<T>::Walker_t Walker_t;
-    typedef typename ScalarEstimatorBase<T>::WalkerIterator WalkerIterator;
-    typedef typename ScalarEstimatorBase<T>::BufferType BufferType;
-  
     /** constructor
      * @param h QMCHamiltonian to define the components
      */
-    LocalEnergyEstimator(QMCHamiltonian& h):Href(h) { 
-      int hterms(h.size());
-      SizeOfHamiltonians = hterms;
-      FirstHamiltonian = h.startIndex();
-      elocal.resize(SizeOfHamiltonians+LE_MAX);
-      elocal_name.resize(SizeOfHamiltonians+LE_MAX);
-      elocal_name[ENERGY_INDEX] = "LocalEnergy";
-      elocal_name[ENERGY_SQ_INDEX] = "Variance";
-      elocal_name[POTENTIAL_INDEX] = "LocalPotential";
-      int ii(LE_MAX);
-      for(int i=0; i < SizeOfHamiltonians; i++) elocal_name[ii++] = h.getName(i);
-    }
+    LocalEnergyEstimator(QMCHamiltonian& h);
 
     /**  add the local energy, variance and all the Hamiltonian components to the scalar record container
      * @param record storage of scalar records (name,value)
      * @param msg buffer for message passing
      */
-    void add2Record(RecordNamedProperty<T>& record, BufferType& msg) {
+    void add2Record(RecordListType& record, BufferType& msg) {
       LocalEnergyIndex = record.add(elocal_name[ENERGY_INDEX].c_str());
-      for(int i=1; i<elocal_name.size(); i++)
-	record.add(elocal_name[i].c_str());
-
+      for(int i=1; i<elocal_name.size(); i++) record.add(elocal_name[i].c_str());
       //add elocal to the message buffer
       msg.add(elocal.begin(),elocal.end());
     }
 
-    inline void accumulate(const Walker_t& awalker, T wgt) {
-      const T* restrict ePtr = awalker.getPropertyBase();
-      T e = ePtr[LOCALENERGY];
+    inline void accumulate(const Walker_t& awalker, RealType wgt) {
+      const RealType* restrict ePtr = awalker.getPropertyBase();
+      RealType e = ePtr[LOCALENERGY];
       elocal[ENERGY_INDEX] += wgt*e;
       elocal[ENERGY_SQ_INDEX] += wgt*e*e;
       elocal[POTENTIAL_INDEX] += wgt*ePtr[LOCALPOTENTIAL];
       for(int i=0, target=LE_MAX, source=FirstHamiltonian; i<SizeOfHamiltonians; 
           i++,target++,source++) {
-	elocal[target] += wgt*ePtr[source];
+        elocal[target] += wgt*ePtr[source];
       }
       /*
       T e = awalker.Properties(LOCALENERGY);
@@ -125,26 +102,25 @@ namespace qmcplusplus {
       elocal[ENERGY_SQ_INDEX] += wgt*e*e;
       elocal[POTENTIAL_INDEX] += wgt*awalker.Properties(LOCALPOTENTIAL);
       for(int ii=LE_MAX, i=0; i<SizeOfHamiltonians; ii++,i++) {
-	elocal[ii] += wgt*e_ptr[i]; //wgt*(awalker.E[i]);
+        elocal[ii] += wgt*e_ptr[i]; //wgt*(awalker.E[i]);
       }
       */
     }
 
-    void accumulate(WalkerIterator first, WalkerIterator last, T wgtnorm) {
-      T deltaE = Href.getEnsembleAverage();
+    inline RealType accumulate(WalkerIterator first, WalkerIterator last) {
+      //RealType deltaE = Href.getEnsembleAverage();
       int wsum=0;
       while(first != last) {
-        accumulate(**first,(*first)->Weight*wgtnorm);
+        accumulate(**first,(*first)->Weight);
         ++first; wsum++;
       }
-      elocal[ENERGY_INDEX] += static_cast<T>(wsum)*deltaE;
+      return wsum;
+      //elocal[ENERGY_INDEX] += static_cast<RealType>(wsum)*deltaE;
     }
 
     ///reset all the cumulative sums to zero
     inline void reset() { 
-      for(int i=0; i<elocal.size(); i++) {
-	elocal[i] = T();
-      }
+      std::fill(elocal.begin(), elocal.end(),0.0);
     }
 
     ///copy the value to a message buffer
@@ -156,19 +132,7 @@ namespace qmcplusplus {
      *\param record a container class for storing scalar records (name,value)
      *\param wgtinv the inverse weight
      */
-    inline void report(RecordNamedProperty<T>& record, T wgtinv, BufferType& msg) {
-      if(CollectSum) msg.get(elocal.begin(),elocal.end());
-      register int ir=LocalEnergyIndex;
-      b_average =  elocal[ENERGY_INDEX]*wgtinv;
-      b_variance = elocal[ENERGY_SQ_INDEX]*wgtinv-b_average*b_average;
-      record[ir++] = b_average;
-      record[ir++] = b_variance;
-      record[ir++] = elocal[POTENTIAL_INDEX]*wgtinv;
-      for(int i=0, ii=LE_MAX; i<SizeOfHamiltonians; i++,ii++) {
-	record[ir++] = elocal[ii]*wgtinv;
-      }
-      reset();
-    }
+    void report(RecordListType& record, RealType wgtinv, BufferType& msg);
   };
 
 }
