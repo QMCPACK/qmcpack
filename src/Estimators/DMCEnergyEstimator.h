7fe509056d77d71a3df84274ebfcad84e1638888
Jeongnim Kim <jeongnim.kim@intel.com>
2006-10-18 16:00:32 +0000

    DMCEstimatorMangager is retired.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1385 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Estimators/DMCEnergyEstimator.h
+++ src/Estimators/DMCEnergyEstimator.h
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////
-// (c) Copyright 2003- by Jeongnim Kim 
+// (c) Copyright 2006- by Jeongnim Kim 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //   Jeongnim Kim
@@ -13,110 +13,92 @@
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
-//   Department of Physics, Ohio State University
-//   Ohio Supercomputer Center
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
 #ifndef QMCPLUSPLUS_DMC_LOCALENERGYESTIMATOR_H
 #define QMCPLUSPLUS_DMC_LOCALENERGYESTIMATOR_H
-#include "Estimators/ScalarEstimatorBase.h"
 #include "Message/CommOperators.h"
+#include "Estimators/ScalarEstimatorBase.h"
+#include "QMCDrivers/WalkerControlBase.h"
 
 namespace qmcplusplus {
 
-  template<class T>
-  struct DMCEnergyEstimator: public ScalarEstimatorBase<T> {
+  /** EnergyEstimator specialized for DMC
+   *
+   * This is a fake ScalarEstimatorBase<T> in that nothing is accumulated by
+   * this object. However, it uses WalkerControlBase* which performs 
+   * the functions of an estimator and more.
+   * The enum should be identical to WalkerControlBase
+   */
+  struct DMCEnergyEstimator: public ScalarEstimatorBase {
 
-    enum {ENERGY_INDEX, ENERGY_SQ_INDEX, WEIGHT_INDEX,LE_MAX};
+    enum {ENERGY_INDEX=0, ENERGY_SQ_INDEX, WALKERSIZE_INDEX, WEIGHT_INDEX, EREF_INDEX, LE_MAX};
 
-    typedef typename ScalarEstimatorBase<T>::Walker_t Walker_t;
-    typedef typename ScalarEstimatorBase<T>::WalkerIterator WalkerIterator;
-    typedef typename ScalarEstimatorBase<T>::BufferType BufferType;
-
-    using ScalarEstimatorBase<T>::CollectSum;
-    using ScalarEstimatorBase<T>::b_average;
-    using ScalarEstimatorBase<T>::b_variance;
-
+    ///index for LocalEnergy
     int averageIndex;
+    ///index for Variance
     int varianceIndex;
-    TinyVector<std::string,LE_MAX> obName;
-    TinyVector<T,LE_MAX> observables;
+    ///index for number of walkers
+    int popIndex;
 
+    int Current;
+    WalkerControlBase* walkerControl;
+    
     /** constructor
      */
-    DMCEnergyEstimator() { 
-      obName[ENERGY_INDEX]="LocalEnergy";
-      obName[ENERGY_SQ_INDEX]="Variance";
-    }
+    DMCEnergyEstimator():walkerControl(0),Current(0) {}
 
-    inline T getWeight() const { return observables[WEIGHT_INDEX];}
+    void setWalkerControl(WalkerControlBase* wc) { 
+      walkerControl=wc;
+    }
 
     /**  add the local energy, variance and all the Hamiltonian components to the scalar record container
      * @param record storage of scalar records (name,value)
      * @param msg buffer for message passing
+     *
+     * Nothing to be added to msg.
      */
-    void add2Record(RecordNamedProperty<T>& record, BufferType& msg) {
-      averageIndex = record.add(obName[ENERGY_INDEX].c_str());
-      varianceIndex= record.add(obName[ENERGY_SQ_INDEX].c_str());
-      //add elocal to the message buffer
-      msg.add(observables.begin(),observables.begin()+1);
+    void add2Record(RecordListType& record, BufferType& msg) {
+      averageIndex = record.add("LocalEnergy");
+      varianceIndex= record.add("Variance");
+      popIndex     = record.add("Population");
     }
 
-    inline void accumulate(const Walker_t& awalker, T wgt) {
-      T e(awalker.Properties(LOCALENERGY));
-      observables[ENERGY_INDEX] += wgt*e;
-      observables[ENERGY_SQ_INDEX] += wgt*e*e;
-      observables[WEIGHT_INDEX] += wgt;
+    inline void accumulate(const Walker_t& awalker, RealType wgt) {
+      app_error() << "Disabled DMCEnergyEstimator::accumulate(const Walker_t& awalker, T wgt) " << endl;
     }
 
-    inline void collect(WalkerIterator first, WalkerIterator last) {
-      TinyVector<T,LE_MAX> temp;
-      while(first != last) {
-        T wgt((*first)->Weight);
-        T e((*first)->Properties(LOCALENERGY));
-        temp[ENERGY_INDEX] += wgt*e;
-        temp[ENERGY_SQ_INDEX] += wgt*e*e;
-        temp[WEIGHT_INDEX] += wgt;
-        ++first;
-      }
-      gsum(temp,0);
-      T wgtInv=1.0/temp[WEIGHT_INDEX];
-      observables[ENERGY_INDEX] += temp[ENERGY_INDEX]*wgtInv;
-      observables[ENERGY_SQ_INDEX] += temp[ENERGY_SQ_INDEX]*wgtInv;
-      observables[WEIGHT_INDEX] += temp[WEIGHT_INDEX];
-    }
-
-    inline void accumulate(WalkerIterator first, WalkerIterator last, T wgtnorm) {
-      while(first != last) {
-        T wgt((*first)->Weight*wgtnorm);
-        T e((*first)->Properties(LOCALENERGY));
-        observables[ENERGY_INDEX] += wgt*e;
-        observables[ENERGY_SQ_INDEX] += wgt*e*e;
-        observables[WEIGHT_INDEX] += wgt;
-        ++first;
-      }
+    //accumulate: nothing
+    inline RealType accumulate(WalkerIterator first, WalkerIterator last) { 
+      Current++;
+      return walkerControl->getValue(WEIGHT_INDEX); 
     }
 
     ///reset all the cumulative sums to zero
     inline void reset() { 
-      observables=0.0;
+      walkerControl->reset();
     }
 
-    ///copy the value to a message buffer
-    inline void copy2Buffer(BufferType& msg) {
-      msg.put(observables.begin(),observables.begin()+1);
-    }
+    /** copy the value to a message buffer
+     *
+     * msg buffer is used to collect data over MPI. There is nothing to be collected.
+     */
+    inline void copy2Buffer(BufferType& msg) { }
 
     /** calculate the averages and reset to zero
-     *\param record a container class for storing scalar records (name,value)
-     *\param wgtinv the inverse weight
+     * @param record a container class for storing scalar records (name,value)
+     * @param wgtinv the inverse weight
+     *
+     * The weight is overwritten by 1.0/Current, since the weight has been already
+     * included by WalkerControlBase::branch operation.
      */
-    inline void report(RecordNamedProperty<T>& record, T wgtinv, BufferType& msg) {
-      b_average =  observables[ENERGY_INDEX]*wgtinv;
-      b_variance = observables[ENERGY_SQ_INDEX]*wgtinv-b_average*b_average;
-      record[averageIndex]=b_average;
-      record[varianceIndex]=b_variance;
-      observables=0.0;
+    inline void report(RecordListType& record, RealType wgtinv, BufferType& msg) {
+      wgtinv=1.0/static_cast<RealType>(Current);
+      record[averageIndex]= b_average =  wgtinv*walkerControl->getValue(ENERGY_INDEX);
+      record[varianceIndex]= b_variance = wgtinv*walkerControl->getValue(ENERGY_SQ_INDEX)-b_average*b_average;
+      record[popIndex]= wgtinv*walkerControl->getValue(WALKERSIZE_INDEX);
+      walkerControl->reset();
+      Current=0;
     }
   };
 
