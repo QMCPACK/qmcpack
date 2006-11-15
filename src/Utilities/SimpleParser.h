885a4848cac347523b54787cbb44f2582b29f93f
Jeongnim Kim <jeongnim.kim@intel.com>
2006-11-15 15:13:29 +0000

    Move ASCII parser to SimpleParser.h
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1516 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Utilities/SimpleParser.h
+++ src/Utilities/SimpleParser.h
@@ -44,6 +44,57 @@ unsigned parsewords(char *inbuf, list<string>& slist);
 
 void readXmol(istream& ,double*,int);
 
+struct OhmmsAsciiParser {
+
+  static const int bufferSize=200;
+  char dbuffer[bufferSize];
+  std::vector<std::string> currentWords;
+
+  inline void skiplines(std::istream& is, int n) {
+    while(n>0) {
+      is.getline( dbuffer, bufferSize);--n;
+    }
+  }
+  template<class T>
+  inline void getValue(std::istream& is, T& aval) {
+    is.getline( dbuffer,bufferSize);
+    std::istringstream a(dbuffer); a>> aval;
+  }
+
+  template<class T1, class T2>
+  inline void getValue(std::istream& is, T1& aval, T2& bval) {
+    is.getline( dbuffer,bufferSize);
+    std::istringstream a(dbuffer); a>> aval>>bval;
+  }
+
+  template<class IT>
+  inline void getValues(std::istream& is, IT first, IT last) {
+ 
+    while(first != last) {
+      is.getline( dbuffer,bufferSize);
+      std::istringstream a(dbuffer);
+      while(first != last && a >> *first){first++;}
+    }
+
+  }
+
+  int search(std::istream& is, const std::string& keyword) {
+    bool notfound = true;
+    while(notfound) {
+      std::string aline;
+      getline(is,aline,'\n');
+      if(! is){
+	cout << "KEYWORD " << keyword << " : NOT FOUND. " << endl;
+	abort();
+      }
+      if(aline.find(keyword) < aline.size()) {
+        notfound = false;
+      } 
+    }
+    return 1;
+  }
+};
+
 #endif
 
 /***************************************************************************
