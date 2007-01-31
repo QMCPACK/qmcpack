788eaeb781cdfe15ce71531fe72450e41ebaef87
Jeongnim Kim <jeongnim.kim@intel.com>
2007-01-31 03:01:29 +0000

    Complete CubicSpline with first derivatives.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1675 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Numerics/CubicSpline.h
+++ src/Numerics/CubicSpline.h
@@ -19,15 +19,22 @@
 
 #include "Numerics/CubicSplineGrid.h"
 
-template<class T, unsigned GRIDTYPE, bool PBC>
-struct CubicSpline: public CubicSplineGrid<T,GRIDTYPE,PBC>
-{
-  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC>::point_type point_type;
-  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC>::value_type value_type;
-  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC>::container_type container_type;
+///dummy class for CubicSpline function
+template<class T, unsigned GRIDTYPE, unsigned PBC>
+struct CubicSpline { };
 
-  using CubicSplineGrid<T,GRIDTYPE,PBC>::GridDelta;
-  using CubicSplineGrid<T,GRIDTYPE,PBC>::GridDeltaInv;
+/** specialization for a cubic spline on any grid with PBC
+ */
+template<class T, unsigned GRIDTYPE>
+struct CubicSpline<T,GRIDTYPE,PBC_CONSTRAINTS>: public CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>
+{
+  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>::point_type point_type;
+  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>::value_type value_type;
+  typedef typename CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>::container_type container_type;
+
+  using CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>::GridDelta;
+  using CubicSplineGrid<T,GRIDTYPE,PBC_CONSTRAINTS>::GridDeltaInv;
+  point_type cL, cR;
   point_type p1,p2,q1,q2;
   point_type dp1,dq1,dq2;
   point_type d2p1,d2q1,d2q2;
@@ -40,86 +47,39 @@ struct CubicSpline: public CubicSplineGrid<T,GRIDTYPE,PBC>
 
   inline CubicSpline() { }
 
-  void Init(T start, T end, const container_type& datain, bool closed)
+  void Init(point_type start, point_type end, 
+      const container_type& datain, bool closed)
   {
     this->spline(start,end,datain,P,dP,closed);
   }
 
-  inline void updateFirstOrder0(point_type x) {
-    point_type cL = this->getDelta(x,i0); 
-    point_type cR = cL-1.0;
-    p1=cR*cR*(1.0+2.0*cL);
-    p2=cL*cL*(3.0-2.0*cL);
-    q1=cL*cR*cR;
-    q2=cL*cL*cR;
-  }
-
-  inline void updateFirstOrder1(point_type x) {
-    point_type cL = this->getDelta(x,i0); 
-    point_type cR = cL-1.0;
-    dp1=6.0*cL*cR*GridDeltaInv;
-    dq1=1.0-4.0*cL+3.0*cL*cL;
-    dq2=cL*(3.0*cL-2.0);
-  }
-
-  inline void updateFirstOrder2(point_type x) {
-    point_type cL = this->getDelta(x,i0); 
-    point_type cR = cL-1.0;
-    d2p1=(12.0*cL-6.0)*GridDeltaInv*GridDeltaInv;
-    d2q1=(6.0*cL-4.0)*GridDeltaInv;
-    d2q2=(6.0*cL-2.0)*GridDeltaInv;
-  }
-
-  /** evaluate coefficients upto second order */
-  inline void updateFirstOrder(point_type x) {
-    point_type cL = this->getDelta(x,i0); 
-    point_type cR = cL-1.0;
-    p1=cR*cR*(1.0+2.0*cL);
-    p2=cL*cL*(3.0-2.0*cL);
-    q1=cL*cR*cR;
-    q2=cL*cL*cR;
-
-    dp1=6.0*cL*cR*GridDeltaInv;
-    dq1=1.0-4.0*cL+3.0*cL*cL;
-    dq2=cL*(3.0*cL-2.0);
-
-    d2p1=(12.0*cL-6.0)*GridDeltaInv*GridDeltaInv;
-    d2q1=(6.0*cL-4.0)*GridDeltaInv;
-    d2q2=(6.0*cL-2.0)*GridDeltaInv;
-  }
-
-  inline value_type getValue(point_type x)
-  {
-    updateFirstOrder0(x);
-    return  p1*P[i0]+p2*P[i0+1]+GridDelta*(q1*dP[i0]+q2*dP[i0+1]);
-  }
-
-  inline value_type getDeriv(point_type x)
-  {
-    updateFirstOrder1(x);
-    return dp1*(P[i0]-P[i0+1])+dq1*dP[i0]+dq2*dP[i0+1];
-  }
-
-  inline value_type getDeriv2(point_type x)
-  {
-    updateFirstOrder2(x);
-    return d2p1*(P[i0]-P[i0+1])+d2q1*dP[i0]+d2q2*dP[i0+1];
-  }
-
-  inline value_type operator()(T x) 
-  {
-    return getValue(x);
-  }
-
   inline value_type splint(point_type x)
   {
-    updateFirstOrder0(x);
+    cL=getDelta(x,i0);
+    cR = cL-1.0;
+    p1=cR*cR*(1.0+2.0*cL);
+    p2=cL*cL*(3.0-2.0*cL);
+    q1=cL*cR*cR;
+    q2=cL*cL*cR;
     return  p1*P[i0]+p2*P[i0+1]+GridDelta*(q1*dP[i0]+q2*dP[i0+1]);
   }
 
   inline value_type splint(point_type x, value_type& dy, value_type& d2y)
   {
-    updateFirstOrder(x);
+    cL = this->getDelta(x,i0); 
+    cR = cL-1.0;
+    p1=cR*cR*(1.0+2.0*cL);
+    p2=cL*cL*(3.0-2.0*cL);
+    q1=cL*cR*cR;
+    q2=cL*cL*cR;
+
+    dp1=6.0*cL*cR*GridDeltaInv;
+    dq1=1.0-4.0*cL+3.0*cL*cL;
+    dq2=cL*(3.0*cL-2.0);
+
+    d2p1=(12.0*cL-6.0)*GridDeltaInv*GridDeltaInv;
+    d2q1=(6.0*cL-4.0)*GridDeltaInv;
+    d2q2=(6.0*cL-2.0)*GridDeltaInv;
     return interpolate(P[i0],P[i0+1],dP[i0],dP[i0+1],dy,d2y);
     //dy = dp1*(P[i0]-P[i0+1])+dq1*dP[i0]+dq2*dP[i0+1];
     //d2y = d2p1*(P[i0]-P[i0+1])+d2q1*dP[i0]+d2q2*dP[i0+1];
@@ -134,8 +94,129 @@ struct CubicSpline: public CubicSplineGrid<T,GRIDTYPE,PBC>
     d2y = d2p1*(a-b)+d2q1*a1+d2q2*b1;
     return p1*a+p2*b+GridDelta*(q1*a1+q2*b1);
   }
+
+  inline value_type getValue(point_type x)
+  {
+    point_type cL=getDelta(x,i0);
+    point_type cR = cL-1.0;
+    p1=cR*cR*(1.0+2.0*cL);
+    p2=cL*cL*(3.0-2.0*cL);
+    q1=cL*cR*cR;
+    q2=cL*cL*cR;
+    return  p1*P[i0]+p2*P[i0+1]+GridDelta*(q1*dP[i0]+q2*dP[i0+1]);
+  }
+
+  inline value_type getDeriv(point_type x)
+  {
+    point_type cL=getDelta(x,i0);
+    point_type cR = cL-1.0;
+    dp1=6.0*cL*cR*GridDeltaInv;
+    dq1=1.0-4.0*cL+3.0*cL*cL;
+    dq2=cL*(3.0*cL-2.0);
+    return dp1*(P[i0]-P[i0+1])+dq1*dP[i0]+dq2*dP[i0+1];
+  }
+
+  inline value_type getDeriv2(point_type x)
+  {
+    point_type cL=getDelta(x,i0);
+    point_type cR = cL-1.0;
+    d2p1=(12.0*cL-6.0)*GridDeltaInv*GridDeltaInv;
+    d2q1=(6.0*cL-4.0)*GridDeltaInv;
+    d2q2=(6.0*cL-2.0)*GridDeltaInv;
+    return d2p1*(P[i0]-P[i0+1])+d2q1*dP[i0]+d2q2*dP[i0+1];
+  }
+
+  inline value_type operator()(T x) 
+  {
+    return getValue(x);
+  }
 };
 
+/** specialization for a cubic spline on any grid with PBC
+ */
+template<class T, unsigned GRIDTYPE>
+struct CubicSpline<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>: 
+public CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>
+{
+  typedef typename CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::point_type point_type;
+  typedef typename CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::value_type value_type;
+  typedef typename CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::container_type container_type;
+
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::GridStart;
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::GridEnd;
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::GridDelta;
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::GridDeltaInv;
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::StartDeriv;
+  using CubicSplineGrid<T,GRIDTYPE,FIRSTDERIV_CONSTRAINTS>::EndDeriv;
+
+  int EndIndex;
+  int i0;
+  value_type ConstValue;
+  point_type cL, cR;
+  point_type q1,q2;
+  point_type dq1,dq2;
+  
+  /// data
+  container_type P;
+  /// first derivatives
+  container_type d2P;
+
+  inline CubicSpline():ConstValue(0.0) { }
+
+  void Init(point_type end, const container_type& datain, bool closed)
+  {
+    this->spline(0.0,end,datain,P,d2P,closed);
+    ConstValue=datain.back();
+  }
+
+  void Init(point_type end, const container_type& datain,  bool closed,
+      value_type yp1, value_type ypn)
+  {
+    this->spline(0.0,end,datain,yp1,ypn,P,d2P,closed);
+    ConstValue=datain.back();
+  }
+
+  inline value_type splint(point_type x)
+  {
+    if (x>GridEnd) return ConstValue;
+    cL = this->getDelta(x,i0); 
+    cR = 1.0-cL;
+    const point_type onesixth = 1.0/6.0;
+    point_type h6(GridDelta*GridDelta*onesixth);
+    q1 = cR*(cR*cR-1.0)*h6; //C
+    q2 = cL*(cL*cL-1.0)*h6; //D
+    return cR*P[i0]+cL*P[i0+1]+q1*d2P[i0]+q2*d2P[i0+1];
+  }
+
+  inline value_type splint(point_type x, value_type& dy, value_type& d2y)
+  {
+    if (x>GridEnd)
+    {
+      dy=0.0;d2y=0.0;
+      return ConstValue;
+    }
+    cL = this->getDelta(x,i0); 
+    cR = 1.0-cL;
+    const point_type onesixth = 1.0/6.0;
+    point_type h6(GridDelta*onesixth);
+    q1 = cR*(cR*cR-1.0)*h6*GridDelta; //C
+    q2 = cL*(cL*cL-1.0)*h6*GridDelta; //D
+    dq1 = h6*(1.0-3.0*cR*cR);
+    dq2 = h6*(3.0*cL*cL-1.0);
+
+    return interpolate(P[i0],P[i0+1],d2P[i0],d2P[i0+1],dy,d2y);
+  }
+
+  inline value_type interpolate(value_type y1, value_type y2,
+      value_type d2y1, value_type d2y2,
+      value_type& du, value_type& d2u)
+  {
+    du = GridDeltaInv*(y2-y1)+dq1*d2y1+dq2*d2y2;
+    d2u = cR*d2y1+cL*d2y2; 
+    return cR*y1+cL*y2+q1*d2y1+q2*d2y2;
+  }
+
+};
 #endif
 /***************************************************************************
  * $RCSfile$   $Author: jnkim $
--- src/Numerics/CubicSplineGrid.h
+++ src/Numerics/CubicSplineGrid.h
@@ -76,62 +76,90 @@ struct CubicSplineGrid<T,LINEAR_1DGRID,PBC_CONSTRAINTS>
   }
 };
 
-///** specialization for linear grid with fixed boundary condition */
-//template<class T>
-//struct CubicSplineGrid<T,LINEAR_1DGRID,false>
-//{
-//  typedef typename GridTraits<T>::point_type point_type;
-//  typedef typename GridTraits<T>::value_type value_type;
-//  typedef std::vector<T>                     container_type;
-//
-//  /** index to observe boundary condiition */
-//  int endN;
-//  point_type GridStart, GridEnd, GridDelta, GridDeltaInv, GridDeltaInv2, L, Linv;
-//
-//  inline CubicSplineGrid(){}
-//
-//  /** evaluate i , where \f$x[i] <= xIn < x[i+1]\f$
-//   * @param xinx input grid point
-//   * @param i return index
-//   * @return (xIn-x[i])/(x[i+1]-x[i])
-//   */
-//  inline point_type getDelta(point_type xIn, int& i)
-//  {
-//    if(xIn<GridStart||xIn>GridEnd) {
-//      i=endN;
-//      return -1.0;
-//    }
-//    point_type delta = xIn - GridStart;
-//    i = static_cast<int>(delta*GridDeltaInv);
-//    return delta*GridDeltaInv-i;
-//  }
-//
-//  void spline(point_type start, point_type end, 
-//      const container_type& datain, container_type& p, container_type& dp, bool closed) 
-//  {
-//    int N(datain.size());
-//    if(closed) N--;
-//    GridStart=start;
-//    GridEnd=end;
-//    L=end-start;
-//    Linv=1.0/L;
-//    GridDelta=L/static_cast<point_type>(N);
-//    GridDeltaInv=1.0/GridDelta;
-//
-//    p.resize(N+3);
-//    std::copy(datain.begin(),datain.end(),p.begin());
-//    dp.resize(N+3);
-//    container_type gr(N+1),d2p(N+1);
-//    //don't forget to include the end point
-//    for(int i=0; i<=N; i++) gr[i]=i*GridDelta+GridStart;
-//    NRCubicSplineFirst(&(gr[0]),&(p[0]),N,&(dp[0]),&(d2p[0]));
-//    endN=N+1;
-//    p[N+1]=0.0;
-//    p[N+2]=0.0;
-//    dp[N+1]=0.0;
-//    dp[N+2]=0.0;
-//  }
-//};
+/** specialization for linear grid with First-Deriv */
+template<class T>
+struct CubicSplineGrid<T,LINEAR_1DGRID,FIRSTDERIV_CONSTRAINTS>
+{
+  typedef typename GridTraits<T>::point_type point_type;
+  typedef typename GridTraits<T>::value_type value_type;
+  typedef std::vector<T>                     container_type;
+  point_type L, Linv;
+  point_type GridStart, GridEnd;
+  point_type GridDelta, GridDeltaInv, GridDeltaInv2;
+  value_type StartDeriv;
+  value_type EndDeriv;
+
+  inline CubicSplineGrid():StartDeriv(0.0),EndDeriv(0.0){}
+
+  /** evaluate i , where \f$x[i] <= xIn < x[i+1]\f$
+   * @param xinx input grid point
+   * @param i return index
+   * @return (xIn-x[i])/(x[i+1]-x[i])
+   */
+  inline point_type getDelta(point_type xIn, int& i)
+  {
+    point_type delta = xIn - GridStart;
+    delta -= std::floor(delta*Linv)*L;
+    i = static_cast<int>(delta*GridDeltaInv);
+    return delta*GridDeltaInv-i;
+  }
+
+  inline bool checkGrid(point_type xIn, int& i, point_type& dl)
+  {
+    if(xIn<GridStart||xIn> GridEnd) return false;
+    point_type delta = xIn - GridStart;
+    delta -= std::floor(delta*Linv)*L;
+    i = static_cast<int>(delta*GridDeltaInv);
+    dl=delta*GridDeltaInv-i;
+    return true;
+  }
+
+  template<typename GRIDCONTAINER>
+  void setGrid(point_type start, point_type end, int n, GRIDCONTAINER& gr)
+  {
+    GridStart=start;
+    GridEnd=end;
+    GridDelta=(end-start)/static_cast<point_type>(n);
+    GridDeltaInv=1.0/GridDelta;
+    L=end-start;
+    Linv=1.0/L;
+    for(int i=0; i<=n; i++) 
+      gr[i]=start+static_cast<point_type>(i)*GridDelta;
+  }
+
+  void spline(point_type start, point_type end, const container_type& datain, 
+      container_type& p, container_type& d2p, bool closed) 
+  {
+    int n=datain.size();
+    if(closed) n--;
+    std::vector<point_type> gr(n+1);
+    setGrid(start,end,n,gr);
+
+    p.resize(n+1);
+    d2p.resize(n+1,0.0);
+    std::copy(datain.begin(), datain.end(),p.begin());
+    StartDeriv=(datain[1]-datain[0])*GridDeltaInv;
+    EndDeriv=0.0;//cheating
+    NRCubicSpline(&gr[0],&datain[0],n+1,StartDeriv,EndDeriv,&d2p[0]);
+  }
+
+  void spline(point_type start, point_type end, const container_type& datain,  
+      value_type yp1, value_type ypn,
+      container_type& p, container_type& d2p, bool closed) 
+  {
+    int n=datain.size();
+    if(closed) n--;//odd thing about the grid. 
+    std::vector<point_type> gr(n+1);
+    setGrid(start,end,n,gr);
+
+    p.resize(n+1);
+    d2p.resize(n+1,0.0);
+    std::copy(datain.begin(), datain.end(),p.begin());
+    NRCubicSpline(&gr[0],&datain[0],n+1,StartDeriv,EndDeriv,&d2p[0]);
+    std::cout << "Last point " << p[n] << " " << d2p[n] << std::endl;
+  }
+};
+
 #endif
 /***************************************************************************
  * $RCSfile$   $Author: jnkim $
