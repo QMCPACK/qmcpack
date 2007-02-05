105d977bf410014c7ac08c1f712edb7c86f1938d
Jeongnim Kim <jeongnim.kim@intel.com>
2007-02-05 00:55:25 +0000

    Basisset builder using CubicBspline for the radial functions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1709 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/Jastrow/CBSOBuilder.cpp
@@ -0,0 +1,139 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003 by Jeongnim Kim and Jordan Vincent
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+#include "Utilities/OhmmsInfo.h"
+#include "Numerics/LibxmlNumericIO.h"
+#include "Numerics/GaussianBasisSet.h"
+#include "QMCWaveFunctions/Jastrow/CBSOBuilder.h"
+#include "OhmmsData/AttributeSet.h"
+
+namespace qmcplusplus {
+
+  CBSOBuilder::CBSOBuilder(xmlNodePtr cur): 
+    Normalized(true),m_rcut(5.0),NumGridPoints(51){
+      if(cur != NULL) {
+        putCommon(cur);
+      }
+  }
+
+  bool 
+    CBSOBuilder::putCommon(xmlNodePtr cur) {
+    return true;
+  }
+
+  void 
+    CBSOBuilder::setOrbitalSet(CenteredOrbitalType* oset, const std::string& acenter) { 
+
+    m_orbitals = oset;
+    m_species = acenter;
+  }
+
+  bool 
+    CBSOBuilder::addGrid(xmlNodePtr cur) {
+    if(!m_orbitals) {
+      ERRORMSG("m_orbitals, SphericalOrbitals<ROT,GT>*, is not initialized")
+      return false;
+    }
+
+    RealType delta=-1.0;
+    OhmmsAttributeSet attrib;
+    attrib.add(m_rcut,"rf");
+    attrib.add(NumGridPoints,"npts");
+    attrib.add(delta,"step");
+    attrib.put(cur);
+    if(delta>0) NumGridPoints = static_cast<int>(m_rcut/delta)+1;
+    app_log() << "   CBSOBuilder::addGrid Rcut = " << m_rcut << "  NumGridPoints = " <<  NumGridPoints << endl;
+    return true;
+  }
+
+  /** Add a new Slater Type Orbital with quantum numbers \f$(n,l,m,s)\f$ 
+   * \param cur  the current xmlNode to be processed
+   * \param nlms a vector containing the quantum numbers \f$(n,l,m,s)\f$
+   * \return true is succeeds
+   *
+   This function puts the STO on a logarithmic grid and calculates the boundary 
+   conditions for the 1D Cubic Spline.  The derivates at the endpoint 
+   are assumed to be all zero.  Note: for the radial orbital we use
+   \f[ f(r) = \frac{R(r)}{r^l}, \f] where \f$ R(r) \f$ is the usual
+   radial orbital and \f$ l \f$ is the angular momentum.
+  */
+  bool
+    CBSOBuilder::addRadialOrbital(xmlNodePtr cur, const QuantumNumberType& nlms) {
+
+    if(!m_orbitals) {
+      ERRORMSG("m_orbitals, SphericalOrbitals<ROT,GT>*, is not initialized")
+      return false;
+    }
+
+    string radtype("Gaussian");
+    OhmmsAttributeSet attrib;
+    attrib.add(radtype,"type");
+    attrib.put(cur);
+
+    int lastRnl = m_orbitals->Rnl.size();
+
+    m_nlms = nlms;
+    int L= m_nlms[1];
+
+    typedef RadialOrbitalType::FNIN InputFuncType;
+    InputFuncType* infunc=0;
+    //create one
+    if(radtype == "Gaussian" || radtype == "GTO") 
+    {
+      GaussianCombo<RealType> *gset = new GaussianCombo<RealType>(L,Normalized);
+      gset->putBasisGroup(cur);
+      infunc=gset;
+    } 
+    //else if(radtype == "WM")
+    //{
+    //  xmlNodePtr tcur=cur->children;
+    //  while(tcur != NULL)
+    //  {
+    //     string cname((const char*)(tcur->name));
+    //     if(cname == "parameter") {
+    //       WMFunctor<RealType>* a = new WMFunctor<RealType>(1.0,m_rcut);
+    //       a->put(tcur);
+    //       infunc=a;
+    //     }
+    //    tcur=tcur->next;
+    //  }
+    //}
+
+    if(infunc) 
+    {
+      app_log() 
+        << "   CBSOBuilder::addRadialOrbital  input " << radtype 
+        << " output = CubicBspline " << endl;
+
+      RadialOrbitalType *radorb=new RadialOrbitalType;
+      radorb->initialize(infunc,m_rcut,NumGridPoints);
+
+      m_orbitals->Rnl.push_back(radorb);
+      m_orbitals->RnlID.push_back(m_nlms);
+
+      //if(lastRnl && m_orbitals->Rnl.size()> lastRnl) {
+      //  //LOGMSG("\tSetting GridManager of " << lastRnl << " radial orbital to false")
+      //  m_orbitals->Rnl[lastRnl]->setGridManager(false);
+      //}
+    }
+    return true;
+  }
+
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1269 $   $Date: 2006-08-15 16:27:38 -0500 (Tue, 15 Aug 2006) $
+ * $Id: CBSOBuilder.cpp 1269 2006-08-15 21:27:38Z jnkim $ 
+ ***************************************************************************/
---
+++ src/QMCWaveFunctions/Jastrow/CBSOBuilder.h
@@ -0,0 +1,83 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2005-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+#ifndef QMCPLUSPLUS_SPHERICALBASISBUILDER_H
+#define QMCPLUSPLUS_SPHERICALBASISBUILDER_H
+
+#include "Configuration.h"
+#include "Message/Communicate.h"
+#include "QMCWaveFunctions/Jastrow/SplineFunctors.h"
+//#include "QMCWaveFunctions/Jastrow/CubicBsplineGroup.h"
+#include "QMCWaveFunctions/SphericalBasisSet.h"
+
+namespace qmcplusplus {
+
+  /** Builder of cubic-bspline radial orbitals associated with a center
+   *
+   * CBSO stands for CubicBSplineOrbitals
+   * For a center,
+   *   - only one grid is used
+   *   - any number of radial orbitals  of CubicBsplineGroup<T,GRIDTYPE>
+   */
+  class CBSOBuilder: public QMCTraits {
+
+    public:
+      typedef CubicBsplineGrid<RealType,LINEAR_1DGRID,FIRSTDERIV_CONSTRAINTS> GridType;
+      typedef CubicBsplineSingle<RealType> RadialOrbitalType;
+      //typedef CubicBsplineGroup<RealType,LINEAR_1DGRID> RadialOrbitalType;
+      typedef SphericalBasisSet<RadialOrbitalType>      CenteredOrbitalType;
+
+    ///true, if the RadialOrbitalType is normalized
+    bool Normalized;
+    ///number of grid points [0,Npts]
+    int NumGridPoints;
+    ///maximum cutoff
+    RealType m_rcut;
+    ///the quantum number of this node
+    QuantumNumberType m_nlms;
+    ///the radial orbitals
+    CenteredOrbitalType* m_orbitals;
+    ///the species
+    std::string m_species;
+
+    ///constructor
+    CBSOBuilder(xmlNodePtr cur=NULL);
+
+    ///assign a CenteredOrbitalType to work on
+    void setOrbitalSet(CenteredOrbitalType* oset, const std::string& acenter);
+
+    ///add a grid
+    bool addGrid(xmlNodePtr cur);
+
+    /** add a radial functor
+     * @param cur xml element
+     * @param nlms quantum number
+     */
+    bool addRadialOrbital(xmlNodePtr cur, const QuantumNumberType& nlms);
+
+    /** put common element
+     * @param cur xml element
+     */
+    bool putCommon(xmlNodePtr cur);
+
+    private:
+  };
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1252 $   $Date: 2006-08-13 10:15:01 -0500 (Sun, 13 Aug 2006) $
+ * $Id: CBSOBuilder.h 1252 2006-08-13 15:15:01Z jnkim $ 
+ ***************************************************************************/
