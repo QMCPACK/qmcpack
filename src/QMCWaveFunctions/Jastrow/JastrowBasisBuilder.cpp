876a08509962a2f96b40c6ac8b1bf2fb41505e11
Jeongnim Kim <jeongnim.kim@intel.com>
2007-02-05 00:56:44 +0000

    Ready for generic three-body Jastrow functions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1710 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/Jastrow/JastrowBasisBuilder.cpp
@@ -0,0 +1,92 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "QMCWaveFunctions/Jastrow/JastrowBasisBuilder.h"
+#include "QMCWaveFunctions/MolecularOrbitals/AtomicBasisBuilder.h"
+#include "Message/Communicate.h"
+
+namespace qmcplusplus {
+
+    /** constructor
+     * \param els reference to the electrons
+     * \param ions reference to the ions
+     */
+  JastrowBasisBuilder::JastrowBasisBuilder(ParticleSet& els, ParticleSet& ions):
+    targetPtcl(els), sourcePtcl(ions), thisBasisSet(0) 
+    { }   
+
+  bool JastrowBasisBuilder::put(xmlNodePtr cur) 
+  {
+
+    if(myBasisSet) return true;
+
+    DistanceTableData* d_table=DistanceTable::add(sourcePtcl,targetPtcl);
+
+    //create the BasisSetType
+    thisBasisSet = new ThisBasisSetType(sourcePtcl,targetPtcl);
+
+    //create the basis set
+    //go thru the tree
+    cur = cur->xmlChildrenNode;
+    while(cur!=NULL) {
+      string cname((const char*)(cur->name));
+      if(cname == "atomicBasisSet") {
+        const xmlChar* eptr=xmlGetProp(cur,(const xmlChar*)"elementType");
+        if(eptr == NULL) {
+          app_error() << "   Missing elementType attribute of atomicBasisSet.\n"
+            << " Abort at MOBasisBuilder::put " << endl;
+          OHMMS::Controller->abort();
+        }
+        string elementType((const char*)eptr);
+
+        map<string,BasisSetBuilder*>::iterator it = aoBuilders.find(elementType); 
+        if(it == aoBuilders.end()) {
+          AtomicBasisBuilder<CBSOBuilder>* any = new AtomicBasisBuilder<CBSOBuilder>(elementType);
+          any->put(cur);
+          COT* aoBasis= any->createAOSet(cur);
+
+          if(aoBasis) { //add the new atomic basis to the basis set
+            int activeCenter =sourcePtcl.getSpeciesSet().findSpecies(elementType);
+            thisBasisSet->add(activeCenter, aoBasis);
+            aoBuilders[elementType]=any;
+            //ofstream fout("test.dat");
+            //int nr=aoBasis->Rnl.size();
+            //double r=0.0;
+            //while(r<20)
+            //{
+            //  fout << r ;
+            //  for(int i=0; i<nr; i++) fout << " " << aoBasis->Rnl[i]->evaluate(r,1.0/r);
+            //  fout << endl;
+            //  r += 0.013;
+            //}
+          }
+        } else {
+          WARNMSG("Species " << elementType << " is already initialized. Ignore the input.")
+        }
+      }
+      cur = cur->next;
+    }
+
+    //resize the basis set
+    thisBasisSet->setBasisSetSize(-1);
+    myBasisSet=thisBasisSet;
+    return true;
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1623 $   $Date: 2007-01-18 18:01:15 -0600 (Thu, 18 Jan 2007) $
+ * $Id: JastrowBasisBuilder.h 1623 2007-01-19 00:01:15Z jnkim $ 
+ ***************************************************************************/
---
+++ src/QMCWaveFunctions/Jastrow/JastrowBasisBuilder.h
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_JASTROWBASISBUILDER_H
+#define QMCPLUSPLUS_JASTROWBASISBUILDER_H
+
+#include "QMCWaveFunctions/Jastrow/CBSOBuilder.h"
+#include "QMCWaveFunctions/LocalizedBasisSet.h"
+
+namespace qmcplusplus {
+
+
+ /** derived class from BasisSetBuilder 
+  *
+  * Create a basis set of molecular orbital types as defined in MolecularOrbitalBasis
+  * with radial wave functions on the radial grids.
+  */
+  class JastrowBasisBuilder: public BasisSetBuilder {
+
+  public:
+
+    typedef CBSOBuilder::CenteredOrbitalType COT;
+    typedef LocalizedBasisSet<COT>           ThisBasisSetType;
+
+    /** constructor
+     * \param els reference to the electrons
+     * \param ions reference to the ions
+     */
+    JastrowBasisBuilder(ParticleSet& els, ParticleSet& ions);
+
+    bool put(xmlNodePtr cur);
+
+    /** do not create anything. */
+    SPOSetBase* createSPOSet(xmlNodePtr cur) 
+    {
+      return 0;
+    }
+
+  private:
+    ///target ParticleSet
+    ParticleSet& targetPtcl;
+    ///source ParticleSet
+    ParticleSet& sourcePtcl;
+    ///BasisSet
+    ThisBasisSetType* thisBasisSet;
+    ///save AtomiBasisBuilder<RFB>*
+    map<string,BasisSetBuilder*> aoBuilders;
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1623 $   $Date: 2007-01-18 18:01:15 -0600 (Thu, 18 Jan 2007) $
+ * $Id: JastrowBasisBuilder.h 1623 2007-01-19 00:01:15Z jnkim $ 
+ ***************************************************************************/
