b6dfafbbc62cae8b072d4e4f1c2a1f5a99cf2ba8
Jeongnim Kim <jeongnim.kim@intel.com>
2007-02-08 23:05:20 +0000

    Fix ar/ranlib problem with 64 bit.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1734 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- CMake/IBMCompilers.cmake
+++ CMake/IBMCompilers.cmake
@@ -24,18 +24,19 @@ ENDIF(QMC_BITS MATCHES 64)
 #SET(AIX_CXX_COMMON_FLAGS "-qarch=pwr4 -qtune=pwr4 -qkeyword=restrict -qstrict -qansialias -qalias=typeptr -qnoeh -qrtti=dyna -qtemplateregistry=${LIBRARY_OUTPUT_PATH}/tempegistry")
 #SET(AIX_CXX_OPT_FLAGS "-O3 -qipa -qinline -qspill=2000")
 SET(AIX_CXX_COMMON_FLAGS "-qarch=pwr4 -qtune=pwr4 -qkeyword=restrict -qstrict -qansialias -qrtti=dyna ")
-SET(AIX_CXX_OPT_FLAGS "-O3")
+#SET(AIX_CXX_OPT_FLAGS "-O3 -DINLINE_ALL=  -Q -qipa=inline -qinline ")
+SET(AIX_CXX_OPT_FLAGS "-O3 -DINLINE_ALL=  -Q ")
 SET(AIX_CXX_FLAGS "-O")
 
 ######################################################################
 #set the CXX flags: common + opt + bit
 ######################################################################
-SET(CMAKE_CXX_FLAGS "${AIX_CXX_COMMON_FLAGS} ${AIX_CXX_OPT_FLAGS} ${AIX_BIT_FLAGS}")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${AIX_CXX_COMMON_FLAGS} ${AIX_CXX_OPT_FLAGS} ${AIX_BIT_FLAGS}")
 
 ######################################################################
 #add openmp-related flags: common + opt + bit + thread
 ######################################################################
-IF(OHMMS_OMP)
+IF(QMC_OMP)
   # check if the compiler is correct xlC_r xlc_r mpCC_r mpcc_r
   IF($ENV{CXX} MATCHES "_r")
     SET(ENABLE_OPENMP 1)
@@ -47,11 +48,18 @@ IF(OHMMS_OMP)
     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qnothreaded")
     SET(F77 xlf)
   ENDIF($ENV{CXX} MATCHES "_r")
-ELSE(OHMMS_OMP)
+ELSE(QMC_OMP)
   SET(F77 xlf)
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -qnothreaded")
-ENDIF(OHMMS_OMP)
+ENDIF(QMC_OMP)
 
 SET(F77OPTFLAGS  -O3 -qstrict)
 SET(FORTRAN_LIBS " -lxlf90 -lxlf")
 SET(F77FLAGS ${F77OPTFLAGS})
+
+IF(QMC_BITS MATCHES 64)
+  #SET(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_FLAGS})
+  SET(CMAKE_CXX_CREATE_STATIC_LIBRARY
+        "<CMAKE_AR> -X64 cr <TARGET> <LINK_FLAGS> <OBJECTS> " 
+        "<CMAKE_RANLIB> <TARGET> ")
+ENDIF(QMC_BITS MATCHES 64)
