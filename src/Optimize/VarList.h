fea95d94b858a429f4c1e15b37f9ea210a645f56
Jeongnim Kim <jeongnim.kim@intel.com>
2007-02-17 22:28:14 +0000

    Use a map function to handle optimizable variables.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1759 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Optimize/VarList.h
+++ src/Optimize/VarList.h
@@ -8,74 +8,37 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
+/** @file VarList.h
+ * @brief Define VarRegistry 
+ */
 #ifndef QMCPLUSPLUS_OPTIMIZE_VARREGISTRY_H
 #define QMCPLUSPLUS_OPTIMIZE_VARREGISTRY_H
+#include <map>
 
-/**class VarRegistry
- *\brief register variables for optimization. 
- *
- For a set of variables, register the name, pointer and size.  
- *\note a set can consist of one variable or an array of variables
-*/
-
+/** Set of variables that can be modified during optimizations
+ */
 template<class T>
-struct VarRegistry {
+struct VarRegistry: public std::map<std::string,T> {
 
-  ///vector containing the names of all the sets of optimizable variables
-  std::vector<std::string> Names;
-  ///vector containing the pointers to all the sets of optimizable variables 
-  std::vector<T*> Pointers;
-  ///vector containing the sizes of the sets of optimizable variables 
-  std::vector<int> Sizes;
+  typedef typename std::map<std::string,T>::iterator iterator;
 
-  /** return the current size of variables */
-  inline int size() const { 
-    return Names.size();
-  }
-
-  /** Add a new optimizable variable set to the list of sets.
-   * \param aname name of the optimizable set
-   * \param ptr pointer to the set
-   * \param size size of the set 
-   */
-  void add(const std::string& aname, T* ptr, int size=1) {
-    std::vector<std::string>::iterator it = Names.begin();
-    while(it != Names.end()) {
-      if((*it) == aname) {
-	return;
-      }
-      it++;
-    }
-    Names.push_back(aname);
-    Pointers.push_back(ptr);
-    Sizes.push_back(size);
-  }
-
-  ///return the index of aname
-  int find(const std::string& aname) {
-    int id = 0;
-    std::vector<std::string>::iterator it = Names.begin();
-    while(it != Names.end()) {
-      if((*it) == aname) {
-	return id;
-      }
-      it++;id++;
-    }
-    return -1;
-  }
-
-  void print(std::ostream& os) {
+  void print(std::ostream& os) 
+  {
+    iterator it(this->begin());
     os << "Optimizable variable list " << std::endl;
-    for(int i=0; i<Names.size(); i++)
-      os << Names[i] << " " << *(Pointers[i]) << std::endl;
+    while(it != this->end())
+    {
+      os << (*it).first << " " << (*it).second << std::endl;
+      ++it;
+    }
   }
+
 };
 #endif
 /***************************************************************************
