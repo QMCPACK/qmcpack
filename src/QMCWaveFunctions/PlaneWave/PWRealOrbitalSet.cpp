eb815e610b9f4368e8eb97590e960002fa81324f
Jeongnim Kim <jeongnim.kim@intel.com>
2007-04-06 20:59:20 +0000

    Add PWOrbitalSet specialized for real trial wavefunctions.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1878 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/PlaneWave/PWRealOrbitalSet.cpp
@@ -0,0 +1,128 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Kris Delaney and Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file PWRealOrbitalSet.cpp
+ * @brief declaration of the member functions of PWRealOrbitalSet
+ *
+ * Not the most optimized method to use wavefunctions in a plane-wave basis.
+ */
+#include "Message/Communicate.h"
+#include "QMCWaveFunctions/PlaneWave/PWRealOrbitalSet.h"
+#include "Numerics/MatrixOperators.h"
+
+namespace qmcplusplus {
+  PWRealOrbitalSet::~PWRealOrbitalSet() {
+#if !defined(ENABLE_SMARTPOINTER)
+    if(OwnBasisSet&&myBasisSet) delete myBasisSet;
+#endif
+  }
+
+  void PWRealOrbitalSet::resetParameters(OptimizableSetType& optVariables)
+  {
+    //DO NOTHING FOR NOW
+  }
+
+  void PWRealOrbitalSet::setOrbitalSetSize(int norbs)
+  {
+  }
+
+  void PWRealOrbitalSet::resetTargetParticleSet(ParticleSet& P) {
+    app_error() << "PWRealOrbitalSet::resetTargetParticleSet not yet coded." << endl;
+    OHMMS::Controller->abort();
+  }
+
+  void PWRealOrbitalSet::resize(PWBasisPtr bset, int nbands, bool cleanup) {
+    myBasisSet=bset;
+    OrbitalSetSize=nbands;
+    OwnBasisSet=cleanup;
+    BasisSetSize=myBasisSet->NumPlaneWaves;
+    CC.resize(OrbitalSetSize,BasisSetSize);
+    Temp.resize(OrbitalSetSize,PW_MAXINDEX);
+    tempPsi.resize(OrbitalSetSize);
+    app_log() << "  PWRealOrbitalSet::resize OrbitalSetSize =" << OrbitalSetSize << " BasisSetSize = " << BasisSetSize << endl;
+  }
+
+  void PWRealOrbitalSet::addVector(const std::vector<RealType>& coefs,int jorb)
+  {
+     int ng=myBasisSet->inputmap.size();
+     if(ng != coefs.size()) {
+       app_error() << "  Input G map does not match the basis size of wave functions " << endl;
+       OHMMS::Controller->abort();
+     }
+
+     //drop G points for the given TwistAngle
+     const vector<int> &inputmap(myBasisSet->inputmap);
+     for(int ig=0; ig<ng; ig++)
+     {
+       if(inputmap[ig]>-1) CC[jorb][inputmap[ig]]=coefs[ig];
+     }
+  }
+
+  void PWRealOrbitalSet::addVector(const std::vector<ComplexType>& coefs,int jorb)
+  {
+     int ng=myBasisSet->inputmap.size();
+     if(ng != coefs.size()) {
+       app_error() << "  Input G map does not match the basis size of wave functions " << endl;
+       OHMMS::Controller->abort();
+     }
+
+     //drop G points for the given TwistAngle
+     const vector<int> &inputmap(myBasisSet->inputmap);
+     for(int ig=0; ig<ng; ig++)
+     {
+       if(inputmap[ig]>-1) CC[jorb][inputmap[ig]]=coefs[ig];
+     }
+  }
+
+  void 
+  PWRealOrbitalSet::evaluate(const ParticleSet& P, int iat, ValueVector_t& psi)
+  {
+    myBasisSet->evaluate(P.R[iat]);
+    MatrixOperators::product(CC,myBasisSet->Zv,tempPsi.data());
+    for(int j=0; j<OrbitalSetSize; j++) psi[j]=tempPsi[j].real();
+  }
+
+  void 
+  PWRealOrbitalSet::evaluate(const ParticleSet& P, int iat, 
+          ValueVector_t& psi, GradVector_t& dpsi, ValueVector_t& d2psi)
+  {
+    myBasisSet->evaluateAll(P,iat);
+    MatrixOperators::product(CC,myBasisSet->Z,Temp);
+
+    const ComplexType* restrict tptr=Temp.data();
+    for(int j=0; j< OrbitalSetSize; j++, tptr+=PW_MAXINDEX) {
+      psi[j]   =tptr[PW_VALUE].real();
+      d2psi[j] =tptr[PW_LAP].real();
+      dpsi[j]  =GradType(tptr[PW_GRADX].real(),tptr[PW_GRADY].real(),tptr[PW_GRADZ].real());
+    }
+  }
+    
+  void 
+  PWRealOrbitalSet::evaluate(const ParticleSet& P, int first, int last,
+        ValueMatrix_t& logdet, GradMatrix_t& dlogdet, ValueMatrix_t& d2logdet)
+  {
+    for(int iat=first,i=0; iat<last; iat++,i++){
+      myBasisSet->evaluateAll(P,iat);
+      MatrixOperators::product(CC,myBasisSet->Z,Temp);
+      const ComplexType* restrict tptr=Temp.data();
+      for(int j=0; j< OrbitalSetSize; j++,tptr+=PW_MAXINDEX) {
+        logdet(j,i)  = tptr[PW_VALUE].real();
+        d2logdet(i,j)= tptr[PW_LAP].real();
+        dlogdet(i,j) = GradType(tptr[PW_GRADX].real(),tptr[PW_GRADY].real(),tptr[PW_GRADZ].real());
+      }
+    }
+  }
+}
---
+++ src/QMCWaveFunctions/PlaneWave/PWRealOrbitalSet.h
@@ -0,0 +1,126 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2006-  by Kris Delaney and Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file PWRealOrbitalSet.h
+ * @brief Define PWRealOrbitalSet derived from SPOSetBase
+ *
+ * This is a specialized single-particle orbital set for real trial
+ * wavefunctions and enabled with QMC_COMPLEX=0
+ */
+#ifndef QMCPLUSPLUS_PLANEWAVE_REALORBITALSET_BLAS_H
+#define QMCPLUSPLUS_PLANEWAVE_REALORBITALSET_BLAS_H
+
+#include "QMCWaveFunctions/PlaneWave/PWBasis.h"
+#include "QMCWaveFunctions/SPOSetBase.h"
+#include "Numerics/OhmmsBlas.h"
+
+namespace qmcplusplus {
+
+  class PWRealOrbitalSet: public SPOSetBase {
+
+    public:
+
+      typedef PWBasis                    BasisSet_t;
+#if defined(ENABLE_SMARTPOINTER)
+      typedef boost::shared_ptr<PWBasis> PWBasisPtr;
+#else
+      typedef PWBasis*                   PWBasisPtr;
+#endif
+
+      /** inherit the enum of BasisSet_t */
+      enum {PW_VALUE=   BasisSet_t::PW_VALUE, 
+        PW_LAP=     BasisSet_t::PW_LAP, 
+        PW_GRADX=   BasisSet_t::PW_GRADX, 
+        PW_GRADY=   BasisSet_t::PW_GRADY, 
+        PW_GRADZ=   BasisSet_t::PW_GRADZ,
+        PW_MAXINDEX=BasisSet_t::PW_MAXINDEX
+      };
+
+      /** default constructor
+      */
+      PWRealOrbitalSet(): OwnBasisSet(false) {
+      }
+
+      /** delete BasisSet only it owns this
+       *
+       * Builder takes care of who owns what
+       */
+      ~PWRealOrbitalSet();
+
+      /** resize  the orbital base
+       * @param bset PWBasis
+       * @param nbands number of bands
+       * @param cleaup if true, owns PWBasis. Will clean up.
+       */
+      void resize(PWBasisPtr bset, int nbands, bool cleanup=false);
+
+      /** add eigenstate for jorb-th orbital
+       * @param coefs real input data
+       * @param jorb orbital index
+       */
+      void addVector(const std::vector<RealType>& coefs,int jorb);
+
+      /** add eigenstate for jorb-th orbital
+       * @param coefs complex input data
+       * @param jorb orbital index
+       */
+      void addVector(const std::vector<ComplexType>& coefs,int jorb);
+
+      void resetParameters(OptimizableSetType& optVariables);
+
+      void setOrbitalSetSize(int norbs);
+
+      void resetTargetParticleSet(ParticleSet& P);
+
+      inline ValueType evaluate(int ib, const PosType& pos)
+      {
+        myBasisSet->evaluate(pos);
+        return real(BLAS::dot(BasisSetSize,CC[ib],myBasisSet->Zv.data()));
+      }
+
+      void 
+        evaluate(const ParticleSet& P, int iat, ValueVector_t& psi);
+
+      void 
+        evaluate(const ParticleSet& P, int iat, 
+            ValueVector_t& psi, GradVector_t& dpsi, ValueVector_t& d2psi);
+
+      void evaluate(const ParticleSet& P, int first, int last,
+          ValueMatrix_t& logdet, GradMatrix_t& dlogdet, ValueMatrix_t& d2logdet);
+
+      /** boolean
+       *
+       * If true, this has to delete the BasisSet
+       */
+      bool OwnBasisSet;
+      ///TwistAngle of this PWRealOrbitalSet
+      PosType TwistAngle;
+      ///My basis set
+      PWBasisPtr myBasisSet;
+      ///Plane-wave coefficients of complex: (iband,g-vector)
+      Matrix<ComplexType> CC;
+      /// temporary array to perform gemm operation 
+      Matrix<ComplexType> Temp;
+      ///temporary complex vector before assigning to a real psi
+      Vector<ComplexType> tempPsi;
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1769 $   $Date: 2007-02-17 17:38:34 -0600 (Sat, 17 Feb 2007) $
+ * $Id: PWRealOrbitalSet.h 1769 2007-02-17 23:38:34Z jnkim $
+ ***************************************************************************/
