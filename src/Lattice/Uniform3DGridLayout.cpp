b4949d7b45885709643cae20e6c1d027f2cc2c65
Jeongnim Kim <jeongnim.kim@intel.com>
2007-05-03 16:59:51 +0000

    Bug fix for generalized cells.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@1973 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Lattice/Uniform3DGridLayout.cpp
+++ src/Lattice/Uniform3DGridLayout.cpp
@@ -23,6 +23,46 @@
 
 using namespace APPNAMESPACE;
 
+  ///Set LR_rc = radius of smallest sphere inside box and kc=dim/rc
+void Uniform3DGridLayout::SetLRCutoffs() {
+    //Compute rc as the real-space cutoff of 1/2 the unit-cell. 
+    //Radius of maximum shere that fits in a...
+    TinyVector<value_type,3> b,c,d,x; //Unit vector of each surface will be in here
+    //Compute the coordinate of the box center
+    c = 0.5*(a(0)+a(1)+a(2));
+
+    LR_rc = 1.e+6;
+    Tensor<int,3> Cyclic(0,1,2, 1,2,0, 2,0,1);
+
+    for(int i=0; i<3; i++) 
+    {
+      b = cross(a(Cyclic(i,1)),a(Cyclic(i,2)));
+      value_type binv=1.0/std::sqrt(dot(b,b));
+      b *= binv;
+      ////Unit vector normal to surface i. Cyclic permutations of i.
+      //b = cross(a(i-2<0?i-2+3:i-2),a(i-1<0?i-1+3:i-1));
+      //b = b/std::sqrt(b[0]*b[0] + b[1]*b[1] + b[2]*b[2]);
+      //Now find multiple of 'b' that moves to centre of box
+      //d = 0.5*(a(i-2<0?i-2+3:i-2)+a(i-1<0?i-1+3:i-1))-c;
+      d = 0.5*(a(Cyclic(i,1))+a(Cyclic(i,2)))-c;
+
+      x[i]=1.e+6;
+      for(int l=0;l<3;l++){
+	if(std::abs(d[l]*b[l]) < 1.e-6) continue; //Don't treat 0 elements.
+	d[l] = d[l]/b[l];
+	x[i] = std::min(x[i],std::abs(d[l]));
+      }
+      //Real-space cutoff is minimal x[i] => sphere fits entirely inside cell.
+      LR_rc = std::min(LR_rc,x[i]);
+    }
+
+    //Set KC for structure-factor and LRbreakups.
+    LR_kc = LR_dim_cutoff/LR_rc;
+
+    LOGMSG("\tLong-range breakup parameters:");
+    LOGMSG("\trc*kc = " << LR_dim_cutoff << "; rc = " << LR_rc << "; kc = " << LR_kc << "\n");
+  }
+
 void Uniform3DGridLayout::makeShell(std::vector<SingleParticleIndex_t>& RS,
 				    std::vector<bool>& FS,
 				    const SingleParticleIndex_t& nc,
