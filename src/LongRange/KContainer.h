6abb19e928cd4c00915ea813fc08185b7d32cf14
Jeongnim Kim <jeongnim.kim@intel.com>
2007-06-02 17:41:36 +0000

    Move accumulate_elements to Utility/IteratorUtility.h
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2039 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/LongRange/KContainer.h
+++ src/LongRange/KContainer.h
@@ -3,6 +3,7 @@
 
 #include "Particle/ParticleSet.h"
 #include "Utilities/PooledData.h"
+#include "Utilities/IteratorUtility.h"
 #include "Utilities/OhmmsInfo.h"
 #if !defined(HAVE_SINCOS)
 template<typename T> inline void sincos(T a, T* restrict s, T*  restrict c)
@@ -13,13 +14,6 @@ template<typename T> inline void sincos(T a, T* restrict s, T*  restrict c)
 #endif
 
 namespace qmcplusplus {
-
-  template<typename IT>
-    inline void accumulate_elements(IT first, IT last, IT res)
-    {
-      while(first != last) *res++ += *first++;
-    }
-
 
   /** @ingroup longrange
    *\brief A handler class for generating and storing lists of
