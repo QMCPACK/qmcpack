61be37e3f71f34f1117df7c8ee547235dc782967
Jeongnim Kim <jeongnim.kim@intel.com>
2007-06-17 14:36:08 +0000

    Disable tricubic spline. Always use TricubicBsplineSetBuilder.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2093 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/BasisSetFactory.cpp
+++ src/QMCWaveFunctions/BasisSetFactory.cpp
@@ -19,7 +19,7 @@
 #include "QMCWaveFunctions/MolecularOrbitals/GTOBuilder.h"
 #include "QMCWaveFunctions/MolecularOrbitals/STOBuilder.h"
 #include "QMCWaveFunctions/MolecularOrbitals/MolecularBasisBuilder.h"
-#include "QMCWaveFunctions/SplineSetBuilder.h"
+//#include "QMCWaveFunctions/SplineSetBuilder.h"
 #include "QMCWaveFunctions/TricubicBsplineSetBuilder.h"
 #include "Message/Communicate.h"
 #include "OhmmsData/AttributeSet.h"
@@ -54,11 +54,12 @@ namespace qmcplusplus {
     if(rootNode != NULL)  aAttrib.put(rootNode);
 
     BasisSetBuilder* bb=0;
-    if(typeOpt == "spline") {
-      app_log() << "  SplineSetBuilder: spline on 3D TriCubicGrid " << endl;
-      bb = new SplineSetBuilder(targetPtcl,ptclPool);
-    } 
-    else if(typeOpt == "bspline")
+    //if(typeOpt == "spline") {
+    //  app_log() << "  SplineSetBuilder: spline on 3D TriCubicGrid " << endl;
+    //  bb = new SplineSetBuilder(targetPtcl,ptclPool);
+    //} 
+    //else if(typeOpt == "bspline")
+    if(typeOpt == "bspline" || typeOpt== "spline")
     {
       app_log() << "  TricubicBsplineSetBuilder: b-spline on 3D TriCubicGrid " << endl;
       bb = new TricubicBsplineSetBuilder(targetPtcl,ptclPool,rootNode);
