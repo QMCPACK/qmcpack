74591665fb5b260480006fffb97df05dc70d8873
Ken Esler <kpesler@gmail.com>
2007-08-06 13:22:57 +0000

    Adding a set builder for EinsplineSet.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2149 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/EinsplineSetBuilder.h
@@ -0,0 +1,46 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2007-  by Ken Esler and Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_EINSPLINE_SET_BUILDER_H
+#define QMCPLUSPLUS_EINSPLINE_SET_BUILDER_H
+
+#include "QMCWaveFunctions/BasisSetBase.h"
+#include "QMCWaveFunctions/EinsplineSet.h"
+#include "Numerics/HDFNumericAttrib.h"
+
+namespace qmcplusplus {
+  
+  class EinsplineSetBuilder : public BasisSetBuilder {
+  public:
+    EinsplineSetBuilder(ParticleSet& p, PtclPoolType& psets, xmlNodePtr cur);
+    
+    ~EinsplineSetBuilder();
+    
+    bool put (xmlNodePtr cur);
+
+        /** initialize the Antisymmetric wave function for electrons
+     *@param cur the current xml node
+     */
+    SPOSetBase* createSPOSet(xmlNodePtr cur);
+    
+  protected:
+    
+
+  };
+
+}
+
+
+#endif
