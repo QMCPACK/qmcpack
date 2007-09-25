613adb88a1d23b4dd17149c5d024185da11a0c92
Jeongnim Kim <jeongnim.kim@intel.com>
2007-09-25 15:15:24 +0000

    Change while to for loop to make it easy to readers.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2204 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/DMC/DMCUpdatePbyP.cpp
+++ src/QMCDrivers/DMC/DMCUpdatePbyP.cpp
@@ -22,6 +22,7 @@
 #include "ParticleBase/RandomSeqGenerator.h"
 #include "QMCDrivers/DriftOperators.h"
 #include "Message/CommCreate.h"
+//#define TEST_INNERBRANCH
 
 namespace qmcplusplus { 
 
@@ -42,17 +43,98 @@ namespace qmcplusplus {
   void DMCUpdatePbyPWithRejection::advanceWalkers(WalkerIter_t it, WalkerIter_t it_end,
       bool measure) 
   {
-    int item=0;
-    while(it != it_end) {
+#if defined(TEST_INNERBRANCH)
+    for(;it != it_end;) 
+    {
       //MCWalkerConfiguration::WalkerData_t& w_buffer = *(W.DataSet[iwalker]);
       Walker_t& thisWalker(**it);
       Walker_t::Buffer_t& w_buffer(thisWalker.DataSet);
 
-      //thisWalker.Weight = 1.0e0;
-      //thisWalker.Multiplicity=1.0e0;
-      //save old local energy
-      RealType eold(thisWalker.Properties(LOCALENERGY));
-      RealType enew(eold);
+      W.R = thisWalker.R;
+      w_buffer.rewind();
+      W.copyFromBuffer(w_buffer);
+      Psi.copyFromBuffer(W,w_buffer);
+
+      for(int step=0; step<5; step++)
+      {
+        //create a 3N-Dimensional Gaussian with variance=1
+        makeGaussRandomWithEngine(deltaR,RandomGen);
+        int nAcceptTemp(0);
+        int nRejectTemp(0);
+        RealType eold(thisWalker.Properties(LOCALENERGY));
+        RealType enew(eold);
+        RealType rr_proposed=0.0;
+        RealType rr_accepted=0.0;
+        for(int iat=0; iat<NumPtcl; iat++) 
+        {
+          PosType dr(m_sqrttau*deltaR[iat]+thisWalker.Drift[iat]);
+          PosType newpos(W.makeMove(iat,dr));
+
+          RealType ratio=Psi.ratio(W,iat,dG,dL);
+
+          RealType rr=dot(dr,dr);
+          rr_proposed+=rr;
+
+          //if(ratio < 0.0) {//node is crossed reject the move
+          if(Psi.getPhase() > numeric_limits<RealType>::epsilon()) {
+            ++nRejectTemp;
+            ++nNodeCrossing;
+            W.rejectMove(iat); Psi.rejectMove(iat);
+          } else {
+            G = W.G+dG;
+            RealType logGf = -0.5*dot(deltaR[iat],deltaR[iat]);
+            RealType scale=getDriftScale(Tau,G);
+            //ValueType vsq = Dot(G,G);
+            //ValueType scale = ((-1.0+sqrt(1.0+2.0*Tau*vsq))/vsq);
+            dr = thisWalker.R[iat]-newpos-scale*real(G[iat]); 
+            RealType logGb = -m_oneover2tau*dot(dr,dr);
+            RealType prob = std::min(1.0,ratio*ratio*std::exp(logGb-logGf));
+            if(RandomGen() < prob) { 
+              ++nAcceptTemp;
+              W.acceptMove(iat);
+              Psi.acceptMove(W,iat);
+              W.G = G;
+              W.L += dL;
+
+              assignDrift(scale,G,thisWalker.Drift);
+
+              rr_accepted+=rr;
+            } else {
+              ++nRejectTemp; 
+              W.rejectMove(iat); Psi.rejectMove(iat);
+            }
+          } 
+        }
+
+        if(nAcceptTemp>0) {//need to overwrite the walker properties
+          thisWalker.R = W.R;
+          w_buffer.rewind();
+          W.copyToBuffer(w_buffer);
+          RealType psi = Psi.evaluate(W,w_buffer);
+          enew= H.evaluate(W);
+          thisWalker.resetProperty(std::log(abs(psi)),psi,enew);
+          H.saveProperty(thisWalker.getPropertyBase());
+          //emixed = (eold+enew)*0.5e0;
+        } else {
+          thisWalker.Age++;
+          ++nAllRejected;
+          rr_accepted=0.0;
+          enew=eold;//copy back old energy
+        }
+
+        thisWalker.Weight *= branchEngine->branchWeight(Tau*rr_accepted/rr_proposed,eold,enew);
+        eold=enew;
+        nAccept += nAcceptTemp;
+        nReject += nRejectTemp;
+      }
+      ++it;
+    }
+#else
+    for(;it != it_end;) 
+    {
+      //MCWalkerConfiguration::WalkerData_t& w_buffer = *(W.DataSet[iwalker]);
+      Walker_t& thisWalker(**it);
+      Walker_t::Buffer_t& w_buffer(thisWalker.DataSet);
 
       W.R = thisWalker.R;
       w_buffer.rewind();
@@ -61,14 +143,14 @@ namespace qmcplusplus {
 
       //create a 3N-Dimensional Gaussian with variance=1
       makeGaussRandomWithEngine(deltaR,RandomGen);
-      bool notcrossed(true);
       int nAcceptTemp(0);
       int nRejectTemp(0);
-      int iat=0;
-
+      RealType eold(thisWalker.Properties(LOCALENERGY));
+      RealType enew(eold);
       RealType rr_proposed=0.0;
       RealType rr_accepted=0.0;
-      while(iat<NumPtcl) {//particle-by-particle move
+      for(int iat=0; iat<NumPtcl; iat++) 
+      {
         PosType dr(m_sqrttau*deltaR[iat]+thisWalker.Drift[iat]);
         PosType newpos(W.makeMove(iat,dr));
 
@@ -106,8 +188,6 @@ namespace qmcplusplus {
             W.rejectMove(iat); Psi.rejectMove(iat);
           }
         } 
-
-        ++iat;
       }
 
       if(nAcceptTemp>0) {//need to overwrite the walker properties
@@ -129,21 +209,11 @@ namespace qmcplusplus {
       //thisWalker.Weight *= branchEngine->branchGF(Tau*rr_accepted/rr_proposed,(eold+enew)*0.5,0.0);
       thisWalker.Weight *= branchEngine->branchWeight(Tau*rr_accepted/rr_proposed,eold,enew);
 
-      //if(MaxAge) {
-      //  RealType M=thisWalker.Weight;
-      //  if(thisWalker.Age > MaxAge) M = std::min(0.5,M);
-      //  else if(thisWalker.Age > 0) M = std::min(1.0,M);
-      //  thisWalker.Multiplicity = M + RandomGen();
-      //  branchEngine->accumulate(eold,M);
-      //} else {
-      //  branchEngine->accumulate(eold,1);
-      //}
-      //branchEngine->accumulate(eold,1);
-      
       nAccept += nAcceptTemp;
       nReject += nRejectTemp;
       ++it;
     }
+#endif
   }
 
   /// Constructor.
