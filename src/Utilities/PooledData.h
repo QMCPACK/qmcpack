73879df9cf7aec7f930b5313800419e334d4be9f
Jeongnim Kim <jeongnim.kim@intel.com>
2007-12-06 15:40:15 +0000

    Add iterator-based implementation to solve optimization bugs on Mac+Intel 10.x.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2430 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Utilities/PooledData.h
+++ src/Utilities/PooledData.h
@@ -7,21 +7,177 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
+/** @file PooledData.h
+ * @brief Define a serialized buffer to store anonymous data
+ *
+ * Two implementations differ in how the iterators are handled.
+ * USE_POOLED_DATA_ITERATOR selects an implementation using std::vector<T>::iterator 
+ * Otherwise, the original implementation using an index Current is used.
+ */
 #ifndef QMCPLUSPLUS_POOLEDDATA_H
 #define QMCPLUSPLUS_POOLEDDATA_H
 #include <vector>
 #include <complex>
-using namespace std;
 
-/**class PooledData<T>
- */
+#if defined(USE_POOLED_DATA_ITERATOR)
+template<class T>
+class PooledData: public std::vector<T> {
+
+  typename std::vector<T>::iterator Anchor;
+
+public:
+  
+  //default constructor
+  inline PooledData() { }
+
+  //copy constructor
+  inline PooledData(const PooledData& a): std::vector<T>(a) { 
+    Anchor=this->begin();
+  }
+
+  //constructor with a size
+  explicit inline PooledData(int n): std::vector<T>(n)
+  {
+    Anchor=this->begin();
+  }
+
+  //assignement operator
+  PooledData<T>& operator=(const PooledData<T>& a) 
+  {
+    this->clear();
+    if(a.size()) {
+      this->insert(this->begin(), a.begin(), a.end());
+    }
+    return *this;
+  }
+
+  ///return the location of the Anchor
+  inline int current() const { return Anchor-this->begin();}
+
+  ///set the Anchor at the first iterator
+  inline void rewind() { Anchor=this->begin();}
+
+  ///@{Add data to the pool
+  template<class T1>
+  inline void add(T1 x) { this->push_back(static_cast<T>(x));}
+
+  inline void add(T x) { this->push_back(x);}
+
+  inline void add(complex<T>& x) { 
+    this->push_back(x.real()); 
+    this->push_back(x.imag());
+  }
+
+  template<class _InputIterator>
+    inline void add(_InputIterator first, _InputIterator last) {
+      this->insert(this->end(),first,last);
+    }
+
+  inline void add(std::complex<T>* first,std::complex<T>* last) {
+    while(first != last) {
+      this->push_back((*first).real()); 
+      this->push_back((*first).imag());
+      ++first;
+    }
+  }
+  ///@}
+
+  ///@{Assign value to the arguments from the pool and advance the Anchor
+  template<class T1>
+  inline void get(T1& x) {
+    x = static_cast<T1>(*Anchor++);
+  }
+
+  inline void get(T& x) { x = *Anchor++;} 
+
+  inline void get(std::complex<T>& x) 
+  { 
+    x.real()=*Anchor++;
+    x.imag()=*Anchor++;
+  }
+
+  template<class _ForwardIterator>
+    inline void get(_ForwardIterator first, _ForwardIterator last) {
+      typename std::vector<T>::iterator here=Anchor;
+      Anchor += last-first;
+      std::copy(here,Anchor,first);
+    }
+
+  inline void get(std::complex<T>* first, std::complex<T>* last)
+  {
+    while(first != last) {
+      (*first).real()=*Anchor++;
+      (*first).imag()=*Anchor++;
+      ++first;
+    }
+  }
+  ///@}
+  
+  ///@{Assign value from the arguments to the pool and advnace the Anchor
+  /** Add  a value
+   * @param x value to add
+   */
+  inline void put(T x) { *Anchor++ = x;}
+  /** Add a complex<T>
+   * @param x a complex value to add
+   */
+  inline void put(std::complex<T>& x) { 
+    *Anchor++ = x.real();  
+    *Anchor++ = x.imag();
+  }
+
+  /** Add values from [first,last)
+   * @param first starting iterator
+   * @param last ending iterator
+   */
+  template<class _FowardIterator>
+  inline void put(_FowardIterator first, _FowardIterator last)
+  {
+    std::copy(first,last,Anchor);
+    Anchor += last-first;
+  }
+
+  /** Add complex values from [first,last)
+   * @param first starting iterator
+   * @param last ending iterator
+   */
+  inline void put(std::complex<T>* first, std::complex<T>* last) 
+  {
+    while(first != last) {
+      *Anchor++ = (*first).real();  
+      *Anchor++ = (*first).imag();
+      ++first;
+    }
+  }
+  ///@}
+
+
+  /** return the address of the first element **/
+  inline T* data() { return &((*this)[0]);}
+
+  inline void print(std::ostream& os) {
+    std::copy(this->begin(), this->end(), ostream_iterator<T>(os," "));
+  }
+
+  template<class Msg>
+  inline Msg& putMessage(Msg& m) {
+    m.Pack(&((*this)[0]),this->size());
+    return m;
+  }
+
+  template<class Msg>
+  inline Msg& getMessage(Msg& m) {
+    m.Unpack(&((*this)[0]),this->size());
+    return m;
+  }
+};
+#else
 template<class T>
 class PooledData: public std::vector<T> {
 
@@ -143,6 +299,7 @@ public:
     return m;
   }
 };
+#endif
 #endif
 /***************************************************************************
  * $RCSfile$   $Author$
