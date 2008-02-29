634dc807668a6b1112f9a0fea4073d625fc3c3b9
Jeongnim Kim <jeongnim.kim@intel.com>
2008-02-29 16:57:49 +0000

    Add spherical jellium potential.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2490 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/SQD/SphericalPotential/SHEGPotential.cpp
@@ -0,0 +1,103 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2008-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include <math.h>
+#include "SQD/SphericalPotential/SHEGPotential.h"
+#include "Numerics/RadialFunctorUtility.h"
+#include "OhmmsData/ParameterSet.h"
+
+namespace ohmmshf {
+  SHEGPotential::SHEGPotential(int nel, value_type rs): 
+    Ntot(nel), Zext(nel), Rs(rs) 
+    { 
+      MaxEigenValue=0.0;
+    }
+
+  RadialPotentialBase::value_type
+    SHEGPotential::evaluate(const BasisSetType& psi, 
+        RadialOrbitalSet_t& V, 
+        int norb) {
+      RadialOrbital_t integrand(psi(0));
+
+      //intialize the external potential
+      if(!Vext) {
+        //evaluate Rmax from the density
+        Rmax=Rs*std::pow(Ntot,1.0/3.0);
+
+        Qinfty=Zext;//assign Zext to Qinfty
+        Rcut=Rmax;//assign Rmax to Rcut 
+
+        value_type normin=-Zext/2.0/Rmax;
+        value_type r2=1.0/Rmax/Rmax;
+
+        XMLReport("  Total charge            = " << Ntot);
+        XMLReport("  Total background charge = " << Zext);
+        XMLReport("  Rs (density)            = " << Rs);
+        XMLReport("  Rmax (background)       = " << Rmax);
+
+        Vext = new RadialOrbital_t(psi(0));
+        for(int ig=0; ig < psi.m_grid->size(); ++ig)  {
+          value_type r=(*psi.m_grid)(ig);
+          if(r<=Rmax) 
+            (*Vext)(ig)=normin*(3.0-(r*r)*r2);
+          else
+            (*Vext)(ig)=-Zext/r;
+          //cout << r << " " << (*Vext)(ig) << endl;
+        }
+      }
+
+      for(int ig=0; ig < psi.m_grid->size(); ++ig)  {
+        value_type t = (*Vext)(ig);
+        value_type sum = 0.0;
+        for(int o=0; o < norb; o++) {
+          V[o](ig) += t;
+          sum += psi(o,ig)*psi(o,ig);
+          //sum += pow(psi(o,ig),2);
+        }
+        integrand(ig) = t*sum;
+      }
+      return integrate_RK2(integrand);
+    }
+
+  /** return the number of nodes for n and l
+   * @param n principal quantum number
+   * @param l angular number
+   * @return number of nodes
+   */
+  int SHEGPotential::getNumOfNodes(int n, int l){
+    MinEigenValue=-Ntot*3./2./Rmax;
+    //double check this
+    return n;
+  }
+
+  bool SHEGPotential::put(xmlNodePtr cur) {
+    ParameterSet params;
+    params.add(Rs,"rs","double");
+    params.add(Zext,"Z","double");
+    params.put(cur);
+
+    if(Zext<Ntot)
+    {
+      XMLReport("Invalid background charge Z=" << Zext << "  Overwriting by " << Ntot);
+      Zext=Ntot;
+    }
+    return true;
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 322 $   $Date: 2005-05-07 11:55:23 -0500 (Sat, 07 May 2005) $
+ * $Id: SHEGPotential.cpp 322 2005-05-07 16:55:23Z jnkim $ 
+ ***************************************************************************/
---
+++ src/SQD/SphericalPotential/SHEGPotential.h
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003  by Jeongnim Kim and Jordan Vincent
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef OHMMSHF_SPHERICAL_HEG_POTENTIAL_H
+#define OHMMSHF_SPHERICAL_HEG_POTENTIAL_H
+
+#include "SQD/SphericalPotential/RadialPotential.h"
+
+namespace ohmmshf {
+
+  /**
+     @ingroup RadialPotential
+     @class SHEGPotential
+     @brief implements spherical jellium
+   */
+  struct SHEGPotential: public RadialPotentialBase {
+    ///number of particles
+    value_type Ntot;
+    ///external background charge
+    value_type Zext;
+    ///density
+    value_type Rs;
+    ///maximum radius of the background charge
+    value_type Rmax;
+
+    SHEGPotential(int nel, value_type rs=1);
+    value_type evaluate(const BasisSetType& psi, 
+			RadialOrbitalSet_t& V, int norb);
+    ///return \f$ n \f$
+    int getNumOfNodes(int n, int l);
+
+    bool put(xmlNodePtr cur);
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: qmc $
+ * $Revision: 42 $   $Date: 2004-09-15 18:05:20 -0500 (Wed, 15 Sep 2004) $
+ * $Id: SHEGPotential.h 42 2004-09-15 23:05:20Z qmc $ 
+ ***************************************************************************/
