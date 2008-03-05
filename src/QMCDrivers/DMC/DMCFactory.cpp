ef88cfd05f1e6d66d1dfe85189d72e4af20b88f4
Jeongnim Kim <jeongnim.kim@intel.com>
2008-03-05 16:23:16 +0000

    Add DMCPeta for testing.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2520 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/DMC/DMCFactory.cpp
+++ src/QMCDrivers/DMC/DMCFactory.cpp
@@ -8,27 +8,31 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
-//   Department of Physics, Ohio State University
-//   Ohio Supercomputer Center
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
 #include "QMCDrivers/DMC/DMCFactory.h" 
 #include "QMCDrivers/DMC/DMC.h" 
+#include "QMCDrivers/DMC/DMCPeta.h" 
 #if defined(ENABLE_OPENMP)
 #include "QMCDrivers/DMC/DMCOMP.h" 
 #endif
 #include "Message/OpenMP.h"
+
+//#define PETA_DMC_TEST
 
 namespace qmcplusplus {
 
   QMCDriver* DMCFactory::create(MCWalkerConfiguration& w, TrialWaveFunction& psi, 
       QMCHamiltonian& h, HamiltonianPool& hpool) {
     QMCDriver* qmc=0;
+#if defined(PETA_DMC_TEST)
+    qmc = new DMCPeta(w,psi,h);
+#else
+
 #if defined(ENABLE_OPENMP)
     int np=omp_get_max_threads();
     if(np>1) {
@@ -41,6 +45,8 @@ namespace qmcplusplus {
 #else
     app_log() << "Creating DMC for the qmc driver" << endl;
     qmc = new DMC(w,psi,h); 
+#endif
+
 #endif
     qmc->setUpdateMode(PbyPUpdate);
 
