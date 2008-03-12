8097edd6846250f81efe07a8e7b0c0ee5598d4ee
Jeongnim Kim <jeongnim.kim@intel.com>
2008-03-12 15:15:45 +0000

    Base class to handle wavefunction derivatives.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2527 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/DiffOrbitalBase.cpp
@@ -0,0 +1,128 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2008-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include "QMCWaveFunctions/DiffOrbitalBase.h"
+
+/**@file DiffOrbitalBase.cpp
+ *@brief Definition of NumericalDiffOrbital
+ */
+namespace qmcplusplus {
+
+  //@{implementation of the base class of DiffOrbitalBase
+  void DiffOrbitalBase::resize(int nptcls)
+  {
+    dLogPsi.resize(nptcls);
+    d2LogPsi.resize(nptcls);
+  }
+
+  void DiffOrbitalBase::setParameter(const string& a, RealType v)
+  {
+    targetParam.first=a;
+    targetParam.second=v;
+  }
+
+  bool DiffOrbitalBase::isOptimizable(OptimizableSetType& optVariables)
+  {
+    return optVariables.find(target.first) != optiVariables.end();
+  }
+
+  void DiffOrbitalBase::evaluateDerivatives(ParticleSet& P, RealType ke0)
+  {
+    gradLogPsi=0.0;
+    lapLogPsi=0.0;
+
+    dLogPsi=differenciate(P);
+
+    dHPsi=Sum(lapLogPsi)+2.0*Dot(P.G,gradLogPsi)+dLogPsi*ke0;
+  }
+  //@}
+
+  //@{implementation of NumericalDiffOrbital
+  void NumericalDiffOrbital::resetParameters(OptimizableSetType& optVariables)
+  {
+    OptimizableSetType::iterator oit(optVariables.find(target.first));
+    if(oit!=optVariables.end())
+    {
+      target.second=(*oit).second;
+    }
+  }
+
+  void NumericalDiffOrbital::resetTargetParticleSet(ParticleSet& P)
+  {
+    //do nothing
+  }
+
+  DiffOrbitalBase::RealType NumericalDiffOrbital::differentiate(ParticleSet& P)
+  {
+    OptimizableSetType v;
+    RealType curvar=targetParam.second;
+
+    GradVectorType dg_p(gradLogPsi.size());
+    GradVectorType dg_m(gradLogPsi.size());
+    ValueVectorType dl_p(gradLogPsi.size());
+    ValueVectorType dl_m(gradLogPsi.size());
+    
+    //this should be modified later
+    const RealType delta=0.001;
+
+    //reset reference orbital with +
+    v[targetParam.first]=curvar+delta;
+    refOrbital->resetParameters(v);
+    RealType plus=evaluateLog(P,dg_p,dl_p);
+
+    //reset reference orbital with -
+    v[targetParam.first]=curvar-delta;
+    refOrbital->resetParameters(v);
+    RealType minus=evaluateLog(P,dg_m,dl_m);
+
+    //restore the variable to the original state
+    v[targetParam.first]=curvar;
+    refOrbital->resetParameters(v);
+    
+    const RealType dh=1.0/(2.0*delta);
+    gradLogPsi=dh*(dg_p-dg_m);
+    lapLogPsi=dh*(dl_p-dl_m);
+    return dh*(plus-minus);
+  }
+  //@}
+  
+  //@{implementation of AnalyticDiffOrbital
+  void AnalyticDiffOrbital::resetParameters(OptimizableSetType& optVariables)
+  {
+    OptimizableSetType::iterator oit(optVariables.find(target.first));
+    if(oit!=optVariables.end())
+    {
+      target.second=(*oit).second;
+      refOrbital->resetParameters(optVariables);
+    }
+  }
+
+  void AnalyticDiffOrbital::resetTargetParticleSet(ParticleSet& P)
+  {
+    refOrbital->resetTargetParticleSet(P);
+  }
+
+  DiffOrbitalBase::RealType AnalyticDiffOrbital::differentiate(ParticleSet& P)
+  {
+    return refOrbital->evaluateLog(P,gradLogPsi,lapLogPsi);
+  }
+  //@}
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1770 $   $Date: 2007-02-17 17:45:38 -0600 (Sat, 17 Feb 2007) $
+ * $Id: OrbitalBase.h 1770 2007-02-17 23:45:38Z jnkim $ 
+ ***************************************************************************/
+
---
+++ src/QMCWaveFunctions/DiffOrbitalBase.h
@@ -0,0 +1,141 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2008-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_DIFFERENTIAL_ORBITAL_BASE_H
+#define QMCPLUSPLUS_DIFFERENTIAL_ORBITAL_BASE_H
+#include "Configuration.h"
+#include "QMCWaveFunctions/OrbitalBase.h"
+
+/**@file DiffOrbitalBase.h
+ *@brief Declaration of DiffOrbitalBase, NumericalDiffOrbital and AnalyticDiffOrbital
+ */
+namespace qmcplusplus {
+
+
+  /**@defgroup OrbitalComponent Orbital group
+   * @brief Base base class for derivatives of OrbitalBase
+   *
+   * Derived classes implement the differentiate function which evaluates
+   * - \f$\fraction{\partial \log\Psi}{\partial \alpha}\$
+   * - \f$\nabla \fraction{\partial \log\Psi}{\partial \alpha}\$
+   * - \f$\nabla^2 \fraction{\partial \log\Psi}{\partial \alpha}\$
+   */
+  struct DiffOrbitalBase
+  {
+    //@{typedefs inherited from OrbitalBase
+    typedef OrbitalBase::RealType           RealType;
+    typedef OrbitalBase::ValueType          ValueType;
+    typedef OrbitalBase::GradVectorType     GradVectorType;
+    typedef OrbitalBase::ValueVectorTypet   ValueVectorType;
+    typedef OrbitalBase::OptimizableSetType OptimizableSetType;
+    //@}
+    //
+    ///\f$\frac{\partial_{\alpha}\Psi}{\Psi} = \partial_{\alpha}\log\Psi\f$
+    RealType dLogPsi;
+    ///\f$\frac{H\partial_{\alpha}\Psi}{\Psi} = \partial_{\alpha}\nabla^2 \log\Psi\f$
+    RealType dHPsi;
+    ///\f$\nabla \partial_{\alpha} log\Psi\f$
+    GradVectorType gradLogPsi;
+    ///\f$\nabla^2 \partial_{\alpha} log\Psi\f$
+    ValueVectorType lapLogPsi;
+    /** target parameter
+     *
+     * OptimizableSetType is derived from a map and data_type is pair<string,T>
+     */
+    OptimizableSetType::data_type targetParam;
+
+    /// default constructor
+    inline DiffOrbitalBase(){ }
+
+    ///default destructor
+    virtual ~DiffOrbitalBase(){ }
+
+    /** resize internal container
+     * @param nptcls the number of particles
+     */
+    void resize(int nptcls);
+
+    /**  set the name of the optimizable variable 
+     * @param a name of the variable
+     * @param v initial value of the variable
+     */
+    void setParameter(const string& a, RealType v);
+
+    /** true, if optVariables has this object as active variables
+     */
+    bool isOptimizable(OptimizableSetType& optVariables);
+
+    /** evaluate derivatives at \f$\{R\}\f$
+     * @param P current configuration
+     * @param ke0 current kinetic energy
+     */
+    void evaluateDerivatives(ParticleSet& P, RealType ke0);
+
+    /** reset the parameters during optimizations
+     */
+    void resetParameters(OptimizableSetType& optVariables)=0;
+
+    /** reset properties, e.g., distance tables, for a new target ParticleSet
+     * @param P ParticleSet
+     */
+    virtual void resetTargetParticleSet(ParticleSet& P)=0;
+
+    /** evaluate the value of the orbital
+     * @param P active ParticleSet
+     * @return \f$\partial_{\alpha}\log\Psi\f$
+     */
+    virtual RealType differentiate(ParticleSet& P) = 0;
+
+  };
+
+  /** a generic DiffOrbitalBase using a finite-difference method
+   *
+   * This class handles any orbital whose analytic derivatives are not implemented nor easily available.
+   */
+  struct NumericalDiffOrbital: public DiffOrbitalBase
+  {
+
+    NumericalDiffOrbital(OrbitalBase* orb=0): refOrbital(orb) {}
+
+    void resetParameters(OptimizableSetType& optVariables);
+    void resetTargetParticleSet(ParticleSet& P);
+    RealType differentiate(ParticleSet& P);
+
+    ///pointer to the reference orbital
+    OrbitalBase* refOrbital;
+  };
+
+  /** a generic DiffOrbitalBase using analytic derivates
+   */
+  struct AnalyticDiffOrbital: public DiffOrbitalBase
+  {
+
+    AnalyticlDiffOrbital(OrbitalBase* orb=0): refOrbital(orb) {}
+
+    void resetParameters(OptimizableSetType& optVariables);
+    void resetTargetParticleSet(ParticleSet& P);
+    RealType differentiate(ParticleSet& P);
+
+    ///pointer to the reference orbital
+    OrbitalBase* refOrbital;
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 1770 $   $Date: 2007-02-17 17:45:38 -0600 (Sat, 17 Feb 2007) $
+ * $Id: OrbitalBase.h 1770 2007-02-17 23:45:38Z jnkim $ 
+ ***************************************************************************/
+
