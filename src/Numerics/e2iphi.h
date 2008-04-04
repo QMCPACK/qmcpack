da91da0dda4fbdf3d488f2495cdf76ec890d18bd
Ken Esler <kpesler@gmail.com>
2008-04-04 15:10:40 +0000

    Adding a helper for quickly evaluating e^{i \phi}.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2558 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Numerics/e2iphi.h
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2007-  Ken Esler
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: esler@uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//////////////////////////////////////////////////////////////////
+#ifndef QMCPLUSPLUS_E2IPHI_H
+#define QMCPLUSPLUS_E2IPHI_H
+
+#include <vector>
+#include <complex>
+
+#ifdef HAVE_ACML
+#include <acml.h>
+#endif
+
+#ifdef HAVE_MKL
+#include <mkl.h>
+#endif
+
+
+inline void
+e2iphi (std::vector<double> phi, std::vector<std::complex<double> > z)
+{
+#ifdef HAVE_ACML
+  int n = phi.size();
+  double c[n], s[n];
+  vrda_sincos(n, &(phi[0]), s, c);
+  for (int i=0; i<n; i++)
+    z[i] = std::complex<double>(c[i],s[i]);
+#elifdef HAVE_MKL
+  vzCIS (n, &(phi[0]), (double*) &(z[0]));
+#else
+  for (int i=0; i<n; i++)
+    sincos (phi[i], &(z[i].imag()), &(z[i].real()));
+#endif
+}
+
+#endif
