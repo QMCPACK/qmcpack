9dc3b3c6b0359acc9dad48be610cebf1d01608f7
Jeongnim Kim <jeongnim.kim@intel.com>
2008-05-05 18:35:50 +0000

    An engine to start/end of a function call.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2652 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Utilities/ProgressReportEngine.cpp
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2008- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+
+#include "Configuration.h"
+#include "Utilities/ProgressReportEngine.h"
+
+namespace APPNAMESPACE
+{
+  void ReportEngine::echo(xmlNodePtr cur, bool recursive)
+  {
+    if(cur==NULL) return;
+    app_log()<< "<input node=\""<<(const char*)(cur->name) << "\"";
+    xmlAttrPtr att = cur->properties;
+    char atext[32];
+    while(att != NULL) 
+    {
+      sprintf(atext,"  %s=\"%s\"",(const char*)(att->name),(const char*)(att->children->content));
+      app_log() << atext;
+      att = att->next;
+    }
+    app_log() << "/>\n"; 
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 2468 $   $Date: 2008-02-22 09:27:30 -0500 (Fri, 22 Feb 2008) $
+ * $Id: Communicate.h 2468 2008-02-22 14:27:30Z jnkim $ 
+ ***************************************************************************/
---
+++ src/Utilities/ProgressReportEngine.h
@@ -0,0 +1,127 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2008- by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file ProgressReportEngine.h
+ * @brief declaration of ProgressReportEngine
+ */
+
+#ifndef QMCPLUSPLUS_PROGRESSREPORTENGINE_H
+#define QMCPLUSPLUS_PROGRESSREPORTENGINE_H
+
+#include "Utilities/OhmmsInfo.h"
+#include "Message/Communicate.h"
+#include "Utilities/Timer.h"
+#include "OhmmsData/OhmmsElementBase.h"
+
+namespace APPNAMESPACE
+{
+  /** 
+   *
+   * Final class and should not be derived.
+   */
+  class ReportEngine
+  {
+    public:
+
+      inline ReportEngine(const std::string& cname, const std::string& fname, int atype=1):
+        ReportType(atype),ClassName(cname), FuncName(fname), LogBuffer(*OhmmsInfo::Log)
+        { 
+          //if(ReportType)
+          //  LogBuffer << ("<echo className=\""+ClassName+"\" funcName=\""+FuncName+"\">\n");
+          //else
+          //  LogBuffer << ("<"+ClassName+">\n");
+          LogBuffer.flush();//always flush
+        }
+
+      inline ~ReportEngine() 
+      {
+        //if(ReportType)
+        //  LogBuffer << "</echo>\n";
+        //else
+        //  LogBuffer << ("</"+ClassName+">\n");
+        LogBuffer.flush();
+      }
+
+      inline void flush()
+      {
+        LogBuffer << '\n'; LogBuffer.flush();
+      }
+
+      inline void startWarning()
+      {
+        LogBuffer << "<warning>\n";
+      }
+      inline void endWarning()
+      {
+        LogBuffer << "</warning>\n";
+      }
+
+      inline void warning(const std::string& msg)
+      {
+        LogBuffer << ("<warning>"+msg+"</warning>\n");
+      }
+
+      inline void startError()
+      {
+        LogBuffer << "<error node=\"" << OHMMS::Controller->rank() << "\">\n";
+      }
+
+      inline void endError(bool fatal=false)
+      {
+        LogBuffer << "</error>\n";
+        if(fatal) APP_ABORT(ClassName+"::"+FuncName);
+      }
+
+      inline void error(const std::string& msg, bool fatal=false)
+      {
+        LogBuffer << ("<error>"+msg+"</error>\n");
+        if(fatal) APP_ABORT(ClassName+"::"+FuncName);
+      }
+
+      void echo(xmlNodePtr cur, bool recursive=false);
+
+    private:
+      ///type of report
+      int ReportType;
+      /** class Name
+       */
+      std::string ClassName;
+      /** name of the current  member function
+       */
+      std::string FuncName;
+      /** arguments
+       */
+      std::vector<std::string> ArgList;
+      /** stream for log message
+       */
+      OhmmsInform& LogBuffer;
+      //disable copy constructor
+      ReportEngine(const ReportEngine& a):LogBuffer(*OhmmsInfo::Log) {}
+  };
+
+  // templated version of operator<< for Inform objects
+  template<class T>
+    inline
+    ReportEngine& operator<<(ReportEngine& o, const T& val) {
+      app_log()<< val;
+      return o;
+    }
+}
+#endif // QMCPLUSPLUS_MPIOBJECTBASE_H
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 2468 $   $Date: 2008-02-22 09:27:30 -0500 (Fri, 22 Feb 2008) $
+ * $Id: Communicate.h 2468 2008-02-22 14:27:30Z jnkim $ 
+ ***************************************************************************/
