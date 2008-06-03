d0a0daa2efd695aba7c9f13db57e6a296bd001b9
Jeongnim Kim <jeongnim.kim@intel.com>
2008-06-03 15:08:18 +0000

    Add compiler checker to enable mmx/sse.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2720 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- CMake/GNUCompilers.cmake
+++ CMake/GNUCompilers.cmake
@@ -2,9 +2,40 @@
 IF(CMAKE_COMPILER_IS_GNUCXX) 
   ADD_DEFINITIONS(-Drestrict=__restrict__ -DADD_ -DINLINE_ALL=inline)
   SET(CMAKE_CXX_FLAGS "-O6 -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -funroll-all-loops   -finline-limit=1000 -ffast-math -Wno-deprecated ")
-  SET(CMAKE_C_FLAGS "-O6 -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -funroll-all-loops   -finline-limit=1000 -ffast-math -Wno-deprecated ")
-#  SET(CMAKE_CXX_FLAGS "-O6 -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -funroll-all-loops   -finline-limit=1000 -ffast-math -Wno-deprecated -pg")
-#  SET(CMAKE_CXX_FLAGS "-g -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -Wno-deprecated")
+  SET(CMAKE_C_FLAGS "-O3 -Drestrict=__restrict__ -fstrict-aliasing -funroll-all-loops   -finline-limit=1000 -ffast-math -std=gnu99 -fomit-frame-pointer ")
+
+  SET(CMAKE_TRY_GNU_CC_FLAGS "-mmmx")
+  CHECK_CC_ACCEPTS_FLAG(${CMAKE_TRY_GNU_CC_FLAGS} GNU_CC_FLAGS)
+  IF(GNU_CC_FLAGS)
+    SET(HAVE_MMX 1)
+  ENDIF(GNU_CC_FLAGS)
+
+  SET(CMAKE_TRY_GNU_CC_FLAGS "-msse")
+  CHECK_CC_ACCEPTS_FLAG(${CMAKE_TRY_GNU_CC_FLAGS} GNU_CC_FLAGS)
+  IF(GNU_CC_FLAGS)
+    SET(HAVE_SSE 1)
+  ENDIF(GNU_CC_FLAGS)
+
+  SET(CMAKE_TRY_GNU_CXX_FLAGS "-msse2")
+  CHECK_CC_ACCEPTS_FLAG(${CMAKE_TRY_GNU_CC_FLAGS} GNU_CC_FLAGS)
+  IF(GNU_CC_FLAGS)
+    SET(HAVE_SSE2 1)
+  ENDIF(GNU_CC_FLAGS)
+
+  SET(CMAKE_TRY_GNU_CC_FLAGS "-msse3")
+  CHECK_CC_ACCEPTS_FLAG(${CMAKE_TRY_GNU_CC_FLAGS} GNU_CC_FLAGS)
+  IF(GNU_CC_FLAGS)
+    SET(HAVE_SSE3 1)
+  ENDIF(GNU_CC_FLAGS)
+
+  SET(CMAKE_TRY_GNU_CC_FLAGS "-mssse3")
+  CHECK_CC_ACCEPTS_FLAG(${CMAKE_TRY_GNU_CC_FLAGS} GNU_CC_FLAGS)
+  IF(GNU_CC_FLAGS)
+    SET(HAVE_SSSE3 1)
+  ENDIF(GNU_CC_FLAGS)
+
+  #  SET(CMAKE_CXX_FLAGS "-O6 -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -funroll-all-loops   -finline-limit=1000 -ffast-math -Wno-deprecated -pg")
+  #  SET(CMAKE_CXX_FLAGS "-g -ftemplate-depth-60 -Drestrict=__restrict__ -fstrict-aliasing -Wno-deprecated")
 
   IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   # SET(CMAKE_SHARED_LIBRARY_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CXX_FLAGS} -faltivec -framework Accelerate -bind_at_load")
