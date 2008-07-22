a528483e04c21e1f197388c096c71cafad0b83f2
Ken Esler <kpesler@gmail.com>
2008-07-22 17:38:40 +0000

    Added a needed bcast operator for Arry<complex<double>,1>.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@2902 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Message/CommOperatorsMPI.h
+++ src/Message/CommOperatorsMPI.h
@@ -365,6 +365,7 @@ Communicate::bcast(APPNAMESPACE::Vector<int>& g)
   MPI_Bcast(&(g[0]),g.size(),MPI_INT,0,myMPI);
 }
 
+
 template<>
 inline void
 Communicate::bcast(APPNAMESPACE::Vector<APPNAMESPACE::TinyVector<double,2> >& g)
@@ -385,6 +386,22 @@ Communicate::bcast(Array<double,3> &g)
 {
   MPI_Bcast(g.data(), g.size(), MPI_DOUBLE, 0, myMPI);
 }
+
+template<>
+inline void
+Communicate::bcast(Array<int,1> &g)
+{
+  MPI_Bcast(g.data(), g.size(), MPI_INT, 0, myMPI);
+}
+
+
+template<>
+inline void
+Communicate::bcast(Array<complex<double>,1> &g)
+{
+  MPI_Bcast(g.data(), 2*g.size(), MPI_DOUBLE, 0, myMPI);
+}
+
 
 template<>
 inline void
