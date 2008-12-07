a54466e368dc8d770aad82c39f07d32a538e4a27
Jeongnim Kim <jeongnim.kim@intel.com>
2008-12-07 18:40:36 +0000

    Move templated version of Dirac and Slater determinants to Experimental. Use classes in Fermion.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3422 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/GroupedOrbitalSet.h
+++ src/QMCWaveFunctions/GroupedOrbitalSet.h
@@ -7,102 +7,86 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
-#ifndef QMCPLUSPLUS_GROUPEDORBITALSET_H
-#define QMCPLUSPLUS_GROUPEDORBITALSET_H
+/* @file GroupedOrbialSet.h
+ * @brief Declaration/definition of GroupedOrbitalSet
+ */
+#ifndef QMCPLUSPLUS_GROUPED_SPORBITALSET_H
+#define QMCPLUSPLUS_GROUPED_SPORBITALSET_H
 #include "QMCWaveFunctions/SPOSetBase.h"
 
-namespace qmcplusplus {
+namespace qmcplusplus 
+{
 
-/** A derived class from SPOSetBase
- *
- * Handles multiple groups of Single-particle orbitals.
- * @example GroupedOrbitalSet<TricubicBsplineSet<T> >
- */
-  template<class OT>
-    struct GroupedOrbitalSet: public SPOSetBase {
+  /** A derived class from SPOSetBase
+   *
+   * Handles multiple groups of Single-particle orbitals.
+   * @example GroupedOrbitalSet<TricubicBsplineSet<T> >
+   */
+  template<class OT> 
+    struct GroupedOrbitalSet: public SPOSetBase 
+  {
 
-      ///the type of single-particle orbtials 
-      typedef vector<OT*>             SPOContainer_t;
-      typedef typename OT::value_type value_type;
+    ///the type of single-particle orbtials 
+    typedef vector<OT*>             SPOContainer_t;
+    typedef typename OT::value_type value_type;
 
-      SPOContainer_t Phi;
+    SPOContainer_t Phi;
 
-      ///constructor
-      GroupedOrbitalSet(int norbs=0)
-      { 
-        setOrbitalSetSize(norbs);
-      }
+    ///constructor
+    GroupedOrbitalSet(int norbs=0)
+    { 
+      setOrbitalSetSize(norbs);
+    }
 
-      /**add a single-particle orbital */
-      int add(OT* afunction) 
-      {
-        Phi.push_back(afunction);
-        return Phi.size()-1;
-      }
+    /**add a single-particle orbital */
+    int add(OT* afunction) 
+    {
+      Phi.push_back(afunction);
+      return Phi.size()-1;
+    }
 
-      void setOrbitalSetSize(int norbs) { 
-        if(norbs == OrbitalSetSize ) return;
-        OrbitalSetSize=norbs;
-        BasisSetSize=norbs;
-      }
+    void setOrbitalSetSize(int norbs) 
+    { 
+      if(norbs == OrbitalSetSize ) return;
+      OrbitalSetSize=norbs;
+      BasisSetSize=norbs;
+    }
 
-      void resetParameters(const opt_variables_type& active) 
-      { 
-        for(int i=0; i<Phi.size(); i++) Phi[i]->resetParameters(active); 
-      }
+    void resetParameters(const opt_variables_type& active) 
+    { 
+      for(int i=0; i<Phi.size(); i++) Phi[i]->resetParameters(active); 
+    }
 
-      void resetTargetParticleSet(ParticleSet& P) { }
+    void resetTargetParticleSet(ParticleSet& P) { }
 
-      void 
-        evaluate(const ParticleSet& P, int iat, ValueVector_t& psi) 
-        {
-          for(int m=0; m<Phi.size(); m++)
-          {
-            Phi[m]->evaluate(P.R[iat],psi);
-          }
-        }
+    void evaluate(const ParticleSet& P, int iat, ValueVector_t& psi) 
+    {
+      for(int m=0; m<Phi.size(); m++)
+        Phi[m]->evaluate(P.R[iat],psi);
+    }
 
-      void 
-        evaluate(const ParticleSet& P, int iat, 
-            ValueVector_t& psi, GradVector_t& dpsi, ValueVector_t& d2psi) 
-        {
-          for(int m=0; m<Phi.size(); m++)
-          {
-            Phi[m]->evaluate(P.R[iat],psi,dpsi,d2psi);
-          }
-        }
+    void evaluate(const ParticleSet& P, int iat
+        , ValueVector_t& psi, GradVector_t& dpsi, ValueVector_t& d2psi) 
+    {
+      for(int m=0; m<Phi.size(); m++)
+        Phi[m]->evaluate(P.R[iat],psi,dpsi,d2psi);
+    }
 
-      void evaluate(const ParticleSet& P, int first, int last,
-          ValueMatrix_t& logdet, GradMatrix_t& dlogdet, ValueMatrix_t& d2logdet)
-      {
-        //ValueVector_t psi(OrbitalSetSize);
-        //GradVector_t dpsi(OrbitalSetSize);
-        //ValueVector_t d2psi(OrbitalSetSize);
-        //for(int iat=first, i=0; iat<last; iat++,i++)
-        //{
-        //  for(int m=0; m<Phi.size(); m++)
-        //  {
-        //    Phi[m]->evaluate(P.R[iat],psi,dpsi,d2psi);
-        //  }
-        //  for(int m=0;m<OrbitalSetSize;m++) logdet(m,i)=psi[m];
-        //  std::copy(dpsi.begin(),dpsi.end(),dlogdet[i]);
-        //  std::copy(d2psi.begin(),d2psi.end(),d2logdet[i]);
-        //}
-        for(int iat=first, i=0; iat<last; iat++,i++)
-        {
-          for(int m=0; m<Phi.size(); m++)
-            Phi[m]->evaluate(P.R[iat],i,logdet,dlogdet,d2logdet);
-        }
-      }
+    void evaluate(const ParticleSet& P, int first, int last
+        , ValueMatrix_t& logdet, GradMatrix_t& dlogdet, ValueMatrix_t& d2logdet)
+    {
+      for(int iat=first, i=0; iat<last; iat++,i++)
+        for(int m=0; m<Phi.size(); m++)
+          Phi[m]->evaluate(P.R[iat],i,logdet,dlogdet,d2logdet);
+    }
 
-    };
+  };
 }
 #endif
 /***************************************************************************
