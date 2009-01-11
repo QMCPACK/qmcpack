08a0f07134827f4dd8c184c81d0a1549afe61bce
Jeongnim Kim <jeongnim.kim@intel.com>
2009-01-11 22:52:10 +0000

    Revised HFDHE2Potential with tail correction.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3462 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCHamiltonians/HFDHE2PotentialWithCorrection.cpp
@@ -0,0 +1,73 @@
+#include "QMCHamiltonians/HFDHE2Potential.h"
+#include "Particle/DistanceTableData.h"
+namespace qmcplusplus {
+
+  HFDHE2Potential::HFDHE2Potential(ParticleSet& P)
+  {
+    Dependants=1;
+    A = 18.63475757;
+    alpha = -2.381392669;
+    c1=1.460008056;
+    c2=14.22016431;
+    c3=187.2033646;
+    D = 6.960524706;
+
+    rc = P.Lattice.WignerSeitzRadius;
+    kpre_factor = 3.157733e+5/static_cast<Return_t>(P.getTotalNum());
+
+    tailcorr=mycorrection(rc);
+    //Return_t r2 = (rc*rc);
+    //Return_t rm2 = 1.0/r2;
+    //Return_t rm6 = std::pow(rm2,3);
+    //Return_t rm8 = rm6*rm2;
+    //Return_t rm10 = rm8*rm2;
+    //tailcorr = (A*std::exp(alpha*rc) - (c1*rm6+c2*rm8+c3*rm10)*dampF(rc));
+
+  }
+
+  void HFDHE2Potential::resetTargetParticleSet(ParticleSet& P)  
+  {
+    rc = P.Lattice.WignerSeitzRadius;
+    tailcorr=mycorrection(rc);
+  }
+
+  HFDHE2Potential::Return_t HFDHE2Potential::evaluate(ParticleSet& P) 
+  {
+    Value = 0.0;
+    TCValue = 0.0;
+
+    const DistableTableData* d_table=P.DistTables[0];
+    for(int i=0; i<d_table->getTotNadj(); ++i) 
+    {
+      Return_t r1 = d_table->r(i);
+      if ( r1 < rc) {
+        Value += mycorrection(r1)-tailcorr;
+        //Return_t r2 = (r1*r1);
+        //Return_t rm2 = 1.0/r2;
+        //Return_t rm6 = std::pow(rm2,3);
+        //Return_t rm8 = rm6*rm2;
+        //Return_t rm10 = rm8*rm2;
+        //Value += (A*std::exp(alpha*r1) - (c1*rm6+c2*rm8+c3*rm10)*dampF(r1)) - tailcorr;
+        TCValue +=tailcorr;
+      }
+    }
+    //TCorr->set_TC(TCValue);
+    return Value;
+  }
+
+  void  HFDHE2Potential::add2Hamiltonian(ParticleSet& qp, TrialWaveFunction& psi
+      , QMCHamiltonian& targetH)
+  {
+    HFDHE2Potential* myclone=new HFDHE2Potential(qp); 
+    targetH.addOperator(myclone,myName,true);
+    myclone->addCorrection(targetH);
+  }
+
+  void  HFDHE2Potential::addCorrection(QMCHamiltonian& targetH)
+  {
+    HFDHE2Potential_tail* tcrr = new HFDHE2Potential_tail(this);
+    string corrected=myName+"_tail";
+    targetH.addOperator(tcrr,corrected,false);
+  }
+
+}
