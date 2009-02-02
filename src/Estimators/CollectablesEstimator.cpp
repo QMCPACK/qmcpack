b14554fcf57a0a1f09b56057eb7ae3f28cda727b
Jeongnim Kim <jeongnim.kim@intel.com>
2009-02-02 16:04:01 +0000

    Add a estimator for collectables. Will have more updates.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3503 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Estimators/CollectablesEstimator.cpp
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2009- by Jeongnim Kim 
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#include <Estimators/CollectablesEstimator.h>
+#include <QMCHamiltonians/observable_helper.h>
+
+namespace qmcplusplus {
+
+  CollectablesEstimator::CollectablesEstimator(QMCHamiltonian& h)
+    : refH(h)
+  { 
+    scalars.resize(h.sizeOfCollectables());
+    scalars_saved.resize(h.sizeOfCollectables());
+  }
+
+  void CollectablesEstimator::registerObservables(vector<observable_helper*>& h5desc
+      , hid_t gid)
+  {
+    int loc=h5desc.size();
+    refH.registerCollectables(h5desc,gid);
+    for(int i=loc; i<h5desc.size(); ++i) h5desc[i]->lower_bound += FirstIndex;
+  }
+
+  CollectablesEstimator* CollectablesEstimator::clone()
+  {
+    return new CollectablesEstimator(*this);
+  }
+
+  /**  add the local energy, variance and all the Hamiltonian components to the scalar record container
+   * @param record storage of scalar records (name,value)
+   */
+  void CollectablesEstimator::add2Record(RecordListType& record) 
+  {
+    FirstIndex = record.size();
+    for(int i=0; i<refH.sizeOfCollectables(); ++i)
+    {
+      ostringstream o;
+      o<<"a"<<i;
+      int dummy=record.add(o.str());
+    }
+    LastIndex = record.size();
+    clear();
+  }
+
+  void CollectablesEstimator::accumulate(const MCWalkerConfiguration& W
+      , WalkerIterator first, WalkerIterator last , RealType wgt)
+  {
+    for(int i=0; i<refH.sizeOfCollectables(); ++i)
+      scalars[i](W.Collectables[i],1.0);
+  }
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 3020 $   $Date: 2008-08-18 16:49:48 -0500 (Mon, 18 Aug 2008) $
+ * $Id: LocalEnergyEstimatorHDF.cpp 3020 2008-08-18 21:49:48Z jnkim $ 
+ ***************************************************************************/
---
+++ src/Estimators/CollectablesEstimator.h
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2009- by Jeongnim Kim 
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_COLLECTABLES_ESTIMATOR_H
+#define QMCPLUSPLUS_COLLECTABLES_ESTIMATOR_H
+#include <Estimators/ScalarEstimatorBase.h>
+#include <QMCHamiltonians/QMCHamiltonian.h>
+
+namespace qmcplusplus {
+
+  /** Handle an ensemble average of Hamiltonian components
+  */
+  class CollectablesEstimator: public ScalarEstimatorBase
+  {
+    ///save the reference hamiltonian
+    const QMCHamiltonian& refH;
+
+    public:
+    /** constructor
+     * @param h QMCHamiltonian to define the components
+     */
+    CollectablesEstimator(QMCHamiltonian& h);
+    //LocalEnergyEstimatorHDF(const LocalEnergyEstimatorHDF& est);
+
+    /** implement virtual function
+    */
+    CollectablesEstimator* clone();
+
+    void registerObservables(vector<observable_helper*>& h5dec, hid_t gid);
+    void add2Record(RecordListType& record);
+    void accumulate(const MCWalkerConfiguration& W
+        , WalkerIterator first, WalkerIterator last , RealType wgt);
+
+  };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: mcminis2 $
+ * $Revision: 3421 $   $Date: 2008-12-09 10:21:11 -0600 (Tue, 09 Dec 2008) $
+ * $Id: LocalEnergyEstimatorHDF.h 3421 2008-12-09 16:21:11Z mcminis2 $ 
+ ***************************************************************************/
