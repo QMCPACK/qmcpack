cf2129246ecc643494b5af7449f95a89518b8e90
Bryan K. Clark <bkclark@gmail.com>
2009-03-24 01:44:42 +0000

    Double excitations now work! Next Step: clean up code and remove some of the things that are being recomputed..then: gradients
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3691 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/Fermion/ExcitationClass.h
+++ src/QMCWaveFunctions/Fermion/ExcitationClass.h
@@ -16,23 +16,89 @@ namespace qmcplusplus {
      vector<int> unoccupied_orbitals_to_use;
     vector<int> orbitals_to_replace;
     ValueMatrix_t dotProducts; // orbitals_to_replace.size() x unoccupied_orbitals_to_use.size()
+      TinyVector<int,2> ii;
+      TinyVector<int,2> jj;
+
+    TinyVector<int,2> second_replaces_first;
+    list<TinyVector<int,2> > replaceList;
+    int orbitals_to_replace_index;
+    int unoccupied_orbitals_to_use_index;
     void testMe()
     {
       int i=5;
     }
+
+
+
+
+    TinyVector<int,2>  begin()
+    {
+      orbitals_to_replace_index=0;
+      unoccupied_orbitals_to_use_index=0;
+      second_replaces_first[0]=orbitals_to_replace[orbitals_to_replace_index];
+      second_replaces_first[1]=unoccupied_orbitals_to_use[unoccupied_orbitals_to_use_index];
+      return second_replaces_first;
+    }
+
+    TinyVector<int,2> next()
+    {
+      unoccupied_orbitals_to_use_index++;
+      if (unoccupied_orbitals_to_use_index==unoccupied_orbitals_to_use.size()){
+	unoccupied_orbitals_to_use_index=0;
+	orbitals_to_replace_index++;
+	if (orbitals_to_replace_index==orbitals_to_replace.size()){
+	  second_replaces_first[0]=-1;
+	  second_replaces_first[1]=-1;
+	  return second_replaces_first;
+	}
+      }
+      second_replaces_first[0]=orbitals_to_replace[orbitals_to_replace_index];
+      second_replaces_first[1]=unoccupied_orbitals_to_use[unoccupied_orbitals_to_use_index];
+      return second_replaces_first;
+    }
+    
+
+
+//     list<TinyVector<int,2> > begin_doubleExcitation()
+//     {
+//       ii[0]=0;
+//       ii[1]=ii[0]+1;
+//       jj[0]=0;
+//       jj[1]=jj[0]+1;
+//       list<TinyVector<int,2> > myList;
+//       myList.push_back(ii);
+//       myList.push_back(jj)
+      
+//       for (int ii1=0;ii1<orbitals_to_replace.size();ii1++){
+// 	for (int ii2=ii1+1;ii2<orbitals_to_replace.size();ii2++){
+// 	  for (int jj1=0;jj1<unoccupied_orbitals_to_use.size();jj1++){
+// 	    for (int jj2=jj1+1;jj2<unoccupied_orbitals_to_use.size();jj2++){
+
+
+
+
+      
+//     }
+
+//     list<TinyVector<int,2> > next_doubleExcitation()
+//     {
+
+
+//     }
 
     //gs_inverse needs to be orbital x ptcl
     //psiM_actual needs to be orbtial x ptcl
     void BuildDotProducts(ValueMatrix_t &gs_inverse,ValueMatrix_t &psiM_actual)
     {
       int NumPtcls=psiM_actual.extent(1);
+      cerr<<"NUMPTLCS IS "<<NumPtcls<<endl;
       for (int ii=0;ii<orbitals_to_replace.size();ii++)
 	for (int jj=0;jj<unoccupied_orbitals_to_use.size();jj++){
 	  int i=orbitals_to_replace[ii];
 	  int j=unoccupied_orbitals_to_use[jj];
-	  dotProducts(i,j)=0.0;
+	  dotProducts(ii,jj)=0.0;
 	  for (int ptcl=0;ptcl<NumPtcls;ptcl++)
-	    dotProducts(i,j)+=gs_inverse(i,ptcl)*psiM_actual(j,ptcl);
+	    dotProducts(ii,jj)+=gs_inverse(i,ptcl)*psiM_actual(j,ptcl);
 	}
     }
     
@@ -56,12 +122,14 @@ namespace qmcplusplus {
     //val should be sent with 1.0 likely upon entering this
     void CalcSingleExcitations(ValueVector_t &coefs,ValueType &val,int &coefIndex)
     {
+      cerr<<"VAL STARTS: "<<val<<endl;
       for (int i=0;i<orbitals_to_replace.size();i++)
 	for (int j=0;j<unoccupied_orbitals_to_use.size();j++){
-	  val+=coefs(coefIndex)*dotProducts(i,j);
+	  cerr<<"ADDING! "<<dotProducts(i,j)<<" "<<i<<" "<<j<<endl;
+	  val+=dotProducts(i,j);// *coefs(coefIndex);
 	  coefIndex++;
 	}
-	  
+      cerr<<"VAL ENDS: "<<val<<endl;
 
 
     }
@@ -70,17 +138,22 @@ namespace qmcplusplus {
     void set_excitations(int start_uo,int end_uo,int start_rep,int end_rep,ValueVector_t &coefs)
     {
       unoccupied_orbitals_to_use.resize(end_uo-start_uo+1);
-      for (int i=start_uo;i<=end_uo;i++)
-	unoccupied_orbitals_to_use[i-start_uo]=start_uo;
+      for (int i=start_uo;i<=end_uo;i++){
+	unoccupied_orbitals_to_use[i-start_uo]=i;
+	cerr<<"UNOCCUPIED "<<start_uo<<endl;
+      }
       orbitals_to_replace.resize(end_rep-start_rep+1);
-      for (int i=start_rep;i<=end_rep;i++)
-	orbitals_to_replace[i-start_rep]=start_rep;
+      for (int i=start_rep;i<=end_rep;i++){
+	orbitals_to_replace[i-start_rep]=i;
+	cerr<<"TO REPLACE"<<start_rep<<endl;
+      }
       dotProducts.resize(orbitals_to_replace.size(),unoccupied_orbitals_to_use.size());
       int m=orbitals_to_replace.size();
       int n=unoccupied_orbitals_to_use.size();
-      coefs.resize(m*(m+1)*n*(n+1)/4);
+      coefs.resize(m*n+m*(m-1)*n*(n-1)/4);
       //for now let's set al teh ocefs to 1;
       coefs=1.0;
+      cerr<<"The coeficient size is "<<m<<" "<<n<<" "<<coefs.size()<<endl;
     }
     
 
