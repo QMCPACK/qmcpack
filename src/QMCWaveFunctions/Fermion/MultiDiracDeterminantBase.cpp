709e0c15c44fc14092d1077dffae480368acc15d
Bryan K. Clark <bkclark@gmail.com>
2009-03-24 03:35:34 +0000

    Cleaned up code. A factor of 2 is still acheivable
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3692 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/Fermion/MultiDiracDeterminantBase.cpp
+++ src/QMCWaveFunctions/Fermion/MultiDiracDeterminantBase.cpp
@@ -227,43 +227,35 @@ namespace qmcplusplus {
    */
   MultiDiracDeterminantBase::ValueType MultiDiracDeterminantBase::ratio(ParticleSet& P, int iat)
   {
-    cerr<<"Calling multi-dirac-determinat-base-ratio"<<" "<<psiMInv.size()<<" "<<psiM_temp.size()<<endl;
     UpdateMode=ORB_PBYP_ALL;
     WorkingIndex = iat-FirstIndex;
-    cerr<<"Working Index is "<<WorkingIndex<<endl;
-    cerr<<"pre-a"<<psiMInv.size()<<" "<<psiM_temp.size()<<endl;
     Phi->evaluate(P, iat, psiV);
-    cerr<<"also pre-a"<<" "<<psiMInv.size()<<" "<<psiM_temp.size()<<endl;
 
 
-	///Copying ground state data to psiM
-	for (int orbital=0;orbital<NumOrbitals;orbital++)
-	  for (int ptcl=0;ptcl<NumPtcls;ptcl++) 
-	    psiM_temp(orbital,ptcl)=psiM_actual(orbital,ptcl); //Note: Because psiM is typically an inverse, it's typically ptcl,orbital
-	//done copying
-	LogValue=InvertWithLog(psiM_temp.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValue);
+// 	///Copying ground state data to psiM
+// 	for (int orbital=0;orbital<NumOrbitals;orbital++)
+// 	  for (int ptcl=0;ptcl<NumPtcls;ptcl++) 
+// 	    psiM_temp(orbital,ptcl)=psiM_actual(orbital,ptcl); //Note: Because psiM is typically an inverse, it's typically ptcl,orbital
+// 	//done copying
+// 	LogValue=InvertWithLog(psiM_temp.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValue);
 
 
     psiMInv=psiM_temp;
-    cerr<<"and pre-tranpose"<<endl;
     MatrixOperators::transpose(psiMInv);
-    cerr<<"A"<<endl;
     Excitations.BuildDotProducts(psiMInv,psiM_actual);
     ValueType oldVal=1.0; //eventually store previous valuess
     int coefIndex=0;
     Excitations.CalcSingleExcitations(coefs,oldVal,coefIndex);
     Excitations.CalcDoubleExcitations(coefs,oldVal,coefIndex);
-    cerr<<"B"<<endl;
     ValueType gs_ratio=0.0;
     for (int orbital=0;orbital<NumOrbitals;orbital++)
       gs_ratio+=psiM_temp(WorkingIndex,orbital)*psiV(orbital);
 
-    cerr<<"ratio gs_ratio is "<<gs_ratio<<endl;
 
 
     //relying here on the fact that DetUpdate only uses the first (psiM_temp.cols()) rows of psiV!
     //update psiM_temp with the row substituted
-    cerr<<"C"<<endl;
+
 
     for (int orbital=0;orbital<NumOrbitals_total;orbital++){
       psiV_old(orbital)=psiM_actual(orbital,WorkingIndex);
@@ -273,27 +265,24 @@ namespace qmcplusplus {
     DetUpdate(psiM_temp,psiV,workV1,workV2,WorkingIndex,gs_ratio);
 
 
-	///Copying ground state data to psiM
-	for (int orbital=0;orbital<NumOrbitals;orbital++)
-	  for (int ptcl=0;ptcl<NumPtcls;ptcl++) 
-	    psiM_temp(orbital,ptcl)=psiM_actual(orbital,ptcl); //Note: Because psiM is typically an inverse, it's typically ptcl,orbital
-	//done copying
-	LogValue=InvertWithLog(psiM_temp.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValue);
+// 	///Copying ground state data to psiM
+// 	for (int orbital=0;orbital<NumOrbitals;orbital++)
+// 	  for (int ptcl=0;ptcl<NumPtcls;ptcl++) 
+// 	    psiM_temp(orbital,ptcl)=psiM_actual(orbital,ptcl); //Note: Because psiM is typically an inverse, it's typically ptcl,orbital
+// 	//done copying
+// 	LogValue=InvertWithLog(psiM_temp.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValue);
 
 
 
     psiMInv=psiM_temp;
     MatrixOperators::transpose(psiMInv);
-    cerr<<"D"<<endl;
    //currnetly psiM sending incorrectly transposed!
     Excitations.BuildDotProducts(psiMInv,psiM_actual);
     ValueType val=1.0;
     coefIndex=0;
-    cerr<<"E"<<endl;
     Excitations.CalcSingleExcitations(coefs,val,coefIndex);
     Excitations.CalcDoubleExcitations(coefs,val,coefIndex);
     curRatio=(gs_ratio*val)/oldVal;
-    cerr<<"curr Ratio is "<<curRatio<<" "<<psiMInv.size()<<" "<<psiM_temp.size()<<endl;
     return curRatio;
   }
 
@@ -305,7 +294,6 @@ namespace qmcplusplus {
   */
   void MultiDiracDeterminantBase::acceptMove(ParticleSet& P, int iat) 
   {
-    cerr<<"MultiDirac accepting"<<endl;
     PhaseValue += evaluatePhase(curRatio);
     LogValue +=std::log(std::abs(curRatio));
     switch(UpdateMode)
@@ -327,13 +315,11 @@ namespace qmcplusplus {
 
         break;
       default:
-	cerr<<"Accepting move"<<endl;
         myG = myG_temp;
         myL = myL_temp;
         psiM = psiM_temp;
         std::copy(dpsiV.begin(),dpsiV.end(),dpsiM[WorkingIndex]);
         std::copy(d2psiV.begin(),d2psiV.end(),d2psiM[WorkingIndex]);
-	cerr<<"Done Accepting move"<<endl;
         break;
     }
 
@@ -343,15 +329,12 @@ namespace qmcplusplus {
   /** move was rejected. copy the real container to the temporary to move on
   */
   void MultiDiracDeterminantBase::restore(int iat) {
-    cerr<<"INto restore"<<endl;
     if(UpdateMode == ORB_PBYP_ALL) {
-      cerr<<"Rejecting move"<<endl;
       psiM_temp = psiM;
       for (int orbital=0;orbital<NumOrbitals_total;orbital++)
 	psiM_actual(orbital,WorkingIndex)=psiV_old(orbital);
       std::copy(dpsiM[WorkingIndex],dpsiM[WorkingIndex+1],dpsiM_temp[WorkingIndex]);
       std::copy(d2psiM[WorkingIndex],d2psiM[WorkingIndex+1],d2psiM_temp[WorkingIndex]);
-      cerr<<"Done Rejecting move"<<endl;
     }
     else if (UpdateMode== ORB_PBYP_RATIO){
       for (int orbital=0;orbital<NumOrbitals_total;orbital++)
@@ -366,7 +349,6 @@ namespace qmcplusplus {
         ParticleSet::ParticleGradient_t& G, 
         ParticleSet::ParticleLaplacian_t& L)
     {
-      cerr<<"I'm calling evaluate log"<<endl;
       //      Phi->evaluate(P, FirstIndex, LastIndex, psiM,dpsiM, d2psiM);
       Phi->evaluate(P, FirstIndex, LastIndex, psiM_actual,dpsiM, d2psiM);
 
@@ -383,7 +365,6 @@ namespace qmcplusplus {
         LogValue = evaluateLogAndPhase(det,PhaseValue);
       } else {
 	
-	cerr<<"WHEN COPYING NUMPTCLS IS "<<NumPtcls<<endl;
 	///Copying ground state data to psiM
 	for (int orbital=0;orbital<NumOrbitals;orbital++)
 	  for (int ptcl=0;ptcl<NumPtcls;ptcl++) 
@@ -405,7 +386,6 @@ namespace qmcplusplus {
         }
       }
       psiM_temp = psiM;
-      cerr<<"EVAL LOG gs_ratio is "<<std::exp(LogValue)<<endl;
       double val1=std::exp(LogValue)*std::cos(abs(PhaseValue));
       //Single Excitations!
       TinyVector<int,2> second_replaces_first=Excitations.begin();
@@ -420,17 +400,14 @@ namespace qmcplusplus {
 	}
 	double PhaseValuep;
 	double LogValuep=InvertWithLog(psiM.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValuep);
-	cerr<<"Pre-LOGVALUE PHASEVALUE "<<LogValue<<" "<<PhaseValue<<" "<<LogValuep<<" "<<PhaseValuep<<endl;
 
 	double val2=std::exp(LogValuep)*std::cos(abs(PhaseValuep));
-	cerr<<"RATIO: "<<val2/val1<<endl;
 	double val=std::exp(LogValue)*std::cos(abs(PhaseValue))+std::exp(LogValuep)*std::cos(abs(PhaseValuep));
 	LogValue=std::log(abs(val));
 	if (val>0)
 	  PhaseValue=0.0;
 	else 
 	  PhaseValue=M_PI;
-	cerr<<"Post-LOGVALUE PHASEVALUE "<<LogValue<<" "<<PhaseValue<<" "<<LogValuep<<" "<<PhaseValuep<<endl;
 	psiM=psiM_temp;
 	second_replaces_first=Excitations.next();
       }
@@ -443,7 +420,6 @@ namespace qmcplusplus {
 	for (int ii2=ii1+1;ii2<Excitations.orbitals_to_replace.size();ii2++){
 	  for (int jj1=0;jj1<Excitations.unoccupied_orbitals_to_use.size();jj1++){
 	    for (int jj2=jj1+1;jj2<Excitations.unoccupied_orbitals_to_use.size();jj2++){
-	      cerr<<"Second excitations are here!"<<endl;
 	       
 	      int or1=Excitations.orbitals_to_replace[ii1];
 	      int or2=Excitations.orbitals_to_replace[ii2];
@@ -460,17 +436,16 @@ namespace qmcplusplus {
 	      }
 	      double PhaseValuep;
 	      double LogValuep=InvertWithLog(psiM.data(),NumPtcls,NumOrbitals,WorkSpace.data(),Pivot.data(),PhaseValuep);
-	      cerr<<"Pre-LOGVALUE PHASEVALUE "<<LogValue<<" "<<PhaseValue<<" "<<LogValuep<<" "<<PhaseValuep<<endl;
 	      
 	      double val2=std::exp(LogValuep)*std::cos(abs(PhaseValuep));
-	      cerr<<"RATIO: "<<val2/val1<<endl;
+
 	      double val=std::exp(LogValue)*std::cos(abs(PhaseValue))+std::exp(LogValuep)*std::cos(abs(PhaseValuep));
 	      LogValue=std::log(abs(val));
 	      if (val>0)
 		PhaseValue=0.0;
 	      else 
 		PhaseValue=M_PI;
-	      cerr<<"Post-LOGVALUE PHASEVALUE "<<LogValue<<" "<<PhaseValue<<" "<<LogValuep<<" "<<PhaseValuep<<endl;
+
 	      psiM=psiM_temp;
 	      second_replaces_first=Excitations.next();
 	    }
@@ -479,7 +454,7 @@ namespace qmcplusplus {
       }
       //double excitations done!
 
-      cerr<<"LOGVALUE PHASEVALUE "<<LogValue<<" "<<PhaseValue<<endl;
+
       return LogValue;
     }
 
