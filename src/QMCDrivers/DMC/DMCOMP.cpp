af5baa335085da8c3519aef732fd135b3f81cdb9
Jeongnim Kim <jeongnim.kim@intel.com>
2009-04-05 20:04:30 +0000

    Make the fast (partial grad) update the default and use local/non-local merged methods.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3734 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/DMC/DMCOMP.cpp
+++ src/QMCDrivers/DMC/DMCOMP.cpp
@@ -23,35 +23,40 @@
 #include "Message/OpenMP.h"
 #include "Utilities/Timer.h"
 #include "OhmmsApp/RandomNumberControl.h"
+#include "Utilities/ProgressReportEngine.h"
 
 namespace qmcplusplus { 
 
   /// Constructor.
-  DMCOMP::DMCOMP(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h,
-      HamiltonianPool& hpool):
-    QMCDriver(w,psi,h), CloneManager(hpool),
-    KillNodeCrossing(0),
-    Reconfiguration("no"), BenchMarkRun("no"), BranchInterval(-1),mover_MaxAge(-1){
-    RootName = "dummy";
-    QMCType ="DMCOMP";
+  DMCOMP::DMCOMP(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h, HamiltonianPool& hpool)
+    : QMCDriver(w,psi,h), CloneManager(hpool)
+    , KillNodeCrossing(0) ,Reconfiguration("no"), BenchMarkRun("no"), UseFastGrad("yes")
+    , BranchInterval(-1),mover_MaxAge(-1)
+    {
+      RootName = "dmc";
+      QMCType ="DMCOMP";
 
-    QMCDriverMode.set(QMC_UPDATE_MODE,1);
-    QMCDriverMode.set(QMC_MULTIPLE,1);
+      QMCDriverMode.set(QMC_UPDATE_MODE,1);
 
-    m_param.add(KillWalker,"killnode","string");
-    m_param.add(BenchMarkRun,"benchmark","string");
-    m_param.add(Reconfiguration,"reconfiguration","string");
-    m_param.add(BranchInterval,"branchInterval","string");
-    m_param.add(NonLocalMove,"nonlocalmove","string");
-    m_param.add(NonLocalMove,"nonlocalmoves","string");
-    m_param.add(mover_MaxAge,"MaxAge","double");
-    m_param.add(UseFastGrad,"fastgrad", "string");
-  }
+      m_param.add(KillWalker,"killnode","string");
+      m_param.add(BenchMarkRun,"benchmark","string");
+      m_param.add(Reconfiguration,"reconfiguration","string");
+      m_param.add(BranchInterval,"branchInterval","string");
+      m_param.add(NonLocalMove,"nonlocalmove","string");
+      m_param.add(NonLocalMove,"nonlocalmoves","string");
+      m_param.add(mover_MaxAge,"MaxAge","double");
+      m_param.add(UseFastGrad,"fastgrad", "string");
+    }
 
-  void DMCOMP::resetUpdateEngines() {
+  void DMCOMP::resetUpdateEngines() 
+  {
+
+    ReportEngine PRE("DMCOMP","resetUpdateEngines");
 
     bool fixW = (Reconfiguration == "yes");
     
+    Timer init_timer;
+
     makeClones(W,Psi,H);
 
     if(Movers.empty()) 
@@ -68,13 +73,31 @@ namespace qmcplusplus {
       Rng.resize(NumThreads,0);
       estimatorClones.resize(NumThreads,0);
       FairDivideLow(W.getActiveWalkers(),NumThreads,wPerNode);
-      app_log() << "  Initial partition of walkers ";
-      std::copy(wPerNode.begin(),wPerNode.end(),ostream_iterator<int>(app_log()," "));
-      app_log() << endl;
 
-//#pragma omp parallel 
-//      {
-//        int ip = omp_get_thread_num();
+      {//log file
+        ostringstream o;
+        o << "  Initial partition of walkers on a node: ";
+        std::copy(wPerNode.begin(),wPerNode.end(),ostream_iterator<int>(o," "));
+        o << "\n";
+
+        if(QMCDriverMode[QMC_UPDATE_MODE]) 
+          o << "  Updates by particle-by-particle moves";
+        else 
+          o << "  Updates by walker moves";
+
+        if(UseFastGrad == "yes") 
+          o << " using fast gradient version ";
+        else 
+          o << " using full-ratio version ";
+
+        if(KillNodeCrossing)  
+          o << "\n  Walkers are killed when a node crossing is detected";
+        else
+          o << "\n  DMC moves are rejected when a node crossing is detected";
+
+        app_log() << o.str() << endl;
+      }
+
 #pragma omp parallel for
       for(int ip=0; ip<NumThreads; ++ip)
       {
@@ -87,126 +110,62 @@ namespace qmcplusplus {
         branchClones[ip] = new BranchEngineType(*branchEngine);
         if(QMCDriverMode[QMC_UPDATE_MODE])
         {
-          if(NonLocalMove == "yes")
-          {
-	    if (!ip) {
-	      app_log() << "  Non-local update is used." << endl;
-	    }
-	    if (UseFastGrad == "yes") {
-	      if (!ip)
-		app_log() << "  Using fast gradient version." << endl;
-	      DMCNonLocalUpdatePbyPFast * nlocMover= 
-		new DMCNonLocalUpdatePbyPFast
-		(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
-	      nlocMover->put(qmcNode);
-	      Movers[ip] = nlocMover;
-	    }
-	    else {
-	      if (!ip)
-		app_log() << "  Using slow gradient version." << endl;
-	      DMCNonLocalUpdatePbyP *nlocMover = 
-	      new DMCNonLocalUpdatePbyP
-		(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
-	      nlocMover->put(qmcNode);
-	      Movers[ip] = nlocMover;
-	    }	      
-          } 
+          if(UseFastGrad == "yes")
+            Movers[ip] = new DMCUpdatePbyPWithRejectionFast(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
           else
-          {
-	    if (UseFastGrad == "yes") {
-	      if (!ip)
-		app_log() << "  Using fast gradient version." << endl;
-	      DMCUpdatePbyPWithRejectionFast *Mover= 
-		new DMCUpdatePbyPWithRejectionFast
-		(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
-	      // Mover->put(qmcNode);
-	      Movers[ip] = Mover;
-	    }
-	    else {
-	      if (!ip)
-		app_log() << "  Using slow gradient version." << endl;
-	      DMCUpdatePbyPWithRejection *Mover = 
-		new DMCUpdatePbyPWithRejection
-		(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
-	      // Mover->put(qmcNode);
-	      Movers[ip] = Mover;
-	    }	      
-          }
+            Movers[ip] = new DMCUpdatePbyPWithRejection(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]); 
+          Movers[ip]->put(qmcNode);
           Movers[ip]->resetRun(branchClones[ip],estimatorClones[ip]);
           Movers[ip]->initWalkersForPbyP(W.begin()+wPerNode[ip],W.begin()+wPerNode[ip+1]);
-        } 
+        }
         else
         {
-          if(NonLocalMove == "yes") {
-            app_log() << "  Non-local update is used." << endl;
-            DMCNonLocalUpdate* nlocMover= new DMCNonLocalUpdate(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]);
-            nlocMover->put(qmcNode);
-            Movers[ip]=nlocMover;
-          } else {
-            if(KillNodeCrossing) {
+          if(NonLocalMove == "yes") 
+            Movers[ip]= new DMCNonLocalUpdate(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]);
+          else 
+          {
+            if(KillNodeCrossing) 
               Movers[ip] = new DMCUpdateAllWithKill(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]);
-            } else {
+            else 
               Movers[ip] = new DMCUpdateAllWithRejection(*wClones[ip],*psiClones[ip],*hClones[ip],*Rng[ip]);
-            }
           }
+          Movers[ip]->put(qmcNode);
           Movers[ip]->resetRun(branchClones[ip],estimatorClones[ip]);
           Movers[ip]->initWalkers(W.begin()+wPerNode[ip],W.begin()+wPerNode[ip+1]);
         }
-      } 
-    }
+      }
+    } 
 
     branchEngine->checkParameters(W);
-//#pragma omp parallel  for
-//    for(int ip=0; ip<NumThreads; ++ip)
-//    {
-////    {
-////      int ip = omp_get_thread_num();
-//      MCWalkerConfiguration::iterator 
-//        wit(W.begin()+wPerNode[ip]), wit_end(W.begin()+wPerNode[ip+1]);
-//      if(QMCDriverMode[QMC_UPDATE_MODE])
-//        Movers[ip]->initWalkersForPbyP(wit,wit_end);
-//      else
-//        Movers[ip]->initWalkers(wit,wit_end);
-//    }
 
+    int mxage=mover_MaxAge;
     if(fixW) 
     {
-      if(QMCDriverMode[QMC_UPDATE_MODE])
-        app_log() << "  DMCOMP PbyP Update with reconfigurations" << endl;
-      else
-        app_log() << "  DMCOMP walker Update with reconfigurations" << endl;
-      for(int ip=0; ip<Movers.size(); ip++) {
-        if(mover_MaxAge!=-1) Movers[ip]->MaxAge=mover_MaxAge;
-	else Movers[ip]->MaxAge=0;
-      }
-      if(BranchInterval<0)
-      {
-        BranchInterval=nSteps;
-      }
+      if(BranchInterval<0) BranchInterval=nSteps;
+      mxage=(mover_MaxAge<0)?0:mover_MaxAge;
+      for(int ip=0; ip<Movers.size(); ++ip) Movers[ip]->MaxAge=mxage;
     } 
     else 
     {
-      if(QMCDriverMode[QMC_UPDATE_MODE])
-      {
-        app_log() << "  DMCOMP PbyP Update with a fluctuating population" << endl;
-        for(int ip=0; ip<Movers.size(); ip++) {
-          if(mover_MaxAge!=-1) Movers[ip]->MaxAge=mover_MaxAge;
-	  else Movers[ip]->MaxAge=1;
-        }
-      }
-      else
-      {
-        app_log() << "  DMCOMP walker Update with a fluctuating population" << endl;
-        for(int ip=0; ip<Movers.size(); ip++){
-          if(mover_MaxAge!=-1) Movers[ip]->MaxAge=mover_MaxAge;
-	  else Movers[ip]->MaxAge=5;
-        }
-      }
       if(BranchInterval<0) BranchInterval=1;
+      int miage=(QMCDriverMode[QMC_UPDATE_MODE])?1:5;
+      mxage=(mover_MaxAge<0)?miage:mover_MaxAge;
+      for(int ip=0; ip<Movers.size(); ++ip) Movers[ip]->MaxAge=mxage;
     }
-    app_log() << "  BranchInterval = " << BranchInterval << endl;
-    app_log() << "  Steps per block = " << nSteps << endl;
-    app_log() << "  Number of blocks = " << nBlocks << endl;
+
+    {
+      ostringstream o;
+      if(fixW)
+        o << "  Fixed population using reconfiguration method\n";
+      else
+        o << "  Fluctuating population\n";
+      o << "  Persisent walkers are killed after " << mxage << " MC sweeps\n";
+      o << "  BranchInterval = " << BranchInterval << "\n";
+      o << "  Steps per block = " << nSteps << "\n";
+      o << "  Number of blocks = " << nBlocks << "\n";
+      app_log() << o.str() << endl;
+    }
+    app_log() << "  DMC Engine Initialization = " << init_timer.elapsed() << " secs " << endl;
   }
 
   bool DMCOMP::run() {
@@ -242,7 +201,6 @@ namespace qmcplusplus {
 
           wClones[ip]->resetCollectables();
           Movers[ip]->advanceWalkers(wit,wit_end,false);
-
           Movers[ip]->setMultiplicity(wit,wit_end);
 
           if(QMCDriverMode[QMC_UPDATE_MODE] && now%updatePeriod == 0) Movers[ip]->updateWalkers(wit, wit_end);
@@ -299,78 +257,11 @@ namespace qmcplusplus {
   }
   
   bool 
-  DMCOMP::put(xmlNodePtr q){ 
+  DMCOMP::put(xmlNodePtr q)
+  { 
     //nothing to do
     return true;
   }
-
-//  void DMCOMP::dmcWithBranching() {
-//
-//    RealType Eest = branchEngine->E_T;
-//
-//    resetRun();
-//
-//
-//    IndexType block = 0;
-//    do {//start a block
-//      for(int ip=0; ip<NumThreads; ip++) {
-//        Movers[ip]->startBlock();
-//      } 
-//
-//      FairDivideLow(W.getActiveWalkers(),NumThreads,wPerNode);
-//      Estimators->startBlock();
-//      IndexType step = 0;
-//      IndexType pop_acc=0; 
-//      do {
-//#pragma omp parallel  
-//        {
-//          int ip = omp_get_thread_num();
-//          Movers[ip]->resetEtrial(Eest);
-//          Movers[ip]->advanceWalkers(W.begin()+wPerNode[ip], W.begin()+wPerNode[ip+1]);
-//        }
-//
-//        ++step; ++CurrentStep;
-//        Estimators->accumulate(W);
-//
-//        //int cur_pop = branchEngine->branch(CurrentStep,W, branchClones);
-//        //pop_acc += cur_pop;
-//        //Eest = branchEngine->CollectAndUpdate(cur_pop, Eest); 
-//
-//        FairDivideLow(W.getActiveWalkers(),NumThreads,wPerNode);
-//
-//        for(int ip=0; ip<NumThreads; ip++) Movers[ip]->resetEtrial(Eest); 
-//
-//        if(CurrentStep%100 == 0) {
-//#pragma omp parallel  
-//          {
-//            int ip = omp_get_thread_num();
-//            Movers[ip]->updateWalkers(W.begin()+wPerNode[ip], W.begin()+wPerNode[ip+1]);
-//          }
-//        }
-//      } while(step<nSteps);
-//      
-//      Estimators->stopBlock(acceptRatio());
-//      
-//      //update estimator
-//      //Estimators->setColumn(PopIndex,static_cast<RealType>(pop_acc)/static_cast<RealType>(nSteps));
-//      //Estimators->setColumn(EtrialIndex,Eest); 
-//      //Eest = Estimators->average(0);
-//      //RealType totmoves=1.0/static_cast<RealType>(step*W.getActiveWalkers());
-//
-//      //Need MPI-IO
-//      //app_log() 
-//      //  << setw(4) << block 
-//      //  << setw(20) << static_cast<RealType>(nAllRejected)*totmoves
-//      //  << setw(20) << static_cast<RealType>(nNodeCrossing)*totmoves << endl;
-//      block++;
-//
-//      recordBlock(block);
-//
-//    } while(block<nBlocks);
-//  }
-//
-
-
 }
 
 /***************************************************************************
