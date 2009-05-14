a0cd0ce7fce4fdcd066d84666e5b5fa217babc56
Jeongnim Kim <jeongnim.kim@intel.com>
2009-05-14 13:52:36 +0000

    Reformat the files.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3839 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/ForwardWalking/FWSingle.h
+++ src/QMCDrivers/ForwardWalking/FWSingle.h
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////
-// (c) Copyright 2003- by Jeongnim Kim
+// (c) Copyright 2009- by Jeremy McMinis and Jeongnim Kim
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //   Jeongnim Kim
@@ -8,13 +8,10 @@
 //   University of Illinois, Urbana-Champaign
 //   Urbana, IL 61801
 //   e-mail: jnkim@ncsa.uiuc.edu
-//   Tel:    217-244-6319 (NCSA) 217-333-3324 (MCC)
 //
 // Supported by 
 //   National Center for Supercomputing Applications, UIUC
 //   Materials Computation Center, UIUC
-//   Department of Physics, Ohio State University
-//   Ohio Supercomputer Center
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
 #ifndef QMCPLUSPLUS_FW_ANALYSIS_H
@@ -27,50 +24,52 @@ namespace qmcplusplus {
   /** @ingroup QMCDrivers  PbyP
    *@brief Implements the VMC algorithm using particle-by-particle move. 
    */
-  class FWSingle: public QMCDriver {
-  public:
-    /// Constructor.
-    FWSingle(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h);
-    bool run();
-    bool put(xmlNodePtr cur);
- 
-  private:
-    FWSingle(const FWSingle& a): QMCDriver(a) { }
-    /// Copy operator (disabled).
-    FWSingle& operator=(const FWSingle&) { return *this;}
-    
-//main drivers
-    void FWOneStep();
-    void transferParentsOneGeneration();
+  class FWSingle: public QMCDriver 
+  {
+    public:
+      /// Constructor.
+      FWSingle(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h);
+      bool run();
+      bool put(xmlNodePtr cur);
 
-//helper functions
-    void fillIDMatrix();
-    void fillWalkerPositionsandWeights(int nstep);
-    void readInLong(int step, string IDstring, vector<long>& data_out);
-    void readInFloat(int step, vector<float>& data_out);
-    void resetWeights();
-    int getNumberOfSamples(int omittedSteps);
-//debugging functions
-    void printIDs(vector<long> vi);
-    void printInts(vector<int> vi);
-    
-    string xmlrootName;
-    stringstream fname;
-    int weightFreq, weightLength, numSteps;
-    
-    vector<int> walkersPerBlock;
-    vector<int> pointsToCalculate;
-    vector<vector<int> > Weights;
-    vector<vector<long> > IDs, PIDs, realPIDs, realIDs;
-    hid_t c_file;
-    string WIDstring,PIDstring;
-    int verbose,startStep;
-    int gensTransferred;
+    private:
+      FWSingle(const FWSingle& a): QMCDriver(a) { }
+      /// Copy operator (disabled).
+      FWSingle& operator=(const FWSingle&) { return *this;}
+
+      //main drivers
+      void FWOneStep();
+      void transferParentsOneGeneration();
+
+      //helper functions
+      void fillIDMatrix();
+      void fillWalkerPositionsandWeights(int nstep);
+      void readInLong(int step, string IDstring, vector<long>& data_out);
+      void readInFloat(int step, vector<float>& data_out);
+      void resetWeights();
+      int getNumberOfSamples(int omittedSteps);
+      //debugging functions
+      void printIDs(vector<long> vi);
+      void printInts(vector<int> vi);
+
+      string xmlrootName;
+      stringstream fname;
+      int weightFreq, weightLength, numSteps;
+
+      vector<int> walkersPerBlock;
+      vector<int> pointsToCalculate;
+      vector<vector<int> > Weights;
+      vector<vector<long> > IDs, PIDs, realPIDs, realIDs;
+      hid_t c_file;
+      string WIDstring,PIDstring;
+      int verbose,startStep;
+      int gensTransferred;
   };
 }
 
 #endif
-/***************************************************************************
+/
+***************************************************************************
  * $RCSfile: VMCSingle.h,v $   $Author: jnkim $
  * $Revision: 1.5 $   $Date: 2006/07/17 14:29:40 $
  * $Id: VMCSingle.h,v 1.5 2006/07/17 14:29:40 jnkim Exp $ 
