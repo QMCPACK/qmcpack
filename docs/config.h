ddf6420c71a3d76d63481e09d49b9b4a2788b975
Jeongnim Kim <jeongnim.kim@intel.com>
2009-06-11 02:50:26 +0000

    Update.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@3915 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- docs/config.h
+++ docs/config.h
@@ -13,15 +13,29 @@
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
-/**\mainpage QMCPACK Code Documentation
- * \section intro_sec Introduction
+/**\mainpage QMCPACK Documentation
+ * \section intro_sec Summary
  *
- * Framework for Quantum Monte Carlo simulations.
  *
- * \section Major changes
+ * QMCPACK, framework for Quantum Monte Carlo simulations, implements advanced
+ * QMC algorithms.  Generic programming enabled by templates in C++ is
+ * extensively utilized to achieve high efficiency. It is designed for high
+ * performance systems using MPI and OpenMP.
  *
+ * The code development is led by J. Kim and the main
+ * contributors are the members of the electron structure group of Profs.
+ * Martin and Ceperley at University of Illinois at Urbana-Champaign. 
+ * - Active developers: J. Kim, K. Esler, J. McMinis, B. Clark, J. Gergely, C. Yang
+ * - Past contributors: S. Chiesa, K. Delaney, J. Vincent 
+ *
+ * The development of QMCPACK is supported by Materials Computation Center and
+ * National Center for Supercomputing Applications at University of Illinois at
+ * Urbana-Champaign, and funded by the National Science Foundation and
+ * Department of Energy.
+ *
+ * \section change_secs Major changes
  * \htmlonly
- * <h2>2009-05-xx</h2>
+ * <h2>2009-06-xx</h2>
  * <h2>2008-07-22</h2>
   <ul> 
   <li>Numerous bug fixes.
@@ -29,33 +43,68 @@
   <li>EinsplineOrbitalSet uses Einspline library 
   <li>BsplinFunctor for One- and Two-Body Jastrow
   </ul>
-  <ul> 2007-09-07
-    <li> A new update
-  </ul>    
-  <ul> 2005-07-25
-   <li> updated doxygen documentations
-   <ul>
-     <li> docs directory is added to cvs repository. 
-     <ul> To generate doxygen documentation on a local host,
-     <li> cd docs; doxygen Doxyfile
-     <li> doxygen/html/index.html is the main page
-     </ul>
-     <li> Introduced groups that follow the directory structure
-     <ul>
-       <li>Orbital group
-       <li>Orbital builder group
-       <li>Many-body wave function group
-       <li>Hamiltonian group
-       <li>QMC Driver group
-       <li>QMC Drivers using walker-by-walker update
-       <li>QMC Drivers using particle-by-particle update
-       <li>QMC Drivers for energy differences
-       <li>QMC Application group
-     </ul>
-    </ul>
+  <h2> 2005-07-25</h2>
+  <ul> 
+    <li> updated doxygen documentations
+    <li> docs directory is added to cvs repository. 
+    <li> To generate doxygen documentation on a local host,
+      <ul>
+      <li> cd docs; doxygen Doxyfile
+      <li> doxygen/html/index.html is the main page
+      </ul>
+  <li> Introduced groups that follow the directory structure
+  <ul>
+   <li>Orbital group
+   <li>Orbital builder group
+   <li>Many-body wave function group
+   <li>Hamiltonian group
+   <li>QMC Driver group
+   <li>QMC Drivers using walker-by-walker update
+   <li>QMC Drivers using particle-by-particle update
+   <li>QMC Drivers for energy differences
+   <li>QMC Application group
+  </ul>
   </ul>
  \endhtmlonly
- * \section license
+
+ * \page license University of Illinois/NCSA Open Source License
+
+Copyright (c) 2003, University of Illinois Board of Trustees.
+All rights reserved.
+
+Developed by:   
+  Jeongnim Kim
+  Condensed Matter Physics,
+  National Center for Supercomputing Applications, University of Illinois
+  Materials computation Center, University of Illinois
+  http://www.mcc.uiuc.edu/qmc/
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+``Software''), to deal with the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+        * Redistributions of source code must retain the above copyright 
+          notice, this list of conditions and the following disclaimers.
+        * Redistributions in binary form must reproduce the above copyright 
+          notice, this list of conditions and the following disclaimers in 
+          the documentation and/or other materials provided with the 
+          distribution.
+        * Neither the names of the NCSA, the MCC, the University of Illinois, 
+          nor the names of its contributors may be used to endorse or promote 
+          products derived from this Software without specific prior written 
+          permission.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+OTHER DEALINGS WITH THE SOFTWARE.
  *
  */
 #ifndef OHMMS_QMC_CONFIGURATION_H
