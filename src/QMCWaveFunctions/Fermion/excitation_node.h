3539e27e31c0eafb152369e8b391be574be69672
Jeongnim Kim <jeongnim.kim@intel.com>
2009-10-05 18:45:08 +0000

    Add ratio with partial updates. _debug uses inverse matrix for each node.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4247 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/Fermion/excitation_node.h
+++ src/QMCWaveFunctions/Fermion/excitation_node.h
@@ -36,6 +36,8 @@ namespace qmcplusplus
     int to;
     ///ratio with respect to the parent
     T ratio;
+    ///product
+    T utv;
     unsigned long ground;
     unsigned long excited;
     matrix_type inverse;
@@ -45,7 +47,7 @@ namespace qmcplusplus
      * Invalidate the excitation
      */
     inline explicit excitation_node(unsigned long vid=0u, unsigned long cid=0u)
-      : my_id(0), parent_id(0),from(0),to(0),ratio(1.0),ground(vid),excited(cid) {}
+      : my_id(0), parent_id(0),from(0),to(0),ratio(1.0),utv(0.0), ground(vid),excited(cid) {}
 
     inline void resize(int m)
     {
@@ -63,37 +65,79 @@ namespace qmcplusplus
       return children.empty();
     }
 
-    inline void get_ratios_root(const matrix_type& psi_big
-        , std::vector<excitation_node>& ci
-        , std::vector<T>& ratios
-        , int m)
+    inline void get_ratios_root(const matrix_type& psi_big, std::vector<excitation_node>& ci
+        , std::vector<T>& ratios, int vmin)
     {
       ratios[0]=1.0;
+      const int m=inverse.rows();
       for(int i=0; i<children.size(); ++i)
-        ci[ children[i] ].get_ratios(psi_big,ci,ratios,m,1.0);
+        ci[ children[i] ].get_ratios_partial(inverse,psi_big,ci,ratios,m,vmin,1.0,1.0);
     }
 
-    inline void get_ratios(const matrix_type& psi_big
+    inline void get_ratios_partial(const matrix_type& inv0
+        , const matrix_type& psi_big
         , std::vector<excitation_node>& ci
         , std::vector<T>& ratios
         , int m
-        , T ratio_base)
+        , int vmin
+        , T ratio_base
+        , T inv_utv
+        )
+    {
+      const int n=m-vmin;
+      T temp[n];
+
+      BLAS::copy((m-vmin)*m,inv0[vmin],inverse[vmin]);
+      //update inverse matrix partially
+      //do all the blocks to be updated: this can be further optimized by bit masks
+      if(parent_id>0)
+      {
+        int pv=m-1-ci[parent_id].from;
+        int pc=m+ci[parent_id].to;
+        for(int vv=vmin,iv=0; vv<m; ++vv,++iv)
+        {
+          T  gamma=BLAS::dot(m,inv0[vv],psi_big[pc])*inv_utv;
+          for(int j=0; j<m; ++j) inverse(vv,j) -= gamma*inv0(pv,j);
+        }
+      }
+
+      int v=m-1-from;
+      int c=m+to;
+      utv=BLAS::dot(m,inverse[v],psi_big[c]);
+      ratios[my_id]=ratio_base*utv;
+
+      for(int i=0; i<children.size(); ++i)
+        ci[children[i]].get_ratios_partial(inverse,psi_big,ci,ratios,m,vmin,ratios[my_id],1.0/utv);
+    }
+
+    inline void get_ratios_root_debug(const matrix_type& psi_big
+        , std::vector<excitation_node>& ci
+        , std::vector<T>& ratios
+        )
+    {
+      const int m=inverse.rows();
+      ratios[0]=1.0;
+      for(int i=0; i<children.size(); ++i)
+        ci[ children[i] ].get_ratios_debug(psi_big,ci,ratios,m,1.0);
+    }
+
+    void get_ratios_debug(const matrix_type& psi_big , std::vector<excitation_node>& ci , std::vector<T>& ratios
+        , int m, T ratio_base)
     {
       int v=m-1-from;
       int c=m+to;
-      cout << "my_id =" << my_id << " " << " ratio by promotion from="<< v << " to=" << c << endl;
       ratio=BLAS::dot(m,ci[parent_id].inverse[v],psi_big[c]);
       ratios[my_id]=ratio_base*ratio;
-      //T r=ratios[my_id]=BLAS::dot(m,ci[parent_id].inverse.data()+v,m,psi_big[c],1);
-      //if(children.size()) 
+
+      //if(children.size())
       //{
         inverse=ci[parent_id].inverse;
         det_row_update(inverse.data(),psi_big[c],m,v,ratio);
-      //}
 
         ratio_base *= ratio;
-      for(int i=0; i<children.size(); ++i)
-        ci[ children[i] ].get_ratios(psi_big,ci,ratios,m,ratio_base);
+        for(int i=0; i<children.size(); ++i)
+          ci[ children[i] ].get_ratios_debug(psi_big,ci,ratios,m,ratio_base);
+      //}
     }
 
     template<unsigned CMAX>
