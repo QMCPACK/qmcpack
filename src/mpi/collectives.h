c3edaa95f47344a709a3276de9403753a5ec3b72
Jeongnim Kim <jeongnim.kim@intel.com>
2010-02-08 16:10:46 +0000

    Add mpi directory to use boost::mpi interfaces but boost::mpi is NOT required.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4573 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/mpi/collectives.h
@@ -0,0 +1,155 @@
+#ifndef QMCPLUSPLUS_BOOSTADAPTOR_COLLECTIVES_OPERATIONS_H
+#define QMCPLUSPLUS_BOOSTADAPTOR_COLLECTIVES_OPERATIONS_H
+
+#include <type_traits/container_proxy.h>
+#include <mpi/mpi_datatype.h>
+
+namespace APPNAMESPACE {
+
+  namespace mpi {
+
+#if defined(HAVE_MPI)
+    //free function for allreduce
+    template<typename T, typename OP>
+      inline void all_dreduce(const Communicate& comm,T& in, T& out)
+      {
+        container_proxy<T> t_in(in),t_out(out);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        MPI_Allreduce(t_in.data(),t_out.data(),t_in.size(),type_id, OP, comm());
+      }
+
+    /** generic function to perform allreduce */
+    template<typename T, typename OP>
+      inline void all_dreduce(const communicator& comm, T& in)
+      {
+        T out(in);
+        allreduace<T,OP>(comm,in,out);
+        in=out;
+      }
+
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename T, typename CT>
+      inline void all_gather(const communicator& comm,T& in, CT& out)
+      {
+        container_proxy<T> t_in(in);
+        container_proxy<CT> t_out(out);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Allgather(t_in.data(),t_in.size(),type_id,t_out.data(),t_in.size(),type_id,comm());
+      }
+
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename CT, typename IV>
+      inline void all_gatherv(const communicator& comm, CT& in, CT& out, IV& counts, IV& displ)
+      {
+        container_proxy<CT> t_in(in),t_out(out);
+        container_proxy<IV> t_counts(counts), t_displ(displ);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Allgatherv(t_in.data(),t_in.size(),type_id,t_out.data(),t_counts.data(),t_displ.data(),type_id,comm());
+      }
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename CT, typename IV>
+      inline void gatherv(const communicator& comm, CT& in, CT& out, IV& counts, IV& displ, int dest=0)
+      {
+        container_proxy<CT> t_in(in),t_out(out);
+        container_proxy<IV> t_counts(counts), t_displ(displ);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Gatherv(t_in.data(),t_in.size(),type_id,t_out.data(),t_counts.data(),t_displ.data(),type_id,dest,comm());
+      }
+
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename CT>
+      inline void gather(const communicator& comm, CT& in, CT& out, int dest=0)
+      {
+        container_proxy<CT> t_in(in),t_out(out);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Gather(t_in.data(),t_in.size(),type_id
+            ,t_out.data(),t_in.size(),type_id
+            ,dest,comm());
+      }
+
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename CT, typename IV>
+      inline void scatterv(const communicator& comm, CT& in, CT& out, IV& counts, IV& displ, int dest=0)
+      {
+        container_proxy<CT> t_in(in),t_out(out);
+        container_proxy<IV> t_counts(counts), t_displ(displ);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Scatterv(t_in.data(),t_counts.data(),t_displ.data(),type_id
+            ,t_out.data(),t_out.size(),type_id,dest,comm());
+      }
+
+    /** generic function to perform allgather
+     *
+     * allgather of a scalar to a vectorized container
+     */
+    template<typename CT>
+      inline void scatter(const communicator& comm, CT& in, CT& out, int dest=0)
+      {
+        container_proxy<CT> t_in(in),t_out(out);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        int ierr=MPI_Scatter(t_in.data(),t_out.size(),type_id
+            ,t_out.data(),t_out.size(),type_id,dest,comm());
+      }
+
+    /** generic function to perform bcast
+     *
+     */
+    template<typename CT>
+      inline void bcast(const communicator& comm, CT& inout, int source=0)
+      {
+        if(comm.size()==1) return;
+        container_proxy<CT> t_in(inout);
+        MPI_Datatype type_id=get_mpi_datatype(*t_in.data());
+        MPI_Bcast(t_in.data(),t_in.size(),type_id,source,comm());
+      }
+#else
+    template<typename T, typename OP>
+      inline void all_dreduce(const communicator& comm,T& in, T& out)
+      { out=in; }
+    template<typename T, typename OP>
+      inline void all_dreduce(const communicator& comm,T& in) { }
+    template<typename T, typename CT>
+      inline void all_gather(const communicator& comm,T& in, CT& out)
+      {out=in;}
+    template<typename CT, typename IT>
+      inline void all_gatherv(const communicator& comm, CT& in, CT& out, IT& counts, IT& displ)
+      {out=in;}
+    template<typename CT, typename IT>
+      inline void gatherv(const communicator& comm, CT& in, CT& out, IT& counts, IT& displ, int dest=0)
+      { out=in; }
+    template<typename CT>
+      inline void gather(const communicator& comm, CT& in, CT& out, int dest=0)
+      { out=in; }
+    template<typename CT, typename IV>
+      inline void scatterv(const communicator& comm, CT& in, CT& out, IV& counts, IV& displ, int dest=0)
+      { out=in;}
+    template<typename CT>
+      inline void scatter(const communicator& comm, CT& in, CT& out, int dest=0)
+      { out=in;}
+    template<typename CT>
+      inline void bcast(const communicator& comm, CT& inout, int source=0)
+      {}
+#endif
+  }
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 2458 $   $Date: 2008-02-20 10:45:51 -0500 (Wed, 20 Feb 2008) $
+ * $Id: CommOperators.h 2458 2008-02-20 15:45:51Z jnkim $ 
+ ***************************************************************************/
---
+++ src/mpi/mpi_datatype.h
@@ -0,0 +1,78 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 1998-2002 by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   Jeongnim Kim
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_MPI_DATATYPEDEFINE_H
+#define QMCPLUSPLUS_MPI_DATATYPEDEFINE_H
+
+#include <type_traits/scalar_traits.h>
+#if defined(HAVE_MPI)
+#include <mpi.h>
+#endif
+
+namespace APPNAMESPACE {
+  namespace mpi {
+
+    typedef Communicate communicator;
+
+#if defined(HAVE_MPI)
+template <typename T>                              
+inline MPI_Datatype
+get_mpi_datatype(const T&) { return MPI_BYTE;}
+
+#define BOOSTSUB_MPI_DATATYPE(CppType, MPITYPE)                \
+template<>                                                     \
+inline MPI_Datatype                                             \
+get_mpi_datatype< CppType >(const CppType&) { return MPITYPE; }         
+
+BOOSTSUB_MPI_DATATYPE(short, MPI_SHORT);
+
+BOOSTSUB_MPI_DATATYPE(int, MPI_INT);
+
+BOOSTSUB_MPI_DATATYPE(long, MPI_LONG);
+
+BOOSTSUB_MPI_DATATYPE(float, MPI_FLOAT);
+
+BOOSTSUB_MPI_DATATYPE(double, MPI_DOUBLE);
+
+BOOSTSUB_MPI_DATATYPE(long double, MPI_LONG_DOUBLE);
+
+BOOSTSUB_MPI_DATATYPE(unsigned char, MPI_UNSIGNED_CHAR);
+
+BOOSTSUB_MPI_DATATYPE(unsigned short, MPI_UNSIGNED_SHORT);
+
+BOOSTSUB_MPI_DATATYPE(unsigned int, MPI_UNSIGNED);
+
+BOOSTSUB_MPI_DATATYPE(unsigned long, MPI_UNSIGNED_LONG);
+
+BOOSTSUB_MPI_DATATYPE(std::complex<double>, MPI_DOUBLE);
+
+BOOSTSUB_MPI_DATATYPE(std::complex<float>, MPI_FLOAT);
+
+#else
+typedef int MPI_Datatype;
+  //return a non-sense integer
+template <typename T> 
+  inline MPI_Datatype get_mpi_datatype(const T&) { return 0;}
+#endif
+  }
+}//end of mpi
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 894 $   $Date: 2006-02-03 10:52:38 -0600 (Fri, 03 Feb 2006) $
+ * $Id: hdf_datatype.h 894 2006-02-03 16:52:38Z jnkim $ 
+ ***************************************************************************/
+
