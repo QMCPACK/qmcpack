23711e1804d3f3ad591763a11d98d8c1f4ba9e75
Jeongnim Kim <jeongnim.kim@intel.com>
2010-02-08 15:50:06 +0000

    Add traits to handler and container for mpi and hdf.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4571 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/type_traits/container_proxy.h
@@ -0,0 +1,134 @@
+#ifndef QMCPLUSPLUS_MPI_CONTAINER_PROXY_H
+#define QMCPLUSPLUS_MPI_CONTAINER_PROXY_H
+
+#include <OhmmsPETE/TinyVector.h>
+#include <OhmmsPETE/Tensor.h>
+#include <OhmmsPETE/OhmmsVector.h>
+#include <OhmmsPETE/OhmmsArray.h>
+#include <Utilities/PooledData.h>
+
+namespace APPNAMESPACE {
+
+  template<typename T> 
+    struct container_proxy
+    {
+      enum {DIM=scalar_traits<T>::DIM};
+      typedef typename scalar_traits<T>::real_type* pointer;
+      T& ref;
+      inline container_proxy(T& a):ref(a){}
+      inline size_t size() const {return DIM;}
+      inline pointer data() { return &ref;}
+    };
+
+  template<typename T, unsigned D>
+    struct container_proxy<TinyVector<T,D> >
+    {
+      enum {DIM=scalar_traits<T>::DIM*D};
+      typedef typename scalar_traits<T>::real_type* pointer;
+      TinyVector<T,D>& ref;
+      inline container_proxy(TinyVector<T,D>& a):ref(a) { }
+      inline size_t size() const {return DIM;}
+      inline pointer data() { return ref.data();}
+    };
+
+  template<typename T, unsigned D>
+    struct container_proxy<Tensor<T,D> >
+    {
+      enum {DIM=scalar_traits<T>::DIM*D*D};
+      typedef typename scalar_traits<T>::real_type* pointer;
+      Tensor<T,D>& ref;
+      inline container_proxy(Tensor<T,D>& a):ref(a){}
+      inline size_t size() const {return DIM;}
+      inline pointer data() { return ref.data();}
+    };
+
+  template<typename T>
+    struct container_proxy<std::vector<T> >
+    {
+      typedef typename container_proxy<T>::pointer pointer;
+      std::vector<T>& ref;
+      inline container_proxy(std::vector<T>& a):ref(a){}
+      inline size_t size() const {return ref.size()*container_proxy<T>::DIM;}
+      inline pointer data() { return ref.data();}
+    };
+
+  template<>
+    struct container_proxy<std::vector<bool> >
+    {
+      typedef int* pointer;
+      std::vector<bool>& ref;
+      std::vector<int> my_copy;
+      inline container_proxy(std::vector<bool>& a):ref(a)
+      {
+        my_copy.resize(a.size());
+        std::copy(a.begin(),a.end(),my_copy.begin());
+      }
+      ~container_proxy()
+      {
+        std::copy(my_copy.begin(),my_copy.end(),ref.begin());
+      }
+      inline size_t size() const {return my_copy.size();}
+      inline pointer data() { return my_copy.data();}
+    };
+
+  template<typename T, unsigned D>
+    struct container_proxy<std::vector<TinyVector<T,D> > >
+    {
+      enum {DIM=D*scalar_traits<T>::DIM};
+      typedef typename container_proxy<T>::pointer pointer;
+      typedef std::vector<TinyVector<T,D> > data_type;
+      data_type& ref;
+      inline container_proxy(data_type& a):ref(a) { }
+      inline size_t size() const {return ref.size()*DIM;}
+      inline pointer data() { return ref[0].data();}
+    };
+
+
+  template<typename T>
+    struct container_proxy<PooledData<T> >
+    {
+      typedef typename container_proxy<T>::pointer pointer;
+      PooledData<T>& ref;
+      inline container_proxy(PooledData<T>& a):ref(a){}
+      inline size_t size() const {return ref.size()*container_proxy<T>::DIM;}
+      inline pointer data() { return ref.data();}
+    };
+
+  template<typename T>
+    struct container_proxy<Vector<T> >
+    {
+      typedef typename container_proxy<T>::pointer pointer;
+      Vector<T>& ref;
+      inline container_proxy(Vector<T>& a):ref(a){}
+      inline size_t size() const {return ref.size()*container_proxy<T>::DIM;}
+      inline pointer data() { return ref.data();}
+    };
+
+  template<typename T, unsigned D>
+    struct container_proxy<Vector<TinyVector<T,D> > >
+    {
+      enum {DIM=D*scalar_traits<T>::DIM};
+      typedef typename container_proxy<T>::pointer pointer;
+      typedef Vector<TinyVector<T,D> > data_type;
+      data_type& ref;
+      inline container_proxy(data_type& a):ref(a) { }
+      inline size_t size() const {return ref.size()*DIM;}
+      inline pointer data() { return ref[0].data();}
+    };
+
+  template<typename T, unsigned D>
+    struct container_proxy<Array<T,D> >
+    {
+      typedef typename container_proxy<T>::pointer pointer;
+      Array<T,D>& ref;
+      inline container_proxy(Array<T,D>& a):ref(a){}
+      inline size_t size() const {return ref.size()*container_proxy<T>::DIM;}
+      inline pointer data() { return ref.data();}
+    };
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 2458 $   $Date: 2008-02-20 10:45:51 -0500 (Wed, 20 Feb 2008) $
+ * $Id: container_proxy.h 2458 2008-02-20 15:45:51Z jnkim $ 
+ ***************************************************************************/
---
+++ src/type_traits/scalar_traits.h
@@ -0,0 +1,110 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2003-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: jnkim@ncsa.uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+#ifndef QMCPLUSPLUS_SCLAR_TRAITS_H
+#define QMCPLUSPLUS_SCLAR_TRAITS_H
+#include <complex>
+#include <OhmmsPETE/OhmmsMatrix.h>
+#include <OhmmsPETE/OhmmsVector.h>
+#include <OhmmsPETE/TinyVector.h>
+namespace qmcplusplus {
+
+  template<class T> struct scalar_traits 
+  {
+    enum {DIM=1};
+    typedef T real_type;
+    typedef T value_type;
+  };
+
+  template<typename T> struct scalar_traits<std::complex<T> > 
+  {
+    enum {DIM=2};
+    typedef double          real_type;
+    typedef std::complex<T> value_type;
+  };
+
+  /** generic conversion from type T1 to type T2 using implicit conversion
+  */
+  template<typename T1, typename T2>
+    inline void convert(const T1& in, T2& out)
+    {
+      out=static_cast<T2>(in);
+    }
+
+  /** specialization of conversion from complex to real
+  */
+  template<typename T1>
+    inline void convert(const std::complex<T1>& in, double& out)
+    {
+      out=in.real();
+    }
+
+  template<typename T1>
+    inline void convert(const std::complex<T1>& in, float& out)
+    {
+      out=in.real();
+    }
+
+  /* specialization of D-dim vectors
+   *
+   */
+  template<typename T1, typename T2, unsigned D>
+    inline void convert(const TinyVector<T1,D>& in, TinyVector<T2,D>& out)
+    {
+      for(int i=0; i<D;++i) convert(in[i],out[i]);
+    }
+
+  /** specialization for 3D */
+  template<typename T1, typename T2>
+    inline void convert(const TinyVector<T1,3>& in, TinyVector<T2,3>& out)
+    {
+      convert(in[0],out[0]);
+      convert(in[1],out[1]);
+      convert(in[2],out[2]);
+    }
+
+  /** generic function to convert arrays
+   * @param in starting address of type T1
+   * @param out starting address of type T2
+   * @param n size of in/out
+   */
+  template<typename T1, typename T2>
+    inline void convert(const T1* restrict in, T2* restrict out, std::size_t n)
+    {
+      for(int i=0; i<n;++i) convert(in[i],out[i]);
+    }
+
+  /** specialization for a vector */
+  template<typename T1, typename T2>
+    inline void convert(const Vector<T1>& in, Vector<T2>& out)
+    {
+      convert(in.data(),out.data(),in.size());
+    }
+
+  /** specialization for a vector */
+  template<typename T1, typename T2>
+    inline void convert(const Matrix<T1>& in, Matrix<T2>& out)
+    {
+      convert(in.data(),out.data(),in.size());
+    }
+
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jeongnim.kim $
+ * $Revision: 4479 $   $Date: 2009-12-09 17:58:57 -0600 (Wed, 09 Dec 2009) $
+ * $Id: scalar_traits.h 4479 2009-12-09 23:58:57Z jeongnim.kim $ 
+ ***************************************************************************/
