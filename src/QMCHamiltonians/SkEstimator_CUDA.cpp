45a2aa114aace18194b1209e527eb2284105e6cb
Ken Esler <kpesler@gmail.com>
2010-03-04 00:40:16 +0000

    SkEstimator_CUDA::addEnergy does something, but not yet right.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4695 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCHamiltonians/SkEstimator_CUDA.cpp
+++ src/QMCHamiltonians/SkEstimator_CUDA.cpp
@@ -13,12 +13,39 @@
 //////////////////////////////////////////////////////////////////
 
 #include "QMCHamiltonians/SkEstimator_CUDA.h"
+#include "Particle/MCWalkerConfiguration.h"
 
 namespace qmcplusplus {
   void 
   SkEstimator_CUDA::addEnergy(MCWalkerConfiguration &W, 
 			      vector<RealType> &LocalEnergy)
   {
+    int nw = W.WalkerList.size();
+    gpu::host_vector<CUDA_PRECISION> rhok_host;
+    int stride = (int)(W.WalkerList[0]->get_rhok_ptr(1) - 
+		       W.WalkerList[0]->get_rhok_ptr(0));
 
+    vector<CUDA_PRECISION> rhok_total(2*NumK, 0.0);
+    for (int iw=0; iw<nw; iw++) {
+      Walker_t &walker = *(W.WalkerList[iw]);
+      rhok_host = walker.Rhok_GPU;
+      fill (rhok_total.begin(), rhok_total.end(), 0.0);
+      for (int isp=0; isp<NumSpecies; isp++) 
+	for (int ik=0; ik<2*NumK; ik++)
+	  rhok_total[ik] += rhok_host[ik+isp*stride];
+      if(hdf5_out) {
+	for (int ik=0; ik<NumK; ik++)
+	  W.Collectables[myIndex+ik] += OneOverN*
+	    (rhok_total[2*ik+0]*rhok_total[2*ik+0] +
+	     rhok_total[2*ik+1]*rhok_total[2*ik+1]);
+      }
+      else {
+	for (int ik=0; ik<NumK; ik++)
+	  W.WalkerList[iw]->getPropertyBase()[NUMPROPERTIES+myIndex] = 
+	    OneOverN * (rhok_total[2*ik+0]*rhok_total[2*ik+0] +
+			rhok_total[2*ik+1]*rhok_total[2*ik+1]);
+	
+      }
+    }
   }
 }
