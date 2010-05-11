ce46bb4203c7ac0c2562a5202f05cd3a31eadff6
Jeongnim Kim <jeongnim.kim@intel.com>
2010-05-11 17:15:43 +0000

    Generalize the test.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4783 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/benchmark/fft1d.cpp
+++ src/benchmark/fft1d.cpp
@@ -23,50 +23,131 @@
 #include "Message/Communicate.h"
 #include <fft/fft.h>
 
-template<typename T>
-inline T abs_diff(T a, T b) { return abs(a-b); }
 
-template<typename T>
-inline T abs_diff(std::complex<T>& a, std::complex<T>& b) 
-{ 
-  return abs(a.real()-b.real())+abs(a.imag()-b.imag()); 
-}
-
-template<typename T>
-inline T abs_diff(std::complex<T>& a, T b)
-{ 
-  return abs(a.real()-b); 
-}
-
-template<typename T1, typename T2, unsigned fft_eng>
-struct fft1d_test
+namespace qmcplusplus
 {
-  typedef fft1d_engine<T1,T2,fft_eng> fft1d_engine_t;
-  typedef fft1d_engine_t::space_type space_type;
-  typedef fft1d_engine_t::spectral_type spectral_type;
+  template<typename T>
+    inline T abs_diff(T a, T b) { return std::abs(a-b); }
 
-  fft1d_engine_t myfft;
-  Matrix<space_type> in;
-  Matrix<spectral_type> out;
-  std::vector<space_type> sample;
+  template<typename T>
+    inline T abs_diff(std::complex<T>& a, std::complex<T>& b) 
+    { 
+      return std::abs(a.real()-b.real())+std::abs(a.imag()-b.imag()); 
+    }
 
-  fft1d_test(int fft_dim, int howmany)
-  {
-    myfft.set_defaults(fft_dim,howmany);
-    in.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_IN_DISTANCE));
-    out.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_OUT_DISTANCE));
-    sample.resize(fft_dim);
-    real_type phase=2*M_PI/static_cast<real_type>(fft_dim);
-    real_type norm=1.0/static_cast<real_type>(fft_dim);
-    for(int i=0; i<sample.size(); ++i) 
-      sample[i]=0.5*sin(phase*i)+0.1*sin(phase*2*i)+0.3*sin(phase*3*i)+0.5*sin(phase*4.1*i);;
-  }
+  template<typename T>
+    inline T abs_diff(std::complex<T>& a, T b)
+    { 
+      return std::abs(a.real()-b); 
+    }
+  template<typename T1, typename T2, unsigned fft_eng>
+    struct fft1d_test
+    {
+      typedef fft1d_engine<T1,T2,fft_eng> fft1d_engine_t;
+      typedef typename fft1d_engine_t::space_type space_type;
+      typedef typename fft1d_engine_t::spectral_type spectral_type;
+      typedef typename fft1d_engine_t::real_type real_type;
 
-};
+      bool InPlaceFFT;
+      fft1d_engine_t myfft;
+      Matrix<space_type> in;
+      Matrix<spectral_type> out;
+      std::vector<space_type> sample;
+
+      fft1d_test(int fft_dim, int howmany, bool inplace):InPlaceFFT(inplace)
+      {
+        reset(fft_dim,howmany,inplace);
+      }
+
+      void reset(int fft_dim, int howmany, bool inplace)
+      {
+        InPlaceFFT=inplace;
+        myfft.set_defaults(fft_dim,howmany);
+        in.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_IN_DISTANCE));
+        out.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_OUT_DISTANCE));
+        sample.resize(fft_dim);
+        real_type phase=2*M_PI/static_cast<real_type>(fft_dim);
+        for(int i=0; i<sample.size(); ++i) 
+          sample[i]=0.5*std::sin(phase*i)+0.1*std::sin(phase*2*i)+0.3*std::sin(phase*3*i)+0.5*std::sin(phase*4.1*i);;
+
+        for(int i=0; i<howmany; ++i) std::copy(sample.begin(),sample.end(),in[i]);
+        if(inplace) 
+          myfft.create(in.data());
+        else
+          myfft.create(in.data(),out.data());
+      }
+
+      void debug()
+      {
+        if(InPlaceFFT) 
+        {
+          myfft.fft_forward(in.data());
+          myfft.fft_backward(in.data());
+        }
+        else
+        {
+          myfft.fft_forward(in.data(),out.data());
+          myfft.fft_backward(out.data(),in.data());
+        }
+
+        int fft_dim=myfft(FFT_LENGTH);
+        int howmany=myfft(FFT_NUMBER_OF_TRANSFORMS);
+        real_type norm=1.0/static_cast<real_type>(fft_dim);
+        real_type eps=10*numeric_limits<real_type>::epsilon();
+        in *= norm;
+        for(int i=0; i<howmany; ++i)
+        {
+          cout << "State index = " << i << endl;
+          for(int k=0; k<fft_dim; ++k)
+          {
+            //cout << target(i,k) << " " << sample[k] << endl;
+            if(abs_diff(in(i,k),sample[k])>eps)
+              cerr << "WRONG " << in(i,k) << sample[k] <<  " " << endl;
+          }
+        }
+
+      }
+    };
+
+  template<unsigned fft_id>
+    struct fft1d_debug
+    {
+      static void doit(int fft_dim, int howmany)
+      {
+        {
+          cout << "Double precision, r2c, in-place " << endl;
+          fft1d_test<double,complex<double>,fft_id> test(fft_dim,howmany,true);
+          test.debug();
+        }
+        {
+          cout << "Double precision, r2c, out-place " << endl;
+          fft1d_test<double,complex<double>,fft_id> test(fft_dim,howmany,false);
+          test.debug();
+        }
+        {
+          cout << "Double precision, c2c, in-place " << endl;
+          fft1d_test<complex<double>,complex<double>,fft_id> test(fft_dim,howmany,true);
+          test.debug();
+        }
+        {
+          cout << "Double precision, c2c, out-place " << endl;
+          fft1d_test<complex<double>,complex<double>,fft_id> test(fft_dim,howmany,false);
+          test.debug();
+        }
+      }
+    };
+
+}
+
+using namespace qmcplusplus;
+
+inline void print_help(const string& msg)
+{
+  printf("%s -d fft_dim -m numer_of_fft -i iterations -p [d|s] -t [r2c|c2c] -e [fftw|mkl|essl] \n",msg.c_str());
+}
 
 int main(int argc, char** argv)
 {
-  using namespace qmcplusplus;
 
   OHMMS::Controller->initialize(argc,argv);
   Communicate* mycomm=OHMMS::Controller;
@@ -77,51 +158,104 @@ int main(int argc, char** argv)
   int niters=10;
   int fft_dim=16;
 
-  typedef double real_type;
-  typedef complex<double> complex_type;
-
-  //const unsigned int fft_id=FFTW_ENG;
-  const unsigned int fft_id=FFTMKL_ENG;
-  typedef fft1d_engine<real_type,complex_type,fft_id> fft1d_engine_t;
-  //typedef fft1d_engine<complex_type,complex_type,fft_id> fft1d_engine_t;
-  typedef fft1d_engine_t::space_type space_type;
-  typedef fft1d_engine_t::spectral_type spectral_type;
-
-  fft1d_engine_t myfft;
-  myfft.set_defaults(fft_dim,howmany);
-
-  Matrix<space_type> in(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_IN_DISTANCE));
-  Matrix<spectral_type> out(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_OUT_DISTANCE));
-  std::vector<space_type> sample(fft_dim);
-  real_type phase=2*M_PI/static_cast<real_type>(fft_dim);
-  real_type norm=1.0/static_cast<real_type>(fft_dim);
-  for(int i=0; i<sample.size(); ++i) 
-    sample[i]=0.5*sin(phase*i)+0.1*sin(phase*2*i)+0.3*sin(phase*3*i)+0.5*sin(phase*4.1*i);;
-
-
-  //myfft.create(in.data(),out.data());
-  myfft.create(in.data());
-
-  for(int i=0; i<howmany; ++i) std::copy(sample.begin(),sample.end(),in[i]);
-
-  myfft.fft_forward(in.data());
-  myfft.fft_backward(in.data());
-  //myfft.fft_forward(in.data(),out.data());
-  //myfft.fft_backward(out.data(),in.data());
-
-  real_type eps=10*numeric_limits<real_type>::epsilon();
-  in *= norm;
-  for(int i=0; i<howmany; ++i)
-  {
-    cout << "State index = " << i << endl;
-    for(int k=0; k<fft_dim; ++k)
-    {
-      //cout << target(i,k) << " " << sample[k] << endl;
-      if(abs_diff(in(i,k),sample[k])>eps)
-        cerr << "WRONG " << in(i,k) << sample[k] <<  " " << endl;
+  //accepted: r2c  or c2c
+  string fft_type("r2c");
+  string fft_eng("fftw");
+  bool inplace=true;
+  bool single_precision=false;
+  int opt;
+  while((opt = getopt(argc, argv, "hp:d:m:i:e:t:")) != -1) {
+    switch(opt) {
+      case 'h':
+        print_help("Help Message");
+        return 1;
+      case 'd':
+        fft_dim=atoi(optarg);
+        break;
+      case 'm':
+        howmany=atoi(optarg);
+        break;
+      case 'i':
+        niters=atoi(optarg);
+        break;
+      case 'p':
+        if(optarg[0]=='s') single_precision=true;
+        break;
+      case 'e':
+        fft_eng=optarg;
+        break;
+      case 't':
+        fft_type=optarg;
+        break;
+      default:
+        print_help("Unknown options");
+        return 1;
     }
   }
 
+  if(fft_eng =="fftw")
+  {
+    cout << "Testing FFT1D with FFTW " << endl;
+    fft1d_debug<FFTW_ENG>::doit(fft_dim,howmany);
+  }
+#if defined(HAVE_MKL)
+  else if(fft_eng=="mkl")
+  {
+    cout << "Testing FFT1D with MKL " << endl;
+    fft1d_debug<FFTMKL_ENG>::doit(fft_dim,howmany);
+  }
+#endif
+#if defined(HAVE_ESSL)
+  else if(fft_eng =="essl")
+  {
+    cout << "Testing FFT1D with ESSL " << endl;
+    fft1d_debug<FFTESSL_ENG>::doit(fft_dim,howmany);
+  }
+#endif
+  else
+  {
+    cout << fft_eng << " is not available. Choose fftw, mkl or essl" << endl;
+  }
+
+//  typedef fft1d_engine<real_type,complex_type,fft_id> fft1d_engine_t;
+//  //typedef fft1d_engine<complex_type,complex_type,fft_id> fft1d_engine_t;
+//  typedef fft1d_engine_t::space_type space_type;
+//  typedef fft1d_engine_t::spectral_type spectral_type;
+//
+//  fft1d_engine_t myfft;
+//  myfft.set_defaults(fft_dim,howmany);
+//
+//  Matrix<space_type> in(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_IN_DISTANCE));
+//  Matrix<spectral_type> out(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_OUT_DISTANCE));
+//  std::vector<space_type> sample(fft_dim);
+//  real_type phase=2*M_PI/static_cast<real_type>(fft_dim);
+//  real_type norm=1.0/static_cast<real_type>(fft_dim);
+//  for(int i=0; i<sample.size(); ++i) 
+//    sample[i]=0.5*sin(phase*i)+0.1*sin(phase*2*i)+0.3*sin(phase*3*i)+0.5*sin(phase*4.1*i);;
+//
+//
+//  //myfft.create(in.data(),out.data());
+//  myfft.create(in.data());
+//  for(int i=0; i<howmany; ++i) std::copy(sample.begin(),sample.end(),in[i]);
+//
+//  myfft.fft_forward(in.data());
+//  myfft.fft_backward(in.data());
+//  //myfft.fft_forward(in.data(),out.data());
+//  //myfft.fft_backward(out.data(),in.data());
+//
+//  real_type eps=10*numeric_limits<real_type>::epsilon();
+//  in *= norm;
+//  for(int i=0; i<howmany; ++i)
+//  {
+//    cout << "State index = " << i << endl;
+//    for(int k=0; k<fft_dim; ++k)
+//    {
+//      //cout << target(i,k) << " " << sample[k] << endl;
+//      if(abs_diff(in(i,k),sample[k])>eps)
+//        cerr << "WRONG " << in(i,k) << sample[k] <<  " " << endl;
+//    }
+//  }
+//
   OHMMS::Controller->finalize();
   return 0;
 }
