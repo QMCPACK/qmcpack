6314c4192340a9352abc6aee94f167c873e9d6c9
Jeongnim Kim <jeongnim.kim@intel.com>
2010-05-12 01:50:32 +0000

    Add out-place bench marks.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4790 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/benchmark/fft1d_bench.cpp
+++ src/benchmark/fft1d_bench.cpp
@@ -24,23 +24,8 @@
 //#define SINGLE_PREC
 #include <fft/fft.h>
 
-
 namespace qmcplusplus
 {
-  template<typename T>
-    inline T abs_diff(T a, T b) { return std::abs(a-b); }
-
-  template<typename T>
-    inline T abs_diff(std::complex<T>& a, std::complex<T>& b) 
-    { 
-      return std::abs(a.real()-b.real())+std::abs(a.imag()-b.imag()); 
-    }
-
-  template<typename T>
-    inline T abs_diff(std::complex<T>& a, T b)
-    { 
-      return std::abs(a.real()-b); 
-    }
   template<typename T1, typename T2, unsigned fft_eng>
     struct fft1d_test
     {
@@ -49,95 +34,43 @@ namespace qmcplusplus
       typedef typename fft1d_engine_t::spectral_type spectral_type;
       typedef typename fft1d_engine_t::real_type real_type;
 
-      bool InPlaceFFT;
       fft1d_engine_t myfft;
       Matrix<space_type> in;
       Matrix<spectral_type> out;
-      std::vector<space_type> sample;
 
-      fft1d_test(int fft_dim, int howmany, bool inplace):InPlaceFFT(inplace)
+      fft1d_test(int fft_dim, int howmany)
       {
-        reset(fft_dim,howmany,inplace);
+        reset(fft_dim,howmany);
       }
 
-      void reset(int fft_dim, int howmany, bool inplace)
+      void reset(int fft_dim, int howmany)
       {
-        InPlaceFFT=inplace;
         myfft.set_defaults(fft_dim,howmany);
         in.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_IN_DISTANCE));
         out.resize(myfft(FFT_NUMBER_OF_TRANSFORMS),myfft(FFT_OUT_DISTANCE));
-        sample.resize(fft_dim);
+        std::vector<space_type> sample(fft_dim);
         real_type phase=2*M_PI/static_cast<real_type>(fft_dim);
         for(int i=0; i<sample.size(); ++i) 
           sample[i]=0.5*std::sin(phase*i)+0.1*std::sin(phase*2*i)+0.3*std::sin(phase*3*i)+0.5*std::sin(phase*4.1*i);;
-
         for(int i=0; i<howmany; ++i) std::copy(sample.begin(),sample.end(),in[i]);
-        if(inplace) 
-          myfft.create(in.data());
-        else
-          myfft.create(in.data(),out.data());
+        myfft.create(in.data(),out.data());
       }
 
-      void debug()
+      double gflops() const 
       {
-        if(InPlaceFFT) 
-        {
-          myfft.fft_forward(in.data());
-          myfft.fft_backward(in.data());
-        }
-        else
-        {
-          myfft.fft_forward(in.data(),out.data());
-          myfft.fft_backward(out.data(),in.data());
-        }
+        return myfft.gflops();
+      }
 
-        int fft_dim=myfft(FFT_LENGTH);
-        int howmany=myfft(FFT_NUMBER_OF_TRANSFORMS);
-        real_type norm=1.0/static_cast<real_type>(fft_dim);
-        real_type eps=10*numeric_limits<real_type>::epsilon();
-        in *= norm;
-        for(int i=0; i<howmany; ++i)
-        {
-          cout << "State index = " << i << endl;
-          for(int k=0; k<fft_dim; ++k)
-          {
-            //cout << target(i,k) << " " << sample[k] << endl;
-            if(abs_diff(in(i,k),sample[k])>eps)
-              cerr << "WRONG " << in(i,k) << " " << sample[k] <<  " " << endl;
-          }
-        }
+      inline void fft() 
+      {
+        myfft.fft_forward(in.data(),out.data());
+      }
 
+      inline void ifft() 
+      {
+        myfft.fft_backward(out.data(),in.data());
       }
     };
-
-  template<typename T, unsigned fft_id>
-    struct fft1d_debug
-    {
-      static void doit(int fft_dim, int howmany)
-      {
-        {
-          cout << "r2c, in-place " << endl;
-          fft1d_test<T,complex<T>,fft_id> test(fft_dim,howmany,true);
-          test.debug();
-        }
-        {
-          cout << "r2c, out-place " << endl;
-          fft1d_test<T,complex<T>,fft_id> test(fft_dim,howmany,false);
-          test.debug();
-        }
-        {
-          cout << "c2c, in-place " << endl;
-          fft1d_test<complex<T>,complex<T>,fft_id> test(fft_dim,howmany,true);
-          test.debug();
-        }
-        {
-          cout << "c2c, out-place " << endl;
-          fft1d_test<complex<T>,complex<T>,fft_id> test(fft_dim,howmany,false);
-          test.debug();
-        }
-      }
-    };
-
 }
 
 
@@ -145,6 +78,7 @@ inline void print_help(const string& msg)
 {
   printf("%s -d fft_dim -m numer_of_fft -i iterations -p [d|s] -t [r2c|c2c] -e [fftw|mkl|essl] \n",msg.c_str());
 }
+
 
 int main(int argc, char** argv)
 {
@@ -155,7 +89,7 @@ int main(int argc, char** argv)
   OhmmsInfo Welcome(argc,argv,mycomm->rank());
   qmcplusplus::Random.init(0,1,11);
 
-  int howmany=1;
+  int howmany=2;
   int niters=10;
   int fft_dim=16;
 
@@ -194,41 +128,38 @@ int main(int argc, char** argv)
     }
   }
 
-  if(fft_eng =="fftw")
+  double fft_time=0.0;
+  double ifft_time=0.0;
+  double gflops=0.0;
+#pragma omp parallel  reduction(+:fft_time, ifft_time, gflops)
   {
-    cout << "Testing FFT1D with FFTW " << endl;
-#if defined(SINGLE_PREC)
-    cout << "Single precision " << endl;
-    fft1d_debug<float,FFTW_ENG>::doit(fft_dim,howmany);
-#else
-    cout << "Double precision " << endl;
-    fft1d_debug<double,FFTW_ENG>::doit(fft_dim,howmany);
-#endif
+    //fft1d_test<double,complex<double>,FFTW_ENG> fft_tester(fft_dim,howmany);
+    //fft1d_test<float,complex<float>,FFTESSL_ENG> fft_tester(fft_dim,howmany);
+    fft1d_test<complex<float>,complex<float>,FFTESSL_ENG> fft_tester(fft_dim,howmany);
+    double t_f=0.0;
+    double t_b=0.0;
+    Timer clock;
+    for(int i=0; i<niters; ++i)
+    {
+      clock.restart();
+      fft_tester.fft();
+      t_f += clock.elapsed();
+      clock.restart();
+      fft_tester.ifft();
+      t_b += clock.elapsed();
+    }
+
+    fft_time+= t_f;
+    ifft_time+= t_b;
+    gflops += fft_tester.gflops();
   }
-#if defined(HAVE_MKL)
-  else if(fft_eng=="mkl")
-  {
-    cout << "Testing FFT1D with MKL " << endl;
-    cout << "Double precision " << endl;
-    fft1d_debug<double,FFTMKL_ENG>::doit(fft_dim,howmany);
-    cout << "Single precision " << endl;
-    fft1d_debug<float,FFTMKL_ENG>::doit(fft_dim,howmany);
-  }
-#endif
-#if defined(HAVE_ESSL)
-  else if(fft_eng =="essl")
-  {
-    cout << "Testing FFT1D with ESSL " << endl;
-    cout << "Double precision " << endl;
-    fft1d_debug<double,FFTESSL_ENG>::doit(fft_dim,howmany);
-    cout << "Single precision " << endl;
-    fft1d_debug<float,FFTESSL_ENG>::doit(fft_dim,howmany);
-  }
-#endif
-  else
-  {
-    cout << fft_eng << " is not available. Choose fftw, mkl or essl" << endl;
-  }
+
+  gflops /= omp_get_max_threads();
+  fft_time /= omp_get_max_threads()*niters;
+  ifft_time /= omp_get_max_threads()*niters;
+
+  cout << "gflops " << gflops << endl;
+  cout << fft_time <<" " << ifft_time << " " << gflops/fft_time << " " << gflops/ifft_time << endl;
 
   OHMMS::Controller->finalize();
   return 0;
