29bc2505d6b9b1d7a5db3acf6e1adb68c35fa2f9
Jeongnim Kim <jeongnim.kim@intel.com>
2010-05-21 18:33:38 +0000

    Update with new env 2010-05-21. Removed dynamic link problems.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4825 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/JaguarGNU.cmake
+++ config/JaguarGNU.cmake
@@ -1,40 +1,30 @@
-# tool chain for Jaguar, Cray XT5  using PrgEnv-gnu
-# Fri Feb 20 15:50:10 EST 2009 by Jeongnim Kim
-# module list
-# Currently Loaded Modulefiles:
-# 1) modules/3.1.6                           13) Base-opts/2.1.41HD
-# 2) DefApps                                 14) subversion/1.5.0
-# 3) torque/2.3.2-snap.200807092141          15) gcc/4.2.0.quadcore
-# 4) moab/5.2.4                              16) fftw/3.1.1
-# 5) xtpe-quadcore                           17) xt-libsci/10.3.1
-# 6) MySQL/5.0.45                            18) xt-mpt/3.1.0
-# 7) xt-service/2.1.41HD                     19) xt-pe/2.1.41HD
-# 8) xt-libc/2.1.41HD                        20) xt-asyncpe/2.0
-# 9) xt-os/2.1.41HD                          21) PrgEnv-gnu/2.1.41HD
-# 10) xt-boot/2.1.41HD                        22) cmake/2.6.1
-# 11) xt-lustre-ss/2.1.UP00_ORNL.nic52_1.6.5  23) acml/4.1.0
-# 12) xtpe-target-cnl                         24) hdf5/1.6.8
-
 SET(CMAKE_SYSTEM_PROCESSOR "XT5")
-SET_PROPERTY(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
 
-set(ACML_HOME /opt/acml/4.3.0/gfortran64)
 
-set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/3.2/bin/cc)
-set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/3.2/bin/CC)
+set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/3.7/bin/cc)
+set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/3.7/bin/CC)
 set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline")
-set(GNU_FLAGS "-fopenmp -O3 -ftemplate-depth-60 -Drestrict=__restrict__  -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
+set(GNU_FLAGS "-fopenmp -O3 -Drestrict=__restrict__  -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
 set(XT_FLAGS "-march=amdfam10 -msse3 -D_CRAYMPI")
 set(CMAKE_CXX_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -ftemplate-depth-60 ${GNU_OPTS}")
 set(CMAKE_C_FLAGS "${XT_FLAGS} ${GNU_FLAGS}")
 
+FOREACH(type SHARED_LIBRARY SHARED_MODULE EXE)
+  SET(CMAKE_${type}_LINK_STATIC_C_FLAGS "-Wl,-Bstatic")
+  SET(CMAKE_${type}_LINK_DYNAMIC_C_FLAGS "-Wl,-Bstatic")
+  SET(CMAKE_${type}_LINK_STATIC_CXX_FLAGS "-Wl,-Bstatic")
+  SET(CMAKE_${type}_LINK_DYNAMIC_CXX_FLAGS "-Wl,-Bstatic")
+ENDFOREACH(type)
+
+
 set(CMAKE_FIND_ROOT_PATH
-      /sw/xt5/hdf5/1.6.8/cnl2.1_gnu4.2.4
-      /sw/xt5/szip/2.1/sles10.1_gnu4.2.4
-      /sw/xt5/fftw/3.2/cnl2.1_gnu4.2.0
-      /ccs/proj/mat001/xt5/gnu2.1.4/einspline
-      /ccs/proj/mat001/cnl/boost
-     )
+  /sw/xt5/hdf5/1.8.2/cnl2.1_gnu4.2.0
+  /sw/xt5/szip/2.1/sles10.1_gnu4.2.4
+  /sw/xt5/fftw/3.2/cnl2.1_gnu4.2.0
+  /ccs/home/jnkim/3.7/einspline
+  /ccs/proj/mat001/cnl/boost
+)
+set(ACML_HOME /opt/acml/4.3.0/gfortran64)
 
 SET(ENABLE_OPENMP 1)
 SET(HAVE_MPI 1)
@@ -44,32 +34,6 @@ SET(HAVE_SSE3 1)
 SET(HAVE_SSSE3 1)
 SET(USE_PREFETCH 1)
 SET(PREFETCH_AHEAD 12)
+
 SET(ACML_LIBRARIES ${ACML_HOME}/lib/libacml.a ${ACML_HOME}/lib/libacml_mv.a)
 link_libraries(${ACML_LIBRARIES})
-
-#----------------------------------------------------------
-# disable dynamic links
-#----------------------------------------------------------
-SET(CMAKE_SKIP_RPATH TRUE)
-SET(CMAKE_SHARED_LIBRARY_C_FLAGS "")            # -pic 
-SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "")       # -shared
-SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")         # +s, flag for exe link to use shared lib
-SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")       # -rpath
-SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP "")   # : or empty
-
-SET(CMAKE_LINK_LIBRARY_SUFFIX "")
-SET(CMAKE_STATIC_LIBRARY_PREFIX "lib")
-SET(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
-SET(CMAKE_SHARED_LIBRARY_PREFIX "lib")          # lib
-SET(CMAKE_SHARED_LIBRARY_SUFFIX ".a")           # .a
-SET(CMAKE_EXECUTABLE_SUFFIX "")          # .exe
-SET(CMAKE_DL_LIBS "" )
-
-SET(CMAKE_FIND_LIBRARY_PREFIXES "lib")
-SET(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-
-SET(CMAKE_CXX_LINK_SHARED_LIBRARY)
-SET(CMAKE_CXX_LINK_MODULE_LIBRARY)
-SET(CMAKE_C_LINK_SHARED_LIBRARY)
-SET(CMAKE_C_LINK_MODULE_LIBRARY)
-
