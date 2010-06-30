e6c0fc2a60a304bd7d4ad04dbfad0b4c43ca3195
Ken Esler <kpesler@gmail.com>
2010-06-30 19:18:18 +0000

    Added missing increment in second half of GPU woodbury update.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4912 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/Fermion/determinant_update.cu
+++ src/QMCWaveFunctions/Fermion/determinant_update.cu
@@ -2137,7 +2137,7 @@ woodbury_update_16b (T** Ainv_trans, T** delta,
     for (int i=0; i<4; i++, row+=4) 
       B1[row][col] = myAinv_delta[row*16+col+16*block];
     row = tid >> 4;
-    for (int irow=0; irow<4; irow++) {
+    for (int irow=0; irow<4; irow++,row+=4) {
       T mysum = myAinv[(16*block+row)*rowstride + c];
       for (int k=0; k<16; k++)
 	mysum += B3[row][k] * B1[col][k];
