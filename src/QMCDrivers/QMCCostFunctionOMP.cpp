b29cb2ca48e5ee5316f8296aa37bfc0344b16564
Jeremy McMinis <jmcminis@gmail.com>
2010-08-23 22:25:32 +0000

    for OMP
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@4951 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCDrivers/QMCCostFunctionOMP.cpp
+++ src/QMCDrivers/QMCCostFunctionOMP.cpp
@@ -334,11 +334,22 @@ namespace qmcplusplus
     if (final_reset) {
       for (int i=0; i<psiClones.size(); ++i)
         psiClones[i]->stopOptimization();
+      
+#pragma omp parallel
+    {
+      int ip = omp_get_thread_num();
+      MCWalkerConfiguration& wRef(*wClones[ip]);
+      
+      MCWalkerConfiguration::iterator it(wRef.begin());
+      MCWalkerConfiguration::iterator it_end(wRef.end());
+      for (; it!=it_end; ++it) (**it).DataSetForDerivatives.clear();
+    }
+      
 // is this correct with OMP?
-      MCWalkerConfiguration::iterator it(W.begin());
-      MCWalkerConfiguration::iterator it_end(W.end());
-      for (; it!=it_end; ++it)
-        (**it).DataSetForDerivatives.clear();
+//       MCWalkerConfiguration::iterator it(W.begin());
+//       MCWalkerConfiguration::iterator it_end(W.end());
+//       for (; it!=it_end; ++it)
+//         (**it).DataSetForDerivatives.clear();
 
     }
     //cout << "######### QMCCostFunctionOMP::resetPsi " << endl;
