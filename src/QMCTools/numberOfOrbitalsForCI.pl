3247e5607af0a23018ce31b9bc4f8d4c306f5798
Miguel Morales <miguel.mmorales@gmail.com>
2010-12-14 01:54:02 +0000

    added simple script to determine number of orbitals required for a particular cutoff in the number of determinants
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5088 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCTools/numberOfOrbitalsForCI.pl
@@ -0,0 +1,42 @@
+#! /usr/bin/perl
+# only works for csf's right now
+
+if($#ARGV != 1)
+{
+  print "Usage: script filename cutoff \n";
+  exit(1)
+}
+
+$filename = $ARGV[0];
+$dbl1 = $ARGV[1];
+# check file exists
+open(INN,"< $filename");
+@alldata = <INN>;
+close(INN);
+
+$dcnt = 0;
+$int1 = 0;
+
+while($dcnt < $#alldata)
+{
+ if($alldata[$dcnt] =~ /<csf/)
+ {
+   $alldata[$dcnt] =~ /qchem_coeff.\"(\S*)\"/;
+   $somevar = $1;
+   #print "my num $somevar\n";
+   if(abs($somevar) > abs($dbl1))
+   { 
+    $alldata[$dcnt] =~ /occ.\"(.*)[12](0+)"/;
+    $mylength = length($1)+1;
+    #print "dol1 $1 dol2 $2 \n";
+    #print "my length is $mylength\n";
+    if($mylength > $int1) 
+    {
+      $int1 = $mylength;
+    } 
+   }
+ } 
+ $dcnt++;
+}
+
+print "Required number of orbitals: $int1 \n"; 
