e7796fe3251553de41e0b43eaf5b5b79ce30a313
Jeongnim Kim <jeongnim.kim@intel.com>
2011-05-05 01:45:54 +0000

    Update BGP toolchain with the libraries in /home/projects/qmcpack.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5217 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/BGToolChain.cmake
+++ config/BGToolChain.cmake
@@ -16,8 +16,9 @@ set(CMAKE_CXX_COMPILER  /soft/apps/darshan/bin/default/mpixlcxx_r)
 # set the search path for the environment coming with the compiler
 # and a directory where you can install your own compiled software
 set(CMAKE_FIND_ROOT_PATH
-/home/jnkim/share/boost_1_45_0
-/home/jnkim/share
+/home/projects/qmcpack/boost-1.45_0
+/home/projects/qmcpack/einspline-0.9.2
+/home/projects/qmcpack/libxml2-2.7.8
 /soft/apps/hdf5-1.6.6
 /soft/apps/fftw-3.1.2-double
 )
@@ -30,34 +31,22 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_SHARED_LINKER_FLAGS " ")
 
-set(AIX_ARCH "450")
-#SET(AIX_ARCH_FLAGS "-qarch=${AIX_ARCH}d  -qtune=${AIX_ARCH}d  -qcache=${AIX_ARCH} -qsmp=omp")
-SET(AIX_ARCH_FLAGS "-qarch=${AIX_ARCH}  -qtune=${AIX_ARCH} -qsmp=omp")
-#SET(AIX_CXX_COMMON_FLAGS " -qkeyword=restrict -qstrict -qhot -qrtti=dyna -qtemplaterecompile -qnoeh -qsuppress=1540-1090 ")
-SET(AIX_CXX_COMMON_FLAGS " -qkeyword=restrict -qstrict -qhot -qtemplaterecompile -qnoeh -qsuppress=1540-1090:1540-1088 ")
+set(AIX_ARCH "450d")
+SET(AIX_ARCH_FLAGS "-qarch=${AIX_ARCH}  -qsmp=omp -qthreaded")
+SET(AIX_CXX_COMMON_FLAGS " -qkeyword=restrict -qstrict -qhot -qnoeh -qsuppress=1540-1090:1540-1088 ")
+SET(AIX_OPT_FLAGS "-O3 -qmaxmem=-1 -qprefetch ")
+
 #SET(AIX_OPT_FLAGS "-O3 -Q -qmaxmem=-1 -qipa=inline -qinline -qlargepage -qprefetch ")
-SET(AIX_OPT_FLAGS "-O3 -qmaxmem=-1  -qlargepage -qprefetch ")
 #SET(AIX_CXX_OPT_FLAGS "-O3 -Q -qlargepage -qprefetch")
 #SET(AIX_CXX_FLAGS "-O3 -Q -qlargepage -qprefetch")
 
 SET(CMAKE_CXX_FLAGS "${AIX_ARCH_FLAGS} ${AIX_CXX_COMMON_FLAGS} ${AIX_OPT_FLAGS}")
 SET(CMAKE_C_FLAGS "${AIX_ARCH_FLAGS} ${AIX_OPT_FLAGS}")
 
-#link_libraries(-I$(ESSLPATH)/include -L$(ESSLPATH)/lib -lesslbg \
-link_libraries(-L/soft/apps/blas-lapack-lib -llapack -L/soft/apps/ESSL-4.4.1-0/lib -lesslbg 
-   -L/soft/apps/ibmcmp/xlf/bg/11.1/lib -lxlf90_r -lxlsmp  
-   -L/soft/apps/ibmcmp/xlmass/bg/4.4/lib -lmass  
-)
-
-#SET(CMAKE_CXX_CREATE_STATIC_LIBRARY
-#       "<CMAKE_AR> -X64 cr <TARGET> <LINK_FLAGS> <OBJECTS> "
-#       "<CMAKE_RANLIB> <TARGET> ")
-
-#SET(CMAKE_C_LINK_EXECUTABLE
-#"<CMAKE_C_COMPILER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> "
-#"  -o <TARGET> <LINK_LIBRARIES> -Wl,-lc ")
-#
-#SET(CMAKE_CXX_LINK_EXECUTABLE
-#"<CMAKE_CXX_COMPILER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> "
-#" -o <TARGET> <LINK_LIBRARIES> -Wl,-lstdc++")
+#link lapack, essl, mass
+link_libraries(/soft/apps/LAPACK/lapack_3.3_BGP.a 
+    /soft/apps/ESSL/lib/libesslbg.a
+    -L/soft/apps/ibmcmp/xlf/bg/11.1/lib -lxlf90_r -lxlsmp  
+    -L/soft/apps/ibmcmp/xlmass/bg/4.4/lib -lmass  
+    )
 
