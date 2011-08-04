0dc6bebc7607b9b065de06e1bff45ccbbfe87286
Jeongnim Kim <jeongnim.kim@intel.com>
2011-08-04 01:10:44 +0000

    Update Kraken toolchain.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5327 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/KrakenGNU.cmake
+++ config/KrakenGNU.cmake
@@ -1,25 +1,12 @@
-# tool chain for Jaguar, Cray XT5  using PrgEnv-gnu
-# Fri Feb 20 15:50:10 EST 2009 by Jeongnim Kim
-# module list
-# Currently Loaded Modulefiles:
-# 1) modules/3.1.6                           13) Base-opts/2.1.41HD
-# 2) DefApps                                 14) subversion/1.5.0
-# 3) torque/2.3.2-snap.200807092141          15) gcc/4.2.0.quadcore
-# 4) moab/5.2.4                              16) fftw/3.1.1
-# 5) xtpe-quadcore                           17) xt-libsci/10.3.1
-# 6) MySQL/5.0.45                            18) xt-mpt/3.1.0
-# 7) xt-service/2.1.41HD                     19) xt-pe/2.1.41HD
-# 8) xt-libc/2.1.41HD                        20) xt-asyncpe/2.0
-# 9) xt-os/2.1.41HD                          21) PrgEnv-gnu/2.1.41HD
-# 10) xt-boot/2.1.41HD                        22) cmake/2.6.1
-# 11) xt-lustre-ss/2.1.UP00_ORNL.nic52_1.6.5  23) acml/4.1.0
-# 12) xtpe-target-cnl                         24) hdf5/1.6.8
+# tool chain for Kraken, Cray XT5  using PrgEnv-gnu
+# switch the prog. env to use GNU compilers
+# module swap PrgEnv-pgi PrgEnv-gnu
 
 SET(CMAKE_SYSTEM_PROCESSOR "XT5")
 SET_PROPERTY(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
 
-set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/4.0/bin/cc)
-set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/4.0/bin/CC)
+set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/4.9/bin/cc)
+set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/4.9/bin/CC)
 set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline")
 set(GNU_FLAGS "-fopenmp -O3 -ftemplate-depth-60 -Drestrict=__restrict__  -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
 set(XT_FLAGS "-march=amdfam10 -msse3 -D_CRAYMPI")
@@ -33,7 +20,7 @@ FOREACH(type SHARED_LIBRARY SHARED_MODULE EXE)
   SET(CMAKE_${type}_LINK_DYNAMIC_CXX_FLAGS "-Wl,-Bstatic")
 ENDFOREACH(type)
 
-set(ACML_HOME /opt/acml/4.2.0/gfortran64)
+set(ACML_HOME /opt/acml/4.4.0/gfortran64/)
 
 set(CMAKE_FIND_ROOT_PATH
       /opt/fftw/3.2.2.1
