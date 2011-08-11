2f8c35ce09d9d052e786002c1aecbe887ff4bc63
Jeremy McMinis <jmcminis@gmail.com>
2011-08-11 05:05:42 +0000

    forgotten builder
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5332 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/AFMSPOBuilder.cpp
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2010-  by Ken Esler and Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//   National Center for Supercomputing Applications &
+//   Materials Computation Center
+//   University of Illinois, Urbana-Champaign
+//   Urbana, IL 61801
+//   e-mail: esler@uiuc.edu
+//
+// Supported by 
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+
+#include "QMCWaveFunctions/AFMSPOBuilder.h"
+#include "OhmmsData/AttributeSet.h"
+
+namespace qmcplusplus
+{
+  
+  AFMSPOBuilder::AFMSPOBuilder
+  (ParticleSet& p, PtclPoolType& psets, xmlNodePtr cur) :
+    targetPtcl (&p)
+  {
+
+  }
+  
+  bool 
+  AFMSPOBuilder::put (xmlNodePtr cur)
+  {
+    return true;
+  }
+  
+
+  SPOSetBase* 
+  AFMSPOBuilder::createSPOSet(xmlNodePtr cur)
+  {
+    AFMSPOSet *spo =  new AFMSPOSet();
+    return spo;
+  }
+
+}
