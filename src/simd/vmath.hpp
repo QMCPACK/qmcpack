578525b46b5f95800bd11e10e54be9e360de3ca7
Jeongnim Kim <jeongnim.kim@intel.com>
2012-08-01 18:15:36 +0000

    API for simd::sqrt, simd::inv. Use MKL or MASSV if available.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5570 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/simd/vmath.hpp
+++ src/simd/vmath.hpp
@@ -14,6 +14,12 @@
 //   Materials Computation Center, UIUC
 //////////////////////////////////////////////////////////////////
 // -*- C++ -*-
+/** @file vmath.h
+ *
+ * Define vectorized math functions. 
+ * HAVE_MKL_VML 
+ * HAVE_MASSV 
+ */
 #ifndef QMCPLUSPLUS_VECTORIZED_STDMATH_HPP
 #define QMCPLUSPLUS_VECTORIZED_STDMATH_HPP
 
@@ -38,6 +44,77 @@ namespace qmcplusplus {
         for(SIZET i=0; i<n; ++i) inout[i]-=std::floor(inout[i]);
       }
 
+    template<typename T, typename SIZET>
+      inline void sqrt(T* restrict inout, SIZET n)
+      {
+        for(SIZET i=0; i<n; ++i) inout[i]=std::sqrt(inout[i]);
+      }
+
+    template<typename T>
+      inline void sqrt(const T* restrict in, T* restrict out, int n)
+      {
+        for(int i=0; i<n; ++i) out[i]=std::sqrt(in[i]);
+      }
+    template<typename T>
+      inline void inv(const T* restrict in, T* restrict out, int n)
+      {
+        for(int i=0; i<n; ++i) out[i]=1.0/in[i];
+      }
+
+#if defined(HAVE_MKL_VML)
+#include <mkl_vml_functions.h>
+
+    inline void sqrt(const double* restrict in, double* restrict out, int n)
+    {
+      vdSqrt(n,in,out);
+    }
+
+    inline void sqrt(const float* restrict in, float* restrict out, int n)
+    {
+      vsSqrt(n,in,out);
+    }
+
+    inline void inv(const double* restrict in, double* restrict out, int n)
+    {
+      vdInv(n,in,out);
+    }
+
+    inline void inv(const float* restrict in, float* restrict out, int n)
+    {
+      vsInv(n,in,out);
+    }
+
+    inline void inv_sqrt(const double* restrict in, double* restrict out, int n)
+    {
+      vdInvSqrt(n,in,out);
+    }
+
+    inline void inv_sqrt(const float* restrict in, float* restrict out, int n)
+    {
+      vsInvSqrt(n,in,out);
+    }
+
+#elif defined(HAVE_MASSV)
+#include <mass.h>
+#include <massv.h>
+    inline void sqrt(double* restrict in, double* restrict out, int n)
+    {
+      vsqrt(out,in,&n);
+    }
+    inline void sqrt(float* restrict in, float* restrict out, int n)
+    {
+      vssqrt(out,in,&n);
+    }
+    inline void inv_sqrt(double* restrict in, double* restrict out, int n)
+    {
+      vrsqrt(out,in,&n);
+    }
+
+    inline void inv_sqrt(float* restrict in, float* restrict out, int n)
+    {
+      vsrsqrt(out,in,&n);
+    }
+#endif
   }
 }
 #endif
