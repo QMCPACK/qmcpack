d5823418a276d5f966689ff314e41c3956bd428e
Jeongnim Kim <jeongnim.kim@intel.com>
2012-08-01 18:13:37 +0000

    Class to handle distance tables for non-local PP.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5569 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/Particle/ParticleSetProxy.cpp
@@ -0,0 +1,134 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2012-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file ParticleSetProxy.cpp
+ *
+ * Helper classes to fine-grained or nested threading
+ */
+#include <Particle/ParticleSetProxy.h>
+#include <Lattice/ParticleBConds.h>
+#include <simd/simd.hpp>
+
+namespace qmcplusplus {
+
+//  template <int SC> 
+//    struct D2OnSphere: public DistTableOnSphere
+//  {
+//
+//    D2OnSphere(const DistanceTableData& dt):DistTableOnSphere(dt)
+//    {}
+//
+//    inline void makeMoves(int iel, const DistanceTableData& dt, const vector<PosType>& displs)
+//    {
+//      const int nS=dt.N[SourceIndex];
+//      const int nK=r_m.cols();
+//      const PosType* dr_iel = dt.dr_full[iel];
+//      for(int jat=0; jat<nS; ++jat)
+//      {
+//        PosType* restrict dr_ptr=dr_m[jat];
+//        RealType* restrict r_ptr=r_m[jat];
+//        for(int k=0; k<nK; ++k)
+//        {
+//          dr_ptr[k]=displs[k]-dr_iel[jat];
+//          r_ptr[k]=std::sqrt(dot(dr_ptr[k],dr_ptr[k]));
+//        }
+//      }
+//    }
+//  };
+//
+//  template <int SC> 
+//    struct D1OnSphere: public DistTableOnSphere
+//  {
+//    D1OnSphere(const DistanceTableData& dt):DistTableOnSphere(dt)
+//    {}
+//
+//    inline void makeMoves(int iel, const DistanceTableData& dt, const vector<PosType>& displs)
+//    {
+//      const int nC=dt.N[SourceIndex];
+//      const int nV=dt.N[VisitorIndex];
+//      const int nK=r_m.cols();
+//      for(int iat=0, loc=iel; iat<nC; ++iat,loc+=nV) 
+//      {
+//        const PosType& dr0(dt.dr(loc));
+//        PosType* restrict dr_ptr=dr_m[iat];
+//        RealType* restrict r_ptr=r_m[iat];
+//        for(int k=0; k<nK; ++k)
+//        {
+//          dr_ptr[k]=dr0+displs[k];
+//          r_ptr[k]=std::sqrt(dot(dr_ptr[k],dr_ptr[k]));
+//        }
+//      }
+//    }
+//  };
+
+  template <typename T, unsigned D, int SC> 
+    struct D2OnSphere: public DistTableOnSphere
+  {
+    DTD_BConds<T,D,SC> myBconds;
+
+    D2OnSphere(const ParticleSet& P, const DistanceTableData& dt)
+      :DistTableOnSphere(dt), myBconds(P.Lattice)
+    {}
+
+    inline void makeMoves(int iel, const DistanceTableData& dt, const vector<PosType>& displs)
+    {
+      const int nS=dt.N[SourceIndex];
+      const int nK=r_m.cols();
+      const PosType* dr_iel = dt.dr_full[iel];
+      for(int jat=0; jat<nS; ++jat)
+      {
+        PosType* restrict dr_ptr=dr_m[jat];
+        for(int k=0; k<nK; ++k) dr_ptr[k]=displs[k]-dr_iel[jat];
+      }
+      myBconds.evaluate_rsq(dr_m.data(),r_m.data(), r_m.size());
+      simd::sqrt(r_m.data(), r_m.size());
+    }
+  };
+
+  template <typename T, unsigned D, int SC> 
+    struct D1OnSphere: public DistTableOnSphere
+  {
+    DTD_BConds<T,D,SC> myBconds;
+
+    D1OnSphere(const ParticleSet& P, const DistanceTableData& dt)
+      :DistTableOnSphere(dt), myBconds(P.Lattice)
+    {}
+
+    inline void makeMoves(int iel, const DistanceTableData& dt, const vector<PosType>& displs)
+    {
+      const int nC=dt.N[SourceIndex];
+      const int nV=dt.N[VisitorIndex];
+      const int nK=r_m.cols();
+      for(int iat=0, loc=iel; iat<nC; ++iat,loc+=nV) 
+      {
+        const PosType& dr0(dt.dr(loc));
+        PosType* restrict dr_ptr=dr_m[iat];
+        for(int k=0; k<nK; ++k) dr_ptr[k]=displs[k]+dr0;
+      }
+      myBconds.evaluate_rsq(dr_m.data(),r_m.data(), r_m.size());
+      simd::sqrt(r_m.data(), r_m.size());
+    }
+  };
+
+  ParticleSetProxy::ParticleSetProxy(const ParticleSet* pt)
+    : myPtcl(pt), ActivePtcl(-1), GroupID(0)
+  {
+    myTables.reserve(pt->DistTables.size());
+
+    myTables.push_back(new D2OnSphere<double,3,SUPERCELL_BULK>(*pt, *(pt->DistTables[0])));
+    for(int t=1; t<pt->DistTables.size(); ++t)
+      myTables.push_back(new D1OnSphere<double,3,SUPERCELL_BULK>(*pt, *(pt->DistTables[t])));
+  }
+
+  ParticleSetProxy::~ParticleSetProxy()
+  {
+    delete_iter(myTables.begin(), myTables.end());
+  }
+
+}
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 5545 $   $Date: 2012-07-10 08:04:38 -0500 (Tue, 10 Jul 2012) $
+ * $Id: ParticleSetProxy.cpp 5545 2012-07-10 13:04:38Z jnkim $ 
+ ***************************************************************************/
---
+++ src/Particle/ParticleSetProxy.h
@@ -0,0 +1,111 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2012-  by Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file ParticleSetProxy.h
+ *
+ * Helper classes to fine-grained or nested threading
+ */
+#ifndef QMCPLUSPLUS_PARTICLESETPROXY_H
+#define QMCPLUSPLUS_PARTICLESETPROXY_H
+
+#include <Particle/ParticleSet.h>
+#include <Particle/DistanceTableData.h>
+#include <Utilities/IteratorUtility.h>
+
+namespace qmcplusplus {
+
+  /** Distance table to handle multiple displacements of one active particle
+   *
+   * j is the index of the target particle that are replaced by \f$\{\delta\}\f$
+   * - r_m(i,k) distance \f$|{\bf r}_j+\delta_k-{\bf r}_i|\f$
+   * - dr_m(i,k) displacement vector \f${\bf r}_j+\delta_k-{\bf r}_i \f$
+   */
+  struct DistTableOnSphere
+  {
+    enum { 
+      SourceIndex=DistanceTableData::SourceIndex,
+      VisitorIndex=DistanceTableData::VisitorIndex
+    };
+
+    typedef DistanceTableData::RealType RealType;
+    typedef DistanceTableData::PosType PosType;
+
+    Matrix<RealType> r_m;
+    Matrix<PosType>  dr_m;
+
+    inline DistTableOnSphere(const DistanceTableData& dt)
+    {
+      r_m.resize(dt.centers(),12);
+      dr_m.resize(dt.centers(),12);
+    }
+
+    inline void resize(int nknot)
+    {
+      this->resize(r_m.rows(), nknot);
+    }
+
+    inline void resize(int n, int nknot)
+    {
+      if(n)
+      {
+        r_m.resize(n,nknot);
+        dr_m.resize(n,nknot);
+      }
+      else
+        APP_ABORT("DistTableOnSphere initialized with an empty particleset");
+    }
+    virtual void makeMoves(int iel, const DistanceTableData& dt, const vector<PosType>& displs)=0;
+  };
+
+  /** proxy class to handle multiple moves
+   *
+   * Used by Non-local PP 
+   * This object cannot modify the target ParticleSet or its DistTables.
+   */
+  struct ParticleSetProxy
+  {
+
+    typedef ParticleSet::ParticlePos_t ParticlePos_t;
+    typedef ParticleSet::SingleParticlePos_t PosType;
+
+    ///particleset
+    const ParticleSet* myPtcl;
+    ///ID of the moved particle
+    int ActivePtcl;
+    ///species index for the active particle
+    int GroupID;
+    ///new positions of activePtcl
+    ParticlePos_t R;
+    ///matching tables
+    vector<DistTableOnSphere*>  myTables;
+
+    ParticleSetProxy(const ParticleSet* pt);
+    ~ParticleSetProxy();
+
+    inline void resize(int nknot)
+    {
+      R.resize(nknot);
+      for(int t=0; t<myTables.size(); ++t)
+        myTables[t]->resize(nknot);
+    }
+
+    template<typename PA>
+      inline void makeMoves(ParticleSet& P, int iat, const PA& displs)
+      {
+        ActivePtcl=iat;
+        GroupID=myPtcl->GroupID[iat];
+        for(int k=0; k<displs.size(); ++k) R[k]=P.R[iat]+displs[k];
+
+        for(int t=0; t<myTables.size(); ++t)
+          myTables[t]->makeMoves(iat, *(P.DistTables[t]), displs);
+      }
+  };
+
+}
+#endif
+/***************************************************************************
+ * $RCSfile$   $Author: jnkim $
+ * $Revision: 5545 $   $Date: 2012-07-10 08:04:38 -0500 (Tue, 10 Jul 2012) $
+ * $Id: ParticleSetProxy.h 5545 2012-07-10 13:04:38Z jnkim $ 
+ ***************************************************************************/
