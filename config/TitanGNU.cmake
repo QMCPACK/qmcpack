7a57fb2eed54e4f9ccb4f14aa44e13161f390429
Jeongnim Kim <jeongnim.kim@intel.com>
2013-01-18 22:12:35 +0000

    Update GNU toolchain on Titian. /ccs/home/jnkim/titan has binaries and libxml2 library with gnu4.7.2.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5661 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/TitanGNU.cmake
+++ config/TitanGNU.cmake
@@ -1,11 +1,12 @@
 SET(CMAKE_SYSTEM_PROCESSOR "XK6")
 #2011-12-06
 
-set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/5.11/bin/cc)
-set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/5.11/bin/CC)
-set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline")
+set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/default/bin/cc)
+set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/default/bin/CC)
+set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DDISABLE_TIMER=1 ") 
+#set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DUSE_REAL_STRUCT_FACTOR -DDISABLE_TIMER=1 -DHAVE_FMA4=1 -DHAVE_AMDLIBM=1")
 set(GNU_FLAGS "-malign-double -fomit-frame-pointer -ffast-math -fopenmp -O3 -Drestrict=__restrict__ -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
-set(XT_FLAGS "-march=bdver1 -msse3 -D_CRAYMPI")
+set(XT_FLAGS "-march=bdver1 -D_CRAYMPI -DHAVE_FMA4=1")
 #set(XT_FLAGS "-msse3 -D_CRAYMPI")
 set(CMAKE_CXX_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -ftemplate-depth-60 ${GNU_OPTS}")
 set(CMAKE_C_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -std=c99")
@@ -17,6 +18,7 @@ SET(HAVE_SSE 1)
 SET(HAVE_SSE2 1)
 SET(HAVE_SSE3 1)
 SET(HAVE_SSSE3 1)
+SET(HAVE_SSE41 1)
 SET(USE_PREFETCH 1)
 SET(PREFETCH_AHEAD 12)
 
@@ -36,9 +38,10 @@ set(CMAKE_FIND_ROOT_PATH
   /opt/cray/hdf5/1.8.8/gnu/47
   /opt/fftw/3.3.0.0/interlagos
   /sw/xk6/boost/1.44.0/cle4.0_gnu4.5.3
-  /ccs/proj/mat034/jnkim/xk6/gnu45/libxml2
+  /ccs/home/jnkim/titan/gnu47/libxml2
 )
 
-#set(EINSPLINE_SSE_BUG 1)
-set(HAVE_EINSPLINE 1)
-set(HAVE_EINSPLINE_EXT 0)
+#AMD math lib
+include_directories(/ccs/home/jnkim/lib/amdlibm/include)
+link_libraries(/ccs/home/jnkim/lib/amdlibm/lib/static/libamdlibm.a)
+
