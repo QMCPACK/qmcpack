3ffad231213aba363f78bd87b8abefe68ff76368
Jeongnim Kim <jeongnim.kim@intel.com>
2013-03-13 14:54:40 +0000

    Update generic toolchain with Intel Cluster Suite on Linux. Tested on PSIs at ONRL.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5747 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/LinuxIntel.cmake
+++ config/LinuxIntel.cmake
@@ -4,22 +4,21 @@
 #--------------------------------------------------------------------------
 # setting compilers, compiler options and MKL_HOME
 #--------------------------------------------------------------------------
-set(CMAKE_CXX_COMPILER mpicxx)
+set(CMAKE_CXX_COMPILER mpiicpc)
 set(CMAKE_C_COMPILER  icc)
-set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline")
-set(INTEL_OPTS "-g  -restrict -unroll  -O3 -ip -xT -openmp -Wno-deprecated")
+set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DDISABLE_TIMER")
+#set(INTEL_OPTS "-g  -restrict -unroll  -O3 -ip  -xSSE4.2 -openmp -Wno-deprecated")
+set(INTEL_OPTS "-g  -restrict -unroll  -O3 -ip  -xAVX -openmp -Wno-deprecated")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${INTEL_OPTS} ${GNU_OPTS}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${INTEL_OPTS} -std=c99")
-set(MKL_HOME "/opt/intel/mkl/10.0.3.020" CACHE STRING "MKL HOME")
 
 #--------------------------------------------------------------------------
 # path where the libraries are located
 # boost,hdf,szip,libxml2,fftw,essl
 #--------------------------------------------------------------------------
 set(CMAKE_FIND_ROOT_PATH
-    $ENV{EINSPLINE_HOME}
-    $ENV{BOOST_HOME}
-    $ENV{HDF5_HOME}
+ /opt/apps/gnu44/fftw-3.3.2
+ /opt/apps/gnu44/hdf5-1.8.10
 )
 
 #--------------------------------------------------------------------------
@@ -31,14 +30,14 @@ set(HAVE_SSE 1)
 set(HAVE_SSE2 1)
 set(HAVE_SSE3 1)
 set(HAVE_SSSE3 1)
+set(HAVE_SSE41 0)
 set(USE_PREFETCH 1)
 set(PREFETCH_AHEAD 10)
 set(HAVE_MKL 1)
 set(HAVE_MKL_VML 1)
 
-include_directories(${MKL_HOME}/include)
-set(LAPACK_LIBRARY -L${MKL_HOME}/lib/em64t -lmkl_lapack)
-set(BLAS_LIBRARY -L${MKL_HOME}/lib/em64t -lmkl -lguide)
+include_directories($ENV{MKLROOT}/include)
+link_libraries(-L$ENV{MKLROOT}/lib/intel64 -mkl=sequential)
 
 INCLUDE(Platform/UnixPaths)
 
