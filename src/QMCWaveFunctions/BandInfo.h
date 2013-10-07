a5e8af39c75efb2744998ced42fc8eeaaf849410
Jeongnim Kim <jeongnim.kim@intel.com>
2013-10-07 16:04:17 +0000

    BandInfo will be taken out of EinsplineSetBuilder for use by other BasisSetBuilder classes.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@5992 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCWaveFunctions/BandInfo.h
@@ -0,0 +1,64 @@
+//////////////////////////////////////////////////////////////////
+// (c) Copyright 2007-  by Ken Esler and Jeongnim Kim
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//   e-mail: jeongnim.kim@gmail.com
+//
+// Supported by
+//   National Center for Supercomputing Applications, UIUC
+//   Materials Computation Center, UIUC
+//////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file BandInfo.h
+ *
+ * Define helper class to sort bands according to the band and twist and functions
+ */
+#ifndef QMCPLUSPLUS_BANDINFO_H
+#define QMCPLUSPLUS_BANDINFO_H
+
+namespace qmcplusplus
+{
+
+struct BandInfo
+{
+  ///twist index
+  int TwistIndex;
+  ///band index 
+  int BandIndex;
+  /// band group index
+  int BandGroup;
+  ///spin index 
+  int Spin;
+  ///energy associated with this band
+  double Energy;
+  ///  This is true if we should make distinct copies represeninting a +k, -k pair
+  bool MakeTwoCopies;
+  /// True if this state is a core state
+  bool IsCoreState;
+  ///default constructor
+  BandInfo()
+    :TwistIndex(0),BandIndex(-1),BandGroup(0),Spin(0),Energy(1e9),MakeTwoCopies(false),IsCoreState(false)
+  { }
+
+  /** operator to determine the order of any band
+   * 
+   * - energy
+   * - twist index if the energies are similar
+   */
+  inline bool operator<(BandInfo other) const
+  {
+    if  ((Energy < other.Energy+1e-6)&&(Energy > other.Energy-1e-6))
+    {
+      if (TwistIndex == other.TwistIndex)
+        return BandIndex<other.BandIndex;
+      else
+        return TwistIndex < other.TwistIndex;
+    }
+    else
+      return Energy < other.Energy;
+  }
+};
+
+}
+
+#endif
