1d9c0e7c1a4bc600ea37f10c402a91d230f79e6a
Luke Shulenburger <lshulen@sandia.gov>
2013-10-22 15:56:41 +0000

    Make test for TwistNum<0 more robust
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@6012 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/QMCWaveFunctions/EinsplineSetBuilderCommon.cpp
+++ src/QMCWaveFunctions/EinsplineSetBuilderCommon.cpp
@@ -420,10 +420,13 @@ EinsplineSetBuilder::AnalyzeTwists2()
   }
   if(TwistNum<0)
   {
-    float eps=1e-4;
-    for (int si=0; si<numSuperTwists; si++)
-      if((abs(givenTwist[0]-superFracs[si][0])<eps) and (abs(givenTwist[1]-superFracs[si][1])<eps) and (abs(givenTwist[2]-superFracs[si][2])<eps))
-        TwistNum=si;
+    PosType gtFrac = FracPart(givenTwist);
+    float eps=1e-6;
+    for (int si=0; si<numSuperTwists; si++) {
+      PosType locDiff = gtFrac - superFracs[si];
+      if (dot(locDiff,locDiff) < eps)
+	TwistNum=si;
+    }
   }
   // Check supertwist for this node
   if (!myComm->rank())
