980505e87b90853edc012664a7e80a00eec06ef7
Paul Kent <kentpr@ornl.gov>
2014-07-29 21:14:33 +0000

    Bugfix for exported subversion number Updated CMakeLists for safer handling of svn absence Updated release script to edit version numbers in CMakeLists
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@6332 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- utils/make_qmcpack_release.sh
+++ utils/make_qmcpack_release.sh
@@ -22,14 +22,32 @@ mkdir /tmp/_tmp_qmcpack_release_work_dir
 cd /tmp/_tmp_qmcpack_release_work_dir
 svn export https://subversion.assembla.com/svn/qmcdev/trunk qmcpack >export.log
 if [ -e qmcpack/utils/energy.pl ]; then
+
+# Exclude this script from the release tarball
+rm -f qmcpack/utils/make_qmcpack_release.sh
+
 therev=`tail -1 export.log|sed -e 's/Exported revision //g' -e 's/\.//g'`
-echo Parsed revision $rev
+echo Parsed revision $therev
+thedatedashed=`date "+%Y-%m-%d"`
 thedate=`date "+%Y%m%d"`
 echo Date is $thedate
 
+# Fragile editing of version and dates in CMakeLists.txt
+# Requires an exact match
+cd qmcpack
+cp -p -f CMakeLists.txt CMakeLists.txt_orig
+oldrevstr='QMCPLUSPLUS_BRANCH "00000"'
+newrevstr='QMCPLUSPLUS_BRANCH "'$therev'"'
+olddatestr='QMCPLUSPLUS_LAST_CHANGED_DATE "2001-01-01"'
+newdatestr='QMCPLUSPLUS_LAST_CHANGED_DATE "'$thedatedashed'"'
+sed -e "s/$oldrevstr/$newrevstr/g" -e "s/$olddatestr/$newdatestr/g" CMakeLists.txt_orig >CMakeLists.txt
+#diff CMakeLists.txt_orig CMakeLists.txt
+rm -f CMakeLists.txt_orig
+cd ..
+
 OS=`uname`
 if [ "$OS" == "Darwin" ]; then
-echo Using Mac gnutar workaround to avoid extended attributes
+echo Using Mac gnutar workaround to avoid extended attributes. Install from macports
 gnutar cvf qmcpack_rev${therev}_${thedate}.tar qmcpack
 else
 tar cvf qmcpack_rev${therev}_${thedate}.tar qmcpack
