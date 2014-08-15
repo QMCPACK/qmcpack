29cc248c0193591771690403ab05a3bf131c4b6a
Norbert Podhorszki <pnorbert@ornl.gov>
2014-08-15 13:27:18 +0000

    Updated ADIOS build config files.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@6343 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- config/ChesterADIOSDebug.cmake
+++ config/ChesterADIOSDebug.cmake
@@ -2,11 +2,11 @@ SET(CMAKE_SYSTEM_PROCESSOR "XK7")
 #2011-12-06
 
 set(HAVE_ADIOS 1)
-set(CMAKE_C_COMPILER  /opt/cray/xt-asyncpe/default/bin/cc)
-set(CMAKE_CXX_COMPILER  /opt/cray/xt-asyncpe/default/bin/CC)
+set(CMAKE_C_COMPILER  cc)
+set(CMAKE_CXX_COMPILER  CC)
 set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DDISABLE_TIMER=1 -DUSE_REAL_STRUCT_FACTOR") 
 #set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DUSE_REAL_STRUCT_FACTOR -DDISABLE_TIMER=1 -DHAVE_FMA4=1 -DHAVE_AMDLIBM=1")
-set(GNU_FLAGS "-malign-double -fomit-frame-pointer -ffast-math -fopenmp -O3 -Drestrict=__restrict__ -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated")
+set(GNU_FLAGS "-g -O0 -malign-double -fomit-frame-pointer -ffast-math -fopenmp -Drestrict=__restrict__ -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated")
 set(XT_FLAGS "-march=bdver1 -D_CRAYMPI -DHAVE_FMA4=1 -DHAVE_AMDLIBM=1")
 #set(XT_FLAGS "-msse3 -D_CRAYMPI")
 set(CMAKE_CXX_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -ftemplate-depth-60 ${GNU_OPTS}")
@@ -37,7 +37,7 @@ ENDFOREACH(type)
 
 set(CMAKE_FIND_ROOT_PATH
   /opt/cray/hdf5/1.8.8/gnu/47
-  /opt/fftw/3.3.0.3/interlagos
+  /opt/fftw/3.3.0.3/x86_64/lib
   /sw/xk6/boost/1.44.0/cle4.0_gnu4.5.3
   /ccs/home/jnkim/titan/gnu47/libxml2
 )
@@ -47,21 +47,15 @@ include_directories(/ccs/home/jnkim/lib/amdlibm/include)
 link_libraries(/ccs/home/jnkim/lib/amdlibm/lib/static/libamdlibm.a)
 
 include_directories(
-  #/ccs/home/zgu/adioshub/adios_titan/include
-  #/sw/xk6/adios/1.6.0/cle4.0_gnu4.7.2/include
-  /ccs/proj/e2e/pnorbert/ADIOS/xk6.gnu/include
-  /sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/include 
-  /sw/xk6/dataspaces/1.4.0/cle4.2_gnu4.8.2/include 
-  /opt/cray/pmi/default/include 
-  /opt/cray/gni-headers/default/include
-  #/ccs/home/zgu/adioshub/adios_titan/include
-  /opt/cray/pmi/default/include
-  /opt/cray/gni-headers/default/include
+  /ccs/proj/e2e/pnorbert/ADIOS/chester.gnu/include
+  #/opt/cray/pmi/default/include 
+  #/opt/cray/gni-headers/default/include
+  #/sw/xk6/adios/1.6.0/cle4.0_gnu4.7.2/include 
+  #/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/include 
+  #/sw/xk6/dataspaces/1.3.0/cle4.0_gnu4.7.2/include 
   )
 link_libraries(
--L/ccs/proj/e2e/pnorbert/ADIOS/xk6.gnu/lib -ladios -L/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/lib -lmxml -L/sw/xk6/dataspaces/1.4.0/cle4.2_gnu4.8.2/lib -L/sw/xk6/dataspaces/1.4.0/cle4.2_gnu4.8.2/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/opt/cray/pmi/default/lib64 -L/opt/cray/ugni/default/lib -lm -lmxml -ldspaces -ldscommon -ldart -ldspaces -ldscommon -ldart -levpath -lffs -latl -ldill -lcercs_env -lpmi -lugni -lz -lbz2 -llustreapi
-#  -L/ccs/home/zgu/adioshub/adios_titan/lib -ladios -lm
-#  -L/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/lib -lmxml
+-L/ccs/proj/e2e/pnorbert/ADIOS/chester.gnu/lib -ladios -L/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/lib -lmxml -L/sw/xk6/dataspaces/1.4.0/cle4.2_gnu4.8.2/lib -L/sw/xk6/dataspaces/1.4.0/cle4.2_gnu4.8.2/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/ccs/proj/e2e/chaos/titan/gnu/lib -L/opt/cray/pmi/default/lib64 -L/opt/cray/ugni/default/lib -lm -lmxml -ldspaces -ldscommon -ldart -ldspaces -ldscommon -ldart -levpath -lffs -latl -ldill -lcercs_env -lpmi -lugni
   #-L/sw/xk6/adios/1.6.0/cle4.0_gnu4.7.2/lib -ladios -L/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/lib -lmxml -L/sw/xk6/dataspaces/1.3.0/cle4.0_gnu4.7.2/lib -L/sw/xk6/dataspaces/1.3.0/cle4.0_gnu4.7.2/lib -L/opt/cray/pmi/default/lib64 -L/opt/cray/ugni/default/lib64 -lm -lmxml -ldspaces -ldscommon -ldart -ldspaces -ldscommon -ldart -lpmi -lugni
   #-L/ccs/home/zgu/adioshub/adios_titan/lib -ladios -lm
   #-L/sw/xk6/mxml/2.6/cle4.0_gnu4.5.3/lib -lmxml
--- config/RheaOMPI_ADIOS.cmake
+++ config/RheaOMPI_ADIOS.cmake
@@ -18,19 +18,16 @@
 
 #export HDF5_HOME=/sw/redhat6/hdf5-parallel/1.8.11/rhel6.4_gnu4.7.1
 
-SET(CMAKE_SYSTEM_PROCESSOR "XK7")
+SET(CMAKE_SYSTEM_PROCESSOR "x86")
 
-#set(HAVE_ADIOS 0)
+set(HAVE_ADIOS 1)
 
-#set(ADIOS_HOME /ccs/home/zgu/adioshub/adios_sith)
-set(ADIOS_HOME /ccs/proj/e2e/pnorbert/ADIOS/sith.gnu)
+#set(ADIOS_HOME /sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1)
+set(ADIOS_HOME /ccs/proj/e2e/pnorbert/ADIOS/rhea.gnu)
+#set(ADIOS_HOME ${ADIOS_DIR})
 set(ADIOS_INCLUDES ${ADIOS_HOME}/include)
 set(ADIOS_LIBRARY ${ADIOS_HOME}/lib/libadios.a)
-#set(MXML_LIBRARY /sw/redhat6/mxml/2.6/rhel6_gnu4.7.1/lib/libmxml.a)
 set(MXML_LIBRARY /sw/redhat6/mxml/2.7/rhel6_gnu4.7.1/lib/libmxml.a)
-set(ISOBAR_LIBRARY /ccs/proj/e2e/ncsu/sith.gnu/lib/libisobar.a)
-set(APLOD_LIBRARY /ccs/proj/e2e/ncsu/sith.gnu/lib/libaplod.a) 
-set(BZ2_LIBRARY /sw/sith/bzip2/1.0.6/rhel6_gnu4.7.1/lib/libbz2.a)
 #set(DSPACES_LIBRARY ${DATASPACES_DIR}/lib/libdspaces.a)
 #set(DSCOMMON_LIBRARY ${DATASPACES_DIR}/lib/libdscommon.a)
 #set(DART_LIBRARY ${DATASPACES_DIR}/lib/libdart.a)
@@ -43,11 +40,11 @@ set(LUSTREAPI_LIBRARY /usr/lib64/liblustreapi.a)
 set(IBVERBS_LIBRARY /usr/lib64/libibverbs.so)
 
 set(CMAKE_C_COMPILER  gcc)
-set(CMAKE_CXX_COMPILER  /sw/sith/ompi/1.6.5/rhel6_gnu4.7.1/bin/mpicxx)
+set(CMAKE_CXX_COMPILER  mpiCC)
 set(GNU_OPTS "-DADD_ -DINLINE_ALL=inline -DDISABLE_TIMER=1 -DUSE_REAL_STRUCT_FACTOR") 
 set(GNU_FLAGS "-malign-double -fomit-frame-pointer -ffast-math -fopenmp -O3 -Drestrict=__restrict__ -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
-#set(GNU_FLAGS "-g -O0 -malign-double -fomit-frame-pointer -ffast-math -fopenmp -Drestrict=__restrict__ -finline-limit=1000 -fstrict-aliasing -funroll-all-loops -Wno-deprecated ")
-set(XT_FLAGS "-march=amdfam10 -msse3")
+set(XT_FLAGS "-msse3")
+#set(XT_FLAGS "-march=amdfam10 -msse3")
 #set(XT_FLAGS "-march=bdver1 -D_CRAYMPI -DHAVE_FMA4=1 -DHAVE_AMDLIBM=1")
 set(CMAKE_CXX_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -ftemplate-depth-60 ${GNU_OPTS}")
 set(CMAKE_C_FLAGS "${XT_FLAGS} ${GNU_FLAGS} -std=c99")
@@ -76,26 +73,20 @@ set(CMAKE_SHARED_LINKER_FLAGS "")
 #ENDFOREACH(type)
 
 set(CMAKE_FIND_ROOT_PATH
-  /sw/redhat6/hdf5-parallel/1.8.11/rhel6.4_gnu4.7.1
-  /sw/redhat6/szip/2.1/rhel6_gnu4.7.1
-  #/ccs/home/zgu/adioshub/adios_sith
-  /ccs/proj/e2e/pnorbert/ADIOS/sith.gnu
-  #/sw/redhat6/dataspaces/1.2.0/rhel6_gnu4.7.1
-  /sw/sith/netcdf/4.1.3/rhel6_gnu4.7.1
-  #/sw/redhat6/adios/1.6.0/rhel6_gnu4.7.1
-  #/ccs/home/zgu/Software_sith
   /sw/redhat6/libxml2/2.9.1/rhel6_gnu4.7.1
   /sw/redhat6/fftw/3.3.3/rhel6.4_gnu4.7.1
-  #/ccs/proj/e2e/pnorbert/qmcpack/Software_sith_cynthia/
+  /sw/redhat6/hdf5/1.8.11/rhel6.4_gnu4.7.1
+  /sw/redhat6/szip/2.1/rhel6_gnu4.7.1
+  /ccs/proj/e2e/pnorbert/ADIOS/rhea.gnu
+  #/sw/redhat6/adios/1.6.0/rhel6_gnu4.7.1
+  #/ccs/home/zgu/Software_sith
+)
+
+include_directories(
+  /ccs/proj/e2e/pnorbert/ADIOS/rhea.gnu
 )
 
 link_libraries(/sw/redhat6/acml/5.3.0/gfortran64/lib/libacml.a -lz)
-link_libraries(/ccs/compilers/gcc/rhel6-x86_64/4.7.1/lib64/libgfortran.so)
+#link_libraries(/ccs/compilers/gcc/rhel6-x86_64/4.7.1/lib64/libgfortran.so)
 #link_libraries(-L/sw/redhat6/adios/1.6.0/rhel6_gnu4.7.1/lib -ladios -L/sw/sith/mxml/2.6/rhel6_gnu4.7.1/lib -lmxml -L/sw/redhat6/dataspaces/1.3.0/rhel6_gnu4.7.1/lib -L/usr/lib64/lib -L/lib64/lib -L/lib64/lib64 -L/lib -L/sw/redhat6/adios/1.6.0/rhel6_gnu4.7.1/lib -lm -lmxml -lpthread -ldspaces -ldscommon -ldart -lrdmacm -libverbs -llustreapi -lz -lbz2 -lisobar -lz)
-#link_libraries(/ccs/home/zgu/adioshub/adios_sith/lib/libadios.a)
-link_libraries(/sw/redhat6/libxml2/2.9.1/rhel6_gnu4.7.1/lib/libxml2.a)
-link_libraries(/sw/redhat6/fftw/3.3.3/rhel6.4_gnu4.7.1/lib/libfftw3.a)
-link_libraries(/ccs/proj/e2e/pnorbert/ADIOS/sith.gnu/lib/libadios.a)
-link_libraries(/ccs/proj/e2e/ncsu/sith.gnu/lib/libisobar.a)
-link_libraries(/ccs/proj/e2e/ncsu/sith.gnu/lib/libaplod.a)
-link_libraries(/sw/sith/bzip2/1.0.6/rhel6_gnu4.7.1/lib/libbz2.a)
+link_libraries(${ADIOS_HOME}/lib/libadios.a)
