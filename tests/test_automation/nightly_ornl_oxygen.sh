bab67c0e768d655021e9fda33f277eeb815c1774
Paul Kent <kentpr@ornl.gov>
2016-01-23 15:38:10 +0000

    Path fix to find OpenMPI in GCC tests on oxygen
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@6707 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- tests/test_automation/nightly_ornl_oxygen.sh
+++ tests/test_automation/nightly_ornl_oxygen.sh
@@ -32,12 +32,12 @@ mkdir $sys
 cd $sys
 echo --- Building for $sys `date`
 
-export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/cuda/bin/:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=/opt/local/bin:/opt/local/sbin:/usr/local/cuda/bin/:/usr/lib64/openmpi/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 case $sys in
 "build_gcc")
-    module() { eval `/usr/bin/modulecmd sh $*`; }
-    module load mpi
+#    module() { eval `/usr/bin/modulecmd sh $*`; }
+#    module load mpi
     export QMCPACK_TEST_SUBMIT_NAME=GCC-Release
     ctest -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -VV
     ;;
@@ -54,8 +54,8 @@ case $sys in
     ctest -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -VV
     ;;
 "build_gcc_complex")
-    module() { eval `/usr/bin/modulecmd sh $*`; }
-    module load mpi
+#    module() { eval `/usr/bin/modulecmd sh $*`; }
+#    module load mpi
     export QMCPACK_TEST_SUBMIT_NAME=GCC-Complex-Release
     ctest -DQMC_COMPLEX=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -VV
     ;;
@@ -72,8 +72,8 @@ case $sys in
     ctest -DQMC_COMPLEX=1 -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -VV
     ;;
 "build_gcc_cuda")
-    module() { eval `/usr/bin/modulecmd sh $*`; }
-    module load mpi
+#    module() { eval `/usr/bin/modulecmd sh $*`; }
+#    module load mpi
     export QMCPACK_TEST_SUBMIT_NAME=GCC-CUDA-Release
     ctest -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DQMC_CUDA=1 -S $PWD/../CMake/ctest_script.cmake,release -VV
     ;;
