533ea838b20205a3948b2ec6bc2251e798b94fb5
Anouar Benali <abenali.sci@gmail.com>
2016-03-05 02:04:04 +0000

    Fixing introduced bug on PPconvert
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@6814 e5b18d87-469d-4833-9cc0-8cdfa06e9491

---
+++ src/QMCTools/QPParser.h
@@ -0,0 +1,42 @@
+//#ifndef QMCPLUSPLUS_TOOLS_GAMESS_OUT_H
+//#define QMCPLUSPLUS_TOOLS_GAMESS_OUT_H
+#include "QMCTools/QMCGaussianParserBase.h"
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include "OhmmsPETE/TinyVector.h"
+#include "OhmmsData/OhmmsElementBase.h"
+
+class QPParser: public QMCGaussianParserBase,
+  public OhmmsAsciiParser
+{
+
+public:
+
+  QPParser();
+
+  QPParser(int argc, char** argv);
+
+  streampos pivot_begin;
+  vector<std::string> tags;
+  bool usingECP;
+  std::string MOtype;
+  int readtype, numAO;
+  int NFZC, NEXT, NTOT, NAC;
+
+  void parse(const std::string& fname);
+
+  void getGeometry(std::istream& is);
+
+  void getGaussianCenters(std::istream& is);
+
+  void getMO(std::istream& is);
+
+  void getMO_single_set(std::istream& is, Matrix<double> &CartMat, std::vector<value_type>& EigVal_alpha);
+
+  void getQPCI(std::istream& is);
+
+
+};
+//#endif
