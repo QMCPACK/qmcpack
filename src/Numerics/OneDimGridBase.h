8c7a5c574f5a4c4b89825624fa315d95d2e34b2f
Raymond Clay <j.k.rofling@gmail.com>
2016-11-09 23:37:14 +0000

    Fencepost error in getIndex() for OneDimGridBase.  Previously, if x was equal to last element of array, getIndex(x) would return next-to-last index.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@7217 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/Numerics/OneDimGridBase.h
+++ src/Numerics/OneDimGridBase.h
@@ -27,6 +27,7 @@
 #include <cmath>
 #include "OhmmsPETE/OhmmsVector.h"
 #include "Numerics/GridTraits.h"
+#include "einspline/bspline_base.h"
 
 namespace qmcplusplus
 {
@@ -85,7 +86,7 @@ struct OneDimGridBase
   {
     int k;
     int klo=0;
-    int khi=this->size()-1;
+    int khi=this->size();
     while(khi-klo > 1)
     {
       k=(khi+klo) >> 1;
@@ -339,7 +340,18 @@ struct LinearGrid: public OneDimGridBase<T,CT>
     for(int i=0; i<n; i++)
       X[i] = ri+Delta_DP*i;
   }
+  
+  inline Ugrid einspline_grid()
+  {
+    Ugrid grid;
+    grid.start = lower_bound;
+    grid.end   = upper_bound;
+    grid.num   = num_points;
+    grid.delta = Delta;
+    grid.delta_inv= DeltaInv;
 
+    return grid;   
+  }
 };
 
 /** One-Dimensional logarithmic-grid.
@@ -507,7 +519,7 @@ struct NumericalGrid: public OneDimGridBase<T,CT>
   {
     int k;
     int klo=0;
-    int khi=num_points-1;
+    int khi=num_points;
     //int khi=this->size()-1;
     while(khi-klo > 1)
     {
