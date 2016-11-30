7f5cb73d4a69865645dc8d60c31e537cf496ac34
Ye Luo <xw111luoye@gmail.com>
2016-11-30 20:28:43 +0000

    A minor change in the helper script of BG.
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/trunk@7301 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- utils/bgrunjobhelper.sh
+++ utils/bgrunjobhelper.sh
@@ -8,6 +8,6 @@ echo "COBALT_PARTNAME " $COBALT_PARTNAME
 echo "MPI tasks       " $1
 echo "Threads         " $OMP_NUM_THREADS
 echo "Binary          " $2
-echo "Input files     " $3 $4 $5 $6 $7
+echo "Input files     " $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15
 
-runjob --np $1 -p 1 --block $COBALT_PARTNAME --envs OMP_NUM_THREADS=$OMP_NUM_THREADS : $2 $3 $4 $5 $6 $7
+runjob --np $1 -p 1 --block $COBALT_PARTNAME --envs OMP_NUM_THREADS=$OMP_NUM_THREADS : $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15
