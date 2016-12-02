ed43d4abf9255dc30f10744562bbf96e27ce0647
Mark Dewing <mdewing@anl.gov>
2016-12-02 16:31:13 +0000

    Remove explicit include of mpi.h.
    Now it builds in serial w/o MPI.
    
    
    git-svn-id: https://subversion.assembla.com/svn/qmcdev/branches/new_optimizer@7306 e5b18d87-469d-4833-9cc0-8cdfa06e9491

--- src/formic/utils/lmyengine/engine_timing.cpp
+++ src/formic/utils/lmyengine/engine_timing.cpp
@@ -15,7 +15,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
 
-#include <mpi.h>
+#include<formic/utils/mpi_interface.h>
 
 #include<formic/utils/lmyengine/engine_timing.h>
 #include<formic/utils/exception.h>
