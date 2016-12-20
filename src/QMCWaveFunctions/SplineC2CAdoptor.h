c17f45e2aabe916f6e0345fc6fdee14818738233
jeongnim <jeongnim.kim@intel.com>
2016-12-20 10:20:35 -0800

    Add C2C adoptor of SoA version.

--- src/QMCWaveFunctions/SplineC2CAdoptor.h
+++ src/QMCWaveFunctions/SplineC2CAdoptor.h
@@ -4,13 +4,9 @@
 //
 // Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
 //
-// File developed by: Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
-//                    Jeongnim Kim, jeongnim.kim@intel.com, University of Illinois at Urbana-Champaign
-//                    Ye Luo, yeluo@anl.gov, Argonne National Laboratory
-//                    Anouar Benali, benali@anl.gov, Argonne National Laboratory
-//                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+// File developed by: 
 //
-// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+// File created by: Jeongnim Kim, jeongnim.kim@intel.com, Intel Corp.
 //////////////////////////////////////////////////////////////////////////////////////
     
     
@@ -56,8 +52,6 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
   using BaseType::kPoints;
   using BaseType::MakeTwoCopies;
 
-  ///number of complex bands
-  int nComplexBands;
   ///number of points of the original grid
   int BaseN[3];
   ///offset of the original grid, always 0
@@ -82,7 +76,7 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
   {
     this->is_complex=true;
     this->AdoptorName="SplineC2CSoA";
-    this->KeyWord="C2RSoA";
+    this->KeyWord="C2CSoA";
   }
 
   ///** copy the base property */
@@ -95,7 +89,7 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
 
   SplineC2CSoA(const SplineC2CSoA& a):
     SplineAdoptorBase<ST,3>(a),SplineInst(a.SplineInst),MultiSpline(nullptr), 
-    nComplexBands(a.nComplexBands),mKK(a.mKK), myKcart(a.myKcart)
+    mKK(a.mKK), myKcart(a.myKcart)
   {
     const size_t n=a.myL.size();
     myV.resize(n); myG.resize(n); myL.resize(n); myH.resize(n);
@@ -182,7 +176,7 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
   template<typename VV>
   inline void assign_v(const PointType& r, int bc_sign, VV& psi)
   {
-    typedef typename psi::value_type ComplexT;
+    typedef std::complex<TT> ComplexT;
     const size_t N=kPoints.size();
     const ST x=r[0], y=r[1], z=r[2];
     const ST* restrict kx=myKcart.data(0);
@@ -194,7 +188,7 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
         KdotR[j]=-(x*kx[j]+y*ky[j]+z*kz[j]);
       eval_e2iphi(nk,KdotR.data(),CosV.data(),SinV.data());
     }
-    for (size_t j=0,psiIndex=first_spo; j<N; j++, psiIndex++)
+    for (size_t j=0, psiIndex=first_spo; psiIndex<last_spo; j++,psiIndex++)
     {
       const ST val_r=myV[2*j  ];
       const ST val_i=myV[2*j+1];
@@ -202,8 +196,8 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
     }
 #else
     ST s, c;
-#pragma omp simd private(s,c)
-    for (size_t j=0,psiIndex=first_spo; j<N; j++, psiIndex++)
+//#pragma omp simd private(s,c)
+    for (size_t j=0, psiIndex=first_spo; psiIndex<last_spo; j++,psiIndex++)
     {
       const ST val_r=myV[2*j  ];
       const ST val_i=myV[2*j+1];
@@ -221,19 +215,12 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
     assign_v(r,0,psi);
   }
 
-  template<typename VV, typename GV>
-  inline void evaluate_vgl(const PointType& r, VV& psi, GV& dpsi, VV& d2psi)
-  {
-    //DO NOT USE THESE PLEASE
-  }
-
-  /** identical to assign_vgl but the output container is SoA container
+  /** assign_vgl 
    */
-  template<typename VV, typename GL>
-  inline void assign_vgl_soa(const PointType& r, int bc_sign, VV& psi, GL& dpsi)
+  template<typename VV, typename GV>
+  inline void assign_vgl(const PointType& r, VV& psi, GV& dpsi, VV& d2psi)
   {
-    typedef typename VV::value_type ComplexT;
-
+    typedef std::complex<TT> ComplexT;
     CONSTEXPR ST zero(0);
     CONSTEXPR ST two(2);
     const ST g00=PrimLattice.G(0), g01=PrimLattice.G(1), g02=PrimLattice.G(2),
@@ -264,12 +251,106 @@ struct SplineC2CSoA: public SplineAdoptorBase<ST,3>
     }
 #endif
 
-    ComplexT* restrict vg_x=dpsi.data(0); ASSUME_ALIGNED(vl_x);
-    ComplexT* restrict vg_y=dpsi.data(1); ASSUME_ALIGNED(vl_y);
-    ComplexT* restrict vg_z=dpsi.data(2); ASSUME_ALIGNED(vl_z);
+    for (size_t j=0, psiIndex=first_spo; psiIndex<last_spo; j++,psiIndex++)
+    {
+      const size_t jr=j<<1;
+      const size_t ji=jr+1;
+
+      const ST kX=k0[j];
+      const ST kY=k1[j];
+      const ST kZ=k2[j];
+      const ST val_r=myV[jr];
+      const ST val_i=myV[ji];
+
+      //phase
+      ST s, c;
+      sincos(-(x*kX+y*kY+z*kZ),&s,&c);
+
+      //dot(PrimLattice.G,myG[j])
+      const ST dX_r = g00*g0[jr]+g01*g1[jr]+g02*g2[jr];
+      const ST dY_r = g10*g0[jr]+g11*g1[jr]+g12*g2[jr];
+      const ST dZ_r = g20*g0[jr]+g21*g1[jr]+g22*g2[jr];
+
+      const ST dX_i = g00*g0[ji]+g01*g1[ji]+g02*g2[ji];
+      const ST dY_i = g10*g0[ji]+g11*g1[ji]+g12*g2[ji];
+      const ST dZ_i = g20*g0[ji]+g21*g1[ji]+g22*g2[ji];
+
+      // \f$\nabla \psi_r + {\bf k}\psi_i\f$
+      const ST gX_r=dX_r+val_i*kX;
+      const ST gY_r=dY_r+val_i*kY;
+      const ST gZ_r=dZ_r+val_i*kZ;
+      const ST gX_i=dX_i-val_r*kX;
+      const ST gY_i=dY_i-val_r*kY;
+      const ST gZ_i=dZ_i-val_r*kZ;
+
+#if defined(PRECOMPUTE_L)
+      const ST lap_r=myL[jr]+mKK[j]*val_r+two*(kX*dX_i+kY*dY_i+kZ*dZ_i);
+      const ST lap_i=myL[ji]+mKK[j]*val_i-two*(kX*dX_r+kY*dY_r+kZ*dZ_r);
+#else
+      const ST lcart_r=SymTrace(h00[jr],h01[jr],h02[jr],h11[jr],h12[jr],h22[jr],GGt.data());
+      const ST lcart_i=SymTrace(h00[ji],h01[ji],h02[ji],h11[ji],h12[ji],h22[ji],GGt.data());
+      const ST lap_r=lcart_r+mKK[j]*val_r+two*(kX*dX_i+kY*dY_i+kZ*dZ_i);
+      const ST lap_i=lcart_i+mKK[j]*val_i-two*(kX*dX_r+kY*dY_r+kZ*dZ_r);
+#endif
+      psi[psiIndex ]   = ComplexT(c*val_r-s*val_i,c*val_i+s*val_r);
+      dpsi[psiIndex][0]= ComplexT(c*gX_r -s*gX_i, c*gX_i +s*gX_r);
+      dpsi[psiIndex][1]= ComplexT(c*gY_r -s*gY_i, c*gY_i +s*gY_r);
+      dpsi[psiIndex][2]= ComplexT(c*gZ_r -s*gZ_i, c*gZ_i +s*gZ_r);
+      d2psi[psiIndex]  = ComplexT(c*lap_r-s*lap_i,c*lap_i+s*lap_r);
+    }
+  }
+
+
+  template<typename VV, typename GV>
+  inline void evaluate_vgl(const PointType& r, VV& psi, GV& dpsi, VV& d2psi)
+  {
+    PointType ru(PrimLattice.toUnit_floor(r));
+    SplineInst->evaluate_vgh(ru,myV,myG,myH);
+    assign_vgl(r,psi,dpsi,d2psi);
+  }
+
+  /** identical to assign_vgl but the output container is SoA container
+   */
+  template<typename VV, typename GL>
+  inline void assign_vgl_soa(const PointType& r, VV& psi, GL& dpsi)
+  {
+    typedef std::complex<TT> ComplexT;
+    CONSTEXPR ST zero(0);
+    CONSTEXPR ST two(2);
+    const ST g00=PrimLattice.G(0), g01=PrimLattice.G(1), g02=PrimLattice.G(2),
+             g10=PrimLattice.G(3), g11=PrimLattice.G(4), g12=PrimLattice.G(5),
+             g20=PrimLattice.G(6), g21=PrimLattice.G(7), g22=PrimLattice.G(8);
+    const ST x=r[0], y=r[1], z=r[2];
+
+    const ST* restrict k0=myKcart.data(0);
+    const ST* restrict k1=myKcart.data(1);
+    const ST* restrict k2=myKcart.data(2);
+
+    const ST* restrict g0=myG.data(0);
+    const ST* restrict g1=myG.data(1);
+    const ST* restrict g2=myG.data(2);
+    const ST* restrict h00=myH.data(0);
+    const ST* restrict h01=myH.data(1);
+    const ST* restrict h02=myH.data(2);
+    const ST* restrict h11=myH.data(3);
+    const ST* restrict h12=myH.data(4);
+    const ST* restrict h22=myH.data(5);
+
+    const size_t N=kPoints.size();
+    const size_t nsplines=myL.size();
+#if defined(PRECOMPUTE_L)
+    for(size_t j=0; j<nsplines; ++j)
+    {
+      myL[j]=SymTrace(h00[j],h01[j],h02[j],h11[j],h12[j],h22[j],GGt.data());
+    }
+#endif
+
+    ComplexT* restrict vg_x=dpsi.data(0); ASSUME_ALIGNED(vg_x);
+    ComplexT* restrict vg_y=dpsi.data(1); ASSUME_ALIGNED(vg_y);
+    ComplexT* restrict vg_z=dpsi.data(2); ASSUME_ALIGNED(vg_z);
     ComplexT* restrict vl_l=dpsi.data(3); ASSUME_ALIGNED(vl_l);
 
-    for (size_t j=0, psiIndex=first_spo; j<N; j++,psiIndex++)
+    for (size_t j=0, psiIndex=first_spo; psiIndex<last_spo; j++,psiIndex++)
     {
       const size_t jr=j<<1;
       const size_t ji=jr+1;
