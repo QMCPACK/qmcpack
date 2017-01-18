ca1515d016860b409886649b20463563d7ca92d4
Nichols A. Romero <naromero@anl.gov>
2017-01-18 13:51:14 -0600

    Update hyperion nightly script (part2)
    Fix paths and use flash drive.

--- tests/test_automation/nightly_alcf_hyperion.sh
+++ tests/test_automation/nightly_alcf_hyperion.sh
@@ -7,7 +7,7 @@ source /opt/intel/2017/parallel_studio_xe_2017.1.043/bin/psxevars.sh intel64
 timeout=1800
 
 # topdir must exist, otherwise it fails
-topdir=/home/wcrusher/ci
+topdir=/data/ci
 testdir=${topdir}/scratch/QMCPACK_CI_BUILDS_DO_NOT_REMOVE
 
 if [ -e $topdir ]; then
@@ -41,7 +41,7 @@ do
 
 echo --- Building for $sys `date`
 
-cd ${test_dir}
+cd ${testdir}
 
 if [ -e $sys ]; then
 rm -rf $sys
@@ -55,42 +55,42 @@ cd $sys
 case $sys in
     "build_gcc")
 	export QMCPACK_TEST_SUBMIT_NAME=GCC-MKL-Release
-	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_gcc_complex")
 	export QMCPACK_TEST_SUBMIT_NAME=GCC-MKL-Complex-Release
-	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_gcc_mixed")
 	export QMCPACK_TEST_SUBMIT_NAME=GCC-MKL-Mixed-Release
-	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_gcc_complex_mixed")
 	export QMCPACK_TEST_SUBMIT_NAME=GCC-MKL-Complex-Mixed-Release
-	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_intel2017")
 	source /opt/intel/2017/parallel_studio_xe_2017.1.043/bin/psxevars.sh intel64
 	export QMCPACK_TEST_SUBMIT_NAME=Intel2017-Release
-	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_intel2017_complex")
 	# intel compiler should already be loaded 
 	# source /opt/intel/2017/parallel_studio_xe_2017.1.043/bin/psxevars.sh intel64
 	export QMCPACK_TEST_SUBMIT_NAME=Intel2017-Complex-Release
-	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=0 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_intel2017_mixed")
 	# intel compiler should already be loaded
 	# source /opt/intel/2017/parallel_studio_xe_2017.1.043/bin/psxevars.sh intel64
 	export QMCPACK_TEST_SUBMIT_NAME=Intel2017-Mixed-Release
-	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=0 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     "build_intel2017_complex_mixed")
 	# intel compiler should already be loaded
 	# source /opt/intel/2017/parallel_studio_xe_2017.1.043/bin/psxevars.sh intel64
 	export QMCPACK_TEST_SUBMIT_NAME=Intel2017-Complex-Mixed-Release
-	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
+	ctest -DQMC_COMPLEX=1 -DQMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=mpiicpc -S $PWD/../qmcpack/CMake/ctest_script.cmake,release -E 'long' -VV --timeout $timeout
 	;;
     *)
 	echo "ERROR: Unknown build type $sys"
