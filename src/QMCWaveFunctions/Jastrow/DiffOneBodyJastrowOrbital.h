ac88ef70804c18c5e912bf17cb848de1148da114
jeongnim <jeongnim.kim@intel.com>
2017-01-27 15:39:56 -0800

    Iplement SOA version for diff classes. Not tested.

--- src/QMCWaveFunctions/Jastrow/DiffOneBodyJastrowOrbital.h
+++ src/QMCWaveFunctions/Jastrow/DiffOneBodyJastrowOrbital.h
@@ -57,7 +57,9 @@ public:
     :CenterRef(centers),NumVars(0)
   {
     NumPtcls=els.getTotalNum();
+    std::cout << "HELPME " << std::endl;
     myTableIndex=els.addTable(CenterRef,DT_SOA_PREFERRED);
+    std::cout << "HELPME " << std::endl;
   }
 
   ~DiffOneBodyJastrowOrbital()
@@ -158,33 +160,78 @@ public:
       for (int p=0; p<NumVars; ++p)
         (*lapLogPsi[p])=0.0;
       std::vector<TinyVector<RealType,3> > derivs(NumVars);
-      for (int i=0; i<d_table->size(SourceIndex); ++i)
+
+      if(d_table->DTType == DT_SOA)
       {
-        FT* func=Fs[i];
-        if (func == 0)
-          continue;
-        int first(OffSet[i].first);
-        int last(OffSet[i].second);
-        bool recalcFunc(false);
-        for (int rcs=first; rcs<last; rcs++)
-          if (rcsingles[rcs]==true)
-            recalcFunc=true;
-        if (recalcFunc)
+        constexpr RealType cone(1);
+        constexpr RealType lapfac(OHMMS_DIM-cone);
+        const size_t ns=d_table->size(SourceIndex);
+        const size_t nt=P.getTotalNum();
+
+        aligned_vector<int> iadj(nt);
+        aligned_vector<RealType> dist(nt);
+        std::vector<PosType> displ(nt);
+
+        for(size_t i=0; i<ns; ++i)
         {
-          for (int nn=d_table->M[i]; nn<d_table->M[i+1]; ++nn)
+          FT* func=Fs[i];
+          if (func == 0) continue;
+          int first(OffSet[i].first);
+          int last(OffSet[i].second);
+          bool recalcFunc(false);
+          for (int rcs=first; rcs<last; rcs++)
+            if (rcsingles[rcs]==true) recalcFunc=true;
+          if (recalcFunc)
           {
-            std::fill(derivs.begin(),derivs.end(),0.0);
-            if (!func->evaluateDerivatives(d_table->r(nn),derivs))
-              continue;
-            int j = d_table->J[nn];
-            RealType rinv(d_table->rinv(nn));
-            PosType dr(d_table->dr(nn));
-            for (int p=first, ip=0; p<last; ++p,++ip)
+            size_t nn=d_table->get_neighbors(i,func->cutoff_radius,iadj.data(),dist.data(),displ.data());
+            for(size_t nj=0; nj<nn; ++nj)
             {
-              dLogPsi[p] -= derivs[ip][0];
-              RealType dudr(rinv*derivs[ip][1]);
-              (*gradLogPsi[p])[j] -= dudr*dr;
-              (*lapLogPsi[p])[j] -= derivs[ip][2]+2.0*dudr;
+              std::fill(derivs.begin(),derivs.end(),0);
+              if (!func->evaluateDerivatives(dist[nj],derivs)) continue;
+              int j = iadj[nj];
+              RealType rinv(cone/dist[nj]);
+              PosType& dr=displ[nj];
+              for (int p=first, ip=0; p<last; ++p,++ip)
+              {
+                dLogPsi[p] -= derivs[ip][0];
+                RealType dudr(rinv*derivs[ip][1]);
+                (*gradLogPsi[p])[j] -= dudr*dr;
+                (*lapLogPsi[p])[j] -= derivs[ip][2]+lapfac*dudr;
+              }
+            }
+          }
+        }
+      }
+      else
+      {
+        for (int i=0; i<d_table->size(SourceIndex); ++i)
+        {
+          FT* func=Fs[i];
+          if (func == 0)
+            continue;
+          int first(OffSet[i].first);
+          int last(OffSet[i].second);
+          bool recalcFunc(false);
+          for (int rcs=first; rcs<last; rcs++)
+            if (rcsingles[rcs]==true)
+              recalcFunc=true;
+          if (recalcFunc)
+          {
+            for (int nn=d_table->M[i]; nn<d_table->M[i+1]; ++nn)
+            {
+              std::fill(derivs.begin(),derivs.end(),0.0);
+              if (!func->evaluateDerivatives(d_table->r(nn),derivs))
+                continue;
+              int j = d_table->J[nn];
+              RealType rinv(d_table->rinv(nn));
+              PosType dr(d_table->dr(nn));
+              for (int p=first, ip=0; p<last; ++p,++ip)
+              {
+                dLogPsi[p] -= derivs[ip][0];
+                RealType dudr(rinv*derivs[ip][1]);
+                (*gradLogPsi[p])[j] -= dudr*dr;
+                (*lapLogPsi[p])[j] -= derivs[ip][2]+2.0*dudr;
+              }
             }
           }
         }
