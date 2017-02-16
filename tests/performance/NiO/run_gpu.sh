466144eb4fe23d8f4fd7136c6f168501c3378b57
Ye Luo <yeluo@anl.gov>
2017-02-16 15:57:50 -0600

    Update NiO test README and test files.

--- tests/performance/NiO/run_gpu.sh
+++ tests/performance/NiO/run_gpu.sh
@@ -1,10 +1,12 @@
-script=qmcpack-gpu.sub
+script=qmcpack-gpu-cooley.sub
 walker=128
+appendix=n2-w$walker-K80
+subjob=qsub
 
 for size in 8 16 32 64
 do
   source_folder=dmc-S$size-gpu
-  folder=$source_folder-n2-w$walker-K80
+  folder=$source_folder-${appendix}
   echo $folder
   mkdir $folder
   cp sample/$source_folder/* $folder/
@@ -12,6 +14,6 @@ do
   cd $folder
   sed -i "s/XX/$size/" $script
   sed -i "/walkers/s/32/$walker/" *.xml
-  qsub $script
+  $subjob $script
   cd ..
 done
