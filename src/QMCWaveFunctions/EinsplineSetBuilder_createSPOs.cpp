6822f8c276ae7085e88767f2bfc05dd2c65df8d9
Ye Luo <yeluo@anl.gov>
2017-03-03 12:36:58 -0600

    Correct a bug and set hybridrep default off.

--- src/QMCWaveFunctions/EinsplineSetBuilder_createSPOs.cpp
+++ src/QMCWaveFunctions/EinsplineSetBuilder_createSPOs.cpp
@@ -70,7 +70,7 @@ EinsplineSetBuilder::createSPOSetFromXML(xmlNodePtr cur)
   std::string sourceName;
   std::string spo_prec("double");
   std::string truncate("no");
-  std::string hybrid_rep("yes");
+  std::string hybrid_rep("no");
 #if defined(QMC_CUDA)
   std::string useGPU="yes";
 #else
