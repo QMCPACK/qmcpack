a5ee10d73843a323e52ea155529ec167676641f2
Ye Luo <yeluo@anl.gov>
2017-04-14 17:33:00 -0500

    Improve memory report based on system calls.

--- src/Platforms/sysutil.h
+++ src/Platforms/sysutil.h
@@ -32,24 +32,8 @@ std::string getDateAndTime();
  */
 std::string getDateAndTime(const char* format);
 
-#ifdef __linux__
-#include "sys/sysinfo.h"
+size_t freemem();
 
-inline size_t freemem()
-{
-  struct sysinfo si;
-  sysinfo(&si);
-  si.freeram+=si.bufferram;
-  return si.freeram>>20;
-  //return (si.freeram + si.bufferram);
-}
-#else
-
-inline size_t freemem()
-{
-  return 0;
-}
-
-#endif
+void print_mem(const char* title, std::ostream& log);
 
 #endif
