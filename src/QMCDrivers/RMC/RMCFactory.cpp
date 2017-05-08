15e52789fc5da31c2da9cc0de1fe3b05a9449027
Ye Luo <yeluo@anl.gov>
2017-05-08 16:36:30 -0500

    Guard running RMC on GPU and avoid tests.

--- src/QMCDrivers/RMC/RMCFactory.cpp
+++ src/QMCDrivers/RMC/RMCFactory.cpp
@@ -30,6 +30,11 @@ namespace qmcplusplus
     int np = omp_get_max_threads ();
     //(SPACEWARP_MODE,MULTIPE_MODE,UPDATE_MODE)
     QMCDriver *qmc = 0;
+#ifdef QMC_CUDA
+    if (GPU)
+      APP_ABORT("RMC is not supported on GPU.\n");
+#endif
+
     if (RMCMode == 0 || RMCMode == 1)	//(0,0,0) (0,0,1) pbyp and all electron
       {
 	qmc = new RMCSingleOMP (w, psi, h, hpool, ppool);
