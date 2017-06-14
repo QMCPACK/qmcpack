288973ac45aab1695b75f88b2fb279e011b4f3af
Mark Dewing <markdewing@gmail.com>
2017-06-14 10:22:53 -0500

    Add unit tests for DMC walker propagation
    Use some simplified wavefunctions for testing - constant orbital (gradient is zero)
    and linear orbital (gradient is constant).
    
    The methods in LinearOrbital contain abort statements so only the ones strictly
    needed for the unit tests are added.  There is some implementation in 'evaluate',
    but since it's not called by these tests, it may not be completely correct.
    
    Move return statements in ConstantOrbital to the end of the function.

--- src/QMCWaveFunctions/LinearOrbital.h
+++ src/QMCWaveFunctions/LinearOrbital.h
@@ -2,24 +2,27 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2017 Jeongnim Kim and QMCPACK developers.
 //
-// File developed by: Mark Dewing, markdewing@gmail.com, University of Illinois at Urbana-Champaign
+// File developed by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory 
 //
-// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+// File created by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
-    
-    
-#ifndef QMCPLUSPLUS_CONSTANTORBITAL_H
-#define QMCPLUSPLUS_CONSTANTORBITAL_H
+
+
+#ifndef QMCPLUSPLUS_LINEARORBITAL_H
+#define QMCPLUSPLUS_LINEARORBITAL_H
 
 #include "QMCWaveFunctions/OrbitalBase.h"
 
 namespace qmcplusplus
 {
 
+// Only for testing - has constant gradient and zero laplacian
+// phi = coeff[0]*x + coeff[1]*y + coeff[2]*z
 
-class ConstantOrbital: public OrbitalBase
+
+class LinearOrbital: public OrbitalBase
 {
 public:
   virtual void checkInVariables(opt_variables_type &active) {}
@@ -28,24 +31,32 @@ public:
   virtual void reportStatus(std::ostream& os) {}
   virtual void resetTargetParticleSet(ParticleSet& P) {}
 
-  ValueType FakeGradRatio;
+  TinyVector<ValueType,3> coeff;
 
-  ConstantOrbital() : FakeGradRatio(1.0) {}
+  LinearOrbital() {coeff[0]=1.0; coeff[1]= 2.0; coeff[2]=3.0;}
 
   virtual ValueType
   evaluate(ParticleSet& P,
            ParticleSet::ParticleGradient_t& G,
            ParticleSet::ParticleLaplacian_t& L)
   {
-    G = 0.0;
+    APP_ABORT("LinearOrbital. evaluate");
+    ValueType v = 0.0;
+    for (int i = 0; i < P.R.size(); i++) {
+      for (int d = 0; d < OHMMS_DIM; d++) {
+        v += coeff[d]*P.R[i][d];
+      }
+      G[i] = coeff;
+    }
     L = 0.0;
-    return 1.0;
+    return v;
   }
 
   virtual RealType
   evaluateLog(ParticleSet& P,
               ParticleSet::ParticleGradient_t& G, ParticleSet::ParticleLaplacian_t& L)
   {
+    APP_ABORT("LinearOrbital. evaluateLog");
     G = 0.0;
     L = 0.0;
     return 0.0;
@@ -55,9 +66,14 @@ public:
                           ParticleSet::ParticleGradient_t& dG,
                           ParticleSet::ParticleLaplacian_t& dL)
   {
-    dG = 0.0;
+    ValueType v = 0.0;
+    APP_ABORT("LinearOrbital. ratio");
+    for (int d = 0; d < OHMMS_DIM; d++) {
+      v += coeff[d]*P.R[iat][d];
+    }
+    dG = 1.0;
     dL = 0.0;
-    return 1.0;
+    return v;
   }
 
   virtual void acceptMove(ParticleSet& P, int iat) {}
@@ -74,23 +90,25 @@ public:
                       ParticleSet::ParticleLaplacian_t& dL,
                       int iat)
   {
+    APP_ABORT("LinearOrbital. update");
     dG = 0.0;
     dL = 0.0;
   }
 
   virtual RealType evaluateLog(ParticleSet& P,BufferType& buf)
   {
+    APP_ABORT("LinearOrbital. evaluteLogbuffer");
     return 0.0;
   }
 
   virtual GradType evalGrad(ParticleSet &P, int iat)
   {
-    return GradType(0.0);
+    return GradType(coeff);
   }
 
   virtual ValueType ratioGrad(ParticleSet &P, int iat, GradType& grad_iat)
   {
-    return FakeGradRatio;
+    return 1.0;
   }
 
   virtual RealType registerData(ParticleSet& P, BufferType& buf) {return 0.0;}
