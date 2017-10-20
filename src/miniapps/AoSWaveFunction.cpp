01cea94e51e0a3d5b1e01e9a467be4867a139bee
Ye Luo <yeluo@anl.gov>
2017-10-19 21:21:33 -0500

    Remove buffer in AoSWaveFunction.

--- src/miniapps/AoSWaveFunction.cpp
+++ src/miniapps/AoSWaveFunction.cpp
@@ -40,18 +40,8 @@ namespace qmcplusplus
     constexpr valT czero(0);
     P.G=czero;
     P.L=czero;
-    if(FirstTime)
-    {
-      Buffer.rewind();
-      LogValue=J2->registerData(P,Buffer);
-      FirstTime=false;
-    }
-    else
-    {
-      P.update();
-      Buffer.rewind();
-      J2->copyFromBuffer(P,Buffer);
-    }
+    LogValue=J2->evaluateLog(P,P.G,P.L);
+    FirstTime=false;
   }
 
   FakeWaveFunctionBase::posT AoSWaveFunction::evalGrad(ParticleSet& P, int iat)
