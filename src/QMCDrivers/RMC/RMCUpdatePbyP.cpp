e59bbfa7ba5e12dbbf6688780e68c8501e8db7dc
Raymond Clay <rclay@sandia.gov>
2017-11-02 10:55:20 -0600

    Add comments and clean up RMCUpdatePbyP.cpp

--- src/QMCDrivers/RMC/RMCUpdatePbyP.cpp
+++ src/QMCDrivers/RMC/RMCUpdatePbyP.cpp
@@ -37,14 +37,6 @@ namespace qmcplusplus
 {
 
 
-/*  void add_rmc_timers(std::vector<NewTimer*>& timers)
-  {
-    timers.push_back(new NewTimer("RMCUpdatePbyP::advance")); //timer for the walker loop
-    timers.push_back(new NewTimer("RMCUpdatePbyP::movePbyP")); //timer for MC, ratio etc
-   // timers.push_back(new NewTimer("RMCUpdatePbyP::updateMBO")); //timer for measurements
-   // timers.push_back(new NewTimer("RMCUpdatePbyP::energy")); //timer for measurements
-    for (int i=0; i<timers.size(); ++i) TimerManager.addTimer(timers[i]);
-  }*/
 /// Constructor.
   RMCUpdatePbyPWithDrift::RMCUpdatePbyPWithDrift (MCWalkerConfiguration & w,
 						  TrialWaveFunction & psi,
@@ -94,18 +86,10 @@ namespace qmcplusplus
 	RealType logpsi2 = Psi.updateBuffer (W, awalker.DataSet, false);
 	awalker.G = W.G;
 	awalker.L = W.L;
-	//  268   W.saveWalker(awalker);
 	RealType eloc = H.evaluate (W);
-	//   BadState |= isnan(eloc);
-	//thisWalker.resetProperty(std::log(std::abs(psi)), psi,eloc);
 	awalker.resetProperty (logpsi, Psi.getPhase (), eloc);
-//      randomize(awalker);
       }
 
-
-    //  IndexType initsteps = W.reptile->nbeads + 10;
-
-//  for (int n=0; n < initsteps; n++) advanceWalkersVMC();
   }
 
   bool RMCUpdatePbyPWithDrift::put (xmlNodePtr cur)
@@ -149,10 +133,7 @@ namespace qmcplusplus
   }
   void RMCUpdatePbyPWithDrift::advanceWalkersVMC ()
   {
-    //  double starttime = cpu_clock ();
     myTimers[0]->start ();
-    // app_log () << "advanceWalkersVMC()::CALLED.. " << cpu_clock () -
-    //   starttime << std::endl;
     IndexType direction = W.reptile->direction;
     IndexType forward = (1 - direction) / 2;
     IndexType backward = (1 + direction) / 2;
@@ -160,20 +141,6 @@ namespace qmcplusplus
     Walker_t prophead (curhead);
     Walker_t::Buffer_t & w_buffer (prophead.DataSet);
     W.loadWalker (prophead, true);
-    //app_log () << "advanceWalkersVMC()::initialized variables... " <<
-    //   cpu_clock () - starttime << std::endl;
-    //  starttime = cpu_clock ();
-//      W.update();
-    //W.loadWalker(awalker,UpdatePbyP);
-//      if (prophead.DataSet.size())
-//        prophead.DataSet.clear();
-//      prophead.DataSet.rewind();
-//      RealType logpsi=Psi.registerData(W,prophead.DataSet);
-//      RealType logpsi2=Psi.updateBuffer(W,prophead.DataSet,false);
-    // curhead.G=W.G;
-    //  curhead.L=W.L;
-    // Walker_t& thisWalker(**it);
-    //Psi.copyFromBuffer (W, w_buffer);
     Psi.copyFromBuffer (W, w_buffer);
     //create a 3N-Dimensional Gaussian with variance=1
     makeGaussRandomWithEngine (deltaR, RandomGen);
@@ -200,7 +167,6 @@ namespace qmcplusplus
 	    PosType dr;
 	    getScaledDrift (tauovermass, grad_iat, dr);
 	    dr += sqrttau * deltaR[iat];
-	    //RealType rr=dot(dr,dr);
 	    RealType rr = tauovermass * dot (deltaR[iat], deltaR[iat]);
 	    rr_proposed += rr;
 	    if (rr > m_r2max)
@@ -208,15 +174,12 @@ namespace qmcplusplus
 		++nRejectTemp;
 		continue;
 	      }
-	    //PosType newpos(W.makeMove(iat,dr));
 	    if (!W.makeMoveAndCheck (iat, dr))
 	      continue;
 	    PosType newpos (W.R[iat]);
 	    RealType ratio = Psi.ratioGrad (W, iat, grad_iat);
 	    bool valid_move = false;
 	    //node is crossed reject the move
-	    //if(Psi.getPhase() > std::numeric_limits<RealType>::epsilon())
-	    //if(branchEngine->phaseChanged(Psi.getPhase(),thisWalker.Properties(SIGN)))
 	    if (branchEngine->phaseChanged (Psi.getPhaseDiff ()))
 	      {
 		++nRejectTemp;
@@ -228,8 +191,6 @@ namespace qmcplusplus
 	      {
 		RealType logGf = -0.5 * dot (deltaR[iat], deltaR[iat]);
 		//Use the force of the particle iat
-		//RealType scale=getDriftScale(m_tauovermass,grad_iat);
-		//dr = thisWalker.R[iat]-newpos-scale*real(grad_iat);
 		getScaledDrift (tauovermass, grad_iat, dr);
 		dr = prophead.R[iat] - newpos - dr;
 		RealType logGb = -oneover2tau * dot (dr, dr);
@@ -254,8 +215,6 @@ namespace qmcplusplus
       }
     myTimers[1]->stop ();
     W.donePbyP();
-    //  if(UseTMove)
-    //    nonLocalOps.reset();
     bool advanced = true;
 
     if (nAcceptTemp > 0)
@@ -265,19 +224,12 @@ namespace qmcplusplus
 	myTimers[2]->start ();
 	prophead.Age = 0;
 	prophead.R = W.R;
-	//w_buffer.rewind();
-	//W.updateBuffer(w_buffer);
 	RealType logpsi = Psi.updateBuffer (W, w_buffer, false);
 	W.saveWalker (prophead);
 	myTimers[2]->stop ();
 	myTimers[3]->start ();
-//      if(UseTMove)
-//        enew= H.evaluate(W,nonLocalOps.Txy);
-//      else
 	enew = H.evaluate (W);
 	myTimers[3]->stop ();
-	//nodecorr=getNodeCorrection(W.G,thisWalker.Drift);
-	//thisWalker.resetProperty(logpsi,Psi.getPhase(),enew,rr_accepted,rr_proposed,nodecorr);
 	prophead.resetProperty (logpsi, Psi.getPhase (), enew, rr_accepted,
 				rr_proposed, 0.0);
 	prophead.Weight = 1.0;
@@ -370,8 +322,6 @@ namespace qmcplusplus
 	    RealType ratio = Psi.ratioGrad (W, iat, grad_iat);
 	    bool valid_move = false;
 	    //node is crossed reject the move
-	    //if(Psi.getPhase() > std::numeric_limits<RealType>::epsilon())
-	    //if(branchEngine->phaseChanged(Psi.getPhase(),thisWalker.Properties(SIGN)))
 	    if (branchEngine->phaseChanged (Psi.getPhaseDiff ()))
 	      {
 		++nRejectTemp;
@@ -383,8 +333,6 @@ namespace qmcplusplus
 	      {
 		RealType logGf = -0.5 * dot (deltaR[iat], deltaR[iat]);
 		//Use the force of the particle iat
-		//RealType scale=getDriftScale(m_tauovermass,grad_iat);
-		//dr = thisWalker.R[iat]-newpos-scale*real(grad_iat);
 		getScaledDrift (tauovermass, grad_iat, dr);
 		dr = prophead.R[iat] - newpos - dr;
 		RealType logGb = -oneover2tau * dot (dr, dr);
@@ -409,98 +357,6 @@ namespace qmcplusplus
       }
     myTimers[1]->stop ();
     W.donePbyP();
-    //  if(UseTMove)
-/*
-  RealType logpsiold = prophead.Properties(LOGPSI);
-  makeGaussRandomWithEngine(deltaR,RandomGen);
-  int nAcceptTemp(0);
-  int nRejectTemp(0);
-  //copy the old energy and scale factor of drift
-  RealType eold(prophead.Properties(LOCALENERGY));
-  RealType vqold(prophead.Properties(DRIFTSCALE));
-  RealType enew(eold);
-  RealType rr_proposed=0.0;
-  RealType rr_accepted=0.0;
-  RealType gf_acc=1.0;
-  RealType logGf=0;
-  RealType logGb=0;
-  RealType ratio_acc=1;
-  //   myTimers[1]->start();
-  for(int iat=0; iat<NumPtcl; ++iat)
-    {
-      PosType dr;
-      //get the displacement
-      //RealType sc=getDriftScale(m_tauovermass,W.G[iat]);
-      //PosType dr(m_sqrttau*deltaR[iat]+sc*real(W.G[iat]));
-      getScaledDrift(m_tauovermass,W.G[iat],dr);
-      dr += m_sqrttau*deltaR[iat];
-      //RealType rr=dot(dr,dr);
-      RealType rr=m_tauovermass*dot(deltaR[iat],deltaR[iat]);
-      rr_proposed+=rr;
-      // if (W.reptile->r2
-      if(rr>m_r2max)
-        {
-          ++nRejectTemp;
-         // W.reptile->flip();
-          continue;
-        }
-      //PosType newpos(W.makeMove(iat,dr));
-      if(!W.makeMoveAndCheck(iat,dr))
-        {
-          ++nRejectTemp;
-          continue;
-          // W.reptile->flip();
-          // return;
-        }
-      PosType newpos(W.R[iat]);
-      RealType ratio=Psi.ratio(W,iat,dG,dL);
-      bool valid_move=false;
-      //node is crossed reject the move
-      //if(Psi.getPhase() > std::numeric_limits<RealType>::epsilon())
-      if(branchEngine->phaseChanged(Psi.getPhaseDiff()))
-        {
-          ++nRejectTemp;
-          ++nNodeCrossing;
-          W.rejectMove(iat);
-          Psi.rejectMove(iat);
-        }
-      else
-        {
-          G = W.G+dG;
-          RealType logGf_pbyp = -0.5*dot(deltaR[iat],deltaR[iat]);
-          logGf+=logGf_pbyp;
-          //Use the force of the particle iat
-          //RealType scale=getDriftScale(m_tauovermass,G[iat]);
-          //dr = thisWalker.R[iat]-newpos-scale*real(G[iat]);
-          getScaledDrift(m_tauovermass, G[iat], dr);
-          dr = prophead.R[iat] - newpos - dr;
-          RealType logGb_pbyp = -m_oneover2tau*dot(dr,dr);
-          logGb+=logGb_pbyp;
-          RealType prob = ratio*ratio*std::exp(logGb_pbyp-logGf_pbyp);
-          //this is useless
-          //RealType prob = std::min(1.0,ratio*ratio*std::exp(logGb-logGf));
-          if(RandomGen() < prob)
-            {
-              valid_move=true;
-              ++nAcceptTemp;
-              W.acceptMove(iat);
-              Psi.acceptMove(W,iat);
-              W.G = G;
-              W.L += dL;
-              rr_accepted+=rr;
-              gf_acc *=prob;//accumulate the ratio
-              ratio_acc*=ratio;
-            }
-          else
-            {
-              ++nRejectTemp;
-              W.rejectMove(iat);
-              Psi.rejectMove(iat);
-              // W.reptile->flip();
-              // return;
-            }
-        }
-    }*/
 // In the rare case that all proposed moves fail, we bounce.
     if (nAcceptTemp == 0)
       {
@@ -530,17 +386,13 @@ namespace qmcplusplus
 							   getNewHead ());
 	if (curhead.Age >= MaxAge || lastbead.Age >= MaxAge)
 	  app_log () << "\tForce Acceptance...\n";
-	// RealType logpsi = Psi.updateBuffer(W,w_buffer,false);
-	//W.saveWalker(prophead);
 
 	prophead.Properties (LOCALENERGY) = eloc;
 	prophead.Properties (R2ACCEPTED) = rr_accepted;
 	prophead.Properties (R2PROPOSED) = rr_proposed;
 	H.auxHevaluateProperties (W, prophead);
-	//H.saveProperty(overwriteWalker.getPropertyBase());
 	H.saveProperty (prophead.getPropertyBase ());
         
-	//overwriteWalker.Age=0;
 	prophead.Age = 0;
 
 	overwriteWalker = prophead;
@@ -556,10 +408,10 @@ namespace qmcplusplus
 	W.reptile->flip ();
 	// return;
       }
-
+      //Collectables should be evaluated on center bead.  Here we go.
       Walker_t& centerbead = W.reptile->getCenter();
       W.loadWalker(centerbead,true);
-      W.update(false);  //skip S(k) evaluation?  False
+      W.update(false);  //Called to recompute S(k) and distance tables.  Recompute S(k)?  False.
       H.auxHevaluateCollectables(W,centerbead);
   }
 
@@ -580,8 +432,6 @@ namespace qmcplusplus
   void RMCUpdatePbyPWithDrift::accumulate (WalkerIter_t it,
 					   WalkerIter_t it_end)
   {
-    // if (vmcToDoSteps==0 && equilToDoSteps==0) Estimators->accumulate(W,it,it_end);
-//  else;       
     Estimators->accumulate (W, it, it_end);
   }
 
