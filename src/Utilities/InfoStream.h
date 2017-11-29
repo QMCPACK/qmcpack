d7068ac9b41c058c302f4b2191428d64f0ad7030
Mark Dewing <markdewing@gmail.com>
2017-11-29 11:53:26 -0600

    Wrapper for output stream handling.
    This class can direct output to an existing stream (cout/cerr), a file, or a null stream.
    
    This is a replacement for the OhmmsInform class. Differences include
     - the null output is really a null output, not an ostringstream (which still uses memory)
     - the turnoff/reset methods are renamed to pause/resume, which better describes their behavior.

---
+++ src/Utilities/InfoStream.h
@@ -0,0 +1,89 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2017 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
+//
+// File created by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+/** @file InfoStream.h
+ * @brief Declaration of InfoStream class.
+ */
+
+#ifndef INFOSTREAM_H
+#define INFOSTREAM_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+// Bit bucket to discard output
+// From https://stackoverflow.com/questions/7818371/printing-to-nowhere-with-ostream
+class NullStreamBuf : public std::streambuf
+{
+protected:
+    virtual int overflow(int c) { return c; }
+};
+
+class NullStream : public NullStreamBuf, public std::ostream
+{
+public:
+    NullStream() : std::ostream(this) {}
+};
+
+/**
+ *  Interface to output streams.  Can redirect output to stdout/stderr, a file, or a null stream.
+ */
+
+class InfoStream
+{
+public:
+  InfoStream(std::ostream *output_stream): prevStream(NULL), nullStream(new NullStream),
+      ownStream(false) {
+    currStream = output_stream;
+  }
+
+  ~InfoStream();
+
+  std::ostream &getStream(const std::string &tag = "") {
+    return *currStream;
+  }
+
+
+  void flush() {
+    currStream->flush();
+  }
+
+  /// Stop output (redirect to a null stream)
+  void pause();
+
+  ///  Continue output on the stream used before pausing
+  void resume();
+
+  /// Open a file and output to that file
+  void redirectToFile(const std::string &fname);
+
+  /// Copy a stream
+  void redirectToSameStream(InfoStream &info);
+
+private:
+
+  // Keep track of whether we should delete the stream or not
+  bool ownStream;
+
+  std::ostream *currStream;
+
+  // save stream during pause
+  std::ostream *prevStream;
+
+  // Created at construction. Used during pause
+  std::ostream *nullStream;
+};
+
+
+#endif
