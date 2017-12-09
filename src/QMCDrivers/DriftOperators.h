1a8761425c85746160248c417ce18af74c03212d
Paul Young <yyang173@illinois.edu>
2017-12-09 17:39:32 -0600

    edit comments

--- src/QMCDrivers/DriftOperators.h
+++ src/QMCDrivers/DriftOperators.h
@@ -136,17 +136,21 @@ inline T setScaledDriftPbyPandNodeCorr(T tau,
 /** scale drift
  * @param tau_au timestep au
  * @param massinv 1/m per particle
- * @param qf quantum forces
- * @param drift scaled quantum forces
+ * @param qf quantum forces, i.e. grad_psi_over_psi
+ * @param drift scaled importance-sampling drift
  * @param return correction term
  *
  * Fill the drift vector one particle at a time (pbyp).
  *
- * The naive drift is tau/mass*grad_psi_over_psi,
- *  namely the log derivative of the guiding wavefunction; tau is timestep; mass is particle mass
- * The naive drift diverges at a node, causing persistent configurations.
+ * The naive drift is tau/mass*|grad_psi_over_psi|,
+ *  grad_psi_over_psi the log derivative of the guiding wavefunction;
+ *  tau is timestep; mass is particle mass
+ * The naive drift diverges at a node and is large near a nucleus.
+ * Both cases may cause persistent configurations, because the
+ *  reverse proposal probability is virtually zero.
+ *
  * The norm of the drift vector should be limited in two ways:
- *  1. Umrigar: suppress drift divergence to mimic wf divergence
+ *  1. Umrigar: suppress drift divergence with a sclaing factor
  *  2. Ceperley: limit max drift rate to diffusion rate -> set Umrigar "a" parameter
  * The choice of drift vector does not affect VMC correctness
  *  so long as the proposal probabilities are correctly calculated.
@@ -161,11 +165,12 @@ inline T setScaledDriftPbyPandNodeCorr(T tau_au, const std::vector<T>& massinv,
                                        const ParticleAttrib<TinyVector<T1,D> >& qf,
                                        ParticleAttrib<TinyVector<T,D> >& drift)
 {
+  // Umrigar eq. (34) "a" parameter is set to 1.0
   T norm=0.0, norm_scaled=0.0; // variables to be accumulated
   for(int iat=0; iat<massinv.size(); ++iat)
   {
-    // effectively set the Umrigar "a" parameter to particle mass
-    T tau_over_mass = tau_au*massinv[iat]; // !!!! assume timestep is scaled by mass
+    // !!!! assume timestep is scaled by mass
+    T tau_over_mass = tau_au*massinv[iat]; 
     // save real part of wf log derivative in drift
     convert(qf[iat],drift[iat]);
     T vsq = dot(drift[iat],drift[iat]);
