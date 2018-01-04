876f68b732605c17a640319df59ee1ec8178091b
Ye Luo <yeluo@anl.gov>
2018-01-03 23:23:45 -0600

    Add the build script for Mira.

--- config/build_alcf_mira.sh
+++ config/build_alcf_mira.sh
@@ -1,20 +1,12 @@
-module unload cray-libsci
-module load cray-hdf5-parallel
+#!/bin/bash
 
-export CC=cc
-export CXX=CC
-export BOOST_ROOT=/soft/libraries/boost/1.64.0/intel
-export CRAYPE_LINK_TYPE=dynamic
-
-#TYPE=RelWithDebInfo
-TYPE=Release
-Compiler=Intel
+Compiler=Clang++11
 
 for name in real_SoA real_SoA_MP cplx_SoA cplx_SoA_MP \
             real real_MP cplx cplx_MP
 do
 
-CMAKE_FLAGS="-D CMAKE_BUILD_TYPE=$TYPE"
+CMAKE_FLAGS="-D CMAKE_TOOLCHAIN_FILE=../config/BGQ_${Compiler}_ToolChain.cmake"
 
 if [[ $name == *"cplx"* ]]; then
   CMAKE_FLAGS="$CMAKE_FLAGS -D QMC_COMPLEX=1"
@@ -28,7 +20,7 @@ if [[ $name == *"_MP"* ]]; then
   CMAKE_FLAGS="$CMAKE_FLAGS -D QMC_MIXED_PRECISION=1"
 fi
 
-folder=build_KNL_${Compiler}_${name}
+folder=build_${Compiler}_${name}
 echo "**********************************"
 echo "$folder"
 echo "$CMAKE_FLAGS"
@@ -37,8 +29,9 @@ mkdir $folder
 cd $folder
 if [ ! -f CMakeCache.txt ] ; then
 cmake $CMAKE_FLAGS ..
+cmake $CMAKE_FLAGS ..
 fi
-make -j32
+make -j24
 cd ..
 
 echo
