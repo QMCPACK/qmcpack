e7d16b1b126781cfb8df01a4f41554653c090344
Peter Doak <doakpw@ornl.gov>
2018-03-05 11:48:00 -0500

    Now the main build works again for OSX llvm 5.0.1

--- CMake/FindMKL.cmake
+++ CMake/FindMKL.cmake
@@ -6,32 +6,45 @@ IF(COMMAND cmake_policy)
   cmake_policy(SET CMP0056 NEW)
 ENDIF(COMMAND cmake_policy)
 
-
 # if MKL_ROOT is set, use that
 if ( MKL_ROOT )
+   message ("MKL_ROOT: ${MKL_ROOT}")
+   set(MKL_FLAGS "-m64")
    set(MKL_LINK_DIRECTORIES "${MKL_ROOT}/lib")
-   set(MKL_LINKER_FLAGS "-L${MKL_LINK_DIRECTORIES}")
+   set(MKL_LINKER_FLAGS "-L${MKL_LINK_DIRECTORIES} -Wl,-rpath,${MKL_LINK_DIRECTORIES}")
    set(MKL_INCLUDE_DIRECTORIES "${MKL_ROOT}/include")
    set(MKL_FLAGS "-I${MKL_INCLUDE_DIRECTORIES}")
-   set(MKL_LIBRARIES "-lmkl_intel_lp64 -lmkl_sequential -lmkl_core")
+   set(MKL_LIBRARIES "-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl")
 else ( MKL_ROOT )
 # this takes away build control but is what the cmake does now
    string(TOLOWER CMAKE_CXX_COMPILER_ID LCASE_CMAKE_CXX_COMPILER_ID)
    if ( ${LCASE_CMAKE_CXX_COMPILER_ID} MATCHES "intel" )
      set(MKL_LIBRARIES "-mkl")
      set(MKL_FLAGS "-mkl")
-   endif ()
-endif ( MKL_ROOT ) 
+   endif ( ${LCASE_CMAKE_CXX_COMPILER_ID} MATCHES "intel" )
+endif ( MKL_ROOT )
+
+set( org_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" )
+if ( CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
+  string(REPLACE "-fopenmp" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+endif ( CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
+
+ 
 # Check for mkl.h
 FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx"
     "#include <iostream>\n #include <mkl.h>\n int main() { return 0; }\n" )
 try_compile(HAVE_MKL ${CMAKE_BINARY_DIR}
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx
   CMAKE_FLAGS
+  "${MKL_FLAGS}"
   "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
   "-DLINK_DIRECTORIES=${MKL_LINK_DIRECTORIES}"
   LINK_LIBRARIES "${MKL_LIBRARIES}"
   OUTPUT_VARIABLE MKL_OUT)
+
+if ( NOT HAVE_MKL )
+  MESSAGE( "${MKL_OUT}" )
+endif ( NOT HAVE_MKL )
 
 # Check for mkl_vml_functions.h
 FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx"
@@ -39,6 +52,7 @@ FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cx
 try_compile(HAVE_MKL_VML ${CMAKE_BINARY_DIR}
   ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx
   CMAKE_FLAGS
+  "${MKL_FLAGS}"
   "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
   "-DLINK_DIRECTORIES=${MKL_LINK_DIRECTORIES}"
   OUTPUT_VARIABLE MKL_OUT)
@@ -49,11 +63,14 @@ FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_fftw3.
 try_compile(HAVE_MKL_FFTW3 ${CMAKE_BINARY_DIR}
       ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_fftw3.cxx
       CMAKE_FLAGS
+      "${MKL_FLAGS}"
       "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
       "-DLINK_DIRECTORIES=${MKL_LINK_DIRECTORIES}"
       LINK_LIBRARIES "${MKL_LIBRARIES}"
       OUTPUT_VARIABLE MKL_OUT)
 
+set( CMAKE_CXX_FLAGS "${org_CMAKE_CXX_FLAGS}" )
+    
 IF ( HAVE_MKL )
     SET( MKL_FOUND 1 )
     SET( MKL_FLAGS ${MKL_FLAGS} )
