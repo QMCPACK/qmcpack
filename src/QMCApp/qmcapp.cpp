20ae625e1493b5c8a00f19627732d3bbb2dab937
Mark Dewing <markdewing@gmail.com>
2018-04-17 17:39:28 -0500

    Move init in qmc_common to QMCApp
    Move qmc_common.initialize out of Communicate and into QMCApp.
    This simplifies the MPI-related code a little bit.

--- src/QMCApp/qmcapp.cpp
+++ src/QMCApp/qmcapp.cpp
@@ -51,6 +51,7 @@ int main(int argc, char **argv)
   using namespace qmcplusplus;
   //qmc_common  and MPI is initialized
   OHMMS::Controller->initialize(argc,argv);
+  qmcplusplus::qmc_common.initialize(argc,argv);
   int clones=1;
   bool useGPU=(qmc_common.compute_device == 1);
   std::vector<std::string> fgroup1,fgroup2;
