be2acc885fa55272d825e34d1215e71abb2c72c8
Jaron Krogel <krogeljt@ornl.gov>
2018-04-30 14:57:39 -0400

    Remove model insulator

--- src/QMCHamiltonians/HamiltonianFactory.cpp
+++ src/QMCHamiltonians/HamiltonianFactory.cpp
@@ -49,11 +49,6 @@
 #if OHMMS_DIM == 3
 #include "QMCHamiltonians/ChiesaCorrection.h"
 #include "QMCHamiltonians/SkAllEstimator.h"
-#if defined(HAVE_LIBFFTW_LS)
-#include "QMCHamiltonians/model/ModInsKineticEnergy.h"
-#include "QMCHamiltonians/MomentumDistribution.h"
-#include "QMCHamiltonians/DispersionRelation.h"
-#endif
 #endif
 // #include "QMCHamiltonians/ZeroVarObs.h"
 #if !defined(QMC_CUDA) && QMC_BUILD_LEVEL>2
@@ -292,10 +287,6 @@ bool HamiltonianFactory::build(xmlNodePtr cur, bool buildtree)
         //HFD->addCorrection(*targetPtcl,*targetH);
         targetH->addOperator(HFD->makeDependants(*targetPtcl),HFD->depName,false);
         app_log() << "  Adding HFDHE2Potential(Au) " << std::endl;
-      }
-      else if(cname == "modInsKE")
-      {
-        addModInsKE(cur);
       }
 #endif
 #endif
@@ -648,121 +639,6 @@ bool HamiltonianFactory::build(xmlNodePtr cur, bool buildtree)
   return true;
 }
 
-void
-HamiltonianFactory::addModInsKE(xmlNodePtr cur)
-{
-#if defined(HAVE_LIBFFTW_LS)
-  typedef QMCTraits::RealType    RealType;
-  typedef QMCTraits::IndexType   IndexType;
-  typedef QMCTraits::PosType     PosType;
-  std::string Dimensions, DispRelType, PtclSelType, MomDistType;
-  RealType Cutoff, GapSize(0.0), FermiMomentum(0.0);
-  OhmmsAttributeSet pAttrib;
-  pAttrib.add(Dimensions, "dims");
-  pAttrib.add(DispRelType, "dispersion");
-  pAttrib.add(PtclSelType, "selectParticle");
-  pAttrib.add(Cutoff, "cutoff");
-  pAttrib.add(GapSize, "gapSize");
-  pAttrib.add(FermiMomentum, "kf");
-  pAttrib.add(MomDistType, "momdisttype");
-  pAttrib.put(cur);
-  if (MomDistType == "")
-    MomDistType = "FFT";
-  TrialWaveFunction* psi;
-  psi = (*(psiPool.begin())).second->targetPsi;
-  Vector<PosType> LocLattice;
-  Vector<IndexType> DimSizes;
-  Vector<RealType> Dispersion;
-  if (Dimensions == "3")
-  {
-    gen3DLattice(Cutoff, *targetPtcl, LocLattice, Dispersion, DimSizes);
-  }
-  else if (Dimensions == "1" || Dimensions == "1averaged")
-  {
-    gen1DLattice(Cutoff, *targetPtcl, LocLattice, Dispersion, DimSizes);
-  }
-  else if (Dimensions == "homogeneous")
-  {
-    genDegenLattice(Cutoff, *targetPtcl, LocLattice, Dispersion, DimSizes);
-  }
-  else
-  {
-    ERRORMSG("Dimensions value not recognized!")
-  }
-  if (DispRelType == "freeParticle")
-  {
-    genFreeParticleDispersion(LocLattice, Dispersion);
-  }
-  else if (DispRelType == "simpleModel")
-  {
-    genSimpleModelDispersion(LocLattice, Dispersion, GapSize, FermiMomentum);
-  }
-  else if (DispRelType == "pennModel")
-  {
-    genPennModelDispersion(LocLattice, Dispersion, GapSize, FermiMomentum);
-  }
-  else if (DispRelType == "debug")
-  {
-    genDebugDispersion(LocLattice, Dispersion);
-  }
-  else
-  {
-    ERRORMSG("Dispersion relation not recognized");
-  }
-  PtclChoiceBase* pcp;
-  if (PtclSelType == "random")
-  {
-    pcp = new RandomChoice(*targetPtcl);
-  }
-  else if (PtclSelType == "randomPerWalker")
-  {
-    pcp = new RandomChoicePerWalker(*targetPtcl);
-  }
-  else if (PtclSelType == "constant")
-  {
-    pcp = new StaticChoice(*targetPtcl);
-  }
-  else
-  {
-    ERRORMSG("Particle choice policy not recognized!");
-  }
-  MomDistBase* mdp;
-  if (MomDistType == "direct")
-  {
-    mdp = new RandomMomDist(*targetPtcl, LocLattice, pcp);
-  }
-  else if (MomDistType == "FFT" || MomDistType =="fft")
-  {
-    if (Dimensions == "3")
-    {
-      mdp = new ThreeDimMomDist(*targetPtcl, DimSizes, pcp);
-    }
-    else if (Dimensions == "1")
-    {
-      mdp = new OneDimMomDist(*targetPtcl, DimSizes, pcp);
-    }
-    else if (Dimensions == "1averaged")
-    {
-      mdp = new AveragedOneDimMomDist(*targetPtcl, DimSizes, pcp);
-    }
-    else
-    {
-      ERRORMSG("Dimensions value not recognized!");
-    }
-  }
-  else
-  {
-    ERRORMSG("MomDistType value not recognized!");
-  }
-  delete pcp;
-  QMCHamiltonianBase* modInsKE = new ModInsKineticEnergy(*psi, Dispersion, mdp);
-  modInsKE->put(cur);
-  targetH->addOperator(modInsKE, "ModelInsKE");
-  delete mdp;
-#else
-  app_error() << "  ModelInsulatorKE cannot be used without FFTW " << std::endl;
-#endif
-}
 
 void HamiltonianFactory::renameProperty(const std::string& a, const std::string& b)
 {
