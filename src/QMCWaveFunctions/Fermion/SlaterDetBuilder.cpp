ac22dc9b728d6ca69238007280683df906c6008d
Jaron Krogel <krogeljt@ornl.gov>
2018-05-03 12:47:51 -0400

    Remove AFM determinant

--- src/QMCWaveFunctions/Fermion/SlaterDetBuilder.cpp
+++ src/QMCWaveFunctions/Fermion/SlaterDetBuilder.cpp
@@ -46,7 +46,6 @@
 #include "QMCWaveFunctions/Fermion/SPOSetProxy.h"
 #include "QMCWaveFunctions/Fermion/SPOSetProxyForMSD.h"
 #include "QMCWaveFunctions/Fermion/DiracDeterminantOpt.h"
-#include "QMCWaveFunctions/Fermion/DiracDeterminantAFM.h"
 
 #include <bitset>
 #include <unordered_map>
@@ -423,7 +422,7 @@ bool SlaterDetBuilder::put(xmlNodePtr cur)
  * - id unique name
  * - sposet reference to the pre-defined sposet; when missing, use id
  * - group electron species name, u or d
- * - type variantion of a determinant, type="AFM" uses a specialized determinant builder for Anti-Ferromagnetic system
+magnetic system
  * Extra attributes to handled the original released-node case
  */
 bool SlaterDetBuilder::putDeterminant(xmlNodePtr cur, int spin_group, bool slater_det_opt)
@@ -437,7 +436,6 @@ bool SlaterDetBuilder::putDeterminant(xmlNodePtr cur, int spin_group, bool slate
   std::string basisName("invalid");
   std::string detname("0"), refname("0");
   std::string s_detSize("0");
-  std::string afm("no");
   std::string usesoa("no");
 
   OhmmsAttributeSet aAttrib;
@@ -445,7 +443,6 @@ bool SlaterDetBuilder::putDeterminant(xmlNodePtr cur, int spin_group, bool slate
   aAttrib.add(detname,"id"); 
   aAttrib.add(sposet,"sposet");
   aAttrib.add(refname,"ref");
-  aAttrib.add(afm,"type");
   aAttrib.add(s_detSize,"DetSize");
   aAttrib.add(usesoa,"soa");
 
@@ -518,11 +515,6 @@ bool SlaterDetBuilder::putDeterminant(xmlNodePtr cur, int spin_group, bool slate
 #else
     if(UseBackflow)
       adet = new DiracDeterminantWithBackflow(targetPtcl,psi,BFTrans,firstIndex);
-    else if (afm=="AFM")
-    {
-      app_log()<<"Using the AFM determinant"<< std::endl;
-      adet = new DiracDeterminantAFM(targetPtcl, psi, firstIndex);
-    }
     else if (slater_det_opt)
     {
 #ifdef QMC_COMPLEX
