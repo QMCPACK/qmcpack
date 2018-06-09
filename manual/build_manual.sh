6fbbd3182cfae61a24cbc25344d36dc5c39127d7
Peter Doak <doakpw@ornl.gov>
2018-06-09 12:50:25 -0400

    building the manual with xelatex appears to create not problems
    has been in the texlive a long time. It understands unicode we can
    keep the directory tree diagrams which are nice.
    
    While pdflatex can read utf8 it has no idea what to do with some/all unicode
    characters.

--- manual/build_manual.sh
+++ manual/build_manual.sh
@@ -6,11 +6,11 @@ if [ $# -eq 1 ]; then
   sed -i "s/Development Version/$QMCPACK_VER/" version.tex
 fi
 
-pdflatex qmcpack_manual.tex
+xelatex qmcpack_manual.tex
 bibtex qmcpack_manual1
 bibtex qmcpack_manual2
-pdflatex qmcpack_manual.tex
-pdflatex qmcpack_manual.tex
+xelatex qmcpack_manual.tex
+xelatex qmcpack_manual.tex
 
 if [ ! -z "$QMCPACK_VER" ]; then
   mv version.save.tex version.tex
