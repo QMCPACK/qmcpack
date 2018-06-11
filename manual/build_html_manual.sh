5497a90d1921bb69c6c4f7276914c3dc7892343a
Peter Doak <doakpw@ornl.gov>
2018-06-11 13:44:05 -0400

    need qualified relative paths to figures for html

--- manual/build_html_manual.sh
+++ manual/build_html_manual.sh
@@ -8,7 +8,7 @@ if [ $# -eq 1 ]; then
   sed -i "s/Development Version/$QMCPACK_VER/" version.tex
 fi
 
-./prep_pdf.sh 'figures/*.pdf'
+./prep_pdf.sh './figures/*.pdf'
 make4ht -x -e qmcpack_manual.mk4 -c qmcpack_manual.cfg qmcpack_manual.tex "xhtml,2,html5,graphics-144" "" " -cvalidate"
 
 if [ ! -z "$QMCPACK_VER" ]; then
