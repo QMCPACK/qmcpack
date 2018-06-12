51ec386899de1755fcf1fe9a5b1168c616f0383f
Peter Doak <doakpw@ornl.gov>
2018-06-12 11:06:40 -0400

    better directory tree output in html. verbatim more verbatim

--- manual/build_html_manual.sh
+++ manual/build_html_manual.sh
@@ -9,7 +9,7 @@ if [ $# -eq 1 ]; then
 fi
 
 ./prep_pdf.sh './figures/*.pdf'
-make4ht -x -e qmcpack_manual.mk4 -c qmcpack_manual.cfg qmcpack_manual.tex "xhtml,2,html5,graphics-144" "" " -cvalidate"
+make4ht -u -x -e qmcpack_manual.mk4 -c qmcpack_manual.cfg qmcpack_manual.tex "xhtml,2,html5,graphics-144" "" " -cvalidate"
 
 if [ ! -z "$QMCPACK_VER" ]; then
   mv version.save.tex version.tex
