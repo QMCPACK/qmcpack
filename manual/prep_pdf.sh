ca5f2db025c7588aec9c09e26f316b31c1ea1f3e
Peter Doak <doakpw@ornl.gov>
2018-06-15 17:41:35 -0400

    define a dmn graphics extension for pdf->svg workaround
    to-do: \lstset doesn't do what someone thought.
    \emwidth should relate to document font size

--- manual/prep_pdf.sh
+++ manual/prep_pdf.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-set -x
+
 for file in $1; do
-    stat $file 2>%1
-    #if the png doesn't exist
-    if [ $? -neq 0 ]
+    filename=${file%.*}
+    pngfile=${filename}.png
+    stat $pngfile
+    #if the whatever doesn't exist
+    if [ $? -ne 0 ]
     then
-       filename=${file%.*}
+       echo "converting ${filename}.pdf to ${filename}.png"
+       convert -density 300x300 "$filename.pdf" "$filename.png"
+    fi
+    svgfile=${filename}.svg
+    stat $svgfile
+    if [ $? -ne 0 ]
+    then
        echo "converting ${filename}.pdf to ${filename}.svg"
        pdfcrop --margins 10 --clip --resolution 144 "$filename.pdf" "$filename.pdf"
        pdf2svg "$filename.pdf" "$filename.svg"
-       echo "converting ${filename}.pdf to ${filename}.png"
-       convert -density 300x300 "$filename.pdf" "$filename.png"
+       ebb -x ${filename}.pdf
     fi
 done
