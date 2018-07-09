33bbad30dd3877a1dfa766121e63156772392cf7
Ye Luo <yeluo@anl.gov>
2018-07-09 18:44:15 -0500

    Add QE configure example.

--- external_codes/quantum_espresso/download_and_patch_qe6.3.sh
+++ external_codes/quantum_espresso/download_and_patch_qe6.3.sh
@@ -49,6 +49,7 @@ if [ -e $codename/PP/src/pw2qmcpack.f90 ]; then
 echo --- SUCCESS: ${codename} patched for pw2qmcpack converter
 echo There are two ways to build
 echo "1) if your system already has HDF5 installed with Fortran, use the --with-hdf5 configuration option."
+echo "   Example configure: ./configure --with-hdf5=path_to_hdf5_include_and_lib"
 echo "   Currently HDF5 support in QE itself is preliminary. To enable use of pw2qmcpack"
 echo "   but use the old non-HDF5 I/O within QE, replace '-D__HDF5' with '-D__HDF5_C' in make.inc."
 echo "2) if your system has HDF5 with C only, manually edit make.inc by adding '-D__HDF5_C -DH5_USE_16_API'"
