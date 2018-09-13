0378f1fb06f8cfd78e11caae5ef073faa13f9501
Peter Doak <doakpw@ornl.gov>
2018-09-13 14:36:09 -0400

    two additional files to deal with inherited declaration issues.

---
+++ src/QMCWaveFunctions/NLjob.h
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Ken Esler, kpesler@gmail.com, University of Illinois at Urbana-Champaign
+//                    Miguel Morales, moralessilva2@llnl.gov, Lawrence Livermore National Laboratory
+//                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//                    Raymond Clay III, j.k.rofling@gmail.com, Lawrence Livermore National Laboratory
+//                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+//
+// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//////////////////////////////////////////////////////////////////////////////////////
+    
+    
+#ifndef QMCPLUSPLUS_NLJOB_H
+#define QMCPLUSPLUS_NLJOB_H
+
+struct NLjob
+{
+  int walker;
+  int elec;
+  int numQuadPoints;
+  NLjob(int w, int e, int n) :
+    walker(w), elec(e), numQuadPoints(n)
+  { }
+};
+
+#endif
+
