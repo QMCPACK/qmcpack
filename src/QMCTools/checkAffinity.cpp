4880625478cdd16001fffe92ade92667a89d11a2
Ye Luo <yeluo@anl.gov>
2018-10-11 15:04:59 -0500

    Add license header in checkAffinity.cpp

--- src/QMCTools/checkAffinity.cpp
+++ src/QMCTools/checkAffinity.cpp
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//
+// File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
 #ifdef __bgq__
 #include </bgsys/drivers/ppcfloor/spi/include/kernel/location.h>
 #elif __linux__
