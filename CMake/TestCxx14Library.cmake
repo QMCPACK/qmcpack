cb685bb50c9f805a6f84792aa1e2a06f6433751c
Mark Dewing <markdewing@gmail.com>
2018-10-29 14:44:15 -0500

    Write failed CPP 14 compiler output to a file
    Update the message and suggestion for Intel compilers.

--- CMake/TestCxx14Library.cmake
+++ CMake/TestCxx14Library.cmake
@@ -1,20 +1,26 @@
 
 # Test the compiler is configured with a C++14 library
 
-try_compile(CXX14_LIBRARY_OKAY ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/CMake/try_cxx14_library.cpp)
-message("C++ 14 library support = ${CXX14_LIBRARY_OKAY}")
+try_compile(CXX14_LIBRARY_OKAY ${CMAKE_BINARY_DIR}
+            ${CMAKE_SOURCE_DIR}/CMake/try_cxx14_library.cpp
+            OUTPUT_VARIABLE COMPILE_OUTPUT)
 
 if (NOT CXX14_LIBRARY_OKAY)
+  set(COMPILE_FAIL_OUTPUT cpp14_compile_fail.txt)
+  file(WRITE "${CMAKE_BINARY_DIR}/${COMPILE_FAIL_OUTPUT}" "${COMPILE_OUTPUT}")
+
   message("C++ 14 library support not found")
   message("compiler is ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
   if (CMAKE_CXX_COMPILER_ID MATCHES "GNU") 
-    message("Compiler detected is g++.  Use version 5.0 or newer for a C++14 compatible library")
+    message("Compiler detected is g++.\n  Use version 5.0 or newer for a C++14 compatible library")
   elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    message("Compiler deteceted is clang++. If not using libcxx, ensure a g++ version greater than 5.0 is on the path")
+    message("Compiler deteceted is clang++.\n  If not using libcxx, ensure a g++ version greater than 5.0 is on the path")
   elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-    message("Compiler detected is icpc.  Ensure a g++ version greater than 5.0 is on the path.
-                 Or use the --blah switch")
+    message("Compiler detected is icpc.\n  Ensure a gcc version greater than 5.0 is on the path.  Or use the --cxxlib switch to point to a newer gcc install.")
   endif()
+  message("  Output of test compile is in ${COMPILE_FAIL_OUTPUT}")
   message(FATAL_ERROR "stopping")
+else()
+  message("C++ 14 library supported")
 endif()
 
