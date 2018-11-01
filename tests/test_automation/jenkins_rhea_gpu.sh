084302940cb074b9d80cda719c1686d188f70057
Graham Lopez <lopezmg@ornl.gov>
2018-11-01 13:33:07 -0400

    bugfix: equality test was backwards

--- tests/test_automation/jenkins_rhea_gpu.sh
+++ tests/test_automation/jenkins_rhea_gpu.sh
@@ -117,7 +117,7 @@ cp $BUILD_DIR/$BUILD_TAG.o* ../
 cp $BUILD_DIR/$BUILD_TAG.o* ../
 
 # check for correct test output from all builds
-if [ $(grep '100% tests passed, 0 tests failed out of [0-9]*' ../$BUILD_TAG.o* | wc -l) -eq 4 ]
+if [ $(grep '100% tests passed, 0 tests failed out of [0-9]*' ../$BUILD_TAG.o* | wc -l) -ne 4 ]
 then
    echo; echo
    echo One or more build variants failed. Check the build log for details.
