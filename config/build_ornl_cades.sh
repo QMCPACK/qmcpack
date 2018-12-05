6164c82879981e8e3caec034e14c90e3d61c264c
Paul Kent <kentpr@ornl.gov>
2018-12-05 11:15:26 -0500

    Update compiler and module version usage

--- config/build_ornl_cades.sh
+++ config/build_ornl_cades.sh
@@ -2,25 +2,25 @@
 
 ################################################################
 ## * This script builds available configurations of QMCPACK   ##
-##   on Cades, at Oak Ridge National Lab.                     ##
+##   on CADES SHPC Condos , at Oak Ridge National Lab.        ##
 ##                                                            ##
 ## * Execute this script in trunk/                            ##
 ##   ./config/build_ornl_cades.sh                             ##
 ##                                                            ##
-## Last modified: Oct 7, 2018                                 ##
+## Last verified: Dec 5, 2018                                 ##
 ################################################################
-
 
 source $MODULESHOME/init/bash
 module purge
 module load PE-intel
 module load xalt/0.7.6
-module load openmpi/3.0.0
 module load hdf5-parallel/1.8.17
-module load fftw/3.3.5
 module load boost/1.64.0
-module load cmake/3.6.1
-
+module load cmake/3.12.0
+module swap intel intel/18.0.0
+module swap openmpi openmpi/1.10.3
+module load gcc/6.3.0
+module list
 
 # Set environment variables
 export FFTW_HOME=$FFTW_DIR/..
@@ -33,45 +33,43 @@ CMAKE_FLAGS="-DCMAKE_C_COMPILER=mpicc \
 # Configure and build cpu real AoS
 echo ""
 echo ""
-echo "building qmcpack for cpu AoS real for cades"
+echo "building qmcpack for cpu AoS real for CADES SHPC Condo"
 mkdir -p build_cades_cpu_real
 cd build_cades_cpu_real
 cmake $CMAKE_FLAGS ..
-make -j 16 
+make -j 16
 cd ..
 ln -sf ./build_cades_cpu_real/bin/qmcpack ./qmcpack_cades_cpu_real
 
 # Configure and build cpu complex AoS
 echo ""
 echo ""
-echo "building qmcpack for cpu AoS complex for cades"
+echo "building qmcpack for cpu AoS complex for CADES SHPC Condo"
 mkdir -p build_cades_cpu_comp
 cd build_cades_cpu_comp
 cmake -DQMC_COMPLEX=1 $CMAKE_FLAGS ..
-make -j 16 
+make -j 16
 cd ..
 ln -sf ./build_cades_cpu_comp/bin/qmcpack ./qmcpack_cades_cpu_comp
-
 
 # Configure and build cpu real SoA
 echo ""
 echo ""
-echo "building qmcpack for cpu SoA real for cades"
+echo "building qmcpack for cpu SoA real for CADES SHPC Condo"
 mkdir -p build_cades_cpu_real_SoA
 cd build_cades_cpu_real_SoA
 cmake -DENABLE_SOA=1 $CMAKE_FLAGS ..
-make -j 16 
+make -j 16
 cd ..
 ln -sf ./build_cades_cpu_real_SoA/bin/qmcpack ./qmcpack_cades_cpu_real_SoA
 
 # Configure and build cpu complex SoA
 echo ""
 echo ""
-echo "building qmcpack for cpu SoA complex for cades"
+echo "building qmcpack for cpu SoA complex for CADES SHPC Condo"
 mkdir -p build_cades_cpu_comp_SoA
 cd build_cades_cpu_comp_SoA
 cmake -DQMC_COMPLEX=1 -DENABLE_SOA=1 $CMAKE_FLAGS ..
-make -j 16 
+make -j 16
 cd ..
 ln -sf ./build_cades_cpu_comp_SoA/bin/qmcpack ./qmcpack_cades_cpu_comp_SoA
-
