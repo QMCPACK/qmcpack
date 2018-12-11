43c38f89638cf3550d3270d0a7ed9f962c0d16c4
Mark Dewing <markdewing@gmail.com>
2018-12-11 17:15:22 -0600

    Fix PGI compiler build with GCC 7
    Move the CMake C++ standard settings before the compiler-specific configuration files.
    The compiler configs can then override these settings if necessary.

--- CMake/PGICompilers.cmake
+++ CMake/PGICompilers.cmake
@@ -24,6 +24,12 @@ SET( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fast" )
 SET( CMAKE_C_FLAGS_RELWITHDEBINFO     "${CMAKE_C_FLAGS_RELWITHDEBINFO} -fast" )
 SET( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -fast" )
 
+
+# Setting this to 'OFF' adds the -A flag, which enforces strict standard compliance
+#  and causes the compilation to fail with some GNU header files
+SET(CMAKE_CXX_EXTENSIONS ON)
+
+
 # Add static flags if necessary
 IF(QMC_BUILD_STATIC)
     SET(CMAKE_CXX_LINK_FLAGS " -Bstatic")
