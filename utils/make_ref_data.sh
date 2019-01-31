e2218b2981c06b8832c1bb9a1d6a521e9be52351
Peter Doak <doakpw@ornl.gov>
2019-01-31 17:35:59 -0500

    update to get deterministic error bars from make_ref_data.sh
    in general the Gaussian_pp long ref data and tests will need
    additional work.

--- utils/make_ref_data.sh
+++ utils/make_ref_data.sh
@@ -46,12 +46,13 @@ TEST_PARENT=$(dirname $PWD)
 echo `pwd`
 echo ${run_file}
 echo ${ref_file}
-${QMCA} --fp=24.12f $run_file 2>/dev/null | gawk -e '/\s\s[a-zA-Z]+\s+=/ {print $1 " " $3 " " $5}' > run_vals
-${QMCA} --fp=24.12f $ref_file 2>/dev/null | gawk -e '/\s\s[a-zA-Z]+\s+=/ {print $1 " " $3 " " $5}' > ref_vals
+${QMCA} --fp=24.12f -e 3 $run_file 2>/dev/null | gawk -e '/\s\s[a-zA-Z]+\s+=/ {print $1 " " $3 " " $5}' > run_vals
+${QMCA} --fp=24.12f -e 3 $ref_file 2>/dev/null | gawk -e '/\s\s[a-zA-Z]+\s+=/ {print $1 " " $3 " " $5}' > ref_vals
 
 PYTHONFILE=temp.py
 (
 cat <<'EOF'
+import numpy as np
 import math
 import sys
 
@@ -71,9 +72,9 @@ rlines = [ line.split(' ') for line in f.readlines() ]
 
 ref_data = []
 for ref, short in zip(rlines, slines):
-    combined = math.sqrt(float(ref[2])**2 + float(short[2])**2)
+    combined = math.sqrt(np.float64(ref[2])**2 + np.float64(short[2])**2)
     if ref[0] in why_why_why:
-        ref_data.append([why_why_why[ref[0]], float(ref[1]), float(combined)])
+        ref_data.append([why_why_why[ref[0]], np.float64(ref[1]), np.float64(combined)])
 print(ref_data)
 for rd in ref_data:
     print ("LIST(APPEND {0}_SCALARS \"{1}\" \"{2:.12f} {3:.12f}\")".format(SCALAR_TAG, rd[0], rd[1], rd[2]))
