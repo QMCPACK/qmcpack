d8a155a98a29773325b16588911fd24176043c28
Ye Luo <yeluo@anl.gov>
2019-02-17 02:03:20 -0600

    check-affinity prints threads in order.

--- src/QMCTools/check-affinity.cpp
+++ src/QMCTools/check-affinity.cpp
@@ -60,8 +60,8 @@ int main()
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 #endif
 
-  if (rank==0)
-    std::cout << "Note: -1 means the property is not accessible on this platform." << std::endl;
+  bool hwthread_id_supported = (get_hwthread()!=-1);
+
   #pragma omp parallel
   {
     int L1_thread_id  = omp_get_thread_num();
@@ -69,20 +69,36 @@ int main()
     #pragma omp parallel
     {
       #pragma omp master
-      if (L1_thread_id==0)
+      if(L1_thread_id==0)
         std::cout << "OpenMP enables " << L1_num_thread << " 1st level threads, "
                   << "and " << omp_get_num_threads() << " 2nd level threads." << std::endl << std::endl;
     }
-
     #pragma omp barrier
 
-    #pragma omp parallel
+    for(int i=0; i<L1_num_thread; i++)
     {
-      int L2_thread_id  = omp_get_thread_num();
-      std::ostringstream o;
-      o << "MPI rank " << rank << " L1 tid " << L1_thread_id << " L2 tid " << L2_thread_id
-        << " is placed on Core ID " << get_core() << " HW thread ID " << get_hwthread() << std::endl;
-      std::cout << o.str();
+      if(L1_thread_id==i)
+      {
+        #pragma omp parallel
+        {
+          int L2_thread_id  = omp_get_thread_num();
+          int L2_num_thread = omp_get_num_threads();
+          for(int j=0; j<L2_num_thread; j++)
+          {
+            if(L2_thread_id==j)
+            {
+              std::ostringstream o;
+              o << "MPI rank " << rank << " L1 tid " << L1_thread_id << " L2 tid " << L2_thread_id
+                << " is placed on Core ID " << get_core();
+              if(hwthread_id_supported) o << " HW thread ID " << get_hwthread();
+              o << std::endl;
+              std::cout << o.str();
+            }
+            #pragma omp barrier
+          }
+        }
+      }
+      #pragma omp barrier
     }
   }
 #ifdef HAVE_MPI
