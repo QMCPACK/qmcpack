a9921ee58c14ce6a8742a7f18244a9c1dc757fd2
Jaron Krogel <krogeljt@ornl.gov>
2019-02-19 09:27:36 -0500

    nexus: format gp code

--- nexus/lib/gaussian_process2.py
+++ nexus/lib/gaussian_process2.py
@@ -14,6 +14,9 @@ class GParmFlags:
     Define an empty class to store GP parameters and information
     """
     pass
+#end class GParmFlags
+
+
 
 def lhs(n, samples=None, iterations=None):
     """
@@ -52,14 +55,16 @@ def lhs(n, samples=None, iterations=None):
     
     if samples is None:
         samples = n
+    #end if
     
     if iterations is None:
         iterations = 5
-    
+    #end if
+
     H = _lhsmaximin(n, samples, iterations)
     
-    
     return H
+#end def lhs
 
 ###############################################################################
 
@@ -74,15 +79,15 @@ def _lhsclassic(n, samples):
     rdpoints = np.zeros_like(u)
     for j in range(n):
         rdpoints[:, j] = u[:, j]*(b-a) + a
-    
+    #end for
     # Make the random pairings
     H = np.zeros_like(rdpoints)
     for j in range(n):
         order = np.random.permutation(range(samples))
         H[:, j] = rdpoints[order, j]
-    
+    #end for
     return H
-    
+#end def _lhsclassic
     
 ###############################################################################
 
@@ -97,9 +102,10 @@ def _lhsmaximin(n, samples, iterations):
         if maxdist<np.min(d):
             maxdist = np.min(d)
             H = Hcandidate.copy()
-    
+        #end if
     return H
-    
+#end def _lhsmaximin
+
 ###############################################################################
 
 def _pdist(x):
@@ -139,20 +145,20 @@ def _pdist(x):
     m, n = x.shape
     if m<2:
         return []
-    
+    #end if
     d = np.zeros((m**2-m)/2)
     c_v = 0
     for i in range(m - 1):
         d[0+c_v:m-i-1+c_v]=np.sqrt(np.sum((np.repeat(np.reshape(x[i, :],\
                         [1,n]),m-i-1,axis=0)-x[i+1:m+1, :])**2,axis=1))
         c_v = c_v+m-i-1
-    
+    #end for
     return d
+#end def _pdist
 
 ###############################################################################
     
 def _fdist(x):
-
     """
     Calculate Distance between all points
     
@@ -173,9 +179,11 @@ def _fdist(x):
     for jn in range(n):
         Dm[jn,:] = np.sqrt(np.sum((np.repeat(np.reshape(x[jn,:],[1,d]),\
              n,axis=0)-x)**2,axis=1))
-    
+    #end for
     
     return Dm
+#end def _fdist
+
 ###############################################################################
     
 def opt_surface_f(x):
@@ -197,12 +205,11 @@ def opt_surface_f(x):
     #y = np.reshape(np.prod(np.sin(2*np.pi*x**2),axis=1),[n,1])
 
     return y
-
+#end def opt_surface
 
 ###############################################################################
 
 def cfgp(r,ep):
-
     """
     Calculate correlation function for Gaussian Process
     
@@ -221,11 +228,11 @@ def cfgp(r,ep):
     y = np.exp(-ep*r**2)
     
     return y
+#end def cfgp
 
 ###############################################################################
     
 def cmgp(S1,S2,ep):
-
     """
     Calculate correlation matrix for Gaussian Process
     
@@ -249,15 +256,15 @@ def cmgp(S1,S2,ep):
     for jn in range(n):
         K[jn,:] = np.sqrt(np.sum((np.repeat(np.reshape(S1[jn,:],[1,d]),\
              m,axis=0)-S2)**2,axis=1))
-    
+    #end for
     K = cfgp(K,ep)
     
     return K
+#end def cmgp
 
 ###############################################################################
 
 def find_hyperparameters(mol_pos,E,dE):
-
     """
     Finds best Gaussian Process hyperparameters
     
@@ -301,7 +308,7 @@ def find_hyperparameters(mol_pos,E,dE):
         sigma_calc = np.reshape(np.maximum(sigma_calc,(sigma/10)**2),[Nc,1])
         b_loocv_err = np.sum(-(loocv**2)/sigma_calc-np.log(sigma_calc)/2)- \
             Nc*np.sum(np.abs(np.abs(E-np.matmul(K,Co_GP))-dE)/dE)
-            
+    #end if
             
     while d_ep>tol and d_sigma>tol_sigma:
         did_move = 0;
@@ -318,17 +325,19 @@ def find_hyperparameters(mol_pos,E,dE):
             sigma_calc = np.reshape(np.maximum(sigma_calc,(sigma/10)**2),[Nc,1])
             c_loocv_err = np.sum(-(loocv**2)/sigma_calc-np.log(sigma_calc)/2)- \
                 Nc*np.sum(np.abs(np.abs(E-np.matmul(K,Co_GP))-dE)/dE)
-            
+        #end if 
             
         if c_loocv_err>b_loocv_err:
             b_loocv_err = c_loocv_err
             ep = ep_c
             did_move = 1
-            
+        #end if
+
         ep_c = ep-d_ep
         if ep_c<=0:
             ep_c = ep
-            
+        #end if
+
         K = cfgp(D,ep_c)
         Kw_inv = np.linalg.pinv(K+ sigma*W)
         Co_GP = np.matmul(Kw_inv,E)
@@ -341,18 +350,20 @@ def find_hyperparameters(mol_pos,E,dE):
             sigma_calc = np.reshape(np.maximum(sigma_calc,(sigma/10)**2),[Nc,1])
             c_loocv_err = np.sum(-(loocv**2)/sigma_calc-np.log(sigma_calc)/2)- \
                 Nc*np.sum(np.abs(np.abs(E-np.matmul(K,Co_GP))-dE)/dE)
-            
+        #end if
             
         if c_loocv_err>b_loocv_err:
             b_loocv_err = c_loocv_err
             ep = ep_c
             did_move = 1
-            
+        #end if
+
         if did_move == 1:
             d_ep = 1.25*d_ep
         else:
             d_ep = .75*d_ep
-            
+        #end if
+
         if sigma>0:
             did_move = 0;
             sigma_c = sigma+d_sigma
@@ -371,11 +382,13 @@ def find_hyperparameters(mol_pos,E,dE):
                 b_loocv_err = c_loocv_err
                 sigma = sigma_c
                 did_move = 1
-                
+            #end if
+ 
             sigma_c = sigma-d_sigma
             if sigma_c<=0:
                 sigma_c = sigma
-                
+            #end if
+
             Kw_inv = np.linalg.pinv(K+ sigma_c*W)
             Co_GP = np.matmul(Kw_inv,E)
             loocv = Co_GP/np.reshape(np.diag(Kw_inv),[Nc,1])
@@ -390,24 +403,25 @@ def find_hyperparameters(mol_pos,E,dE):
                 b_loocv_err = c_loocv_err
                 sigma = sigma_c
                 did_move = 1
-                
+            #end if
+
             if did_move == 1:
                 d_sigma = 1.25*d_sigma
             else:
                 d_sigma = .75*d_sigma
-        
-        
+            #end if
+        #end if
         K = cfgp(D,ep)
         Kw_inv = np.linalg.pinv(K+ sigma*W)
         Co_GP = np.matmul(Kw_inv,E)
-        
-    return ep,sigma,Co_GP 
+    #end while
 
+    return ep,sigma,Co_GP 
+#end def find_hyperparameters
 
 ###############################################################################
 
 def find_min(Co_GP,mol_pos,E,dE,ep):
-
     """
     Finds local minimum of Gaussian Process
     
@@ -447,31 +461,38 @@ def find_min(Co_GP,mol_pos,E,dE,ep):
                 ind_T = T.find_simplex(c_pos[jp,:])
                 if ind_T == -1:
                     c_pos[jp,jd]= best_pos[jp,jd]
-                    
+                #end if
+
                 Kc = cmgp(np.reshape(c_pos[jp,:],[1,d]),mol_pos,ep)
                 c_E = np.matmul(Kc,Co_GP)
                 if c_E<best_E[jp]:
                     best_E[jp] = c_E
                     best_pos = np.copy(c_pos)
                     did_move = 1
-                    
+                #end if
+
                 c_pos = np.copy(best_pos)
                 c_pos[jp,jd]=best_pos[jp,jd]-delta_pos[jp,jd]
                 ind_T = T.find_simplex(c_pos[jp,:])
                 if ind_T == -1:
                     c_pos[jp,jd]= best_pos[jp,jd]
+                #end if
                 Kc = cmgp(np.reshape(c_pos[jp,:],[1,d]),mol_pos,ep)
                 c_E = np.matmul(Kc,Co_GP)
                 if c_E<best_E[jp]:
                     best_E[jp] = c_E
                     best_pos = np.copy(c_pos)
                     did_move = 1
-                    
+                #end if
+
                 if did_move == 1:
                     delta_pos[jp,jd] = 1.25*delta_pos[jp,jd]
                 else:
                     delta_pos[jp,jd] = 0.75*delta_pos[jp,jd]
-                
+                #end if
+            #end for
+        #end for
+    #end while
      
     ## Remove in same simplex ##
     jp = 0
@@ -486,9 +507,9 @@ def find_min(Co_GP,mol_pos,E,dE,ep):
             best_E = np.delete(best_E,us_ind[1:us_ind.shape[0],0],0)
             best_pos = np.delete(best_pos,us_ind[1:us_ind.shape[0],0],0)
             ind_T = np.delete(ind_T,us_ind[1:us_ind.shape[0],0],0)
-            
+        #end if
         jp += 1
-        
+    #end while
     jp = 0
     while jp<best_pos.shape[0]:
         c_dis = np.sqrt(np.sum((np.repeat(np.reshape(best_pos[jp,:],[1,d]),\
@@ -500,9 +521,9 @@ def find_min(Co_GP,mol_pos,E,dE,ep):
             best_pos[us_ind[0,0],:] = best_pos[us_ind[min_ind,0],:]
             best_E = np.delete(best_E,us_ind[1:us_ind.shape[0],0],0)
             best_pos = np.delete(best_pos,us_ind[1:us_ind.shape[0],0],0)
-            
+        #end if
         jp += 1
-    
+    #end while
     ind_T = T.find_simplex(best_pos)
     ind_T = np.reshape(ind_T,[ind_T.shape[0],1])
     
@@ -514,23 +535,22 @@ def find_min(Co_GP,mol_pos,E,dE,ep):
                 ind_rm = 1
             else:
                 ind_rm = ind_rm[0,0]
-                
+            #end if  
             ind_sort = ind_sort[0:ind_rm]
-        
+        #end if
         best_E = best_E[ind_sort,:]
         best_pos = best_pos[ind_sort,:]
         ind_T = ind_T[ind_sort,:]
-            
+    #end if
     neig_data = E[T.simplices[ind_T[:,0],:],0]
     neig_data = np.append(neig_data,dE[T.simplices[ind_T[:,0],:],0],axis=0)
-    
         
     return best_pos,best_E,neig_data
-
+#end def find_min
 
 ###############################################################################
-def find_new_points(mol_pos,best_pos,delta_r,Nc0):
 
+def find_new_points(mol_pos,best_pos,delta_r,Nc0):
     """
     Estimate optimal new points for Gaussian Process
     
@@ -582,14 +602,16 @@ def find_new_points(mol_pos,best_pos,delta_r,Nc0):
             if c_mes>b_mes:
                 new_points = np.copy(cnew_points)
                 b_mes = c_mes
-            
+            #end if
+        #end for
+    #end for
     
     return new_points
+#end def find_new_points
     
 ###############################################################################
 
 def multiscale_E(E,dE,scale_thres):
-
     """
     Multiscale normalization of E
     
@@ -620,19 +642,20 @@ def multiscale_E(E,dE,scale_thres):
             E_scaled[us_ind[:,0],0] = scale_thres*(E_scaled[us_ind[:,0],0]-1)/(E_b-1)+1
         else:
             E_b= 0
+        #end if
     else:
         E_scaled = E
         dE_scaled = dE
         E_a = 0
         E_b= 0
-
+    #end if
         
     return E_a,E_b,E_scaled,dE_scaled
+#end def multiscale_E
 
 ###############################################################################
 
 def inv_multiscale_E(E_a,E_b,E_scaled,scale_thres):
-
     """
     Reverses Multiscale normalization of E
     
@@ -655,18 +678,18 @@ def inv_multiscale_E(E_a,E_b,E_scaled,scale_thres):
         if us_ind.shape[0]>0:
             if E_b!=0:
                 E_scaled[us_ind[:,0],0] = (E_scaled[us_ind[:,0],0]-1)*(E_b-1)/scale_thres+1
-        
+            #end if
             E_scaled[us_ind[:,0],0] = np.exp(E_scaled[us_ind[:,0],0]-1)
-            
+        #end if
         E = scale_thres*E_scaled+E_a
-            
+    #end if
         
     return E
+#end def inv_multiscale_E
 
 ###############################################################################
     
 def inv_norm_pos(mol_pos_norm,hyper_cube):
-
     """
     Reverses normalization of molecular positions to unit hyper cube
     
@@ -687,14 +710,12 @@ def inv_norm_pos(mol_pos_norm,hyper_cube):
                 ,n,axis=0)*mol_pos_norm+ \
                 np.repeat(np.reshape(hyper_cube[:,0],[1,d]),n,axis=0)
 
-        
     return mol_pos
-
+#end def inv_norm_pos
 
 ###############################################################################
     
 def norm_pos(mol_pos,hyper_cube):
-
     """
     Reverses normalization of molecular positions to unit hyper cube
     
@@ -716,6 +737,7 @@ def norm_pos(mol_pos,hyper_cube):
 
         
     return mol_pos_norm
+#end def norm_pos
 
 ###############################################################################
 
@@ -750,45 +772,44 @@ def gp_sdg_setup(GP_info):
         GP_info.n_its
     except:
         GP_info.n_its = 5               # Number of maximum iterations
-        
+    #end try
     try:
         GP_info.do_vis
     except:
         GP_info.do_vis = 1              # Visualize Error
-        
+    #end try
     try:
         GP_info.do_scale_energy
     except:
         GP_info.do_scale_energy = 0     # Scale energy   
-        
+    #end try
     try:
         GP_info.scale_thres
     except:
         GP_info.scale_thres = 0.        # Energy scale paramter      
-        
-        
+    #end try
     try:
         GP_info.Nc0
     except:
-        GP_info.Nc0 = (d+2)*(d+1)       # Number of points to add per iteration  
-        
+        GP_info.Nc0 = (d+2)*(d+1)       # Number of points to add per iteration
+    #end try
     try:
         GP_info.delta_r
     except:
         GP_info.delta_r = 1.0           # Initial Radius of Convergence     
-    
+    #end try
     try:
         GP_info.delta_r_s
     except:
         GP_info.delta_r_s = 2.0         # Radius shrink factor
-    
-    
+    #end try
     try:
         GP_info.n_min_d
     except:
         GP_info.n_min_d = 5             # Number of times to resample LHC 
-    
+    #end try
     return E,dE,mol_pos,best_pos,best_E,GP_info
+#end def gp_sdg_setup
 
 ###############################################################################
 
@@ -840,7 +861,8 @@ def gp_sgd(E,dE,mol_pos,GP_info):
         else:
             ep,sigma,Co_GP = find_hyperparameters(mol_pos_norm,E,dE)
             best_pos_norm,best_E,neig_data = find_min(Co_GP,mol_pos_norm,E,dE,ep)
-            
+        #end if
+
         best_pos = inv_norm_pos(best_pos_norm,GP_info.hyper_cube)
         
         new_points = find_new_points(mol_pos_norm,best_pos_norm,GP_info.delta_r,GP_info.Nc0)
@@ -855,7 +877,8 @@ def gp_sgd(E,dE,mol_pos,GP_info):
             for jx in range(neig_bool.shape[0]):
                 for jy in range(neig_bool.shape[1]):
                     neig_percent[jx,jy] = float(neig_bool[jx,jy])
-                
+                #end for
+            #end for
             neig_percent = 100*np.mean(neig_percent,axis=1)
             x = np.zeros((d+1)*best_E.shape[0])
             y = np.zeros((d+1)*best_E.shape[0])
@@ -866,7 +889,8 @@ def gp_sgd(E,dE,mol_pos,GP_info):
                 y[jx*(d+1):(jx+1)*(d+1)] = neig_data[jx,:]
                 dy[jx*(d+1):(jx+1)*(d+1)]= neig_data[jx+best_E.shape[0],:]
                 Ex[jx] = jx
-                
+            #end for
+
             plt.figure()
             plt.errorbar(x, y, yerr=dy, fmt='o',label='sampled point with uncertainty')
             plt.plot(Ex,best_E[:,0], 'r+',label='estimated minumum')
@@ -874,11 +898,11 @@ def gp_sgd(E,dE,mol_pos,GP_info):
             plt.title('Neigborhood Information')
             plt.xlabel('Local Minimum')
             plt.ylabel('E')
-            
-            
-         
+        #end if
+    #end if
         
     return new_points,best_pos,best_E,GP_info
+#end def gp_sgd
 
 ###############################################################################
     
@@ -906,13 +930,16 @@ try:
     E,dE,mol_pos,best_pos,best_E,GP_info = gp_sdg_setup(GP_info)
 except:
     print 'Please Define GP_info.hyper_cube'
-    exit
+    exit()
+#end try
 
 ## Do Gausian Process Iteration ##
     
 for jits in range(GP_info.n_its):
     GP_info.jits = jits
-    new_points,best_pos,best_E,GP_info = gp_sgd(E,dE,mol_pos,GP_info) 
+    new_points,best_pos,best_E,GP_info = gp_sgd(E,dE,mol_pos,GP_info)
+    print jits,'best_P',best_pos
+    print jits,'best_E',best_E
     if jits < GP_info.n_its-1:
         ## Simulate random uncertainty in optimization function
         new_dE = (sigma_b-sigma_a)*np.random.rand(GP_info.Nc0,1)+sigma_a
@@ -925,4 +952,6 @@ for jits in range(GP_info.n_its):
             mol_pos = np.append(mol_pos,new_points,axis=0)
             E = np.append(E,new_E,axis=0)
             dE = np.append(dE,new_dE,axis=0)
-        
+        #end if
+    #end if
+#end for
