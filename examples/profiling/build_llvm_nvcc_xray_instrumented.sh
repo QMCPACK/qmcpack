1d803b69f59737ebe51055e5394087327645be4d
Peter Doak <doakpw@ornl.gov>
2019-03-05 10:44:39 -0500

    removing XRAY_GPU_MOST option from example

--- examples/profiling/build_llvm_nvcc_xray_instrumented.sh
+++ examples/profiling/build_llvm_nvcc_xray_instrumented.sh
@@ -27,7 +27,7 @@ spack load ninja
 spack load llvm@4.0.1 ^ncurses+termlib
 spack load zlib%clang@4.0.1
  
-CXXFLAGS="-stdlib=libstdc++" LDFLAGS="-stdlib=libstdc++" cmake -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_C_COMPILER=mpicc -DENABLE_MKL=1 -DMKL_ROOT="/opt/intel2018/mkl" -GNinja -DQMC_MPI=1 -DQMC_CUDA=1 -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-9.1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCUDA_NVCC_FLAGS="-arch=sm_60;-Drestrict=__restrict__;-DNO_CUDA_MAIN;-O3" -DHDF5_ROOT=/data/epd/spack/opt/spack/linux-rhel7-x86_64/gcc-4.8.5/hdf5-1.10.2-qqmot24bg6uetn3xhpxjlwafvxr4p5pp/ -DXRAY_PROFILE=1 -DXRAY_INSTRUCTION_THRESHOLD=50 -DXRAY_GPU_MOST=1 ..
+CXXFLAGS="-stdlib=libstdc++" LDFLAGS="-stdlib=libstdc++" cmake -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_C_COMPILER=mpicc -DENABLE_MKL=1 -DMKL_ROOT="/opt/intel2018/mkl" -GNinja -DQMC_MPI=1 -DQMC_CUDA=1 -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-9.1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCUDA_NVCC_FLAGS="-arch=sm_60;-Drestrict=__restrict__;-DNO_CUDA_MAIN;-O3" -DHDF5_ROOT=/data/epd/spack/opt/spack/linux-rhel7-x86_64/gcc-4.8.5/hdf5-1.10.2-qqmot24bg6uetn3xhpxjlwafvxr4p5pp/ -DXRAY_PROFILE=1 -DXRAY_INSTRUCTION_THRESHOLD=50 ..
 
 ninja
 
