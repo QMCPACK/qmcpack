1c5c4c5c6164d8977b6d9a4d7fa6baff49d2d930
Jaron Krogel <krogeljt@ornl.gov>
2019-03-11 15:10:05 -0400

    nexus: add tiling tests and fix

--- nexus/lib/structure.py
+++ nexus/lib/structure.py
@@ -266,6 +266,9 @@ def reduce_tilematrix(tiling):
     dim = len(t)
     matrix_tiling = t.shape == (dim,dim)
     if matrix_tiling:
+        if abs(det(t))==0:
+            Structure.class_error('requested tiling matrix is singular\ntiling requested: {0}'.format(t))
+        #end if
         #find a tiling tuple from the tiling matrix
         # do this by shearing the tiling matrix (or equivalently the tiled cell)
         # until it is orthogonal (in the untiled cell axes)
@@ -276,7 +279,6 @@ def reduce_tilematrix(tiling):
         T = t  #tiling matrix
         tilematrix = T.copy()
         del t
-        Tnew = array(T,dtype=float) #sheared/orthogonal tiling matrix
         tbar = identity(dim) #basis for shearing
         dr = range(dim)
         #dr = [1,0,2]
@@ -287,8 +289,10 @@ def reduce_tilematrix(tiling):
         #move each axis to be parallel to barred directions
         # these are volume preserving shears of the supercell
         # each shear keeps two cell face planes fixed while moving the others
+        tvecs = []
         for dp in [(0,1,2),(2,0,1),(1,2,0),(2,1,0),(0,2,1),(1,0,2)]:
             success = True
+            Tnew = array(T,dtype=float) #sheared/orthogonal tiling matrix
             for d in dr:
                 tb = tbar[dp[d]] 
                 t  = T[d]
@@ -304,29 +308,32 @@ def reduce_tilematrix(tiling):
                 Tnew[d] = tn
             #end for
             if success:
-                break
+                # apply inverse permutation, if needed
+                Tn = Tnew.copy()
+                for d in dr:
+                    d2 = dp[d]
+                    Tnew[d2] = Tn[d]
+                #end for
+                #the resulting tiling matrix should be diagonal and integer
+                tr = diag(Tnew)
+                nondiagonal = abs(Tnew-diag(tr)).sum()>1e-6
+                if nondiagonal:
+                    Structure.class_error('could not find a diagonal tiling matrix for generating tiled coordinates')
+                #end if
+                tvecs.append(abs(tr))
             #end if
         #end for
-        # apply inverse permutation, if needed
-        Tn = Tnew.copy()
-        for d in dr:
-            d2 = dp[d]
-            Tnew[d2] = Tn[d]
+        tvecs_old = tvecs
+        tvecs = []
+        tvset = set()
+        for tv in tvecs_old:
+            tvk = tuple(array(around(1e7*tv),dtype=uint64))
+            if tvk not in tvset:
+                tvset.add(tvk)
+                tvecs.append(tv)
+            #end if
         #end for
-        #the resulting tiling matrix should be diagonal and integer
-        tr = diag(Tnew)
-        t  = array(around(tr),dtype=int)
-        nondiagonal = abs(Tnew-diag(tr)).sum()>1e-6
-        noninteger  = abs(tr-t).sum()>1e-6
-        if nondiagonal:
-            Structure.class_error('could not find a diagonal tiling matrix for generating tiled coordinates')
-        #end if
-        #non-integer tile vectors are handled directly by tile_points now
-        #if noninteger:
-        #    Structure.class_error('calculated diagonal tiling matrix is non-integer\n  tiled coordinates cannot be determined')
-        ##end if
-        #tilevector = abs(t)
-        tilevector = abs(tr)
+        tilevector = array(tvecs)
     else:
         tilevector = t
         tilematrix = diag(t)
@@ -334,6 +341,7 @@ def reduce_tilematrix(tiling):
 
     return tilematrix,tilevector
 #end def reduce_tilematrix
+
 
 
 def tile_magnetization(mag,tilevec,mag_order,mag_prim):
@@ -348,7 +356,7 @@ def tile_magnetization(mag,tilevec,mag_order,mag_prim):
     #  if magnetic primitive is requested, a small tiling vector should first be used
     #   (ie 221,212,122,222 periods all involve a 211 prim tiling w/ a simple reshaping/reassignment of the cell axes
     #  Structure should have a function providing labels to each magnetic species
-    mag = array(int(round( tilevec.prod() ))*list(mag),dtype=object)
+    #mag = array(int(round( tilevec.prod() ))*list(mag),dtype=object)
     return mag
 #end def tile_magnetization
 
@@ -3029,6 +3037,48 @@ class Structure(Sobj):
         if tilevec is None:
             tilemat,tilevec = reduce_tilematrix(tilemat)
         #end if
+        if not isinstance(tilemat,ndarray):
+            tilemat = array(tilemat)
+        #end if
+        matrix_tiling = abs(tilemat-diag(diag(tilemat))).sum()>0.1
+        if not matrix_tiling:
+            return self.tile_points_simple(points,axes,diag(tilemat))
+        else:
+            if not isinstance(axes,ndarray):
+                axes = array(axes)
+            #end if
+            if not isinstance(tilevec,ndarray):
+                tilevec = array(tilevec)
+            #end if
+            dim     = len(axes)
+            npoints = len(points)
+            ntpoints = npoints*int(round(abs(det(tilemat))))
+            if tilevec.size==dim:
+                tilevec.shape = 1,dim
+            #end if
+            taxes = dot(tilemat,axes)
+            success = False
+            for tvec in tilevec:
+                tpoints = self.tile_points_simple(points,axes,tvec)
+                tpoints,weights,pmap = self.unique_points_fast(tpoints,taxes)
+                if len(tpoints)==ntpoints:
+                    success = True
+                    break
+                #end if
+            #end for
+            if not success:
+                tpoints = self.tile_points_brute(points,axes,tilemat)
+                tpoints,weights,pmap = self.unique_points_fast(tpoints,taxes)
+                if len(tpoints)!=ntpoints:
+                    self.error('brute force tiling failed')
+                #end if
+            #end if
+        #end if
+        return tpoints
+    #end def tile_points
+
+
+    def tile_points_simple(self,points,axes,tilevec):
         if not isinstance(points,ndarray):
             points = array(points)
         #end if
@@ -3038,57 +3088,64 @@ class Structure(Sobj):
         if not isinstance(axes,ndarray):
             axes = array(axes)
         #end if
-        t = tilevec
-        ti = array(around(t),dtype=int)
-        noninteger = abs(t-ti).sum()>1e-6
         if len(points.shape)==1:
             npoints,dim = len(points),1
         else:
             npoints,dim = points.shape
         #end if
-        ntpoints = npoints*int(round( t.prod() ))
-        if not noninteger:
-            t = ti
-            if ntpoints==0:
-                tpoints = array([])
-            else:
-                tpoints  = empty((ntpoints,dim))
-                ns=0
-                ne=npoints
-                for k in range(t[2]):
-                    for j in range(t[1]):
-                        for i in range(t[0]):
-                            v = dot(array([[i,j,k]]),axes)
-                            for d in range(dim):
-                                tpoints[ns:ne,d] = points[:,d]+v[0,d]
-                            #end for
-                            ns+=npoints 
-                            ne+=npoints
-                        #end for
-                    #end for
-                #end for
-            #end if
-        else:
-            if abs(ntpoints-int(around(ntpoints)))>1e-6:
+        t = tilevec
+        ti = array(around(t),dtype=int)
+        noninteger = abs(t-ti).sum()>1e-6
+        if noninteger:
+            tp = t.prod()
+            if abs(tp-int(tp))>1e-6:
                 self.error('tiling vector does not correspond to an integer volume change\ntiling vector: {0}\nvolume change: {1}  {2}  {3}'.format(tilevec,tilevec.prod(),ntpoints,int(ntpoints)))
             #end if
-            ntpoints = int(around(ntpoints))
-            # round up to larger tiling
-            #  +1 added for greater cell coverage
-            #  add more if error below is tripped w/ fewer points than expected
-            t = array(ceil(t),dtype=int)+1 
-            # get the tiled points
-            tpoints = self.tile_points(points,axes,tilemat,t)
-            # remove any that are not unique
-            taxes = dot(tilemat,axes)
-            #tpoints,weights,pmap = self.unique_points(tpoints,taxes)
-            tpoints,weights,pmap = self.unique_points_fast(tpoints,taxes)
-            if len(tpoints)!=ntpoints:
-                self.error('tiling by non-integer tiling vector failed\npoints expected after tiling: {0}\npoints resulted from tiling: {1}'.format(ntpoints,len(tpoints)))
-            #end if
+            t = array(ceil(t),dtype=int)+1
+        else:
+            t = ti
+        #end if
+        ntpoints = npoints*int(round( t.prod() ))
+        if ntpoints==0:
+            tpoints = array([])
+        else:
+            tpoints = empty((ntpoints,dim))
+            ns=0
+            ne=npoints
+            for k in range(t[2]):
+                for j in range(t[1]):
+                    for i in range(t[0]):
+                        v = dot(array([[i,j,k]]),axes)
+                        for d in range(dim):
+                            tpoints[ns:ne,d] = points[:,d]+v[0,d]
+                        #end for
+                        ns+=npoints 
+                        ne+=npoints
+                    #end for
+                #end for
+            #end for
         #end if
         return tpoints
-    #end def tile_points
+    #end def tile_points_simple
+
+
+    def tile_points_brute(self,points,axes,tilemat):
+        tcorners = [[0,0,0],
+                    [1,0,0],
+                    [0,1,0],
+                    [0,0,1],
+                    [0,1,1],
+                    [1,0,1],
+                    [1,1,0],
+                    [1,1,1]]
+        tcorners = dot(tcorners,tilemat)
+        tmin = tcorners.min(axis=0)
+        tmax = tcorners.max(axis=0)
+        tilevec = tmax-tmin
+        tpoints = self.tile_points_simple(points,axes,tilevec)
+        return tpoints
+    #end def tile_points_brute
+
 
 
     def opt_tilematrix(self,*args,**kwargs):
@@ -3102,9 +3159,10 @@ class Structure(Sobj):
     #end def tile_opt
 
 
-    def check_tiling(self,tol=1e-6):
+    def check_tiling(self,tol=1e-6,exit=True):
+        msg = ''
         if not self.is_tiled():
-            return
+            return msg
         #end if
         msgs = []
         st = self
@@ -3130,8 +3188,11 @@ class Structure(Sobj):
             for m in msgs:
                 msg += '\n'+m
             #end for
-            self.error(msg)
+            if exit:
+                self.error(msg)
+            #end if
         #end if
+        return msg
     #end def check_tiling
 
 
