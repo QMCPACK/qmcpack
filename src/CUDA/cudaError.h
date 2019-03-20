c0841c90f569e5a129da3f074a6012b7b1a8d102
Ye Luo <yeluo@anl.gov>
2019-03-20 17:10:57 -0500

    Add comments in cudaError

--- src/CUDA/cudaError.h
+++ src/CUDA/cudaError.h
@@ -20,15 +20,15 @@
 #include <cuda_runtime_api.h>
 
 #define cudaErrorCheck(ans, cause) { cudaAssert((ans), cause, __FILE__, __LINE__); }
-
-inline void cudaAssert(cudaError_t code, const std::string& cause, const char *file, int line, bool abort=true)
+/// prints CUDA error messages. Always use cudaErrorCheck macro.
+inline void cudaAssert(cudaError_t code, const std::string& cause, const char *filename, int line, bool abort=true)
 {
   if (code != cudaSuccess) 
   {
     std::ostringstream err;
     err << "cudaAssert: " << cudaGetErrorName(code)
         << " " << cudaGetErrorString(code)
-        << ", file " << file << ", line " << line << std::endl
+        << ", file " << filename << ", line " << line << std::endl
         << cause << std::endl;
     std::cerr << err.str();
     //if (abort) exit(code);
