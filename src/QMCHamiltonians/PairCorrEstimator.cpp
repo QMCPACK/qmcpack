4997243e06ab7b9125d2f6165390e287f18e36b5
Peter Doak <doakpw@ornl.gov>
2019-03-28 20:30:30 -0400

    clang-format QMCFactor, QMCHamiltonians

--- src/QMCHamiltonians/PairCorrEstimator.cpp
+++ src/QMCHamiltonians/PairCorrEstimator.cpp
@@ -12,8 +12,8 @@
 //
 // File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //////////////////////////////////////////////////////////////////////////////////////
-    
-    
+
+
 #include <QMCHamiltonians/PairCorrEstimator.h>
 #include <Particle/DistanceTableData.h>
 #include <OhmmsData/AttributeSet.h>
@@ -22,66 +22,64 @@
 
 namespace qmcplusplus
 {
-
-PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
-  :Dmax(10.), Delta(0.5), num_species(2)
+PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources) : Dmax(10.), Delta(0.5), num_species(2)
 {
-  UpdateMode.set(COLLECTABLE,1);
-  num_species=elns.groups();
-  n_vec.resize(num_species,0);
-  for(int i(0); i<num_species; i++)
-    n_vec[i]=1.0/(elns.last(i)-elns.first(i));
-  N_e=elns.getTotalNum();
+  UpdateMode.set(COLLECTABLE, 1);
+  num_species = elns.groups();
+  n_vec.resize(num_species, 0);
+  for (int i(0); i < num_species; i++)
+    n_vec[i] = 1.0 / (elns.last(i) - elns.first(i));
+  N_e = elns.getTotalNum();
   //use the simulation cell radius if any direction is periodic
-  if(elns.Lattice.SuperCellEnum)
+  if (elns.Lattice.SuperCellEnum)
   {
-    Dmax=elns.Lattice.WignerSeitzRadius;
-    Volume=elns.Lattice.Volume;
+    Dmax   = elns.Lattice.WignerSeitzRadius;
+    Volume = elns.Lattice.Volume;
   }
   else
-    Volume=1.0; //open bcs
-  NumBins=static_cast<int>(Dmax/Delta);
-  Delta=Dmax/static_cast<RealType>(NumBins);
-  DeltaInv=1.0/Delta;
+    Volume = 1.0; //open bcs
+  NumBins  = static_cast<int>(Dmax / Delta);
+  Delta    = Dmax / static_cast<RealType>(NumBins);
+  DeltaInv = 1.0 / Delta;
   //ostringstream h;
   //h<<"gofr_" << elns.getName();
   //gof_r_prefix.push_back(h.str());
-  std::map<int,int> pair_map;
-  int npairs=0;
-  for(int i=0; i<num_species; ++i)
-    for(int j=i; j<num_species; ++j, ++npairs)
+  std::map<int, int> pair_map;
+  int npairs = 0;
+  for (int i = 0; i < num_species; ++i)
+    for (int j = i; j < num_species; ++j, ++npairs)
     {
       std::ostringstream os;
       os << "gofr_" << elns.getName() << "_" << i << "_" << j;
       gof_r_prefix.push_back(os.str());
-      pair_map[i*num_species+j]=npairs;
+      pair_map[i * num_species + j] = npairs;
     }
-  const DistanceTableData&  dii(*elns.DistTables[0]);
-  if(dii.DTType == DT_AOS)
+  const DistanceTableData& dii(*elns.DistTables[0]);
+  if (dii.DTType == DT_AOS)
   {
     pair_ids.resize(dii.getTotNadj());
-    for(int iat=0; iat<dii.centers(); ++iat)
+    for (int iat = 0; iat < dii.centers(); ++iat)
     {
-      int ig=elns.GroupID[iat]*num_species;
-      for(int nn=dii.M[iat]; nn<dii.M[iat+1]; ++nn)
-        pair_ids[nn]=pair_map[ig+elns.GroupID[dii.J[nn]]];
+      int ig = elns.GroupID[iat] * num_species;
+      for (int nn = dii.M[iat]; nn < dii.M[iat + 1]; ++nn)
+        pair_ids[nn] = pair_map[ig + elns.GroupID[dii.J[nn]]];
     }
   }
   // source-target tables
   std::vector<std::string> slist, dlist;
-  for(int k=1; k<elns.DistTables.size(); ++k)
+  for (int k = 1; k < elns.DistTables.size(); ++k)
     dlist.push_back(elns.DistTables[k]->origin().getName());
-  parsewords(sources.c_str(),slist);
-  int ntables=elns.DistTables.size();
+  parsewords(sources.c_str(), slist);
+  int ntables = elns.DistTables.size();
   std::set<int> others_sorted;
-  for(int i=0; i<slist.size(); ++i)
+  for (int i = 0; i < slist.size(); ++i)
   {
-    int k=0;
-    while(k<dlist.size())
+    int k = 0;
+    while (k < dlist.size())
     {
-      if(slist[i] ==dlist[k])
+      if (slist[i] == dlist[k])
       {
-        others_sorted.insert(k+1);
+        others_sorted.insert(k + 1);
         break;
       }
       ++k;
@@ -89,16 +87,16 @@ PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
   }
   other_ids.resize(others_sorted.size());
   other_offsets.resize(others_sorted.size());
-  copy(others_sorted.begin(),others_sorted.end(),other_ids.begin());
-  int toff=gof_r_prefix.size();
-  for(int k=0; k<other_ids.size(); ++k)
+  copy(others_sorted.begin(), others_sorted.end(), other_ids.begin());
+  int toff = gof_r_prefix.size();
+  for (int k = 0; k < other_ids.size(); ++k)
   {
     const DistanceTableData& t(*elns.DistTables[other_ids[k]]);
     app_log() << "  GOFR for " << t.getName() << " starts at " << toff << std::endl;
-    other_offsets[k]=toff;
+    other_offsets[k] = toff;
     const SpeciesSet& species(t.origin().getSpeciesSet());
-    int ng=species.size();
-    for(int i=0; i<ng; ++i)
+    int ng = species.size();
+    for (int i = 0; i < ng; ++i)
     {
       std::ostringstream os;
       os << "gofr_" << t.getName() << "_" << species.speciesName[i];
@@ -108,81 +106,79 @@ PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
   }
 }
 
-void PairCorrEstimator::resetTargetParticleSet(ParticleSet& P)
-{
-}
+void PairCorrEstimator::resetTargetParticleSet(ParticleSet& P) {}
 
 PairCorrEstimator::Return_t PairCorrEstimator::evaluate(ParticleSet& P)
 {
   BufferType& collectables(P.Collectables);
-  const DistanceTableData&  dii(*P.DistTables[0]);
-  if(dii.DTType == DT_SOA)
+  const DistanceTableData& dii(*P.DistTables[0]);
+  if (dii.DTType == DT_SOA)
   {
-    for(int iat=1; iat<dii.centers(); ++iat)
+    for (int iat = 1; iat < dii.centers(); ++iat)
     {
-      const RealType* restrict dist=dii.Distances[iat];
-      const int ig=P.GroupID[iat];
-      for(int j=0; j<iat; ++j)
+      const RealType* restrict dist = dii.Distances[iat];
+      const int ig                  = P.GroupID[iat];
+      for (int j = 0; j < iat; ++j)
       {
-        const RealType r=dist[j];
-        if(r>=Dmax)
+        const RealType r = dist[j];
+        if (r >= Dmax)
           continue;
-        const int loc=static_cast<int>(DeltaInv*r);
-        const int jg=P.GroupID[j];
-        const int pair_id=ig*(ig+1)/2+jg;
-        collectables[pair_id*NumBins+loc+myIndex] += norm_factor(pair_id,loc);
+        const int loc     = static_cast<int>(DeltaInv * r);
+        const int jg      = P.GroupID[j];
+        const int pair_id = ig * (ig + 1) / 2 + jg;
+        collectables[pair_id * NumBins + loc + myIndex] += norm_factor(pair_id, loc);
       }
     }
-    for(int k=0; k<other_ids.size(); ++k)
+    for (int k = 0; k < other_ids.size(); ++k)
     {
-      const DistanceTableData&  d1(*P.DistTables[other_ids[k]]);
+      const DistanceTableData& d1(*P.DistTables[other_ids[k]]);
       const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
-      int koff=other_offsets[k];
-      RealType overNI=1.0/d1.centers();
-      for(int iat=0; iat<d1.targets(); ++iat)
+      int koff        = other_offsets[k];
+      RealType overNI = 1.0 / d1.centers();
+      for (int iat = 0; iat < d1.targets(); ++iat)
       {
-        const RealType* restrict dist=d1.Distances[iat];
-        for(int j=0; j<d1.centers(); ++j)
+        const RealType* restrict dist = d1.Distances[iat];
+        for (int j = 0; j < d1.centers(); ++j)
         {
-          const RealType r=dist[j];
-          if(r>=Dmax)
+          const RealType r = dist[j];
+          if (r >= Dmax)
             continue;
-          int toff= (gid[j]+koff)*NumBins;
-          int loc=static_cast<int>(DeltaInv*r);
-          collectables[toff+loc+myIndex] += norm_factor(0,loc)*overNI;
+          int toff = (gid[j] + koff) * NumBins;
+          int loc  = static_cast<int>(DeltaInv * r);
+          collectables[toff + loc + myIndex] += norm_factor(0, loc) * overNI;
         }
       }
     }
   }
   else
   {
-    for(int iat=0; iat<dii.centers(); ++iat)
+    for (int iat = 0; iat < dii.centers(); ++iat)
     {
-      for(int nn=dii.M[iat]; nn<dii.M[iat+1]; ++nn)
+      for (int nn = dii.M[iat]; nn < dii.M[iat + 1]; ++nn)
       {
-        RealType r=dii.r(nn);
-        if(r>=Dmax)
+        RealType r = dii.r(nn);
+        if (r >= Dmax)
           continue;
-        int loc=static_cast<int>(DeltaInv*r);
-        collectables[pair_ids[nn]*NumBins+loc+myIndex] += norm_factor(pair_ids[nn]+1,loc);
+        int loc = static_cast<int>(DeltaInv * r);
+        collectables[pair_ids[nn] * NumBins + loc + myIndex] += norm_factor(pair_ids[nn] + 1, loc);
       }
     }
-    for(int k=0; k<other_ids.size(); ++k)
+    for (int k = 0; k < other_ids.size(); ++k)
     {
-      const DistanceTableData&  d1(*P.DistTables[other_ids[k]]);
+      const DistanceTableData& d1(*P.DistTables[other_ids[k]]);
       const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
-      int koff=other_offsets[k];
-      for(int iat=0; iat<d1.centers(); ++iat)
+      int koff = other_offsets[k];
+      for (int iat = 0; iat < d1.centers(); ++iat)
       {
-        RealType overNI=1.0/d1.centers();
-        int toff= (gid[iat]+koff)*NumBins;
-        for(int nn=d1.M[iat]; nn<d1.M[iat+1]; ++nn)
+        RealType overNI = 1.0 / d1.centers();
+        int toff        = (gid[iat] + koff) * NumBins;
+        for (int nn = d1.M[iat]; nn < d1.M[iat + 1]; ++nn)
         {
-          RealType r=dii.r(nn);
-          if(r>=Dmax)
+          RealType r = dii.r(nn);
+          if (r >= Dmax)
             continue;
-          int loc=static_cast<int>(DeltaInv*r);
-          collectables[toff+loc+myIndex] += norm_factor(0,loc)*overNI;
+          int loc = static_cast<int>(DeltaInv * r);
+          collectables[toff + loc + myIndex] += norm_factor(0, loc) * overNI;
         }
       }
     }
@@ -190,32 +186,31 @@ PairCorrEstimator::Return_t PairCorrEstimator::evaluate(ParticleSet& P)
   return 0.0;
 }
 
-void PairCorrEstimator::registerCollectables(std::vector<observable_helper*>& h5list
-    , hid_t gid) const
+void PairCorrEstimator::registerCollectables(std::vector<observable_helper*>& h5list, hid_t gid) const
 {
-  std::vector<int> onedim(1,NumBins);
-  int offset=myIndex;
-  for(int i=0; i<gof_r_prefix.size(); ++i)
+  std::vector<int> onedim(1, NumBins);
+  int offset = myIndex;
+  for (int i = 0; i < gof_r_prefix.size(); ++i)
   {
-    observable_helper* h5o=new observable_helper(gof_r_prefix[i]);
-    h5o->set_dimensions(onedim,offset);
+    observable_helper* h5o = new observable_helper(gof_r_prefix[i]);
+    h5o->set_dimensions(onedim, offset);
     h5o->open(gid);
-    h5o->addProperty(const_cast<RealType&>(Delta),"delta");
-    h5o->addProperty(const_cast<RealType&>(Dmax),"cutoff");
-//       h5o->addProperty(const_cast<std::vector<RealType>&>(norm_factor),"norm_factor");
-//       std::string blob("norm_factor[i]=1/r_m[i]^2 for r_m[i]=(r[i]+r[i+1])/2");
-//       h5o->addProperty(blob,"dictionary");
+    h5o->addProperty(const_cast<RealType&>(Delta), "delta");
+    h5o->addProperty(const_cast<RealType&>(Dmax), "cutoff");
+    //       h5o->addProperty(const_cast<std::vector<RealType>&>(norm_factor),"norm_factor");
+    //       std::string blob("norm_factor[i]=1/r_m[i]^2 for r_m[i]=(r[i]+r[i+1])/2");
+    //       h5o->addProperty(blob,"dictionary");
     h5list.push_back(h5o);
-    offset+=NumBins;
+    offset += NumBins;
   }
 }
 
 
 void PairCorrEstimator::addObservables(PropertySetType& plist, BufferType& collectables)
 {
-  myIndex=collectables.size();
-  std::vector<RealType> g(gof_r_prefix.size()*NumBins,0);
-  collectables.add(g.begin(),g.end());
+  myIndex = collectables.size();
+  std::vector<RealType> g(gof_r_prefix.size() * NumBins, 0);
+  collectables.add(g.begin(), g.end());
   ////only while debugging
   //if(gof_r.size())
   //{
@@ -238,8 +233,7 @@ void PairCorrEstimator::setObservables(PropertySetType& plist)
   //std::copy(gof_r.first_address(),gof_r.last_address(),plist.begin()+myIndex);
 }
 
-void PairCorrEstimator::setParticlePropertyList(PropertySetType& plist
-    , int offset)
+void PairCorrEstimator::setParticlePropertyList(PropertySetType& plist, int offset)
 {
   //std::copy(gof_r.first_address(),gof_r.last_address(),plist.begin()+myDebugIndex+offset);
 }
@@ -247,39 +241,39 @@ void PairCorrEstimator::setParticlePropertyList(PropertySetType& plist
 bool PairCorrEstimator::put(xmlNodePtr cur)
 {
   //set resolution
-  int nbins=(int)std::ceil(Dmax*DeltaInv);
+  int nbins = (int)std::ceil(Dmax * DeltaInv);
   std::string debug("no");
   OhmmsAttributeSet attrib;
-  attrib.add(nbins,"num_bin");
-  attrib.add(Dmax,"rmax");
-  attrib.add(Delta,"dr");
-  attrib.add(debug,"debug");
+  attrib.add(nbins, "num_bin");
+  attrib.add(Dmax, "rmax");
+  attrib.add(Delta, "dr");
+  attrib.add(debug, "debug");
   attrib.put(cur);
-  Delta=Dmax/static_cast<RealType>(nbins);
-  DeltaInv=1.0/Delta;
-  NumBins=nbins;
-  norm_factor.resize((num_species*num_species-num_species)/2+num_species+1,NumBins);
-  RealType r=Delta*0.5;
-  RealType pf=Volume*DeltaInv/(4*M_PI);
-  for(int i=0; i<NumBins; ++i, r+=Delta)
+  Delta    = Dmax / static_cast<RealType>(nbins);
+  DeltaInv = 1.0 / Delta;
+  NumBins  = nbins;
+  norm_factor.resize((num_species * num_species - num_species) / 2 + num_species + 1, NumBins);
+  RealType r  = Delta * 0.5;
+  RealType pf = Volume * DeltaInv / (4 * M_PI);
+  for (int i = 0; i < NumBins; ++i, r += Delta)
   {
-    RealType rm2=pf/r/r;
-    norm_factor(0,i)=rm2/N_e;
+    RealType rm2      = pf / r / r;
+    norm_factor(0, i) = rm2 / N_e;
     int indx(1);
-    for(int m(0); m<num_species; m++)
-      for(int n(m); n<num_species; n++)
+    for (int m(0); m < num_species; m++)
+      for (int n(m); n < num_species; n++)
       {
-        norm_factor(indx,i)=(m==n?2:1)*rm2*n_vec[n]*n_vec[m];
+        norm_factor(indx, i) = (m == n ? 2 : 1) * rm2 * n_vec[n] * n_vec[m];
         indx++;
       }
   }
-//     for(int m(0);m<norm_factor.size1();m++)
-//     {
-//       std::cerr <<m<<": ";
-//       for(int i=0; i<NumBins; ++i)
-//         std::cerr <<" "<<norm_factor(m,i);
-//       std::cerr << std::endl;
-//     }
+  //     for(int m(0);m<norm_factor.size1();m++)
+  //     {
+  //       std::cerr <<m<<": ";
+  //       for(int i=0; i<NumBins; ++i)
+  //         std::cerr <<" "<<norm_factor(m,i);
+  //       std::cerr << std::endl;
+  //     }
   ////resize(nbins);
   //if(debug == "yes")
   //  gof_r.resize(gof_r_prefix.size(),NumBins);
@@ -289,18 +283,18 @@ bool PairCorrEstimator::put(xmlNodePtr cur)
   return true;
 }
 
-  
+
 void PairCorrEstimator::report()
 {
-  app_log()<<"PairCorrEstimator report"<< std::endl;
-  app_log()<<"  num_species = "<< num_species << std::endl;
-  app_log()<<"  Volume      = "<< Volume << std::endl;
-  app_log()<<"  Dmax        = "<< Dmax << std::endl;
-  app_log()<<"  NumBins     = "<< NumBins << std::endl;
-  app_log()<<"  Delta       = "<< Delta << std::endl;
-  app_log()<<"  DeltaInv    = "<< DeltaInv << std::endl;
+  app_log() << "PairCorrEstimator report" << std::endl;
+  app_log() << "  num_species = " << num_species << std::endl;
+  app_log() << "  Volume      = " << Volume << std::endl;
+  app_log() << "  Dmax        = " << Dmax << std::endl;
+  app_log() << "  NumBins     = " << NumBins << std::endl;
+  app_log() << "  Delta       = " << Delta << std::endl;
+  app_log() << "  DeltaInv    = " << DeltaInv << std::endl;
   //app_log()<<"  x = "<< x << std::endl;
-  app_log()<<"end PairCorrEstimator report"<< std::endl;
+  app_log() << "end PairCorrEstimator report" << std::endl;
 }
 
 bool PairCorrEstimator::get(std::ostream& os) const
@@ -309,34 +303,32 @@ bool PairCorrEstimator::get(std::ostream& os) const
   return true;
 }
 
-QMCHamiltonianBase* PairCorrEstimator::makeClone(ParticleSet& qp
-    , TrialWaveFunction& psi)
+QMCHamiltonianBase* PairCorrEstimator::makeClone(ParticleSet& qp, TrialWaveFunction& psi)
 {
   //default constructor is sufficient
   return new PairCorrEstimator(*this);
 }
 
-void  PairCorrEstimator::resize(int nbins)
+void PairCorrEstimator::resize(int nbins)
 {
-  NumBins=nbins;
-  norm_factor.resize((num_species*num_species-num_species)/2+num_species+1,NumBins);
-  RealType r=Delta*0.5;
-  RealType pf=Volume*DeltaInv/(4*3.14159265);
-  for(int i=0; i<NumBins; ++i, r+=Delta)
+  NumBins = nbins;
+  norm_factor.resize((num_species * num_species - num_species) / 2 + num_species + 1, NumBins);
+  RealType r  = Delta * 0.5;
+  RealType pf = Volume * DeltaInv / (4 * 3.14159265);
+  for (int i = 0; i < NumBins; ++i, r += Delta)
   {
-    RealType rm2=pf/r/r;
-    norm_factor(0,i)=rm2/N_e;
+    RealType rm2      = pf / r / r;
+    norm_factor(0, i) = rm2 / N_e;
     int indx(1);
-    for(int m(0); m<num_species; m++)
-      for(int n(m); n<num_species; n++,indx++)
-        norm_factor(indx,i)=rm2*n_vec[n]*n_vec[m];
+    for (int m(0); m < num_species; m++)
+      for (int n(m); n < num_species; n++, indx++)
+        norm_factor(indx, i) = rm2 * n_vec[n] * n_vec[m];
   }
-//     norm_factor.resize(nbins);
-//     RealType r=Delta*0.5;
-//     for(int i=0; i<norm_factor.size(); ++i, r+=Delta)
-//     {
-//       norm_factor[i]=1.0/r/r;
-//     }
+  //     norm_factor.resize(nbins);
+  //     RealType r=Delta*0.5;
+  //     for(int i=0; i<norm_factor.size(); ++i, r+=Delta)
+  //     {
+  //       norm_factor[i]=1.0/r/r;
+  //     }
 }
-}
-
+} // namespace qmcplusplus
--- src/QMCHamiltonians/QMCHamiltonianBase.cpp
+++ src/QMCHamiltonians/QMCHamiltonianBase.cpp
@@ -11,8 +11,8 @@
 //
 // File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //////////////////////////////////////////////////////////////////////////////////////
-    
-    
+
+
 /**@file QMCHamiltonianBase.cpp
  *@brief Definition of QMCHamiltonianBase
  */
@@ -22,24 +22,22 @@
 
 namespace qmcplusplus
 {
-
-QMCHamiltonianBase::QMCHamiltonianBase()
-  :myIndex(-1),Value(0.0),Dependants(0),tWalker(0)
+QMCHamiltonianBase::QMCHamiltonianBase() : myIndex(-1), Value(0.0), Dependants(0), tWalker(0)
 {
-  quantum_domain       = no_quantum_domain;
-  energy_domain        = no_energy_domain;
+  quantum_domain = no_quantum_domain;
+  energy_domain  = no_energy_domain;
 
 #if !defined(REMOVE_TRACEMANAGER)
   streaming_scalars    = false;
   streaming_particles  = false;
   have_required_traces = false;
 #endif
-  UpdateMode.set(PRIMARY,1);
+  UpdateMode.set(PRIMARY, 1);
 }
 
 void QMCHamiltonianBase::set_energy_domain(energy_domains edomain)
 {
-  if(energy_domain_valid(edomain))
+  if (energy_domain_valid(edomain))
     energy_domain = edomain;
   else
     APP_ABORT("QMCHamiltonainBase::set_energy_domain\n  input energy domain is invalid");
@@ -47,7 +45,7 @@ void QMCHamiltonianBase::set_energy_domain(energy_domains edomain)
 
 void QMCHamiltonianBase::set_quantum_domain(quantum_domains qdomain)
 {
-  if(quantum_domain_valid(qdomain))
+  if (quantum_domain_valid(qdomain))
     quantum_domain = qdomain;
   else
     APP_ABORT("QMCHamiltonainBase::set_quantum_domain\n  input quantum domain is invalid");
@@ -55,9 +53,9 @@ void QMCHamiltonianBase::set_quantum_domain(quantum_domains qdomain)
 
 void QMCHamiltonianBase::one_body_quantum_domain(const ParticleSet& P)
 {
-  if(P.is_classical())
+  if (P.is_classical())
     quantum_domain = classical;
-  else if(P.is_quantum())
+  else if (P.is_quantum())
     quantum_domain = quantum;
   else
     APP_ABORT("QMCHamiltonianBase::one_body_quantum_domain\n  quantum domain of input particles is invalid");
@@ -65,63 +63,57 @@ void QMCHamiltonianBase::one_body_quantum_domain(const ParticleSet& P)
 
 void QMCHamiltonianBase::two_body_quantum_domain(const ParticleSet& P)
 {
-  if(P.is_classical())
+  if (P.is_classical())
     quantum_domain = classical_classical;
-  else if(P.is_quantum())
+  else if (P.is_quantum())
     quantum_domain = quantum_quantum;
   else
     APP_ABORT("QMCHamiltonianBase::two_body_quantum_domain(P)\n  quantum domain of input particles is invalid");
 }
 
-void QMCHamiltonianBase::two_body_quantum_domain(const ParticleSet& P1,const ParticleSet& P2)
+void QMCHamiltonianBase::two_body_quantum_domain(const ParticleSet& P1, const ParticleSet& P2)
 {
   bool c1 = P1.is_classical();
   bool c2 = P2.is_classical();
   bool q1 = P1.is_quantum();
   bool q2 = P2.is_quantum();
-  if(c1 && c2)
+  if (c1 && c2)
     quantum_domain = classical_classical;
-  else if((q1 && c2) || (c1 && q2))
+  else if ((q1 && c2) || (c1 && q2))
     quantum_domain = quantum_classical;
-  else if(q1 && q2)
+  else if (q1 && q2)
     quantum_domain = quantum_quantum;
   else
     APP_ABORT("QMCHamiltonianBase::two_body_quantum_domain(P1,P2)\n  quantum domain of input particles is invalid");
 }
 
-bool QMCHamiltonianBase::quantum_domain_valid(quantum_domains qdomain)
+bool QMCHamiltonianBase::quantum_domain_valid(quantum_domains qdomain) { return qdomain != no_quantum_domain; }
+
+void QMCHamiltonianBase::add2Hamiltonian(ParticleSet& qp, TrialWaveFunction& psi, QMCHamiltonian& targetH)
 {
-  return qdomain!=no_quantum_domain;
+  QMCHamiltonianBase* myclone = makeClone(qp, psi);
+  if (myclone)
+    targetH.addOperator(myclone, myName, UpdateMode[PHYSICAL]);
 }
 
-void QMCHamiltonianBase::add2Hamiltonian(ParticleSet& qp, TrialWaveFunction& psi
-    ,QMCHamiltonian& targetH)
+void QMCHamiltonianBase::registerObservables(std::vector<observable_helper*>& h5desc, hid_t gid) const
 {
-  QMCHamiltonianBase* myclone=makeClone(qp,psi);
-  if(myclone)
-    targetH.addOperator(myclone,myName,UpdateMode[PHYSICAL]);
-}
-
-void QMCHamiltonianBase::registerObservables(std::vector<observable_helper*>& h5desc
-    , hid_t gid)  const
-{
-  bool collect=UpdateMode.test(COLLECTABLE);
+  bool collect = UpdateMode.test(COLLECTABLE);
   //exclude collectables
-  if(!collect)
+  if (!collect)
   {
-    int loc=h5desc.size();
+    int loc = h5desc.size();
     h5desc.push_back(new observable_helper(myName));
-    std::vector<int> onedim(1,1);
-    h5desc[loc]->set_dimensions(onedim,myIndex);
+    std::vector<int> onedim(1, 1);
+    h5desc[loc]->set_dimensions(onedim, myIndex);
     h5desc[loc]->open(gid);
   }
 }
 
-void QMCHamiltonianBase::addEnergy(MCWalkerConfiguration &W,
-    std::vector<RealType> &LocalEnergy)
+void QMCHamiltonianBase::addEnergy(MCWalkerConfiguration& W, std::vector<RealType>& LocalEnergy)
 {
-  APP_ABORT("Need specialization for "+myName+"::addEnergy(MCWalkerConfiguration &W).\n Required functionality not implemented\n");
+  APP_ABORT("Need specialization for " + myName +
+            "::addEnergy(MCWalkerConfiguration &W).\n Required functionality not implemented\n");
 }
 
-}
-
+} // namespace qmcplusplus
