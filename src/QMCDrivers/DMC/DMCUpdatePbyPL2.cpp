7e5bbd5470e8ee88b50f9c8a6c62d00b8d3aed61
Jaron Krogel <krogeljt@ornl.gov>
2019-04-11 10:27:03 -0400

    incorporate L2 adjustments into DMC projector

--- src/QMCDrivers/DMC/DMCUpdatePbyPL2.cpp
+++ src/QMCDrivers/DMC/DMCUpdatePbyPL2.cpp
@@ -61,29 +61,87 @@ void DMCUpdatePbyPL2::advanceWalker(Walker_t& thisWalker, bool recompute)
   RealType rr_proposed = 0.0;
   RealType rr_accepted = 0.0;
   RealType gf_acc      = 1.0;
+  mPosType K;
+  mTensorType D;
+  mTensorType Dchol;
+  PosType Ktmp;
+  TensorType Dtmp;
+  bool L2_proj = H.has_L2();
+  if(L2_proj)
+  {
+    Ktmp = 0.0;
+    Dtmp = 0.0;
+    for(int d=0;d<DIM;d++)
+      Dtmp(d,d) = 1.0;
+  }
   myTimers[DMC_movePbyP]->start();
   for (int ig = 0; ig < W.groups(); ++ig) //loop over species
   {
     RealType tauovermass = Tau * MassInvS[ig];
     RealType oneover2tau = 0.5 / (tauovermass);
     RealType sqrttau     = std::sqrt(tauovermass);
+    RealType rr;
     for (int iat = W.first(ig); iat < W.last(ig); ++iat)
     {
       W.setActive(iat);
       //get the displacement
       GradType grad_iat = Psi.evalGrad(W, iat);
       mPosType dr;
-      getScaledDrift(tauovermass, grad_iat, dr);
-      dr += sqrttau * deltaR[iat];
-      RealType rr = tauovermass * dot(deltaR[iat], deltaR[iat]);
-      rr_proposed += rr;
-      if (rr > m_r2max)
+      mPosType dr_diff = deltaR[iat];
+      if(!L2_proj) // normal projector
       {
-        ++nRejectTemp;
-        continue;
+        getScaledDrift(tauovermass, grad_iat, dr);
+        dr += sqrttau * dr_diff;
+        rr = tauovermass * dot(dr_diff, dr_diff);
+        rr_proposed += rr;
+        if (rr > m_r2max)
+        {
+          ++nRejectTemp;
+          continue;
+        }
+        if (!W.makeMoveAndCheck(iat, dr))
+          continue;
       }
-      if (!W.makeMoveAndCheck(iat, dr))
-        continue;
+      else // projector including L2 potential
+      {
+        // do a fake move (zero distance)
+        // this ensures the temporary distance data is correct 
+        // will need to remove this later, but requires reimplementation of computeL2DK
+        dr = 0.0;
+        if (!W.makeMoveAndCheck(iat, dr))
+          continue;
+
+        H.computeL2DK(W,iat,Dtmp,Ktmp);
+        D = Dtmp; // upcast for mixed precision
+        K = Ktmp;
+        getScaledDriftL2(tauovermass,grad_iat,D,K,dr);
+
+        W.rejectMove(iat);
+        rr = tauovermass * dot(dr_diff, dr_diff);
+        rr_proposed += rr;
+        if (rr > m_r2max)
+        {
+          ++nRejectTemp;
+          continue;
+        }
+
+        // move with just drift to update distance tables
+        if (!W.makeMoveAndCheck(iat, dr))
+          continue;
+
+        // compute diffusion step 
+        H.computeL2D(W,iat,Dtmp);
+        D = Dtmp; // upcast for mixed precision
+        Dchol = cholesky(D);
+        dr_diff = dot(Dchol,dr_diff);
+        dr += sqrttau * dr_diff;
+
+        // reverse the intermediate drift move
+        W.rejectMove(iat);
+        // move with drift and diffusion together
+        if (!W.makeMoveAndCheck(iat, dr))
+          continue;
+      }
       RealType ratio = Psi.ratioGrad(W, iat, grad_iat);
       //node is crossed reject the move
       if (branchEngine->phaseChanged(Psi.getPhaseDiff()))
@@ -95,7 +153,7 @@ void DMCUpdatePbyPL2::advanceWalker(Walker_t& thisWalker, bool recompute)
       }
       else
       {
-        EstimatorRealType logGf = -0.5 * dot(deltaR[iat], deltaR[iat]);
+        EstimatorRealType logGf = -0.5 * dot(dr_diff, dr_diff);
         //Use the force of the particle iat
         getScaledDrift(tauovermass, grad_iat, dr);
         dr                      = W.R[iat] - W.activePos - dr;
