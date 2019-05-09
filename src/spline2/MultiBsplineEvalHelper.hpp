ed76e42a343598c180ba600be8bdcb7b9aebd9de
Ye Luo <yeluo@anl.gov>
2019-05-09 18:12:18 -0500

    Add computeLocationAndFractional in spline2.

--- src/spline2/MultiBsplineEvalHelper.hpp
+++ src/spline2/MultiBsplineEvalHelper.hpp
@@ -62,6 +62,58 @@ inline void getSplineBound(T x, TRESIDUAL& dx, int& ind, int nmax)
   }
 }
 
+/** define computeLocationAndFractional: common to any implementation
+ * compute the location of the spline grid point and residual coordinates
+ * also it precomputes auxilary array a, b and c
+ */
+template<typename T>
+inline void computeLocationAndFractional(const typename qmcplusplus::bspline_traits<T, 3>::SplineType* restrict spline_m,
+                            T x, T y, T z,
+                            int& ix, int& iy, int& iz,
+                            T a[4], T b[4], T c[4])
+{
+  x -= spline_m->x_grid.start;
+  y -= spline_m->y_grid.start;
+  z -= spline_m->z_grid.start;
+
+  T tx, ty, tz;
+
+  getSplineBound(x * spline_m->x_grid.delta_inv, tx, ix, spline_m->x_grid.num - 1);
+  getSplineBound(y * spline_m->y_grid.delta_inv, ty, iy, spline_m->y_grid.num - 1);
+  getSplineBound(z * spline_m->z_grid.delta_inv, tz, iz, spline_m->z_grid.num - 1);
+
+  MultiBsplineData<T>::compute_prefactors(a, tx);
+  MultiBsplineData<T>::compute_prefactors(b, ty);
+  MultiBsplineData<T>::compute_prefactors(c, tz);
+}
+
+/** define computeLocationAndFractional: common to any implementation
+ * compute the location of the spline grid point and residual coordinates
+ * also it precomputes auxilary array (a,b,c) (da,db,dc) (d2a,d2b,d2c)
+ */
+template<typename T>
+inline void computeLocationAndFractional(const typename qmcplusplus::bspline_traits<T, 3>::SplineType* restrict spline_m,
+                            T x, T y, T z,
+                            int& ix, int& iy, int& iz,
+                            T a[4], T b[4], T c[4],
+                            T da[4], T db[4], T dc[4],
+                            T d2a[4], T d2b[4], T d2c[4])
+{
+  x -= spline_m->x_grid.start;
+  y -= spline_m->y_grid.start;
+  z -= spline_m->z_grid.start;
+
+  T tx, ty, tz;
+
+  getSplineBound(x * spline_m->x_grid.delta_inv, tx, ix, spline_m->x_grid.num - 1);
+  getSplineBound(y * spline_m->y_grid.delta_inv, ty, iy, spline_m->y_grid.num - 1);
+  getSplineBound(z * spline_m->z_grid.delta_inv, tz, iz, spline_m->z_grid.num - 1);
+
+  MultiBsplineData<T>::compute_prefactors(a, da, d2a, tx);
+  MultiBsplineData<T>::compute_prefactors(b, db, d2b, ty);
+  MultiBsplineData<T>::compute_prefactors(c, dc, d2c, tz);
+}
+
 } // namespace spline2
 
 #endif
