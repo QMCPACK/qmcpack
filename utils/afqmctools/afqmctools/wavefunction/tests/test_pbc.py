7d657386fc86e1c521e5b9ad3b6f39c832c9669d
Fionn Malone <fionn.malone@gmail.com>
2019-05-30 17:12:38 -0700

    Update unit tests.

--- utils/afqmctools/afqmctools/wavefunction/tests/test_pbc.py
+++ utils/afqmctools/afqmctools/wavefunction/tests/test_pbc.py
@@ -43,14 +43,8 @@ class TestMolWavefunction(unittest.TestCase):
             pass
         pbc.write_wfn_pbc(scf_data, True, 'wfn.h5', rediag=True)
         with h5py.File('wfn.h5', 'r') as fh5:
-            dims = fh5['Wavefunction/dims'][:]
-            orbs = fh5['Wavefunction/orbs'][:]
-            wfn_type = fh5['Wavefunction/type'][()]
-            wlk_type = fh5['Wavefunction/walker_type'][()]
-        self.assertEqual(wfn_type, 'NOMSD')
-        self.assertEqual(wlk_type, 'CLOSED')
-        self.assertTrue(numpy.allclose(dims, [1,64,64]))
-        self.assertAlmostEqual(numpy.max(orbs), 1.0)
+            dims = fh5['Wavefunction/NOMSD/dims'][:]
+        self.assertTrue(numpy.allclose(dims, [64,32,32,1,1]))
 
     def tearDown(self):
         cwd = os.getcwd()
