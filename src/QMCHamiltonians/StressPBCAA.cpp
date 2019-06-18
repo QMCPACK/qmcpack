11cc833609815c0abb7e39a58bf4751490003c1c
Ye Luo <yeluo@anl.gov>
2019-06-17 22:39:58 -0500

    Minor change.

--- src/QMCHamiltonians/StressPBCAA.cpp
+++ src/QMCHamiltonians/StressPBCAA.cpp
@@ -26,7 +26,7 @@ StressPBCAA::StressPBCAA(ParticleSet& ref, bool active)
   //save source tag
   SourceID = ref.tag();
   //create a distance table: just to get the table name
-  DistanceTableData* d_aa = DistanceTable::add(ref, DT_AOS);
+  DistanceTableData* d_aa = ref.DistanceTables[ref.addTable(ref, DT_AOS)];
   PtclRefName             = d_aa->Name;
   initBreakup(ref);
   prefix = "S_" + PtclRefName;
