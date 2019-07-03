164f1ceb93ddc17768cfb48f7cf2735fe3795309
bvdg <bvdg@berkeley.edu>
2019-07-03 02:27:27 -0700

    small revert

--- src/QMCWaveFunctions/tests/test_counting_jastrow.cpp
+++ src/QMCWaveFunctions/tests/test_counting_jastrow.cpp
@@ -228,6 +228,12 @@ TEST_CASE("CountingJastrow","[wavefunction]")
   
   // test automatic/minimal voronoi generator
   const char * cj_voronoi_xml = "<jastrow name=\"ncjf_voronoi\" type=\"Counting\" source=\"ion0\">\
+      <var name=\"F\" opt=\"true\">\
+        4.4903e-01 5.3502e-01 5.2550e-01 6.8081e-01\
+                   5.1408e-01 4.8658e-01 6.2182e-01\
+                              2.7189e-01 9.4951e-01\
+                                         0.0000e+00\
+      </var>\
       <region type=\"voronoi\" opt=\"true\">\
         <var name=\"alpha\">1.0</var>\
       </region>\
