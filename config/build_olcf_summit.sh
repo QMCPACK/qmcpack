4077a8042a7a59d42249fe4e41f4c9d3776458f5
Peter Doak <doakpw@ornl.gov>
2019-07-18 14:46:43 -0400

    less repetitive build script, just legacy cuda

--- config/build_olcf_summit.sh
+++ config/build_olcf_summit.sh
@@ -4,7 +4,7 @@ echo "Purging current module set"
 
 echo "Loading QMCPACK dependency modules for summit"
 module load gcc
-module load spectrum-mpi/10.3.0.1-20190611
+module load spectrum-mpi
 module load essl
 module load netlib-lapack
 module load hdf5
@@ -14,54 +14,24 @@ module load cmake
 module load boost
 module load cuda
 module load python/2.7.15-anaconda2-5.3.0
-mkdir build_summit_legacy_cuda
-cd build_summit_legacy_cuda
-cmake -DCMAKE_C_COMPILER="mpicc" \
+
+declare -A builds=( ["legacy_gpu"]="-DQMC_CUDA=1 " \
+		    ["complex_legacy_gpu"]="-DQMC_CUDA=1 -DQMC_COMPLEX=1 ")
+
+mkdir bin
+
+for build in "${!builds[@]}"
+do
+    echo "building: $build with ${builds[$build]}"
+    mkdir build_summit_${build}
+    cd build_summit_${build}
+    cmake -DCMAKE_C_COMPILER="mpicc" \
       -DCMAKE_CXX_COMPILER="mpicxx" \
       -DBUILD_LMYENGINE_INTERFACE=0 \
-      -DQMC_CUDA=1 \
+      ${builds[$build]} \
       -DCUDA_ARCH="sm_70" \
-..
-make -j 20
-cd ..
-ln -sf ./build_summit_legacy_cuda/bin/qmcpack ./qmcpack_summit_legacy_cuda
-
-mkdir build_summit_complex_legacy_cuda
-cd build_summit_complex_legacy_cuda
-cmake -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DBUILD_LMYENGINE_INTERFACE=0 \
-      -DQMC_CUDA=1 \
-      -DQMC_COMPLEX=1 \
-      -DCUDA_ARCH="sm_70" \
-..
-make -j 20
-cd ..
-ln -sf ./build_summit_complex_legacy_cuda/bin/qmcpack ./qmcpack_summit_complex_legacy_cuda
-
-mkdir build_summit_enable_cuda
-cd build_summit_enable_cuda
-cmake -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DCUDA_TOOLKIT_ROOT_DIR=${OLCF_CUDA_ROOT} \
-      -DENABLE_CUDA=1 \
-      -DCUDA_ARCH="sm_70" \
-..
-make -j 20
-cd ..
-ln -sf ./build_summit_enable_cuda/bin/qmcpack ./qmcpack_summit_enable_cuda
-
-mkdir build_summit_complex_enable_cuda
-cd build_summit_complex_enable_cuda
-cmake -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DCUDA_TOOLKIT_ROOT_DIR=${OLCF_CUDA_ROOT} \
-      -DENABLE_CUDA=1 \
-      -DQMC_COMPLEX=1 \
-      -DCUDA_ARCH="sm_70" \
-..
-make -j 20
-cd ..
-ln -sf ./build_summit_complex_enable_cuda/bin/qmcpack ./qmcpack_summit_complex_enable_cuda
-
-
+      ..
+    make -j 20
+    cd ..
+    ln -sf ./build_summit_${build}/bin/qmcpack ./bin/qmcpack_${build}
+done
