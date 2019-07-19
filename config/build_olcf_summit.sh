53a567e755c45f06e8d56c9616dbf6c43fed7071
Peter Doak <doakpw@ornl.gov>
2019-07-19 10:54:22 -0400

    Added CPU only builds.

--- config/build_olcf_summit.sh
+++ config/build_olcf_summit.sh
@@ -2,27 +2,19 @@
 module purge
 echo "Purging current module set"
 
-echo "Loading QMCPACK dependency modules for summit"
-module load gcc
-module load spectrum-mpi
-module load essl
-module load netlib-lapack
-module load hdf5
-module load fftw
-export FFTW_ROOT=$OLCF_FFTW_ROOT
-module load cmake
-module load boost
-module load cuda
-module load python/2.7.15-anaconda2-5.3.0
+. config/load_summit_modules.sh
 
-declare -A builds=( ["legacy_gpu"]="-DQMC_CUDA=1 " \
-		    ["complex_legacy_gpu"]="-DQMC_CUDA=1 -DQMC_COMPLEX=1 ")
+declare -A builds=( ["cpu"]=" " \
+                    ["complex_cpu"]="-DQMC_COMPLEX=1 " \
+                    ["legacy_gpu"]="-DQMC_CUDA=1 " \
+		    ["complex_legacy_gpu"]="-DQMC_CUDA=1 -DQMC_COMPLEX=1 " )
 
 mkdir bin
 
 for build in "${!builds[@]}"
 do
     echo "building: $build with ${builds[$build]}"
+    rm bin/qmcpack_${build}
     mkdir build_summit_${build}
     cd build_summit_${build}
     cmake -DCMAKE_C_COMPILER="mpicc" \
@@ -32,6 +24,9 @@ do
       -DCUDA_ARCH="sm_70" \
       ..
     make -j 20
+    if [ $? -eq 0 ]; then
+      build_dir=$(pwd)
+      ln -sf ${build_dir}/bin/qmcpack ${build_dir}/../bin/qmcpack_${build}
+    fi
     cd ..
-    ln -sf ./build_summit_${build}/bin/qmcpack ./bin/qmcpack_${build}
 done
