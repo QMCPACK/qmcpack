ac5498dab53e24fd37d7b3de0bd6b3dca33557d3
rcclay <j.k.rofling@gmail.com>
2019-07-31 11:07:14 -0600

    Change header info

--- src/QMCHamiltonians/SOECPComponent.h
+++ src/QMCHamiltonians/SOECPComponent.h
@@ -2,15 +2,11 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2019 Jeongnim Kim and QMCPACK developers.
 //
-// File developed by: Ken Esler, kpesler@gmail.com, University of Illinois at Urbana-Champaign
-//                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
-//                    Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
-//                    Jaron T. Krogel, krogeljt@ornl.gov, Oak Ridge National Laboratory
-//                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+// File developed by: Raymond Clay, rclay@sandia.gov, Sandia National Laboratory
 //
-// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+// File created by: Raymond Clay, rclay@sandia.gov, Sandia National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
 
 
