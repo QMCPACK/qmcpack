77ba6ac9d31f7142987e4c6192ce00be945cd000
Ye Luo <yeluo@anl.gov>
2019-08-16 07:30:59 -0500

    Avoid inf. loop when hitting badbit but not eof.

--- src/OhmmsData/libxmldefs.h
+++ src/OhmmsData/libxmldefs.h
@@ -126,6 +126,12 @@ inline bool putContent(std::vector<T>& a, xmlNodePtr cur)
   {
     if (stream >> t)
       b.push_back(t);
+    else if (!stream.eof() && stream.fail())
+    {
+      std::cerr << "failbit detected when reading type (type_info::name) "
+                << typeid(T).name() << ", value " << t << std::endl;
+      stream.clear();
+    }
   }
   a = b;
   return true;
