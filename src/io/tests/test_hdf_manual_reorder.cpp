f83315987ae751e18f351ef9867db2ec7817f05b
Peter Doak <doakpw@ornl.gov>
2019-08-21 14:21:50 -0400

    fixed formatting

--- src/io/tests/test_hdf_manual_reorder.cpp
+++ src/io/tests/test_hdf_manual_reorder.cpp
@@ -62,7 +62,7 @@ TEST_CASE("hdf_write_reorder_with_matrix", "[hdf]")
   hdf_archive hd;
   bool okay = hd.create("test_write_matrix_reorder.hdf");
   REQUIRE(okay);
-  
+
   Vector<double> v(6);
   v[0] = 0.0;
   v[1] = 0.1;
@@ -73,23 +73,23 @@ TEST_CASE("hdf_write_reorder_with_matrix", "[hdf]")
 
   Matrix<double> matrix_view(v.data(), 2, 3);
   hd.write(matrix_view, "matrix_from_vector");
-  
+
   hdf_archive hd2;
   hd2.open("test_write_matrix_reorder.hdf");
-  
-  Matrix<double> m(2,3);
+
+  Matrix<double> m(2, 3);
   hd2.read(m, "matrix_from_vector");
 
-  REQUIRE(m(0,0) == Approx(v[0]));
-  REQUIRE(m(0,1) == Approx(v[1]));
-  REQUIRE(m(0,2) == Approx(v[2]));
-  REQUIRE(m(1,0) == Approx(v[3]));
-  REQUIRE(m(1,1) == Approx(v[4]));
-  REQUIRE(m(1,2) == Approx(v[5]));
+  REQUIRE(m(0, 0) == Approx(v[0]));
+  REQUIRE(m(0, 1) == Approx(v[1]));
+  REQUIRE(m(0, 2) == Approx(v[2]));
+  REQUIRE(m(1, 0) == Approx(v[3]));
+  REQUIRE(m(1, 1) == Approx(v[4]));
+  REQUIRE(m(1, 2) == Approx(v[5]));
 }
 
 
-// do this in three ways.  
+// do this in three ways.
 // 1. relying on my overloading in std::vector (do not perfer this, too specific)
 // 2. rely on my understanding of hyperslab_proxy (seems to require you to know
 //          too much about the dimensions in the dataset and is prone to user error)
@@ -101,11 +101,13 @@ TEST_CASE("hdf_read_partial", "[hdf]")
   hdf_archive hd;
   bool okay = hd.create("test_read_partial.hdf");
   REQUIRE(okay);
-  
-  Matrix<double> allData(3,4);
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-      allData(i,j) = i+j*0.1;
+
+  Matrix<double> allData(3, 4);
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      allData(i, j) = i + j * 0.1;
     }
   }
 
@@ -117,89 +119,94 @@ TEST_CASE("hdf_read_partial", "[hdf]")
   REQUIRE(okay);
 
   // method 1 (relying on vector in hdf_stl)
-  vector<int> readSpec{1,-1};
+  vector<int> readSpec{1, -1};
   vector<double> readBuffer(4);
 
   hd2.read(readBuffer, readSpec, "matrix");
-  for (int i = 0; i < 4; i++) 
+  for (int i = 0; i < 4; i++)
   {
-    REQUIRE(readBuffer[i] == Approx(allData(1,i)));
+    REQUIRE(readBuffer[i] == Approx(allData(1, i)));
   }
 
   readSpec[0] = -1;
   readSpec[1] = 2;
   hd2.read(readBuffer, readSpec, "matrix");
-  for (int i = 0; i < 3; i++) 
+  for (int i = 0; i < 3; i++)
   {
-    REQUIRE(readBuffer[i] == Approx(allData(i,2)));
+    REQUIRE(readBuffer[i] == Approx(allData(i, 2)));
   }
 
   readSpec[0] = 2;
   readSpec[1] = 0;
   hd2.read(readBuffer, readSpec, "matrix");
-  REQUIRE(readBuffer[0] == Approx(allData(2,0)));
+  REQUIRE(readBuffer[0] == Approx(allData(2, 0)));
 
   // method 2 (direct utilization of hyperslab_proxy in client code)
-  Matrix<double> outbuffer1(1,4);
-  Matrix<double> outbuffer2(3,1);
-  Matrix<double> outbuffer3(1,1);
+  Matrix<double> outbuffer1(1, 4);
+  Matrix<double> outbuffer2(3, 1);
+  Matrix<double> outbuffer3(1, 1);
 
-  TinyVector<int,2> dims_unused;
+  TinyVector<int, 2> dims_unused;
   dims_unused[0] = 1;
   dims_unused[1] = 2;
-  TinyVector<int,2> dims_local;
+  TinyVector<int, 2> dims_local;
   dims_local[0] = 1;
   dims_local[1] = 4;
-  TinyVector<int,2> offsets;
+  TinyVector<int, 2> offsets;
   offsets[0] = 1;
   offsets[1] = 0;
 
-  hyperslab_proxy<Matrix<double>,7> pxy1(outbuffer1, dims_unused, dims_local, offsets);
+  hyperslab_proxy<Matrix<double>, 7> pxy1(outbuffer1, dims_unused, dims_local, offsets);
   hd2.read(pxy1, "matrix");
-  for (int i = 0; i < 4; i++) {
-    REQUIRE(outbuffer1(0,i) == Approx(allData(1,i)));
+  for (int i = 0; i < 4; i++)
+  {
+    REQUIRE(outbuffer1(0, i) == Approx(allData(1, i)));
   }
 
   dims_local[0] = 3;
   dims_local[1] = 1;
-  offsets[0] = 0;
-  offsets[1] = 2;
-  hyperslab_proxy<Matrix<double>,7> pxy2(outbuffer2, dims_unused, dims_local, offsets);
+  offsets[0]    = 0;
+  offsets[1]    = 2;
+  hyperslab_proxy<Matrix<double>, 7> pxy2(outbuffer2, dims_unused, dims_local, offsets);
   hd2.read(pxy2, "matrix");
-  for (int i = 0; i < 3; i++) {
-    REQUIRE(outbuffer2(i,0) == Approx(allData(i,2)));
+  for (int i = 0; i < 3; i++)
+  {
+    REQUIRE(outbuffer2(i, 0) == Approx(allData(i, 2)));
   }
-  
+
   dims_local[0] = 1;
   dims_local[1] = 1;
-  offsets[0] = 2;
-  offsets[1] = 0;
-  hyperslab_proxy<Matrix<double>,7> pxy3(outbuffer3, dims_unused, dims_local, offsets);
+  offsets[0]    = 2;
+  offsets[1]    = 0;
+  hyperslab_proxy<Matrix<double>, 7> pxy3(outbuffer3, dims_unused, dims_local, offsets);
   hd2.read(pxy3, "matrix");
-  REQUIRE(outbuffer3(0,0) == Approx(allData(2,0)));
+  REQUIRE(outbuffer3(0, 0) == Approx(allData(2, 0)));
 
   // method 3 here
   //  buftype sto(4);
-  Matrix<double> locob1(1,4);
-  Matrix<double> locob2(3,1);
-  Matrix<double> locob3(1,1);
-  TinyVector<int,2> locReadSpec{1,-1};
+  Matrix<double> locob1(1, 4);
+  Matrix<double> locob2(3, 1);
+  Matrix<double> locob3(1, 1);
+  TinyVector<int, 2> locReadSpec{1, -1};
   hd2.read2(locob1, locReadSpec, "matrix");
-  for (int i = 0; i < 4; i++) {
-    REQUIRE(locob1(0,i) == Approx(allData(1,i)));
+  for (int i = 0; i < 4; i++)
+  {
+    REQUIRE(locob1(0, i) == Approx(allData(1, i)));
   }
 
   locReadSpec[0] = -1;
   locReadSpec[1] = 2;
   hd2.read2(locob2, locReadSpec, "matrix");
-  for (int i = 0; i < 3; i++) {
-          REQUIRE(locob2.data()[i] == Approx(allData(i,2)));
+  for (int i = 0; i < 3; i++)
+  {
+    REQUIRE(locob2.data()[i] == Approx(allData(i, 2)));
+    REQUIRE(locob2(i) == Approx(allData(i,2)));
   }
-  
+
   locReadSpec[0] = 2;
   locReadSpec[1] = 0;
   hd2.read2(locob3, locReadSpec, "matrix");
-  REQUIRE(locob3.data()[0] == Approx(allData(2,0)));
+  REQUIRE(locob3.data()[0] == Approx(allData(2, 0)));
 
   hd2.close();
 }
