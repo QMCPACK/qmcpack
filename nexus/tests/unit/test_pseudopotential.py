0fb5c1b9df49aa46b33a0c12b3a87e7a225d1ea5
Jaron Krogel <krogeljt@ornl.gov>
2019-09-20 19:36:36 -0400

    nexus: start pseudopotential test

---
+++ nexus/tests/unit/test_pseudopotential.py
@@ -0,0 +1,19 @@
+
+
+def test_import():
+    import pseudopotential
+    from pseudopotential import Pseudopotentials
+    from pseudopotential import PseudoFile
+    from pseudopotential import gamessPPFile
+    from pseudopotential import PPset
+    from pseudopotential import Pseudopotential
+    from pseudopotential import SemilocalPP
+    from pseudopotential import GaussianPP
+    from pseudopotential import QmcpackPP
+    from pseudopotential import CasinoPP
+#end def test_import
+
+
+
+
+
