86038b5720b7fb58f30bdfd8d1f9d88a33bb07f9
Leon Otis <leon_otis@berkeley.edu>
2019-09-20 17:24:05 -0700

    Add unit test for hybrid engine's query store

--- src/QMCDrivers/Optimizers/HybridEngine.h
+++ src/QMCDrivers/Optimizers/HybridEngine.h
@@ -35,15 +35,27 @@ private:
   //number of updates in each individual method of hybrid method
   std::vector<int> num_updates_opt_methods_;
 
+  //Helper method for determining the index in opt_methods_ that corresponds to a method based on current step
   int identifyMethodIndex() const;
 
 public:
   //Constructor for engine
   HybridEngine(Communicate* comm, const xmlNodePtr cur);
 
+  //Fake constructor only used in unit test for hybrid engine.
+  HybridEngine() {step_num_ = 0;}
+
+  //Returns the appropriate XML for a optimization method to be handled inside QMCFixedSampleLinearOptimize
   xmlNodePtr getSelectedXML();
 
-  bool queryStore(int store_num, OptimizerType methodType) const;
+  //Determines whether to store a vector based on how many have been requested and what the current step is
+  bool queryStore(int store_num, OptimizerType method_type) const;
+
+  //adds an optimization method to list. Used only in unit test for hybrid engine.
+  void addMethod(OptimizerType method) {opt_methods_.push_back(method);}
+
+  //adds a number of update steps to list. Used only in unit test for hybrid engine.
+  void addUpdates(int num_steps) {num_updates_opt_methods_.push_back(num_steps);}
 };
 
 } // namespace qmcplusplus
