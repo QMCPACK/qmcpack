3e4c5c74c0b622d37fdbc5913461fb6510e122a5
Peter Doak <doakpw@ornl.gov>
2019-10-22 12:48:02 -0400

    removing have boost ifdef, not having boost is a fatal build error

--- src/QMCDrivers/BranchIO.cpp
+++ src/QMCDrivers/BranchIO.cpp
@@ -167,7 +167,6 @@ bool BranchIO::read(const std::string& fname)
 {
   int found_config = 0;
 
-#if defined(HAVE_LIBBOOST)
   if (myComm->rank() == 0)
   {
     initAttributes();
@@ -197,51 +196,6 @@ bool BranchIO::read(const std::string& fname)
       found_config = 1;
     }
   }
-#else
-  if (myComm->rank() == 0)
-  {
-    HDFVersion res_version(0, 4);  //start using major=0 and minor=4
-    HDFVersion res_20080624(0, 5); //major revision on 2008-06-24 0.5
-    HDFVersion in_version(0, 1);
-
-    //append .config.h5 if missing
-    std::string h5name(fname);
-    if (fname.find("qmc.h5") >= fname.size())
-      h5name.append(".qmc.h5");
-
-    hdf_archive prevconfig(myComm, true);
-    found_config = prevconfig.open(h5name, H5F_ACC_RDONLY);
-
-    if (found_config)
-    {
-      int n = ref.vParam.size() + ref.iParam.size();
-      /** temporary storage to broadcast restart data */
-      prevconfig.read(in_version.version, hdf::version);
-      myComm->bcast(in_version.version);
-      prevconfig.push(hdf::main_state, false);
-      if (in_version >= res_20080624)
-      {
-        //need a better version control
-        prevconfig.push(hdf::qmc_status, false);
-        prevconfig.read(ref.vParam, "vparam");
-        prevconfig.read(ref.iParam, "iparam");
-        prevconfig.read(ref.BranchMode, "branchmode");
-
-        prevconfig.push("histogram", false);
-        prevconfig.read(ref.EnergyHist, "energy");
-        prevconfig.read(ref.VarianceHist, "variance");
-        prevconfig.read(ref.R2Accepted, "r2accepted");
-        prevconfig.read(ref.R2Proposed, "r2proposed");
-        prevconfig.pop();
-
-        prevconfig.pop();
-      }
-      else
-        found_config = false;
-      prevconfig.pop();
-    }
-  }
-#endif
   myComm->bcast(found_config);
 
   if (!found_config)
