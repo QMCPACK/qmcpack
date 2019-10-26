bebfef8ca7bb8836ad2ec03e39fc4d383920b599
Paul Young <yyang173@illinois.edu>
2019-10-26 12:45:17 -0500

    test ewald3d df

--- src/LongRange/tests/test_ewald3d.cpp
+++ src/LongRange/tests/test_ewald3d.cpp
@@ -69,4 +69,61 @@ TEST_CASE("ewald3d", "[lrhandler]")
   }
 }
 
+/** evalaute bare Coulomb derivative using EwaldHandler3D
+ */
+TEST_CASE("ewald3d df", "[lrhandler]")
+{
+  CrystalLattice<OHMMS_PRECISION, OHMMS_DIM> Lattice;
+  Lattice.BoxBConds = true;
+  Lattice.LR_dim_cutoff = 30.;
+  Lattice.R.diagonal(5.0);
+  Lattice.reset();
+  REQUIRE(Lattice.Volume == Approx(125));
+  Lattice.SetLRCutoffs(Lattice.Rv);
+  //Lattice.printCutoffs(app_log());
+  REQUIRE(Lattice.LR_rc == Approx(2.5));
+  REQUIRE(Lattice.LR_kc == Approx(12));
+
+  ParticleSet ref; // handler needs ref.SK.KLists
+  ref.Lattice = Lattice;  // !!!! crucial for access to Volume
+  ref.LRBox = Lattice;  // !!!! crucial for S(k) update
+  StructFact *SK = new StructFact(ref, Lattice.LR_kc);
+  ref.SK = SK;
+  EwaldHandler3D handler(ref, Lattice.LR_kc);
+
+  // make sure initBreakup changes the default sigma
+  REQUIRE(handler.Sigma == Approx(Lattice.LR_kc));
+  handler.initBreakup(ref);
+  REQUIRE(handler.Sigma == Approx(std::sqrt(Lattice.LR_kc/(2.0*Lattice.LR_rc))));
+
+  REQUIRE(handler.MaxKshell == 78);
+  REQUIRE(handler.LR_rc == Approx(2.5));
+  REQUIRE(handler.LR_kc == Approx(12));
+
+  mRealType r, dr, rinv;
+  mRealType rm, rp; // minus (m), plus (p)
+  mRealType vsrm, vsrp, dvsr, vlrm, vlrp, dvlr;
+  dr = 0.00001; // finite difference step
+  std::vector<mRealType> rlist={0.1, 0.5, 1.0, 2.0, 2.5};
+  for (auto it=rlist.begin(); it!=rlist.end(); ++it)
+  {
+    r = *it;
+    // test short-range piece
+    rm = r-dr;
+    rinv = 1./rm;
+    vsrm = handler.evaluate(rm, rinv);
+    vlrm = handler.evaluateLR(rm);
+    rp = r+dr;
+    rinv = 1./rp;
+    vsrp = handler.evaluate(rp, rinv);
+    vlrp = handler.evaluateLR(rp);
+    dvsr = (vsrp-vsrm)/(2*dr);
+    rinv = 1./r;
+    REQUIRE(handler.srDf(r, rinv) == Approx(dvsr));
+    // test long-range piece
+    dvlr = (vlrp-vlrm)/(2*dr);
+    REQUIRE(handler.lrDf(r) == Approx(dvlr));
+  }
+}
+
 } // namespace qmcplusplus
