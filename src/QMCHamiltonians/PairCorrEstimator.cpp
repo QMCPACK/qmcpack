61bdf899f6312002752b3269c2c68f178ec60d10
Ye Luo <yeluo@anl.gov>
2019-11-05 20:00:32 -0600

    Format source code

--- src/QMCHamiltonians/PairCorrEstimator.cpp
+++ src/QMCHamiltonians/PairCorrEstimator.cpp
@@ -23,22 +23,22 @@
 namespace qmcplusplus
 {
 PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
-  : Dmax(10.), Delta(0.5), num_species(2), d_aa_ID_(elns.addTable(elns, DT_SOA_PREFERRED))
+    : Dmax(10.), Delta(0.5), num_species(2), d_aa_ID_(elns.addTable(elns, DT_SOA_PREFERRED))
 {
   UpdateMode.set(COLLECTABLE, 1);
   num_species = elns.groups();
   n_vec.resize(num_species, 0);
-  for ( int i=0; i<num_species; i++ )
+  for (int i = 0; i < num_species; i++)
     n_vec[i] = elns.last(i) - elns.first(i);
   N_e = elns.getTotalNum();
 
   // use the simulation cell radius if any direction is periodic
-  if ( elns.Lattice.SuperCellEnum )
+  if (elns.Lattice.SuperCellEnum)
   {
     Dmax   = elns.Lattice.WignerSeitzRadius;
     Volume = elns.Lattice.Volume;
   }
-  else  // Open BC's
+  else // Open BC's
     Volume = 1.0;
   NumBins  = static_cast<int>(Dmax / Delta);
   Delta    = Dmax / static_cast<RealType>(NumBins);
@@ -48,77 +48,73 @@ PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
   //gof_r_prefix.push_back(h.str());
   std::map<int, int> pair_map;
   int npairs = 0;
-  for ( int i=0; i<num_species; ++i )
+  for (int i = 0; i < num_species; ++i)
+  {
+    for (int j = i; j < num_species; ++j)
     {
-      for ( int j=i; j<num_species; ++j )
-	{
-	  std::ostringstream os;
-	  os << "gofr_" << elns.getName() << "_" << i << "_" << j;
-	  gof_r_prefix.push_back(os.str());
-	  pair_map[i * num_species + j] = npairs;
-	  ++npairs;
-	}
+      std::ostringstream os;
+      os << "gofr_" << elns.getName() << "_" << i << "_" << j;
+      gof_r_prefix.push_back(os.str());
+      pair_map[i * num_species + j] = npairs;
+      ++npairs;
     }
+  }
   const DistanceTableData& dii(elns.getDistTable(d_aa_ID_));
   if (dii.DTType == DT_AOS)
-    {
+  {
 #ifndef ENABLE_SOA
-      pair_ids.resize(dii.getTotNadj());
-      for ( int iat=0; iat<dii.centers(); ++iat )
-	{
-	  int ig = elns.GroupID[iat] * num_species;
-	  for ( int nn=dii.M[iat]; nn<dii.M[iat + 1]; ++nn)
-	    {
-	      pair_ids[nn] = pair_map[ig + elns.GroupID[dii.J[nn]]];
-	    }
-	}
-#endif
+    pair_ids.resize(dii.getTotNadj());
+    for (int iat = 0; iat < dii.centers(); ++iat)
+    {
+      int ig = elns.GroupID[iat] * num_species;
+      for (int nn = dii.M[iat]; nn < dii.M[iat + 1]; ++nn)
+        pair_ids[nn] = pair_map[ig + elns.GroupID[dii.J[nn]]];
     }
+#endif
+  }
 
   // source-target tables
   std::vector<std::string> slist, dlist;
   const int ntables = elns.getNumDistTables();
-  for ( int k=0; k<ntables; ++k )
-    {
-      if ( elns.getName() != elns.getDistTable(k).origin().getName() )
-	{
-	  dlist.push_back(elns.getDistTable(k).origin().getName());
-	}
-    }
+  for (int k = 0; k < ntables; ++k)
+  {
+    if (elns.getName() != elns.getDistTable(k).origin().getName())
+      dlist.push_back(elns.getDistTable(k).origin().getName());
+  }
   parsewords(sources.c_str(), slist);
   std::set<int> others_sorted;
-  for ( int i=0; i<slist.size(); ++i )
+  for (int i = 0; i < slist.size(); ++i)
+  {
+    int k = 0;
+    while (k < dlist.size())
     {
-      int k = 0;
-      while ( k < dlist.size() )
-	{
-	  if ( slist[i] == dlist[k] )
-	    {
-	      others_sorted.insert(k + 1);
-	      break;
-	    }
-	  ++k;
-	}
+      if (slist[i] == dlist[k])
+      {
+        others_sorted.insert(k + 1);
+        break;
+      }
+      ++k;
     }
+  }
   other_ids.resize(others_sorted.size());
   other_offsets.resize(others_sorted.size());
   copy(others_sorted.begin(), others_sorted.end(), other_ids.begin());
   int toff = gof_r_prefix.size();
-  for ( int k=0; k<other_ids.size(); ++k )
+  for (int k = 0; k < other_ids.size(); ++k)
+  {
+    const DistanceTableData& t(elns.getDistTable(other_ids[k]));
+    app_log() << "  GOFR for " << t.getName() << " starts at " << toff << std::endl;
+    other_offsets[k] = toff;
+    const SpeciesSet& species(t.origin().getSpeciesSet());
+    int ng = species.size();
+    for (int i = 0; i < ng; ++i)
     {
-      const DistanceTableData& t(elns.getDistTable(other_ids[k]));
-      app_log() << "  GOFR for " << t.getName() << " starts at " << toff << std::endl;
-      other_offsets[k] = toff;
-      const SpeciesSet& species(t.origin().getSpeciesSet());
-      int ng = species.size();
-      for ( int i=0; i<ng; ++i )
-	{
-	  std::ostringstream os;
-	  os << "gofr_" << t.getName() << "_" << species.speciesName[i];
-	  gof_r_prefix.push_back(os.str());
-	}
-      toff += ng;
+      std::ostringstream os;
+      os << "gofr_" << t.getName() << "_" << species.speciesName[i];
+      gof_r_prefix.push_back(os.str());
     }
+    toff += ng;
+  }
 }
 
 void PairCorrEstimator::resetTargetParticleSet(ParticleSet& P) {}
@@ -127,85 +123,85 @@ PairCorrEstimator::Return_t PairCorrEstimator::evaluate(ParticleSet& P)
 {
   BufferType& collectables(P.Collectables);
   const DistanceTableData& dii(P.getDistTable(d_aa_ID_));
-  if ( dii.DTType == DT_SOA )  // Start SoA eval
+  if (dii.DTType == DT_SOA) // Start SoA eval
+  {
+    for (int iat = 1; iat < dii.centers(); ++iat)
     {
-      for ( int iat=1; iat<dii.centers(); ++iat )
-	{
-	  const RealType* restrict dist = dii.Distances[iat];
-	  const int ig                  = P.GroupID[iat];
-	  for ( int j=0; j<iat; ++j )
-	    {
-	      const RealType r = dist[j];
-	      if ( r < Dmax )
-		{
-		  const int loc     = static_cast<int>(DeltaInv * r);
-		  const int jg      = P.GroupID[j];
-		  const int pair_id = ig * ( ig + 1 ) / 2 + jg;
-		  collectables[pair_id * NumBins + loc + myIndex] += norm_factor(pair_id + 1, loc);
-		}
-	    }
-	}
-      for (int k=0; k<other_ids.size(); ++k)
-	{
-	  const DistanceTableData& d1(P.getDistTable(other_ids[k]));
-	  const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
-	  int koff        = other_offsets[k];
-	  RealType overNI = 1.0 / d1.centers();
-	  for ( int iat=0; iat<d1.targets(); ++iat )
-	    {
-	      const RealType* restrict dist = d1.Distances[iat];
-	      for ( int j=0; j<d1.centers(); ++j )
-		{
-		  const RealType r = dist[j];
-		  if ( r < Dmax )
-		    {
-		      int toff = (gid[j] + koff) * NumBins;
-		      int loc  = static_cast<int>(DeltaInv * r);
-		      collectables[toff + loc + myIndex] += norm_factor(0, loc) * overNI;
-		    }
-		}
-	    }
-	}
-    }  // End SoA eval
-  else  // Start AoS eval
-    {
-#ifndef ENABLE_SOA
-      for ( int iat=0; iat<dii.centers(); ++iat )
-	{
-	  for ( int nn=dii.M[iat]; nn<dii.M[iat + 1]; ++nn )
-	    {
-	      const RealType r = dii.r(nn);
-	      if (r < Dmax)
-		{
-		  const int loc = static_cast<int>(DeltaInv * r);
-		  const int bin = pair_ids[nn] * NumBins + loc + myIndex;
-		  collectables[bin] += norm_factor(pair_ids[nn] + 1, loc);
-		}
-	    }
-	}
-      for ( int k=0; k<other_ids.size(); ++k )
-	{
-	  const DistanceTableData& d1(P.getDistTable(other_ids[k]));
-	  const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
-	  int koff = other_offsets[k];
-	  for ( int iat=0; iat<d1.centers(); ++iat)
-	    {
-	      RealType overNI = 1.0 / d1.centers();
-	      int toff        = (gid[iat] + koff) * NumBins;
-	      for ( int nn=d1.M[iat]; nn<d1.M[iat + 1]; ++nn )
-		{
-		  RealType r = dii.r(nn);
-		  if ( r < Dmax )
-		    {
-		      const int loc = static_cast<int>(DeltaInv * r);
-		      const int bin = toff + loc + myIndex;
-		      collectables[bin] += norm_factor(0, loc) * overNI;
-		    }
-		}
-	    }
-	}
-#endif
+      const RealType* restrict dist = dii.Distances[iat];
+      const int ig                  = P.GroupID[iat];
+      for (int j = 0; j < iat; ++j)
+      {
+        const RealType r = dist[j];
+        if (r < Dmax)
+        {
+          const int loc     = static_cast<int>(DeltaInv * r);
+          const int jg      = P.GroupID[j];
+          const int pair_id = ig * (ig + 1) / 2 + jg;
+          collectables[pair_id * NumBins + loc + myIndex] += norm_factor(pair_id + 1, loc);
+        }
+      }
     }
+    for (int k = 0; k < other_ids.size(); ++k)
+    {
+      const DistanceTableData& d1(P.getDistTable(other_ids[k]));
+      const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
+      int koff        = other_offsets[k];
+      RealType overNI = 1.0 / d1.centers();
+      for (int iat = 0; iat < d1.targets(); ++iat)
+      {
+        const RealType* restrict dist = d1.Distances[iat];
+        for (int j = 0; j < d1.centers(); ++j)
+        {
+          const RealType r = dist[j];
+          if (r < Dmax)
+          {
+            int toff = (gid[j] + koff) * NumBins;
+            int loc  = static_cast<int>(DeltaInv * r);
+            collectables[toff + loc + myIndex] += norm_factor(0, loc) * overNI;
+          }
+        }
+      }
+    }
+  }    // End SoA eval
+  else // Start AoS eval
+  {
+#ifndef ENABLE_SOA
+    for (int iat = 0; iat < dii.centers(); ++iat)
+    {
+      for (int nn = dii.M[iat]; nn < dii.M[iat + 1]; ++nn)
+      {
+        const RealType r = dii.r(nn);
+        if (r < Dmax)
+        {
+          const int loc = static_cast<int>(DeltaInv * r);
+          const int bin = pair_ids[nn] * NumBins + loc + myIndex;
+          collectables[bin] += norm_factor(pair_ids[nn] + 1, loc);
+        }
+      }
+    }
+    for (int k = 0; k < other_ids.size(); ++k)
+    {
+      const DistanceTableData& d1(P.getDistTable(other_ids[k]));
+      const ParticleSet::ParticleIndex_t& gid(d1.origin().GroupID);
+      int koff = other_offsets[k];
+      for (int iat = 0; iat < d1.centers(); ++iat)
+      {
+        RealType overNI = 1.0 / d1.centers();
+        int toff        = (gid[iat] + koff) * NumBins;
+        for (int nn = d1.M[iat]; nn < d1.M[iat + 1]; ++nn)
+        {
+          RealType r = dii.r(nn);
+          if (r < Dmax)
+          {
+            const int loc = static_cast<int>(DeltaInv * r);
+            const int bin = toff + loc + myIndex;
+            collectables[bin] += norm_factor(0, loc) * overNI;
+          }
+        }
+      }
+    }
+#endif
+  }
   return 0.0;
 }
 
@@ -294,9 +290,9 @@ void PairCorrEstimator::set_norm_factor()
      Note the addition +1 bin, which is for the total gofr of the system
      (which seems not to get saved to h5 file. Why compute it then?)
   */
-  const RealType n_channels = num_species*(num_species - 1) / 2 + num_species + 1;
+  const RealType n_channels = num_species * (num_species - 1) / 2 + num_species + 1;
   norm_factor.resize(n_channels, NumBins);
-  
+
   /* 
      Compute the normalization V/Npairs/Nid, with
      V the volume of the system
@@ -304,42 +300,42 @@ void PairCorrEstimator::set_norm_factor()
      Nid the number of particles expected for a uniformly random distribution
      with the same number density
   */
-  RealType r  = 0.;
-  const RealType ftpi = 4./3 * M_PI;
-  const RealType N_tot_pairs = N_e*(N_e - 1) / 2;
-  for ( int i=0; i<NumBins; i++ )
+  RealType r                 = 0.;
+  const RealType ftpi        = 4. / 3 * M_PI;
+  const RealType N_tot_pairs = N_e * (N_e - 1) / 2;
+  for (int i = 0; i < NumBins; i++)
+  {
+    // Separation for current bin
+    r = static_cast<RealType>(i) * Delta;
+
+    // Number density of pairs
+    RealType rho = N_tot_pairs / Volume;
+
+    // Volume of spherical shell of thickness Delta
+    RealType bin_volume = ftpi * (std::pow(r + Delta, 3) - std::pow(r, 3));
+
+    // Expected number of pairs of particles separated by distance r assuming
+    // they are uniformly randomly distributed (ideal gas-like)
+    RealType nid      = rho * bin_volume;
+    norm_factor(0, i) = 1. / nid;
+    int indx(1);
+
+    // Do same as above, but for each unique pair of species
+    // e.g. uu, ud, dd...
+    for (int m = 0; m < num_species; m++)
     {
-	// Separation for current bin
-	r = static_cast<RealType>(i)*Delta;
-
-	// Number density of pairs
-	RealType rho = N_tot_pairs / Volume;
-
-	// Volume of spherical shell of thickness Delta
-	RealType bin_volume = ftpi * ( std::pow(r+Delta, 3) - std::pow(r, 3) );
-
-	// Expected number of pairs of particles separated by distance r assuming
-	// they are uniformly randomly distributed (ideal gas-like)
-	RealType nid = rho * bin_volume;
-	norm_factor(0, i) = 1. / nid;
-	int indx(1);
-
-	// Do same as above, but for each unique pair of species
-	// e.g. uu, ud, dd...
-	for ( int m=0; m<num_species; m++ )
-	  {
-	    const RealType nm = n_vec[m];
-	    for ( int n=m; n<num_species; n++ )
-	      {
-		const RealType nn = n_vec[n];
-		const RealType npairs = ( m == n ? (nn*(nn-1)/2.) : (nn*nm) );
-		rho = npairs / Volume;
-		nid = rho * bin_volume;
-		norm_factor(indx, i) = 1. / nid;
-		indx++;
-	      }
-	  }
+      const RealType nm = n_vec[m];
+      for (int n = m; n < num_species; n++)
+      {
+        const RealType nn     = n_vec[n];
+        const RealType npairs = (m == n ? (nn * (nn - 1) / 2.) : (nn * nm));
+        rho                   = npairs / Volume;
+        nid                   = rho * bin_volume;
+        norm_factor(indx, i)  = 1. / nid;
+        indx++;
+      }
     }
+  }
 
   // ***DEBUG*** print norm_factor
   /*
@@ -357,7 +353,6 @@ void PairCorrEstimator::set_norm_factor()
     }
   std::cout << std::endl;
   */
-
 }
 
 
---
+++ src/QMCHamiltonians/tests/test_PairCorrEstimator.cpp
@@ -0,0 +1,203 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Mark Dewing, markdewing@gmail.com, University of Illinois at Urbana-Champaign
+//
+// File created by: Mark Dewing, markdewing@gmail.com, University of Illinois at Urbana-Champaign
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "catch.hpp"
+#include "OhmmsData/Libxml2Doc.h"
+#include "Lattice/CrystalLattice.h"
+#include "Lattice/ParticleBConds.h"
+#include "Particle/ParticleSet.h"
+#include "Particle/DistanceTableData.h"
+#include "QMCHamiltonians/PairCorrEstimator.h"
+#include "QMCApp/ParticleSetPool.h"
+
+#include <stdio.h>
+#include <string>
+
+using std::string;
+
+
+/*
+  Minimal input block (based off HEG problem).
+  This is a dumb way to construct a test, but I can't figure out
+  the right way to initialize a ParticleSet, so instead hijack 
+  the ParticleSetPool class to make a ParticleSet for me.
+
+  NB: Hardcoded 8 particles (4 each of u/d) on a B1 (NaCl) lattice.
+*/
+
+// Lattice block
+const char* lat_xml = "<simulationcell> \
+                         <parameter name=\"bconds\"> \
+                           p p p \
+                        </parameter> \
+                        <parameter name=\"LR_dim_cutoff\" >     6  </parameter> \
+                        <parameter name=\"rs\"            >   1.0  </parameter> \
+                        <parameter name=\"nparticles\"    >     8  </parameter> \
+                        </simulationcell>";
+
+// Particleset block
+const char* pset_xml = "<particleset name=\"e\" random=\"yes\"> \
+                          <group name=\"u\" size=\"4\" mass=\"1.0\"> \
+                            <parameter name=\"charge\" >   -1  </parameter> \
+                            <parameter name=\"mass\"   >  1.0  </parameter> \
+                          </group> \
+                          <group name=\"d\" size=\"4\" mass=\"1.0\"> \
+                            <parameter name=\"charge\" >   -1  </parameter> \
+                            <parameter name=\"mass\"   >  1.0  </parameter> \
+                          </group> \
+                        </particleset>";
+
+// PairCorrEstimator block
+const char* gofr_xml = "<estimator type=\"gofr\" name=\"gofr\" rmax=\"2.0\" num_bin=\"20\" />";
+
+
+namespace qmcplusplus
+{
+TEST_CASE("Pair Correlation", "[hamiltonian]")
+{
+  std::cout << std::fixed;
+  std::cout << std::setprecision(8);
+  typedef QMCTraits::RealType RealType;
+
+  Communicate* c;
+  OHMMS::Controller->initialize(0, NULL);
+  c = OHMMS::Controller;
+
+  CrystalLattice<OHMMS_PRECISION, OHMMS_DIM> lattice;
+  lattice.BoxBConds = true; // periodic
+  lattice.R.diagonal(2.0);
+  lattice.reset();
+
+  // XML parser
+  Libxml2Document doc;
+
+  std::cout << "\n\n\ntest_paircorr:: START\n";
+
+  // TEST new idea: ParticlesetPool to make a ParticleSet
+  bool lat_okay = doc.parseFromString(lat_xml);
+  REQUIRE(lat_okay);
+  xmlNodePtr lat_xml_root = doc.getRoot();
+
+  ParticleSetPool pset_builder(c, "pset_builder");
+  pset_builder.putLattice(lat_xml_root); // Builds lattice
+
+  bool pset_okay = doc.parseFromString(pset_xml);
+  REQUIRE(pset_okay);
+  xmlNodePtr pset_xml_root = doc.getRoot();
+  pset_builder.put(pset_xml_root); // Builds ParticleSet
+
+  // Get the (now assembled) ParticleSet, do simple sanity checks, then print info
+  ParticleSet* elec = pset_builder.getParticleSet("e");
+  elec->Lattice     = lattice; // copy in the new Lattice
+
+  REQUIRE(elec->SameMass);
+  REQUIRE(elec->getName() == "e");
+
+  // Move the particles manually onto B1 lattice
+  // NB: Spins are grouped contiguously
+  // Up spins
+  elec->R[0][0] = 0.0;
+  elec->R[0][1] = 0.0;
+  elec->R[0][2] = 0.0;
+  elec->R[1][0] = 1.0;
+  elec->R[1][1] = 1.0;
+  elec->R[1][2] = 0.0;
+  elec->R[2][0] = 1.0;
+  elec->R[2][1] = 0.0;
+  elec->R[2][2] = 1.0;
+  elec->R[3][0] = 0.0;
+  elec->R[3][1] = 1.0;
+  elec->R[3][2] = 1.0;
+
+  // Down spins
+  elec->R[4][0] = 1.0;
+  elec->R[4][1] = 0.0;
+  elec->R[4][2] = 0.0;
+  elec->R[5][0] = 0.0;
+  elec->R[5][1] = 1.0;
+  elec->R[5][2] = 0.0;
+  elec->R[6][0] = 0.0;
+  elec->R[6][1] = 0.0;
+  elec->R[6][2] = 1.0;
+  elec->R[7][0] = 1.0;
+  elec->R[7][1] = 1.0;
+  elec->R[7][2] = 1.0;
+
+  elec->get(std::cout); // print particleset info to stdout
+
+  // Set up the distance table, match expected layout
+#ifdef ENABLE_SOA
+  const int ee_table_id = elec->addTable(*elec, DT_SOA, false);
+#else
+  const int ee_table_id = elec->addTable(*elec, DT_AOS, false);
+#endif
+
+  const DistanceTableData& dii(elec->getDistTable(ee_table_id));
+  elec->update(); // distance table evaluation here
+
+  // Make a PairCorrEstimator, call put() to set up internals
+  std::string name = elec->getName();
+  PairCorrEstimator paircorr(*elec, name);
+  bool gofr_okay = doc.parseFromString(gofr_xml);
+  REQUIRE(gofr_okay);
+  xmlNodePtr gofr_xml_root = doc.getRoot();
+  paircorr.put(gofr_xml_root);
+  paircorr.addObservables(elec->PropertyList, elec->Collectables);
+
+  // Compute g(r) then print it to stdout
+  // NB: Hardcoded to match hardcoded xml above. ***Fragile!!!***
+  paircorr.evaluate(*elec);
+
+  auto gofr = elec->Collectables;
+  std::cout << "\n";
+  std::cout << "gofr:\n";
+  std::cout << std::fixed;
+  std::cout << std::setprecision(6);
+  std::cout << std::setw(4) << "i"
+            << "  " << std::setw(12) << "r"
+            << "  " << std::setw(12) << "uu"
+            << "  " << std::setw(12) << "ud"
+            << "  " << std::setw(12) << "dd"
+            << "\n";
+  std::cout << "============================================================\n";
+  for (int i(0); i < 20; i++)
+  {
+    std::cout << std::setw(4) << i << "  " << std::setw(12) << i * 0.10 << "  " << std::setw(12) << gofr[i] << "  "
+              << std::setw(12) << gofr[i + 20] << "  " << std::setw(12) << gofr[i + 40] << "\n";
+  }
+
+  // Verify against analytic result.
+  // NB: At the moment the tolerance is fairly loose because there
+  //     is about 0.08-0.01 disagreement between QMCPACK and analytic
+  //     result, depending on the bin. I'm not sure about the source
+  //     of the disagreement because norm_factor is now exact.
+  //     The only thing left is the distance table (I think)...
+  const RealType eps = 1E-01; // tolerance
+
+  // Nearest neighbor peak (ud) | Distance = 1
+  REQUIRE(std::fabs(gofr[10] - 0.00000) < eps);
+  REQUIRE(std::fabs(gofr[30] - 4.32744) < eps);
+  REQUIRE(std::fabs(gofr[50] - 0.00000) < eps);
+
+  // 2nd-nearest neighbor peak (uu/dd) | Distance = sqrt(2)
+  REQUIRE(std::fabs(gofr[14] - 2.96827) < eps);
+  REQUIRE(std::fabs(gofr[34] - 0.00000) < eps);
+  REQUIRE(std::fabs(gofr[54] - 2.96827) < eps);
+
+  // 3rd-nearest neighbor peak (ud) | Distance = sqrt(3)
+  REQUIRE(std::fabs(gofr[17] - 0.00000) < eps);
+  REQUIRE(std::fabs(gofr[37] - 0.50103) < eps);
+  REQUIRE(std::fabs(gofr[57] - 0.00000) < eps);
+
+  std::cout << "test_paircorr:: STOP\n";
+}
+} // namespace qmcplusplus
