a71be39b663f4e9e630d85618cc7ec9cac3aa30a
Ye Luo <yeluo@anl.gov>
2019-11-15 09:30:08 -0600

    Need cassert header.

--- src/OhmmsPETE/OhmmsTinyMeta.h
+++ src/OhmmsPETE/OhmmsTinyMeta.h
@@ -57,6 +57,7 @@ struct OTDot
 // TinyVectorTensorOps.h  binary operators for TinyVector and Tensor combinations
 // TinyMatrixOps.h        assignment/unary and binary operators  for TinyMatrix
 ////////////////////////////////////////////////////////////////////////////////
+#include<cassert>
 #define PAssert(condition) assert(condition)
 #include "OhmmsPETE/TinyVectorOps.h"
 #include "OhmmsPETE/TensorOps.h"
