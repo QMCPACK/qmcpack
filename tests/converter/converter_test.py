43401c3661e91fbc2e2abcacd16734ab37e55cfa
Mark Dewing <markdewing@gmail.com>
2019-12-09 23:13:01 -0600

    Changes tabs to spaces.
    Python 3 is more particular about mixing spaces and tabs.

--- tests/converter/converter_test.py
+++ tests/converter/converter_test.py
@@ -36,9 +36,9 @@ def compare(gold_file,test_file):
                     print('< diff truncated due to line limit >')
                     break
 
-	    return False
-	else:
-	    return True
+            return False
+        else:
+            return True
 
 
 def run_test(test_name, c4q_exe, h5diff_exe, conv_inp, gold_file, expect_fail, extra_cmd_args,code):
@@ -85,7 +85,7 @@ def run_test(test_name, c4q_exe, h5diff_exe, conv_inp, gold_file, expect_fail, e
         if not os.path.exists(gold_file):
             print("Gold file missing")
             okay = False
-	else:
+        else:
             if (code != 'pyscf'): 
                 if '-hdf5' in extra_cmd_args:
                    ret = os.system(h5diff_exe + ' -d 0.000001 gold.orbs.h5 test.orbs.h5')
