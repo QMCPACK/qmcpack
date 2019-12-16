5b2c017f7fa9290ec4401b116cda6a93411f784e
rcclay <j.k.rofling@gmail.com>
2019-12-16 11:01:19 -0700

    Add a mixed precision guard for workaround

--- src/LongRange/KContainer.cpp
+++ src/LongRange/KContainer.cpp
@@ -253,7 +253,13 @@ void KContainer::BuildKLists(ParticleLayout_t& lattice, bool useSphere)
   //create the map: use simple integer with resolution of 0.00000001 in ksq
   for (int ik = 0; ik < numk; ik++)
   {
-    long long k_ind = static_cast<long long>(ksq_tmp[ik] * 100000000);
+    #ifdef MIXED_PRECISION
+    long long k_ind = static_cast<long long>(ksq_tmp[ik] * 1000);
+    #else
+    //This is a workaround for ewald bug (Issue #2105) for FULL PRECISION ONLY.  Basically, 1e-7 is the resolution of |k|^2 for doubles,
+    //so we jack up the tolerance to match that.   
+    long long k_ind = static_cast<long long>(ksq_tmp[ik] * 10000000);
+    #endif
     std::map<long long, std::vector<int>*>::iterator it(kpts_sorted.find(k_ind));
     if (it == kpts_sorted.end())
     {
