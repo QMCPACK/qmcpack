d3c29eee26f26854a7bd24db3676aee97727613a
Ye Luo <yeluo@anl.gov>
2019-12-18 23:58:02 +0800

    Update license header

--- src/QMCWaveFunctions/BsplineFactory/BsplineSet.h
+++ src/QMCWaveFunctions/BsplineFactory/BsplineSet.h
@@ -2,7 +2,7 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2019 QMCPACK developers.
 //
 // File developed by: Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
 //                    Jaron T. Krogel, krogeljt@ornl.gov, Oak Ridge National Laboratory
