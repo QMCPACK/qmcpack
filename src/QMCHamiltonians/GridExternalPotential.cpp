41dc5ace1d041c6a3f3150b7aba369d0ad3a4248
Kevin Ryczko <kevinryczko@me.com>
2020-01-05 16:07:59 -0500

    Changed edit date and editors

--- src/QMCHamiltonians/GridExternalPotential.cpp
+++ src/QMCHamiltonians/GridExternalPotential.cpp
@@ -2,7 +2,7 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2019 QMCPACK developers.
 //
 // File developed by: Jaron T. Krogel, krogeljt@ornl.gov, Oak Ridge National Laboratory
 //                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
