1e1fde9969d4dc0b2c09f783c8c47787a74daf2a
Ye Luo <yeluo@anl.gov>
2020-01-13 23:37:09 -0500

    Add a missing transfer.

--- src/Particle/ParticleSet.cpp
+++ src/Particle/ParticleSet.cpp
@@ -388,6 +388,10 @@ void ParticleSet::flex_update(const RefVector<ParticleSet>& p_list, bool skipSK)
   if (p_list.size() > 1)
   {
     ScopedTimer update_scope(p_list[0].get().myTimers[PS_update]);
+
+    for (ParticleSet& pset : p_list)
+      pset.RSoA->setAllParticlePos(pset.R);
+
     auto& dts = p_list[0].get().DistTables;
     for (int i = 0; i < dts.size(); ++i)
     {
