b0490f1ac7b34b8b7e5e871856d989e76a7942a5
Mark Dewing <markdewing@gmail.com>
2020-03-04 15:58:31 -0600

    Remove old definition of MCSample
    Oops, forgot to remove the definition of MCSample from MCWalkerConfiguration.cpp

--- src/Particle/MCWalkerConfiguration.cpp
+++ src/Particle/MCWalkerConfiguration.cpp
@@ -36,55 +36,6 @@
 
 namespace qmcplusplus
 {
-/** store minimum Walker data for the next section
- */
-struct MCSample
-{
-  using WP = WalkerProperties::Indexes;
-  typedef ParticleSet::Walker_t Walker_t;
-
-  ParticleSet::ParticlePos_t R;
-  ParticleSet::ParticleGradient_t G;
-  ParticleSet::ParticleLaplacian_t L;
-  ParticleSet::RealType LogPsi, Sign, PE, KE;
-
-  inline MCSample(const Walker_t& w) : R(w.R), G(w.G), L(w.L)
-  {
-    LogPsi = w.Properties(WP::LOGPSI);
-    Sign   = w.Properties(WP::SIGN);
-    PE     = w.Properties(WP::LOCALPOTENTIAL);
-    KE     = w.Properties(WP::LOCALENERGY) - PE;
-  }
-
-  inline MCSample(int n)
-  {
-    R.resize(n);
-    G.resize(n);
-    L.resize(n);
-  }
-
-  inline void put(const Walker_t& w)
-  {
-    R      = w.R;
-    G      = w.G;
-    L      = w.L;
-    LogPsi = w.Properties(WP::LOGPSI);
-    Sign   = w.Properties(WP::SIGN);
-    PE     = w.Properties(WP::LOCALPOTENTIAL);
-    KE     = w.Properties(WP::LOCALENERGY) - PE;
-  }
-
-  inline void get(Walker_t& w) const
-  {
-    w.R                          = R;
-    w.G                          = G;
-    w.L                          = L;
-    w.Properties(WP::LOGPSI)         = LogPsi;
-    w.Properties(WP::SIGN)           = Sign;
-    w.Properties(WP::LOCALPOTENTIAL) = PE;
-    w.Properties(WP::LOCALENERGY)    = PE + KE;
-  }
-};
 
 MCWalkerConfiguration::MCWalkerConfiguration()
     :
