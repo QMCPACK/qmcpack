fba926c5f58e3d3ed814b75486395bb3ff3b8cdf
Peter Doak <doakpw@ornl.gov>
2020-03-09 15:59:57 -0400

    working around slightly mashed vm spack

--- tests/test_automation/vm_spack_env_cpu.sh
+++ tests/test_automation/vm_spack_env_cpu.sh
@@ -6,9 +6,8 @@
 
 QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1"
 
-spack load llvm@$llvm_vnew
-spack load boost@$boost_vnew%gcc@$gcc_vnew
 spack load gcc@$gcc_vnew
+spack load boost@$boost_vnew%gcc@$gcc_vnew
 spack load hdf5@$hdf5_vnew%gcc@$gcc_vnew~mpi
 spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load openmpi@$ompi_vnew%gcc@$gcc_vnew
