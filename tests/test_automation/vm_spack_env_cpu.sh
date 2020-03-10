eeaf1bf42b2f4dffa51f69d48f0dccb2356bc307
Peter Doak <doakpw@ornl.gov>
2020-03-10 18:15:06 -0400

    as does hwloc

--- tests/test_automation/vm_spack_env_cpu.sh
+++ tests/test_automation/vm_spack_env_cpu.sh
@@ -11,6 +11,7 @@ spack load boost@$boost_vnew%gcc@$gcc_vnew
 spack load hdf5@$hdf5_vnew%gcc@$gcc_vnew~mpi
 spack load openssl@1.1.1d%gcc@9.2.0+systemcerts
 spack load cmake@$cmake_vnew%gcc@$gcc_vnew
+spack load hwloc%gcc@$gcc_vnew
 spack load openmpi@$ompi_vnew%gcc@$gcc_vnew
 spack load libxml2@$libxml2_vnew%gcc@$gcc_vnew
 spack load fftw@$fftw_vnew%gcc@$gcc_vnew
