cda9daf38af457fc150425092485074daed9da67
Peter Doak <doakpw@ornl.gov>
2020-03-10 18:07:10 -0400

    openssl library path needs to be there for clean centos 7

--- tests/test_automation/vm_spack_env_cpu.sh
+++ tests/test_automation/vm_spack_env_cpu.sh
@@ -9,6 +9,7 @@ QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1"
 spack load gcc@$gcc_vnew%gcc@$gcc_vold
 spack load boost@$boost_vnew%gcc@$gcc_vnew
 spack load hdf5@$hdf5_vnew%gcc@$gcc_vnew~mpi
+spack load openssl@1.1.1d%gcc@9.2.0+systemcerts
 spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load openmpi@$ompi_vnew%gcc@$gcc_vnew
 spack load libxml2@$libxml2_vnew%gcc@$gcc_vnew
