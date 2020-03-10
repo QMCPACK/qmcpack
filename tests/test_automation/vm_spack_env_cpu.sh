fce4f151e57739a181feba111ac17cdf3eeaf85a
Peter Doak <doakpw@ornl.gov>
2020-03-10 18:17:10 -0400

    and libiconv

--- tests/test_automation/vm_spack_env_cpu.sh
+++ tests/test_automation/vm_spack_env_cpu.sh
@@ -12,6 +12,7 @@ spack load hdf5@$hdf5_vnew%gcc@$gcc_vnew~mpi
 spack load openssl@1.1.1d%gcc@9.2.0+systemcerts
 spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load hwloc%gcc@$gcc_vnew
+spack load libiconv%gcc@$gcc_vnew
 spack load openmpi@$ompi_vnew%gcc@$gcc_vnew
 spack load libxml2@$libxml2_vnew%gcc@$gcc_vnew
 spack load fftw@$fftw_vnew%gcc@$gcc_vnew
