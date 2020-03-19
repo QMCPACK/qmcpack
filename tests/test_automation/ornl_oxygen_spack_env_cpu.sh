4ef5fa5095f7441e7a5613ccad0dd1eeec8991ff
Peter Doak <doakpw@ornl.gov>
2020-03-19 00:09:11 -0400

    trying preflags

--- tests/test_automation/ornl_oxygen_spack_env_cpu.sh
+++ tests/test_automation/ornl_oxygen_spack_env_cpu.sh
@@ -12,4 +12,8 @@ spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load -r openmpi@$ompi_vnew%clang@$llvm_vnew
 spack load fftw@$fftw_vnew%gcc@$gcc_vnew
 GCC_TOOLCHAIN_PATH=$(spack find -p gcc@$gcc_vnew | awk -v gcc_version="gcc@$gcc_vnew" -e '{ if ($1 == gcc_version) { print $2 }}')
-QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1 -DCMAKE_CXX_FLAGS=\'--gcc-toolchain=${GCC_TOOLCHAIN_PATH} -stdlib=libstdc++\' -DCMAKE_EXE_LINKER_FLAGS=\'-L${GCC_TOOLCHAIN_PATH}/lib -Wl,-rpath,${GCC_TOOLCHAIN_PATH}/lib\'"
+
+CXXFLAGS="--gcc-toolchain=${GCC_TOOLCHAIN_PATH} -stdlib=libstdc++"
+CFLAGS="--gcc-toolchain=${GCC_TOOLCHAIN_PATH}"
+LDFLAGS="-L${GCC_TOOLCHAIN_PATH}/lib64 -Wl,-rpath,${GCC_TOOLCHAIN_PATH}/lib64"
+QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1"
