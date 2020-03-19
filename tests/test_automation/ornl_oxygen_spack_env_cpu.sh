fe38079f3e0d4d51b5fac54de3f8b51f15a59f70
Peter Doak <doakpw@ornl.gov>
2020-03-19 00:03:52 -0400

    different escaping scheme

--- tests/test_automation/ornl_oxygen_spack_env_cpu.sh
+++ tests/test_automation/ornl_oxygen_spack_env_cpu.sh
@@ -12,4 +12,4 @@ spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load -r openmpi@$ompi_vnew%clang@$llvm_vnew
 spack load fftw@$fftw_vnew%gcc@$gcc_vnew
 GCC_TOOLCHAIN_PATH=$(spack find -p gcc@$gcc_vnew | awk -v gcc_version="gcc@$gcc_vnew" -e '{ if ($1 == gcc_version) { print $2 }}')
-QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1 -DCMAKE_CXX_FLAGS=--gcc-toolchain=${GCC_TOOLCHAIN_PATH};-stdlib=libstdc++ -DCMAKE_EXE_LINKER_FLAGS=-L${GCC_TOOLCHAIN_PATH}/lib;-Wl,-rpath,${GCC_TOOLCHAIN_PATH}/lib"
+QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1 -DCMAKE_CXX_FLAGS=\'--gcc-toolchain=${GCC_TOOLCHAIN_PATH} -stdlib=libstdc++\' -DCMAKE_EXE_LINKER_FLAGS=\'-L${GCC_TOOLCHAIN_PATH}/lib -Wl,-rpath,${GCC_TOOLCHAIN_PATH}/lib\'"
