a062bd990bd208270500399a73936639f0746eb7
Peter Doak <doakpw@ornl.gov>
2020-03-18 23:41:13 -0400

    stdlib c++ issue

--- tests/test_automation/ornl_oxygen_spack_env_cpu.sh
+++ tests/test_automation/ornl_oxygen_spack_env_cpu.sh
@@ -4,8 +4,6 @@
 
 . ../tests/test_automation/spack_supported_package_versions.sh
 
-QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1"
-
 spack load llvm@$llvm_vnew
 spack load boost@$boost_vnew%gcc@$gcc_vnew
 spack load gcc@$gcc_vnew
@@ -13,3 +11,5 @@ spack load hdf5@$hdf5_vnew%gcc@$gcc_vnew~mpi
 spack load cmake@$cmake_vnew%gcc@$gcc_vnew
 spack load -r openmpi@$ompi_vnew%clang@$llvm_vnew
 spack load fftw@$fftw_vnew%gcc@$gcc_vnew
+GCC_TOOLCHAIN_PATH=$(spack find -p gcc@$gcc_vnew | awk -v gcc_version="gcc@$gcc_vnew" -e '{ if ($1 == gcc_version) { print $2 }}')
+QMC_IMMUTABLE_FLAGS="-DBUILD_AFQMC=1 -DCMAKE_CXX_FLAGS=--gcc-toolchain=${GCC_TOOLCHAIN_PATH};--stdlib=stdlibc++ -DCMAKE_EXE_LINKER_FLAGS=-L${GCC_TOOLCHAIN_PATH}/lib;-Wl,-rpath,${GCC_TOOLCHAIN_PATH}/lib"
