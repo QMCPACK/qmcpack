d54c07d72c00db0866d46db0fa8914414fd1aaf0
Peter Doak <PDoakORNL@users.noreply.github.com>
2020-03-20 14:35:35 -0400

    Ci pipeline network retry (#2351)
    Ci pipeline network retry

--- tests/test_automation/jenkins_ornl_oxygen.sh
+++ tests/test_automation/jenkins_ornl_oxygen.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -x
-
-#this is will be general move cpu build soon
+#!/bin/bash --login
 
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+
+echo "starting spack using ${SPACK_ENV_FILE}"
 
 # this depends on SPACK_ROOT being set in Jenkinsfile_xxx
 # it also supplies QMC_IMMUTABLE_FLAGS which makes it a bit more than the
 # environment from the set of loaded spack packages.
 . ${SPACK_ENV_FILE}
+
+module list
 
 exit_code=0
 
@@ -38,17 +40,26 @@ echo "at $(date)"
 echo ""
 echo ""
 
+rm -rf build_${1}_${2}
 mkdir build_${1}_${2}
 cd build_${1}_${2}
 
-cmake ${QMCNSPACE_FLAG} ${QMCPRECISION_FLAG} -DENABLE_SOA=1 -DCMAKE_C_COMPILER="mpicc" -DCMAKE_CXX_COMPILER="mpicxx" ${QMC_IMMUTABLE_FLAGS} -DQMC_NO_SLOW_CUSTOM_TESTING_COMMANDS=1 ../.. 2>&1 | tee cmake.out
+which cmake
+pwd
+echo "cmake ${QMCNSPACE_FLAG} ${QMCPRECISION_FLAG} -DENABLE_SOA=1 -DCMAKE_C_COMPILER=\"mpicc\" -DCMAKE_CXX_COMPILER=\"mpicxx\" ${QMC_IMMUTABLE_FLAGS} -DQMC_NO_SLOW_CUSTOM_TESTING_COMMANDS=1 ../.. 2>&1 | tee cmake.out"
 
+# this keeps tee from eating the exit status
+set -o pipefail
+
+cmake ${QMCNSPACE_FLAG} ${QMCPRECISION_FLAG} -DENABLE_SOA=1 -DCMAKE_C_COMPILER="mpicc" -DCMAKE_CXX_COMPILER="mpicxx" ${QMC_IMMUTABLE_FLAGS} -DQMC_NO_SLOW_CUSTOM_TESTING_COMMANDS=1 ../.. 2>&1 | tee ${1}_${2}_cmake.out
 if [[ $? -ne 0 ]] ; then
   exit 1
 fi
 
-make -j ${JNK_THREADS}
-if [[ $? -ne 0 ]] ; then
+make -j ${JNK_THREADS} 2>&1 | tee ${1}_${2}_build.out
+MAKE_PIPE_RESULT=$?
+echo "make pipe result $MAKE_PIPE_RESULT"
+if [[ $MAKE_PIPE_RESULT -ne 0 ]] ; then
   exit 1
 fi
 
