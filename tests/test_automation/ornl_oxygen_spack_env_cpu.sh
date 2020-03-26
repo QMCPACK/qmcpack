cbf3747a49d1fb6f94345950d76b950a8296deab
Peter Doak <doakpw@ornl.gov>
2020-03-26 11:20:20 -0400

    this should fix the workspace relative patch issue

--- tests/test_automation/ornl_oxygen_spack_env_cpu.sh
+++ tests/test_automation/ornl_oxygen_spack_env_cpu.sh
@@ -2,7 +2,19 @@
 
 . $SPACK_ROOT/share/spack/setup-env.sh
 
-. ../tests/test_automation/spack_supported_package_versions.sh
+# This is to deal with the fact that we don't know the relationship between the working directory
+# and checkout directory in a relative way.
+# This is from https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+# licensing issue?
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+. ${DIR}/tests/test_automation/spack_supported_package_versions.sh
 
 #temporary change from clang$llvm_vnew
 
