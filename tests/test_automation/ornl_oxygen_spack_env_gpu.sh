90a5d55d00841cecea3fc03513a64b1cb34ac3f3
Peter Doak <doakpw@ornl.gov>
2020-03-26 13:43:06 -0400

    repeating somewhat less bash script

--- tests/test_automation/ornl_oxygen_spack_env_gpu.sh
+++ tests/test_automation/ornl_oxygen_spack_env_gpu.sh
@@ -1,14 +1,8 @@
 #/bin/bash
 
-. $SPACK_ROOT/share/spack/setup-env.sh
-
-# This is to deal with the fact that we don't know the relationship between the working directory
-# and checkout directory in a relative way.
-# this doesn't deal with symlinks
 SRC="${BASH_SOURCE[0]}"
 SRC_DIR="$( cd -P "$( dirname "$SRC" )" >/dev/null 2>&1 && pwd )"
-
-. ${SRC_DIR}/spack_supported_package_versions.sh
+. ${SRC_DIR}/start_spack_env.sh
 
 QMC_IMMUTABLE_FLAGS="-DQMC_CUDA=1"
 
