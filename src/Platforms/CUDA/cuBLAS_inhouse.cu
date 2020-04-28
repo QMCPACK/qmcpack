34ef6e8aee884216bdb48b3243c5851be08680e0
Ye Luo <yeluo@anl.gov>
2020-04-28 01:33:54 -0500

    Formatting cuBLAS_inhouse.

--- src/Platforms/CUDA/cuBLAS_inhouse.cu
+++ src/Platforms/CUDA/cuBLAS_inhouse.cu
@@ -18,16 +18,15 @@ namespace qmcplusplus
 {
 namespace cuBLAS_inhouse
 {
-
 template<typename T, int ROWBS, int COLBS>
-__global__ void gemvT_batched_kernel(const int       m,
-                                     const int       n,
-                                     const T*        alpha,
-                                     const T* const  A[],
-                                     const int       lda,
-                                     const T* const  x[],
-                                     const T*        beta,
-                                     T* const        y[])
+__global__ void gemvT_batched_kernel(const int m,
+                                     const int n,
+                                     const T* __restrict__ alpha,
+                                     const T* const A[],
+                                     const int lda,
+                                     const T* const x[],
+                                     const T* __restrict__ beta,
+                                     T* const y[])
 {
   static_assert(ROWBS <= COLBS, "Row block size must not be larger than column block size!");
 
@@ -38,11 +37,11 @@ __global__ void gemvT_batched_kernel(const int       m,
   for (int i = 0; i < ROWBS; i++)
     sum[i][tid] = T(0.0);
 
-  const T* A_iw = A[blockIdx.x];
-  const T* x_iw = x[blockIdx.x];
+  const T* __restrict__ A_iw = A[blockIdx.x];
+  const T* __restrict__ x_iw = x[blockIdx.x];
 
   const int row_begin = blockIdx.y * ROWBS;
-  const int row_max = (m - row_begin) < ROWBS ? (m - row_begin) : ROWBS;
+  const int row_max   = (m - row_begin) < ROWBS ? (m - row_begin) : ROWBS;
 
   const int num_col_blocks = (n + COLBS - 1) / COLBS;
   for (int ib = 0; ib < num_col_blocks; ib++)
@@ -63,35 +62,36 @@ __global__ void gemvT_batched_kernel(const int       m,
     if (tid < row_max)
       dot_sum += sum[tid][col_id];
 
-  T* y_iw = y[blockIdx.x];
+  T* __restrict__ y_iw = y[blockIdx.x];
   if (tid < row_max)
     y_iw[row_begin + tid] = alpha[blockIdx.x] * dot_sum + beta[blockIdx.x] * y_iw[row_begin + tid];
 }
 
 template<typename T>
 cuBLAS_inhouse_status gemv_batched_impl(cuBLAS_inhouse_handle& handle,
-                                 const char      trans,
-                                 const int       m,
-                                 const int       n,
-                                 const T*        alpha,
-                                 const T* const  A[],
-                                 const int       lda,
-                                 const T* const  x[],
-                                 const int       incx,
-                                 const T*        beta,
-                                 T* const        y[],
-                                 const int       incy,
-                                 const int       batch_count)
+                                        const char trans,
+                                        const int m,
+                                        const int n,
+                                        const T* alpha,
+                                        const T* const A[],
+                                        const int lda,
+                                        const T* const x[],
+                                        const int incx,
+                                        const T* beta,
+                                        T* const y[],
+                                        const int incy,
+                                        const int batch_count)
 {
-  if (batch_count == 0) return cudaSuccess;
+  if (batch_count == 0)
+    return cudaSuccess;
 
   if (trans == 'T')
   {
-    if (incx !=1 || incy != 1)
+    if (incx != 1 || incy != 1)
       throw std::runtime_error("incx !=1 or incy != 1 are not implemented in cuBLAS_inhouse::gemv_batched_impl!");
 
-    const int ROWBS = 32;
-    const int COLBS = 32;
+    const int ROWBS          = 32;
+    const int COLBS          = 32;
     const int num_row_blocks = (m + ROWBS - 1) / ROWBS;
     dim3 dimBlock(COLBS);
     dim3 dimGrid(batch_count, num_row_blocks);
@@ -104,71 +104,71 @@ cuBLAS_inhouse_status gemv_batched_impl(cuBLAS_inhouse_handle& handle,
   }
 }
 
-cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle&    handle,
-                            const char         trans,
-                            const int          m,
-                            const int          n,
-                            const float*       alpha,
-                            const float* const A[],
-                            const int          lda,
-                            const float* const x[],
-                            const int          incx,
-                            const float*       beta,
-                            float* const       y[],
-                            const int          incy,
-                            const int          batch_count)
+cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle& handle,
+                                   const char trans,
+                                   const int m,
+                                   const int n,
+                                   const float* alpha,
+                                   const float* const A[],
+                                   const int lda,
+                                   const float* const x[],
+                                   const int incx,
+                                   const float* beta,
+                                   float* const y[],
+                                   const int incy,
+                                   const int batch_count)
 {
   return gemv_batched_impl(handle, trans, m, n, alpha, A, lda, x, incx, beta, y, incy, batch_count);
 }
 
-cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle&     handle,
-                            const char          trans,
-                            const int           m,
-                            const int           n,
-                            const double*       alpha,
-                            const double* const A[],
-                            const int           lda,
-                            const double* const x[],
-                            const int           incx,
-                            const double*       beta,
-                            double* const       y[],
-                            const int           incy,
-                            const int           batch_count)
+cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle& handle,
+                                   const char trans,
+                                   const int m,
+                                   const int n,
+                                   const double* alpha,
+                                   const double* const A[],
+                                   const int lda,
+                                   const double* const x[],
+                                   const int incx,
+                                   const double* beta,
+                                   double* const y[],
+                                   const int incy,
+                                   const int batch_count)
 {
   return gemv_batched_impl(handle, trans, m, n, alpha, A, lda, x, incx, beta, y, incy, batch_count);
 }
 
-cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle&                  handle,
-                            const char                       trans,
-                            const int                        m,
-                            const int                        n,
-                            const std::complex<float>*       alpha,
-                            const std::complex<float>* const A[],
-                            const int                        lda,
-                            const std::complex<float>* const x[],
-                            const int                        incx,
-                            const std::complex<float>*       beta,
-                            std::complex<float>* const       y[],
-                            const int                        incy,
-                            const int                        batch_count)
+cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle& handle,
+                                   const char trans,
+                                   const int m,
+                                   const int n,
+                                   const std::complex<float>* alpha,
+                                   const std::complex<float>* const A[],
+                                   const int lda,
+                                   const std::complex<float>* const x[],
+                                   const int incx,
+                                   const std::complex<float>* beta,
+                                   std::complex<float>* const y[],
+                                   const int incy,
+                                   const int batch_count)
 {
   //return gemv_batched_impl(handle, trans, m, n, (const cuComplex*)alpha, (const cuComplex* const*)A, lda, (const cuComplex* const*)x, incx, (const cuComplex*)beta, (cuComplex* const*)y, incy, batch_count);
   return cudaSuccess;
 }
 
-cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle&                   handle,
-                            const char                        trans,
-                            const int                         m,
-                            const int                         n,
-                            const std::complex<double>*       alpha,
-                            const std::complex<double>* const A[],
-                            const int                         lda,
-                            const std::complex<double>* const x[],
-                            const int                         incx,
-                            const std::complex<double>*       beta,
-                            std::complex<double>* const       y[],
-                            const int                         incy,
-                            const int                         batch_count)
+cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle& handle,
+                                   const char trans,
+                                   const int m,
+                                   const int n,
+                                   const std::complex<double>* alpha,
+                                   const std::complex<double>* const A[],
+                                   const int lda,
+                                   const std::complex<double>* const x[],
+                                   const int incx,
+                                   const std::complex<double>* beta,
+                                   std::complex<double>* const y[],
+                                   const int incy,
+                                   const int batch_count)
 {
   //return gemv_batched_impl(handle, trans, m, n, (const cuDoubleComplex*)alpha, (const cuDoubleComplex* const*)A, lda, (const cuDoubleComplex* const*)x, incx, (const cuDoubleComplex*)beta, (cuDoubleComplex* const*)y, incy, batch_count);
   return cudaSuccess;
@@ -177,23 +177,24 @@ cuBLAS_inhouse_status gemv_batched(cuBLAS_inhouse_handle&                   hand
 
 template<typename T>
 cuBLAS_inhouse_status ger_batched_impl(cuBLAS_inhouse_handle& handle,
-                                const int       m,
-                                const int       n,
-                                const T*        alpha,
-                                const T* const  x[],
-                                const int       incx,
-                                const T* const  y[],
-                                const int       incy,
-                                T* const        A[],
-                                const int       lda,
-                                const int       batch_count)
+                                       const int m,
+                                       const int n,
+                                       const T* alpha,
+                                       const T* const x[],
+                                       const int incx,
+                                       const T* const y[],
+                                       const int incy,
+                                       T* const A[],
+                                       const int lda,
+                                       const int batch_count)
 {
-  if (batch_count == 0) return cudaSuccess;
+  if (batch_count == 0)
+    return cudaSuccess;
 
-  if (incx !=1 || incy != 1)
+  if (incx != 1 || incy != 1)
     throw std::runtime_error("incx !=1 or incy != 1 are not implemented in cuBLAS_inhouse::ger_batched_impl!");
 
-/*
+  /*
   PRAGMA_OFFLOAD("omp target teams distribute parallel for collapse(3) is_device_ptr(A, x, y, alpha)")
   for(size_t ib = 0; ib < batch_count; ib++)
     for(size_t i = 0; i < n; i++)
@@ -203,62 +204,62 @@ cuBLAS_inhouse_status ger_batched_impl(cuBLAS_inhouse_handle& handle,
   return cudaPeekAtLastError();
 }
 
-cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle&    handle,
-                           const int          m,
-                           const int          n,
-                           const float*       alpha,
-                           const float* const x[],
-                           const int          incx,
-                           const float* const y[],
-                           const int          incy,
-                           float* const       A[],
-                           const int          lda,
-                           const int          batch_count)
+cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle& handle,
+                                  const int m,
+                                  const int n,
+                                  const float* alpha,
+                                  const float* const x[],
+                                  const int incx,
+                                  const float* const y[],
+                                  const int incy,
+                                  float* const A[],
+                                  const int lda,
+                                  const int batch_count)
 {
   return ger_batched_impl(handle, m, n, alpha, x, incx, y, incy, A, lda, batch_count);
 }
 
-cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle&     handle,
-                           const int           m,
-                           const int           n,
-                           const double*       alpha,
-                           const double* const x[],
-                           const int           incx,
-                           const double* const y[],
-                           const int           incy,
-                           double* const       A[],
-                           const int           lda,
-                           const int           batch_count)
+cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle& handle,
+                                  const int m,
+                                  const int n,
+                                  const double* alpha,
+                                  const double* const x[],
+                                  const int incx,
+                                  const double* const y[],
+                                  const int incy,
+                                  double* const A[],
+                                  const int lda,
+                                  const int batch_count)
 {
   return ger_batched_impl(handle, m, n, alpha, x, incx, y, incy, A, lda, batch_count);
 }
 
-cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle&                  handle,
-                           const int                        m,
-                           const int                        n,
-                           const std::complex<float>*       alpha,
-                           const std::complex<float>* const x[],
-                           const int                        incx,
-                           const std::complex<float>* const y[],
-                           const int                        incy,
-                           std::complex<float>* const       A[],
-                           const int                        lda,
-                           const int                        batch_count)
+cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle& handle,
+                                  const int m,
+                                  const int n,
+                                  const std::complex<float>* alpha,
+                                  const std::complex<float>* const x[],
+                                  const int incx,
+                                  const std::complex<float>* const y[],
+                                  const int incy,
+                                  std::complex<float>* const A[],
+                                  const int lda,
+                                  const int batch_count)
 {
   return ger_batched_impl(handle, m, n, alpha, x, incx, y, incy, A, lda, batch_count);
 }
 
-cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle&                   handle,
-                           const int                         m,
-                           const int                         n,
-                           const std::complex<double>*       alpha,
-                           const std::complex<double>* const x[],
-                           const int                         incx,
-                           const std::complex<double>* const y[],
-                           const int                         incy,
-                           std::complex<double>* const       A[],
-                           const int                         lda,
-                           const int                         batch_count)
+cuBLAS_inhouse_status ger_batched(cuBLAS_inhouse_handle& handle,
+                                  const int m,
+                                  const int n,
+                                  const std::complex<double>* alpha,
+                                  const std::complex<double>* const x[],
+                                  const int incx,
+                                  const std::complex<double>* const y[],
+                                  const int incy,
+                                  std::complex<double>* const A[],
+                                  const int lda,
+                                  const int batch_count)
 {
   return ger_batched_impl(handle, m, n, alpha, x, incx, y, incy, A, lda, batch_count);
 }
