64933cdd8d354f020cd4388e3e367cedc6c94ab0
Fionn Malone <fionn.malone@gmail.com>
2020-05-06 11:23:07 -0700

    WIP.

--- src/AFQMC/Numerics/tests/test_dense_numerics.cpp
+++ src/AFQMC/Numerics/tests/test_dense_numerics.cpp
@@ -148,14 +148,15 @@ void test_dense_matrix_mult()
 	}
 
 	{
-	vector<double> m = {
-		1.,2.,1.,
-		2.,5.,8.,
-		1.,8.,9.
-	};
-	array_ref<double, 2> M(m.data(), {3,3});
-	REQUIRE( ma::is_hermitian(M) );
-	}{
+    vector<double> m = {
+      1.,2.,1.,
+      2.,5.,8.,
+      1.,8.,9.
+    };
+    array_ref<double, 2> M(m.data(), {3,3});
+    REQUIRE( ma::is_hermitian(M) );
+	}
+  {
 	vector<double> m = {
 		1.,0.  , 2.,0. ,  1.,0.,
 		2.,0.  , 5.,0. ,  8.,-1.,
@@ -163,78 +164,79 @@ void test_dense_matrix_mult()
 	};
 	array_ref<complex<double>, 2> M(reinterpret_cast<complex<double>*>(m.data()), {3,3});
 	REQUIRE( ma::is_hermitian(M) );
-	}{
-	vector<double> m = {
-		1.,2.,1.,
-		2.,5.,8.,
-		1.,8.,9.
-	};
-	array_ref<double, 2> M(m.data(), {3,3});
-	REQUIRE( ma::is_hermitian(M) );
+	}
+  {
+    vector<double> m = {
+      1.,2.,1.,
+      2.,5.,8.,
+      1.,8.,9.
+    };
+    array_ref<double, 2> M(m.data(), {3,3});
+    REQUIRE( ma::is_hermitian(M) );
 	}
 	{
-	vector<double> a = {
-		1.,0.,1.,
-		3.,5.,8.,
-		4.,8.,9.
-	};
-	array_ref<double, 2> A(a.data(), {3,3});
-	REQUIRE( A.num_elements() == a.size() );
-	vector<double> b = {
-		6.,2.,8.,
-		9.,5.,5.,
-		1.,7.,9.
-	};
-	array_ref<double, 2> B(b.data(), {3,3});
-	REQUIRE( B.num_elements() == b.size() );
+    vector<double> a = {
+      1.,0.,1.,
+      3.,5.,8.,
+      4.,8.,9.
+    };
+    array_ref<double, 2> A(a.data(), {3,3});
+    REQUIRE( A.num_elements() == a.size() );
+    vector<double> b = {
+      6.,2.,8.,
+      9.,5.,5.,
+      1.,7.,9.
+    };
+    array_ref<double, 2> B(b.data(), {3,3});
+    REQUIRE( B.num_elements() == b.size() );
 
-	vector<double> c(9);
-	array_ref<double, 2> C(c.data(), {3,3});
-	REQUIRE( C.num_elements() == c.size() );
+    vector<double> c(9);
+    array_ref<double, 2> C(c.data(), {3,3});
+    REQUIRE( C.num_elements() == c.size() );
 
-	ma::product(A, B, C);
+    ma::product(A, B, C);
 
-	vector<double> ab = {
-		7., 9., 17.,
-		71., 87., 121.,
-		105., 111., 153.
-	};
-	array_ref<double, 2> AB(ab.data(), {3,3});
-	REQUIRE( AB.num_elements() == ab.size() );
+    vector<double> ab = {
+      7., 9., 17.,
+      71., 87., 121.,
+      105., 111., 153.
+    };
+    array_ref<double, 2> AB(ab.data(), {3,3});
+    REQUIRE( AB.num_elements() == ab.size() );
 
-	verify_approx(C, AB);
+    verify_approx(C, AB);
 
-	using ma::N;
-	ma::product(N(A), N(B), C); // same as ma::product(A, B, C);
-	verify_approx(C, AB);
+    using ma::N;
+    ma::product(N(A), N(B), C); // same as ma::product(A, B, C);
+    verify_approx(C, AB);
 
-	using ma::T;
+    using ma::T;
 
-	ma::product(T(A), B, C);
-	vector<double> atb = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
-	array_ref<double, 2> AtB(atb.data(), {3,3});
-	verify_approx(C, AtB);
+    ma::product(T(A), B, C);
+    vector<double> atb = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
+    array_ref<double, 2> AtB(atb.data(), {3,3});
+    verify_approx(C, AtB);
 
-	ma::product(A, T(B), C);
-	vector<double> abt = {14., 14., 10., 92., 92., 110., 112., 121., 141.};
-	array_ref<double, 2> ABt(abt.data(), {3,3});
-	verify_approx(C, ABt);
+    ma::product(A, T(B), C);
+    vector<double> abt = {14., 14., 10., 92., 92., 110., 112., 121., 141.};
+    array_ref<double, 2> ABt(abt.data(), {3,3});
+    verify_approx(C, ABt);
 
-	ma::product(T(A), T(B), C);
-	vector<double> atbt = {44., 44., 58., 74., 65., 107., 94., 94., 138.};
-	array_ref<double, 2> AtBt(atbt.data(), {3,3});
-	verify_approx(C, AtBt);
+    ma::product(T(A), T(B), C);
+    vector<double> atbt = {44., 44., 58., 74., 65., 107., 94., 94., 138.};
+    array_ref<double, 2> AtBt(atbt.data(), {3,3});
+    verify_approx(C, AtBt);
 
-	using ma::H;
-        ma::product(H(A), T(B), C);
-        vector<double> ahbt = {44., 44., 58., 74., 65., 107., 94., 94., 138.};
-        array_ref<double, 2> AhBt(ahbt.data(), {3,3});
-        verify_approx(C, AhBt);
+    using ma::H;
+    ma::product(H(A), T(B), C);
+    vector<double> ahbt = {44., 44., 58., 74., 65., 107., 94., 94., 138.};
+    array_ref<double, 2> AhBt(ahbt.data(), {3,3});
+    verify_approx(C, AhBt);
 
-        ma::product(A, H(B), C);
-        vector<double> abh = {14., 14., 10., 92., 92., 110., 112., 121., 141.};
-        array_ref<double, 2> ABh(abh.data(), {3,3});
-        verify_approx(C, ABh);
+    ma::product(A, H(B), C);
+    vector<double> abh = {14., 14., 10., 92., 92., 110., 112., 121., 141.};
+    array_ref<double, 2> ABh(abh.data(), {3,3});
+    verify_approx(C, ABh);
 
 	}
 
@@ -251,139 +253,139 @@ void test_dense_matrix_mult()
 		array<double, 2> Id2({3,3});
 		ma::product(A, B, Id2);
 
-		verify_approx(Id, Id2);
-	}
-        {
-                std::vector<double> WORK;
-                array<double,1> TAU(iextensions<1u>{3});
+    verify_approx(Id, Id2);
+  }
+  {
+    std::vector<double> WORK;
+    array<double,1> TAU(iextensions<1u>{3});
 
-                vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
-                array_ref<double, 2> A(a.data(), {3,3});
-                REQUIRE(A.num_elements() == a.size());
-cout<<" lqf: " <<ma::gelqf_optimal_workspace_size(A) <<" " <<ma::glq_optimal_workspace_size(A) <<std::endl;
-                WORK.resize( std::max(ma::gelqf_optimal_workspace_size(A),
-                                      ma::glq_optimal_workspace_size(A)) );
-                ma::gelqf(A,TAU,WORK);
-                ma::glq(A,TAU,WORK);
+    vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
+    array_ref<double, 2> A(a.data(), {3,3});
+    REQUIRE(A.num_elements() == a.size());
+    cout<<" lqf: " <<ma::gelqf_optimal_workspace_size(A) <<" " <<ma::glq_optimal_workspace_size(A) <<std::endl;
+    WORK.resize( std::max(ma::gelqf_optimal_workspace_size(A),
+          ma::glq_optimal_workspace_size(A)) );
+    ma::gelqf(A,TAU,WORK);
+    ma::glq(A,TAU,WORK);
 
-                array<double, 2> Id({3,3});
-                ma::set_identity(Id);
+    array<double, 2> Id({3,3});
+    ma::set_identity(Id);
 
-                using ma::H;
-                array<double, 2> Id2({3,3});
-                ma::product(H(A), A, Id2);
+    using ma::H;
+    array<double, 2> Id2({3,3});
+    ma::product(H(A), A, Id2);
 
-                verify_approx(Id, Id2);
-        }
-        {
-                std::vector<double> WORK;
-                array<double,1> TAU(iextensions<1u>{4});
+    verify_approx(Id, Id2);
+  }
+  {
+    std::vector<double> WORK;
+    array<double,1> TAU(iextensions<1u>{4});
 
-                vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.,10.,23.,35.};
-                array_ref<double, 2> A(a.data(), {4,3});
-                REQUIRE(A.num_elements() == a.size());
-                WORK.resize( std::max(ma::gelqf_optimal_workspace_size(A),
-                                      ma::glq_optimal_workspace_size(A)) );
-                ma::gelqf(A,TAU,WORK);
-                ma::glq(A,TAU,WORK);
+    vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.,10.,23.,35.};
+    array_ref<double, 2> A(a.data(), {4,3});
+    REQUIRE(A.num_elements() == a.size());
+    WORK.resize( std::max(ma::gelqf_optimal_workspace_size(A),
+          ma::glq_optimal_workspace_size(A)) );
+    ma::gelqf(A,TAU,WORK);
+    ma::glq(A,TAU,WORK);
 
-                array<double, 2> Id({3,3});
-                ma::set_identity(Id);
+    array<double, 2> Id({3,3});
+    ma::set_identity(Id);
 
-                using ma::H;
-                array<double, 2> Id2({3,3});
-                ma::product(H(A), A, Id2);
+    using ma::H;
+    array<double, 2> Id2({3,3});
+    ma::product(H(A), A, Id2);
 
-                verify_approx(Id, Id2);
-        }
-        {
-                std::vector<double> WORK;
-                array<double,1> TAU(iextensions<1u>{3});
+    verify_approx(Id, Id2);
+  }
+  {
+    std::vector<double> WORK;
+    array<double,1> TAU(iextensions<1u>{3});
 
-                vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
-                array_ref<double, 2> A(a.data(), {3,3});
-                REQUIRE(A.num_elements() == a.size());
-                WORK.resize( std::max(ma::geqrf_optimal_workspace_size(A),
-                                      ma::gqr_optimal_workspace_size(A)) );
-                ma::geqrf(A,TAU,WORK);
-                ma::gqr(A,TAU,WORK);
+    vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.};
+    array_ref<double, 2> A(a.data(), {3,3});
+    REQUIRE(A.num_elements() == a.size());
+    WORK.resize( std::max(ma::geqrf_optimal_workspace_size(A),
+          ma::gqr_optimal_workspace_size(A)) );
+    ma::geqrf(A,TAU,WORK);
+    ma::gqr(A,TAU,WORK);
 
-                array<double, 2> Id({3,3});
-                ma::set_identity(Id);
+    array<double, 2> Id({3,3});
+    ma::set_identity(Id);
 
-                using ma::H;
-                array<double, 2> Id2({3,3});
-                ma::product(H(A), A, Id2);
+    using ma::H;
+    array<double, 2> Id2({3,3});
+    ma::product(H(A), A, Id2);
 
-                verify_approx(Id, Id2);
-        }
-        {
-                std::vector<double> WORK;
-                array<double,1> TAU(iextensions<1u>{4});
+    verify_approx(Id, Id2);
+  }
+  {
+    std::vector<double> WORK;
+    array<double,1> TAU(iextensions<1u>{4});
 
-                vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.,10.,23.,35.};
-                array_ref<double, 2> A(a.data(), {3,4});
-                REQUIRE(A.num_elements() == a.size());
-                WORK.resize( std::max(ma::geqrf_optimal_workspace_size(A),
-                                      ma::gqr_optimal_workspace_size(A)) );
-                ma::geqrf(A,TAU,WORK);
-                ma::gqr(A,TAU,WORK);
+    vector<double> a = {37., 45., 59., 53., 81., 97., 87., 105., 129.,10.,23.,35.};
+    array_ref<double, 2> A(a.data(), {3,4});
+    REQUIRE(A.num_elements() == a.size());
+    WORK.resize( std::max(ma::geqrf_optimal_workspace_size(A),
+          ma::gqr_optimal_workspace_size(A)) );
+    ma::geqrf(A,TAU,WORK);
+    ma::gqr(A,TAU,WORK);
 
-                array<double, 2> Id({3,3});
-                ma::set_identity(Id);
+    array<double, 2> Id({3,3});
+    ma::set_identity(Id);
 
-                using ma::H;
-                array<double, 2> Id2({3,3});
-                ma::product(A, H(A), Id2);
+    using ma::H;
+    array<double, 2> Id2({3,3});
+    ma::product(A, H(A), Id2);
 
-                verify_approx(Id, Id2);
-        }
-        {
-                vector<double> a = {
-                        9.,24.,30., 45.,
-                        4.,10.,12., 12.
-                };
-                array_ref<double, 2> A(a.data(), {2,4});
-                vector<double> at = {
-                        9.,4.,
-                        24.,10.,
-                        30.,12.,
-                        45.,12.
-                };
-                array_ref<double, 2> AT(at.data(), {4,2});
-                array<double, 2> B({4,2});
-                ma::transpose(A,B);
-                verify_approx( AT, B );
-        }
-        {
-                using namespace std::complex_literals;
-                vector<std::complex<double>> m_a = {
-                    1.90000,   1.40000 + 0.90000i,   0.40000 + 0.80000i,
-                    1.40000 - 0.90000i,   0.20000,   2.20000 + 0.60000i,
-                    0.40000 - 0.80000i,   2.20000 - 0.60000i,   0.60000
-                };
-                vector<std::complex<double>> m_b = {
-                    25.9622476651464 +  0.0000000000000i,   17.7794485121929 + 13.1574958765530i,
-                    11.2649352514491 + 16.4823940873968i,
-                    17.7794485121928 - 13.1574958765530i,   20.5657808536051 -  0.0000000000000i,
-                    17.9925255171787 +  6.0065935802308i,
-                    11.2649352514491 - 16.4823940873968i,   17.9925255171787 -  6.0065935802308i,
-                    17.9429273455619 -  0.0000000000000i
-                };
+    verify_approx(Id, Id2);
+  }
+  {
+    vector<double> a = {
+      9.,24.,30., 45.,
+      4.,10.,12., 12.
+    };
+    array_ref<double, 2> A(a.data(), {2,4});
+    vector<double> at = {
+      9.,4.,
+      24.,10.,
+      30.,12.,
+      45.,12.
+    };
+    array_ref<double, 2> AT(at.data(), {4,2});
+    array<double, 2> B({4,2});
+    ma::transpose(A,B);
+    verify_approx( AT, B );
+  }
+  {
+    using namespace std::complex_literals;
+    vector<std::complex<double>> m_a = {
+      1.90000,   1.40000 + 0.90000i,   0.40000 + 0.80000i,
+      1.40000 - 0.90000i,   0.20000,   2.20000 + 0.60000i,
+      0.40000 - 0.80000i,   2.20000 - 0.60000i,   0.60000
+    };
+    vector<std::complex<double>> m_b = {
+      25.9622476651464 +  0.0000000000000i,   17.7794485121929 + 13.1574958765530i,
+      11.2649352514491 + 16.4823940873968i,
+      17.7794485121928 - 13.1574958765530i,   20.5657808536051 -  0.0000000000000i,
+      17.9925255171787 +  6.0065935802308i,
+      11.2649352514491 - 16.4823940873968i,   17.9925255171787 -  6.0065935802308i,
+      17.9429273455619 -  0.0000000000000i
+    };
 
-                array<std::complex<double>,2> A({3,3});
-                array<std::complex<double>,2> B({3,3});
+    array<std::complex<double>,2> A({3,3});
+    array<std::complex<double>,2> B({3,3});
 
-                for(int i=0, k=0; i<A.size(0); i++)
-                    for(int j=0; j<A.size(1); j++,k++)
-                        A[i][j] = m_a[k];
-                for(int i=0, k=0; i<A.size(0); i++)
-                    for(int j=0; j<A.size(1); j++,k++)
-                        B[i][j] = m_b[k];
+    for(int i=0, k=0; i<A.size(0); i++)
+      for(int j=0; j<A.size(1); j++,k++)
+        A[i][j] = m_a[k];
+    for(int i=0, k=0; i<A.size(0); i++)
+      for(int j=0; j<A.size(1); j++,k++)
+        B[i][j] = m_b[k];
 
-                array<std::complex<double>,2> C = ma::exp(A);
-                verify_approx( C, B );
-        }
+    array<std::complex<double>,2> C = ma::exp(A);
+    verify_approx( C, B );
+  }
 }
 
 TEST_CASE("dense_ma_operations", "[matrix_operations]")
@@ -477,9 +479,9 @@ void test_dense_matrix_mult_device(Allocator alloc)
 
   {
     vector<T> m = {
- 	9.,24.,30., 9.,
-	4.,10.,12., 7.,
-	14.,16.,36., 1.
+    9.,24.,30., 9.,
+    4.,10.,12., 7.,
+    14.,16.,36., 1.
     };
     vector<T> x = {1.,2.,3., 4.};
     vector<T> y = {4.,5.,6.};
@@ -504,9 +506,9 @@ void test_dense_matrix_mult_device(Allocator alloc)
 
   {
     vector<T> m = {
-	1.,2.,1.,
-	2.,5.,8.,
-	1.,8.,9.
+    1.,2.,1.,
+    2.,5.,8.,
+    1.,8.,9.
     };
     array<T,2,Allocator> M({3,3},alloc);
     copy_n(m.data(),m.size(),M.origin()); 
@@ -516,9 +518,9 @@ void test_dense_matrix_mult_device(Allocator alloc)
   }
   {
     vector<T> m = {
-	1.,2.,1.,
-	2.,5.,8.,
-	1.,8.,9.
+    1.,2.,1.,
+    2.,5.,8.,
+    1.,8.,9.
     };
     array<T,2,Allocator> M({3,3},alloc);
     copy_n(m.data(),m.size(),M.origin());          
@@ -530,14 +532,14 @@ void test_dense_matrix_mult_device(Allocator alloc)
   }
   {
     vector<T> a = {
-	1.,0.,1.,
-	3.,5.,8.,
-	4.,8.,9.
+    1.,0.,1.,
+    3.,5.,8.,
+    4.,8.,9.
     };
     vector<T> b = {
-	6.,2.,8.,
-	9.,5.,5.,
-	1.,7.,9.
+    6.,2.,8.,
+    9.,5.,5.,
+    1.,7.,9.
     };
 
     array<T,2,Allocator> A({3,3},alloc);
@@ -553,9 +555,9 @@ void test_dense_matrix_mult_device(Allocator alloc)
     ma::product(A, B, D);
 
     vector<T> ab = {
-	7., 9., 17.,
-	71., 87., 121.,
-	105., 111., 153.
+    7., 9., 17.,
+    71., 87., 121.,
+    105., 111., 153.
     };
     array_ref<T, 2> AB(ab.data(), {3,3});
     verify_approx(D, AB);
