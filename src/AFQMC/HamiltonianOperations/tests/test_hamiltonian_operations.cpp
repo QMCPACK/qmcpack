052de9cc04a04efe3e98caca9302bd6298344a81
mmorale3 <miguel.mmorales@gmail.com>
2020-05-08 15:14:28 -0700

    changed exit(0) by throw in unnit tests, this should fix potential false positives

--- src/AFQMC/HamiltonianOperations/tests/test_hamiltonian_operations.cpp
+++ src/AFQMC/HamiltonianOperations/tests/test_hamiltonian_operations.cpp
@@ -21,7 +21,7 @@
 #include "io/hdf_multi.h"
 
 #undef APP_ABORT
-#define APP_ABORT(x) {std::cout << x <<std::endl; exit(0);}
+#define APP_ABORT(x) {std::cout << x <<std::endl; throw;}
 
 #include <string>
 #include <vector>
