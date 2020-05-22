ec17a638b2a4840f034eae66b8e6edbf73c9d91f
Fionn Malone <fionn.malone@gmail.com>
2020-05-22 12:05:00 -0700

    Add check for major version.

--- src/AFQMC/Memory/CUDA/cuda_init.cpp
+++ src/AFQMC/Memory/CUDA/cuda_init.cpp
@@ -55,6 +55,13 @@ namespace qmc_cuda {
     int num_devices=0;
     cudaGetDeviceCount(&num_devices);
     qmcplusplus::app_log()<<" Running in node with " <<num_devices <<" GPUs. \n";
+    cudaDeviceProp dev;
+    cuda_check(cudaGetDeviceProperties(&dev, 0),"cudaGetDeviceProperties");
+    if (dev.major <= 6) {
+      qmcplusplus::app_error()<<" Error CUDA major compute capability < 6." <<std::endl;
+      qmcplusplus::app_error()<<" Major: " << dev.major << " Minor: " << dev.minor <<std::endl;
+      APP_ABORT("");
+    }
     if(num_devices < node.size()) {
       qmcplusplus::app_error()<<"Error: # GPU < # tasks in node. " <<std::endl;
       qmcplusplus::app_error()<<"# GPU: " <<num_devices <<std::endl;
