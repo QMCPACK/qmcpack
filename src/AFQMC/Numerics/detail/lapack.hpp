0ca12ab8bd38d0b09e94dea4ad809c11b35647aa
Fionn Malone <fionn.malone@gmail.com>
2020-05-26 15:46:26 -0700

    HIP ifdefs.

--- src/AFQMC/Numerics/detail/lapack.hpp
+++ src/AFQMC/Numerics/detail/lapack.hpp
@@ -5,11 +5,11 @@
 // Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
 //
 // File developed by:
-//    Lawrence Livermore National Laboratory 
+//    Lawrence Livermore National Laboratory
 //
 // File created by:
-// Miguel A. Morales, moralessilva2@llnl.gov 
-//    Lawrence Livermore National Laboratory 
+// Miguel A. Morales, moralessilva2@llnl.gov
+//    Lawrence Livermore National Laboratory
 ////////////////////////////////////////////////////////////////////////////////
 
 #ifndef AFQMC_LAPACK_OPTIONS_HPP
@@ -20,5 +20,7 @@
 #if defined(ENABLE_CUDA)
 #include "AFQMC/Numerics/detail/CUDA/lapack_cuda_gpu_ptr.hpp"
 //#include "AFQMC/Numerics/detail/CUDA/lapack_cuda_catch_all.hpp"
+#elif
+#include "AFQMC/Numerics/detail/HIP/lapack_hip_gpu_ptr.hpp"
 #endif
 #endif
