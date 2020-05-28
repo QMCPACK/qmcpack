87963fb7602314b957de8554c8a0216727d8f20e
Paul Kent <kentpr@ornl.gov>
2020-05-28 11:01:36 -0400

    QMCPACK Developers in author and copyright fields

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'QMC Manual'
-copyright = '2020, John Batson'
-author = 'John Batson'
+copyright = '2020, QMCPACK Developers'
+author = 'QMCPACK Developers'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
