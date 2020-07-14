b03fb88efaf8aaec67e9b96a8ed8e974ce6afaea
Ye Luo <yeluo@anl.gov>
2020-07-13 22:41:34 -0500

    Protect the case when some ranks have no walkers.

--- src/QMCDrivers/QMCDriverNew.cpp
+++ src/QMCDrivers/QMCDriverNew.cpp
@@ -481,8 +481,13 @@ QMCDriverNew::AdjustedWalkerCounts QMCDriverNew::adjustGlobalWalkerCount(int num
   // Step 2. decide awc.global_walkers and awc.walkers_per_rank based on input values
   if (required_total != 0)
   {
-    awc.global_walkers   = required_total;
-    awc.walkers_per_rank = fairDivide(required_total, num_ranks);
+    if (required_total < num_ranks)
+    {
+      std::ostringstream error;
+      error << "Running on " << num_ranks << " MPI ranks and the request of " << required_total
+            << " global walkers cannot be satisfied! Need at least one walker per MPI rank.";
+      throw std::runtime_error(error.str());
+    }
     if (walkers_per_rank != 0 && required_total != walkers_per_rank * num_ranks)
     {
       std::ostringstream error;
@@ -490,6 +495,8 @@ QMCDriverNew::AdjustedWalkerCounts QMCDriverNew::adjustGlobalWalkerCount(int num
             << " global walkers and " << walkers_per_rank << " walkers per rank cannot be satisfied!";
       throw std::runtime_error(error.str());
     }
+    awc.global_walkers   = required_total;
+    awc.walkers_per_rank = fairDivide(required_total, num_ranks);
   }
   else
   {
