1c6fafde98c00300c6904d15a549a00be715c6f8
Peter Doak <doakpw@ornl.gov>
2020-09-02 14:38:35 -0400

    fixing bad merge

--- src/Utilities/RunTimeManager.cpp
+++ src/Utilities/RunTimeManager.cpp
@@ -25,9 +25,6 @@ template class RunTimeManager<FakeCPUClock>;
 
 RunTimeManager<CPUClock> run_time_manager;
 
-template class RunTimeManager<CPUClock>;
-template class RunTimeManager<FakeCPUClock>;
-
 template<class CLOCK>
 double LoopTimer<CLOCK>::get_time_per_iteration()
 {
