601ab57b7205304798f7aa7ead90abf982ac2e5f
Mark Dewing <markdewing@gmail.com>
2020-09-25 20:30:11 -0500

    Add comments for variables

--- src/QMCDrivers/WFOpt/QMCFixedSampleLinearOptimizeBatched.h
+++ src/QMCDrivers/WFOpt/QMCFixedSampleLinearOptimizeBatched.h
@@ -184,7 +184,9 @@ private:
   bool block_third;
 
 
+  /// Number of walkers in each crowd to use to process samples during optimization
   int crowd_size_;
+  /// Number of crowds to use to process samples during optimization
   int opt_num_crowds_;
   //Variables for alternatives to linear method
 
