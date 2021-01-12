fd9b5a772569e11decbc9b969e12d4f9798637aa
Jaron Krogel <krogeljt@ornl.gov>
2021-01-12 14:20:15 -0500

    nexus: add rmg simulation class

---
+++ nexus/lib/rmg_analyzer.py
@@ -0,0 +1,10 @@
+##################################################################
+##  (c) Copyright 2020-  by Jaron T. Krogel                     ##
+##################################################################
+
+from simulation import NullSimulationAnalyzer
+
+
+class RmgAnalyzer(NullSimulationAnalyzer):
+    None
+#end class RmgAnalyzer
