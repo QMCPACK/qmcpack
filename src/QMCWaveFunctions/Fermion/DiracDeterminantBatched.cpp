028742714dd92f09c9d10980c4296424ad8207fe
Ye Luo <yeluo@anl.gov>
2021-02-08 10:29:24 -0600

    Update comments

--- src/QMCWaveFunctions/Fermion/DiracDeterminantBatched.cpp
+++ src/QMCWaveFunctions/Fermion/DiracDeterminantBatched.cpp
@@ -249,6 +249,7 @@ void DiracDeterminantBatched<DET_ENGINE_TYPE>::mw_accept_rejectMove(const RefVec
   if (!Phi->isOMPoffload() && n_accepted > 0)
   {
     auto* phi_vgl_v_ptr = phi_vgl_v.data();
+    // transfer host to device, total size 5, v(1) + g(3) + l(1)
     PRAGMA_OFFLOAD("omp target update to(phi_vgl_v_ptr[:phi_vgl_v.capacity()*5])")
   }
 
@@ -358,7 +359,7 @@ void DiracDeterminantBatched<DET_ENGINE_TYPE>::mw_evaluateGL(const RefVector<Wav
         engine_list.push_back(det.det_engine_);
         auto& my_psiM_vgl  = det.psiM_vgl;
         auto* psiM_vgl_ptr = my_psiM_vgl.data();
-        // transfer device to host g(3)l(1)
+        // transfer device to host, total size 4, g(3) + l(1)
         PRAGMA_OFFLOAD("omp target update from(psiM_vgl_ptr[my_psiM_vgl.capacity():my_psiM_vgl.capacity()*4]) nowait")
       }
 
@@ -412,7 +413,7 @@ void DiracDeterminantBatched<DET_ENGINE_TYPE>::copyFromBuffer(ParticleSet& P, WF
   PRAGMA_OFFLOAD("omp target update to(psiMinv_ptr[:psiMinv.size()])")
   auto* psiM_vgl_ptr           = psiM_vgl.data();
   const size_t psiM_vgl_stride = psiM_vgl.capacity();
-  // transfer host to device g(3)l(1)
+  // transfer host to device, total size 4, g(3) + l(1)
   PRAGMA_OFFLOAD("omp target update to(psiM_vgl_ptr[psiM_vgl_stride:psiM_vgl_stride*4])")
   buf.get(LogValue);
   BufferTimer.stop();
@@ -757,7 +758,7 @@ void DiracDeterminantBatched<DET_ENGINE_TYPE>::recompute(ParticleSet& P)
   //if(!Phi->isOMPoffload())
   {
     auto* psiM_vgl_ptr = psiM_vgl.data();
-    // transfer host to device g(3)l(1)
+    // transfer host to device, total size 4, g(3) + l(1)
     PRAGMA_OFFLOAD("omp target update to(psiM_vgl_ptr[psiM_vgl.capacity():psiM_vgl.capacity()*4])")
   }
 
