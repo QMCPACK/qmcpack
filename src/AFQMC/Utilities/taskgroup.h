da5b1ac96ba39267c844500b5438adbe016c5298
Yubo "Paul" Yang <yubo.paul.yang@gmail.com>
2021-03-01 11:49:55 -0600

    remove extra erorrnous check on GPU
    correct check already present in TaskGroup_::setup (ndevices > 0)

--- src/AFQMC/Utilities/taskgroup.h
+++ src/AFQMC/Utilities/taskgroup.h
@@ -436,10 +436,6 @@ public:
     auto t = TGMap.find(nn);
     if (t == TGMap.end())
     {
-#if defined(ENABLE_CUDA) || defined(ENABLE_HIP)
-      if (gTG_.getTotalNodes() % nn != 0)
-        APP_ABORT("Error: nnodes must divide the total number of processors. \n\n\n");
-#endif
       auto p = TGMap.insert(
           std::make_pair(nn, afqmc::TaskGroup_(gTG_, std::string("TaskGroup_") + std::to_string(nn), nn, ncores)));
       if (!p.second)
