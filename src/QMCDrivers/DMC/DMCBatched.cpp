3c90cf4ee44e355acb5e6acdea12193490393e27
Ye Luo <yeluo@anl.gov>
2021-03-12 11:31:20 -0600

    Add step_begin_recompute_timer in DMCBatch.

--- src/QMCDrivers/DMC/DMCBatched.cpp
+++ src/QMCDrivers/DMC/DMCBatched.cpp
@@ -73,25 +73,28 @@ void DMCBatched::advanceWalkers(const StateForThread& sft,
     int nnode_crossing(0);
     auto& walkers = crowd.get_walkers();
     DriverWalkerResourceCollectionLock pbyp_lock(crowd.getSharedResource(), crowd.get_walker_elecs()[0],
-                                              crowd.get_walker_twfs()[0], crowd.get_walker_hamiltonians()[0]);
+                                                 crowd.get_walker_twfs()[0], crowd.get_walker_hamiltonians()[0]);
     const RefVectorWithLeader<ParticleSet> walker_elecs(crowd.get_walker_elecs()[0], crowd.get_walker_elecs());
     const RefVectorWithLeader<TrialWaveFunction> walker_twfs(crowd.get_walker_twfs()[0], crowd.get_walker_twfs());
     const RefVectorWithLeader<QMCHamiltonian> walker_hamiltonians(crowd.get_walker_hamiltonians()[0],
                                                                   crowd.get_walker_hamiltonians());
 
-    ps_dispatcher.flex_loadWalker(walker_elecs, walkers, true);
-    ps_dispatcher.flex_update(walker_elecs, true);
-    std::vector<bool> recompute_mask;
-    recompute_mask.reserve(walkers.size());
-    for (MCPWalker& awalker : walkers)
-      if (awalker.wasTouched)
-      {
-        recompute_mask.push_back(true);
-        awalker.wasTouched = false;
-      }
-      else
-        recompute_mask.push_back(false);
-    twf_dispatcher.flex_recompute(walker_twfs, walker_elecs, recompute_mask);
+    {
+      ScopedTimer recompute_timer(dmc_timers.step_begin_recompute_timer);
+      ps_dispatcher.flex_loadWalker(walker_elecs, walkers, true);
+      ps_dispatcher.flex_update(walker_elecs, true);
+      std::vector<bool> recompute_mask;
+      recompute_mask.reserve(walkers.size());
+      for (MCPWalker& awalker : walkers)
+        if (awalker.wasTouched)
+        {
+          recompute_mask.push_back(true);
+          awalker.wasTouched = false;
+        }
+        else
+          recompute_mask.push_back(false);
+      twf_dispatcher.flex_recompute(walker_twfs, walker_elecs, recompute_mask);
+    }
 
     const int num_walkers = crowd.size();
 
@@ -304,7 +307,7 @@ void DMCBatched::advanceWalkers(const StateForThread& sft,
     for (int iw = 0; iw < walkers.size(); ++iw)
     {
       DriverWalkerResourceCollectionLock tmove_lock(crowd.getSharedResource(), crowd.get_walker_elecs()[iw],
-                                                 crowd.get_walker_twfs()[iw], crowd.get_walker_hamiltonians()[iw]);
+                                                    crowd.get_walker_twfs()[iw], crowd.get_walker_hamiltonians()[iw]);
       walker_non_local_moves_accepted[iw] = walker_hamiltonians[iw].makeNonLocalMoves(walker_elecs[iw]);
 
       if (walker_non_local_moves_accepted[iw] > 0)
@@ -319,7 +322,7 @@ void DMCBatched::advanceWalkers(const StateForThread& sft,
     if (moved_nonlocal_walkers.size())
     {
       DriverWalkerResourceCollectionLock tmove_lock(crowd.getSharedResource(), crowd.get_walker_elecs()[0],
-                                                 crowd.get_walker_twfs()[0], crowd.get_walker_hamiltonians()[0]);
+                                                    crowd.get_walker_twfs()[0], crowd.get_walker_hamiltonians()[0]);
 
       twf_dispatcher.flex_evaluateGL(moved_nonlocal_walker_twfs, moved_nonlocal_walker_elecs, false);
       assert(QMCDriverNew::checkLogAndGL(crowd));
@@ -461,9 +464,8 @@ bool DMCBatched::run()
       }
 
       {
-        int iter = block * qmcdriver_input_.get_max_steps() + step;
-        const int population_now =
-            walker_controller_->branch(iter, population_, iter == 0);
+        int iter                 = block * qmcdriver_input_.get_max_steps() + step;
+        const int population_now = walker_controller_->branch(iter, population_, iter == 0);
         branch_engine_->updateParamAfterPopControl(population_now, walker_controller_->get_ensemble_property(),
                                                    population_.get_num_particles());
         walker_controller_->setTrialEnergy(branch_engine_->getEtrial());
