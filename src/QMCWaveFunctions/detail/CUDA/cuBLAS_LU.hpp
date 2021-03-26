b3f6fef360c7c31efea4cf6e1e8c850be6dc33ab
Peter Doak <doakpw@ornl.gov>
2021-03-26 11:49:04 -0400

    kernels to glue together the batched cuBLAS calls

---
+++ src/QMCWaveFunctions/detail/CUDA/cuBLAS_LU.hpp
@@ -0,0 +1,119 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2021 QMCPACK developers.
+//
+// File developed by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+//
+// File created by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef QMCPLUSPLUS_CUBLAS_LU_HPP
+#define QMCPLUSPLUS_CUBLAS_LU_HPP
+
+#include <complex>
+#include <type_traits>
+#include <cuda_runtime_api.h>
+#include <cuComplex.h>
+namespace qmcplusplus
+{
+namespace cuBLAS_LU
+{
+// template<typename T>
+// class CUDAComplex
+// {
+//   using type = std::complex<T>;
+// };
+
+// template<>
+// class CUDAComplex<double>
+// {
+//   using type = cuDoubleComplex;
+// };
+
+// template<>
+// class CUDAComplex<float>
+// {
+//   using type = cuComplex;
+// };
+
+template<class...> struct disjunction : std::false_type { };
+template<class B1> struct disjunction<B1> : B1 { };
+template<class B1, class... Bn>
+struct disjunction<B1, Bn...> 
+    : std::conditional_t<bool(B1::value), B1, disjunction<Bn...>>  { };
+  
+template <typename V1, typename V2, typename T>
+struct OnTypesEqual : std::integral_constant<bool, std::is_same<V1, V2>::value> {
+    using type = T;
+};
+
+template <typename T>
+struct default_type : std::true_type {
+    using type = T;
+};
+
+template<typename T>
+using TypesMapper = typename disjunction<
+  OnTypesEqual<T, std::complex<double>, cuDoubleComplex>,
+  OnTypesEqual<T, std::complex<float>, cuComplex>,
+  default_type<void>
+>::type;
+
+
+void computeInverseAndDetLog_batched(cublasHandle_t& h_cublas,
+                                     cudaStream_t& hstream,
+                                     const int n,
+                                     const int lda,
+                                     double** Ms,
+                                     double** Cs,
+                                     double* LU_diags,
+                                     int* pivots,
+                                     int* infos,
+                                     std::complex<double>* log_dets,
+                                     const int batch_size);
+
+void computeGetrf_batched(cublasHandle_t& h_cublas,
+                          const int n,
+                          const int lda,
+                          double* Ms[],
+                          int* pivots,
+                          int* infos,
+                          const int batch_size);
+
+void computeLogDet_batched(cudaStream_t& hstream,
+                           const int n,
+                           const double* LU_diags,
+                           const int* pivots,
+                           std::complex<double>* logdets,
+                           const int batch_size);
+
+void computeLUDiag_batched(cudaStream_t& hstream,
+                           const int n,
+                           const int lda,
+                           double* Ms[],
+                           double* LU_diags,
+                           const int batch_size);
+
+
+void computeGetri_batched(cublasHandle_t& h_cublas,
+                          const int n,
+                          const int lda,
+                          double* Ms[],
+                          double* Cs[],
+                          int* pivots,
+                          int* infos,
+                          const int batch_size);
+
+void peekinvM_batched(cudaStream_t& hstream,
+                      double* M[],
+                      double* invM[],
+                      int* pivots,
+                      int* infos,
+                      const int batch_size);
+
+
+} // namespace cuBLAS_LU
+} // namespace qmcplusplus
+#endif
