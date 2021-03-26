8232f5df0d5bb5e07262afec1c2c6ee7494aae28
Mark Dewing <markdewing@gmail.com>
2021-03-26 14:16:25 -0500

    Fix two body Jastrow opt. with inactive variables
    Remove inactive variables from myVars in order to construct correct
    mappings from sub-component variables to component variables, and
    from component variables to global variables (active).

--- src/QMCWaveFunctions/tests/test_DiffTwoBodyJastrowOrbital.cpp
+++ src/QMCWaveFunctions/tests/test_DiffTwoBodyJastrowOrbital.cpp
@@ -9,14 +9,12 @@
 // File created by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
 
-
 #include "catch.hpp"
 
 #include "QMCWaveFunctions/Jastrow/DiffTwoBodyJastrowOrbital.h"
 
 namespace qmcplusplus
 {
-
 // Mock radial function to use in DiffTwoBodyJastrowOrbital
 class FakeJastrow
 {
@@ -27,9 +25,15 @@ public:
 
   using RealType = QMCTraits::RealType;
 
-  bool evaluateDerivatives(RealType r, std::vector<TinyVector<RealType, 3>>& derivs) { return true; }
+  bool evaluateDerivatives(RealType r, std::vector<TinyVector<RealType, 3>>& derivs)
+  {
+    derivs = derivs_;
+    return true;
+  }
 
   void resetParameters(const opt_variables_type& active) {}
+
+  std::vector<TinyVector<RealType, 3>> derivs_;
 };
 
 TEST_CASE("DiffTwoBodyJastrowOrbital simple", "[wavefunction]")
@@ -69,10 +73,20 @@ ParticleSet get_two_species_particleset()
   std::vector<int> ud{2, 2};
   elec.setName("e");
   elec.create(ud);
+
+  elec.R[0] = {1.0, 0.0, 0.0};
+  elec.R[1] = {1.1, 1.0, 0.1};
+  elec.R[2] = {0.9, 0.8, 1.0};
+  elec.R[3] = {0.9, 0.5, 1.1};
+
   SpeciesSet& tspecies = elec.getSpeciesSet();
   int upIdx            = tspecies.addSpecies("u");
   int downIdx          = tspecies.addSpecies("d");
   elec.resetGroups();
+
+  elec.addTable(elec);
+  elec.update();
+
   return elec;
 }
 
@@ -108,7 +122,7 @@ TEST_CASE("DiffTwoBodyJastrowOrbital two variables", "[wavefunction]")
   //j2a.myVars.print(std::cout,0,true);
   //j2b.myVars.print(std::cout,0,true);
   //global_active.print(std::cout,0,true);
-  //jorb.getVars().print(std::cout,0,true);
+  //jorb.getComponentVars().print(std::cout,0,true);
 
   CHECK(global_active.size_of_active() == 2);
 
@@ -119,19 +133,19 @@ TEST_CASE("DiffTwoBodyJastrowOrbital two variables", "[wavefunction]")
   // F[3] is (1,1) b-b (by default uses the same function as a-a)
 
   // Index into global_active
-  auto o1 = jorb.getOffset(0);
+  auto o1 = jorb.getComponentOffset(0);
   CHECK(o1.first == 0);
   CHECK(o1.second == 1);
 
-  auto o2 = jorb.getOffset(1);
+  auto o2 = jorb.getComponentOffset(1);
   CHECK(o2.first == 1);
   CHECK(o2.second == 2);
 
-  auto o3 = jorb.getOffset(2);
+  auto o3 = jorb.getComponentOffset(2);
   CHECK(o3.first == 1);
   CHECK(o3.second == 2);
 
-  auto o4 = jorb.getOffset(3);
+  auto o4 = jorb.getComponentOffset(3);
   CHECK(o4.first == 0);
   CHECK(o4.second == 1);
 }
@@ -168,20 +182,109 @@ TEST_CASE("DiffTwoBodyJastrowOrbital variables fail", "[wavefunction]")
 
   CHECK(global_active.size_of_active() == 1);
   // Not optimizing the parameter in this Jastrow factor, indicated by first index is -1
-  auto o1 = jorb.getOffset(0);
+  auto o1 = jorb.getComponentOffset(0);
   CHECK(o1.first == -1);
 
   // Offset into set of active variables (global_active)
-  auto o2 = jorb.getOffset(1);
+  auto o2 = jorb.getComponentOffset(1);
   CHECK(o2.first == 0);
   CHECK(o2.second == 1);
 
-  auto o3 = jorb.getOffset(2);
+  auto o3 = jorb.getComponentOffset(2);
   CHECK(o3.first == 0);
   CHECK(o3.second == 1);
 
-  auto o4 = jorb.getOffset(3);
+  auto o4 = jorb.getComponentOffset(3);
   CHECK(o4.first == -1);
+
+  using ValueType = QMCTraits::ValueType;
+
+  // Check derivative indexing
+  int num_vars = 1;
+  j2b.derivs_.resize(num_vars);
+  // Elements are d/dp_i u(r), d/dp_i du/dr,  d/dp_i d2u/dr2
+  j2b.derivs_[0] = {0.5, 1.3, 2.4};
+  std::vector<ValueType> dlogpsi(num_vars);
+  jorb.evaluateDerivativesWF(elec, global_active, dlogpsi);
+
+  CHECK(dlogpsi[0] == ValueApprox(-2.0)); // 4 * derivs_[0][0]
+
+  std::vector<ValueType> dlogpsi2(num_vars);
+  std::vector<ValueType> dhpsioverpsi(num_vars);
+
+  jorb.evaluateDerivatives(elec, global_active, dlogpsi2, dhpsioverpsi);
+  CHECK(dlogpsi2[0] == ValueApprox(-2.0));
+}
+
+// Other variational parameters in the wavefunction (e.g. one-body Jastrow)
+
+TEST_CASE("DiffTwoBodyJastrowOrbital other variables", "[wavefunction]")
+{
+  ParticleSet elec = get_two_species_particleset();
+
+  DiffTwoBodyJastrowOrbital<FakeJastrow> jorb(elec);
+
+  FakeJastrow j2a;
+  j2a.myVars.insert("opt1", 1.0);
+  j2a.name = "j2a";
+  // update num_active_vars
+  j2a.myVars.resetIndex();
+  jorb.addFunc(0, 0, &j2a);
+
+  FakeJastrow j2b;
+  j2b.myVars.insert("opt2", 2.0);
+  j2b.name = "j2b";
+  // update num_active_vars
+  j2b.myVars.resetIndex();
+  jorb.addFunc(0, 1, &j2b);
+
+  opt_variables_type global_active;
+  // This is a parameter from another part of the wavefunction
+  global_active.insert("other_opt", 1.0);
+  global_active.insertFrom(j2b.myVars);
+  global_active.resetIndex();
+
+
+  jorb.checkOutVariables(global_active);
+
+  //global_active.print(std::cout,0,true);
+  //jorb.getComponentVars().print(std::cout,0,true);
+
+  CHECK(global_active.size_of_active() == 2);
+  // Not optimizing the parameter in this Jastrow factor, indicated by first index is -1
+  auto o1 = jorb.getComponentOffset(0);
+  CHECK(o1.first < 0);
+
+  // Offset into set of active variables (global_active)
+  auto o2 = jorb.getComponentOffset(1);
+  CHECK(o2.first == 0);
+  CHECK(o2.second == 1);
+
+  auto o3 = jorb.getComponentOffset(2);
+  CHECK(o3.first == 0);
+  CHECK(o3.second == 1);
+
+  auto o4 = jorb.getComponentOffset(3);
+  CHECK(o4.first < 0);
+
+
+  using ValueType = QMCTraits::ValueType;
+
+  // Check derivative indexing
+  int num_vars = 1;
+  j2b.derivs_.resize(num_vars);
+  // Elements are d/dp_i u(r), d/dp_i du/dr,  d/dp_i d2u/dr2
+  j2b.derivs_[0] = {0.5, 1.3, 2.4};
+  std::vector<ValueType> dlogpsi(num_vars);
+  jorb.evaluateDerivativesWF(elec, global_active, dlogpsi);
+
+  CHECK(dlogpsi[1] == ValueApprox(-2.0)); // 4 * derivs_[0][0]
+
+  std::vector<ValueType> dlogpsi2(num_vars);
+  std::vector<ValueType> dhpsioverpsi(num_vars);
+
+  jorb.evaluateDerivatives(elec, global_active, dlogpsi2, dhpsioverpsi);
+  CHECK(dlogpsi2[1] == ValueApprox(-2.0));
 }
 
 } // namespace qmcplusplus
