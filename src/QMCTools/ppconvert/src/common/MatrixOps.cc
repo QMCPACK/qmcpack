3cda70f8842068e9caafa99f4f105f0383e0cb6f
Alfredo Correa <alfredo.correa@gmail.com>
2021-03-28 19:35:55 -0700

    replaced NR LU code

--- src/QMCTools/ppconvert/src/common/MatrixOps.cc
+++ src/QMCTools/ppconvert/src/common/MatrixOps.cc
@@ -581,92 +581,92 @@ void SVdecomp (Array<std::complex<double>,2> &A, Array<std::complex<double>,2> &
 //}
 	       
 
-      // Adapted from Numerical Recipes in C
-void LUdecomp (Array<double,2> &A, Array<int,1> &perm, 
-	       double &sign)
-{
-  int i, imax = 0, j, k;
-  int n = A.rows();
-  double big, dum, sum, temp;
-  Array<double,1> vv(n);
-  sign = 1.0;
+//      // Adapted from Numerical Recipes in C
+//void LUdecomp (Array<double,2> &A, Array<int,1> &perm, 
+//	       double &sign)
+//{
+//  int i, imax = 0, j, k;
+//  int n = A.rows();
+//  double big, dum, sum, temp;
+//  Array<double,1> vv(n);
+//  sign = 1.0;
 
-  perm.resize(n);
+//  perm.resize(n);
 
-  for (i=0; i<n; i++) {
-    big = 0.0;
-    for (int j=0; j<n; j++)
-      if (fabs(A(i,j)) > big) big = fabs(A(i,j));
-    if (big == 0.0) {
-      std::cerr << "Singularity in LUdecomp.\n";
-      abort();
-    }
-    vv(i) = 1.0/big;
-  }
-  for (j=0; j<n; j++) {
-    for (i=0; i<j; i++) {
-      sum=A(i,j);
-      for(k=0; k<i; k++) 
-	sum -= A(i,k)*A(k,j);
-      A(i,j) = sum;
-    }
-    big=0.0;
-    for (i=j; i<n; i++) {
-      sum = A(i,j);
-      for (k=0; k<j; k++)
-	sum-=A(i,k)*A(k,j);
-      A(i,j) = sum;
-      if ((dum=vv(i)*fabs(sum)) >= big) {
-	big = dum;
-	imax = i;
-      }
-    }
-    if (j != imax) {
-      for (k=0; k<n; k++) {
-	dum=A(imax,k);
-	A(imax,k) = A(j,k);
-	A(j,k) = dum;
-      }
-      sign = -sign;
-      vv(imax) = vv(j);
-    }
-    perm(j)=imax;
-    if (A(j,j) == 0.0)
-      A(j,j) = 1.0e-200;
-    if (j != (n-1)) {
-      dum=1.0/A(j,j);
-      for (i=j+1; i<n; i++)
-	A(i,j) *= dum;
-    }
-  }  
-}
+//  for (i=0; i<n; i++) {
+//    big = 0.0;
+//    for (int j=0; j<n; j++)
+//      if (fabs(A(i,j)) > big) big = fabs(A(i,j));
+//    if (big == 0.0) {
+//      std::cerr << "Singularity in LUdecomp.\n";
+//      abort();
+//    }
+//    vv(i) = 1.0/big;
+//  }
+//  for (j=0; j<n; j++) {
+//    for (i=0; i<j; i++) {
+//      sum=A(i,j);
+//      for(k=0; k<i; k++) 
+//	sum -= A(i,k)*A(k,j);
+//      A(i,j) = sum;
+//    }
+//    big=0.0;
+//    for (i=j; i<n; i++) {
+//      sum = A(i,j);
+//      for (k=0; k<j; k++)
+//	sum-=A(i,k)*A(k,j);
+//      A(i,j) = sum;
+//      if ((dum=vv(i)*fabs(sum)) >= big) {
+//	big = dum;
+//	imax = i;
+//      }
+//    }
+//    if (j != imax) {
+//      for (k=0; k<n; k++) {
+//	dum=A(imax,k);
+//	A(imax,k) = A(j,k);
+//	A(j,k) = dum;
+//      }
+//      sign = -sign;
+//      vv(imax) = vv(j);
+//    }
+//    perm(j)=imax;
+//    if (A(j,j) == 0.0)
+//      A(j,j) = 1.0e-200;
+//    if (j != (n-1)) {
+//      dum=1.0/A(j,j);
+//      for (i=j+1; i<n; i++)
+//	A(i,j) *= dum;
+//    }
+//  }  
+//}
 
 
-void LUsolve (Array<double,2> &LU, Array<int,1> &perm,
-	      Array<double,1> &b)
-{
-  int i, ii=-1,ip,j;
-  double sum;
-  int n = LU.rows();
-  
-  for (i=0; i<n; i++) {
-    ip = perm(i);
-    sum = b(ip);
-    b(ip) = b(i);
-    if (ii>=0)
-      for (j=ii; j<i; j++)
-	sum -= LU(i,j)*b(j);
-    else if (sum) 
-      ii = i;
-    b(i) = sum;
-  }
-  for (i=n-1; i>=0; i--) {
-    sum = b(i);
-    for (j=i+1; j<n; j++)
-      sum -= LU(i,j)*b(j);
-    b(i) = sum/LU(i,i);
-  }
-}
+//void LUsolve (Array<double,2> &LU, Array<int,1> &perm,
+//	      Array<double,1> &b)
+//{
+//  int i, ii=-1,ip,j;
+//  double sum;
+//  int n = LU.rows();
+//  
+//  for (i=0; i<n; i++) {
+//    ip = perm(i);
+//    sum = b(ip);
+//    b(ip) = b(i);
+//    if (ii>=0)
+//      for (j=ii; j<i; j++)
+//	sum -= LU(i,j)*b(j);
+//    else if (sum) 
+//      ii = i;
+//    b(i) = sum;
+//  }
+//  for (i=n-1; i>=0; i--) {
+//    sum = b(i);
+//    for (j=i+1; j<n; j++)
+//      sum -= LU(i,j)*b(j);
+//    b(i) = sum/LU(i,i);
+//  }
+//}
 
 
 //Array<double,2> Inverse (Array<double,2> &A)
