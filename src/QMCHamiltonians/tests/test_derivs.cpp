634829eb86ca09d0b578f2e6ebc89f23967768e5
rcclay <j.k.rofling@gmail.com>
2021-05-05 12:45:09 -0600

    Update progress on test_derivs.cpp

--- src/QMCHamiltonians/tests/test_derivs.cpp
+++ src/QMCHamiltonians/tests/test_derivs.cpp
@@ -17,6 +17,7 @@
 #include "Particle/tests/MinimalParticlePool.h"
 #include "QMCWaveFunctions/tests/MinimalWaveFunctionPool.h"
 #include "QMCHamiltonians/tests/MinimalHamiltonianPool.h"
+#include "ParticleIO/XMLParticleIO.h"
 namespace qmcplusplus
 {
 
@@ -80,11 +81,81 @@ void create_CN_particlesets(ParticleSet& elec, ParticleSet& ions)
   elec.addTable(ions);
   elec.update();
 
+}
+
+void doopy(ParticleSet& elec, ParticleSet& ions)
+{
+  const char* particles = "<tmp> \
+  <particleset name=\"ion0\" size=\"2\"> \
+    <group name=\"C\">\
+      <parameter name=\"charge\">4</parameter> \
+      <parameter name=\"valence\">2</parameter> \
+      <parameter name=\"atomicnumber\">6</parameter> \
+    </group> \
+    <group name=\"N\"> \
+      <parameter name=\"charge\">5</parameter> \
+      <parameter name=\"valence\">3</parameter> \
+      <parameter name=\"atomicnumber\">7</parameter> \
+    </group> \
+    <attrib name=\"position\" datatype=\"posArray\"> \
+  0.0000000000e+00  0.0000000000e+00  0.0000000000e+00 \
+  0.0000000000e+00  0.0000000000e+00  2.0786985865e+00 \
+</attrib> \
+    <attrib name=\"ionid\" datatype=\"stringArray\"> \
+ C N \
+</attrib> \
+  </particleset> \
+  <particleset name=\"e\"> \
+    <group name=\"u\" size=\"5\"> \
+      <parameter name=\"charge\">-1</parameter> \
+    <attrib name=\"position\" datatype=\"posArray\"> \
+-0.55936725 -0.26942464 0.14459603 \
+0.19146719 1.40287983 0.63931251 \
+1.14805915 -0.52057335 3.49621107 \
+0.28293870 -0.10273952 0.01707021 \
+0.60626935 -0.25538121 1.75750740 \
+</attrib> \
+    </group> \
+    <group name=\"d\" size=\"4\"> \
+      <parameter name=\"charge\">-1</parameter> \
+    <attrib name=\"position\" datatype=\"posArray\"> \
+-0.47405939 0.59523171 -0.59778601 \
+0.03150661 -0.27343474 0.56279442 \
+-1.32648025 0.00970226 2.26944242 \
+2.42944286 0.64884151 1.87505288 \
+</attrib> \
+    </group> \
+  </particleset>\
+  </tmp>";
+
+  Libxml2Document doc;
+  bool okay = doc.parseFromString(particles);
+  REQUIRE(okay);
+
+  xmlNodePtr root  = doc.getRoot();
+  xmlNodePtr part1 = xmlFirstElementChild(root);
+  xmlNodePtr part2 = xmlNextElementSibling(part1);
+
+  Tensor<int, 3> tmat;
+  tmat(0, 0) = 1;
+  tmat(1, 1) = 1;
+  tmat(2, 2) = 1;
+
+  XMLParticleParser parse_ions(ions, tmat);
+  parse_ions.put(part1);
+
+  XMLParticleParser parse_electrons(elec, tmat);
+  parse_electrons.put(part2);
   
+  elec.addTable(elec);
+  elec.addTable(ions);
+  elec.update();
+
 }
 
 TEST_CASE("Eloc_Derivatives:slater_noj", "[hamiltonian]")
 {
+  using RealType  = QMCTraits::RealType;
   app_log()<<" WOOHOO Eloc_derivatives:slater_noj tested!";
   REQUIRE( true );
 
@@ -94,8 +165,8 @@ TEST_CASE("Eloc_Derivatives:slater_noj", "[hamiltonian]")
   ParticleSet ions;
   ParticleSet elec;
 
-  create_CN_particlesets(elec,ions); 
-
+//  create_CN_particlesets(elec,ions); 
+  doopy(elec,ions);
   HamiltonianFactory::PtclPoolType particle_set_map;
   HamiltonianFactory::PsiPoolType psi_map;
 
@@ -123,6 +194,51 @@ TEST_CASE("Eloc_Derivatives:slater_noj", "[hamiltonian]")
   xmlNodePtr root = doc.getRoot();
   hf.put(root);
 
+  Libxml2Document wfdoc;
+  bool wfokay = wfdoc.parse("cn.wfnoj.xml");
+  REQUIRE(wfokay);
+
+  xmlNodePtr wfroot = wfdoc.getRoot();
+  wff.put(wfroot);
+
+  TrialWaveFunction* psi = wff.getTWF();
+  REQUIRE(psi != nullptr);
+ 
+  //Output of WFTester Eloc test for this ion/electron configuration.
+  //Logpsi: (-1.4233853149e+01,0.0000000000e+00)
+  //HamTest   Total -2.3652298313e+01
+  //HamTest Kinetic 9.1821937928e+00
+  //HamTest ElecElec 1.9015560571e+01
+  //HamTest IonIon 9.6214045316e+00
+  //HamTest LocalECP -6.7839428299e+01
+  //HamTest NonLocalECP 6.3679710911e+00
+
+  //24: eloc = -23.6523123429
+  //24:   HamTest Kinetic 9.1821882589
+  //24:   HamTest ElecElec 19.0155605708
+  //24:   HamTest IonIon 9.6214045316
+  //24:   HamTest LocalECP -67.8394282995
+  //24:   HamTest NonLocalECP 6.3679625953
+
+  
+  RealType logpsi= psi->evaluateLog(elec); 
+  REQUIRE( logpsi == Approx(-14.233853149));
+  
+  QMCHamiltonian* ham = hf.getH();
+
+  RealType eloc = ham->evaluateDeterministic(elec);
+  enum observ_id { KINETIC=0, ELECELEC, IONION, LOCALECP, NONLOCALECP};
+  REQUIRE(eloc                            == Approx(-2.3652298313e+01));
+  REQUIRE(ham->getObservable(ELECELEC)    == Approx( 1.9015560571e+01));
+  REQUIRE(ham->getObservable(IONION)      == Approx( 9.6214045316e+00));
+  REQUIRE(ham->getObservable(LOCALECP)    == Approx(-6.7839428299e+01));
+  REQUIRE(ham->getObservable(KINETIC)     == Approx( 9.1821937928e+00));
+  REQUIRE(ham->getObservable(NONLOCALECP) == Approx( 6.3679710911e+00));
+  
+  for (int i = 0; i < ham->sizeOfObservables(); i++)
+    app_log() << "  HamTest " << ham->getObservableName(i) << " " << ham->getObservable(i) << std::endl;
+
+   
 }
 
 TEST_CASE("Eloc_Derivatives:slater_wj", "[hamiltonian]")
