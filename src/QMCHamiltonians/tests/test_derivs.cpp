bb0a9e9661a24409b2f2c455dd2f5f384f4e3a47
rcclay <j.k.rofling@gmail.com>
2021-05-07 13:23:45 -0600

    More updates (reference numbers) to test_derivs.cpp

--- src/QMCHamiltonians/tests/test_derivs.cpp
+++ src/QMCHamiltonians/tests/test_derivs.cpp
@@ -21,69 +21,8 @@
 namespace qmcplusplus
 {
 
-//This is a snapshot of a slightly compressed CN molecule.  r=1.10A.
-//Electron configurations taken from a DMC snapshot.  
+
 void create_CN_particlesets(ParticleSet& elec, ParticleSet& ions)
-{
-  using pos = ParticleSet::SingleParticlePos_t;
-  int Nions=2; //CN molecule.
-  int Nelec=9; //With ccECP's, 4e for C, 5e for N;
-  ions.setName("ion0");
-  ions.create(Nions);
-
-  ions.R[0]=pos(0.0,0.0,0.0);
-  ions.R[1]=pos(0.0,0.0,2.0786985865e+00);
- 
-  SpeciesSet& ion_species          = ions.getSpeciesSet();
-  int Cid                          = ion_species.addSpecies("C");
-  int pChargeIdx                   = ion_species.addAttribute("charge");
-  int pMembersizeIdx               = ion_species.addAttribute("membersize");
-  int pAtomicNumIdx               = ion_species.addAttribute("atomicnumber");
-  ion_species(pChargeIdx, Cid)     = 4;
-  ion_species(pMembersizeIdx, Cid) = 1;
-  ion_species(pAtomicNumIdx, Cid)  = 6;
-
-  int Nid                          = ion_species.addSpecies("N");
-  ion_species(pChargeIdx, Nid)     = 5;
-  ion_species(pMembersizeIdx, Nid) = 1;
-  ion_species(pAtomicNumIdx, Nid)  = 7;
-
-  elec.setName("elec");
-  elec.create(Nelec);
-
-//up elecs
-  elec.R[0]=pos(-0.55936725, -0.26942464,  0.14459603); 
-  elec.R[1]=pos( 0.19146719,  1.40287983,  0.63931251);
-  elec.R[2]=pos( 1.14805915, -0.52057335,  3.49621107);
-  elec.R[3]=pos( 0.28293870, -0.10273952,  0.01707021);
-  elec.R[4]=pos( 0.60626935, -0.25538121,  1.75750740);
-
-//dn elecs
-  elec.R[5]=pos(-0.47405939,  0.59523171, -0.59778601);
-  elec.R[6]=pos( 0.03150661, -0.27343474,  0.56279442);
-  elec.R[7]=pos(-1.32648025,  0.00970226,  2.26944242);
-  elec.R[8]=pos( 2.42944286,  0.64884151,  1.87505288);
-  
-  SpeciesSet& tspecies           = elec.getSpeciesSet();
-  int upIdx                      = tspecies.addSpecies("u");
-  int dnIdx                      = tspecies.addSpecies("d");
-  int chargeIdx                  = tspecies.addAttribute("charge");
-  int massIdx                    = tspecies.addAttribute("mass");
-  int MembersizeIdx              = tspecies.addAttribute("membersize");
-  tspecies(MembersizeIdx, upIdx) = 5;
-  tspecies(chargeIdx, upIdx)     = -1;
-  tspecies(massIdx, upIdx)       = 1.0;
-
-  tspecies(MembersizeIdx, dnIdx) = 4;
-  tspecies(chargeIdx, dnIdx)     = -1;
-  tspecies(massIdx, dnIdx)       = 1.0;
-
-  elec.addTable(ions);
-  elec.update();
-
-}
-
-void doopy(ParticleSet& elec, ParticleSet& ions)
 {
   const char* particles = "<tmp> \
   <particleset name=\"ion0\" size=\"2\"> \
@@ -165,8 +104,11 @@ TEST_CASE("Eloc_Derivatives:slater_noj", "[hamiltonian]")
   ParticleSet ions;
   ParticleSet elec;
 
-//  create_CN_particlesets(elec,ions); 
-  doopy(elec,ions);
+  create_CN_particlesets(elec,ions); 
+
+  int Nions = ions.getTotalNum();
+  int Nelec = elec.getTotalNum();
+
   HamiltonianFactory::PtclPoolType particle_set_map;
   HamiltonianFactory::PsiPoolType psi_map;
 
@@ -238,7 +180,56 @@ TEST_CASE("Eloc_Derivatives:slater_noj", "[hamiltonian]")
   for (int i = 0; i < ham->sizeOfObservables(); i++)
     app_log() << "  HamTest " << ham->getObservableName(i) << " " << ham->getObservable(i) << std::endl;
 
-   
+  //Now for the derivative tests 
+  ParticleSet::ParticleGradient_t wfgradraw;
+  ParticleSet::ParticlePos_t hf_term;
+  ParticleSet::ParticlePos_t pulay_term;
+  ParticleSet::ParticlePos_t wf_grad;
+ 
+  wfgradraw.resize(Nions);
+  wf_grad.resize(Nions);
+  hf_term.resize(Nions);
+  pulay_term.resize(Nions);
+
+  wfgradraw[0] = psi->evalGradSource(elec, ions, 0); //On the C atom.
+  wfgradraw[1] = psi->evalGradSource(elec, ions, 1); //On the N atom.
+
+  convert(wfgradraw[0], wf_grad[0]); 
+  convert(wfgradraw[1], wf_grad[1]); 
+
+  //Reference from finite differences on this configuration.
+  REQUIRE( wf_grad[0][0] == Approx(-1.9044650674260308));
+  REQUIRE( wf_grad[0][1] == Approx(2.1257764985627148)); 
+  REQUIRE( wf_grad[0][2] == Approx(7.0556319963444016)); 
+  REQUIRE( wf_grad[1][0] == Approx(1.4233346821157509)); 
+  REQUIRE( wf_grad[1][1] == Approx(-0.1446706081154048)); 
+  REQUIRE( wf_grad[1][2] == Approx(0.1440176276013005)); 
+
+  //Kinetic Force
+  hf_term=0.0;
+  pulay_term=0.0;
+  (ham->getHamiltonian(KINETIC))->evaluateWithIonDerivs(elec, ions, *psi, hf_term, pulay_term);
+  REQUIRE( hf_term[0][0]+pulay_term[0][0] == Approx(1.0852823603357820));
+  REQUIRE( hf_term[0][1]+pulay_term[0][1] == Approx(24.2154119471038562));
+  REQUIRE( hf_term[0][2]+pulay_term[0][2] == Approx(111.8849364775797852));
+  REQUIRE( hf_term[1][0]+pulay_term[1][0] == Approx(2.1572063443997536));
+  REQUIRE( hf_term[1][1]+pulay_term[1][1] == Approx(-3.3743242489947529));
+  REQUIRE( hf_term[1][2]+pulay_term[1][2] == Approx(7.5625192454964454));
+
+  psi->recompute(elec);
+  //NLPP Force
+  hf_term=0.0;
+  pulay_term=0.0;
+  double val=(ham->getHamiltonian(NONLOCALECP))->evaluateWithIonDerivsDeterministic(elec, ions, *psi, hf_term, pulay_term);
+  app_log()<<"val = "<<val<<std::endl;
+  app_log()<<"hf_term[0][0] = "<<hf_term[0][0]<<std::endl;
+  app_log()<<"pulay_term[0][0] = "<<pulay_term[0][0]<<std::endl;
+  REQUIRE( hf_term[0][0]+pulay_term[0][0] == Approx(32.8158275110457254));
+  REQUIRE( hf_term[0][1]+pulay_term[0][1] == Approx(-25.9792419671001049));
+  REQUIRE( hf_term[0][2]+pulay_term[0][2] == Approx(-51.8992198405232514));
+  REQUIRE( hf_term[1][0]+pulay_term[1][0] == Approx(-0.0081394725626183));
+  REQUIRE( hf_term[1][1]+pulay_term[1][1] == Approx(1.2062112833888250));
+  REQUIRE( hf_term[1][2]+pulay_term[1][2] == Approx(-0.3732875800910306));
 }
 
 TEST_CASE("Eloc_Derivatives:slater_wj", "[hamiltonian]")
