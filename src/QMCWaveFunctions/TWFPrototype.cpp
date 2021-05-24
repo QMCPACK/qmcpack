425793ddc02aaa7f38e758ba8f67cd9b01c0496b
rcclay <j.k.rofling@gmail.com>
2021-05-24 16:17:14 -0600

    Add initial class file for prototype WF

---
+++ src/QMCWaveFunctions/TWFPrototype.cpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2021 QMCPACK developers.
+//
+// File developed by:   Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
+//
+// File created by:   Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include "QMCWaveFunctions/TWFPrototype.h"
+#include <iostream>
+namespace qmcplusplus
+{
+TWFPrototype::TWFPrototype()
+{
+  std::cout<<"TWFPrototype initialized\n"; 
+}
+}
---
+++ src/QMCWaveFunctions/TWFPrototype.h
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2021 QMCPACK developers.
+//
+// File developed by:   Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
+//
+// File created by:   Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+/**@file TWFPrototype.h
+ *@brief Declaration of TWFPrototype
+ */
+#ifndef QMCPLUSPLUS_TWFPROTOTYPE_H
+#define QMCPLUSPLUS_TWFPROTOTYPE_H
+
+namespace qmcplusplus
+{
+
+class TWFPrototype
+{
+  public:
+    TWFPrototype();
+  private:
+  
+  
+
+};
+
+/**@}*/
+} // namespace qmcplusplus
+#endif
