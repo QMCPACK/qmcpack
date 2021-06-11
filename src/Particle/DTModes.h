110615a56ebd61c8fecb787eec0b87600636ba27
Ye Luo <yeluo@anl.gov>
2021-06-11 17:25:03 -0500

    Add DTModes unit tests.

--- src/Particle/DTModes.h
+++ src/Particle/DTModes.h
@@ -25,12 +25,12 @@ enum class DTModes : uint_fast8_t
    * DT consumers should know if full table is needed or not and request via addTable.
    */
   NEED_FULL_TABLE_ANYTIME   = 0x1,
-  NO_NEED_TEMP_DATA_ON_HOST = 0x2
+  NEED_TEMP_DATA_ON_HOST = 0x2
 };
 
 constexpr bool operator&(DTModes x, DTModes y)
 {
-  return static_cast<uint_fast8_t>(x) & static_cast<uint_fast8_t>(y) != 0x0;
+  return (static_cast<uint_fast8_t>(x) & static_cast<uint_fast8_t>(y)) != 0x0;
 }
 
 constexpr DTModes operator|(DTModes x, DTModes y)
@@ -38,7 +38,7 @@ constexpr DTModes operator|(DTModes x, DTModes y)
   return static_cast<DTModes>(static_cast<uint_fast8_t>(x) | static_cast<uint_fast8_t>(y));
 }
 
-inline constexpr DTModes operator~(DTModes x) { return static_cast<DTModes>(~static_cast<uint_fast8_t>(x)); }
+constexpr DTModes operator~(DTModes x) { return static_cast<DTModes>(~static_cast<uint_fast8_t>(x)); }
 
 inline DTModes& operator|=(DTModes& x, DTModes y)
 {
