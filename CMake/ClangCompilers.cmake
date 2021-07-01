f02a4575ed9d4c45e68bb3fb15927733d889b1ad
Steven Hahn <hahnse@ornl.gov>
2021-07-01 17:09:32 -0400

    Only add Wsuggest-override to GCC 9.2
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- CMake/ClangCompilers.cmake
+++ CMake/ClangCompilers.cmake
@@ -54,9 +54,8 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=vla")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wvla")
 
 # set compiler warnings
-set(CMAKE_CXX_FLAGS
-	"${CMAKE_CXX_FLAGS} -Wall -Wno-unused-variable -Wno-overloaded-virtual -Wno-unused-private-field -Wno-unused-local-typedef -Wsuggest-override"
-)
+string(APPEND CMAKE_CXX_FLAGS " -Wall -Wno-unused-variable -Wno-overloaded-virtual -Wno-unused-private-field -Wno-unused-local-typedef -Wsuggest-override")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-pragmas")
 if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wmisleading-indentation")
