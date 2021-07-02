ae372a81ab1c7c30ae9fab9346acbbf3f2c690a2
Shiv Upadhyay <shivnupadhyay@gmail.com>
2021-07-02 09:36:24 -0400

    Update Copyright

--- src/QMCWaveFunctions/Jastrow/J1OrbitalSoA.h
+++ src/QMCWaveFunctions/Jastrow/J1OrbitalSoA.h
@@ -2,7 +2,7 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2021 QMCPACK developers.
 //
 // File developed by: Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
 //                  Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
