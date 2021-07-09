5ecfb06c80fa0c1d80ab6885f65233312a319698
Peter Doak <doakpw@ornl.gov>
2021-07-09 11:01:16 -0400

    dualallocator testing much more generic now.

--- src/Containers/OhmmsSoA/VectorSoaContainer.h
+++ src/Containers/OhmmsSoA/VectorSoaContainer.h
@@ -300,6 +300,16 @@ struct VectorSoaContainer
     qmc_allocator_traits<Alloc>::deviceSideCopyN(mAllocator, offset_to, nsize, offset_from);
   }
 
+  // For tesing copy on device side from one data index to another
+  template<typename Allocator = Alloc, typename = IsDualSpace<Allocator>>
+  void copyDeviceDataByIndex(unsigned to, unsigned from) {
+    auto* host_ptr = this->data();
+    auto offset_from         = this->data(from) - host_ptr;
+    auto offset_to           = this->data(to) - host_ptr;
+    auto nsize                = this->size();
+    qmc_allocator_traits<Alloc>::deviceSideCopyN(mAllocator, offset_to, nsize, offset_from);
+  }
+
 private:
   /// number of elements
   size_t nLocal;
