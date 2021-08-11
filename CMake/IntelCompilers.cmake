a05b27b28513e0ac3034a36e3136012cd811d68d
Ye Luo <yeluo@anl.gov>
2021-08-10 22:16:03 -0500

    Remove all __forceinline

--- CMake/IntelCompilers.cmake
+++ CMake/IntelCompilers.cmake
@@ -40,7 +40,7 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM" OR INTEL_ONEAPI_COMPILER_FOUND)
   add_compile_definitions(restrict=__restrict__)
 
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fstrict-aliasing")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstrict-aliasing -D__forceinline=inline")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstrict-aliasing")
 
   # Force frame-pointer kept in DEBUG build.
   set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fno-omit-frame-pointer")
