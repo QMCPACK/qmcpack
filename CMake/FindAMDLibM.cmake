b5cccff4b2788c93fc698749f3414969910fb66a
Ye Luo <yeluo@anl.gov>
2021-08-13 11:55:21 -0500

    Separate FindMKLVM from FindMKL

--- CMake/FindAMDLibM.cmake
+++ CMake/FindAMDLibM.cmake
@@ -1,4 +1,6 @@
-message(STATUS "Looking for AMD LibM libraries")
+message(STATUS "Setting up AMD LibM libraries")
+
+include(CheckCXXSourceCompiles)
 
 find_path(
   AMD_LIBM_INCLUDE_DIR NAME amdlibm.h
@@ -7,18 +9,42 @@ find_path(
 
 if(NOT AMD_LIBM_INCLUDE_DIR)
   message(FATAL_ERROR "AMD_LIBM_INCLUDE_DIR not set. Header file amdlib.h not found!")
+else()
+  message(STATUS "Header file amdlib.h found at  ${AMD_LIBM_INCLUDE_DIR}")
 endif()
-message(STATUS "Header file amdlib.h found at  ${AMD_LIBM_INCLUDE_DIR}")
 
 find_library(
   AMD_LIBM_LIBRARY NAME amdlibm
   HINTS ${AMD_LIBM_ROOT} ${AOCL_ROOT}
   PATH_SUFFIXES lib)
 
-target_link_libraries(Math::scalar_vector_functions INTERFACE "${AMD_LIBM_LIBRARY}")
+if(NOT AMD_LIBM_LIBRARY)
+  message(FATAL_ERROR "AMD_LIBM_LIBRARY not set. Library file amdlibm not found!")
+else()
+  message(STATUS "Library file amdlibm found as ${AMD_LIBM_LIBRARY}")
+endif()
 
-#if(HAVE_AMD_LIBM)
-    target_include_directories(Math::scalar_vector_functions INTERFACE "${AMD_LIBM_INCLUDE_DIR}")
-    target_compile_definitions(Math::scalar_vector_functions INTERFACE "HAVE_AMD_LIBM")
-    set(SINCOS_INCLUDE amdlibm.h)
-#endif()
+set(CMAKE_REQUIRED_INCLUDES "${AMD_LIBM_INCLUDE_DIR}")
+set(CMAKE_REQUIRED_LIBRARIES "${AMD_LIBM_LIBRARY}")
+check_cxx_source_compiles("
+#include <amdlibm.h>
+int main() {
+  double d_in(0), d_sin, d_cos;
+  amd_sincos(d_in, &d_sin, &d_cos);
+  float s_in(0), s_sin, s_cos;
+  amd_sincosf(s_in, &s_sin, &s_cos);
+  return 0;
+}
+"
+  HAVE_AMD_LIBM)
+unset(CMAKE_REQUIRED_INCLUDES)
+unset(CMAKE_REQUIRED_LIBRARIES)
+
+if(HAVE_AMD_LIBM)
+  target_link_libraries(Math::scalar_vector_functions INTERFACE "${AMD_LIBM_LIBRARY}")
+  target_include_directories(Math::scalar_vector_functions INTERFACE "${AMD_LIBM_INCLUDE_DIR}")
+  target_compile_definitions(Math::scalar_vector_functions INTERFACE "HAVE_AMD_LIBM")
+  message(STATUS "AMD LIBM found")
+else
+  message(FATAL_ERROR "AMD LIBM not found")
+endif()
--- CMake/FindMKL.cmake
+++ CMake/FindMKL.cmake
@@ -6,20 +6,20 @@ set(MKL_LIBRARIES ${LAPACK_LINKER_FLAGS} ${LAPACK_LIBRARIES})
 
 message(STATUS "Looking for Intel MKL library header files")
 
-# Finding and setting the MKL_INCLUDE_DIRECTORIES based on MKL_ROOT, $ENV{MKLROOT}, $ENV{MKL_ROOT}, $ENV{MKL_HOME}
+# Finding and setting the MKL_INCLUDE based on MKL_ROOT, $ENV{MKLROOT}, $ENV{MKL_ROOT}, $ENV{MKL_HOME}
 # Extremely Basic Support of common mkl module environment variables
 find_path(
-  MKL_INCLUDE_DIRECTORIES "mkl.h"
+  MKL_INCLUDE "mkl.h"
   HINTS ${MKL_ROOT} $ENV{MKLROOT} $ENV{MKL_ROOT} $ENV{MKL_HOME}
   PATH_SUFFIXES include)
-if(NOT MKL_INCLUDE_DIRECTORIES)
+if(NOT MKL_INCLUDE)
   # Finding MKL headers in the system
-  find_path(MKL_INCLUDE_DIRECTORIES "mkl.h" PATH_SUFFIXES mkl)
+  find_path(MKL_INCLUDE "mkl.h" PATH_SUFFIXES mkl)
 endif()
 
-if(MKL_INCLUDE_DIRECTORIES)
-  message(STATUS "MKL_INCLUDE_DIRECTORIES: ${MKL_INCLUDE_DIRECTORIES}")
-else(MKL_INCLUDE_DIRECTORIES)
+if(MKL_INCLUDE)
+  message(STATUS "MKL_INCLUDE: ${MKL_INCLUDE}")
+else(MKL_INCLUDE)
   message(STATUS "mkl.h cannot be found")
   if(COMPILER MATCHES "Intel")
     message(
@@ -30,32 +30,24 @@ else(MKL_INCLUDE_DIRECTORIES)
   else(COMPILER MATCHES "Intel")
     message(FATAL_ERROR "Pass mkl root directory to cmake via MKL_ROOT.")
   endif(COMPILER MATCHES "Intel")
-endif(MKL_INCLUDE_DIRECTORIES)
+endif(MKL_INCLUDE)
 
 # Check for mkl.h
 file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx"
      "#include <iostream>\n #include <mkl.h>\n int main() { return 0; }\n")
 try_compile(
   HAVE_MKL ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx
-  CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
+  CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE} "
   LINK_LIBRARIES "${MKL_LIBRARIES}"
   OUTPUT_VARIABLE MKL_OUT)
 if(NOT HAVE_MKL)
   message("${MKL_OUT}")
 endif(NOT HAVE_MKL)
 
-# Check for mkl_vml_functions.h
-file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx"
-     "#include <iostream>\n #include <mkl_vml_functions.h>\n int main() { return 0; }\n")
-try_compile(
-  HAVE_MKL_VML ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx
-  CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
-  OUTPUT_VARIABLE MKL_OUT)
-
 # Check for fftw3
 find_path(
   MKL_FFTW3 "fftw3.h"
-  HINTS ${MKL_INCLUDE_DIRECTORIES}
+  HINTS ${MKL_INCLUDE}
   PATH_SUFFIXES fftw)
 if(MKL_FFTW3)
   file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_fftw3.cxx"
@@ -71,17 +63,11 @@ endif(MKL_FFTW3)
 
 if(HAVE_MKL)
   set(MKL_FOUND TRUE)
-  message(STATUS "MKL found: HAVE_MKL=${HAVE_MKL}, HAVE_MKL_VML=${HAVE_MKL_VML}, HAVE_MKL_FFTW3=${HAVE_MKL_FFTW3}")
+  message(STATUS "MKL found: HAVE_MKL=${HAVE_MKL}, HAVE_MKL_FFTW3=${HAVE_MKL_FFTW3}")
 
   #Add BLAS_LAPACK header
   target_compile_definitions(Math::BLAS_LAPACK INTERFACE "HAVE_MKL")
-  target_include_directories(Math::BLAS_LAPACK INTERFACE "${MKL_INCLUDE_DIRECTORIES}")
-
-  if(HAVE_MKL_VML)
-    target_compile_definitions(Math::scalar_vector_functions INTERFACE "HAVE_MKL;HAVE_MKL_VML")
-    target_include_directories(Math::scalar_vector_functions INTERFACE "${MKL_INCLUDE_DIRECTORIES}")
-    target_link_libraries(Math::scalar_vector_functions INTERFACE "${MKL_LIBRARIES}")
-  endif(HAVE_MKL_VML)
+  target_include_directories(Math::BLAS_LAPACK INTERFACE "${MKL_INCLUDE}")
 
   if(HAVE_MKL_FFTW3)
     target_compile_definitions(Math::FFTW3 INTERFACE "HAVE_MKL;HAVE_LIBFFTW")
