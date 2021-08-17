5e50e22b66dc3cf60ce9ed5dee34247003631474
William F Godoy <williamfgc@yahoo.com>
2021-08-17 13:37:39 -0400

    Fix uninitialized errors with gcc-11

--- src/Numerics/OneDimGridBase.h
+++ src/Numerics/OneDimGridBase.h
@@ -38,13 +38,13 @@ struct OneDimGridBase
   typedef CT Array_t;
 
 
-  int GridTag;
-  int num_points;
-  value_type lower_bound;
-  value_type upper_bound;
+  int GridTag            = 0;
+  int num_points         = 0;
+  value_type lower_bound = T();
+  value_type upper_bound = T();
   ///differential spacing of the grid
-  value_type Delta;
-  double DeltaInv;
+  value_type Delta = T();
+  double DeltaInv  = 0.;
 
   ///array to store the radial grid data
   Array_t X;
