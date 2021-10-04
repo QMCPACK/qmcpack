fadc82ed1e259ffdd32459ed38e8a625b5b80a72
Ye Luo <yeluo@anl.gov>
2021-10-04 02:09:37 -0500

    Simplify unit tests.

--- src/QMCHamiltonians/tests/test_NonLocalTOperator.cpp
+++ src/QMCHamiltonians/tests/test_NonLocalTOperator.cpp
@@ -29,36 +29,29 @@ TEST_CASE("NonLocalTOperator", "[hamiltonian]")
   Txy.emplace_back(2, -0.3, PosType(0.3, 0.1, 0.2));
   Txy.emplace_back(3, 0.0, PosType(0.3, 0.2, 0.1));
 
-  auto Txy0 = Txy;
-  auto select0 = t_op.selectMove(0.0, Txy0);
+  auto select0 = t_op.selectMove(0.0, Txy);
   CHECK(select0 == nullptr);
 
-  auto Txy1 = Txy;
-  auto select1 = t_op.selectMove(0.4, Txy1);
+  auto select1 = t_op.selectMove(0.4, Txy);
   CHECK(select1 == nullptr);
 
-  auto Txy2 = Txy;
-  auto select2 = t_op.selectMove(0.5, Txy2);
+  auto select2 = t_op.selectMove(0.5, Txy);
   REQUIRE(select2 != nullptr);
   CHECK(select2->PID == 1);
 
-  auto Txy3 = Txy;
-  auto select3 = t_op.selectMove(0.6, Txy3);
+  auto select3 = t_op.selectMove(0.6, Txy);
   REQUIRE(select3 != nullptr);
   CHECK(select3->PID == 1);
 
-  auto Txy4 = Txy;
-  auto select4 = t_op.selectMove(0.85, Txy4);
+  auto select4 = t_op.selectMove(0.85, Txy);
   REQUIRE(select4 != nullptr);
   CHECK(select4->PID == 2);
 
-  auto Txy5 = Txy;
-  auto select5 = t_op.selectMove(0.9, Txy5);
+  auto select5 = t_op.selectMove(0.9, Txy);
   REQUIRE(select5 != nullptr);
   CHECK(select5->PID == 2);
 
-  auto Txy6 = Txy;
-  auto select6 = t_op.selectMove(float(1) - std::numeric_limits<float>::epsilon(), Txy6);
+  auto select6 = t_op.selectMove(float(1) - std::numeric_limits<float>::epsilon(), Txy);
   REQUIRE(select6 != nullptr);
   CHECK(select6->PID == 2);
 }
