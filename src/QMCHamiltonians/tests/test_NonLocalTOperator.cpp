748308217eb972b021c7360019e186d26d25b109
Ye Luo <yeluo@anl.gov>
2021-10-04 02:22:10 -0500

    Expand tests.

--- src/QMCHamiltonians/tests/test_NonLocalTOperator.cpp
+++ src/QMCHamiltonians/tests/test_NonLocalTOperator.cpp
@@ -25,8 +25,10 @@ TEST_CASE("NonLocalTOperator", "[hamiltonian]")
 
   std::vector<NonLocalData> Txy;
   Txy.emplace_back(0, 0.4, PosType(0.1, 0.2, 0.3));
-  Txy.emplace_back(1, -0.7, PosType(0.2, 0.3, 0.1));
-  Txy.emplace_back(2, -0.3, PosType(0.3, 0.1, 0.2));
+  Txy.emplace_back(1, -0.4, PosType(0.2, 0.3, 0.1));
+  Txy.emplace_back(1, -0.3, PosType(0.2, 0.1, 0.3));
+  Txy.emplace_back(2, -0.2, PosType(0.3, 0.1, 0.2));
+  Txy.emplace_back(2, -0.1, PosType(0.3, 0.1, 0.2));
   Txy.emplace_back(3, 0.0, PosType(0.3, 0.2, 0.1));
 
   auto select0 = t_op.selectMove(0.0, Txy);
@@ -54,5 +56,18 @@ TEST_CASE("NonLocalTOperator", "[hamiltonian]")
   auto select6 = t_op.selectMove(float(1) - std::numeric_limits<float>::epsilon(), Txy);
   REQUIRE(select6 != nullptr);
   CHECK(select6->PID == 2);
+
+  t_op.group_by_elec(4, Txy);
+
+  auto select7 = t_op.selectMove(0.7, 1);
+  REQUIRE(select7 != nullptr);
+  CHECK(select7->Weight == Approx(-0.4));
+
+  auto select8 = t_op.selectMove(0.7, 2);
+  REQUIRE(select8 == nullptr);
+
+  auto select9 = t_op.selectMove(0.8, 2);
+  REQUIRE(select9 != nullptr);
+  CHECK(select9->Weight == Approx(-0.2));
 }
 }
