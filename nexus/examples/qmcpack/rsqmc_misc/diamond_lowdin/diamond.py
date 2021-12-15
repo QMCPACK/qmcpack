9c5552d2fafb219c18f1d85d61ae5d8eb32e4cf7
M. Chandler Bennett <mcbennet@ncsu.edu>
2021-12-15 09:13:25 -0500

    Adding workflow file

---
+++ nexus/examples/qmcpack/rsqmc_misc/diamond_lowdin/diamond.py
@@ -0,0 +1,136 @@
+#! /usr/bin/env python
+
+from nexus import settings
+from nexus import job
+from nexus import run_project
+from nexus import generate_physical_system
+from nexus import generate_pwscf
+from nexus import generate_pw2qmcpack
+from nexus import generate_qmcpack
+from nexus import vmc
+
+from structure import *
+
+settings(
+    pseudo_dir    = '../../pseudopotentials',
+    results       = '',
+    status_only   = 0,
+    generate_only = 0,
+    skip_submit   = 0,
+    sleep         = 3,
+    machine       = 'ws4'
+    )
+
+dia16 = generate_physical_system(
+    units  = 'A',
+    axes   = [[ 1.785,  1.785,  0.   ],
+              [ 0.   ,  1.785,  1.785],
+              [ 1.785,  0.   ,  1.785]],
+    elem   = ['C','C'],
+    pos    = [[ 0.    ,  0.    ,  0.    ],
+              [ 0.8925,  0.8925,  0.8925]],
+    tiling = (1,1,1),
+    kgrid  = (1,1,1),
+    kshift = (0,0,0),
+    C      = 4
+    )
+              
+kg = dia.structure.kgrid_from_kspacing(0.5) # Get SCF kmesh from k-spacing
+
+scf = generate_pwscf(
+    identifier   = 'scf',
+    path         = 'scf',
+    job          = job(nodes=1,app='pw.x',hours=1),
+    input_type   = 'generic',
+    calculation  = 'scf',
+    nspin        = 2,
+    input_dft    = 'lda',
+    ecutwfc      = 200,
+    conv_thr     = 1e-8,
+    nosym        = False,
+    wf_collect   = False,
+    system       = dia,
+    tot_magnetization = 0,
+    kgrid        = kg,
+    kshift       = (0,0,0),
+    pseudos      = ['C.BFD.upf'],
+    )
+
+nscf = generate_pwscf(
+    identifier   = 'nscf',
+    path         = 'nscf',
+    job          = job(nodes=1,app='pw.x',hours=1),
+    input_type   = 'generic',
+    calculation  = 'nscf',
+    input_dft    = 'lda',
+    ecutwfc      = 200,
+    nspin        = 2,
+    conv_thr     = 1e-8,
+    nosym        = True,
+    wf_collect   = True,
+    system       = dia,
+    nbnd         = 8,      #a sensible nbnd value can be given
+    verbosity    = 'high', #verbosity must be set to high
+    pseudos      = ['C.BFD.upf'],
+    dependencies = (scf,'charge_density'),
+    )
+
+#conv = generate_pw2qmcpack(
+#    identifier   = 'conv',
+#    path         = 'diamond/nscf',
+#    job          = job(cores=1,app='pw2qmcpack.x', hours = 1),
+#    write_psir   = False,
+#    dependencies = (nscf,'orbitals'),
+#    )
+
+#conv = generate_pw2qmcpack(
+#    identifier   = 'conv',
+#    path         = 'scf',
+#    job          = p2qjob,
+#    write_psir   = False,
+#    dependencies = (scf,'orbitals'),
+#    )
+
+## Define 1RDM Parameters
+#dm_estimator = dm1b(
+#        energy_matrix = False,
+#        integrator    = 'uniform_grid',
+#        points        = 6,
+#        scale         = 1.0,
+#        basis         = sposet(type='bspline',size=29,spindataset=0),
+#        evaluator     = 'matrix',
+#        center        = (0,0,0),
+#        check_overlap = False,
+#        )
+
+#pwf = generate_projwfc(
+#    identifier      = 'pwf',
+#    path            = basepath+'/ccecp_o/scf',
+#    lwrite_overlaps = True,
+#    lsym            = False,
+#    job             = pwfjob,
+#    dependencies    = (scf,'other')
+#    )
+
+#qmc = generate_qmcpack(
+#    identifier   = 'vmc_1rdm_noJ',
+#    path         = 'vmc_1rdm_noJ',
+#    job          = qmcjob_default,
+#    input_type   = 'basic',
+#    system       = dia16,
+#    pseudos      = ['C.BFD.xml'],
+#    jastrows     = [],
+#    calculations = [
+#        vmc(
+#            walkers     =   1,
+#            warmupsteps =  20,
+#            blocks      = 200,
+#            steps       =  10,
+#            substeps    =   2,
+#            timestep    =  .4
+#            )
+#        ],
+#    dependencies = (conv,'orbitals'),
+#    )
+
+run_project()
