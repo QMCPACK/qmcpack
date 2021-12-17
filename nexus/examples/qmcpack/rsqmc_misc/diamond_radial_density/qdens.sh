cecc6d9ec198bbf67dcc7886c75d018dd0e66def
M. Chandler Bennett <mcbennet@ncsu.edu>
2021-12-17 15:29:03 -0500

    Finalizing example for qdens-radial

--- nexus/examples/qmcpack/rsqmc_misc/diamond_radial_density/qdens.sh
+++ nexus/examples/qmcpack/rsqmc_misc/diamond_radial_density/qdens.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-qdens -a -v -e 10 -f xsf -i vmc.g000.twistnum_0.in.xml *.stat.h5
+qdens -a -v -e 10 -f xsf -i runs_spin/vmc/vmc.g000.twistnum_0.in.xml runs_spin/vmc/*.stat.h5
+
