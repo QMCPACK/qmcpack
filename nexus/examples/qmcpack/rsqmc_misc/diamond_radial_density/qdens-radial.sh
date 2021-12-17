db865fa29862719620030cb7ed29ebcbd4c24b8d
M. Chandler Bennett <mcbennet@ncsu.edu>
2021-12-17 15:23:00 -0500

    Add adens-radial example

---
+++ nexus/examples/qmcpack/rsqmc_misc/diamond_radial_density/qdens-radial.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+qdens-radial -s C -r 0.6 vmc.avg.s000.SpinDensity_u+d.xsf
+qdens-radial -s C -r 0.6 -c vmc.avg.s000.SpinDensity_u+d.xsf
---
+++ nexus/examples/qmcpack/rsqmc_misc/diamond_radial_density/qdens.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qdens -a -v -e 10 -f xsf -i vmc.g000.twistnum_0.in.xml *.stat.h5
