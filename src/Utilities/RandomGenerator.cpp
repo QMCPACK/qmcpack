7bba53cfee657a35b7c390c3bd73e5e80939c8cf
Ye Luo <yeluo@anl.gov>
2021-12-30 13:46:16 -0600

    Remove unused code.

--- src/Utilities/RandomGenerator.cpp
+++ src/Utilities/RandomGenerator.cpp
@@ -12,62 +12,7 @@
 
 
 #include "RandomGenerator.h"
-#include "Message/Communicate.h"
-
-//using namespace qmcplusplus;
-//RandomGenerator_t
-//Random(CommCreate::get()->getNodeID(), CommCreate::get()->getNumNodes());
 
 uint32_t make_seed(int i, int n) { return static_cast<uint32_t>(std::time(0)) % 10474949 + (i + 1) * n + i; }
 
 qmcplusplus::RNGThreadSafe qmcplusplus::Random;
-
-// /**class GaussinRandomGenerator
-//  *\brief A filter class that converts random numbers [0,1) -> gaussian
-//  */
-// class GaussianRandomGenerator {
-// public:
-
-//   typedef RandomGenerator_t::Return_t Return_t;
-
-//   GaussianRandomGenerator(RandomGenerator_t& rg):d_engine(rg) { }
-
-//   inline Return_t operator()(){
-//     if(newpair) {
-//       d_engine.bivariate(gauss0,gauss1);
-//       newpair = false;
-//       return gauss0;
-//     } else {
-//       newpair = true;
-//       return gauss1;
-//     }
-//   }
-// private:
-//   RandomGenerator_t d_engine;
-//   bool newpair;
-//   Return_t gauss0, gauss1;
-// };
-// GaussianRandomGenerator GaussianRandom(Random);
-
-
-//   class GaussianRandom {
-//   public:
-//     typedef RandomGenerator_t::Return_t Return_t;
-//     GaussianRandom(RandomGenerator_t& rg, Return_t sig=1.0, Return_t c0=0.0):
-//       d_engine(rg), newpair(true){ Sigma2 = sig*sig; Center = c0;}
-//     inline Return_t operator()(){
-//       if(newpair) {
-// 	d_engine.bivariate(gauss0,gauss1);
-// 	newpair = false;
-// 	return Sigma2*gauss0+Center;
-//       } else {
-// 	newpair = true;
-// 	return Sigma2*gauss1+Center;
-//       }
-//     }
-//   private:
-//     RandomGenerator_t& d_engine;
-//     bool newpair;
-//     Return_t gauss0, gauss1, Sigma2, Center;
-
-//   };
