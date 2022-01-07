7e575bfd3ecf4509943b10b94242e43434387a13
Mark Dewing <markdewing@gmail.com>
2022-01-06 23:53:44 -0600

    Add unit test for vp file override
    Add unit test for the override_variational_parameter element to get
    added or updated in updateXmlNodes.

---
+++ src/QMCDrivers/tests/test_QMCCostFunctionBase.cpp
@@ -0,0 +1,130 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2022 QMCPACK developers.
+//
+// File developed by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
+//
+// File created by: Mark Dewing, mdewing@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+#include "QMCDrivers/WFOpt/QMCCostFunctionBase.h"
+
+
+namespace qmcplusplus
+{
+
+class QMCCostFunctionTest : public QMCCostFunctionBase
+{
+public:
+  QMCCostFunctionTest(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h, Communicate* c)
+      : QMCCostFunctionBase(w, psi, h, c)
+  {}
+
+  virtual ~QMCCostFunctionTest() {}
+
+  virtual void resetPsi(bool final_reset = false) override {}
+  virtual Return_rt fillOverlapHamiltonianMatrices(Matrix<Return_rt>& Left, Matrix<Return_rt>& Right) override
+  {
+    return 0;
+  }
+  virtual void getConfigurations(const std::string& aroot) override {}
+  virtual void checkConfigurations() override {}
+  virtual Return_rt correlatedSampling(bool needGrad = true) override { return 0; }
+
+  void callUpdateXmlNodes()
+  {
+    do_override_output = true;
+    updateXmlNodes();
+  }
+
+  // Print the updated XML nodes. Useful for debugging.
+  void printXml()
+  {
+    char* buf;
+    int out_buflen;
+    xmlDocDumpFormatMemory(m_doc_out, (xmlChar**)&buf, &out_buflen, 1);
+    std::cout << "XML buffer length = " << out_buflen << std::endl;
+    std::cout << "XML: " << std::endl << buf << std::endl;
+    xmlFree(buf);
+  }
+
+  xmlDocPtr getDoc() { return m_doc_out; }
+};
+
+
+TEST_CASE("updateXmlNodes", "[drivers]")
+{
+  MCWalkerConfiguration w;
+  QMCHamiltonian h;
+  TrialWaveFunction psi;
+
+  Communicate* comm = OHMMS::Controller;
+
+  QMCCostFunctionTest cost(w, psi, h, comm);
+
+  cost.setRootName("tmp");
+
+  const char* wf_xml = R"(
+<wavefunction/>
+    )";
+
+  Libxml2Document doc;
+  bool okay = doc.parseFromString(wf_xml);
+  REQUIRE(okay);
+  cost.setWaveFunctionNode(doc.getRoot());
+
+  cost.callUpdateXmlNodes();
+  //cost.printXml();
+
+  xmlXPathContextPtr acontext = xmlXPathNewContext(cost.getDoc());
+  OhmmsXPathObject check_elem("//override_variational_parameters", acontext);
+  REQUIRE(check_elem.size() == 1);
+
+  xmlChar* href = xmlGetProp(check_elem[0], BAD_CAST "href");
+  REQUIRE(href != nullptr);
+  REQUIRE((char*)(href) == std::string("tmp.vp.h5"));
+  xmlFree(href);
+  xmlXPathFreeContext(acontext);
+}
+
+TEST_CASE("updateXmlNodes with existing element", "[drivers]")
+{
+  MCWalkerConfiguration w;
+  QMCHamiltonian h;
+  TrialWaveFunction psi;
+
+  Communicate* comm = OHMMS::Controller;
+
+  QMCCostFunctionTest cost(w, psi, h, comm);
+  cost.setRootName("tmp2");
+
+  const char* wf_xml = R"(
+<wavefunction>
+  <override_variational_parameters href="vp.tmp.h5"/>
+</wavefunction>
+    )";
+
+  Libxml2Document doc;
+  bool okay = doc.parseFromString(wf_xml);
+  REQUIRE(okay);
+  cost.setWaveFunctionNode(doc.getRoot());
+
+  cost.callUpdateXmlNodes();
+  //cost.printXml();
+
+  xmlXPathContextPtr acontext = xmlXPathNewContext(cost.getDoc());
+  OhmmsXPathObject check_elem("//override_variational_parameters", acontext);
+  REQUIRE(check_elem.size() == 1);
+
+  xmlChar* href = xmlGetProp(check_elem[0], BAD_CAST "href");
+  REQUIRE(href != nullptr);
+  REQUIRE((char*)(href) == std::string("tmp2.vp.h5"));
+  xmlFree(href);
+  xmlXPathFreeContext(acontext);
+}
+
+
+} // namespace qmcplusplus
