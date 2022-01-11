327b5e797271e1beb4fd0edc3dc4aa0f6958ca2d
camelto2 <cmelton@sandia.gov>
2022-01-11 15:48:58 -0700

    change  particle index

--- src/QMCWaveFunctions/tests/test_einset_spinor.cpp
+++ src/QMCWaveFunctions/tests/test_einset_spinor.cpp
@@ -444,7 +444,7 @@ TEST_CASE("Einspline SpinorSet from HDF", "[wavefunction]")
   elec_2.R[2]     = elec_.R[0];
   elec_2.spins[0] = elec_.spins[1];
   elec_2.spins[1] = elec_.spins[2];
-  elec_2.spins[2] = elec_.spins[1];
+  elec_2.spins[2] = elec_.spins[0];
   RefVectorWithLeader<ParticleSet> p_list(elec_);
   p_list.push_back(elec_);
   p_list.push_back(elec_2);
