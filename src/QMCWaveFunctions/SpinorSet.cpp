6cf6e7884e33f7347b06cdc270a71a47fd1705cb
camelto2 <cmelton@sandia.gov>
2022-01-12 13:53:45 -0700

    batched evaluation eval_notranspose spinorset

--- src/QMCWaveFunctions/SpinorSet.cpp
+++ src/QMCWaveFunctions/SpinorSet.cpp
@@ -178,6 +178,115 @@ void SpinorSet::evaluate_notranspose(const ParticleSet& P,
   }
 }
 
+void SpinorSet::mw_evaluate_notranspose(const RefVectorWithLeader<SPOSet>& spo_list,
+                                        const RefVectorWithLeader<ParticleSet>& P_list,
+                                        int first,
+                                        int last,
+                                        const RefVector<ValueMatrix_t>& logdet_list,
+                                        const RefVector<GradMatrix_t>& dlogdet_list,
+                                        const RefVector<ValueMatrix_t>& d2logdet_list) const
+{
+  auto& spo_leader = spo_list.getCastedLeader<SpinorSet>();
+  auto& P_leader   = P_list.getLeader();
+  assert(this == &spo_leader);
+
+  IndexType nw    = spo_list.size();
+  IndexType nelec = P_leader.getTotalNum();
+
+  SPOSet& up_spo_leader = *(spo_leader.spo_up);
+  SPOSet& dn_spo_leader = *(spo_leader.spo_dn);
+  RefVectorWithLeader<SPOSet> up_spo_list(up_spo_leader);
+  RefVectorWithLeader<SPOSet> dn_spo_list(dn_spo_leader);
+  up_spo_list.reserve(nw);
+  dn_spo_list.reserve(nw);
+  up_spo_list.clear();
+  dn_spo_list.clear();
+
+  std::vector<ValueMatrix_t> mw_up_logdet, mw_dn_logdet;
+  std::vector<GradMatrix_t> mw_up_dlogdet, mw_dn_dlogdet;
+  std::vector<ValueMatrix_t> mw_up_d2logdet, mw_dn_d2logdet;
+  mw_up_logdet.reserve(nw);
+  mw_dn_logdet.reserve(nw);
+  mw_up_dlogdet.reserve(nw);
+  mw_dn_dlogdet.reserve(nw);
+  mw_up_d2logdet.reserve(nw);
+  mw_dn_d2logdet.reserve(nw);
+  mw_up_logdet.clear();
+  mw_dn_logdet.clear();
+  mw_up_dlogdet.clear();
+  mw_dn_dlogdet.clear();
+  mw_up_d2logdet.clear();
+  mw_dn_d2logdet.clear();
+
+  RefVector<ValueMatrix_t> up_logdet_list, dn_logdet_list;
+  RefVector<GradMatrix_t> up_dlogdet_list, dn_dlogdet_list;
+  RefVector<ValueMatrix_t> up_d2logdet_list, dn_d2logdet_list;
+  up_logdet_list.reserve(nw);
+  dn_logdet_list.reserve(nw);
+  up_dlogdet_list.reserve(nw);
+  dn_dlogdet_list.reserve(nw);
+  up_d2logdet_list.reserve(nw);
+  dn_d2logdet_list.reserve(nw);
+  up_logdet_list.clear();
+  dn_logdet_list.clear();
+  up_dlogdet_list.clear();
+  dn_dlogdet_list.clear();
+  up_d2logdet_list.clear();
+  dn_d2logdet_list.clear();
+
+  ValueMatrix_t tmp_val_mat(nelec, OrbitalSetSize);
+  GradMatrix_t tmp_grad_mat(nelec, OrbitalSetSize);
+  for (int iw = 0; iw < nw; iw++)
+  {
+    SpinorSet& spinor = spo_list.getCastedElement<SpinorSet>(iw);
+    up_spo_list.emplace_back(*(spinor.spo_up));
+    dn_spo_list.emplace_back(*(spinor.spo_dn));
+
+    mw_up_logdet.emplace_back(tmp_val_mat);
+    up_logdet_list.emplace_back(mw_up_logdet.back());
+    mw_dn_logdet.emplace_back(tmp_val_mat);
+    dn_logdet_list.emplace_back(mw_dn_logdet.back());
+
+    mw_up_dlogdet.emplace_back(tmp_grad_mat);
+    up_dlogdet_list.emplace_back(mw_up_dlogdet.back());
+    mw_dn_dlogdet.emplace_back(tmp_grad_mat);
+    dn_dlogdet_list.emplace_back(mw_dn_dlogdet.back());
+
+    mw_up_d2logdet.emplace_back(tmp_val_mat);
+    up_d2logdet_list.emplace_back(mw_up_d2logdet.back());
+    mw_dn_d2logdet.emplace_back(tmp_val_mat);
+    dn_d2logdet_list.emplace_back(mw_dn_d2logdet.back());
+  }
+
+  up_spo_leader.mw_evaluate_notranspose(up_spo_list, P_list, first, last, up_logdet_list, up_dlogdet_list,
+                                        up_d2logdet_list);
+  dn_spo_leader.mw_evaluate_notranspose(dn_spo_list, P_list, first, last, dn_logdet_list, dn_dlogdet_list,
+                                        dn_d2logdet_list);
+
+#pragma omp parallel for
+  for (int iw = 0; iw < nw; iw++)
+  {
+    for (int iat = 0; iat < nelec; iat++)
+    {
+      ParticleSet::Scalar_t s = P_list[iw].activeSpin(iat);
+      RealType coss           = std::cos(s);
+      RealType sins           = std::sin(s);
+      ValueType eis(coss, sins);
+      ValueType emis(coss, -sins);
+
+      for (int no = 0; no < OrbitalSetSize; no++)
+      {
+        logdet_list[iw].get()(iat, no) =
+            eis * up_logdet_list[iw].get()(iat, no) + emis * dn_logdet_list[iw].get()(iat, no);
+        dlogdet_list[iw].get()(iat, no) =
+            eis * up_dlogdet_list[iw].get()(iat, no) + emis * dn_dlogdet_list[iw].get()(iat, no);
+        d2logdet_list[iw].get()(iat, no) =
+            eis * up_d2logdet_list[iw].get()(iat, no) + emis * dn_d2logdet_list[iw].get()(iat, no);
+      }
+    }
+  }
+}
+
 void SpinorSet::evaluate_notranspose_spin(const ParticleSet& P,
                                           int first,
                                           int last,
