73a86e148119e3ca86ac95f1307e674cc2554af8
camelto2 <cmelton@sandia.gov>
2022-01-13 10:14:49 -0700

    update copyright/contributors and cleanup

--- src/QMCWaveFunctions/SpinorSet.cpp
+++ src/QMCWaveFunctions/SpinorSet.cpp
@@ -2,9 +2,10 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2022 QMCPACK developers
 //
 // File developed by: Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
+//                    Cody A. Melton, cmelton@sandia.gov, Sandia National Laboratories
 //
 // File created by:  Raymond Clay III, rclay@sandia.gov, Sandia National Laboratories
 //////////////////////////////////////////////////////////////////////////////////////
--- src/QMCWaveFunctions/tests/test_einset_spinor.cpp
+++ src/QMCWaveFunctions/tests/test_einset_spinor.cpp
@@ -2,9 +2,10 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2020 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2022 QMCPACK developers
 //
 // File developed by: Raymond Clay, rclay@sandia.gov, Sandia National Laboratories
+//                    Cody A. Melton, cmelton@sandia.gov, Sandia National Laboratories
 //
 // File created by: Raymond Clay, rclay@sandia.gov, Sandia National Laboratories
 //////////////////////////////////////////////////////////////////////////////////////
