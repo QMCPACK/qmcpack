6931928ea9541fe08352d1b09aef03ff5aad7550
camelto2 <cmelton@sandia.gov>
2022-02-17 18:05:06 -0700

    add multiplication to Taus

--- src/QMCDrivers/TauParams.hpp
+++ src/QMCDrivers/TauParams.hpp
@@ -61,6 +61,29 @@ struct TauParams<RT, CoordsType::POS_SPIN>
     spin_sqrttau     = std::sqrt(spin_tauovermass);
   }
 };
+
+template<CoordsType CT, typename RT>
+TauParams<RT, CT> operator*(const TauParams<RT, CT>& taus, RT val)
+{
+  TauParams<RT, CT> out = taus;
+  out.tauovermass *= val;
+  out.oneover2tau *= val;
+  out.sqrttau *= std::sqrt(val);
+  if constexpr (CT == CoordsType::POS_SPIN)
+  {
+    out.spin_tauovermass *= val;
+    out.spin_oneover2tau *= val;
+    out.spin_sqrttau *= std::sqrt(val);
+  }
+  return out;
+}
+
+template<CoordsType CT, typename RT>
+TauParams<RT, CT> operator*(RT val, const TauParams<RT, CT>& taus)
+{
+  return taus * val;
+}
+
 } // namespace qmcplusplus
 
 #endif
