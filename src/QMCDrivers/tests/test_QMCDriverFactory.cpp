c34bfd4b0c386b90f4e1f9ba8d19f2611523c4e8
Peter Doak <doakpw@ornl.gov>
2022-03-04 14:30:48 -0500

    support global driver epoch switch

--- src/QMCDrivers/tests/test_QMCDriverFactory.cpp
+++ src/QMCDrivers/tests/test_QMCDriverFactory.cpp
@@ -121,7 +121,7 @@ TEST_CASE("QMCDriverFactory create DMCBatched driver", "[qmcapp]")
   Communicate* comm;
   comm = OHMMS::Controller;
 
-  ProjectData test_project;
+  ProjectData test_project("batched");
 
   QMCDriverFactory driver_factory(test_project);
 
