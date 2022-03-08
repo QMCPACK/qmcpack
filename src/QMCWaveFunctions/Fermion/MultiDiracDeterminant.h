4f5a13ef75daf917019535a9b229e8de4d49b97a
Thomas Applencourt <tapplencourt@anl.gov>
2022-03-08 23:44:17 +0000

    update_ratios_list

--- src/QMCWaveFunctions/Fermion/MultiDiracDeterminant.h
+++ src/QMCWaveFunctions/Fermion/MultiDiracDeterminant.h
@@ -352,20 +352,6 @@ public:
     }
   }
 
-  void  update_ratios_list_for_all_ext_level(
-                          int max_ext_level,
-                          int nw,
-                          const RefVector<OffloadVector<ValueType>>& ratios_list,
-                          const std::vector<size_t>& sum_ndets_per_excitation_level,
-                          const std::vector<size_t>& sum_with_shift_ndets_per_excitation_level,
-                          const std::vector<int>& data,
-                          const std::vector<RealType>& sign,
-                          const std::vector<ValueType>& det0_list,
-                          const RefVector<OffloadMatrix<ValueType>>& dotProducts_list)
-{
-  constexpr auto seq = std::make_index_sequence<5>();
-
-} 
 
   /** Function to calculate the ratio of the excited determinant to the reference determinant in CalculateRatioFromMatrixElements following the paper by Clark et al. JCP 135(24), 244105
    *@param nw Number of walkers in the batch
