01de6641e625e2b1cca7d17db880de735fe8092d
Michael Walsh <walshmm@ornl.gov>
2022-03-25 15:00:40 -0400

    spaces are required for string compare

--- external_codes/github_actions/trigger-rebase-on-push.sh
+++ external_codes/github_actions/trigger-rebase-on-push.sh
@@ -21,7 +21,7 @@ for pr in "${pr_list[@]}"; do
     BODY=$(echo "$pr" | jq -r .[0].body)
     HEAD=$(echo "$pr" | jq -r .[0].head)
     PULL_NUMBER=$(echo "$pr" | jq -r .[0].pr_number)
-    if [["$BODY" == *"!-> Feel free to automatically rebase this PR. <-!"*]]; then
+    if [[ "$BODY" == *"!-> Feel free to automatically rebase this PR. <-!"* ]]; then
         # edit pr to cause rebase
         UPDATE_PARAMETERS="{'body':${BODY}" + "\r\n" + "AUTOMATED CHANGE: Rebase to new base head of ${HEAD}}"
         RESULT = $(curl -X PATCH -H "${AUTH_HEADER}" -H "${API_HEADER}" \
