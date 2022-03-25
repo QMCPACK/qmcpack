0bf08820e2d74428333f81a8652cf67d10f041ef
Michael Walsh <walshmm@ornl.gov>
2022-03-25 14:37:55 -0400

    bash typos

--- external_codes/github_actions/trigger-rebase-on-push.sh
+++ external_codes/github_actions/trigger-rebase-on-push.sh
@@ -17,11 +17,11 @@ pr_list=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
 		"${URI}/repos/$GITHUB_REPOSITORY/pulls")
 
 
-pr_list | jq '.[] | {pr_number: .number, body: .body, head: .base.sha}' | while read object; do
+$pr_list | jq '.[] | {pr_number: .number, body: .body, head: .base.sha}' | while read object; do
     BODY=$(echo "$object" | jq -r .body)
     HEAD=$(echo "$object" | jq -r .head)
     PULL_NUMBER=$(echo "$object" | jq -r .pr_number)
-    if [[BODY == *"!-> Feel free to automatically rebase this PR. <-!"*]]; then
+    if [["$BODY" == *"!-> Feel free to automatically rebase this PR. <-!"*]]; then
         # edit pr to cause rebase
         UPDATE_PARAMETERS="{'body':${BODY}" + "\n" + "AUTOMATED CHANGE: Rebase to new base head of ${HEAD}}"
         RESULT = $(curl -X PATCH -H "${AUTH_HEADER}" -H "${API_HEADER}" \
