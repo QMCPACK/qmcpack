b3d7af727b8a8c72357c75b107b2a4d63cc58646
Kevin Gasperich <kgasperich@gmail.com>
2022-04-13 23:28:42 +0000

    formatting

--- src/Platforms/OMPTarget/ompBLAS.cpp
+++ src/Platforms/OMPTarget/ompBLAS.cpp
@@ -57,8 +57,7 @@ ompBLAS_status gemv_impl(ompBLAS_handle& handle,
   }
   else
   {
-    
-    if (incx !=1 || incy != 1)
+    if (incx != 1 || incy != 1)
       throw std::runtime_error("incx !=1 or incy != 1 are not implemented in ompBLAS::gemv_impl!");
 
     PRAGMA_OFFLOAD("omp target teams distribute num_teams(n) is_device_ptr(A, x, y)")
@@ -167,12 +166,12 @@ ompBLAS_status gemv_batched_impl(ompBLAS_handle& handle,
       throw std::runtime_error("incx !=1 or incy != 1 are not implemented in ompBLAS::gemv_batched_impl!");
 
     PRAGMA_OFFLOAD("omp target teams distribute collapse(2) num_teams(batch_count * n) is_device_ptr(A, x, y, alpha, beta)")
-    for(size_t ib = 0; ib < batch_count; ib++)
-      for(size_t i = 0; i < n; i++)
+    for (size_t ib = 0; ib < batch_count; ib++)
+      for (size_t i = 0; i < n; i++)
       {
         T dot_sum(0);
         PRAGMA_OFFLOAD("omp parallel for simd reduction(+: dot_sum)")
-        for(size_t j = 0; j < m; j++)
+        for (size_t j = 0; j < m; j++)
           dot_sum += x[ib][j] * A[ib][i * lda + j];
         if (beta[ib] == T(0))
           y[ib][i] = alpha[ib] * dot_sum; // protecting NaN from y
