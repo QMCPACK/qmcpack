e01c2eddbe3ab79b62d479e46b16710c6ab14c7b
Ye Luo <yeluo@anl.gov>
2022-04-28 16:28:39 -0500

    Clean up OHMMS::Controller->initialize in unit tests.

--- src/QMCDrivers/tests/test_VMCBatched.cpp
+++ src/QMCDrivers/tests/test_VMCBatched.cpp
@@ -32,8 +32,6 @@ public:
   VMCBatchedTest()
   {
     Concurrency::OverrideMaxCapacity<> override(8);
-    Communicate* comm;
-    OHMMS::Controller->initialize(0, NULL);
     comm_ = OHMMS::Controller;
   }
 
@@ -41,9 +39,6 @@ public:
   {
     using namespace testing;
     Concurrency::OverrideMaxCapacity<> override(8);
-    Communicate* comm;
-    OHMMS::Controller->initialize(0, NULL);
-    comm = OHMMS::Controller;
 
     Libxml2Document doc;
     doc.parseFromString(valid_vmc_input_sections[valid_vmc_input_vmc_batch_index]);
@@ -51,9 +46,9 @@ public:
     QMCDriverInput qmcdriver_input;
     qmcdriver_input.readXML(node);
 
-    auto particle_pool     = MinimalParticlePool::make_diamondC_1x1x1(comm);
-    auto wavefunction_pool = MinimalWaveFunctionPool::make_diamondC_1x1x1(comm, particle_pool);
-    auto hamiltonian_pool  = MinimalHamiltonianPool::make_hamWithEE(comm, particle_pool, wavefunction_pool);
+    auto particle_pool     = MinimalParticlePool::make_diamondC_1x1x1(comm_);
+    auto wavefunction_pool = MinimalWaveFunctionPool::make_diamondC_1x1x1(comm_, particle_pool);
+    auto hamiltonian_pool  = MinimalHamiltonianPool::make_hamWithEE(comm_, particle_pool, wavefunction_pool);
   }
 
 private:
