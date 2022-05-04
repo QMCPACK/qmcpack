1c5037ec1bc43589611b1f2c8a97cf9266bc4d0f
Andrea Zen <zen.andrea.x@gmail.com>
2022-05-04 17:37:54 +0100

    update config/build_archer2.sh

--- config/build_archer2.sh
+++ config/build_archer2.sh
@@ -45,12 +45,10 @@ do
     cmake -DCMAKE_C_COMPILER="cc" \
           -DCMAKE_CXX_COMPILER="CC" \
 	  -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment \
-          -D LIBXML2_LIBRARY="$LIBXML2_ROOT/lib/libxml2.so" \
-          -D LIBXML2_INCLUDE_DIR="$LIBXML2_ROOT//include/libxml2" \
+          -D LibXml2_ROOT=$LIBXML2_ROOT \
           -DBUILD_LMYENGINE_INTERFACE=0 \
           ${builds[$build]} \
           ..
-#          -D LibXml2_ROOT=$LIBXML2_ROOT \
     make -j 20
     if [ $? -eq 0 ]; then
       build_dir=$(pwd)
