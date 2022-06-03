dee1bf3b5b950104b695cbff8ade5370f42384ac
Steven Hahn <hahnse@ornl.gov>
2022-06-03 17:17:37 -0400

    Avoid copy in try_compile. Print COMPILE_OUTPUT
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- CMake/TestGNUCUDAVersion.cmake
+++ CMake/TestGNUCUDAVersion.cmake
@@ -1,16 +1,19 @@
 set(CUDA_GNU_COMPATIBLE TRUE)
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 12.0)
   if(CMAKE_CUDA_COMPILER_VERSION VERSION_LESS_EQUAL 11.7)
-    set(CUDA_GNU_COMPLATIBLE FALSE)	  
+    set(CUDA_GNU_COMPLATIBLE FALSE)
   endif()
 endif()
 
-file(COPY ${PROJECT_SOURCE_DIR}/CMake/try_compile_sources/test_map.cu DESTINATION ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp)
-try_compile(PASS_MAPCUDA ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/test_map.cu
-	    CMAKE_FLAGS "${CMAKE_CUDA_FLAGS}")
-
-if(NOT PASS_MAPCUDA OR NOT CUDA_GNU_COMPATIBLE)
-  message(FATAL_ERROR 
+if(NOT CUDA_GNU_COMPATIBLE)
+  message(FATAL_ERROR
           "CUDA version <=11.7 do not support GCC versions >= 12. Please change CMAKE_CXX_COMPILER"
-          "to an older GCC version compatible with the CUDA version ${CMAKE_CUDA_COMPILER_VERSION}") 
+          "to an older GCC version compatible with the CUDA version ${CMAKE_CUDA_COMPILER_VERSION}"
+endif()
+
+try_compile(PASS_MAPCUDA ${PROJECT_BINARY_DIR} ${PROJECT_SOURCE_DIR}/CMake/try_compile_sources/test_map.cu
+	    CMAKE_FLAGS "${CMAKE_CUDA_FLAGS}" OUTPUT_VARIABLE COMPILE_OUTPUT)
+
+if(NOT PASS_MAPCUDA)
+  message(FATAL_ERROR "CUDA functionality failed!" "See compiler output at ${COMPILE_OUTPUT}")
 endif()
