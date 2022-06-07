81f16b33b871ce8732d29da69dd8db66d472d950
Steven Hahn <hahnse@ornl.gov>
2022-06-07 17:06:37 -0400

    More verbose message
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- CMake/TestCUDAHostCompatibility.cmake
+++ CMake/TestCUDAHostCompatibility.cmake
@@ -2,5 +2,7 @@ try_compile(PASS_MAPCUDA ${PROJECT_BINARY_DIR} ${PROJECT_SOURCE_DIR}/CMake/try_c
             CMAKE_FLAGS "${CMAKE_CUDA_FLAGS}" OUTPUT_VARIABLE COMPILE_OUTPUT)
 
 if(NOT PASS_MAPCUDA)
-  message(FATAL_ERROR "CUDA_HOST_COMPILER is not able to compile a simple test program!" "Compiler output: ${COMPILE_OUTPUT}")
+  message(FATAL_ERROR "Failed to compile host codes containing std::map by nvcc. Please check nvcc and host compiler compatibility."
+                      "You may need to explicitly set an nvcc compatible host compiler as CUDA_HOST_COMPILER."
+                      "Compiler output: ${COMPILE_OUTPUT}")
 endif()
