1229433a557743b89c45f740cbb099003fae0ce4
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-07-03 15:45:46 -0400

    twist in 3D

--- src/Particle/LongRange/tests/test_kcontainer.cpp
+++ src/Particle/LongRange/tests/test_kcontainer.cpp
@@ -68,6 +68,40 @@ TEST_CASE("kcontainer at gamma in 3D", "[longrange]")
   }
 }
 
+TEST_CASE("kcontainer at twist in 3D", "[longrange]")
+{
+  const int ndim = 3;
+  const double alat = 1.0;
+  const double blat = 2*M_PI/alat;
+
+  // twist one shell of kvectors
+  const double kc = blat+1e-6;
+
+  CrystalLattice<OHMMS_PRECISION, OHMMS_DIM> lattice;
+  lattice.R.diagonal(1.0);
+  lattice.set(lattice.R); // compute Rv and Gv from R
+
+  KContainer klists;
+
+  PosType twist;
+  twist[0] = 0.1;
+  klists.updateKLists(lattice, kc, ndim, twist);
+  CHECK(klists.kpts.size() == 1);
+  CHECK(klists.kpts_cart[0][0] == Approx(blat*(twist[0]-1)));
+
+  twist = {-0.5, 0, 0.5};
+  klists.updateKLists(lattice, kc, ndim, twist);
+  int gvecs[3][3] = {
+    { 0,  0,  -1},
+    { 1,  0,  -1},
+    { 1,  0,   0}
+  };
+  CHECK(klists.kpts.size() == 3);
+  for (int ik=0;ik<klists.kpts.size();ik++)
+    for (int ldim=0;ldim<ndim;ldim++)
+      CHECK(klists.kpts_cart[ik][ldim] == Approx(blat*(twist[ldim]+gvecs[ik][ldim])));
+}
+
 TEST_CASE("kcontainer at gamma in 2D", "[longrange]")
 {
   const int ndim = 2;
