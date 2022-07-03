4fdd530bd8ad88d445a9db22a69b4a6587453929
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-07-03 15:35:07 -0400

    twist 2D

--- src/Particle/LongRange/tests/test_kcontainer.cpp
+++ src/Particle/LongRange/tests/test_kcontainer.cpp
@@ -1,9 +1,11 @@
 #include "catch.hpp"
 
 #include "Particle/LongRange/KContainer.h"
+#include "OhmmsPETE/TinyVector.h"
 
 namespace qmcplusplus
 {
+using PosType = TinyVector<double, 3>;
 
 TEST_CASE("kcontainer at gamma in 3D", "[longrange]")
 {
@@ -27,7 +29,7 @@ TEST_CASE("kcontainer at gamma in 3D", "[longrange]")
     CHECK(klists.kpts.size() == nks[ik]);
   }
   const int mxk = klists.kpts.size();
-  int gvecs[mxk][OHMMS_DIM] = {
+  int gvecs[26][3] = {
     {-1,  0,  0},
     { 0, -1,  0},
     { 0,  0, -1},
@@ -88,7 +90,7 @@ TEST_CASE("kcontainer at gamma in 2D", "[longrange]")
     CHECK(klists.kpts.size() == nks[ik]);
   }
   const int mxk = klists.kpts.size();
-  int gvecs[mxk][OHMMS_DIM] = {
+  int gvecs[12][3] = {
     {-1,  0,  0},
     { 0, -1,  0},
     { 0,  1,  0},
@@ -111,6 +113,48 @@ TEST_CASE("kcontainer at gamma in 2D", "[longrange]")
       CHECK(klists.kpts[ik][ldim]*blat == klists.kpts_cart[ik][ldim]);
     }
   }
+}
+
+TEST_CASE("kcontainer at twist in 2D", "[longrange]")
+{
+  const int ndim = 2;
+  const double alat = 1.0;
+  const double blat = 2*M_PI/alat;
+
+  // twist one shell of kvectors
+  const double kc = blat+1e-6;
+
+  CrystalLattice<OHMMS_PRECISION, OHMMS_DIM> lattice;
+  lattice.R.diagonal(1.0);
+  lattice.set(lattice.R); // compute Rv and Gv from R
+
+  KContainer klists;
+
+  PosType twist;
+  twist[0] = 0.1;
+  klists.updateKLists(lattice, kc, ndim, twist);
+  CHECK(klists.kpts.size() == 1);
+  CHECK(klists.kpts_cart[0][0] == Approx(blat*(twist[0]-1)));
+
+  twist[1] = 0.1;
+  klists.updateKLists(lattice, kc, ndim, twist);
+  CHECK(klists.kpts.size() == 2);
+  CHECK(klists.kpts_cart[0][0] == Approx(blat*(twist[0]-1)));
+  CHECK(klists.kpts_cart[0][1] == Approx(blat*twist[1]));
+  CHECK(klists.kpts_cart[1][0] == Approx(blat*(twist[0])));
+  CHECK(klists.kpts_cart[1][1] == Approx(blat*(twist[1]-1)));
+
+  twist = {-0.5, 0.5, 0};
+  klists.updateKLists(lattice, kc, ndim, twist);
+  CHECK(klists.kpts.size() == 3);
+  //for (int ik=0;ik<3;ik++)
+  //  app_log() << klists.kpts_cart[ik] << std::endl;
+  CHECK(klists.kpts_cart[0][0] == Approx(blat*(twist[0]-0)));
+  CHECK(klists.kpts_cart[0][1] == Approx(blat*(twist[1]-1)));
+  CHECK(klists.kpts_cart[1][0] == Approx(blat*(twist[0]+1)));
+  CHECK(klists.kpts_cart[1][1] == Approx(blat*(twist[1]-1)));
+  CHECK(klists.kpts_cart[2][0] == Approx(blat*(twist[0]+1)));
+  CHECK(klists.kpts_cart[2][1] == Approx(blat*twist[1]));
 }
 
 } // qmcplusplus
