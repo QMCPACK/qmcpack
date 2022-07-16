3a43bcc6ab1882d2db205d18127fdcad458cc7d2
Ye Luo <yeluo@anl.gov>
2022-07-15 22:43:18 -0500

    Make sure non-default context/device defined

--- src/Platforms/SYCL/SYCLDeviceManager.cpp
+++ src/Platforms/SYCL/SYCLDeviceManager.cpp
@@ -16,11 +16,24 @@
 #include <stdexcept>
 #include <string>
 #include <algorithm>
-#include "config.h"
-#include "Platforms/Host/OutputManager.h"
-#include "Platforms/Host/determineDefaultDeviceNum.h"
+#include "OutputManager.h"
+#include "determineDefaultDeviceNum.h"
+
 namespace qmcplusplus
 {
+#if defined(ENABLE_OFFLOAD)
+syclDeviceInfo::syclDeviceInfo(const sycl::context& context, const sycl::device& device, const omp_interop_t& interop)
+    : context_(context), device_(device), interop_(interop)
+#else
+syclDeviceInfo::syclDeviceInfo(const sycl::context& context, const sycl::device& device)
+    : context_(context), device_(device)
+#endif
+{}
+
+syclDeviceInfo::~syclDeviceInfo()
+{
+  //#pragma omp interop destroy(interop_)
+}
 
 std::unique_ptr<sycl::queue> SYCLDeviceManager::default_device_queue;
 
@@ -28,28 +41,25 @@ SYCLDeviceManager::SYCLDeviceManager(int& default_device_num, int& num_devices, 
     : sycl_default_device_num(-1)
 {
 #if defined(ENABLE_OFFLOAD)
-  const int sycl_device_count=omp_get_num_devices();
-  sycl_default_device_num = determineDefaultDeviceNum(sycl_device_count, local_rank, local_size);
+  const int sycl_device_count = omp_get_num_devices();
+  sycl_default_device_num     = determineDefaultDeviceNum(sycl_device_count, local_rank, local_size);
   if (default_device_num < 0)
     default_device_num = sycl_default_device_num;
 
-  visible_devices.resize(sycl_device_count);
   for (int id = 0; id < sycl_device_count; id++)
   {
     omp_interop_t interop;
-#pragma omp interop device(id) init(prefer_type("sycl"),targetsync: interop)
-    if(id == sycl_default_device_num)
-    {
-      int result;
-      sycl::queue* omp_queue = static_cast<sycl::queue *>(omp_get_interop_ptr(interop, omp_ipr_targetsync, &result));
-      if(result != omp_irc_success)
-        throw std::runtime_error("SYCLDeviceManager::SYCLDeviceManager fail to obtain sycl::queue by interop");
+#pragma omp interop device(id) init(prefer_type("sycl"), targetsync : interop)
 
-      default_device_queue.reset(new sycl::queue(*omp_queue));
-      visible_devices[id].queue_ = omp_queue;
-    }
-    visible_devices[id].interop_ = interop;
-  } 
+    int result;
+    sycl::queue* omp_queue = static_cast<sycl::queue*>(omp_get_interop_ptr(interop, omp_ipr_targetsync, &result));
+    if (result != omp_irc_success)
+      throw std::runtime_error("SYCLDeviceManager::SYCLDeviceManager fail to obtain sycl::queue by interop");
+
+    visible_devices.emplace_back(omp_queue->get_context(), omp_queue->get_device(), interop);
+    if (id == sycl_default_device_num)
+      default_device_queue = std::make_unique<sycl::queue>(*omp_queue);
+  }
 
 #else
   // Potentially multiple GPU platform.
@@ -77,7 +87,7 @@ SYCLDeviceManager::SYCLDeviceManager(int& default_device_num, int& num_devices, 
 
   visible_devices.reserve(devices.size());
   for (int id = 0; id < devices.size(); id++)
-    visible_devices.push_back({sycl::context(devices[id]), devices[id]});
+    visible_devices.emplace_back(sycl::context(devices[id]), devices[id]);
 
   const size_t sycl_device_count = visible_devices.size();
   if (num_devices == 0)
@@ -95,8 +105,8 @@ SYCLDeviceManager::SYCLDeviceManager(int& default_device_num, int& num_devices, 
       default_device_num = sycl_default_device_num;
     else if (default_device_num != sycl_default_device_num)
       throw std::runtime_error("Inconsistent assigned SYCL devices with the previous record!");
-    default_device_queue = std::make_unique<sycl::queue>(visible_devices[sycl_default_device_num].context_,
-                                                         visible_devices[sycl_default_device_num].device_);
+    default_device_queue = std::make_unique<sycl::queue>(visible_devices[sycl_default_device_num].get_context(),
+                                                         visible_devices[sycl_default_device_num].get_device());
   }
 #endif
 }
