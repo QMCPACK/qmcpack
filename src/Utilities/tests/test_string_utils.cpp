13096c24c0e586e675e847b00f84392a44bcbd61
Ye Luo <yeluo@anl.gov>
2022-08-12 13:02:54 -0500

    Expand test_string_utils

--- src/Utilities/tests/test_string_utils.cpp
+++ src/Utilities/tests/test_string_utils.cpp
@@ -19,15 +19,29 @@ namespace qmcplusplus
 
 TEST_CASE("string_utils_streamToVec", "[utilities]")
 {
-  std::string test_string("abc def 123");
-  auto str_vec = convertStrToVec<std::string>(test_string);
-  REQUIRE(str_vec.size() == 3);
-  CHECK(str_vec[0] == "abc");
-  CHECK(str_vec[1] == "def");
-  CHECK(str_vec[2] == "123");
+  { // empty string
+    std::string test_string("");
+    auto str_vec = convertStrToVec<std::string>(test_string);
+    REQUIRE(str_vec.size() == 0);
+  }
 
-  // won't work with int
-  CHECK_THROWS_WITH(convertStrToVec<int>(test_string),
-                    Catch::Matchers::Contains("Error parsing string 'abc def 123' for type (type_info::name) i."));
+  { // whitespace string
+    std::string test_string("  ");
+    auto str_vec = convertStrToVec<std::string>(test_string);
+    REQUIRE(str_vec.size() == 0);
+  }
+
+  {
+    std::string test_string("abc def 123");
+    auto str_vec = convertStrToVec<std::string>(test_string);
+    REQUIRE(str_vec.size() == 3);
+    CHECK(str_vec[0] == "abc");
+    CHECK(str_vec[1] == "def");
+    CHECK(str_vec[2] == "123");
+
+    // won't work with int
+    CHECK_THROWS_WITH(convertStrToVec<int>(test_string),
+                      Catch::Matchers::Contains("Error parsing string 'abc def 123' for type (type_info::name) i."));
+  }
 }
 } // namespace qmcplusplus
