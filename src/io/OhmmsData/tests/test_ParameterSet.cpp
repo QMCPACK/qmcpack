30dacdb771560bd2d43133a893266adc6f27ae9f
Ye Luo <yeluo@anl.gov>
2022-08-19 08:38:43 -0500

    Update license header.

--- src/io/OhmmsData/tests/test_ParameterSet.cpp
+++ src/io/OhmmsData/tests/test_ParameterSet.cpp
@@ -2,9 +2,10 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2022 QMCPACK developers.
 //
 // File developed by: Mark Dewing, markdewing@gmail.com, University of Illinois at Urbana-Champaign
+//                    Ye Luo, yeluo@anl.gov, Argonne National Laboratory
 //
 // File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //////////////////////////////////////////////////////////////////////////////////////
