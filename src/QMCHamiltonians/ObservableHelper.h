be181b5887ce36d666e47f6624e03afe267cce8f
Peter Doak <doakpw@ornl.gov>
2022-11-23 14:16:01 -0500

    revert output format change for PairCorrEstimator

--- src/QMCHamiltonians/ObservableHelper.h
+++ src/QMCHamiltonians/ObservableHelper.h
@@ -2,7 +2,7 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2022 QMCPACK developers.
 //
 // File developed by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
@@ -35,7 +35,6 @@ using value_type = QMCTraits::FullPrecRealType;
 class ObservableHelper
 {
 public:
-  ObservableHelper() = delete;
   /**
    * Favored constructor
    * \param[in] title         is the ordered hdf5 group path elements of the observable
