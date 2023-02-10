45952b64a28c6b86f253da5c655a48b3c65b78ef
walshmm <walshmm@ornl.gov>
2023-02-10 12:08:02 -0500

    remove cxxflags, manually specify libxml vars

--- tests/test_automation/github-actions/ci/run_step.sh
+++ tests/test_automation/github-actions/ci/run_step.sh
@@ -111,7 +111,8 @@ case "$1" in
       export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/view/lib
       export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/view/include
       export FFTW_HOME=/opt/view
-      export CXXFLAGS=-isystem\ /opt/view/include
+      export LIBXML2_INCLUDE_DIR=/opt/view/include
+      export LIBXML2_LIBRARY=/opt/view/lib
 
       # Make current environment variables available to subsequent steps
       echo "PATH=/opt/view/bin:/opt/spack/bin:$PATH" >> $GITHUB_ENV
@@ -119,7 +120,8 @@ case "$1" in
       echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/view/lib" >> $GITHUB_ENV
       echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/view/include" >> $GITHUB_ENV
       echo "FFTW_HOME=/opt/view" >> $GITHUB_ENV
-      echo "CXXFLAGS=-isystem\ /opt/view/include" >> $GITHUB_ENV
+      echo "LIBXML2_INCLUDE_DIR=/opt/view/include" >> $GITHUB_ENV
+      echo "LIBXML2_LIBRARY=/opt/view/lib" >> $GITHUB_ENV
     fi
     
     case "${GH_JOBNAME}" in
