c1bf7ecba197cabac4094f951c44122ae40346e9
Shiv Upadhyay <shivnupadhyay@gmail.com>
2023-05-09 20:49:16 -0400

    Fix indexing issue

--- src/QMCWaveFunctions/Jastrow/J1Spin.h
+++ src/QMCWaveFunctions/Jastrow/J1Spin.h
@@ -166,8 +166,8 @@ struct J1Spin : public WaveFunctionComponent
         for (int j = 0; j < NumTargetGroups; j++)
         {
           auto igroup = Ions.getGroupID(i);
-          if (Ions.getGroupID(i) == source_type && j == target_type &&
-              J1UniqueFunctors[i * NumTargetGroups + j] == nullptr)
+          if (igroup == source_type && j == target_type &&
+              J1UniqueFunctors[igroup * NumTargetGroups + j] == nullptr)
             J1UniqueFunctors[igroup * Nelec + j] = std::move(afunc);
         }
     }
