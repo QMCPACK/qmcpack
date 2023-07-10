7a35157b412cd62fd40388c556c65b2026ad2135
Steven Hahn <hahnse@ornl.gov>
2023-07-10 11:30:51 -0400

    Remove unused instantiation
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/QMCDrivers/ContextForSteps.cpp
+++ src/QMCDrivers/ContextForSteps.cpp
@@ -16,6 +16,6 @@ namespace qmcplusplus
 {
 ContextForSteps::ContextForSteps(RandomBase<FullPrecRealType>& random_gen) : random_gen_(random_gen) {}
 
-RandomBase<FullPrecRealType>& ContextForSteps<FullPrecRealType>::get_random_gen() { return random_gen_; }
+RandomBase<FullPrecRealType>& ContextForSteps::get_random_gen() { return random_gen_; }
 
 } // namespace qmcplusplus
