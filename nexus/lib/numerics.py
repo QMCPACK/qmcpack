b2c08978d845625d25615490fa1ea10c677b34b6
Paul Kent <kentpr@ornl.gov>
2023-07-12 11:05:06 -0400

    Use simplices in convex_hull

--- nexus/lib/numerics.py
+++ nexus/lib/numerics.py
@@ -1386,7 +1386,7 @@ def convex_hull(points,dimension=None,tol=None):
             i = ns.index(-1)
             inds = all_inds.copy()
             inds[i] = False
-            v = tri.vertices[ni]
+            v = tri.simplices[ni]
             if have_tol:
                 iv = list(range(d1))
                 iv.pop(i)
