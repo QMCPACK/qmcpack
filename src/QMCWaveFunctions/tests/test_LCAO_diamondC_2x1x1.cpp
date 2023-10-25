44175e1b91dfa45e3d9a439f5efcc6a3755d73b2
Ye Luo <yeluo@anl.gov>
2023-10-24 22:20:18 -0500

    Use mw_update.

--- src/QMCWaveFunctions/tests/test_LCAO_diamondC_2x1x1.cpp
+++ src/QMCWaveFunctions/tests/test_LCAO_diamondC_2x1x1.cpp
@@ -15,7 +15,6 @@
 #include "OhmmsData/Libxml2Doc.h"
 #include "OhmmsPETE/OhmmsMatrix.h"
 #include "Particle/ParticleSet.h"
-#include "Particle/ParticleSetPool.h"
 #include "QMCWaveFunctions/WaveFunctionComponent.h"
 #include "LCAO/LCAOrbitalBuilder.h"
 #include <ResourceCollection.h>
@@ -37,28 +36,22 @@ TEST_CASE("LCAO DiamondC_2x1x1", "[wavefunction]")
   ParticleSet::ParticleLayout lattice;
   lattice.R = {6.7463223, 6.7463223, 0.0, 0.0, 3.37316115, 3.37316115, 3.37316115, 0.0, 3.37316115};
 
-
-  ParticleSetPool ptcl = ParticleSetPool(c);
-  ptcl.setSimulationCell(lattice);
-  auto ions_uptr = std::make_unique<ParticleSet>(ptcl.getSimulationCell());
-  auto elec_uptr = std::make_unique<ParticleSet>(ptcl.getSimulationCell());
-  ParticleSet& ions_(*ions_uptr);
-  ParticleSet& elec_(*elec_uptr);
+  SimulationCell simcell(lattice);
+  ParticleSet ions_(simcell);
 
   ions_.setName("ion0");
-  ptcl.addParticleSet(std::move(ions_uptr));
   ions_.create({4});
   ions_.R[0]           = {0.0, 0.0, 0.0};
   ions_.R[1]           = {1.686580575, 1.686580575, 1.686580575};
   ions_.R[2]           = {3.37316115, 3.37316115, 0.0};
   ions_.R[3]           = {5.059741726, 5.059741726, 1.686580575};
   SpeciesSet& ispecies = ions_.getSpeciesSet();
-  int Cidx             = ispecies.addSpecies("C");
+  const int Cidx       = ispecies.addSpecies("C");
 
   ions_.print(app_log());
 
+  ParticleSet elec_(simcell);
   elec_.setName("elec");
-  ptcl.addParticleSet(std::move(elec_uptr));
   elec_.create({8, 8});
   elec_.R[0] = {0.0, 1.0, 0.0};
   elec_.R[1] = {0.0, 1.1, 0.0};
@@ -66,9 +59,9 @@ TEST_CASE("LCAO DiamondC_2x1x1", "[wavefunction]")
   elec_.R[3] = {0.0, 1.3, 0.0};
 
   SpeciesSet& tspecies         = elec_.getSpeciesSet();
-  int upIdx                    = tspecies.addSpecies("u");
-  int downIdx                  = tspecies.addSpecies("d");
-  int chargeIdx                = tspecies.addAttribute("charge");
+  const int upIdx              = tspecies.addSpecies("u");
+  const int downIdx            = tspecies.addSpecies("d");
+  const int chargeIdx          = tspecies.addAttribute("charge");
   tspecies(chargeIdx, upIdx)   = -1;
   tspecies(chargeIdx, downIdx) = -1;
   // const int ei_table_index = elec_.addTable(ions_);
@@ -76,43 +69,32 @@ TEST_CASE("LCAO DiamondC_2x1x1", "[wavefunction]")
   // diamondC_2x1x1
   // from tests/solids/diamondC_2x1x1-Gaussian_pp/C_Diamond-Twist0.wfj.xml
   const std::string wf_xml_str = R"(
-  <wavefunction name="psi0" target="e">
-    <determinantset type="molecularorbital" name="LCAOBSet" source="ion0" transform="yes" twist="0  0  0" href="C_Diamond.h5" PBCimages="5  5  5">
+    <sposet_collection type="molecularorbital" name="LCAOBSet" source="ion0" transform="yes" twist="0  0  0" href="C_Diamond.h5" PBCimages="5  5  5">
       <basisset name="LCAOBSet" key="GTO" transform="yes">
         <grid type="log" ri="1.e-6" rf="1.e2" npts="1001"/>
       </basisset>
-      <slaterdeterminant>
-        <determinant id="updet" size="8">
-          <occupation mode="ground"/>
-          <coefficient size="116" spindataset="0"/>
-        </determinant>
-        <determinant id="downdet" size="8">
-          <occupation mode="ground"/>
-          <coefficient size="116" spindataset="0"/>
-        </determinant>
-      </slaterdeterminant>
-    </determinantset>
-    </wavefunction>
+      <sposet name="spoud" size="8">
+        <occupation mode="ground"/>
+        <coefficient size="116" spindataset="0"/>
+      </sposet>
+    </sposet_collection>
   )";
   Libxml2Document doc;
   bool okay = doc.parseFromString(wf_xml_str);
   REQUIRE(okay);
 
   xmlNodePtr root       = doc.getRoot();
-  xmlNodePtr detset_xml = xmlFirstElementChild(root);
-  xmlNodePtr bset_xml   = xmlFirstElementChild(detset_xml);
-  xmlNodePtr sdet_xml   = xmlNextElementSibling(bset_xml);
-  xmlNodePtr det_xml    = xmlFirstElementChild(sdet_xml);
+  xmlNodePtr bset_xml   = xmlFirstElementChild(root);
+  xmlNodePtr sposet_xml = xmlNextElementSibling(bset_xml);
 
-  LCAOrbitalBuilder lcaoSet(elec_, ions_, c, detset_xml);
-  auto spo = lcaoSet.createSPOSetFromXML(det_xml);
+  LCAOrbitalBuilder lcaoSet(elec_, ions_, c, root);
+  auto spo = lcaoSet.createSPOSetFromXML(sposet_xml);
   REQUIRE(spo);
   auto& lcao_spos = dynamic_cast<const LCAOrbitalSet&>(*spo);
   CHECK(!lcao_spos.isIdentity());
 
   const int norb = spo->getOrbitalSetSize();
-  std::cout << "norb: " << norb << std::endl;
-
+  app_log() << "norb: " << norb << std::endl;
 
   // test batched interfaces
   const size_t nw = 2;
@@ -121,10 +103,8 @@ TEST_CASE("LCAO DiamondC_2x1x1", "[wavefunction]")
   // interchange positions
   elec_2.R[0] = elec_.R[1];
   elec_2.R[1] = elec_.R[0];
-  elec_.update();
-  elec_2.update();
-  RefVectorWithLeader<ParticleSet> p_list(elec_, {elec_, elec_2});
 
+  RefVectorWithLeader<ParticleSet> p_list(elec_, {elec_, elec_2});
 
   std::unique_ptr<SPOSet> spo_2(spo->makeClone());
   RefVectorWithLeader<SPOSet> spo_list(*spo, {*spo, *spo_2});
@@ -137,6 +117,8 @@ TEST_CASE("LCAO DiamondC_2x1x1", "[wavefunction]")
 
   ResourceCollectionTeamLock<ParticleSet> mw_pset_lock(pset_res, p_list);
   ResourceCollectionTeamLock<SPOSet> mw_sposet_lock(spo_res, spo_list);
+
+  ParticleSet::mw_update(p_list);
 
   // make VPs
   const size_t nvp_                  = 4;
