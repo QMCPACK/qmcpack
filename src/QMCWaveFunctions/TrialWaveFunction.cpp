41e22b79b7dedab21b480b4d205ac28a8221c8fd
Ye Luo <yeluo@anl.gov>
2023-10-30 22:42:21 -0500

    Introduce TWF::findMSD().

--- src/QMCWaveFunctions/TrialWaveFunction.cpp
+++ src/QMCWaveFunctions/TrialWaveFunction.cpp
@@ -24,6 +24,7 @@
 #include "Concurrency/Info.hpp"
 #include "type_traits/ConvertToReal.h"
 #include "NaNguard.h"
+#include "Fermion/MultiSlaterDetTableMethod.h"
 
 namespace qmcplusplus
 {
@@ -104,6 +105,14 @@ const SPOSet& TrialWaveFunction::getSPOSet(const std::string& name) const
   if (spoit == spomap_->end())
     throw std::runtime_error("SPOSet " + name + " cannot be found!");
   return *spoit->second;
+}
+
+std::optional<std::reference_wrapper<MultiSlaterDetTableMethod>> TrialWaveFunction::findMSD() const
+{
+  for (auto& component : Z)
+    if (auto* comp_ptr = dynamic_cast<MultiSlaterDetTableMethod*>(component.get()); comp_ptr)
+      return *comp_ptr;
+  return std::nullopt;
 }
 
 /** return log(|psi|)
@@ -709,7 +718,8 @@ void TrialWaveFunction::mw_calcRatioGrad(const RefVectorWithLeader<TrialWaveFunc
     wf_list[iw].PhaseDiff = std::arg(ratios[iw]);
     NaNguard::checkOneParticleRatio(ratios[iw], "TWF::mw_calcRatioGrad at particle " + std::to_string(iat));
     if (ratios[iw] != PsiValue(0))
-      NaNguard::checkOneParticleGradients(grad_new.grads_positions[iw], "TWF::mw_calcRatioGrad at particle " + std::to_string(iat));
+      NaNguard::checkOneParticleGradients(grad_new.grads_positions[iw],
+                                          "TWF::mw_calcRatioGrad at particle " + std::to_string(iat));
   }
 }
 
