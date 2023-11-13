d205c7d5ed3aa634606136adc9ac4c47bc2cd8fa
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-12 23:12:58 -0800

    Update shm_ptr_with_raw_ptr_dispatch.hpp
    declare stub for uninitialized_copy_n of shm_ptr in AFQMC

--- src/AFQMC/Memory/SharedMemory/shm_ptr_with_raw_ptr_dispatch.hpp
+++ src/AFQMC/Memory/SharedMemory/shm_ptr_with_raw_ptr_dispatch.hpp
@@ -480,6 +480,22 @@ shm_ptr_with_raw_ptr_dispatch<T> uninitialized_copy_n(It1 f, Size n, shm_ptr_wit
   return d + n;
 }
 
+template<class T, class Size, typename It2>
+shm_ptr_with_raw_ptr_dispatch<T> uninitialized_copy_n(shm_ptr_with_raw_ptr_dispatch<T> f, Size n, It2 d)
+{
+  if (n == 0)
+    return d;
+  throw std::runtime_error("shm_ptr uninitialized_copy_n not implemented");
+  // d.wSP_->fence();
+  // using std::uninitialized_copy_n;
+  // if (d.wSP_->get_group().root())
+  //   uninitialized_copy_n(f, n, to_address(d));
+  // d.wSP_->fence();
+  // mpi3::communicator(d.wSP_->get_group(), 0).barrier();
+  return d + n;
+}
+
+
 template<class Alloc, class It1, class Size, typename T>
 shm_ptr_with_raw_ptr_dispatch<T> uninitialized_copy_n(Alloc& a, It1 f, Size n, shm_ptr_with_raw_ptr_dispatch<T> d)
 {
