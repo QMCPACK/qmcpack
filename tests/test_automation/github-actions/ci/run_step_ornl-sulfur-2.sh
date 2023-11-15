859f2812e1ed8e97d4c3b64be9eb02df2139ad30
Ye Luo <yeluo@anl.gov>
2023-11-14 19:36:21 -0600

    Switch to CUDA 12.1 in V100-Clang16-MPI-CUDA-AFQMC-Offload

--- tests/test_automation/github-actions/ci/run_step_ornl-sulfur-2.sh
+++ tests/test_automation/github-actions/ci/run_step_ornl-sulfur-2.sh
@@ -60,9 +60,8 @@ case "$1" in
 
         LLVM_DIR=$HOME/opt/spack/linux-rhel9-cascadelake/gcc-9.4.0/llvm-16.0.2-ltjkfjdu6p2cfcyw3zalz4x5sz5do3cr
         
-        echo "Set PATHs to cuda 11.2 due to a regression bug in 11.6"
-        export PATH=$HOME/opt/cuda/11.2/bin:$PATH
-        export LD_LIBRARY_PATH=$HOME/opt/cuda/11.2/lib64:$LD_LIBRARY_PATH
+        echo "Set PATHs to cuda-12.1"
+        export PATH=/usr/local/cuda-12.1/bin:$PATH
         export OMPI_CC=$LLVM_DIR/bin/clang
         export OMPI_CXX=$LLVM_DIR/bin/clang++
 
@@ -72,7 +71,7 @@ case "$1" in
         echo "OMPI_CC=$OMPI_CC" >> $GITHUB_ENV
         echo "OMPI_CXX=$OMPI_CXX" >> $GITHUB_ENV
 
-        # Confirm that cuda 11.2 gets picked up by the compiler
+        # Confirm that cuda 12.1 gets picked up by the compiler
         $OMPI_CXX -v
 
         cmake -GNinja \
@@ -97,7 +96,6 @@ case "$1" in
 
         echo "Set PATHs to cuda-12.1"
         export PATH=/usr/local/cuda-12.1/bin:$PATH
-        export LD_LIBRARY_PATH=/usr/local/cuda-12.1/bin:$LD_LIBRARY_PATH
 
         # Make current environment variables available to subsequent steps
         echo "PATH=$PATH" >> $GITHUB_ENV
