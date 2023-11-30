0f9123a5b0ac7835c6757860da19c40be920e201
Ye Luo <yeluo@anl.gov>
2023-11-30 17:35:06 -0600

    Fuse createComplexSingle+Double and createRealSingle+Double respectively

--- src/QMCWaveFunctions/BsplineFactory/createBsplineComplex.cpp
+++ src/QMCWaveFunctions/BsplineFactory/createBsplineComplex.cpp
@@ -17,25 +17,28 @@
 
 namespace qmcplusplus
 {
-std::unique_ptr<BsplineReader> createBsplineComplexDouble(EinsplineSetBuilder* e,
-                                                              bool hybrid_rep,
-                                                              const std::string& useGPU)
+/** create a reader which handles complex (double size real) splines, C2R or C2C case
+ *  spline storage and computation precision is ST
+ */
+template<typename ST>
+std::unique_ptr<BsplineReader> createBsplineComplex(EinsplineSetBuilder* e, bool hybrid_rep, const std::string& useGPU)
 {
   using RealType = OHMMS_PRECISION;
   std::unique_ptr<BsplineReader> aReader;
 
 #if defined(QMC_COMPLEX)
-  app_summary() << "    Using complex valued spline SPOs with complex double precision storage (C2C)." << std::endl;
+  app_summary() << "    Using complex valued spline SPOs with complex " << SplineStoragePrecision<ST>::value
+                << " precision storage (C2C)." << std::endl;
   if (CPUOMPTargetSelector::selectPlatform(useGPU) == PlatformKind::OMPTARGET)
   {
     app_summary() << "    Running OpenMP offload code path." << std::endl;
     if (hybrid_rep)
     {
       app_summary() << "    Using hybrid orbital representation." << std::endl;
-      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2COMPTarget<double>>>>(e);
+      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2COMPTarget<ST>>>>(e);
     }
     else
-      aReader = std::make_unique<SplineSetReader<SplineC2COMPTarget<double>>>(e);
+      aReader = std::make_unique<SplineSetReader<SplineC2COMPTarget<ST>>>(e);
   }
   else
   {
@@ -43,23 +46,24 @@ std::unique_ptr<BsplineReader> createBsplineComplexDouble(EinsplineSetBuilder* e
     if (hybrid_rep)
     {
       app_summary() << "    Using hybrid orbital representation." << std::endl;
-      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2C<double>>>>(e);
+      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2C<ST>>>>(e);
     }
     else
-      aReader = std::make_unique<SplineSetReader<SplineC2C<double>>>(e);
+      aReader = std::make_unique<SplineSetReader<SplineC2C<ST>>>(e);
   }
 #else //QMC_COMPLEX
-  app_summary() << "    Using real valued spline SPOs with complex double precision storage (C2R)." << std::endl;
+  app_summary() << "    Using real valued spline SPOs with complex " << SplineStoragePrecision<ST>::value
+                << " precision storage (C2R)." << std::endl;
   if (CPUOMPTargetSelector::selectPlatform(useGPU) == PlatformKind::OMPTARGET)
   {
     app_summary() << "    Running OpenMP offload code path." << std::endl;
     if (hybrid_rep)
     {
       app_summary() << "    Using hybrid orbital representation." << std::endl;
-      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2ROMPTarget<double>>>>(e);
+      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2ROMPTarget<ST>>>>(e);
     }
     else
-      aReader = std::make_unique<SplineSetReader<SplineC2ROMPTarget<double>>>(e);
+      aReader = std::make_unique<SplineSetReader<SplineC2ROMPTarget<ST>>>(e);
   }
   else
   {
@@ -67,13 +71,24 @@ std::unique_ptr<BsplineReader> createBsplineComplexDouble(EinsplineSetBuilder* e
     if (hybrid_rep)
     {
       app_summary() << "    Using hybrid orbital representation." << std::endl;
-      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2R<double>>>>(e);
+      aReader = std::make_unique<HybridRepSetReader<HybridRepCplx<SplineC2R<ST>>>>(e);
     }
     else
-      aReader = std::make_unique<SplineSetReader<SplineC2R<double>>>(e);
+      aReader = std::make_unique<SplineSetReader<SplineC2R<ST>>>(e);
   }
 #endif
-
   return aReader;
 }
+
+std::unique_ptr<BsplineReader> createBsplineComplex(EinsplineSetBuilder* e,
+                                                    bool use_single,
+                                                    bool hybrid_rep,
+                                                    const std::string& useGPU)
+{
+  if (use_single)
+    return createBsplineComplex<float>(e, hybrid_rep, useGPU);
+  else
+    return createBsplineComplex<double>(e, hybrid_rep, useGPU);
+}
+
 } // namespace qmcplusplus
--- src/QMCWaveFunctions/BsplineFactory/createBsplineReader.h
+++ src/QMCWaveFunctions/BsplineFactory/createBsplineReader.h
@@ -22,33 +22,36 @@ namespace qmcplusplus
 struct BsplineReader;
 class EinsplineSetBuilder;
 
+template<typename ST>
+struct SplineStoragePrecision;
+
+template<>
+struct SplineStoragePrecision<float>
+{
+  constexpr static std::string_view value = "single";
+};
+
+template<>
+struct SplineStoragePrecision<double>
+{
+  constexpr static std::string_view value = "double";
+};
+
 /** create a reader which handles complex (double size real) splines, C2R or C2C case
  *  spline storage and computation precision is double
  */
-std::unique_ptr<BsplineReader> createBsplineComplexDouble(EinsplineSetBuilder* e,
-                                                              bool hybrid_rep,
-                                                              const std::string& useGPU);
-
-/** create a reader which handles complex (double size real) splines, C2R or C2C case
- *  spline storage and computation precision is float
- */
-std::unique_ptr<BsplineReader> createBsplineComplexSingle(EinsplineSetBuilder* e,
-                                                              bool hybrid_rep,
-                                                              const std::string& useGPU);
+std::unique_ptr<BsplineReader> createBsplineComplex(EinsplineSetBuilder* e,
+                                                    bool use_single,
+                                                    bool hybrid_rep,
+                                                    const std::string& useGPU);
 
 /** create a reader which handles real splines, R2R case
  *  spline storage and computation precision is double
  */
-std::unique_ptr<BsplineReader> createBsplineRealDouble(EinsplineSetBuilder* e,
-                                                           bool hybrid_rep,
-                                                           const std::string& useGPU);
-
-/** create a reader which handles real splines, R2R case
- *  spline storage and computation precision is float
- */
-std::unique_ptr<BsplineReader> createBsplineRealSingle(EinsplineSetBuilder* e,
-                                                           bool hybrid_rep,
-                                                           const std::string& useGPU);
+std::unique_ptr<BsplineReader> createBsplineReal(EinsplineSetBuilder* e,
+                                                 bool use_single,
+                                                 bool hybrid_rep,
+                                                 const std::string& useGPU);
 
 } // namespace qmcplusplus
 #endif
