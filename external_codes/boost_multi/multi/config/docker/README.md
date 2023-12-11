17e56be667cd883457877dd2a94325e8248e89d8
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-12-11 11:38:26 -0800

    remove multi subtree

--- external_codes/boost_multi/multi/config/docker/README.md
+++
@@ -1,5 +0,0 @@
-# Docker containers for QMCPACK
-
-This directory contains Dockerfile for:
-
-- dependencies: pre-packaged dependencies, used for CI (GitHub Actions)
--- external_codes/boost_multi/multi/src/Containers/MinimalContainers/ConstantSizeMatrix.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "ConstantSizeMatrix.hpp"
-
-namespace qmcplusplus
-{
-template class ConstantSizeMatrix<float>;
-template class ConstantSizeMatrix<double>;
-}
