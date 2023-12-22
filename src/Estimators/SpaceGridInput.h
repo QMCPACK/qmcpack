aaad3d58dcf52f98aa0b4fc80ae9d6b1a508bdf4
Peter Doak <doakpw@ornl.gov>
2023-12-22 16:27:52 -0500

    NESpaceGrid input and tests

---
+++ src/Estimators/SpaceGridInput.h
@@ -0,0 +1,200 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2022 QMCPACK developers.
+//
+// File developed by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+//
+// Some code refactored from: SpaceGrid.h & SpaceGrid.cpp
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef QMCPLUSPLUS_SPACEGRID_INPUT_H
+#define QMCPLUSPLUS_SPACEGRID_INPUT_H
+
+#include <set>
+#include <unordered_map>
+
+#include <Configuration.h>
+#include "InputSection.h"
+#include "EstimatorInput.h"
+#include "ParseGridInput.hpp" //for AxisGrid
+
+namespace qmcplusplus
+{
+namespace testing
+{
+template<typename T>
+class EnergyDensityTests;
+}
+
+class SpaceGrid;
+
+class SpaceGridInput
+{
+public:
+  using Consumer = SpaceGrid;
+  using Real     = double;
+
+  enum class CoordForm
+  {
+    CARTESIAN = 0,
+    CYLINDRICAL,
+    SPHERICAL
+  };
+
+  inline static const std::unordered_map<std::string, std::any> lookup_input_enum_value{{"coord-cartesian",
+                                                                                         CoordForm::CARTESIAN},
+                                                                                        {"coord-cyclindrical",
+                                                                                         CoordForm::CYLINDRICAL},
+                                                                                        {"coord-spherical",
+                                                                                         CoordForm::SPHERICAL}};
+
+  using label_set = std::vector<std::string_view>;
+  // legal labels for each coordinate type.  These are actually effectively enums
+  inline static const label_set ax_cartesian{"x", "y", "z"};
+  inline static const label_set ax_cylindrical{"r", "phi", "z"};
+  inline static const label_set ax_spherical{"r", "phi", "theta"};
+
+  inline static const std::unordered_map<CoordForm, label_set> axes_label_sets{{CoordForm::CARTESIAN, ax_cartesian},
+                                                                               {CoordForm::CYLINDRICAL, ax_cylindrical},
+                                                                               {CoordForm::SPHERICAL, ax_spherical}};
+
+  class SpaceGridAxisInput
+  {
+    class SpaceGridAxisInputSection : public InputSection
+    {
+    public:
+      SpaceGridAxisInputSection()
+      {
+        section_name = "axis";
+        attributes   = {"label", "grid", "p1", "p2", "scale"};
+        strings      = {"label", "p1", "p2"};
+        custom = {"grid"}, reals = {"scale"};
+        required = {"label", "p1"};
+      }
+      void setFromStreamCustom(const std::string& ename, const std::string& name, std::istringstream& svalue) override;
+      SpaceGridAxisInputSection(const SpaceGridAxisInputSection& sgais) = default;
+    };
+
+  public:
+    SpaceGridAxisInput(xmlNodePtr cur);
+    SpaceGridAxisInput(const SpaceGridAxisInput& sgai) = default;
+
+    static std::any makeAxis(xmlNodePtr cur, std::string& value_label)
+    {
+      SpaceGridAxisInput space_grid_axis{cur};
+
+      value_label = "axis";
+      return space_grid_axis;
+    }
+
+    const SpaceGridAxisInputSection& get_input() { return input_section_; }
+
+    std::string get_label() const { return label_; }
+    Real get_scale() const { return scale_; }
+    std::string get_p1() const { return p1_; }
+    std::string get_p2() const { return p2_; }
+    AxisGrid<Real> get_grid() const { return grid_; }
+
+  private:
+    SpaceGridAxisInputSection input_section_;
+    std::string label_ = "";
+    Real scale_        = 1.0;
+    std::string p1_    = "";
+    std::string p2_    = "zero";
+    AxisGrid<Real> grid_;
+  };
+
+  class SpaceGridOriginInput
+  {
+    class SpaceGridOriginInputSection : public InputSection
+    {
+    public:
+      SpaceGridOriginInputSection()
+      {
+        section_name = "origin";
+        attributes   = {"p1", "p2", "fraction"};
+        required     = {"p1"};
+        strings      = {"p1", "p2"};
+        reals        = {"fraction"};
+      }
+      SpaceGridOriginInputSection(const SpaceGridOriginInputSection& sgois) = default;
+    };
+
+  public:
+    SpaceGridOriginInput(xmlNodePtr cur);
+
+    static std::any makeOrigin(xmlNodePtr cur, std::string& value_label)
+    {
+      SpaceGridOriginInput space_grid_origin{cur};
+      value_label = "origin";
+      return space_grid_origin;
+    }
+    const std::string& get_p1() const { return p1_; }
+    const std::string& get_p2() const { return p2_; }
+    const Real get_fraction() const { return fraction_; }
+  private:
+    SpaceGridOriginInputSection input_section_;
+    std::string p1_{"zero"};
+    std::string p2_{""};
+    Real fraction_{0.0};
+  };
+
+  class SpaceGridInputSection : public InputSection
+  {
+  public:
+    SpaceGridInputSection()
+    {
+      section_name = "SpaceGrid";
+      attributes   = {"coord"};
+      enums        = {"coord"};
+      delegates    = {"origin", "axis"};
+      multiple     = {"axis"};
+      registerDelegate("origin", SpaceGridOriginInput::makeOrigin);
+      registerDelegate("axis", SpaceGridAxisInput::makeAxis);
+    }
+    std::any assignAnyEnum(const std::string& name) const override;
+    void checkParticularValidity() override;
+    SpaceGridInputSection(const SpaceGridInputSection& sgis) = default;
+  };
+
+  SpaceGridInput(xmlNodePtr cur);
+  SpaceGridInput(const SpaceGridInput& sgi) = default;
+
+  void checkAxes(std::vector<std::any>& axes);
+
+  CoordForm get_coord_form() const { return coord_form_; }
+  bool isPeriodic() const { return !(coord_form_ == CoordForm::CYLINDRICAL || coord_form_ == CoordForm::SPHERICAL); }
+  const std::array<std::string, OHMMS_DIM>& get_axis_p1s() const { return axis_p1s_; }
+  const std::array<std::string, OHMMS_DIM>& get_axis_p2s() const { return axis_p2s_; }
+
+  const std::array<Real, OHMMS_DIM>& get_axis_scales() const { return axis_scales_; }
+  const std::array<std::string, OHMMS_DIM>& get_axis_labels() const { return axis_labels_; }
+  const std::array<AxisGrid<Real>, OHMMS_DIM>& get_axis_grids() const { return axis_grids_; }
+  const std::string& get_origin_p1() const { return origin_p1_; }
+  const std::string& get_origin_p2() const { return origin_p2_; }
+  Real get_origin_fraction() const { return origin_fraction_; }
+  /** axes_label_set accessor, avoids a bunch of switch statements
+   *  at must be used because std::unordered_map::operator[] can't return a const reference
+   */
+  const label_set& get_axes_label_set() const { return axes_label_sets.at(coord_form_); }
+
+private:
+  SpaceGridInputSection input_section_;
+  CoordForm coord_form_;
+  // origin in optional so this is required.
+  std::string origin_p1_{"zero"};
+  std::string origin_p2_{""};
+  Real origin_fraction_{0.0};
+  std::array<std::string, OHMMS_DIM> axis_labels_;
+  std::array<std::string, OHMMS_DIM> axis_p1s_;
+  std::array<std::string, OHMMS_DIM> axis_p2s_;
+  std::array<Real, OHMMS_DIM> axis_scales_;
+  std::array<AxisGrid<Real>, OHMMS_DIM> axis_grids_;
+};
+
+std::any makeSpaceGridInput(xmlNodePtr, std::string& value_label);
+
+} // namespace qmcplusplus
+#endif
---
+++ src/Estimators/tests/test_NESpaceGrid.cpp
@@ -0,0 +1,363 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2023 QMCPACK developers.
+//
+// File developed by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Lab
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+
+#include "NESpaceGrid.h"
+#include "SpaceGridInput.h"
+#include "NEReferencePoints.h"
+#include "ReferencePointsInput.h"
+#include "ValidReferencePointsInput.h"
+#include "ValidSpaceGridInput.h"
+#include "OhmmsData/Libxml2Doc.h"
+#include "EstimatorTesting.h"
+#include "Particle/tests/MinimalParticlePool.h"
+
+/** \file
+ *  This is a postfacto unit testing written for NESpaceGrid during porting of EnergyDensity
+ *  to the batched version of the estimators.
+ */
+
+namespace qmcplusplus
+{
+
+namespace testing
+{
+template<typename T>
+class NESpaceGridTests
+{
+public:
+  static const auto& getData(const NESpaceGrid& nesg) { return nesg.data_; }
+  static int getBufferStart(const NESpaceGrid& nesg) { return nesg.buffer_start_; }
+  static int getBufferEnd(const NESpaceGrid& nesg) { return nesg.buffer_end_; }
+  static auto* getOdu(const NESpaceGrid& nesg) { return nesg.odu_; }
+};
+
+template<ValidSpaceGridInput::valid VALID>
+class SpaceGridEnv
+{
+public:
+  using Input = ValidSpaceGridInput;
+  SpaceGridEnv(Communicate* comm)
+      : particle_pool_(MinimalParticlePool::make_diamondC_1x1x1(comm)),
+        pset_elec_(*(particle_pool_.getParticleSet("e"))),
+        pset_ions_(*(particle_pool_.getParticleSet("ion")))
+  {
+    // Setup particleset
+    // particle positions must be inside the unit cell
+    pset_elec_.R =
+        ParticleSet::ParticlePos{{1.451870349, 1.381521229, 1.165202269}, {1.244515371, 1.382273176, 1.21105285},
+                                 {0.000459944, 1.329603408, 1.265030556}, {0.748660329, 1.63420622, 1.393637791},
+                                 {0.033228526, 1.391869137, 0.654413566}, {1.114198787, 1.654334594, 0.231075822},
+                                 {1.657151589, 0.883870516, 1.201243939}, {0.97317591, 1.245644974, 0.284564732}};
+
+    Libxml2Document doc;
+    bool okay       = doc.parseFromString(Input::xml[VALID]);
+    xmlNodePtr node = doc.getRoot();
+    sgi_            = std::make_unique<SpaceGridInput>(node);
+    ReferencePointsInput rpi;
+    ref_psets_.push_back(pset_ions_);
+    ref_points_ = std::make_unique<NEReferencePoints>(std::move(rpi), pset_elec_, ref_psets_);
+  }
+
+  UPtr<SpaceGridInput> sgi_;
+  UPtr<NEReferencePoints> ref_points_;
+  RefVector<ParticleSet> ref_psets_;
+  ParticleSetPool particle_pool_;
+  ParticleSet pset_elec_;
+  ParticleSet pset_ions_;
+};
+} // namespace testing
+
+constexpr bool generate_test_data = false;
+using Real                        = double;
+
+TEST_CASE("SpaceGrid::Construction", "[estimators]")
+{
+  using Input = testing::ValidSpaceGridInput;
+  Communicate* comm;
+  comm = OHMMS::Controller;
+
+  testing::SpaceGridEnv<Input::valid::ORIGIN> sge(comm);
+
+  // EnergyDensityEstimator gets this from an enum giving indexes into each offset of SOA buffer.
+  // It is a smell.
+  NESpaceGrid space_grid(*(sge.sgi_), sge.ref_points_->get_points(), 1, false);
+
+  using NES         = testing::NESpaceGridTests<double>;
+  auto buffer_start = NES::getBufferStart(space_grid);
+  auto buffer_end   = NES::getBufferEnd(space_grid);
+  space_grid.write_description(std::cout, std::string(""));
+  auto& sgi = *(sge.sgi_);
+  auto& agr = sgi.get_axis_grids();
+  for (int id = 0; id < OHMMS_DIM; ++id)
+    CHECK(NES::getOdu(space_grid)[id] == agr[id].odu);
+
+  // CHECK(buffer_start == 0);
+  // CHECK(buffer_end == 7999);
+}
+
+TEST_CASE("SpaceGrid::Basic", "[estimators]")
+{
+  using Input = testing::ValidSpaceGridInput;
+  Communicate* comm;
+  comm = OHMMS::Controller;
+  testing::SpaceGridEnv<Input::valid::ORIGIN> sge(comm);
+  int num_values = 3;
+  NESpaceGrid space_grid(*(sge.sgi_), sge.ref_points_->get_points(), num_values, false);
+  using NES         = testing::NESpaceGridTests<double>;
+  auto buffer_start = NES::getBufferStart(space_grid);
+  auto buffer_end   = NES::getBufferEnd(space_grid);
+  space_grid.write_description(std::cout, std::string(""));
+  auto& sgi = *(sge.sgi_);
+  auto& agr = sgi.get_axis_grids();
+  for (int id = 0; id < OHMMS_DIM; ++id)
+    CHECK(NES::getOdu(space_grid)[id] == agr[id].odu);
+  //CHECK(buffer_start == 0);
+  //CHECK(buffer_end == 23999);
+
+  Matrix<Real> values;
+  values.resize(sge.pset_elec_.getTotalNum(), num_values);
+
+  for (int ip = 0; ip < sge.pset_elec_.getTotalNum(); ++ip)
+    for (int iv = 0; iv < num_values; ++iv)
+      values(ip, iv) = ip + 0.1 * iv;
+
+  const int ei_tid = sge.pset_elec_.addTable(sge.pset_ions_);
+  sge.pset_elec_.update();
+  sge.pset_ions_.update();
+
+  std::vector<bool> p_outside(8, false);
+  space_grid.accumulate(sge.pset_elec_.R, values, p_outside, sge.pset_elec_.getDistTableAB(ei_tid));
+
+  // check that what's in data_pool is what is expected.
+  const auto& grid_data = NES::getData(space_grid);
+
+  auto tensorAccessor = [](const auto& grid_data, int i, int j, int k, int iv) {
+    return grid_data[1200 * i + 60 * j + 3 * k + iv];
+  };
+
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 0) == Approx(2.0));
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 1) == Approx(2.1));
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 2) == Approx(2.2));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 0) == Approx(4.0));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 1) == Approx(4.1));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 2) == Approx(4.2));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 0) == Approx(3.0));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 1) == Approx(3.1));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 2) == Approx(3.2));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 0) == Approx(6.0));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 1) == Approx(6.1));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 2) == Approx(6.2));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 0) == Approx(1.0));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 1) == Approx(1.2));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 2) == Approx(1.4));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 0) == Approx(7.0));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 1) == Approx(7.1));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 2) == Approx(7.2));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 0) == Approx(5.0));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 1) == Approx(5.1));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 2) == Approx(5.2));
+  // new pset R's
+  // check again
+  auto min_R =
+      ParticleSet::ParticlePos{{-0.6759092808, 0.835668385, 1.985307097},   {0.09710352868, -0.76751858, -1.89306891},
+                               {-0.5605484247, -0.9578875303, 1.476860642}, {2.585144997, 1.862680197, 3.282609463},
+                               {-0.1961335093, 1.111888766, -0.578481257},  {1.794641614, 1.6000278, -0.9474347234},
+                               {2.157717228, 0.9254754186, 2.263158321},    {1.883366346, 2.136350632, 3.188981533}};
+  sge.pset_elec_.applyMinimumImage(min_R);
+  sge.pset_elec_.R = min_R;
+
+  sge.pset_elec_.update();
+
+  std::vector<bool> p_outside_2(8, false);
+  space_grid.accumulate(sge.pset_elec_.R, values, p_outside_2, sge.pset_elec_.getDistTableAB(ei_tid));
+
+  CHECK(tensorAccessor(grid_data, 1, 13, 15, 0) == Approx(2.0));
+  CHECK(tensorAccessor(grid_data, 1, 13, 15, 1) == Approx(2.1));
+  CHECK(tensorAccessor(grid_data, 1, 13, 15, 2) == Approx(2.2));
+  CHECK(tensorAccessor(grid_data, 2, 6, 7, 0) == Approx(5.0));
+  CHECK(tensorAccessor(grid_data, 2, 6, 7, 1) == Approx(5.1));
+  CHECK(tensorAccessor(grid_data, 2, 6, 7, 2) == Approx(5.2));
+  CHECK(tensorAccessor(grid_data, 4, 0, 11, 1) == Approx(0.1));
+  CHECK(tensorAccessor(grid_data, 4, 0, 11, 2) == Approx(0.2));
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 0) == Approx(2.0));
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 1) == Approx(2.1));
+  CHECK(tensorAccessor(grid_data, 10, 17, 9, 2) == Approx(2.2));
+  CHECK(tensorAccessor(grid_data, 12, 0, 18, 0) == Approx(7.0));
+  CHECK(tensorAccessor(grid_data, 12, 0, 18, 1) == Approx(7.1));
+  CHECK(tensorAccessor(grid_data, 12, 0, 18, 2) == Approx(7.2));
+  CHECK(tensorAccessor(grid_data, 12, 13, 0, 0) == Approx(6.0));
+  CHECK(tensorAccessor(grid_data, 12, 13, 0, 1) == Approx(6.1));
+  CHECK(tensorAccessor(grid_data, 12, 13, 0, 2) == Approx(6.2));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 0) == Approx(4.0));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 1) == Approx(4.1));
+  CHECK(tensorAccessor(grid_data, 12, 15, 7, 2) == Approx(4.2));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 0) == Approx(3.0));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 1) == Approx(3.1));
+  CHECK(tensorAccessor(grid_data, 12, 16, 11, 2) == Approx(3.2));
+  CHECK(tensorAccessor(grid_data, 13, 1, 6, 0) == Approx(1.0));
+  CHECK(tensorAccessor(grid_data, 13, 1, 6, 1) == Approx(1.1));
+  CHECK(tensorAccessor(grid_data, 13, 1, 6, 2) == Approx(1.2));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 0) == Approx(6.0));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 1) == Approx(6.1));
+  CHECK(tensorAccessor(grid_data, 13, 11, 15, 2) == Approx(6.2));
+  CHECK(tensorAccessor(grid_data, 13, 17, 1, 0) == Approx(3.0));
+  CHECK(tensorAccessor(grid_data, 13, 17, 1, 1) == Approx(3.1));
+  CHECK(tensorAccessor(grid_data, 13, 17, 1, 2) == Approx(3.2));
+  CHECK(tensorAccessor(grid_data, 14, 12, 4, 0) == Approx(4.0));
+  CHECK(tensorAccessor(grid_data, 14, 12, 4, 1) == Approx(4.1));
+  CHECK(tensorAccessor(grid_data, 14, 12, 4, 2) == Approx(4.2));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 0) == Approx(1.0));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 1) == Approx(1.2));
+  CHECK(tensorAccessor(grid_data, 14, 13, 13, 2) == Approx(1.4));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 0) == Approx(7.0));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 1) == Approx(7.1));
+  CHECK(tensorAccessor(grid_data, 15, 11, 10, 2) == Approx(7.2));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 0) == Approx(5.0));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 1) == Approx(5.1));
+  CHECK(tensorAccessor(grid_data, 17, 12, 9, 2) == Approx(5.2));
+
+  // hdf_archive file(comm, false);
+  // file.create("space_grid_test.hdf");
+  // //std::vector<size_t> my_indexes;
+  // auto nvalues = buffer_end - buffer_start + 1;
+  // //std::array<hsize_t, 2> ng{1, nvalues};
+  // hdf_path hdf_name{"space_grid_3"};
+  // file.push(hdf_name);
+  // Vector<Real> data;
+  // std::size_t offset = 0;
+  // data.attachReference(grid_data, nvalues);
+  // file.write(data, "value");
+  // file.pop();
+}
+
+TEST_CASE("SpaceGrid::BadPeriodic", "[estimators]")
+{
+  using Input = testing::ValidSpaceGridInput;
+  Communicate* comm;
+  comm = OHMMS::Controller;
+  testing::SpaceGridEnv<Input::valid::ORIGIN> sge(comm);
+  int num_values = 3;
+  NESpaceGrid space_grid(*(sge.sgi_), sge.ref_points_->get_points(), num_values, false);
+
+  using NES         = testing::NESpaceGridTests<double>;
+  auto buffer_start = NES::getBufferStart(space_grid);
+  auto buffer_end   = NES::getBufferEnd(space_grid);
+  space_grid.write_description(std::cout, std::string(""));
+  auto& sgi = *(sge.sgi_);
+  auto& agr = sgi.get_axis_grids();
+  for (int id = 0; id < OHMMS_DIM; ++id)
+    CHECK(NES::getOdu(space_grid)[id] == agr[id].odu);
+  // CHECK(buffer_start == Approx(0));
+  // CHECK(buffer_end == Approx(23999));
+
+  Matrix<Real> values;
+  values.resize(sge.pset_elec_.getTotalNum(), num_values);
+
+  for (int ip = 0; ip < sge.pset_elec_.getTotalNum(); ++ip)
+    for (int iv = 0; iv < num_values; ++iv)
+      values(ip, iv) = ip + 0.1 * iv;
+
+  const int ei_tid = sge.pset_elec_.addTable(sge.pset_ions_);
+  sge.pset_elec_.update();
+  sge.pset_ions_.update();
+
+  // set a position outside of the cell
+  sge.pset_elec_.R[2] = {1.451870349, 4.381521229, 1.165202269};
+
+  std::vector<bool> p_outside(8, false);
+
+  CHECK_THROWS_AS(space_grid.accumulate(sge.pset_elec_.R, values, p_outside, sge.pset_elec_.getDistTableAB(ei_tid)),
+                  std::runtime_error);
+}
+
+TEST_CASE("SpaceGrid::hdf5", "[estimators]")
+{
+  using Input = testing::ValidSpaceGridInput;
+  Communicate* comm;
+  comm = OHMMS::Controller;
+  testing::SpaceGridEnv<Input::valid::ORIGIN> sge(comm);
+  int num_values = 3;
+  NESpaceGrid space_grid(*(sge.sgi_), sge.ref_points_->get_points(), num_values, false);
+  using NES         = testing::NESpaceGridTests<double>;
+  auto buffer_start = NES::getBufferStart(space_grid);
+  auto buffer_end   = NES::getBufferEnd(space_grid);
+  space_grid.write_description(std::cout, std::string(""));
+  auto& sgi = *(sge.sgi_);
+  auto& agr = sgi.get_axis_grids();
+  for (int id = 0; id < OHMMS_DIM; ++id)
+    CHECK(NES::getOdu(space_grid)[id] == agr[id].odu);
+  //CHECK(buffer_start == 0);
+  //CHECK(buffer_end == 23999);
+
+  Matrix<Real> values;
+  values.resize(sge.pset_elec_.getTotalNum(), num_values);
+
+  for (int ip = 0; ip < sge.pset_elec_.getTotalNum(); ++ip)
+    for (int iv = 0; iv < num_values; ++iv)
+      values(ip, iv) = ip + 0.1 * iv;
+
+  const int ei_tid = sge.pset_elec_.addTable(sge.pset_ions_);
+  sge.pset_elec_.update();
+  sge.pset_ions_.update();
+
+  std::vector<bool> p_outside(8, false);
+  space_grid.accumulate(sge.pset_elec_.R, values, p_outside, sge.pset_elec_.getDistTableAB(ei_tid));
+
+  hdf_archive hd;
+  std::string test_file{"spacegrid_test.hdf"};
+  bool okay = hd.create(test_file);
+  REQUIRE(okay);
+
+  std::vector<ObservableHelper> h5desc;
+  space_grid.registerGrid(hd, 0);
+
+  space_grid.write(hd);
+
+  hd.close();
+
+  hdf_archive hd_read;
+  bool okay_read = hd.open(test_file);
+  hd.push("spacegrid1");
+  //hdf5 values always end up as doubles
+  Matrix<double> read_values(1,24000);
+  hd.readEntry(read_values, "value");
+
+  auto tensorAccessor = [](const auto& grid_data, int i, int j, int k, int iv) -> double {
+    return grid_data.data()[1200 * i + 60 * j + 3 * k + iv];
+  };
+
+  auto value = tensorAccessor(read_values, 10, 17, 9, 0);
+  
+  CHECK(tensorAccessor(read_values, 10, 17, 9, 0) == Approx(2.0));
+  CHECK(tensorAccessor(read_values, 10, 17, 9, 1) == Approx(2.1));
+  CHECK(tensorAccessor(read_values, 10, 17, 9, 2) == Approx(2.2));
+  CHECK(tensorAccessor(read_values, 12, 15, 7, 0) == Approx(4.0));
+  CHECK(tensorAccessor(read_values, 12, 15, 7, 1) == Approx(4.1));
+  CHECK(tensorAccessor(read_values, 12, 15, 7, 2) == Approx(4.2));
+  CHECK(tensorAccessor(read_values, 12, 16, 11, 0) == Approx(3.0));
+  CHECK(tensorAccessor(read_values, 12, 16, 11, 1) == Approx(3.1));
+  CHECK(tensorAccessor(read_values, 12, 16, 11, 2) == Approx(3.2));
+  CHECK(tensorAccessor(read_values, 13, 11, 15, 0) == Approx(6.0));
+  CHECK(tensorAccessor(read_values, 13, 11, 15, 1) == Approx(6.1));
+  CHECK(tensorAccessor(read_values, 13, 11, 15, 2) == Approx(6.2));
+  CHECK(tensorAccessor(read_values, 14, 13, 13, 0) == Approx(1.0));
+  CHECK(tensorAccessor(read_values, 14, 13, 13, 1) == Approx(1.2));
+  CHECK(tensorAccessor(read_values, 14, 13, 13, 2) == Approx(1.4));
+  CHECK(tensorAccessor(read_values, 15, 11, 10, 0) == Approx(7.0));
+  CHECK(tensorAccessor(read_values, 15, 11, 10, 1) == Approx(7.1));
+  CHECK(tensorAccessor(read_values, 15, 11, 10, 2) == Approx(7.2));
+  CHECK(tensorAccessor(read_values, 17, 12, 9, 0) == Approx(5.0));
+  CHECK(tensorAccessor(read_values, 17, 12, 9, 1) == Approx(5.1));
+  CHECK(tensorAccessor(read_values, 17, 12, 9, 2) == Approx(5.2));
+
+  /// \todo add additional hdf5 output checks
+}
+} // namespace qmcplusplus
