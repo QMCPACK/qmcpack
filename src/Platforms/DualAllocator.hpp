e4eb1449d1c105df21e9475879872eb2511e8e1b
Ye Luo <yeluo@anl.gov>
2024-04-23 16:41:04 -0500

    attachReference(const DualAllocator& from...

--- src/Platforms/DualAllocator.hpp
+++ src/Platforms/DualAllocator.hpp
@@ -90,7 +90,7 @@ struct DualAllocator : public HostAllocator
     host_ptr_   = nullptr;
   }
 
-  void attachReference(DualAllocator& from, T* from_data, T* ref)
+  void attachReference(const DualAllocator& from, T* from_data, T* ref)
   {
     std::ptrdiff_t ptr_offset = ref - from_data;
     host_ptr_                 = ref;
@@ -121,7 +121,7 @@ struct qmc_allocator_traits<DualAllocator<T, DeviceAllocator, HostAllocator>>
 
   static void fill_n(T* ptr, size_t n, const T& value) { qmc_allocator_traits<HostAllocator>::fill_n(ptr, n, value); }
 
-  static void attachReference(DualAlloc& from, DualAlloc& to, T* from_data, T* ref)
+  static void attachReference(const DualAlloc& from, DualAlloc& to, T* from_data, T* ref)
   {
     to.attachReference(from, from_data, ref);
   }
