e7544be5809326ba894e6d4d99824795c66dc448
Jaron Krogel <krogeljt@ornl.gov>
2024-05-29 09:15:10 -0400

    migrate to cpp, cleanup includes

--- src/Estimators/WalkerTraceManager.cpp
+++ src/Estimators/WalkerTraceManager.cpp
@@ -13,8 +13,11 @@
 
 #include "WalkerTraceManager.h"
 
+#include "OhmmsData/OhmmsElementBase.h"
+#include "OhmmsData/AttributeSet.h"
+
 #include "Particle/Walker.h"
-//#include "Particle/ParticleSet.h"
+#include "Particle/ParticleSet.h"
 #include "QMCWaveFunctions/TrialWaveFunction.h"
 #include "QMCHamiltonians/QMCHamiltonian.h"
 
@@ -47,18 +50,51 @@ void report_buffer(WalkerTraceBuffer<T>& wtb)
   for (size_t r=0;r<b.size(0);++r)
   {
     for (size_t c=0;c<b.size(1);++c)
-    {
       app_log()<<"  "<<b(r,c);
-    }
     app_log()<<"\n";
   }
 }
 
 
 
+///////////////////////////////////
+// WalkerTraceCollector methods  //
+///////////////////////////////////
+
+WalkerTraceCollector::WalkerTraceCollector()
+  : properties_include{"R2Accepted","R2Proposed","LocalEnergy","LocalPotential","Kinetic","ElecElec","ElecIon","LocalECP","NonLocalECP"}
+{
+  state.reset_permissions();
+  energy_index = -1;
+  energies.resize(0);
+  walker_property_int_buffer.label  = "walker_property_int";
+  walker_property_real_buffer.label = "walker_property_real";
+  walker_particle_real_buffer.label = "walker_particle_real";
+}
+
+
+
+void WalkerTraceCollector::set_state(const WalkerTraceState& tms)
+{
+  state = tms;
+  walker_property_int_buffer.verbose  = state.verbose;
+  walker_property_real_buffer.verbose = state.verbose;
+  walker_particle_real_buffer.verbose = state.verbose;
+}
+
+
+void WalkerTraceCollector::startBlock(int nsteps)
+{
+  if(!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceCollector::startBlock " << std::endl;
+  reset_buffers();
+}
+
 
 void WalkerTraceCollector::collect(MCPWalker& walker, ParticleSet& pset, TrialWaveFunction& wfn, QMCHamiltonian& ham)
 {
+  if(!state.traces_active || pset.current_step%state.step_period!=0) return;
+
   //app_log()<<"TraceCollector::collect (step "<<pset.current_step<<")"<<std::endl;
 
   auto& bsi = walker_property_int_buffer;
@@ -133,10 +169,8 @@ void WalkerTraceCollector::collect(MCPWalker& walker, ParticleSet& pset, TrialWa
   bar.collect("L", Ltmp);
   bar.reset_collect();
 
-
   // save the energy of this walker
   energies.push_back((WTraceReal)walker.Properties(0,energy_index));
-
 
   //app_log()<<"  walker_property_int buffer contents:\n";
   //report_buffer(bsi);
@@ -153,4 +187,228 @@ void WalkerTraceCollector::collect(MCPWalker& walker, ParticleSet& pset, TrialWa
 
 }
 
+
+void WalkerTraceCollector::reset_step() 
+{ 
+  energies.resize(0);
 }
+
+
+void WalkerTraceCollector::reset_buffers()
+{
+  if (state.verbose) app_log() << "WalkerTraceCollector::reset_buffers"<<std::endl;
+  walker_property_int_buffer.reset_buffer();
+  walker_property_real_buffer.reset_buffer();
+  walker_particle_real_buffer.reset_buffer();
+}
+
+
+
+
+/////////////////////////////////
+// WalkerTraceManager methods  //
+/////////////////////////////////
+
+WalkerTraceManager::WalkerTraceManager(Communicate* comm)
+{
+  state.reset_permissions();
+  communicator   = comm;
+  
+  registered_hdf = false;
+  
+  // new walker buffers, etc
+  write_particle_data = false;
+  write_min_data      = true;
+  write_max_data      = true;
+  write_med_data      = true;
+  
+  wmin_property_int_buffer.label  = "wmin_property_int";
+  wmin_property_real_buffer.label = "wmin_property_real";
+  wmin_particle_real_buffer.label = "wmin_particle_real";
+  
+  wmax_property_int_buffer.label  = "wmax_property_int";
+  wmax_property_real_buffer.label = "wmax_property_real";
+  wmax_particle_real_buffer.label = "wmax_particle_real";
+  
+  wmed_property_int_buffer.label  = "wmed_property_int";
+  wmed_property_real_buffer.label = "wmed_property_real";
+  wmed_particle_real_buffer.label = "wmed_particle_real";
+}
+
+
+void WalkerTraceManager::put(xmlNodePtr cur, bool allow_traces, std::string series_root)
+{
+  if (state.verbose) app_log()<<"WalkerTraceManager::put"<<std::endl;
+  state.reset_permissions();
+  bool traces_requested     = cur != NULL;
+  bool method_allows_traces = allow_traces;
+  file_root                 = series_root;
+  state.traces_active       = traces_requested && method_allows_traces;
+  if (state.traces_active)
+  {
+    if (omp_get_thread_num() == 0)
+    {
+      app_log() << "\n  WalkerTraceManager::put() " << std::endl;
+      app_log() << "    traces requested      : " << traces_requested << std::endl;
+      app_log() << "    method allows traces  : " << method_allows_traces << std::endl;
+      app_log() << "    traces active         : " << state.traces_active << std::endl;
+      app_log() << std::endl;
+    }
+    //read trace attributes
+    std::string verbose_write = "no";
+    OhmmsAttributeSet attrib;
+    attrib.add(state.step_period, "step_period");
+    attrib.add(verbose_write, "verbose");
+    attrib.put(cur);
+    state.verbose = verbose_write == "yes";
+  }
+}
+
+
+WalkerTraceCollector* WalkerTraceManager::makeCollector()
+{
+  if (state.verbose) app_log() << "WalkerTraceManager::makeCollector " << std::endl;
+  WalkerTraceCollector* tc = new WalkerTraceCollector();
+  tc->set_state(get_state());
+  return tc;
+}
+
+
+void WalkerTraceManager::startRun(int blocks, std::vector<WalkerTraceCollector*>& collectors)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::startRun " << std::endl;
+  check_collectors(collectors);
+  open_file(collectors);
+}
+
+
+void WalkerTraceManager::stopRun()
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::stopRun " << std::endl;
+  close_file();
+}
+
+
+void WalkerTraceManager::stopStep()
+{
+  if(!state.traces_active) return;
+  // find min/max/median walker and collect in buffers
+}
+
+
+void WalkerTraceManager::write_buffers(std::vector<WalkerTraceCollector*>& collectors, int block)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::write_buffers "<<std::endl;
+  write_buffers_hdf(collectors);
+}
+
+
+void WalkerTraceManager::check_collectors(std::vector<WalkerTraceCollector*>& collectors)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::check_collectors" << std::endl;
+  if (collectors.size() > 0)
+  {
+    bool all_same = true;
+    WalkerTraceCollector& ref = *collectors[0];
+    for (int i = 0; i < collectors.size(); ++i)
+    {
+      WalkerTraceCollector& tm = *collectors[i];
+      all_same &= tm.walker_property_int_buffer.same_as(ref.walker_property_int_buffer);
+      all_same &= tm.walker_property_real_buffer.same_as(ref.walker_property_real_buffer);
+      all_same &= tm.walker_particle_real_buffer.same_as(ref.walker_particle_real_buffer);
+    }
+    if (!all_same)
+    {
+      throw std::runtime_error("WalkerTraceManager::check_collectors  trace buffer widths of collectors do not match\n  contiguous write is "
+                               "impossible\n  this was first caused by collectors contributing array traces from identical, but "
+                               "differently named, particlesets such as e, e2, e3 ... (fixed)\n  please check the WalkerTraceManager "
+                               "summaries printed above");
+    }
+  }
+}
+
+
+void WalkerTraceManager::open_file(std::vector<WalkerTraceCollector*>& collectors)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::open_file "<<std::endl;
+  open_hdf_file(collectors);
+}
+
+
+void WalkerTraceManager::close_file()
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::close_file " << std::endl;
+  close_hdf_file();
+}
+
+
+void WalkerTraceManager::open_hdf_file(std::vector<WalkerTraceCollector*>& collectors)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::open_hdf_file " << std::endl;
+  if (collectors.size() == 0) 
+    throw std::runtime_error("WalkerTraceManager::open_hdf_file  no trace collectors exist, cannot open file");
+  int nprocs = communicator->size();
+  int rank   = communicator->rank();
+  std::array<char, 32> ptoken;
+  std::string file_name = file_root;
+  if (nprocs > 1)
+  {
+    int length{0};
+    if (nprocs > 10000)
+      length = std::snprintf(ptoken.data(), ptoken.size(), ".p%05d", rank);
+    else if (nprocs > 1000)
+      length = std::snprintf(ptoken.data(), ptoken.size(), ".p%04d", rank);
+    else
+      length = std::snprintf(ptoken.data(), ptoken.size(), ".p%03d", rank);
+    if (length < 0)
+      throw std::runtime_error("Error generating filename");
+    file_name.append(ptoken.data(), length);
+  }
+  file_name += ".wtraces.h5";
+  if (state.verbose) app_log() << "WalkerTraceManager::open_hdf_file  opening traces hdf file " << file_name << std::endl;
+  hdf_file        = std::make_unique<hdf_archive>();
+  bool successful = hdf_file->create(file_name);
+  if (!successful)
+    throw std::runtime_error("WalkerTraceManager::open_hdf_file  failed to open hdf file " + file_name);
+}
+
+
+void WalkerTraceManager::write_buffers_hdf(std::vector<WalkerTraceCollector*>& collectors)
+{
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::write_buffers_hdf " << std::endl;
+  WalkerTraceCollector& tc_lead = *collectors[0];
+  if(!registered_hdf)
+  {
+    tc_lead.walker_property_int_buffer.register_hdf_data(*hdf_file);
+    tc_lead.walker_property_real_buffer.register_hdf_data(*hdf_file);
+    tc_lead.walker_particle_real_buffer.register_hdf_data(*hdf_file);
+    registered_hdf = true;
+  }
+  for (int ip = 0; ip < collectors.size(); ++ip)
+  {
+    WalkerTraceCollector& tc = *collectors[ip];
+    tc.walker_property_int_buffer.write_hdf(*hdf_file, tc_lead.walker_property_int_buffer.hdf_file_pointer);
+    tc.walker_property_real_buffer.write_hdf(*hdf_file, tc_lead.walker_property_real_buffer.hdf_file_pointer);
+    tc.walker_particle_real_buffer.write_hdf(*hdf_file, tc_lead.walker_particle_real_buffer.hdf_file_pointer);
+  }
+}
+
+
+void WalkerTraceManager::close_hdf_file()
+{ 
+  if (!state.traces_active) return;
+  if (state.verbose) app_log() << "WalkerTraceManager::close_hdf_file " << std::endl;
+  hdf_file.reset(); 
+}
+
+
+
+} // namespace qmcplusplus
--- src/Estimators/WalkerTraceManager.h
+++ src/Estimators/WalkerTraceManager.h
@@ -10,26 +10,14 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 
-#ifndef QMCPLUSPLUS_TRACEMANAGERNEW_H
-#define QMCPLUSPLUS_TRACEMANAGERNEW_H
+#ifndef QMCPLUSPLUS_WALKERTRACEMANAGER_H
+#define QMCPLUSPLUS_WALKERTRACEMANAGER_H
 
 
 #include <Configuration.h>
-#include "OhmmsData/OhmmsElementBase.h"
-#include "OhmmsData/AttributeSet.h"
 #include "OhmmsPETE/OhmmsArray.h"
-#include "Particle/ParticleSet.h"
-#include "Utilities/IteratorUtility.h"
-#include "ModernStringUtils.hpp"
 #include "Message/Communicate.h"
 #include "hdf/hdf_archive.h"
-#include "Concurrency/OpenMP.h"
-
-#include <algorithm>
-#include <array>
-#include <map>
-#include <memory>
-#include <set>
 #include <unordered_set>
 
 
@@ -46,7 +34,6 @@ using WTracePsiVal = WTraceReal;
 #else
 using WTracePsiVal = WTraceComp;
 #endif
-
 
 
 
@@ -355,35 +342,30 @@ struct WalkerTraceBuffer
 
 
 
-
-
-struct TraceManagerState 
+struct WalkerTraceState 
 {
-  bool method_allows_traces;
-  bool streaming_traces;
-  bool writing_traces;
-  int throttle;
+  bool traces_active;
+  int step_period;
   bool verbose;
 
-  TraceManagerState()
+  WalkerTraceState()
   {
     reset_permissions();
-    throttle       = 1;
-    verbose        = false;
+    step_period = 1;
+    verbose     = false;
   }
 
   inline void reset_permissions()
   {
-    method_allows_traces = false;
-    streaming_traces     = false;
-    writing_traces       = false;
-    verbose              = false;
+    traces_active = false;
+    verbose       = false;
   }
 };
 
 
 
 template<class QT, class PT> class Walker;
+class ParticleSet;
 class TrialWaveFunction;
 class QMCHamiltonian;
 using MCPWalker = Walker<QMCTraits, PtclOnLatticeTraits>;
@@ -393,18 +375,17 @@ using MCPWalker = Walker<QMCTraits, PtclOnLatticeTraits>;
 class WalkerTraceCollector
 {
 public:
-
-  TraceManagerState state;
-
-  // new walker buffers
-  std::unordered_set<std::string> properties_include;
-  std::vector<size_t>             property_indices;
-  int energy_index;
-
   std::vector<WTraceReal> energies;
   WalkerTraceBuffer<WTraceInt>  walker_property_int_buffer;
   WalkerTraceBuffer<WTraceReal> walker_property_real_buffer;
   WalkerTraceBuffer<WTraceReal> walker_particle_real_buffer;
+
+  std::unordered_set<std::string> properties_include;
+  std::vector<size_t>             property_indices;
+  int energy_index;
+
+private:
+  WalkerTraceState state;
   
   // temporary (contiguous) storage for awful ParticleAttrib<> quantities
   Array<WTraceReal  , 2> Rtmp;
@@ -412,91 +393,32 @@ public:
   Array<WTracePsiVal, 2> Gtmp;
   Array<WTracePsiVal, 1> Ltmp;
 
+public:
+  WalkerTraceCollector();
 
-  WalkerTraceCollector()
-    : properties_include{"R2Accepted","R2Proposed","LocalEnergy","LocalPotential","Kinetic","ElecElec","ElecIon","LocalECP","NonLocalECP"}
-  {
-    state.reset_permissions();
+  void set_state(const WalkerTraceState& tms);
 
-    // new walker buffers, etc
-    energy_index = -1;
-    energies.resize(0);
-    walker_property_int_buffer.label  = "walker_property_int";
-    walker_property_real_buffer.label = "walker_property_real";
-    walker_particle_real_buffer.label = "walker_particle_real";
-  }
-
-
-  inline void set_state(const TraceManagerState& tms)
-  {
-    // JTK: rename this function as "setState"
-    state = tms;
-    walker_property_int_buffer.verbose  = state.verbose;
-    walker_property_real_buffer.verbose = state.verbose;
-    walker_particle_real_buffer.verbose = state.verbose;
-  }
-
-
-  inline void reset_step()
-  {
-    energies.resize(0);
-  }
-
-
-  inline void reset_buffers()
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceCollector::reset_buffers"<<std::endl;
-    walker_property_int_buffer.reset_buffer();
-    walker_property_real_buffer.reset_buffer();
-    walker_particle_real_buffer.reset_buffer();
-  }
-
-
-  inline void startBlock(int nsteps)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceCollector::startBlock " << std::endl;
-    reset_buffers();
-  }
-
-
-  inline void write_summary(std::string pad = "  ")
-  {
-    std::string pad2 = pad + "  ";
-    app_log() << std::endl;
-    app_log() << pad << "TraceCollector (detailed summary)" << std::endl;
-    app_log() << pad2 << "method_allows_traces    = " << state.method_allows_traces << std::endl;
-    app_log() << pad2 << "streaming_traces        = " << state.streaming_traces << std::endl;
-    app_log() << pad2 << "writing_traces          = " << state.writing_traces << std::endl;
-    app_log() << pad << "end TraceCollector" << std::endl;
-  }
-
-
+  void startBlock(int nsteps);
 
   void collect(MCPWalker& walker, ParticleSet& pset, TrialWaveFunction& wfn, QMCHamiltonian& ham);
 
-  void stopStep()
-  {
-    // find min/max/median walker and collect in buffers
-  }
+  void reset_step();
 
+private:
+  void reset_buffers();
 };
-
-
 
 
 
 class WalkerTraceManager
 {
-public:
-
+private:
   std::string file_root;
   Communicate* communicator;
   std::unique_ptr<hdf_archive> hdf_file;
   bool registered_hdf;
 
-  TraceManagerState state;
+  WalkerTraceState state;
 
   // new walker buffers
   bool write_particle_data;
@@ -516,228 +438,41 @@ public:
   WalkerTraceBuffer<WTraceReal> wmed_property_real_buffer;
   WalkerTraceBuffer<WTraceReal> wmed_particle_real_buffer;
 
+public:
+  WalkerTraceManager(Communicate* comm = 0);
 
-  WalkerTraceManager(Communicate* comm = 0)
-  {
-    state.reset_permissions();
-    communicator   = comm;
+  void put(xmlNodePtr cur, bool allow_traces, std::string series_root);
 
-    registered_hdf = false;
+  inline WalkerTraceState get_state() { return state; }
 
-    // new walker buffers, etc
-    write_particle_data = false;
-    write_min_data      = true;
-    write_max_data      = true;
-    write_med_data      = true;
+  WalkerTraceCollector* makeCollector();
 
-    wmin_property_int_buffer.label  = "wmin_property_int";
-    wmin_property_real_buffer.label = "wmin_property_real";
-    wmin_particle_real_buffer.label = "wmin_particle_real";
+  void startRun(int blocks, std::vector<WalkerTraceCollector*>& collectors);
 
-    wmax_property_int_buffer.label  = "wmax_property_int";
-    wmax_property_real_buffer.label = "wmax_property_real";
-    wmax_particle_real_buffer.label = "wmax_particle_real";
+  void stopRun();
 
-    wmed_property_int_buffer.label  = "wmed_property_int";
-    wmed_property_real_buffer.label = "wmed_property_real";
-    wmed_particle_real_buffer.label = "wmed_particle_real";
-  }
-
-
-  inline TraceManagerState get_state()
-  {
-    return state;
-  }
-
-  inline WalkerTraceCollector* makeCollector()
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::makeCollector " << std::endl;
-    WalkerTraceCollector* tc = new WalkerTraceCollector();
-    tc->set_state(get_state());
-    return tc;
-  }
-
-
-  inline void put(xmlNodePtr cur, bool allow_traces, std::string series_root)
-  {
-    if (state.verbose)
-      app_log()<<"WalkerTraceManager::put"<<std::endl;
-    state.reset_permissions();
-    state.method_allows_traces  = allow_traces;
-    file_root             = series_root;
-    bool traces_requested = cur != NULL;
-    state.streaming_traces      = traces_requested && state.method_allows_traces;
-    if (state.streaming_traces)
-    {
-      if (omp_get_thread_num() == 0)
-      {
-        app_log() << "\n  WalkerTraceManager::put() " << std::endl;
-        app_log() << "    traces requested          : " << traces_requested << std::endl;
-        app_log() << "    method allows traces      : " << state.method_allows_traces << std::endl;
-        app_log() << "    streaming traces          : " << state.streaming_traces << std::endl;
-        app_log() << std::endl;
-      }
-      //read trace attributes
-      std::string verbose_write = "no";
-      OhmmsAttributeSet attrib;
-      attrib.add(state.throttle, "throttle");
-      attrib.add(verbose_write, "verbose");
-      attrib.put(cur);
-      state.verbose = verbose_write == "yes";
-    }
-  }
-
-
-  inline void check_clones(std::vector<WalkerTraceCollector*>& clones)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::check_clones" << std::endl;
-    if (state.writing_traces && clones.size() > 0)
-    {
-      bool all_same = true;
-      WalkerTraceCollector& ref = *clones[0];
-      for (int i = 0; i < clones.size(); ++i)
-      {
-        WalkerTraceCollector& tm = *clones[i];
-        all_same &= tm.walker_property_int_buffer.same_as(ref.walker_property_int_buffer);
-        all_same &= tm.walker_property_real_buffer.same_as(ref.walker_property_real_buffer);
-        all_same &= tm.walker_particle_real_buffer.same_as(ref.walker_particle_real_buffer);
-      }
-      if (!all_same)
-      {
-        for (int i = 0; i < clones.size(); ++i)
-          clones[i]->write_summary();
-        throw std::runtime_error("WalkerTraceManager::check_clones  trace buffer widths of clones do not match\n  contiguous write is "
-                  "impossible\n  this was first caused by clones contributing array traces from identical, but "
-                  "differently named, particlesets such as e, e2, e3 ... (fixed)\n  please check the WalkerTraceManager "
-                  "summaries printed above");
-      }
-    }
-  }
-
+  void stopStep();
 
   //write buffered trace data to file
-  inline void write_buffers(std::vector<WalkerTraceCollector*>& clones, int block)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::write_buffers "<<std::endl;
-    if (state.writing_traces)
-    {
-      write_buffers_hdf(clones);
-    }
-  }
+  void write_buffers(std::vector<WalkerTraceCollector*>& collectors, int block);
 
+private:
+  void check_collectors(std::vector<WalkerTraceCollector*>& collectors);
 
-  inline void open_file(std::vector<WalkerTraceCollector*>& clones)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::open_file "<<std::endl;
-    if (state.writing_traces)
-    {
-      if (state.verbose)
-        clones[0]->write_summary();
-      open_hdf_file(clones);
-    }
-  }
+  void open_file(std::vector<WalkerTraceCollector*>& collectors);
 
-
-  inline void close_file()
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::close_file " << std::endl;
-    if (state.writing_traces)
-      close_hdf_file();
-  }
-
-
-  inline void startRun(int blocks, std::vector<WalkerTraceCollector*>& clones)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::startRun " << std::endl;
-    check_clones(clones);
-    open_file(clones);
-  }
-
-
-  inline void stopRun()
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::stopRun " << std::endl;
-    close_file();
-  }
+  void close_file();
 
   //hdf file operations
-  inline void open_hdf_file(std::vector<WalkerTraceCollector*>& clones)
-  {
-    if (state.verbose) 
-      app_log() << "WalkerTraceManager::open_hdf_file " << std::endl;
-    if (clones.size() == 0)
-      throw std::runtime_error("WalkerTraceManager::open_hdf_file  no trace clones exist, cannot open file");
-    int nprocs = communicator->size();
-    int rank   = communicator->rank();
-    std::array<char, 32> ptoken;
-    std::string file_name = file_root;
-    if (nprocs > 1)
-    {
-      int length{0};
-      if (nprocs > 10000)
-        length = std::snprintf(ptoken.data(), ptoken.size(), ".p%05d", rank);
-      else if (nprocs > 1000)
-        length = std::snprintf(ptoken.data(), ptoken.size(), ".p%04d", rank);
-      else
-        length = std::snprintf(ptoken.data(), ptoken.size(), ".p%03d", rank);
-      if (length < 0)
-        throw std::runtime_error("Error generating filename");
-      file_name.append(ptoken.data(), length);
-    }
-    file_name += ".wtraces.h5";
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::open_hdf_file  opening traces hdf file " << file_name << std::endl;
-    hdf_file        = std::make_unique<hdf_archive>();
-    bool successful = hdf_file->create(file_name);
-    if (!successful)
-      throw std::runtime_error("WalkerTraceManager::open_hdf_file  failed to open hdf file " + file_name);
-    // only clones have active buffers and associated data
-    WalkerTraceCollector& tm = *clones[0];
-  }
+  void open_hdf_file(std::vector<WalkerTraceCollector*>& collectors);
 
+  void write_buffers_hdf(std::vector<WalkerTraceCollector*>& collectors);
 
-  inline void write_buffers_hdf(std::vector<WalkerTraceCollector*>& clones)
-  {
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::write_buffers_hdf " << std::endl;
-    WalkerTraceCollector& tm_lead = *clones[0];
-    if(!registered_hdf)
-    {
-      tm_lead.walker_property_int_buffer.register_hdf_data(*hdf_file);
-      tm_lead.walker_property_real_buffer.register_hdf_data(*hdf_file);
-      tm_lead.walker_particle_real_buffer.register_hdf_data(*hdf_file);
-
-      registered_hdf = true;
-    }
-    for (int ip = 0; ip < clones.size(); ++ip)
-    {
-      WalkerTraceCollector& tm = *clones[ip];
-      tm.walker_property_int_buffer.write_hdf(*hdf_file, tm_lead.walker_property_int_buffer.hdf_file_pointer);
-      tm.walker_property_real_buffer.write_hdf(*hdf_file, tm_lead.walker_property_real_buffer.hdf_file_pointer);
-      tm.walker_particle_real_buffer.write_hdf(*hdf_file, tm_lead.walker_particle_real_buffer.hdf_file_pointer);
-    }
-  }
-
-  inline void close_hdf_file() { 
-    if (state.verbose)
-      app_log() << "WalkerTraceManager::close_hdf_file " << std::endl;
-    hdf_file.reset(); 
-  }
-
+  void close_hdf_file();
 };
 
 
 
-
 } // namespace qmcplusplus
-
-
 
 #endif
