544ec629dd8c5f85bd0a0e5add6ad1676d5333fb
Ye Luo <yeluo@anl.gov>
2024-05-30 23:04:39 -0500

    Update copyright info.

--- src/Platforms/OMPTarget/AccelBLAS_OMPTarget.hpp
+++ src/Platforms/OMPTarget/AccelBLAS_OMPTarget.hpp
@@ -2,11 +2,9 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2021 QMCPACK developers.
+// Copyright (c) 2024 QMCPACK developers.
 //
 // File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
-//                    Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
-// File refactored from: MatrixDelayedUpdateCUDA.h
 //////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef QMCPLUSPLUS_ACCELBLAS_OMPTARGET_H
