cc2b4e53249a3ebba29b80217194ce67141e3d5c
Ye Luo <yeluo@anl.gov>
2024-06-02 17:01:39 -0500

    Create AccelMatrixUpdate wrappers.

---
+++ src/QMCWaveFunctions/detail/AccelMatrixUpdate.hpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2024 QMCPACK developers.
+//
+// File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//
+// File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef QMCPLUSPLUS_ACCELMATRIXUPDATE_H
+#define QMCPLUSPLUS_ACCELMATRIXUPDATE_H
+
+#include "config.h"
+#if defined(ENABLE_CUDA)
+#include "CUDA/AccelMatrixUpdateCUDA.hpp"
+#endif
+//#include "OMPTarget/AccelBLAS_OMPTarget.hpp"
+
+#endif
