ceafe8dd04395f62c0be1503d3dd47b17ad39e5a
Ye Luo <yeluo@anl.gov>
2024-06-01 19:52:52 -0500

    Add gemm_batched wrapper in AccelBLAS

--- src/Platforms/OMPTarget/AccelBLAS_OMPTarget.hpp
+++ src/Platforms/OMPTarget/AccelBLAS_OMPTarget.hpp
@@ -24,89 +24,177 @@ class BLASHandle<PlatformKind::OMPTARGET>
 public:
   ompBLAS::ompBLAS_handle h_ompblas;
 
-  BLASHandle(Queue<PlatformKind::OMPTARGET>& queue) : h_ompblas(0) { }
+  BLASHandle(Queue<PlatformKind::OMPTARGET>& queue) : h_ompblas(0) {}
 };
 
 namespace BLAS
 {
 template<>
 inline void gemm<PlatformKind::OMPTARGET, float>(BLASHandle<PlatformKind::OMPTARGET>& handle,
-                                            const char transa,
-                                            const char transb,
-                                            int m,
-                                            int n,
-                                            int k,
-                                            const float alpha,
-                                            const float* A,
-                                            int lda,
-                                            const float* B,
-                                            int ldb,
-                                            const float beta,
-                                            float* C,
-                                            int ldc)
+                                                 const char transa,
+                                                 const char transb,
+                                                 int m,
+                                                 int n,
+                                                 int k,
+                                                 const float alpha,
+                                                 const float* A,
+                                                 int lda,
+                                                 const float* B,
+                                                 int ldb,
+                                                 const float beta,
+                                                 float* C,
+                                                 int ldc)
 {
-  if(ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
+  if (ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
     throw std::runtime_error("ompBLAS::gemm<float> failed!");
 }
 
 template<>
 inline void gemm<PlatformKind::OMPTARGET, double>(BLASHandle<PlatformKind::OMPTARGET>& handle,
-                                             const char transa,
-                                             const char transb,
-                                             int m,
-                                             int n,
-                                             int k,
-                                             const double alpha,
-                                             const double* A,
-                                             int lda,
-                                             const double* B,
-                                             int ldb,
-                                             const double beta,
-                                             double* C,
-                                             int ldc)
+                                                  const char transa,
+                                                  const char transb,
+                                                  int m,
+                                                  int n,
+                                                  int k,
+                                                  const double alpha,
+                                                  const double* A,
+                                                  int lda,
+                                                  const double* B,
+                                                  int ldb,
+                                                  const double beta,
+                                                  double* C,
+                                                  int ldc)
 {
-  if(ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
+  if (ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
     throw std::runtime_error("ompBLAS::gemm<double> failed!");
 }
 
 template<>
 inline void gemm<PlatformKind::OMPTARGET, std::complex<float>>(BLASHandle<PlatformKind::OMPTARGET>& handle,
-                                                          const char transa,
-                                                          const char transb,
-                                                          int m,
-                                                          int n,
-                                                          int k,
-                                                          const std::complex<float> alpha,
-                                                          const std::complex<float>* A,
-                                                          int lda,
-                                                          const std::complex<float>* B,
-                                                          int ldb,
-                                                          const std::complex<float> beta,
-                                                          std::complex<float>* C,
-                                                          int ldc)
+                                                               const char transa,
+                                                               const char transb,
+                                                               int m,
+                                                               int n,
+                                                               int k,
+                                                               const std::complex<float> alpha,
+                                                               const std::complex<float>* A,
+                                                               int lda,
+                                                               const std::complex<float>* B,
+                                                               int ldb,
+                                                               const std::complex<float> beta,
+                                                               std::complex<float>* C,
+                                                               int ldc)
 {
-  if(ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
+  if (ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
     throw std::runtime_error("ompBLAS::gemm<std::complex<float>> failed!");
 }
 
 template<>
 inline void gemm<PlatformKind::OMPTARGET, std::complex<double>>(BLASHandle<PlatformKind::OMPTARGET>& handle,
-                                                           const char transa,
-                                                           const char transb,
-                                                           int m,
-                                                           int n,
-                                                           int k,
-                                                           const std::complex<double> alpha,
-                                                           const std::complex<double>* A,
-                                                           int lda,
-                                                           const std::complex<double>* B,
-                                                           int ldb,
-                                                           const std::complex<double> beta,
-                                                           std::complex<double>* C,
-                                                           int ldc)
+                                                                const char transa,
+                                                                const char transb,
+                                                                int m,
+                                                                int n,
+                                                                int k,
+                                                                const std::complex<double> alpha,
+                                                                const std::complex<double>* A,
+                                                                int lda,
+                                                                const std::complex<double>* B,
+                                                                int ldb,
+                                                                const std::complex<double> beta,
+                                                                std::complex<double>* C,
+                                                                int ldc)
 {
-  if(ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
+  if (ompBLAS::gemm(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc) != 0)
     throw std::runtime_error("ompBLAS::gemm<std::complex<double>> failed!");
+}
+
+template<>
+inline void gemm_batched<PlatformKind::OMPTARGET, float>(BLASHandle<PlatformKind::OMPTARGET>& handle,
+                                                         const char transa,
+                                                         const char transb,
+                                                         int m,
+                                                         int n,
+                                                         int k,
+                                                         const float* alpha,
+                                                         const float* const A[],
+                                                         int lda,
+                                                         const float* const B[],
+                                                         int ldb,
+                                                         const float* beta,
+                                                         float* const C[],
+                                                         int ldc,
+                                                         int batchCount)
+{
+  if (ompBLAS::gemm_batched(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc,
+                            batchCount) != 0)
+    throw std::runtime_error("ompBLAS::gemm_batched<float> failed!");
+}
+
+template<>
+inline void gemm_batched<PlatformKind::OMPTARGET, double>(BLASHandle<PlatformKind::OMPTARGET>& handle,
+                                                          const char transa,
+                                                          const char transb,
+                                                          int m,
+                                                          int n,
+                                                          int k,
+                                                          const double* alpha,
+                                                          const double* const A[],
+                                                          int lda,
+                                                          const double* const B[],
+                                                          int ldb,
+                                                          const double* beta,
+                                                          double* const C[],
+                                                          int ldc,
+                                                          int batchCount)
+{
+  if (ompBLAS::gemm_batched(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc,
+                            batchCount) != 0)
+    throw std::runtime_error("ompBLAS::gemm_batched<double> failed!");
+}
+
+template<>
+inline void gemm_batched<PlatformKind::OMPTARGET, std::complex<float>>(BLASHandle<PlatformKind::OMPTARGET>& handle,
+                                                                       const char transa,
+                                                                       const char transb,
+                                                                       int m,
+                                                                       int n,
+                                                                       int k,
+                                                                       const std::complex<float>* alpha,
+                                                                       const std::complex<float>* const A[],
+                                                                       int lda,
+                                                                       const std::complex<float>* const B[],
+                                                                       int ldb,
+                                                                       const std::complex<float>* beta,
+                                                                       std::complex<float>* const C[],
+                                                                       int ldc,
+                                                                       int batchCount)
+{
+  if (ompBLAS::gemm_batched(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc,
+                            batchCount) != 0)
+    throw std::runtime_error("ompBLAS::gemm_batched<std::complex<float>> failed!");
+}
+
+template<>
+inline void gemm_batched<PlatformKind::OMPTARGET, std::complex<double>>(BLASHandle<PlatformKind::OMPTARGET>& handle,
+                                                                        const char transa,
+                                                                        const char transb,
+                                                                        int m,
+                                                                        int n,
+                                                                        int k,
+                                                                        const std::complex<double>* alpha,
+                                                                        const std::complex<double>* const A[],
+                                                                        int lda,
+                                                                        const std::complex<double>* const B[],
+                                                                        int ldb,
+                                                                        const std::complex<double>* beta,
+                                                                        std::complex<double>* const C[],
+                                                                        int ldc,
+                                                                        int batchCount)
+{
+  if (ompBLAS::gemm_batched(handle.h_ompblas, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc,
+                            batchCount) != 0)
+    throw std::runtime_error("ompBLAS::gemm_batched<std::complex<double>> failed!");
 }
 
 } // namespace BLAS
