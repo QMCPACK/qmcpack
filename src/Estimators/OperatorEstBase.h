5d7a95fdbb46f04c60b8a83605242f2c39af7f36
Peter Doak <doakpw@ornl.gov>
2024-07-31 10:45:33 -0400

    make getFullDataSize const as it should be

--- src/Estimators/OperatorEstBase.h
+++ src/Estimators/OperatorEstBase.h
@@ -92,7 +92,7 @@ public:
   virtual void startBlock(int steps) = 0;
 
   std::vector<QMCT::RealType>& get_data() { return data_; }
-  virtual std::size_t getFullDataSize() { return data_.size(); }
+  virtual std::size_t getFullDataSize() const { return data_.size(); }
 
   /*** create and tie OperatorEstimator's observable_helper hdf5 wrapper to stat.h5 file
    * @param gid hdf5 group to which the observables belong
