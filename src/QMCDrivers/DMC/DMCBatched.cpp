8757a08655c4f2cf00b4c39ba779216d7c44ae0c
Ye Luo <yeluo@anl.gov>
2024-08-22 11:54:11 -0500

    Remove unnecessary use of std::ref

--- src/QMCDrivers/DMC/DMCBatched.cpp
+++ src/QMCDrivers/DMC/DMCBatched.cpp
@@ -463,8 +463,7 @@ bool DMCBatched::run()
   { // walker initialization
     ScopedTimer local_timer(timers_.init_walkers_timer);
     ParallelExecutor<> section_start_task;
-    section_start_task(crowds_.size(), initialLogEvaluation, std::ref(crowds_), std::ref(step_contexts_),
-                       serializing_crowd_walkers_);
+    section_start_task(crowds_.size(), initialLogEvaluation, crowds_, step_contexts_, serializing_crowd_walkers_);
 
     FullPrecRealType energy, variance;
     population_.measureGlobalEnergyVariance(*myComm, energy, variance);
@@ -511,8 +510,7 @@ bool DMCBatched::run()
 
         dmc_state.step        = step;
         dmc_state.global_step = global_step;
-        crowd_task(crowds_.size(), runDMCStep, dmc_state, timers_, dmc_timers_, std::ref(step_contexts_),
-                   std::ref(crowds_));
+        crowd_task(crowds_.size(), runDMCStep, dmc_state, timers_, dmc_timers_, step_contexts_, crowds_);
 
         {
           const int iter = block * steps_per_block_ + step;
