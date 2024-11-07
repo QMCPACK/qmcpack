QMCPACK/qmcpack

commit eb448c61b4235ae3d65ef6b61950ce8f2886c1b9

Clean up OccupyBands

src/QMCWaveFunctions/BsplineFactory/EinsplineSetBuilderESHDF.fft.cpp
@@ -473,12 +473,9 @@ bool EinsplineSetBuilder::ReadGvectors_ESHDF()
   return hasPsig;
 }
 
-void EinsplineSetBuilder::OccupyBands_ESHDF(int spin, int sortBands, int numOrbs)
+int EinsplineSetBuilder::OccupyBands_ESHDF(hdf_archive& h5, int spin, int sortBands, int numOrbs, std::vector<BandInfo>& bandinfo_set) const
 {
-  if (myComm->rank() != 0)
-    return;
-
-  std::vector<BandInfo>& SortBands(*FullBands[spin]);
+  std::vector<BandInfo>& SortBands(bandinfo_set);
   SortBands.clear(); //??? can exit if SortBands is already made?
   int maxOrbs(0);
   for (int ti = 0; ti < DistinctTwists.size(); ti++)
@@ -488,7 +485,7 @@ void EinsplineSetBuilder::OccupyBands_ESHDF(int spin, int sortBands, int numOrbs
     std::ostringstream ePath;
     ePath << "/electrons/kpoint_" << tindex << "/spin_" << spin << "/eigenvalues";
     std::vector<double> eigvals;
-    H5File.read(eigvals, ePath.str());
+    h5.read(eigvals, ePath.str());
     for (int bi = 0; bi < NumBands; bi++)
     {
       BandInfo band;
@@ -507,9 +504,10 @@ void EinsplineSetBuilder::OccupyBands_ESHDF(int spin, int sortBands, int numOrbs
 
   app_log() << SortBands.size() << " complex-valued orbitals supplied by h5 can be expanded up to " << maxOrbs
             << " SPOs." << std::endl;
+
   if (maxOrbs < numOrbs)
-    myComm->barrier_and_abort("EinsplineSetBuilder::OccupyBands_ESHDF user input requests "
-                              "more orbitals than what the h5 file supplies.");
+    throw std::runtime_error("EinsplineSetBuilder::OccupyBands_ESHDF user input requests "
+                             "more orbitals than what the h5 file supplies.\n");
 
   // Now sort the bands by energy
   if (sortBands == 2)
@@ -680,8 +678,8 @@ void EinsplineSetBuilder::OccupyBands_ESHDF(int spin, int sortBands, int numOrbs
       numOrbs_counter++;
     orbIndex++;
   }
-  NumDistinctOrbitals = orbIndex;
   app_log() << "We will read " << NumDistinctOrbitals << " distinct complex-valued orbitals from h5.\n";
+  return orbIndex;
 }
 
 } // namespace qmcplusplus

src/QMCWaveFunctions/BsplineFactory/EinsplineSetBuilder.h
@@ -209,7 +209,6 @@ public:
   bool TwistPair(PosType a, PosType b) const;
   void TileIons();
   void OccupyBands(int spin, int sortBands, int numOrbs, bool skipChecks = false);
-  void OccupyBands_ESHDF(int spin, int sortBands, int numOrbs);
 
   ////////////////////////////////
   // Atomic orbital information //
@@ -252,9 +251,8 @@ public:
   bool makeRotations;
 
 protected:
-  /** broadcast SortBands
-   * @param N number of state
-   * @param root true if it is the i/o node
+  /** broadcast sorted bands
+   * @param sorted_bands intended set
    */
   void bcastSortedBands(std::vector<BandInfo>& sorted_bands) const;
 
@@ -276,6 +274,16 @@ protected:
   static constexpr int TWISTNUM_NO_INPUT = -9999;
   /// twist_inp[i] <= -9999 to indicate no given input after parsing XML
   static constexpr double TWIST_NO_INPUT = -9999;
+
+private:
+  /** read band info from h5, sort it and decide occupation
+   * @param[in] h5 file to read band info
+   * @param[in] spin the intended spin set
+   * @param[in] numOrbs the number of needed bands
+   * @param[out] bandinfo_set sorted and stored bandinfo
+   * @return the number disinct bands
+   */
+  int OccupyBands_ESHDF(hdf_archive& h5, int spin, int sortBands, int numOrbs, std::vector<BandInfo>& bandinfo_set) const;
 };
 
 } // namespace qmcplusplus

src/QMCWaveFunctions/BsplineFactory/EinsplineSetBuilderCommon.cpp
@@ -613,20 +613,32 @@ void EinsplineSetBuilder::AnalyzeTwists2(const int twist_num_inp, const TinyVect
 
 void EinsplineSetBuilder::OccupyBands(int spin, int sortBands, int numOrbs, bool skipChecks)
 {
-  if (myComm->rank() != 0)
-    return;
   if (spin >= NumSpins && !skipChecks)
   {
-    app_error() << "To developer: User is requesting for orbitals in an invalid spin group " << spin
+    std::ostringstream msg;
+    msg << "To developer: User is requesting for orbitals in an invalid spin group " << spin
                 << ". Current h5 file only contains spin groups " << "[0.." << NumSpins - 1 << "]." << std::endl;
-    app_error() << "To user: Orbital H5 file contains no spin down data and is appropriate only for spin unpolarized "
+    msg << "To user: Orbital H5 file contains no spin down data and is appropriate only for spin unpolarized "
                    "calculations. "
                 << "If this is your intent, please replace 'spindataset=1' with 'spindataset=0' in the input file."
                 << std::endl;
-    abort();
+    myComm->barrier_and_abort(msg.str());
   }
 
-  OccupyBands_ESHDF(spin, sortBands, numOrbs);
+  std::string exception_msg;
+  try {
+    if (myComm->rank() == 0)
+      NumDistinctOrbitals = OccupyBands_ESHDF(H5File, spin, sortBands, numOrbs, *FullBands[spin]);
+  }
+  catch (std::exception& e)
+  {
+    NumDistinctOrbitals = 0;
+    exception_msg = e.what();
+  }
+
+  myComm->bcast(NumDistinctOrbitals);
+  if (NumDistinctOrbitals == 0)
+    myComm->barrier_and_abort(exception_msg);
 }
 
 void EinsplineSetBuilder::bcastSortedBands(std::vector<BandInfo>& sorted_bands) const

src/QMCWaveFunctions/BsplineFactory/EinsplineSetBuilder_createSPOs.cpp
@@ -239,7 +239,6 @@ std::unique_ptr<SPOSet> EinsplineSetBuilder::createSPOSetFromXML(xmlNodePtr cur)
   Timer mytimer;
   mytimer.restart();
   OccupyBands(spinSet, sortBands, numOrbs, skipChecks);
-  myComm->bcast(NumDistinctOrbitals);
   if (spinSet == 0)
     TileIons();
 

src/QMCWaveFunctions/BsplineFactory/EinsplineSpinorSetBuilder.cpp
@@ -162,7 +162,6 @@ std::unique_ptr<SPOSet> EinsplineSpinorSetBuilder::createSPOSetFromXML(xmlNodePt
   Timer mytimer;
   mytimer.restart();
   OccupyBands(spinSet, sortBands, numOrbs, skipChecks);
-  myComm->bcast(NumDistinctOrbitals);
   if (spinSet == 0)
     TileIons();
 
@@ -200,7 +199,6 @@ std::unique_ptr<SPOSet> EinsplineSpinorSetBuilder::createSPOSetFromXML(xmlNodePt
 
   //Make the down spin set.
   OccupyBands(spinSet2, sortBands, numOrbs, skipChecks);
-  myComm->bcast(NumDistinctOrbitals);
   bcastSortedBands(*FullBands[spinSet2]);
   auto bspline_zd_d = MixedSplineReader->create_spline_set(spinSet2, spo_cur);
   bspline_zd_d->finalizeConstruction();

commit b91933c48d1a0cdf7cd3c305d617f925d9aca90d

Fix printing caused by eb448c61b4235ae3d65ef6b61950ce8f2886c1b9

src/QMCWaveFunctions/BsplineFactory/EinsplineSetBuilderESHDF.fft.cpp
@@ -685,7 +685,7 @@ int EinsplineSetBuilder::OccupyBands_ESHDF(hdf_archive& h5,
       numOrbs_counter++;
     orbIndex++;
   }
-  app_log() << "We will read " << NumDistinctOrbitals << " distinct complex-valued orbitals from h5.\n";
+  app_log() << "We will read " << orbIndex << " distinct complex-valued orbitals from h5." << std::endl;
   return orbIndex;
 }
 