25d1149d255781848d24e26aae7ff31b0f7b4367
Paul R. C. Kent <kentpr@ornl.gov>
2024-11-11 13:30:43 -0500

    Missed OpenBLAS update

--- config/build_olcf_frontier_ROCm.sh
+++ config/build_olcf_frontier_ROCm.sh
@@ -17,7 +17,7 @@ unset HIP_PATH # it messed up clang as a HIP compiler.
 module unload cray-libsci
 module load cmake/3.27.9
 module load cray-fftw
-module load openblas/0.3.17-omp
+module load openblas/0.3.26-omp
 module load cray-hdf5-parallel
 
 # edit this line if you are not a member of mat151
