8ce54fc3f5280ea152c5f555685e638e17426a98
Jaron Krogel <krogeljt@ornl.gov>
2025-01-29 15:56:49 -0500

    remove commented code

--- src/Estimators/SelfHealingOverlap.cpp
+++ src/Estimators/SelfHealingOverlap.cpp
@@ -49,11 +49,6 @@ SelfHealingOverlap::SelfHealingOverlap(SelfHealingOverlapInput&& inp_, const Tri
   else if (nmsd==0 and nsd==1)
   {// slater-det wavefunction
     throw std::runtime_error("SelfHealingOverlap: slaterdet wavefunction implementation incomplete");
-    //wf_type = sd_rot_wf;
-    //const SlaterDet& sd = sd_refvec[0];
-    //nparams = sd.myVars.size();
-    //if(nparams==0)
-    //  throw std::runtime_error("SelfHealingOverlap: slaterdet wavefunction has no parameters.\n  Please check that <rotated_sposet/>'s appear in the input file.");
   }
   else
   {
@@ -137,11 +132,6 @@ void SelfHealingOverlap::accumulate(const RefVector<MCPWalker>& walkers,
     {
     throw std::runtime_error("SelfHealingOverlap: slaterdet wavefunction implementation incomplete");
       auto sd_refvec = psi.findSD();
-      //SlaterDet& sd = sd_refvec[0];
-      //if(sd.myVars.size()==0)
-      //  throw std::runtime_error("SelfHealingOverlap: in accumulate, slaterdet has no parameters");
-      //// collect parameter derivatives: (dpsi/dc_i)/psi
-      //sd.evaluateDerivativesWF(pset,sd.myVars,det_ratios);
     }
     else
       throw std::runtime_error("SelfHealingOverlap: impossible branch reached, contact the developers");
