70c3cb90d9812eaa74310ac9d23b6b2da1fe7449
Peter Doak (epd) <doakpw@ornl.gov>
2025-01-31 14:14:21 -0500

    correct decay of mw with toperator for energydensity

--- src/QMCHamiltonians/CoulombPBCAB.h
+++ src/QMCHamiltonians/CoulombPBCAB.h
@@ -153,6 +153,12 @@ public:
                               const std::vector<ListenerVector<RealType>>& ion_listeners) const override;
 
 
+  void mw_evaluatePerParticleWithToperator(const RefVectorWithLeader<OperatorBase>& o_list,
+                                           const RefVectorWithLeader<TrialWaveFunction>& wf_list,
+                                           const RefVectorWithLeader<ParticleSet>& p_list,
+                                           const std::vector<ListenerVector<RealType>>& listeners,
+                                           const std::vector<ListenerVector<RealType>>& ion_listeners) const override;
+
   void evaluateIonDerivs(ParticleSet& P,
                          ParticleSet& ions,
                          TrialWaveFunction& psi,
