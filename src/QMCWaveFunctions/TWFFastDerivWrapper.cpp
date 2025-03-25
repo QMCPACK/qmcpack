d776d801018bc77433ec489dce02e22bc9b9120a
Kevin Gasperich <kevin.gasperich@qubit-pharmaceuticals.com>
2025-03-25 18:56:46 +0000

    (wip) md derivs

--- src/QMCWaveFunctions/TWFFastDerivWrapper.cpp
+++ src/QMCWaveFunctions/TWFFastDerivWrapper.cpp
@@ -240,10 +240,11 @@ void TWFFastDerivWrapper::computeMDDerivative(const std::vector<ValueMatrix>& Mi
                                               const std::vector<ValueMatrix>& dM,
                                               const std::vector<ValueMatrix>& dB,
                                               const std::vector<ValueMatrix>& B,
-                                              const std::vector<ValueMatrix>& Mvirt,
+                                              const std::vector<ValueMatrix>& M,
                                               IndexType id;
+                                              const std::vector<int>& ndet_per_exc_level,
+                                              const std::vector<ValueType>& md_sign,
                                               const std::vector<int>& excdata,
-                                              const std::vector<ValueType>& excsign,
                                               std::vector<ValueType>& dvals) const
 {
   IndexType ndet = excsign.size();
@@ -259,7 +260,9 @@ void TWFFastDerivWrapper::computeMDDerivative(const std::vector<ValueMatrix>& Mi
   // dM     [elec, occ+virt]
   // dB     [elec, occ+virt]
   // B      [elec, virt]
-  // Mvirt  [elec, virt]
+  // M      [elec, occ+virt]
+  // Mvirt  [elec, virt] (subset of M)
+  // Mvirt.data() = M.data() + nOcc (ldim is occ+virt)
 
   // O is all occ orbs; o is occ orbs that appear as holes in exc. list; e is all elecs; v is virt orbs that appear as particles in exc. list
 
@@ -267,134 +270,164 @@ void TWFFastDerivWrapper::computeMDDerivative(const std::vector<ValueMatrix>& Mi
   //     - inv({Minv[o,e].M[e,v]}).{Minv[o,e].dM[e,v] - Minv[o,e].dM[e,O].Minv[O,e].M[e,v]}.inv({Minv[o,e].M[e,v]}).{S} ...
   //     + inv({Minv[o,e].M[e,v]}).{Minv[o,e].dB[e,v] - X[o,e].dM[e,v] - (Minv[o,e].dB[e.O] - X[o,e].dM[e,O]).Minv[O,e].M[e,v] - Minv[o,e].dM[e,O].S[O,v]}
   //   )
-  
-  // S = (Minv[O,e].B[e,v] - X[O,e].M[e,v]) ("M" from paper)
-  
+
+
   // X = Minv[O,e].B[e,O].Minv[O,e]
 
   // a = Minv[O,e].M[e,v] (slice this for alpha from paper)
-  
+  // b = Minv[o,e].dM[e,Ov]
+  // c = Minv[o,e].dB[e,Ov] (first dim O for ref det)
+  // d = X[o,e].dM[e,Ov]    (first dim O for ref det)
+  // f = Minv[O,e].B[e,v]
+  // g = X[O,e].M[e,v]
+  // h = c[o,Ov] - d[o,Ov]  (refdet is tr(h[O,O]) )
+
+  // S = (Minv[O,e].B[e,v] - X[O,e].M[e,v]) ("M" from paper)
+  //   = Minv[O,e].(B[e,v] - B[e,O].Minv[O,e].M[e,v])
+  //   = f[O,v] - g[O,v]
+
   // mat1b  = Minv[o,e].dM[e,O].Minv[O,e].M[e,v]
+  //        = Minv[o,e].dM[e,O].a[O,v]
+  //        = b[o,O].a[O,v]
+
   // mat1   = Minv[o,e].dM[e,v] - mat1b[o,v]
-  
-  // mat2ba = Minv[o,e].dB[e.O] - X[o,e].dM[e,O]
-  // mat2b  = mat2ba[o,O].a[O,v]
+  //        = b[o,v] - mat1b[o,v]
+
+  /// NOTE: also need c[o,v] - d[o,v] for mat2, so can construct (c-d)[o,Ov] and slice later
+  // mat2ba = Minv[o,e].dB[e,O] - X[o,e].dM[e,O]
+  //        = c[o,O] - d[o,O]
+  //        = h[o,O]
+
+  // mat2b  = (Minv[o,e].dB[e,O] - X[o,e].dM[e,O]).a[O,v]
+  //        = h[o,O].a[O,v]
+
   // mat2c  = Minv[o,e].dM[e,O].S[O,v]
+  //        = b[o,O].S[O,v]
+
   // mat2   = Minv[o,e].dB[e,v] - X[o,e].dM[e,v] - mat2b[o,v] - mat2c[o,v]
+  //        = c[o,v] - d[o,v] - mat2b[o,v] - mat2c[o,v]
+  //        = (c-d)[o,v] - (c-d)[o,O].a[O,v] - mat2c[o,v]
+  //        = h[o,v] - h[o,O].a[O,v] - mat2c[o,v]
 
   //  dvals[idet] = dval_ref + tr(-inv({a}).{mat1}.inv({a}).{S} + inv({a}).{mat2})
 
   // dval_ref = tr(Minv[O,e].dB[e,O] - X[O,e].dM[e,O])
+  //          = tr(c[O,O] - d[O,O])
 
+
+  /// FIXME: set these correctly; decide how to index into nocc if not contiguous within nOcc
+  size_t nOcc;               // total occ orbs in refdet (should be same as n_elec)
+  size_t nocc;               // number of orbs that appear as holes in exc. list
+  size_t nvirt;              // should be number of virtuals that appear as particles in exc. list
+  size_t virt_offset = nOcc; // first col of virt orbs in M
+
+  /// NOTE: gemms below are reversed from what is written in comments
+  // a_Ov = Minv.Mvirt
+  // a_Ov.T = (Mvirt.T) . (Minv.T)
+  // Minv,M are row-major, but gemm assumes col-major layout, so reorder args
+  // if we want output to be col-major, we can reverse inputs and use 't','t'
 
   /// FIXME: only keep relevant row/col subsets
 
-  // store this and update as single-elec moves are made
-  // Minv[occ,elec].M[elec,virt]
-  ValueMatrix Minv_Mvirt_ov(nocc, nvirt);
-
-  // Minv,Mvirt are row-major, but gemm assumes col-major layout, so reorder args
-  // Minv_Mvirt_ov.T = (Mvirt.T) . (Minv.T)
-  BLAS::gemm('n', 'n', nvirt, nocc, nelec, 1.0, Mvirt[id].data(), Mvirt[id].cols(), Minv[id].data(), Minv[id].cols(),
-             0.0, Minv_Mvirt_ov.data(), Minv_Mvirt_ov.cols());
-
-  // combine these two, just offset correctly later
-  // Minv[occ,elec].dM[elec,occ]
-  // ValueMatrix Minv_dM_oo(nocc, nocc);
-  // Minv[occ,elec].dM[elec,virt]
-  // ValueMatrix Minv_dM_ov(nocc, nvirt);
-
-  ValueMatrix Minv_dM_on(nocc, nocc + nvirt);
-  BLAS::gemm('n', 'n', nocc + nvirt, nocc, nelec, 1.0, dM[id].data(), dM[id].cols(), Minv[id].data(), Minv[id].cols(),
-             0.0, Minv_dM_on.data(), Minv_dM_on.cols());
+  /// TODO: store this and update as single-elec moves are made
+  // a[O,v] = Minv[O,e].M[e,v]
+  ValueMatrix a_Ov(nOcc, nvirt);
+  BLAS::gemm('n', 'n', nvirt, nOcc, nelec, 1.0, M[id].data() + virt_offset, M[id].cols(), Minv[id].data(),
+             Minv[id].cols(), 0.0, a_Ov.data(), a_Ov.cols());
 
 
-  // Minv[occ,elec].B[elec,virt]
-  ValueMatrix Minv_B_ov(nocc, nvirt);
-  BLAS::gemm('n', 'n', nvirt, nocc, nelec, 1.0, B[id].data(), B[id].cols(), Minv[id].data(), Minv[id].cols(), 0.0,
-             Minv_B_ov.data(), Minv_B_ov.cols());
-
-  // X[occ,elec].M[elec,virt]
-  ValueMatrix X_Mvirt_ov(nocc, nvirt);
-  BLAS::gemm('n', 'n', nvirt, nocc, nelec, 1.0, Mvirt[id].data(), Mvirt[id].cols(), X[id].data(), X[id].cols(), 0.0,
-             X_Mvirt_ov.data(), X_Mvirt_ov.cols());
+  /// FIXME: we only need o for first dim
+  // b[O,Ov] = Minv[O,e].dM[e,Ov]
+  ValueMatrix b_On(nOcc, nOcc + nvirt);
+  BLAS::gemm('n', 'n', nOcc + nvirt, nOcc, nelec, 1.0, dM[id].data(), dM[id].cols(), Minv[id].data(), Minv[id].cols(),
+             0.0, b_On.data(), b_On.cols());
 
 
-  // combine these two
-  // X[occ,elec].dM[elec,occ]
-  // ValueMatrix X_dM_oo(nocc, nocc);
-  // X[occ,elec].dM[elec,virt]
-  // ValueMatrix X_dM_ov(nocc, nvirt);
+  /// FIXME: for MD term, we only need o for first dim (for refdet, we need full O first dim)
+  // c[O,Ov] = Minv[O,e].dB[elec,Ov]
+  ValueMatrix c_On(nOcc, nOcc + nvirt);
+  BLAS::gemm('n', 'n', nOcc + nvirt, nOcc, nelec, 1.0, dB[id].data(), dB[id].cols(), Minv[id].data(), Minv[id].cols(),
+             0.0, c_On.data(), c_On.cols());
 
-  ValueMatrix X_dM_on(nocc, nocc + nvirt);
-  BLAS::gemm('n', 'n', nocc + nvirt, nocc, nelec, 1.0, dM[id].data(), dM[id].cols(), X[id].data(), X[id].cols(), 0.0,
-             X_dM_on.data(), X_dM_on.cols());
+  /// FIXME: for MD term, we only need o for first dim (for refdet, we need full O first dim)
+  // d[O,Ov] = X[O,e].dM[e,Ov]
+  ValueMatrix d_On(nOcc, nOcc + nvirt);
+  BLAS::gemm('n', 'n', nOcc + nvirt, nOcc, nelec, 1.0, dM[id].data(), dM[id].cols(), X[id].data(), X[id].cols(), 0.0,
+             d_On.data(), d_On.cols());
 
-  // combine these two
-  // Minv[occ,elec].dB[elec,occ]
-  // ValueMatrix Minv_dB_oo(nocc, nocc);
-  // Minv[occ,elec].dB[elec,virt]
-  // ValueMatrix Minv_dB_ov(nocc, nvirt);
+  // f[O,v] = Minv[O,e].B[e,v]
+  ValueMatrix f_Ov(nOcc, nvirt);
+  BLAS::gemm('n', 'n', nvirt, nOcc, nelec, 1.0, B[id].data() + virt_offset, B[id].cols(), Minv[id].data(),
+             Minv[id].cols(), 0.0, f_Ov.data(), f_Ov.cols());
 
-  ValueMatrix Minv_dB_on(nocc, nocc + nvirt);
-  BLAS::gemm('n', 'n', nocc + nvirt, nocc, nelec, 1.0, dB[id].data(), dB[id].cols(), Minv[id].data(), Minv[id].cols(),
-             0.0, Minv_dB_on.data(), Minv_dB_on.cols());
+  // g[O,v] = X[O,e].M[e,v]
+  ValueMatrix g_Ov(nOcc, nvirt);
+  BLAS::gemm('n', 'n', nvirt, nOcc, nelec, 1.0, M[id].data() + virt_offset, M[id].cols(), X[id].data(), X[id].cols(),
+             0.0, g_Ov.data(), g_Ov.cols());
+
+  /// FIXME: for MD term, we only need o for first dim (for refdet, we need full O first dim)
+  // h[O,Ov] = c[O,Ov] - d[O,Ov]
+  ValueMatrix h_On(nOcc, nOcc + nvirt);
+  for (size_t i = 0; i < h_On.rows(); i++)
+    for (size_t j = 0; j < h_On.cols(); j++)
+      h_On(i, j) = c_On(i, j) - d_On(i, j);
+
+  // S = (Minv[O,e].B[e,v] - X[O,e].M[e,v]) ("M" from paper)
+  //   = f[O,v] - g[O,v]
+  ValueMatrix S_Ov(nOcc, nvirt);
+  for (size_t i = 0; i < S_Ov.rows(); i++)
+    for (size_t j = 0; j < S_Ov.cols(); j++)
+      S_Ov(i, j) = f_Ov(i, j) - g_Ov(i, j);
 
 
-  // S = (Minv_B[v] - X_Mvirt[v]) ("M" from paper)
-  ValueMatrix S(nocc, nvirt);
-  for (size_t i = 0; i < S.rows(); i++)
-    for (size_t j = 0; j < S.cols(); j++)
-      S(i, j) = Minv_B_ov(i, j) - X_Mvirt_ov(i, j);
-
-  // Minv_dM[o,o].Minv_Mv[o,v]
+  /// FIXME: only need o for first dim
+  /// NOTE: second dim of b is Ov
+  // mat1b[O,v] = b[O,O].a[O,v]
   ValueMatrix mat1b(nocc, nvirt);
-  // mat1b.T = Minv_Mv.T @ Minv_dM.T
-  BLAS::gemm('n', 'n', nvirt, nocc, nocc, 1.0, Minv_Mvirt_ov.data(), Minv_Mvirt_ov.cols(), Minv_dM_on.data(),
-             Minv_dM_on.cols(), 0.0, mat1b.data(), mat1b.cols());
+  BLAS::gemm('n', 'n', nvirt, nOcc, nOcc, 1.0, a_Ov.data(), a_Ov.cols(), b_On.data(), b_On.cols(), 0.0, mat1b.data(),
+             mat1b.cols());
 
-  // Minv_dM[o,v] - Minv_dM[o,o].Minv_Mv[o,v]
-  ValueMatrix mat1(nocc, nvirt);
+  /// FIXME: only need o for first dim
+  /// NOTE: second dim of b is Ov
+  //  mat1[O,v] = b[O,v] - mat1b[O,v]
+  ValueMatrix mat1(nOcc, nvirt);
   for (size_t i = 0; i < mat1.rows(); i++)
     for (size_t j = 0; j < mat1.cols(); j++)
-      mat1(i, j) = Minv_dM_on(i, j) - mat1b(i, j);
+      mat1(i, j) = b_On(i, j + virt_offset) - mat1b(i, j);
 
 
-  // Minv_dB[o,o]-X_dM[o,o]
-  ValueMatrix mat2ba(nocc, nocc);
-  for (size_t i = 0; i < mat2ba.rows(); i++)
-    for (size_t j = 0; j < mat2ba.cols(); j++)
-      mat2ba(i, j) = Minv_dB_on(i, j) - X_dM_on(i, j);
-
-  // mat2ba[o,o].Minv_Mv[o,v]
-  ValueMatrix mat2b(nocc, nvirt);
-  BLAS::gemm('n', 'n', nvirt, nocc, nocc, 1.0, Minv_Mvirt_ov.data(), Minv_Mvirt_ov.cols(), mat2ba.data(), mat2ba.cols(),
-             0.0, mat2b.data(), mat2b.cols());
+  /// FIXME: only need o for first dim
+  // mat2b[O,v] = h[O,O].a[O,v]
+  ValueMatrix mat2b(nOcc, nvirt);
+  BLAS::gemm('n', 'n', nvirt, nOcc, nOcc, 1.0, a_Ov.data(), a_Ov.cols(), h_On.data(), h_On.cols(), 0.0, mat2b.data(),
+             mat2b.cols());
 
 
-  // Minv_dM[o,o].S[o,v]
-  ValueMatrix mat2c(nocc, nvirt);
-  BLAS::gemm('n', 'n', nvirt, nocc, nocc, 1.0, S.data(), S.cols(), Minv_dM_on.data(), Minv_dM_on.cols(), 0.0,
-             mat2c.data(), mat2c.cols());
+  /// FIXME: only need o for first dim
+  // mat2c[O,v] = b[O,O].S[O,v]
+  ValueMatrix mat2c(nOcc, nvirt);
+  BLAS::gemm('n', 'n', nvirt, nOcc, nOcc, 1.0, S_Ov.data(), S_Ov.cols(), b_On.data(), b_On.cols(), 0.0, mat2c.data(),
+             mat2c.cols());
 
-
-  // Minv_dB - X_dM - mat2b - mat2c
-  ValueMatrix mat2(nocc, nvirt);
+  /// FIXME: only need o for first dim
+  // mat2[O,v] = h[O,v] - mat2b[O,v] - mat2c[O,v]
+  ValueMatrix mat2(nOcc, nvirt);
   for (size_t i = 0; i < mat2.rows(); i++)
     for (size_t j = 0; j < mat2.cols(); j++)
-      mat2(i, j) = Minv_dB_on(i, j) - X_dM_on(i, j) - mat2b(i, j) - mat2c(i, j);
-
+      mat2(i, j) = h_On(i, j + virt_offset) - mat2b(i, j) - mat2c(i, j);
 
   // tr(Minv_dB - X_dM)
   ValueType dval_refdet = 0.0;
-  for (size_t i = 0; i < mat2ba.rows(); i++)
+  for (size_t i = 0; i < h_On.rows(); i++)
   {
-    dval_refdet += mat2ba(i, i);
+    dval_refdet += h_On(i, i);
   }
 
 
   // TODO: Eq. 43
-  // dvals[idet] = dval_refdet + tr(-inv(Minv_M[P,Q]).mat1[P,Q].inv(Minv_M[P,Q]).S[P,Q] + inv(Minv_M[P,Q]).mat2[P,Q])
+  // {} signify submatrix corresponding to holes/particles rows/cols
+  //
+  // dvals[idet] = dval_refdet + tr(-inv({a}).{mat1}.inv({a}).{S} + inv({a}).{mat2})
   // build full mats for all required pairs, then select submatrices for each excited det
   // use exc index data to map into full arrays and create [k,k] tables
 
@@ -407,21 +440,43 @@ void TWFFastDerivWrapper::computeMDDerivative(const std::vector<ValueMatrix>& Mi
     data_offset += (*ndets_per_excitation_level_)[ext_level] * (3 * ext_level + 1);
   };
 
+  // dets ordered as ref, all singles, all doubles, all triples, etc.
+  //      shift by ndet_per_exc_level after each exc_level
+  // data is ordered in same way, but each det has several contiguous elements
+  //      each det of exc_level k has (3*k+1) elements: [k, {pos}, {ocp}, {uno}]
+  //      k: exc level
+  //      pos : positions of holes in refdet
+  //      ocp : MO idx of holes
+  //      uno : MO idx of particles
+
   for (size_t iexc_level = 1; iexc_level <= max_ext_level; iexc_level++)
   {
+    size_t ndet_exc = (*ndets_per_excitation_level_)[ext_level];
     // calc vals
-    
+
     // see MultiDiracDeterminant::mw_updateRatios, MultiDiracDeterminant::mw_buildTableMatrix_calculateRatios_impl
     // for examples of excited det idx layout
 
     // get hole/particle idx
-    // take corresponding rows cols of Minv_M, S, mat1, mat2
-    // a = inv(Minv_M[P,Q])
-    // dvals[idet] = dval_refdet + tr(-a.mat1[P,Q].a.S[P,Q] + a.S[P,Q])
-    // dvals[idet] = dval_refdet + tr(a.(I - mat1[P,Q].a).S[P,Q])
-    // dvals[idet] = dval_refdet + tr((I - a.mat1[P,Q]).a.S[P,Q])
+    // take corresponding rows/cols of a, S, mat1, mat2
 
-    // a.S[P,Q] also needed for Eq. 29
+    // a' = inv({a})
+    // dvals[idet] = dval_refdet + tr(-a'.{mat1}.a'.{S} + a'.{mat2})
+    // dvals[idet] = dval_refdet + tr(-a'.({mat1}.a'.{S} - {mat2}))
+
+    // a'.{S} also needed for Eq. 29
+
+    for (size_t idet = 0; idet < ndet_exc; idet++)
+    {
+      std::vector<IndexType> hlist(iexc_level, 0);
+      std::vector<IndexType> plist(iexc_level, 0);
+      size_t excdata_offset = data_offset + idet * (3 * iexc_level + 1);
+      for (size_t i = 0; i < iexc_level; i++)
+      {
+        hlist[i] = excdata[excdata_offset + iexc_level + 1 + i];
+        plist[i] = excdata[excdata_offset + 2 * iexc_level + 1 + i];
+      }
+    }
 
 
     // update offsets
