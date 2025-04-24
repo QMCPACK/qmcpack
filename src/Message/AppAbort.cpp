8993cf804fd63371a321a156625b6e3c0d988dc0
Paul Kent <kentpr@ornl.gov>
2025-04-24 13:30:52 -0400

    Coverage on AppAbort

--- src/Message/AppAbort.cpp
+++ src/Message/AppAbort.cpp
@@ -13,18 +13,35 @@
 #include <iostream>
 #include "config.h"
 
+#ifdef ENABLE_GCOV
+#ifdef __GNUC__
+extern "C" void __gcov_dump();
+#endif
+#else
+#endif
+
 #ifdef HAVE_MPI
 #include <mpi.h>
 
 void breakableAppAbort(const std::string& str_msg)
 {
   std::cerr << str_msg << '\n';
+#ifdef ENABLE_GCOV
+#ifdef __GNUC__
+  __gcov_dump();
+#endif
+#endif
   MPI_Abort(MPI_COMM_WORLD, 1);
 }
 #else
 void breakableAppAbort(const std::string& str_msg)
 {
   std::cerr << str_msg << '\n';
-  exit(1);
+#ifdef ENABLE_GCOV
+#ifdef __GNUC__
+  __gcov_dump();
+#endif
+#endif
+exit(1);
 }
 #endif
