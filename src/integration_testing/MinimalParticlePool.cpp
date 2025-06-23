11281fe92a8616efe134cab121df8821e085da26
Peter Doak <doakpw@ornl.gov>
2025-06-23 11:58:55 -0400

    cleaned up cmake wrt minimal pools

--- src/integration_testing/MinimalParticlePool.cpp
+++ src/integration_testing/MinimalParticlePool.cpp
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2025 QMCPACK developers.
+//
+// File developed by: Peter W. Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
 #include "MinimalParticlePool.h"
 
 namespace qmcplusplus
