422090f249c37d85dce99febd7a1ce1554bf258c
Youngjun Lee <youngjunlee7@gmail.com>
2025-08-10 23:49:33 -0500

    Replace shared `x_part` with direct register load

--- src/Platforms/CUDA/cuBLAS_missing_functions.cu
+++ src/Platforms/CUDA/cuBLAS_missing_functions.cu
@@ -50,7 +50,6 @@ __global__ void gemvT_batched_kernel(const int m, // number of columns in row ma
 
   constexpr int SUM_SIZE = ROWBS * COLBS;
   __shared__ uninitialized_array<T, SUM_SIZE> sum;
-  __shared__ uninitialized_array<T, COLBS> x_part;
 
   const int tid = threadIdx.x;
   for (int i = 0; i < ROWBS; i++)
@@ -67,10 +66,11 @@ __global__ void gemvT_batched_kernel(const int m, // number of columns in row ma
   {
     const int col_id = ib * COLBS + tid;
     if (col_id < m)
-      x_part[tid] = x_iw[col_id * incx];
-    for (int row_id = row_begin; row_id < row_begin + row_max; row_id++)
-      if (col_id < m)
-        sum[(row_id - row_begin) * COLBS + tid] += x_part[tid] * A_iw[row_id * lda + col_id];
+    {
+      const T x = x_iw[col_id * incx];
+      for (int row_id = row_begin; row_id < row_begin + row_max; row_id++)
+        sum[(row_id - row_begin) * COLBS + tid] += x * A_iw[row_id * lda + col_id];
+    }
   }
 
   for (int iend = COLBS / 2; iend > 0; iend /= 2)
