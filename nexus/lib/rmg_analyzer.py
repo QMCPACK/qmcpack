e1ee029aaa57b4a9b32ca23cbc60956ab752ea19
Brock Dyer <brockdyer03@gmail.com>
2025-09-25 10:26:04 -0400

    [Nexus] Import Restructure and Cleanup - `rmg_analyzer.py`
    This is the first pass at Issue #5620, designed to remove unused imports, clean up namespace, and generally improve readability of functions from external packages.
    
    This commit changes only the file `rmg_analyzer.py`.

--- nexus/lib/rmg_analyzer.py
+++ nexus/lib/rmg_analyzer.py
@@ -2,17 +2,15 @@
 ##  (c) Copyright 2020-  by Jaron T. Krogel                     ##
 ##################################################################
 
+
 import os
 import numpy as np
-
-from generic import obj
-from developer import to_str
+from developer import obj, to_str
 from fileio import TextFile
 from unit_converter import convert
-from simulation import SimulationAnalyzer,Simulation
+from simulation import SimulationAnalyzer, Simulation
 from structure import generate_structure
-from rmg_input import RmgInput,rmg_modes
-
+from rmg_input import RmgInput, rmg_modes
 
 
 class RmgAnalyzer(SimulationAnalyzer):
