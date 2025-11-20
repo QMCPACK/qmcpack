75925c9ed56f13d9ef8b0f20f919c022396cf8db
Ye Luo <yeluo@anl.gov>
2025-11-20 16:06:10 -0600

    Remove resetTargetParticleSet in src/Particle/LongRange

--- src/Particle/LongRange/LRRPABFeeHandlerTemp.h
+++ src/Particle/LongRange/LRRPABFeeHandlerTemp.h
@@ -96,10 +96,6 @@ struct LRRPABFeeHandlerTemp : public LRHandlerBase
     LR_rc = Basis.get_rc();
   }
 
-  void resetTargetParticleSet(ParticleSet& ref) override { myFunc.reset(ref); }
-
-  void resetTargetParticleSet(ParticleSet& ref, mRealType rs) { myFunc.reset(ref, rs); }
-
   inline mRealType evaluate(mRealType r, mRealType rinv) const override
   {
     mRealType v = 0.0;
