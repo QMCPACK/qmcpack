4d280aac7bc4c771a4c7a708d603295bc5272307
William F Godoy <williamfgc@yahoo.com>
2025-12-01 17:05:03 -0500

    Add ci configuration
    Test AMDClang compiler on ORNL Nitrogen 2 for OpenMP offload support.

---
+++ tests/test_automation/github-actions/ci/run_step_ornl-nitrogen-2.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Executes pipeline steps for the nitrogen-1 CI jobs
+
+set -x
+HOST_NAME=$(hostname -s)
+
+case "$1" in 
+
+  configure)
+
+    # Make current environment variables available to subsequent steps, ctest
+    echo "PATH=$PATH" >> $GITHUB_ENV
+
+    QMC_DATA_DIR=/scratch/pk7/QMC_DATA_FULL
+
+    if [ -d ${GITHUB_WORKSPACE}/../qmcpack-build ]
+    then
+      echo "Found existing out-of-source build directory ${GITHUB_WORKSPACE}/../qmcpack-build, removing"
+      rm -fr ${GITHUB_WORKSPACE}/../qmcpack-build
+    fi
+
+    echo "Creating new out-of-source build directory ${GITHUB_WORKSPACE}/../qmcpack-build"
+    cd ${GITHUB_WORKSPACE}/.. && mkdir qmcpack-build && cd qmcpack-build
+
+    # Build variants
+    # Real or Complex configuration
+    case "${GH_JOBNAME}" in
+      *"Real"*)
+        echo 'Configure for real build -DQMC_COMPLEX=0'
+        IS_COMPLEX=0
+      ;;
+      *"Complex"*)
+        echo 'Configure for complex build -DQMC_COMPLEX=1'
+        IS_COMPLEX=1
+      ;; 
+    esac
+
+    # Mixed or Non-Mixed (default, full) precision, used with GPU code
+    case "${GH_JOBNAME}" in
+      *"Mixed"*)
+        echo 'Configure for mixed precision build -DQMC_MIXED_PRECISION=1'
+        IS_MIXED_PRECISION=1
+      ;; 
+      *)
+        IS_MIXED_PRECISION=0
+      ;;
+    esac
+       
+    # check the GPU architecture in use
+    whoami
+    groups
+    amdgpu-arch
+
+    echo 'Configure for building OpenMP Offload with AMDClang compilers'
+    cmake -GNinja \
+          -DCMAKE_C_COMPILER=amdclang \
+          -DCMAKE_CXX_COMPILER=amdclang++ \
+          -DQMC_MPI=0 \
+          -DQMC_GPU_ARCHS=gfx90a \
+          -DQMC_COMPLEX=$IS_COMPLEX \
+          -DQMC_MIXED_PRECISION=$IS_MIXED_PRECISION \
+          -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+          -DQMC_DATA=$QMC_DATA_DIR \
+          ${GITHUB_WORKSPACE}
+          
+    ;;
+  
+  build)
+    cd ${GITHUB_WORKSPACE}/../qmcpack-build
+    ninja
+    ;;
+   
+  test)
+    echo "Running deterministic tests"
+    rocm-smi --showdriverversion
+    cd ${GITHUB_WORKSPACE}/../qmcpack-build
+    ctest --output-on-failure -L deterministic -j 32 --timeout 120 --repeat after-timeout:4
+    ;;
+    
+esac
