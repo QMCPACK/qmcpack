99a19be83b98089a578ec40e495caf9c60b97cc4
Jaron Krogel <krogeljt@ornl.gov>
2025-12-31 11:57:19 -0500

    nexus: fix imports in tests and executables

--- nexus/nexus/tests/test_qmcpack_input.py
+++ nexus/nexus/tests/test_qmcpack_input.py
@@ -1,5 +1,5 @@
 
-import versions
+from .. import versions
 from .. import testing
 from ..testing import divert_nexus_log,restore_nexus_log
 from ..testing import value_eq,object_eq,check_object_eq
--- nexus/nexus/tests/test_required_dependencies.py
+++ nexus/nexus/tests/test_required_dependencies.py
@@ -1,13 +1,13 @@
 
 def test_numpy_available():
-    import versions
+    from .. import versions
     assert(versions.numpy_available)
 #end def test_numpy_available
 
 
 # skip this since the rest of the test set actually tells you if it is supported
 #def test_numpy_supported():
-#    import versions
+#    from .. import versions
 #    if versions.numpy_available:
 #        assert(versions.numpy_supported)
 #    #end if
