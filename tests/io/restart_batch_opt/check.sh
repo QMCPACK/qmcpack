b619d7d192faa617947087439042e292722c9952
Paul Kent <kentpr@ornl.gov>
2026-02-20 08:31:56 -0500

    Since loop

--- tests/io/restart_batch_opt/check.sh
+++ tests/io/restart_batch_opt/check.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 echo "========================================================================================"
-echo "First run : qmc_short_batch.s003.scalar.dat"
-cat qmc_short_batch.s003.scalar.dat
+echo "First run : qmc_short_batch.s001.scalar.dat"
+cat qmc_short_batch.s001.scalar.dat
 
 echo "========================================================================================"
-echo "Restart run : qmc_short_batch.s004.scalar.dat"
-cat qmc_short_batch.s004.scalar.dat
+echo "Restart run : qmc_short_batch.s002.scalar.dat"
+cat qmc_short_batch.s002.scalar.dat
 
 echo "========================================================================================"
 echo "comparing the Kinetic ElecElec IonIon LocalECP up to the 7th digit after the decimal"
 
-sed "/#/d" qmc_short_batch.s003.scalar.dat | awk '{printf("%.7e %.7e %.7e %.7e\n",$5,$6,$7,$8)}' > s001
-sed "/#/d" qmc_short_batch.s004.scalar.dat | awk '{printf("%.7e %.7e %.7e %.7e\n",$5,$6,$7,$8)}' > s002
+sed "/#/d" qmc_short_batch.s001.scalar.dat | awk '{printf("%.7e %.7e %.7e %.7e\n",$5,$6,$7,$8)}' > s001
+sed "/#/d" qmc_short_batch.s002.scalar.dat | awk '{printf("%.7e %.7e %.7e %.7e\n",$5,$6,$7,$8)}' > s002
 diff s001 s002
