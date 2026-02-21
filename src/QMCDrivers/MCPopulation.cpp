d37c74d1c85b94b3b408cdf03debbff36098d918
Ye Luo <yeluo@anl.gov>
2026-02-21 17:47:09 -0600

    Restore two lines accidentally removed in MCPopulation.cpp

--- src/QMCDrivers/MCPopulation.cpp
+++ src/QMCDrivers/MCPopulation.cpp
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
 // Copyright (c) 2024 QMCPACK developers.
