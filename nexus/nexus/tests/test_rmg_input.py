ea6a303e64297cbbbabe2c855783dc01b515b848
brockdyer03 <brockdyer03@gmail.com>
2026-04-07 11:01:49 -0400

    Move Nexus testing to Pytest

--- nexus/nexus/tests/test_rmg_input.py
+++ nexus/nexus/tests/test_rmg_input.py
@@ -2,6 +2,8 @@ try:
     import pytest
     from . import NexusTestOrder
     pytestmark = pytest.mark.order(NexusTestOrder.RMG_INPUT)
+    from ..generic import generic_settings
+    generic_settings.raise_error = True
 except ImportError:
     pass
 
