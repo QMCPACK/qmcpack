3be89327aa92ae6b4c733926591c53243b7334a2
brockdyer03 <brockdyer03@gmail.com>
2026-04-21 17:37:41 -0400

    Update `test_nexus_base.py` and `test_nxs_redo.py` to reduce `testing.py` dependency.

--- nexus/nexus/tests/test_nxs_redo.py
+++ nexus/nexus/tests/test_nxs_redo.py
@@ -8,21 +8,21 @@ except ImportError:
     pass
 
 from .. import testing
-from ..testing import create_file,create_path,execute
+from ..testing import execute
 
 
-def test_redo():
-    import os
+def test_redo(tmp_path):
 
-    tpath = testing.setup_unit_test_output_directory('nxs_redo','test_redo')
-    
+    tmp_dir = tmp_path / "test_nxs_redo_output"
+    tmp_dir.mkdir(exist_ok=True)
+
     exe = testing.executable_path('nxs-redo')
 
-    command = '{} {}'.format(exe,tpath)
+    command = '{} {}'.format(exe,tmp_dir)
 
 
     # empty directory
-    assert(os.listdir(tpath)==[])
+    assert(list(tmp_dir.iterdir())==[])
 
     out,err,rc = execute(command)
 
@@ -30,32 +30,32 @@ def test_redo():
 
 
     # directory w/ files, but not nexus simulation directory
-    create_file('qmc.in.xml',tpath)
+    (tmp_dir / "qmc.in.xml").touch()
 
     out,err,rc = execute(command)
 
     assert('no simulation directories found' in out)
 
-    assert(set(os.listdir(tpath))==set(['qmc.in.xml']))
+    assert(set(tmp_dir.iterdir())==set([tmp_dir / 'qmc.in.xml']))
 
 
     # nexus simulation directory
-    create_path('sim_qmc',tpath)
+    (tmp_dir / "sim_qmc").mkdir()
 
-    assert(set(os.listdir(tpath))==set(['qmc.in.xml','sim_qmc']))
+    assert(set(tmp_dir.iterdir())==set([tmp_dir / 'qmc.in.xml', tmp_dir / 'sim_qmc']))
 
     out,err,rc = execute(command)
 
-    assert(set(os.listdir(tpath))==set(['attempt1']))
+    assert(set(tmp_dir.iterdir())==set([tmp_dir / 'attempt1']))
 
 
     # nexus simulation directory w/ prior attempt
-    create_file('qmc.in.xml',tpath)
-    create_path('sim_qmc',tpath)
+    (tmp_dir / "qmc.in.xml").touch()
+    (tmp_dir / "sim_qmc").mkdir()
 
-    assert(set(os.listdir(tpath))==set(['attempt1','qmc.in.xml','sim_qmc']))
+    assert(set(tmp_dir.iterdir())==set([tmp_dir / 'attempt1',tmp_dir / 'qmc.in.xml',tmp_dir / 'sim_qmc']))
 
     out,err,rc = execute(command)
 
-    assert(set(os.listdir(tpath))==set(['attempt1','attempt2']))
+    assert(set(tmp_dir.iterdir())==set([tmp_dir / 'attempt1',tmp_dir / 'attempt2']))
 #end def test_redo
