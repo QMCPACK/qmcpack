c7e8cd51a14a910c63abfa6f135f10f164a0b77e
brockdyer03 <brockdyer03@gmail.com>
2026-04-23 09:44:33 -0400

    First pass at updating old Python `spline2` test code

--- src/spline2/tests/bspline_funcs.py
+++ src/spline2/tests/bspline_funcs.py
@@ -1,86 +1,144 @@
+"""Collection of routines for generating symbolic spline functions
 
+Some of these routines should be common with QMCWavefunctions/tests/gen_bspline_jastrow.py
+"""
 
-from sympy import *
 from collections import defaultdict
 
-# Collection of routines for generating symbolic spline functions
-# Some of these routines should be common with QMCWavefunctions/tests/gen_bspline_jastrow.py
+from sympy import (
+    And,
+    Expr,
+    GreaterThan,
+    IndexedBase,
+    Interval,
+    LessThan,
+    Piecewise,
+    StrictGreaterThan,
+    StrictLessThan,
+    Symbol,
+    bspline_basis_set,
+)
 
-def to_interval(ival):
-    """Convert relational expression to an Interval"""
-    min_val = None
+
+def to_interval(ival: And) -> Interval | None:
+    """Convert relational expression to an Interval."""
+    if not isinstance(ival, And):
+        print("error", ival)
+
+    min_val    = None
     lower_open = False
-    max_val = None
+    max_val    = None
     upper_open = True
-    if isinstance(ival, And):
-        for rel in ival.args:
-            if isinstance(rel, StrictGreaterThan):
+    for rel in ival.args:
+        match rel:
+            case StrictGreaterThan():
                 min_val = rel.args[1]
                 #lower_open = True
-            elif isinstance(rel, GreaterThan):
+            case GreaterThan():
                 min_val = rel.args[1]
                 #lower_open = False
-            elif isinstance(rel, StrictLessThan):
+            case StrictLessThan():
                 max_val = rel.args[1]
                 #upper_open = True
-            elif isinstance(rel, LessThan):
+            case LessThan():
                 max_val = rel.args[1]
                 #upper_open = False
-            else:
-                print('unhandled ',rel)
+            case _:
+                print("unhandled ", rel)
 
-    if min_val == None or max_val == None:
-        print('error',ival)
+    if min_val is None or max_val is None:
+        print("error", ival)
     return Interval(min_val, max_val, lower_open, upper_open)
 
-# Transpose the interval and coefficients
-#  Note that interval [0,1) has the polynomial coefficients found in the einspline code
-#  The other intervals could be shifted, and they would also have the same polynomials
-def transpose_interval_and_coefficients(sym_basis):
+
+def transpose_interval_and_coefficients(
+    sym_basis: list[Piecewise]
+) -> defaultdict[Interval, tuple[int, Expr] | list]:
+    """Transpose the interval and coefficients.
+
+    Parameters
+    ----------
+    sym_basis : list of Piecewise
+        3rd-order bspline basis.
+        (output of ``sympy.bspline_basis_set(d, knots, x)`` with ``d=3``)
+
+    Notes
+    -----
+    The interval [0,1) has the polynomial coefficients found in the
+    einspline code. The other intervals could be shifted, and they would
+    also have the same polynomials.
+    """
     cond_map = defaultdict(list)
 
-    i1 = Interval(0,5, False, False) # interval for evaluation
+    i1 = Interval(0, 5, False, False) # Interval for evaluation
     for idx, s0 in enumerate(sym_basis):
         for expr, cond in s0.args:
-            if cond != True:
-                i2 = to_interval(cond)
-                if not i1.is_disjoint(i2):
-                    cond_map[i2].append( (idx, expr) )
+            if cond == True:
+                continue
+
+            i2 = to_interval(cond)
+            if not i1.is_disjoint(i2):
+                cond_map[i2].append((idx, expr))
     return cond_map
 
-# Create piecewise expression from the transposed intervals
-# basis_map - map of interval to list of spline expressions for that interval
-#         c - coefficient symbol (needs to allow indexing)
-#        xs - symbol for the position variable ('x')
-def recreate_piecewise(basis_map, c, xs):
+
+def recreate_piecewise(basis_map: dict, c: IndexedBase, xs: Symbol) -> Piecewise:
+    """Create piecewise expression from the transposed intervals.
+
+    Parameters
+    ----------
+    basis_map : dict
+        Map of interval to list of spline expressions for that interval.
+    c : IndexedBase
+        Coefficient symbol (needs to allow indexing).
+    xs : Symbol
+        Symbol for the position variable ('x').
+
+    See Also
+    --------
+    transpose_interval_and_coefficients
+    """
+
     args = []
     for cond, exprs in basis_map.items():
         e = 0
         for idx, b in exprs:
             e += c[idx] * b
-        args.append( (e, cond.as_relational(xs)))
+        args.append((e, cond.as_relational(xs)))
     args.append((0, True))
     return Piecewise(*args)
 
 
-# Get the values corresponding to the interval starting at 0
-# basis_map - map of interval to list of spline expressions for that interval
-def get_base_interval(basis_map):
+def get_base_interval(
+    basis_map: dict[Interval, tuple[int, Expr]]
+) -> tuple[int, Expr] | None:
+    """Get the values corresponding to the interval starting at 0.
+
+    Parameters
+    ----------
+    basis_map : dict
+        Map of interval to list of spline expressions for that interval.
+    """
     for cond, exprs in basis_map.items():
         if cond.start == 0:
           return exprs
     return None
 
 
-# Create a set of spline functions
-def create_spline(nknots, xs, c, Delta):
-  Delta = Symbol('Delta', positive=True)
-  all_knots = [i*Delta for i in range(-3, nknots+3)]
+def create_spline(
+    nknots: int,
+    xs: Symbol,
+    c: IndexedBase,
+    Delta: Symbol | None = None,
+) -> Piecewise:
+    """Create a set of spline functions."""
+    if Delta is None:
+        Delta = Symbol('Delta', positive=True)
+    all_knots = [i*Delta for i in range(-3, nknots+3)]
 
-  # Third-order bspline
-  sym_basis = bspline_basis_set(3, all_knots, xs)
-  cond_map = transpose_interval_and_coefficients(sym_basis)
-  spline = recreate_piecewise(cond_map, c, xs)
+    # Third-order bspline
+    sym_basis = bspline_basis_set(3, all_knots, xs)
+    cond_map = transpose_interval_and_coefficients(sym_basis)
+    spline = recreate_piecewise(cond_map, c, xs)
 
-  return spline
-
+    return spline
