ffe9cfb6734ad3285cb269ec374a0cc66a69089f
Brock Dyer <brockdyer03@gmail.com>
2026-04-29 23:15:10 -0400

    Nexus: Continue migrating test code away from `testing.py`

--- nexus/nexus/tests/test_qdens_radial.py
+++ nexus/nexus/tests/test_qdens_radial.py
@@ -9,76 +9,69 @@ except ImportError:
 
 from pathlib import Path
 from .. import versions
-from .. import testing
 from ..testing import execute,text_eq,check_value_eq
 
 
 if versions.spglib_available:
     def test_radial_density():
-        import os
-
-        tpath = testing.setup_unit_test_output_directory('qdens_radial','test_radial_density')
 
         exe = Path(__file__).parent.parent / "bin/qdens-radial"
 
-        qr_vmc_files_path = testing.unit_test_file_path('qdens_radial','diamond_twist/vmc')
-        command = 'rsync -a {} {}'.format(qr_vmc_files_path,tpath)
-        out,err,rc = execute(command)
-        assert(rc==0)
-        vmc_path = os.path.join(tpath,'vmc')
-        vmc_infile = os.path.join(vmc_path,'vmc.g000.twistnum_0.in.xml')
-        assert(os.path.exists(vmc_infile))
-        vmc_infile = os.path.join(vmc_path,'vmc.g001.twistnum_1.in.xml')
-        assert(os.path.exists(vmc_infile))
-        vmc_infile = os.path.join(vmc_path,'vmc.g002.twistnum_2.in.xml')
-        assert(os.path.exists(vmc_infile))
-        vmc_infile = os.path.join(vmc_path,'vmc.g003.twistnum_3.in.xml')
-        assert(os.path.exists(vmc_infile))
+        vmc_path = Path(__file__+"/../test_qdens_radial_files/diamond_twist/vmc").resolve()
 
-        files_bef = '''
-        vmc.avg.s000.SpinDensity_u+d+err.xsf  vmc.avg.s000.SpinDensity_u+d-err.xsf
-        vmc.avg.s000.SpinDensity_u+d.xsf      vmc.g000.twistnum_0.in.xml
-        vmc.g000.s000.scalar.dat              vmc.g001.s000.scalar.dat
-        vmc.g000.twistnum_0.in.g000.qmc       vmc.g001.twistnum_1.in.g001.qmc
-        vmc.g001.twistnum_1.in.xml            vmc.g002.twistnum_2.in.xml
-        vmc.g002.s000.scalar.dat              vmc.g003.s000.scalar.dat
-        vmc.g002.twistnum_2.in.g002.qmc       vmc.g003.twistnum_3.in.g003.qmc
-        vmc.g003.twistnum_3.in.xml            vmc.out
-        vmc.in                                vmc.info.xml
-        '''.split()
+        vmc_files_bef = (
+            vmc_path / "vmc.avg.s000.SpinDensity_u+d+err.xsf",
+            vmc_path / "vmc.avg.s000.SpinDensity_u+d-err.xsf",
+            vmc_path / "vmc.avg.s000.SpinDensity_u+d.xsf",
+            vmc_path / "vmc.g000.s000.scalar.dat",
+            vmc_path / "vmc.g001.s000.scalar.dat",
+            vmc_path / "vmc.g002.s000.scalar.dat",
+            vmc_path / "vmc.g003.s000.scalar.dat",
+            vmc_path / "vmc.g000.twistnum_0.in.g000.qmc",
+            vmc_path / "vmc.g001.twistnum_1.in.g001.qmc",
+            vmc_path / "vmc.g002.twistnum_2.in.g002.qmc",
+            vmc_path / "vmc.g003.twistnum_3.in.g003.qmc",
+            vmc_path / "vmc.g000.twistnum_0.in.xml",
+            vmc_path / "vmc.g001.twistnum_1.in.xml",
+            vmc_path / "vmc.g002.twistnum_2.in.xml",
+            vmc_path / "vmc.g003.twistnum_3.in.xml",
+            vmc_path / "vmc.in",
+            vmc_path / "vmc.out",
+            vmc_path / "vmc.info.xml",
+        )
 
-        qr_dmc_files_path = testing.unit_test_file_path('qdens_radial','diamond_twist/dmc')
-        command = 'rsync -a {} {}'.format(qr_dmc_files_path,tpath)
-        out,err,rc = execute(command)
-        assert(rc==0)
-        dmc_path = os.path.join(tpath,'dmc')
-        dmc_infile = os.path.join(dmc_path,'dmc.g000.twistnum_0.in.xml')
-        assert(os.path.exists(dmc_infile))
-        dmc_infile = os.path.join(dmc_path,'dmc.g001.twistnum_1.in.xml')
-        assert(os.path.exists(dmc_infile))
-        dmc_infile = os.path.join(dmc_path,'dmc.g002.twistnum_2.in.xml')
-        assert(os.path.exists(dmc_infile))
-        dmc_infile = os.path.join(dmc_path,'dmc.g003.twistnum_3.in.xml')
-        assert(os.path.exists(dmc_infile))
+        assert(check_value_eq(set(vmc_path.iterdir()),set(vmc_files_bef)))
 
-        files_bef = '''
-        dmc.avg.s001.SpinDensity_u+d+err.xsf  dmc.avg.s001.SpinDensity_u+d-err.xsf
-        dmc.avg.s001.SpinDensity_u+d.xsf      dmc.g001.s001.scalar.dat                               
-        dmc.g000.s000.scalar.dat              dmc.g000.s001.scalar.dat
-        dmc.g000.twistnum_0.in.g000.qmc       dmc.g001.twistnum_1.in.g001.qmc
-        dmc.g000.twistnum_0.in.xml            dmc.g001.twistnum_1.in.xml     
-        dmc.g001.s000.scalar.dat              dmc.g002.s000.scalar.dat       
-        dmc.g002.s001.scalar.dat              dmc.g003.s001.scalar.dat       
-        dmc.g002.twistnum_2.in.g002.qmc       dmc.g003.twistnum_3.in.g003.qmc
-        dmc.g002.twistnum_2.in.xml            dmc.g003.twistnum_3.in.xml
-        dmc.g003.s000.scalar.dat              dmc.in
-        dmc.out
-        '''.split()
+        dmc_path = Path(__file__+"/../test_qdens_radial_files/diamond_twist/dmc").resolve()
 
-        assert(check_value_eq(set(os.listdir(dmc_path)),set(files_bef)))
+        dmc_files_bef = (
+            dmc_path / "dmc.avg.s001.SpinDensity_u+d+err.xsf",
+            dmc_path / "dmc.avg.s001.SpinDensity_u+d-err.xsf",
+            dmc_path / "dmc.avg.s001.SpinDensity_u+d.xsf",
+            dmc_path / "dmc.g000.s000.scalar.dat",
+            dmc_path / "dmc.g000.s001.scalar.dat",
+            dmc_path / "dmc.g001.s000.scalar.dat",
+            dmc_path / "dmc.g001.s001.scalar.dat",
+            dmc_path / "dmc.g002.s000.scalar.dat",
+            dmc_path / "dmc.g002.s001.scalar.dat",
+            dmc_path / "dmc.g003.s000.scalar.dat",
+            dmc_path / "dmc.g003.s001.scalar.dat",
+            dmc_path / "dmc.g000.twistnum_0.in.g000.qmc",
+            dmc_path / "dmc.g001.twistnum_1.in.g001.qmc",
+            dmc_path / "dmc.g002.twistnum_2.in.g002.qmc",
+            dmc_path / "dmc.g003.twistnum_3.in.g003.qmc",
+            dmc_path / "dmc.g000.twistnum_0.in.xml",
+            dmc_path / "dmc.g001.twistnum_1.in.xml",
+            dmc_path / "dmc.g002.twistnum_2.in.xml",
+            dmc_path / "dmc.g003.twistnum_3.in.xml",
+            dmc_path / "dmc.in",
+            dmc_path / "dmc.out",
+        )
+
+        assert(check_value_eq(set(dmc_path.iterdir()),set(dmc_files_bef)))
 
         # VMC non-cumulative
-        command = '{0} -s C -r 0.6 {1}/vmc.avg.s000.SpinDensity_u+d.xsf'.format(exe,vmc_path)
+        command = f'{exe} -s C -r 0.6 {vmc_path}/vmc.avg.s000.SpinDensity_u+d.xsf'
         out,err,rc = execute(command)
 
         # Assert that return code is 0
@@ -93,7 +86,7 @@ Non-Cumulative Value of C Species at Cutoff 0.6 is: 4.77326491
         assert(text_eq(out,out_ref))
 
         # VMC cumulative
-        command = '{0} -s C -r 0.6 -c {1}/vmc.avg.s000.SpinDensity_u+d.xsf'.format(exe,vmc_path)
+        command = f'{exe} -s C -r 0.6 -c {vmc_path}/vmc.avg.s000.SpinDensity_u+d.xsf'
         out,err,rc = execute(command)
 
         # Assert that return code is 0
@@ -107,7 +100,7 @@ Cumulative Value of C Species at Cutoff 0.6 is: 1.06842486
 '''
 
         # DMC extrapolated non-cumulative
-        command = '{0} -s C -r 0.6 --vmc={1}/vmc.avg.s000.SpinDensity_u+d.xsf {2}/dmc.avg.s001.SpinDensity_u+d.xsf'.format(exe,vmc_path,dmc_path)
+        command = f'{exe} -s C -r 0.6 --vmc={vmc_path}/vmc.avg.s000.SpinDensity_u+d.xsf {dmc_path}/dmc.avg.s001.SpinDensity_u+d.xsf'
         out,err,rc = execute(command)
 
         # Assert that return code is 0
@@ -124,7 +117,7 @@ Non-Cumulative Value of C Species at Cutoff 0.6 is: 4.91009396
         assert(text_eq(out,out_ref))
 
         # DMC extrapolated cumulative
-        command = '{0} -s C -r 0.6 -c --vmc={1}/vmc.avg.s000.SpinDensity_u+d.xsf {2}/dmc.avg.s001.SpinDensity_u+d.xsf'.format(exe,vmc_path,dmc_path)
+        command = f'{exe} -s C -r 0.6 -c --vmc={vmc_path}/vmc.avg.s000.SpinDensity_u+d.xsf {dmc_path}/dmc.avg.s001.SpinDensity_u+d.xsf'
         out,err,rc = execute(command)
 
         # Assert that return code is 0
@@ -142,7 +135,7 @@ Cumulative Value of C Species at Cutoff 0.6 is: 1.10782675
 
 
         # DMC extrapolated cumulative with error bar
-        command = '{0} -s C -r 0.6 -c -n 3 --seed=0 --vmc={1}/vmc.avg.s000.SpinDensity_u+d.xsf --vmcerr={1}/vmc.avg.s000.SpinDensity_u+d+err.xsf --dmcerr={2}/dmc.avg.s001.SpinDensity_u+d+err.xsf {2}/dmc.avg.s001.SpinDensity_u+d.xsf'.format(exe,vmc_path,dmc_path)
+        command = f'{exe} -s C -r 0.6 -c -n 3 --seed=0 --vmc={vmc_path}/vmc.avg.s000.SpinDensity_u+d.xsf --vmcerr={vmc_path}/vmc.avg.s000.SpinDensity_u+d+err.xsf --dmcerr={dmc_path}/dmc.avg.s001.SpinDensity_u+d+err.xsf {dmc_path}/dmc.avg.s001.SpinDensity_u+d.xsf'
         out,err,rc = execute(command)
 
         # Assert that return code is 0
