c56e62f580714ad6e9d57f60d9ee5f9f79c61198
Jaron Krogel <krogeljt@ornl.gov>
2026-05-05 18:20:05 -0400

    Nexus: add path guard functions

--- nexus/nexus/utilities.py
+++ nexus/nexus/utilities.py
@@ -109,9 +109,143 @@ def sorted_py2(iterable):
 
 
 def to_str(s):
+    '''Convert a value to a string'''
     if isinstance(s,bytes):
         return str(s,encoding='utf-8')
     else:
         return str(s)
     #end if
 #end def to_str
+
+
+
+import os
+from pathlib import Path
+
+
+def relative_path(path_start,path_end):
+    '''Find the relative path between two paths'''
+    rel_path = os.path.relpath(path_end,path_start)
+    return rel_path
+#end def relative_path
+
+
+def is_relative_path(path, ret_bare=False):
+    '''Determine if a path is relative to some current working directory'''
+    if isinstance(path,Path):
+        path = str(path)
+    relative  = path.startswith('..') 
+    relative |= path.startswith('./') 
+    relative |= path=='.'
+    relative |= path==''
+    bare = False
+    if not relative:
+        bare = not path.startswith('/')
+        relative |= bare
+    elif path=='':
+        bare = True
+    if not ret_bare:
+        return relative
+    else:
+        return relative,bare
+#end def is_relative_path
+
+
+def path_object(path            ,
+                relative = None ,
+                strict   = False,
+                ret_rel  = False,
+                ):
+    """Convert a path to a ``pathlib.Path`` object.
+
+    This function should be called in a pair with ``revert_path()``.
+
+    Parameters
+    ----------
+    path : str or Path
+        A file path or directory path. 
+    relative: bool or None, default=None
+        If bool, ensure the path is relative or not.
+        If None, take no action.
+    strict : bool, default=False
+        If ``True``, require that ``path`` is a ``str``.
+    ret_rel: bool, default=False
+        If True, return whether the path is actually relative or not.
+    is_rel : bool
+        Indicates whether the path is actually relative or not.
+        Returned only when ret_rel==True.
+
+    Returns
+    -------
+    path : Path
+        The path as a ``pathlib.Path`` object.
+    """
+    is_str  = isinstance(path,str)
+    is_Path = isinstance(path,Path)
+    if strict and not is_str:
+        raise ValueError('path must be "str" type')
+    elif not (is_str or is_Path):
+        raise ValueError('path must be of type "str" or "Path"')
+    is_rel = is_relative_path(path)
+    if relative is not None:
+        if relative and not is_rel:
+            raise ValueError('path must be relative but it is non-relative')
+        elif not relative and is_rel:
+            raise ValueError('path must be non-relative but it is relative')
+    if not is_Path:
+        path = Path(path)
+    if not ret_rel:
+        return path
+    else:
+        return path,is_rel
+#end def path_object
+
+
+def path_string(path, 
+                relative = None,
+                ret_rel  = False,
+                ):
+    """Convert a path to a string.
+
+    Parameters
+    ----------
+    path : str or Path
+        A file path or directory path. 
+    relative: bool or None, default=None
+        If bool, ensure the path is relative or not.
+        If None, take no action.
+    ret_rel: bool, default=False
+        If True, return whether the path is actually relative or not.
+
+    Returns
+    -------
+    path : str
+        The path as a ``str``.
+    is_rel : bool
+        Indicates whether the path is actually relative or not.
+        Returned only when ret_rel==True.
+    """
+    is_str  = isinstance(path, str)
+    is_Path = isinstance(path, Path)
+    if not (is_str or is_Path):
+        raise ValueError('path must be of type "str" or "Path"')
+    is_rel,is_bare = is_relative_path(path, ret_bare=True)
+    if relative is not None:
+        if not isinstance(relative, bool):
+            raise ValueError('relative must be "bool" type')
+        if relative and not is_rel:
+            raise ValueError('path must be relative but it is non-relative')
+        elif not relative and is_rel:
+            raise ValueError('path must be non-relative but it is relative')
+    if is_Path:
+        path = str(path)
+    if path=='' or path=='.':
+        path = '.'
+        is_rel = True
+    elif is_rel and is_bare:
+        path = './'+path
+    if not ret_rel:
+        return path
+    else:
+        return path,is_rel
+#end def path_string
