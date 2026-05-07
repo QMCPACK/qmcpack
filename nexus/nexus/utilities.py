900b0cb71a2411652b5a18a7fb699e9489688ccf
Jaron Krogel <krogeljt@ornl.gov>
2026-05-07 02:44:14 -0400

    nexus: evict relatives

--- nexus/nexus/utilities.py
+++ nexus/nexus/utilities.py
@@ -143,27 +143,6 @@ def is_valid_path(path):
 
 
 
-def relative_path(path_start,path_end):
-    '''Find the relative path between two paths'''
-    rel_path = os.path.relpath(path_end,path_start)
-    return rel_path
-#end def relative_path
-
-
-
-def is_relative_path(path):
-    '''Determine if a path is relative to some current working directory'''
-    if isinstance(path,Path):
-        path = str(path)
-    if not isinstance(path,str):
-        raise ValueError('path must be of type Path or str')
-    absolute = len(path)>0 and (path[0]=='/' or path[0]=='~')
-    relative = not absolute
-    return relative
-#end def is_relative_path
-
-
-
 def path_string(path,check=True):
     """Convert a path to a string.
 
