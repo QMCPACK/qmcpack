ac234321977d006c232e1984703b61d4e343e6be
Brock Dyer <brockdyer03@gmail.com>
2026-06-25 00:15:17 -0400

    Nexus: Replace old warning system with Python's `warnings` library

--- nexus/nexus/generic.py
+++ nexus/nexus/generic.py
@@ -30,6 +30,7 @@
 #====================================================================#
 
 
+import os
 import sys
 import traceback
 from typing import TextIO
@@ -38,9 +39,13 @@ import pickle
 from pickle import UnpicklingError
 from random import randint
 from pathlib import Path
+import warnings
+from typing import TypeAlias
+import functools
 
 from .utilities import sorted_py2, path_string
 
+VersionStr: TypeAlias = str
 
 class generic_settings:
     devlog         = sys.stdout
@@ -51,6 +56,42 @@ class generic_settings:
 class NexusError(Exception):
     None
 #end class NexusError
+
+
+class NexusDevWarning(Warning):
+    def __init__(self, msg: str, indent: str = "    ", cls: str | None = None):
+        self.message = msg
+        self.indent  = indent
+        self.cls     = cls
+#end class NexusDevWarning
+
+
+class NexusUserWarning(NexusDevWarning):
+    None
+#end class NexusUserWarning
+
+
+# Hook for replacing `warnings.showwarning`
+def __nexus_showwarning(message, category, filename, lineno, file=None, line=None):
+    if file is None:
+        file = generic_settings.devlog
+
+    if isinstance(message, DeprecationWarning):
+        indent, msg = str(message).split(";", maxsplit=1)
+        cls = ""
+    elif isinstance(message, NexusDevWarning | NexusUserWarning):
+        msg    = message.message
+        indent = message.indent
+        cls    = f"{message.cls}:" if message.cls is not None else ""
+
+    if os.path.exists(filename):
+        # Printing the whole file path can be very confusing, so we just
+        # print the file name and its parent directory.
+        filename = os.path.relpath(filename, os.path.dirname(filename+"/../../"))
+
+    file.write(f"{indent}{filename}:{lineno}:{cls} {category.__name__}: {msg}\n")
+
+warnings.showwarning = __nexus_showwarning
 
 
 exit_call = sys.exit
@@ -128,13 +169,100 @@ def message(msg,header=None,post_header=' message:',indent='    ',logfile=None):
 #end def message
 
 
-def warn(msg,header=None,indent='    ',logfile=None):
-    if logfile is None:
-        logfile = generic_settings.devlog
-    #end if
-    post_header=' warning:'
-    message(msg,header,post_header,indent,logfile)
+def warn(
+    msg      : str,
+    indent   : str        = "    ",
+    warn_type: str        = "user",
+    cls      : str | None = None,
+    ) -> None:
+    """Report a warning.
+
+    Parameters
+    ----------
+    msg : str
+        The message for the warning.
+    indent : str. default="    "
+        The indentation for the warning message.
+    warn_type : {"user", "dev", "deprecate", "class"}
+        See the notes for a description of when you should use each
+        warning type.
+    cls : str, optional
+        Meant to be passed through ``self.warn()`` in a class that
+        derives from ``object_interface``.
+
+    Notes
+    -----
+    If you are a developer who wants to use this function but are unsure
+    of what kind of warning type to use, here are some basic guidelines
+    for choosing:
+
+    - If the function you're putting a warning in will be directly
+      called by a user, you should use ``"user"``, as this will show
+      them where in their own script they've called the function.
+    - If the function you're putting a warning in will be indirectly
+      called by a function that a user has called, **and** if the
+      problem is due to something the user has done you should use the
+      class's ``self.warn()`` functionality if it inherits from
+      ``DevBase``, otherwise use the warning type ``"class"`` and pass
+      in the name of the class and/or function you're calling from.
+    - If the function is called indirectly by a user and it is not
+      necessarily their fault that something has gone awry, you should
+      use ``"dev"``. This should likely only be used in places where
+      you are *mostly* sure there won't be any problems but the code
+      has not been thoroughly tested.
+    - If you are deprecating a function, use the ``@nxs_deprecate``
+      decorator and supply the version and the replacement that a user
+      should use instead of the soon-to-be-removed function.
+    """
+    match warn_type:
+        case "dev":
+            msg = NexusDevWarning(msg, indent, cls)
+            warnings.warn(msg, stacklevel=2)
+        case "user":
+            msg = NexusUserWarning(msg, indent, cls)
+            # len(traceback.format_stack()) brings you all the way back
+            # to the user file no matter where the warning was raised.
+            warnings.warn(msg, stacklevel=len(traceback.format_stack()))
+        case "deprecate":
+            msg = DeprecationWarning(f"{indent};{msg}")
+            warnings.warn(msg, stacklevel=3)
+        case "class":
+            msg = NexusUserWarning(msg, indent, cls)
+            warnings.warn(msg, stacklevel=3)
 #end def warn
+
+
+def nxs_deprecate(
+    since: VersionStr,
+    replacement: str,
+    indent: str = "    "
+    ):
+    """Decorator for signaling the deprecation of a Nexus function.
+
+    This should itself be deprecated when the minimum Python version is
+    3.13, when the official ``warnings.deprecated()`` decorator is added.
+
+    Parameters
+    ----------
+    since : Version
+        The version of Nexus when the function was deprecated.
+    replacement : str
+        The replacement function for the function being deprecated.
+    indent : str, default = "    "
+        Indentation to go before the warning.
+    """
+    def inner(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            warn_msg = f"{f.__qualname__} is deprecated as of Nexus version {since}, and will be removed in a future update."
+            if replacement is not None:
+                warn_msg += f" Please use {replacement} instead."
+
+            warn(msg=warn_msg, indent=indent, warn_type="deprecate")
+            return f(*args, **kwargs)
+        return wrapper
+    return inner
+#end def nxs_deprecate
 
 
 def error(
@@ -561,11 +689,12 @@ class object_interface(object):
         log(*items,**kwargs)
     #end def log
 
-    def warn(self,message,header=None):
-        if header is None:
-            header=self.__class__.__name__
-        #end if
-        warn(message,header,logfile=self._logfile)
+    def warn(self, msg: str, indent: str = "    "):
+        """Warning from inside a Nexus class.
+
+        See ``generic.warn()`` for description.
+        """
+        warn(msg, indent, warn_type="class", cls=type(self).__qualname__)
     #end def warn
 
     def error(self,message,header=None,exit=True,trace=-2):
