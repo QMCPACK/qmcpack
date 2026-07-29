5b6ba9f0f8ac2476c45e95b992d1b41c20563f84
Peter Doak <doakpw@ornl.gov>
2026-07-29 18:02:29 +0000

    Add standard headers to DeepQMC source files

--- src/QMCWaveFunctions/DeepQMC/DeepQMCWaveFunctionBuilder.cpp
+++ src/QMCWaveFunctions/DeepQMC/DeepQMCWaveFunctionBuilder.cpp
@@ -1,6 +1,12 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
+//
+// Copyright (c) 2026 QMCPACK developers.
+//
+// File developed by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+//
+// File created by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
 
 #include "QMCWaveFunctions/DeepQMC/DeepQMCWaveFunctionBuilder.h"
--- src/QMCWaveFunctions/DeepQMC/deepqmc_infer_bridge.py
+++ src/QMCWaveFunctions/DeepQMC/deepqmc_infer_bridge.py
@@ -1,5 +1,13 @@
-# This file is distributed under the University of Illinois/NCSA Open Source License.
-# See LICENSE file in top directory for details.
+######################################################################################
+## This file is distributed under the University of Illinois/NCSA Open Source License.
+## See LICENSE file in top directory for details.
+##
+## Copyright (c) 2026 QMCPACK developers.
+##
+## File developed by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+##
+## File created by: Peter Doak, doakpw@ornl.gov, Oak Ridge National Laboratory
+#######################################################################################
 
 """Python side of QMCPACK's experimental DeepQMC inference bridge.
 
