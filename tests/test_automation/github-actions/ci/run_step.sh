36c9bb503a5776a0baac1d16af115cdc5169b155
Paul Kent <kentpr@ornl.gov>
2026-08-07 09:41:43 -0400

    Fix quotes

--- tests/test_automation/github-actions/ci/run_step.sh
+++ tests/test_automation/github-actions/ci/run_step.sh
@@ -141,7 +141,7 @@ case "$1" in
               # -DCMAKE_EXE_LINKER_FLAGS="-Wl,-ld_classic" used with gcc-14, macos-14
       ;;
       *"macOS-AppleClang"*"-Real"*)
-        echo 'Configure for building on macOS using Apple's clang compiler"
+        echo "Configure for building on macOS using Apple's clang compiler"
         cmake -GNinja $CMAKE_OPTIONS \
               -DCMAKE_C_COMPILER=clang \
               -DCMAKE_CXX_COMPILER=clang++ \
