b78b676c49d2e41e7dad6ecdacaf129de3d8d840
Brock Dyer <brockdyer03@gmail.com>
2026-08-24 17:05:37 -0400

    Nexus: Switch `error()` calls to `raise` statements in several other files

--- nexus/nexus/rmg.py
+++ nexus/nexus/rmg.py
@@ -27,13 +27,15 @@ class Rmg(Simulation):
 
     def get_result(self,result_name,sim):
         result = None
-        self.error('Ability to get result '+result_name+' has not been implemented.')
+        msg = 'Ability to get result '+result_name+' has not been implemented.'
+        raise NotImplementedError(msg)
         return result
     #end def get_result
 
 
     def incorporate_result(self,result_name,result,sim):
-        self.error('ability to incorporate result '+result_name+' has not been implemented')
+        msg = 'ability to incorporate result '+result_name+' has not been implemented'
+        raise NotImplementedError(msg)
     #end def incorporate_result
 
 
