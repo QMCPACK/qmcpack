496ab3ef94e99af8c3741cf1fb4ebf92ea03d435
Alfredo Correa <alfredo.correa@gmail.com>
2026-08-24 16:45:19 -0700

    Remove old boost-multi subtree

--- external_codes/boost_multi/multi/test_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/multi/array.hpp>
-#include <cassert>
-
-auto main() -> int {
-    namespace multi = boost::multi;
-    multi::array<double, 2> A({3, 3});
-    A[1][1] = 42.0;
-    assert(A[1][1] == 42.0);
-}
