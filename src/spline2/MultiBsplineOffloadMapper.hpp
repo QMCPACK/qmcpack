849023025324bc34d6854eeddc75e93da2c0eee9
Ye Luo <yeluo@anl.gov>
2026-09-15 19:42:56 -0500

    Split MultiBsplineOffloadMapper into a base class and a derived one.

--- src/spline2/MultiBsplineOffloadMapper.hpp
+++ src/spline2/MultiBsplineOffloadMapper.hpp
@@ -13,23 +13,16 @@
 #ifndef QMCPLUSPLUS_MULTIEINSPLINEOFFLOADMAPPER_HPP
 #define QMCPLUSPLUS_MULTIEINSPLINEOFFLOADMAPPER_HPP
 
-#include "MultiBsplineBase.hpp"
-#include <vector>
+#include "MultiBsplineOffloadMapperBase.hpp"
 
 namespace qmcplusplus
 {
-/** A mapper class to map host spline coeficients to devices and handle multi-walker evaluation.
- * @tparam T the precision of splines
- */
 template<typename T>
-class MultiBsplineOffloadMapper
+class MultiBsplineOffloadMapper : public MultiBsplineOffloadMapperBase<T>
 {
   using HostBspline = MultiBsplineBase<T>;
-
-  /// reference to a host spline object.
-  const HostBspline& host_bsplines_;
-  /// array of host coefficient pointers for all the blocks.
-  std::vector<const T*> block_coefs_;
+  using MultiBsplineOffloadMapperBase<T>::host_bsplines_;
+  using MultiBsplineOffloadMapperBase<T>::block_coefs_;
 
   /// map host coefficients to devices
   void mapToDevice();
@@ -38,35 +31,6 @@ public:
   MultiBsplineOffloadMapper(const HostBspline& host_bsplines);
 
   ~MultiBsplineOffloadMapper();
-
-  /// update device coeficients
-  void updateToDevice();
-
-  /** evaluate spline values
-   * @param num_pos, number of electron positions
-   * @param pos_arr, array of electron positions [num_pos, pos_stride]
-   * @param pos_stride, stride between two positions [num_pos, pos_stride]
-   * @param spline_v, result pointer
-   * @param walker_stride, result distance between two positions
-   */
-  void mw_evaluate_v(int num_pos, T* pos_arr, int pos_stride, T* spline_v, size_t walker_stride);
-  /** evaluate spline value, gradients and hessian.
-   * @param num_pos, number of electron positions
-   * @param pos_arr, array of electron positions [num_pos, pos_stride]
-   * @param pos_stride, stride between two positions [num_pos, pos_stride]
-   * @param spline_vgh, result pointer
-   * @param walker_stride, result distance between two positions
-   * @param field_stride, result distance of value, gradients and hessian fields for a given electron position.
-   * The layout of spline_vgh is described by walker_stride and field_stride. For example,
-   * [nw, nf, nb], walker_stride = na * nb, field_stride = nb
-   * [nf, nw, nb], walker_stride = nb, field_stride = nw * nb
-   */
-  void mw_evaluate_vgh(int num_pos,
-                       T* pos_arr,
-                       int pos_stride,
-                       T* spline_vgh,
-                       size_t walker_stride,
-                       size_t field_stride);
 };
 
 extern template class MultiBsplineOffloadMapper<float>;
