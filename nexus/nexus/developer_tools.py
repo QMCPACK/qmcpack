e6a9c3f596f22c7aa561369cbf41a39b544040cb
Brock Dyer <brockdyer03@gmail.com>
2026-09-17 17:09:54 -0400

    Nexus: Just delete the logger instead of modifying `to_obj`

--- nexus/nexus/developer_tools.py
+++ nexus/nexus/developer_tools.py
@@ -380,7 +380,7 @@ class DevBase:
 def to_obj(d):
     o = obj()
     for k,v in d.items():
-        if hasattr(v,'__dict__') and k != "logger":
+        if hasattr(v,'__dict__'):
             o[k] = to_obj(v)
         else:
             o[k] = v
