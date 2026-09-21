bf1b8316026e2eb59591be6fa62d971d0d529927
Ye Luo <yeluo@anl.gov>
2026-09-21 17:54:52 -0500

    Add template type for allocators.

--- src/Message/container_proxy.h
+++ src/Message/container_proxy.h
@@ -2,11 +2,12 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2026 QMCPACK developers.
 //
 // File developed by: Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
 //                    Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+//                    Ye Luo, yeluo@anl.gov, Argonne National Laboratory
 //
 // File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
 //////////////////////////////////////////////////////////////////////////////////////
@@ -33,7 +34,7 @@ struct scalar_traits
   {
     DIM = 1
   };
-  using real_type  = T;
+  using real_type = T;
   static inline T* get_address(T* a) { return a; }
 };
 
@@ -44,7 +45,7 @@ struct scalar_traits<std::complex<T>>
   {
     DIM = 2
   };
-  using real_type  = T;
+  using real_type = T;
   static inline T* get_address(std::complex<T>* a) { return reinterpret_cast<T*>(a); }
 };
 
@@ -55,7 +56,7 @@ struct scalar_traits<TinyVector<T, D>>
   {
     DIM = scalar_traits<T>::DIM * D
   };
-  using real_type  = typename scalar_traits<T>::real_type;
+  using real_type = typename scalar_traits<T>::real_type;
   static inline real_type* get_address(TinyVector<T, D>* a) { return scalar_traits<T>::get_address(a->data()); }
 };
 
@@ -66,7 +67,7 @@ struct scalar_traits<Tensor<T, D>>
   {
     DIM = scalar_traits<T>::DIM * D * D
   };
-  using real_type  = typename scalar_traits<T>::real_type;
+  using real_type = typename scalar_traits<T>::real_type;
   static inline real_type* get_address(Tensor<T, D>* a) { return scalar_traits<T>::get_address(a->data()); }
 };
 
@@ -85,31 +86,31 @@ struct container_proxy
   inline pointer data() { return scalar_traits<T>::get_address(&ref); }
 };
 
-template<typename T>
-struct container_proxy<std::vector<T>>
+template<typename T, typename Alloc>
+struct container_proxy<std::vector<T, Alloc>>
 {
   enum
   {
     DIM = scalar_traits<T>::DIM
   };
   using pointer = typename container_proxy<T>::pointer;
-  std::vector<T>& ref;
-  inline container_proxy(std::vector<T>& a) : ref(a) {}
+  std::vector<T, Alloc>& ref;
+  inline container_proxy(std::vector<T, Alloc>& a) : ref(a) {}
   inline size_t size() const { return ref.size() * DIM; }
   inline pointer data() { return scalar_traits<T>::get_address(ref.data()); }
 };
 
-template<>
-struct container_proxy<std::vector<bool>>
+template<typename Alloc>
+struct container_proxy<std::vector<bool, Alloc>>
 {
   enum
   {
     DIM = 1
   };
   using pointer = int*;
-  std::vector<bool>& ref;
+  std::vector<bool, Alloc>& ref;
   std::vector<int> my_copy;
-  inline container_proxy(std::vector<bool>& a) : ref(a)
+  inline container_proxy(std::vector<bool, Alloc>& a) : ref(a)
   {
     my_copy.resize(a.size());
     copy(a.begin(), a.end(), my_copy.begin());
@@ -133,46 +134,44 @@ struct container_proxy<PooledData<T>>
   inline pointer data() { return scalar_traits<T>::get_address(ref.data()); }
 };
 
-template<typename T>
-struct container_proxy<Vector<T>>
+template<typename T, typename Alloc>
+struct container_proxy<Vector<T, Alloc>>
 {
   enum
   {
     DIM = scalar_traits<T>::DIM
   };
   using pointer = typename container_proxy<T>::pointer;
-  Vector<T>& ref;
-  inline container_proxy(Vector<T>& a) : ref(a) {}
+  Vector<T, Alloc>& ref;
+  inline container_proxy(Vector<T, Alloc>& a) : ref(a) {}
   inline size_t size() const { return ref.size() * DIM; }
   inline pointer data() { return scalar_traits<T>::get_address(ref.data()); }
-
 };
 
-template<typename T>
-struct container_proxy<Matrix<T>>
+template<typename T, typename Alloc>
+struct container_proxy<Matrix<T, Alloc>>
 {
   enum
   {
     DIM = scalar_traits<T>::DIM
   };
   using pointer = typename container_proxy<T>::pointer;
-  Matrix<T>& ref;
-  inline container_proxy(Matrix<T>& a) : ref(a) {}
+  Matrix<T, Alloc>& ref;
+  inline container_proxy(Matrix<T, Alloc>& a) : ref(a) {}
   inline size_t size() const { return ref.size() * DIM; }
   inline pointer data() { return scalar_traits<T>::get_address(ref.data()); }
-
 };
 
-template<typename T, unsigned D>
-struct container_proxy<Array<T, D>>
+template<typename T, unsigned D, typename ALLOC>
+struct container_proxy<Array<T, D, ALLOC>>
 {
   enum
   {
     DIM = scalar_traits<T>::DIM
   };
   using pointer = typename container_proxy<T>::pointer;
-  Array<T, D>& ref;
-  inline container_proxy(Array<T, D>& a) : ref(a) {}
+  Array<T, D, ALLOC>& ref;
+  inline container_proxy(Array<T, D, ALLOC>& a) : ref(a) {}
   inline size_t size() const { return ref.size() * DIM; }
   inline pointer data() { return scalar_traits<T>::get_address(ref.data()); }
 };
